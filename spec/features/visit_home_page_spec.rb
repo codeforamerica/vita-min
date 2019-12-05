c8dc3a2cbc7b82aa86ae8f8fa837fa7f1436b9af
Ben Golder <bgolder@codeforamerica.org>
2019-12-05 09:16:38 -0800

    Add splash page with info about VitaTaxHelp.org
    Co-authored-by: Jenny Heath <jheath@codeforamerica.org>

--- spec/features/visit_home_page_spec.rb
+++ spec/features/visit_home_page_spec.rb
@@ -1,8 +1,10 @@
 require "rails_helper"
 
 RSpec.feature "Visit home page" do
-  scenario "default visit" do
+  scenario "has most critical content" do
     visit "/"
-    expect(page).to have_text "Find VITA help near you!"
+    expect(page).to have_text "Free tax help from IRS-certified volunteers."
+    expect(page).to have_text "Find a local VITA site near you and get more of your refund dollars back by filing for free"
+    expect(page).to have_link "Find a location"
   end
 end
