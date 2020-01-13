713c957b17d3ef64448a7b270bbca9342da6a45e
Ben Golder <bgolder@codeforamerica.org>
2020-01-13 14:36:53 -0800

    Deploy to demo and staging from circle
    Signed-off-by: Jenny Heath <jheath@codeforamerica.org>

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,4 +1,5 @@
 Raven.configure do |config|
+  config.dsn = Rails.application.credentials.dig(Rails.env.to_sym, :sentry_dsn)
   config.environments = %w(production staging demo)
   config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
 end
