4ad0c1aacd1d4fbf7eef4521f584ccbba405853d
Ben Golder <bgolder@codeforamerica.org>
2020-01-14 13:46:13 -0800

    Create visitor ids and store in cookies
    Co-authored-by: Jenny Heath <jheath@codeforamerica.org>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,17 @@
 class ApplicationController < ActionController::Base
+  before_action :set_visitor_id
   helper_method :include_google_analytics?
 
   def include_google_analytics?
     false
+  end
+
+  def set_visitor_id
+    return if cookies[:visitor_id].present?
+    cookies.permanent[:visitor_id] = SecureRandom.hex(26)
+  end
+
+  def visitor_id
+    cookies[:visitor_id]
   end
 end
--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -1,9 +1,36 @@
 require "rails_helper"
 
 RSpec.describe ApplicationController do
+  controller do
+    def index
+      head :ok
+    end
+  end
+
   describe "#include_google_analytics?" do
     it "returns false" do
       expect(subject.include_google_analytics?).to eq false
+    end
+  end
+
+  describe "#set_visitor_id" do
+    context "existing visitor id" do
+      before do
+        cookies[:visitor_id] = "123"
+      end
+
+      it "retains the existing visitor id" do
+        get :index
+        expect(cookies[:visitor_id]).to eq "123"
+      end
+    end
+
+    context "no visitor id" do
+      it "generates and sets a visitor id cookie" do
+        get :index
+        expect(cookies[:visitor_id]).to be_a String
+        expect(cookies[:visitor_id]).to be_present
+      end
     end
   end
 end
