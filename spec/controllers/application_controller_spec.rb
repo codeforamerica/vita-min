3fd1997520000edab26c3ed179ab47ac5b03130c
Ben Golder <bgolder@codeforamerica.org>
2020-01-14 14:52:48 -0800

    Include default data with mixpanel events
    Co-authored-by: Jenny Heath <jheath@codeforamerica.org>

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -1,6 +1,8 @@
 require "rails_helper"
 
 RSpec.describe ApplicationController do
+  let(:user_agent_string) { "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.360" }
+
   controller do
     def index
       head :ok
@@ -36,7 +38,7 @@ RSpec.describe ApplicationController do
 
   describe "#user_agent" do
     it "parses the user agent" do
-      request.headers["HTTP_USER_AGENT"] = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36"
+      request.headers["HTTP_USER_AGENT"] = user_agent_string
 
       get :index
 
@@ -44,8 +46,49 @@ RSpec.describe ApplicationController do
     end
   end
 
-  xdescribe "#send_mixpanel_event" do
-    it "sends default data to mixpanel" do
+  describe "#send_mixpanel_event" do
+    let(:mixpanel_spy) { spy(MixpanelService) }
+    let(:expected_data) do
+      {
+        sound: "boop",
+        full_user_agent: user_agent_string,
+        browser_name: "Chrome",
+        browser_full_version: "79.0.3945.117",
+        browser_major_version: "79",
+        os_name: "Mac",
+        os_full_version: "10.15.2",
+        os_major_version: "10",
+        is_bot: false,
+        bot_name: nil,
+        device_brand: nil,
+        device_name: nil,
+        device_type: "desktop",
+        device_browser_version: "Mac desktop Chrome 79",
+        locale: "en",
+        path: "/anonymous",
+        full_path: "/anonymous",
+        controller_name: "Anonymous",
+        controller_action: "AnonymousController#index",
+        controller_action_name: "index",
+      }
+    end
+
+    before do
+      allow(MixpanelService).to receive(:instance).and_return(mixpanel_spy)
+      cookies[:visitor_id] = "123"
+      request.headers["HTTP_USER_AGENT"] = user_agent_string
+    end
+
+    it "sends default data using mixpanel service" do
+      get :index
+
+      subject.send_mixpanel_event(event_name: "beep", data: { sound: "boop" })
+
+      expect(mixpanel_spy).to have_received(:run).with(
+        unique_id: "123",
+        event_name: "beep",
+        data: expected_data
+      )
     end
   end
 end
