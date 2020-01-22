2b7df21054ac511e9819fc996e092c2315516541
Jenny Heath <jheath@codeforamerica.org>
2020-01-22 15:37:04 -0800

    Create users and add devise/omniauth-oauth2 gems
    Co-authored-by: Ben Golder <bgolder@codeforamerica.org>

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+    end
+  end
+end
