43f3828fe8340ce481873185c3f69963a1457677
Ben Golder <bgolder@codeforamerica.org>
2020-01-23 16:38:25 -0800

    Annotate User models
    Co-authored-by: Jenny Heath <jheath@codeforamerica.org>

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :bigint           not null, primary key
+#  email      :string
+#  provider   :string
+#  uid        :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class User < ApplicationRecord
   devise :omniauthable, omniauth_providers: [:idme]
 
