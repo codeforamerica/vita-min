641f81d4ab76fa679eddaf7326f6c5f696a7866d
Ben Golder <bgolder@codeforamerica.org>
2020-01-23 17:37:14 -0800

    Connect ID.me OAuth
    Co-authored-by: Jenny Heath <jheath@codeforamerica.org>

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -3,11 +3,13 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     @user = User.from_omniauth(request.env["omniauth.auth"])
     if @user.persisted?
       flash[:notice] = "You're signed in!"
-      sign_in_and_redirect @user, event: :authentication
+      sign_in @user, event: :authentication
+      redirect_to overview_questions_path
     else
       @user.save
       flash[:notice] = "Thank you for verifying your identity!"
-      sign_in_and_redirect @user, event: :authentication
+      sign_in @user, event: :authentication
+      redirect_to overview_questions_path
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
 
   devise_scope :user do
-    get "sign_in", :to => "devise/sessions#new", :as => :new_user_session
-    get "sign_out", :to => "devise/sessions#destroy", :as => :destroy_user_session
+    get "sign_in", :to => "devise/sessions#new", as: :new_user_session
+    get "sign_out", :to => "devise/sessions#destroy", as: :destroy_user_session
   end
 
   mount Cfa::Styleguide::Engine => "/cfa"
