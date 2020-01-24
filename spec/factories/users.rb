e9d89e59c69ec01168f1496ae1695539381b0671
Ben Golder <bgolder@codeforamerica.org>
2020-01-23 16:38:25 -0800

    Annotate User models
    Co-authored-by: Jenny Heath <jheath@codeforamerica.org>

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :bigint           not null, primary key
+#  email      :string
+#  provider   :string
+#  uid        :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 FactoryBot.define do
   factory :user do
     provider { "idme" }
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :bigint           not null, primary key
+#  email      :string
+#  provider   :string
+#  uid        :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require "rails_helper"
 
 RSpec.describe User, type: :model do
