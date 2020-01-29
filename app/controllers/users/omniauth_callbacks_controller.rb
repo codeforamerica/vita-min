afecc9cf5ee4c3ef222fff54357da6a42f043442
Ben Golder <bgolder@codeforamerica.org>
2020-01-29 09:29:01 -0800

    Refactor Omniauth Callbacks Controller
    Co-authored-by: Tom Dooner <tdooner@codeforamerica.org>
    Co-authored-by: Jenny Heath <jheath@codeforamerica.org>

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,16 +2,13 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def idme
     @user = User.from_omniauth(request.env["omniauth.auth"])
 
-    if @user.persisted?
-      # returning user
-      sign_in @user, event: :authentication
-      redirect_to overview_questions_path
-    else
+    unless @user.persisted?
       # new user
       @user.intake = Intake.create
       @user.save
-      sign_in @user, event: :authentication
-      redirect_to overview_questions_path
     end
+
+    sign_in @user, event: :authentication
+    redirect_to overview_questions_path
   end
 end
