3b7404a450c1219f580ce50e0dc57ada0353c07b
Jenny Heath <jheath@codeforamerica.org>
2020-01-30 14:50:09 -0800

    Add trackable to devise, use sign_in_count to identify new/returning users

--- spec/controllers/users/omniauth_callbacks_controller_spec.rb
+++ spec/controllers/users/omniauth_callbacks_controller_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe Users::OmniauthCallbacksController do
 
     context "when a user successfully authenticates through ID.me" do
       context "with a returning ID.me user" do
-        let(:user) { create :user }
+        let(:user) { create :user, sign_in_count: 1 }
 
         it "signs the user in and redirects them to the overview page and sets success flash message" do
           get :idme
@@ -21,9 +21,14 @@ RSpec.describe Users::OmniauthCallbacksController do
         end
 
         it "does not create a new Intake" do
-          expect{
+          expect {
             get :idme
           }.not_to change(Intake, :count)
+        end
+
+        it "increments user sign_in_count by 1" do
+          get :idme
+          expect(user.sign_in_count).to eq 2
         end
       end
 
@@ -35,7 +40,7 @@ RSpec.describe Users::OmniauthCallbacksController do
             get :idme
           }.to change(User, :count).by(1)
           expect(subject.current_user).to eq user.reload
-          expect(response).to redirect_to(overview_questions_path(is_new_user: true))
+          expect(response).to redirect_to(overview_questions_path)
         end
 
         it "creates a new intake and links the user to it" do
@@ -46,6 +51,11 @@ RSpec.describe Users::OmniauthCallbacksController do
           intake = Intake.last
           expect(subject.current_intake).to eq intake
           expect(subject.current_intake).to eq user.reload.intake
+        end
+
+        it "increments user sign_in_count to 1" do
+          get :idme
+          expect(user.sign_in_count).to eq(1)
         end
       end
     end
