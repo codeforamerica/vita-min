31450702014947ec8f860b27d08f14b232f44064
Ben Golder <bgolder@codeforamerica.org>
2020-02-11 12:26:25 -0800

    Don't request consent redundantly
    Only show consent page if the user has not already consented.
    
    Adds a new fork in the omniauth callbacks controller for returning
    users that have consented already (who are redirected to the welcome
    page) and returning users who have not yet consented (who are redirected
    to the consent page).

--- spec/features/web_intake/returning_filer_spec.rb
+++ spec/features/web_intake/returning_filer_spec.rb
@@ -1,11 +1,30 @@
 require "rails_helper"
 
 RSpec.feature "Returning user to online intake" do
+  before do
+    # sign in and consent, then sign out
+    allow_any_instance_of(Users::SessionsController).to receive(:idme_request).and_return(
+      user_idme_omniauth_callback_path(logout: "success")
+    )
+    visit "/questions/identity"
+    click_on "Sign in with ID.me"
+    check "I agree"
+    click_on "Continue"
+
+    OmniAuth.config.mock_auth[:idme] = :invalid_credentials
+    silence_omniauth_logging do
+      click_on "Sign out"
+    end
+  end
+
   scenario "client tries to return to a sign in protected page after being signed out" do
-    visit "/questions/wages"
+    silence_omniauth_logging do
+      visit "/questions/wages"
+    end
     expect(page).to have_selector("h1", text: "Sign in")
+    OmniAuth.config.mock_auth[:idme] = omniauth_idme_success
     click_on "Sign in with ID.me"
 
-    expect(page).to have_selector("h1", text: "Great! Here's our terms of service.")
+    expect(page).to have_selector("h1", text: "Welcome back Gary!")
   end
 end
