ef65604c72925c45a10324a9ce8f8ceb586818f9
Ben Golder <bgolder@codeforamerica.org>
2020-02-11 14:16:19 -0800

    Ask preferred times for interview call
    Co-authored-by: Jenny Heath <jheath@codeforamerica.org>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_08_210650) do
+ActiveRecord::Schema.define(version: 2020_02_11_213645) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -141,6 +141,7 @@ ActiveRecord::Schema.define(version: 2020_02_08_210650) do
     t.string "zip_code"
     t.bigint "intake_ticket_requester_id"
     t.bigint "intake_ticket_id"
+    t.string "interview_timing_preference"
   end
 
   create_table "users", force: :cascade do |t|
