44641c264995c264ba24caa3ba44d9828b9031cc
Tom Dooner <tdooner@codeforamerica.org>
2020-02-10 21:30:06 -0800

    Add styled Document Overview page
    This commit adds a Document Overview page that looks roughly like the
    intended design. A subsequent commit will add the functionality for the
    "Edit" buttons.
    
    A couple outstanding design questions remain:
    1. What is the destination for the "I have another document to upload".
    Right now, it goes all the way back to W-2's upload -- is that right?
    2. Do we want to adjust the design given the appearance of the existing
    classes in the app? (Edit button is bigger than expected, button--wide
    is not wide enough)
    3. Copy is inconsistent. Do users "upload", "share", or "file"
    documents.

---
+++ spec/controllers/questions/documents_overview_controller_spec.rb
@@ -0,0 +1,44 @@
+require "rails_helper"
+
+RSpec.describe Questions::DocumentsOverviewController do
+  render_views
+
+  let(:intake) { create :intake }
+
+  before do
+    allow(subject).to receive(:user_signed_in?).and_return(true)
+    allow(subject).to receive(:current_intake).and_return intake
+  end
+
+  describe "#edit" do
+    before do
+      intake.documents = documents
+      intake.save
+    end
+
+    context "with no document uploads" do
+      let(:documents) { [] }
+
+      it "displays an empty message" do
+        get :edit
+        expect(response.body).to include("No documents were uploaded.")
+      end
+    end
+
+    context "with documents that have been uploaded" do
+      let(:documents) do
+        [
+          create(:document, :with_upload, intake: intake, document_type: "W-2"),
+          create(:document, :with_upload, intake: intake, document_type: "Other"),
+        ]
+      end
+
+      it "includes the sections" do
+        get :edit
+
+        expect(response.body).to include("W-2")
+        expect(response.body).to include(documents[0].upload.filename.to_s)
+      end
+    end
+  end
+end
