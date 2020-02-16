8af8554494ca4c0c85807537e38d1060c5384257
Ben Golder <bgolder@codeforamerica.org>
2020-02-15 23:17:51 -0800

    Refactor document controllers to use the same form

--- app/controllers/documents/document_upload_question_controller.rb
+++ app/controllers/documents/document_upload_question_controller.rb
@@ -2,17 +2,19 @@ module Documents
   class DocumentUploadQuestionController < Questions::QuestionsController
     layout "document_upload"
 
+    delegate :document_type, to: :class
+
     def section_title
       "Documents"
     end
 
     def edit
       @documents = current_intake.documents.of_type(document_type)
-      super
+      @form = form_class.new(document_type, current_intake, form_params)
     end
 
     def update
-      @form = form_class.new(current_intake, form_params)
+      @form = form_class.new(document_type, current_intake, form_params)
       if @form.valid?
         form_saved = @form.save
         after_update_success
@@ -23,12 +25,18 @@ module Documents
 
     private
 
-    delegate :document_type, to: :class
+
+    def form_name
+      "document_type_upload_form"
+    end
+
+    def self.form_class
+      DocumentTypeUploadForm
+    end
 
     def self.document_type
       DocumentNavigation.document_type(self)
     end
-
 
     def next_path(params = {})
       next_step = form_navigation.next
