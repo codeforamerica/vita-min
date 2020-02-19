2f1a5cd76c26abcf3cc8d7a86e82860a3d45d044
Jenny Heath <jheath@codeforamerica.org>
2020-02-19 10:10:25 -0800

    Add spouse race and ethnicity questions

---
+++ app/controllers/questions/demographic_spouse_ethnicity_controller.rb
@@ -0,0 +1,11 @@
+module Questions
+  class DemographicSpouseEthnicityController < QuestionsController
+    layout "question"
+
+    def self.show?(intake)
+      intake.demographic_questions_opt_in_yes? && intake.filing_joint_yes?
+    end
+
+    def illustration_path; end
+  end
+end
---
+++ app/forms/demographic_spouse_race_form.rb
@@ -0,0 +1,19 @@
+class DemographicSpouseRaceForm < QuestionsForm
+  set_attributes_for :intake,
+                     :demographic_spouse_american_indian_alaska_native,
+                     :demographic_spouse_black_africa_american,
+                     :demographic_spouse_native_hawaiian_pacific_islander,
+                     :demographic_spouse_asian,
+                     :demographic_spouse_white,
+                     :demographic_spouse_prefer_not_to_answer_race
+  def save
+    attributes = attributes_for(:intake)
+    if attributes[:demographic_primary_prefer_not_to_answer_race] == "true"
+      attributes.keys.map do |attribute_name|
+        attributes[attribute_name] = "0" unless attribute_name == :demographic_spouse_prefer_not_to_answer_race
+        hash
+      end
+    end
+    @intake.update(attributes)
+  end
+end
