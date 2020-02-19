0547217b2b35a865d36cb97e700eb7bcec1e700c
Jenny Heath <jheath@codeforamerica.org>
2020-02-19 13:35:28 -0800

    Fix spelling of african american

--- db/migrate/20200218220540_add_demographic_questions_to_intake.rb
+++ db/migrate/20200218220540_add_demographic_questions_to_intake.rb
@@ -7,14 +7,14 @@ class AddDemographicQuestionsToIntake < ActiveRecord::Migration[5.2]
     add_column :intakes, :demographic_veteran, :integer, default: 0, null: false
 
     add_column :intakes, :demographic_primary_american_indian_alaska_native, :boolean
-    add_column :intakes, :demographic_primary_black_africa_american, :boolean
+    add_column :intakes, :demographic_primary_black_african_american, :boolean
     add_column :intakes, :demographic_primary_native_hawaiian_pacific_islander, :boolean
     add_column :intakes, :demographic_primary_asian, :boolean
     add_column :intakes, :demographic_primary_white, :boolean
     add_column :intakes, :demographic_primary_prefer_not_to_answer_race, :boolean
 
     add_column :intakes, :demographic_spouse_american_indian_alaska_native, :boolean
-    add_column :intakes, :demographic_spouse_black_africa_american, :boolean
+    add_column :intakes, :demographic_spouse_black_african_american, :boolean
     add_column :intakes, :demographic_spouse_native_hawaiian_pacific_islander, :boolean
     add_column :intakes, :demographic_spouse_asian, :boolean
     add_column :intakes, :demographic_spouse_white, :boolean
