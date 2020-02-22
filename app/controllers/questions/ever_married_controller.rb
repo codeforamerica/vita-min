6f34b40b80d3509a1865026bad019ee1d132437c
Tom Dooner <tdooner@codeforamerica.org>
2020-02-21 17:19:56 -0800

    Reconcile Icons with Figma
    * Add "ever-married" rings icon to "married" as well
    * Add "notification-preferences" sms bubble icon
    * Add "mailing-address" little house icon

--- app/controllers/questions/ever_married_controller.rb
+++ app/controllers/questions/ever_married_controller.rb
@@ -2,6 +2,10 @@ module Questions
   class EverMarriedController < QuestionsController
     layout "yes_no_question"
 
+    def illustration_path
+      "married.svg"
+    end
+
     def section_title
       "Personal Information"
     end
