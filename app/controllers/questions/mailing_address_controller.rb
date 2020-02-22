df9c10e5a605bc6f1d8c113a7ea1928c992fd54a
Tom Dooner <tdooner@codeforamerica.org>
2020-02-21 17:19:56 -0800

    Reconcile Icons with Figma
    * Add "ever-married" rings icon to "married" as well
    * Add "notification-preferences" sms bubble icon
    * Add "mailing-address" little house icon

--- app/controllers/questions/mailing_address_controller.rb
+++ app/controllers/questions/mailing_address_controller.rb
@@ -4,8 +4,6 @@ module Questions
       "Personal Information"
     end
 
-    def illustration_path; end
-
     def custom_tracking_data
       {
         mailing_address_same_as_idme_address: current_intake.address_matches_primary_user_address?
