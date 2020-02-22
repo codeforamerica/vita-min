201266bb7de83e3a436f1be4f02bf726766c47c1
Ben Golder <bgolder@codeforamerica.org>
2020-02-21 16:07:49 -0800

    Add additional info png to zendesk ticket with completed intake
    Co-authored-by: Tom Dooner <tdooner@codeforamerica.org>

--- app/jobs/send_completed_intake_to_zendesk_job.rb
+++ app/jobs/send_completed_intake_to_zendesk_job.rb
@@ -5,7 +5,7 @@ class SendCompletedIntakeToZendeskJob < ApplicationJob
     intake = Intake.find(intake_id)
 
     service = ZendeskIntakeService.new(intake)
-    success = service.send_final_intake_pdf && service.send_all_docs
+    success = service.send_final_intake_pdf && service.send_all_docs && service.send_additional_info_document
     intake.update(completed_intake_sent_to_zendesk: success)
   end
 end
