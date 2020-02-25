69d5752f3c452c94a77f45ce4124455f2c595177
Jenny Heath <jheath@codeforamerica.org>
2020-02-25 11:52:05 -0800

    Associate intake to user on sign in
    - new user gets associated with intake from session
    - returning users keep their intake, discard intake from session
    - fix eligibility question to check for income limit
    
    Co-authored-by: Laura Kogler <lkogler@codeforamerica.org>

--- spec/controllers/questions/eligibility_controller_spec.rb
+++ spec/controllers/questions/eligibility_controller_spec.rb
@@ -2,22 +2,46 @@ require "rails_helper"
 
 RSpec.describe Questions::EligibilityController do
   describe "#update" do
-    context "when they check one of the boxes" do
-      let(:params) { { eligibility_form: { had_farm_income: "yes", had_rental_income: "no", income_over_limit: "no" } } }
+    let(:had_farm_income) { "no" }
+    let(:had_rental_income) { "no" }
+    let(:income_over_limit) { "no" }
+    let(:params) { { eligibility_form: { had_farm_income: had_farm_income, had_rental_income: had_rental_income, income_over_limit: income_over_limit } } }
 
-      it "creates a new intake and offboards them" do
-        expect {
-          post :update, params: params
-        }.to change(Intake, :count).by(1)
+    RSpec.shared_examples "an offboarding flow" do
+      describe "eligibility checks" do
+        it "creates a new intake and offboards them" do
+          expect {
+            post :update, params: params
+          }.to change(Intake, :count).by(1)
 
-        expect(Intake.last.had_farm_income_yes?).to eq true
-        expect(response).to redirect_to(maybe_ineligible_path)
+          expect(Intake.last.had_farm_income).to eq had_farm_income
+          expect(Intake.last.had_rental_income).to eq had_rental_income
+          expect(Intake.last.income_over_limit).to eq income_over_limit
+
+          expect(response).to redirect_to(maybe_ineligible_path)
+        end
+      end
+    end
+
+    context "when they check had farm income" do
+      it_behaves_like "an offboarding flow" do
+        let(:had_farm_income) { "yes" }
+      end
+    end
+
+    context "when they check had rental income" do
+      it_behaves_like "an offboarding flow" do
+        let(:had_rental_income) { "yes" }
+      end
+    end
+
+    context "when they check had income over limit" do
+      it_behaves_like "an offboarding flow" do
+        let(:income_over_limit) { "yes" }
       end
     end
 
     context "when they do not check any of the boxes" do
-      let(:params) { { eligibility_form: { had_farm_income: "no", had_rental_income: "no", income_over_limit: "no" } } }
-
       it "it creates a new intake and allows them to continue to sign in " do
         expect {
           post :update, params: params
