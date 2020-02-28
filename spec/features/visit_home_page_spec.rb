f927b558a3e2f29223033bfb11d588827f6ddbae
Ben Golder <bgolder@codeforamerica.org>
2020-02-28 14:30:19 -0800

    Copy updates to landing page.
    Co-authored-by: Jenny Heath <jheath@codeforamerica.org>

--- spec/features/visit_home_page_spec.rb
+++ spec/features/visit_home_page_spec.rb
@@ -4,7 +4,7 @@ RSpec.feature "Visit home page" do
   scenario "has most critical content" do
     visit "/"
     expect(page).to have_text "Free tax filing, real human support."
-    expect(page).to have_text "Maximize your refund by filing with our trusted volunteers. Available to you online or in person!"
+    expect(page).to have_text "Maximize your refund by filing with our trusted volunteers."
     expect(page).to have_link "Get started"
   end
 end
