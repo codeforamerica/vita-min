6e5530e610be9b5fe1e9cb7feae3e2938ec863b8
Jenny Heath <jheath@codeforamerica.org>
2020-03-06 10:09:39 -0700

    Refactor and add tests

--- app/helpers/documents_helper.rb
+++ app/helpers/documents_helper.rb
@@ -5,38 +5,12 @@ module DocumentsHelper
     "SSN or ITIN",
   ]
 
-  MIGHT_HAVE_DOC_TYPES = [
-    "W-2",
-    "1098",
-    "1098-E",
-    "1098-T",
-    "1099-A",
-    "1099-B",
-    "1099-C",
-    "1099-DIV",
-    "1099-INT",
-    "1099-K",
-    "1099-MISC",
-    "1099-S",
-    "1099-SA",
-    "1099-G",
-    "5498-SA",
-    "IRA Statement",
-    "RRB-1099",
-    "SSA-1099",
-    "Student Account Statement",
-    "Childcare Statement",
-    "W-2G",
-    "2018 Tax Return",
-    "Other",
-  ]
-
   def must_have?(document_type)
     MUST_HAVE_DOC_TYPES.include?(document_type)
   end
 
   def might_have?(document_type)
-    MIGHT_HAVE_DOC_TYPES.include?(document_type)
+    !MUST_HAVE_DOC_TYPES.include?(document_type)
   end
 
   def any_might_have_docs?(doc_types)
---
+++ spec/helpers/documents_helper_spec.rb
@@ -0,0 +1,75 @@
+require "rails_helper"
+
+RSpec.describe DocumentsHelper do
+  describe "#must_have?" do
+    context "a document that we can be sure someone needs" do
+      let(:doc_type) { "SSN or ITIN" }
+
+      it "returns true" do
+        expect(helper.must_have?(doc_type)).to eq true
+      end
+    end
+
+    context "a document that we cannot be sure someone needs" do
+      let(:doc_type) { "1099-DIV" }
+
+      it "returns false" do
+        expect(helper.must_have?(doc_type)).to eq false
+      end
+    end
+  end
+
+  describe "#might_have?" do
+    context "a document that we can be sure someone needs" do
+      let(:doc_type) { "SSN or ITIN" }
+
+      it "returns true" do
+        expect(helper.might_have?(doc_type)).to eq false
+      end
+    end
+
+    context "a document that we cannot be sure someone needs" do
+      let(:doc_type) { "1099-DIV" }
+
+      it "returns false" do
+        expect(helper.might_have?(doc_type)).to eq true
+      end
+    end
+  end
+
+  describe "#any_might_have_docs?" do
+    context "a person whose recommended document types include some they might have" do
+      let(:doc_types) { ["1099-MISC", "W-2"] }
+
+      it "returns true" do
+        expect(helper.any_might_have_docs?(doc_types)).to eq true
+      end
+    end
+
+    context "a person whose recommended document types do not include any they might have" do
+      let(:doc_types) { ["1095-A"] }
+
+      it "returns true" do
+        expect(helper.any_might_have_docs?(doc_types)).to eq false
+      end
+    end
+  end
+
+  describe "#any_must_have_docs?" do
+    context "a person whose recommended document types include some they must have" do
+      let(:doc_types) { ["1099-R"] }
+
+      it "returns true" do
+        expect(helper.any_must_have_docs?(doc_types)).to eq true
+      end
+    end
+
+    context "a person whose recommended document types do not include any they must have" do
+      let(:doc_types) { ["1099-MISC", "W-2"] }
+
+      it "returns true" do
+        expect(helper.any_must_have_docs?(doc_types)).to eq false
+      end
+    end
+  end
+end
