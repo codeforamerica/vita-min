6fd0ec2820d2a16037d538c5411e911fa1c6afe2
Ben Golder <bgolder@codeforamerica.org>
2020-03-19 17:38:09 -0700

    Remove timeout, update click event naming

--- app/assets/javascripts/ajax_mixpanel_events.js
+++ app/assets/javascripts/ajax_mixpanel_events.js
@@ -9,19 +9,20 @@ var ajaxMixpanelEvents = (function() {
       e.stopPropagation();
 
       var clickedElement = $(e.target);
+      var elementText = clickedElement.text().trim();
+      var eventName = "click_" + clickedElement.attr("data-track-click");
       var eventData = {
-        event_name: "clicked_link",
+        event_name: eventName,
         controller_action: window.mixpanelData.controller_action,
         full_path: window.mixpanelData.full_path,
         data: {
-          link_text: clickedElement.text().trim(),
+          call_to_action: elementText,
         }
       };
 
       $.ajax({
         type: "POST",
         url: "/ajax_mixpanel_events",
-        timeout: 200,
         data: eventData,
       })
       .then(() => clickedElement.trigger("click", {trackedEvent: true}))
