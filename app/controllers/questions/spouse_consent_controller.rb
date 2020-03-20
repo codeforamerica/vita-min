3d85740bbd40d1e703f496f9a0fc8b18c25b858d
Jenny Heath <jheath@codeforamerica.org>
2020-03-20 16:28:22 -0700

    Redirect spouse from link to 'you did it' page after consent

--- app/controllers/questions/spouse_consent_controller.rb
+++ app/controllers/questions/spouse_consent_controller.rb
@@ -3,5 +3,14 @@ module Questions
     def self.show?(intake)
       intake.filing_joint_yes?
     end
+
+    def next_path
+      if session[:authenticate_spouse_only]
+        session[:authenticate_spouse_only] = nil
+        spouse_auth_complete_path
+      else
+        super
+      end
+    end
   end
 end
