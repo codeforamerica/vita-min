d012053c517f88c38074b99d24ef9e90c6466faf
Ben Golder <bgolder@codeforamerica.org>
2020-03-22 13:20:28 -0700

    Handle new provider data and archive old providers

--- app/models/provider_scrape.rb
+++ app/models/provider_scrape.rb
@@ -16,4 +16,44 @@ class ProviderScrape < ApplicationRecord
   def self.get_recent_or_create
     where("created_at >= ?", 12.hours.ago).first || create
   end
+
+  def handle_irs_provider_data(data)
+    existing_provider = VitaProvider.unscraped_by(self).find_by(irs_id: data[:irs_id])
+    if existing_provider.present?
+      if existing_provider.same_as_irs_result?(data) && existing_provider.is_listed?
+        mark_unchanged_provider_as_scraped(existing_provider)
+      else
+        change_existing_provider(existing_provider, data)
+      end
+    else
+      add_new_provider(data)
+    end
+  end
+
+  def archive_all_unscraped_providers
+    # update_all skips save callbacks and does not change updated_at, so we need to set it manually
+    result = VitaProvider.listed.unscraped_by(self).update_all(
+      last_scrape_id: self.id,
+      archived: true,
+      updated_at: Time.now
+    )
+    update(archived_count: archived_count + result)
+  end
+
+  private
+
+  def mark_unchanged_provider_as_scraped(provider)
+    provider.update(last_scrape: self)
+  end
+
+  def change_existing_provider(provider, new_data)
+    provider.last_scrape = self
+    provider.archived = false
+    increment!(:changed_count) if provider.update_with_irs_data(new_data)
+  end
+
+  def add_new_provider(new_data)
+    provider = VitaProvider.new(last_scrape: self)
+    increment!(:created_count) if provider.update_with_irs_data(new_data)
+  end
 end
