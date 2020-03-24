fd4299b62a22257896be5eee2749f94c95c8feba
Jenny Heath <jheath@codeforamerica.org>
2020-03-24 11:47:33 -0700

    add action_mailer default_url_options to demo, staging, prod config

--- config/environments/demo.rb
+++ config/environments/demo.rb
@@ -2,4 +2,6 @@ require_relative "./shared_deployment_config"
 
 Rails.application.configure do
   config.active_storage.service = :s3_demo
+
+  config.action_mailer.default_url_options = { host: 'demo.getyourrefund.org' }
 end
--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -2,4 +2,6 @@ require_relative "./shared_deployment_config"
 
 Rails.application.configure do
   config.active_storage.service = :s3_staging
+
+  config.action_mailer.default_url_options = { host: 'staging.getyourrefund.org' }
 end
