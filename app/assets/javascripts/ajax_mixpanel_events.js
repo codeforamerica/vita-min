8790c9ce6e58507399a279eb6522496c878ce846
Tom Dooner <tdooner@codeforamerica.org>
2020-03-25 20:51:16 -0700

    Track clicks to "IRS Free File" (via sendBeacon API)
    The original `ajaxMixpanelEvents` approach didn't work for standard <a>
    tags due to the fact that browsers do not navigate to a link's
    destination when a jQuery click event is programmatically triggered.
    
    This commit updates the implementation to use a [new-ish API called
    `sendBeacon`][1] which is made for this purpose: to send analytics
    reliably as a user is leaving a page. The API is well-supported except
    for Internet Explorer, for which a polyfill has been included. I
    verified that the polyfill works on IE11 via Browserstack. (No traffic
    to GetYourRefund has been from a version of IE other than 11, according
    to Mixpanel.)
    
    The polyfill was downloaded using this command, and I manually added the
    license to the header of the file:
    
    ```
    wget -O app/assets/javascripts/vendor/navigator.sendbeacon.min.js \
      https://raw.githubusercontent.com/miguelmota/Navigator.sendBeacon/master/dist/navigator.sendbeacon.min.js
    ```
    
    Finally, worth mentioning, the `sendBeacon` API requires a `FormData()`
    javascript object instead of using jQuery's AJAX. So I included the CSRF
    `authenticity_token` param manually, and as such decided to scope all
    the Mixpanel event params under a top-level `event` key so we could not
    have to worry about the extra `authenticity_token` param while
    processing the form params.
    
    [1]: https://developer.mozilla.org/en-US/docs/Web/API/Navigator/sendBeacon

--- app/assets/javascripts/ajax_mixpanel_events.js
+++ app/assets/javascripts/ajax_mixpanel_events.js
@@ -1,33 +1,20 @@
+//= require vendor/navigator.sendbeacon.min
+
 var ajaxMixpanelEvents = (function() {
   var init = function() {
     $("[data-track-click]").on("click", function (e, options) {
-      options = options || {};
-      if (options.trackedEvent === true) {
-        return;
-      }
-
-      e.preventDefault();
-      e.stopPropagation();
-
       var clickedElement = $(e.target);
       var elementText = clickedElement.text().trim();
       var eventName = "click_" + clickedElement.attr("data-track-click");
-      var eventData = {
-        event_name: eventName,
-        controller_action: window.mixpanelData.controller_action,
-        full_path: window.mixpanelData.full_path,
-        data: {
-          call_to_action: elementText,
-        }
-      };
 
-      $.ajax({
-        type: "POST",
-        url: "/ajax_mixpanel_events",
-        data: eventData,
-      })
-      .then(function() { clickedElement.trigger("click", {trackedEvent: true}) })
-      .fail(function() { clickedElement.trigger("click", {trackedEvent: true}) });
+      var eventData = new FormData();
+      eventData.append("event[event_name]", eventName);
+      eventData.append("event[controller_action]", window.mixpanelData.controller_action);
+      eventData.append("event[full_path]", window.mixpanelData.full_path);
+      eventData.append("event[data][call_to_action]", elementText);
+      eventData.append(Rails.csrfParam(), Rails.csrfToken());
+
+      navigator.sendBeacon("/ajax_mixpanel_events", eventData);
     });
   };
 
