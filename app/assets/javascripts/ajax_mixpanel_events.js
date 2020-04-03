635e94d6c670bbf6e9fa09361986b461d8c67638
Jenny Heath <jheath@codeforamerica.org>
2020-04-03 14:54:38 -0700

    Track document type with 'i don't have this' and 'skip this for now' buttons
    Co-authored-by: Ben Golder <bgolder@codeforamerica.org>

--- app/assets/javascripts/ajax_mixpanel_events.js
+++ app/assets/javascripts/ajax_mixpanel_events.js
@@ -1,13 +1,23 @@
 //= require vendor/navigator.sendbeacon.min
 
-var ajaxMixpanelEvents = (function() {
-  var init = function() {
+var ajaxMixpanelEvents = (function () {
+  var init = function () {
     $("[data-track-click]").on("click", function (e, options) {
       var clickedElement = $(e.target).closest("a");
+      var dataAttributes = clickedElement.data();
       var elementText = clickedElement.text().trim();
-      var eventName = "click_" + clickedElement.attr("data-track-click");
+      var eventName = "click_" + dataAttributes["trackClick"];
 
       var eventData = new FormData();
+
+      $.each(dataAttributes, function (key, value) {
+        var attributePrefix = "trackAttribute";
+        if (key.indexOf(attributePrefix) === 0) {
+          var propertyKey = key.substring(attributePrefix.length).toLowerCase();
+          eventData.append(`event[${propertyKey}]`, value);
+        }
+      });
+
       eventData.append("event[event_name]", eventName);
       eventData.append("event[controller_action]", window.mixpanelData.controller_action);
       eventData.append("event[full_path]", window.mixpanelData.full_path);
