46e3b86baaa46c23972ba9b242f475c770d659d5
Molly T-M <mollyt@codeforamerica.org>
2020-04-03 11:55:01 -0700

    Use download_attachments_to_tmp helper method
    - Using Blob#open recursively
    
    [#171415422]
    
    Co-authored-by: Tom Dooner <tdooner@codeforamerica.org>

--- app/services/zendesk_follow_up_docs_service.rb
+++ app/services/zendesk_follow_up_docs_service.rb
@@ -1,5 +1,6 @@
 class ZendeskFollowUpDocsService
   include ZendeskServiceHelper
+  include AttachmentsHelper
 
   def initialize(intake)
     @intake = intake
@@ -9,30 +10,18 @@ class ZendeskFollowUpDocsService
     return if @intake.documents.none?
 
     new_requested_docs = @intake.documents.where(document_type: "Requested", zendesk_ticket_id: nil)
-    file_list = new_requested_docs.map do |document|
-      @document_blob = document.upload
-      blob.open(tmpdir: Dir.tmpdir) do |file|
-        {file: file, filename: document.upload.filename.to_s}
-      end
+    download_attachments_to_tmp(new_requested_docs) do |file_list|
+
+      output = append_multiple_files_to_ticket(
+        ticket_id: @intake.intake_ticket_id,
+        file_list: file_list,
+        comment: "The client added requested follow-up documents:\n" + new_requested_docs.map {|d| "* #{d.upload.filename}\n"}.join,
+      )
+
+      raise CouldNotSendFollowUpDocError unless output
+      new_requested_docs.each {|doc| doc.update(zendesk_ticket_id: @intake.intake_ticket_id)}
+      output
     end
-
-    output = append_multiple_files_to_ticket(
-      ticket_id: @intake.intake_ticket_id,
-      file_list: file_list,
-      comment: "The client added requested follow-up documents:\n" + new_requested_docs.map { |d| "* #{d.upload.filename}\n" }.join,
-    )
-
-    raise CouldNotSendFollowUpDocError unless output
-    new_requested_docs.each { |doc| doc.update(zendesk_ticket_id: @intake.intake_ticket_id) }
-    output
-  ensure
-    if file_list.present?
-      file_list.each { |entry| entry[:file].close! }
-    end
-  end
-
-  def blob
-    @document_blob
   end
 
   class CouldNotSendFollowUpDocError < ZendeskServiceError; end
