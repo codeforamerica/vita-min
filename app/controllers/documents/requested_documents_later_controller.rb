56866a795f862fc63666a9c7002cd8689f8e9eb2
Molly T-M <mollyt@codeforamerica.org>
2020-04-08 17:32:53 -0700

    Fix anonymous session logic
    - We lost the guard for logged-in users somewhere
    - Don't check token validity after first time

--- app/controllers/documents/requested_documents_later_controller.rb
+++ app/controllers/documents/requested_documents_later_controller.rb
@@ -1,6 +1,6 @@
 module Documents
   class RequestedDocumentsLaterController < DocumentUploadQuestionController
-    before_action :check_token_and_create_anonymous_session, only: :edit
+    before_action :handle_session, only: :edit
     skip_before_action :require_sign_in
 
     def self.show?(_)
@@ -17,17 +17,31 @@ module Documents
 
     private
 
+    def handle_session
+      check_token_and_create_anonymous_session unless session_in_progress?
+    end
+
     def check_token_and_create_anonymous_session
       original_intake = Intake.where.not(requested_docs_token: nil).where(requested_docs_token: params[:token]).first
-      return redirect_to documents_requested_docs_not_found_path unless original_intake.present?
-
-      create_anonymous_intake_session(original_intake) unless session[:anonymous_session]
+      if original_intake.present?
+        create_anonymous_intake_session(original_intake)
+      else
+        redirect_to documents_requested_docs_not_found_path
+      end
     end
 
     def create_anonymous_intake_session(original_intake)
       anonymous_intake = Intake.create(intake_ticket_id: original_intake.intake_ticket_id)
       session[:intake_id] = anonymous_intake.id
       session[:anonymous_session] = true
+    end
+
+    def anonymous_session_in_progress?
+      session[:anonymous_session] && session[:intake_id].present?
+    end
+
+    def session_in_progress?
+      current_user.present? || anonymous_session_in_progress?
     end
   end
 end
