26b9260efd3d43b757561a430e3435ec131c8839
Jenny Heath <jheath@codeforamerica.org>
2020-04-09 16:42:33 -0700

    Add page to collect email address, save on intake

--- spec/features/web_intake/new_joint_filers_spec.rb
+++ spec/features/web_intake/new_joint_filers_spec.rb
@@ -88,6 +88,12 @@ RSpec.feature "Web Intake Joint Filers" do
     fill_in "Confirm e-mail address", with: "gary.gardengnome@example.green"
     click_on "Continue"
 
+    # Email
+    expect(page).to have_selector("h1", text: "Please share your e-mail address.")
+    fill_in "E-mail address", with: "gary.gardengnome@example.green"
+    fill_in "Confirm e-mail address", with: "gary.gardengnome@example.green"
+    click_on "Continue"
+
     # Authentication
     expect(page).to have_selector("h1", text: "First, let’s get some basic information.")
     click_on "Sign in with ID.me"
