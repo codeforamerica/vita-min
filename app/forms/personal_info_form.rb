3ba6fbb5ad56040c34323a7886e8ad02d61eaa7a
Tom Dooner <tdooner@codeforamerica.org>
2020-04-10 16:35:49 -0700

    Add "Personal Info" page for name & state
    This adds a simple page for name & state at the beginning of the flow.
    
    I debated putting state in the existing `state` column (populated by
    /questions/mailing-address), but decided to make a new column in case
    the mailing address page will remain.

---
+++ app/forms/personal_info_form.rb
@@ -0,0 +1,10 @@
+class PersonalInfoForm < QuestionsForm
+  set_attributes_for :intake, :state_of_residence, :preferred_name
+
+  validates :state_of_residence, inclusion: { in: States.keys, message: "Please select a state from the list." }
+  validates :preferred_name, presence: { message: "Please enter your preferred name." }
+
+  def save
+    @intake.update(attributes_for(:intake))
+  end
+end
