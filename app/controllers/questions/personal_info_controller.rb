0b8019a3e0338b8ddf6cae966837afd6008ed819
Ben Golder <bgolder@codeforamerica.org>
2020-04-14 15:28:01 -0700

    Remove vestigial section_titles
    Co-authored-by: Molly T-M <mollyt@codeforamerica.org>

--- app/controllers/questions/personal_info_controller.rb
+++ app/controllers/questions/personal_info_controller.rb
@@ -2,10 +2,6 @@ module Questions
   class PersonalInfoController < QuestionsController
     skip_before_action :require_sign_in
 
-    def section_title
-      "Household Information"
-    end
-
     def illustration_path; end
 
     def tracking_data
