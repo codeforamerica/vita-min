f7ad8ffe4ff19fa73edbfd97aea08a268299f186
Jenny Heath <jheath@codeforamerica.org>
2020-04-14 17:26:29 -0700

    Add SSN/ITIN doc uploads for primary and spouse

--- app/lib/document_navigation.rb
+++ app/lib/document_navigation.rb
@@ -4,6 +4,8 @@ class DocumentNavigation
   DOCUMENT_CONTROLLERS = {
     "ID" => Documents::IdsController,
     "Spouse ID" => Documents::SpouseIdsController,
+    "Primary SSN or ITIN" => Documents::PrimarySsnItinsController,
+    "Spouse SSN or ITIN" => Documents::SpouseSsnItinsController,
     "W-2" => Documents::W2sController,
     "1095-A" => Documents::Form1095asController,
     "1098" => Documents::Form1098sController,
