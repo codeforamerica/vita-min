41961301bf42b48ee4853d29857e6e67baf8bfda
Jenny Heath <jheath@codeforamerica.org>
2020-04-15 14:52:12 -0700

    Consolidate all SSN/ITIN pages

--- app/helpers/documents_helper.rb
+++ app/helpers/documents_helper.rb
@@ -2,11 +2,9 @@ module DocumentsHelper
   MUST_HAVE_DOC_TYPES= [
     "ID",
     "Spouse ID",
-    "Primary SSN or ITIN",
-    "Spouse SSN or ITIN",
+    "SSN or ITIN",
     "1095-A",
     "1099-R",
-    "SSN or ITIN",
   ]
 
   def must_have?(document_type)
