8205dadd6793c8f4e8ed06954df9f2eff5c67902
Jenny Heath <jheath@codeforamerica.org>
2020-04-15 17:34:11 -0700

    ID and SSN upload pages: add placeholder for spouse name if we don't have it

--- spec/controllers/documents/ids_controller_spec.rb
+++ spec/controllers/documents/ids_controller_spec.rb
@@ -13,21 +13,34 @@ RSpec.describe Documents::IdsController do
   describe "#edit" do
     context "when they are filing jointly" do
       let(:attributes) { { filing_joint: "yes" } }
-      let!(:user) { create :user, first_name: "Greta", last_name: "Gnome", is_spouse: true, intake: intake }
 
-      it "shows copy " do
-        get :edit
+      context "when we have the spouse name" do
+        let!(:user) { create :user, first_name: "Greta", last_name: "Gnome", is_spouse: true, intake: intake }
 
-        expect(assigns(:title)).to eq "Attach photos of ID cards"
-        expect(assigns(:help_text)).to eq "The IRS requires us to see a current drivers license, passport, or state ID for you and your spouse."
-        expect(assigns(:names)).to eq ["Gary Gnome", "Greta Gnome"]
+        it "shows the spouse name" do
+          get :edit
+
+          expect(assigns(:title)).to eq "Attach photos of ID cards"
+          expect(assigns(:help_text)).to eq "The IRS requires us to see a current drivers license, passport, or state ID for you and your spouse."
+          expect(assigns(:names)).to eq ["Gary Gnome", "Greta Gnome"]
+        end
+      end
+
+      context "when we don't have the spouse name" do
+        it "shows the placeholder" do
+          get :edit
+
+          expect(assigns(:title)).to eq "Attach photos of ID cards"
+          expect(assigns(:help_text)).to eq "The IRS requires us to see a current drivers license, passport, or state ID for you and your spouse."
+          expect(assigns(:names)).to eq ["Gary Gnome", "Your spouse"]
+        end
       end
     end
 
     context "when they are not filing jointly" do
       let(:attributes) { { filing_joint: "no" } }
 
-      it "returns false" do
+      it "shows singular copy" do
         get :edit
 
         expect(assigns(:title)).to eq "Attach a photo of your ID card"
