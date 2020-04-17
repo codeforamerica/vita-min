1db461d9b2719414f94ba33bbd4fdf87098f1c6f
Molly T-M <mollyt@codeforamerica.org>
2020-04-17 14:59:57 -0700

    Selfie instructions and upload page in document flow

---
+++ app/controllers/documents/selfies_controller.rb
@@ -0,0 +1,11 @@
+module Documents
+  class SelfiesController < DocumentUploadQuestionController
+
+    def edit
+      @names = [current_intake.primary_user.full_name]
+      if current_intake.filing_joint_yes?
+        @names << current_intake.spouse_name_or_placeholder
+      end
+    end
+  end
+end
