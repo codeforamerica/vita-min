c5804303525d4481ed72e55d3aacb33395517759
Ben Golder <bgolder@codeforamerica.org>
2020-04-21 20:03:55 -0700

    Remove User model from mixpanel data & name lists
    Co-authored-by: Jenny Heath <jheath@codeforamerica.org>

--- app/controllers/documents/selfies_controller.rb
+++ app/controllers/documents/selfies_controller.rb
@@ -2,7 +2,7 @@ module Documents
   class SelfiesController < DocumentUploadQuestionController
 
     def edit
-      @names = [current_intake.primary_user.full_name]
+      @names = [current_intake.primary_full_name]
       if current_intake.filing_joint_yes?
         @names << current_intake.spouse_name_or_placeholder
       end
