0949bf27eb41f6c003f33c252e2ce8ea11cfccf6
Tom Dooner <tdooner@codeforamerica.org>
2020-04-21 21:49:27 -0700

    Move pages in flow
    Finally, this commit addresses the issue in [the story][1]. Now that we've
    made it here, the diff is pretty much just moving pages around the
    navigators!
    
    [1]: https://www.pivotaltracker.com/story/show/172381027

--- spec/features/web_intake/ineligible_filer_spec.rb
+++ spec/features/web_intake/ineligible_filer_spec.rb
@@ -14,9 +14,6 @@ RSpec.feature "client is not eligible for VITA services" do
     expect(page).to have_selector("h1", text: "Let's get started")
     click_on "Continue"
 
-    expect(page).to have_selector("h1", text: "Our team is here to help!")
-    click_on "Continue"
-
     expect(page).to have_selector("h1", text: "Let’s check a few things.")
     check "I earned money from owning a farm"
     click_on "Continue"
