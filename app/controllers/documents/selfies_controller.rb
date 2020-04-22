fd2692a90cb2497abb18a744142f8b8b1dd555d8
Ben Golder <bgolder@codeforamerica.org>
2020-04-22 15:11:00 -0700

    Upload selfies
    Co-authored-by: Molly T-M <mollyt@codeforamerica.org>

--- app/controllers/documents/selfies_controller.rb
+++ app/controllers/documents/selfies_controller.rb
@@ -1,11 +1,11 @@
 module Documents
   class SelfiesController < DocumentUploadQuestionController
-
     def edit
       @names = [current_intake.primary_full_name]
       if current_intake.filing_joint_yes?
         @names << current_intake.spouse_name_or_placeholder
       end
+      super
     end
 
     def self.document_type
