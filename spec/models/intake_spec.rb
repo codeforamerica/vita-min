dd3161fb28cf3299162b927a3990e75a8fe1d271
Molly T-M <mollyt@codeforamerica.org>
2020-04-23 13:38:36 -0700

    Re-instate user tests on intake_spec.rb

--- spec/models/intake_spec.rb
+++ spec/models/intake_spec.rb
@@ -235,6 +235,40 @@ describe Intake do
     end
   end
 
+  describe "#users" do
+    it "should have many users" do
+      relation = Intake.reflect_on_association(:users).macro
+      expect(relation).to eq :has_many
+    end
+  end
+
+  describe "#primary_user" do
+    let(:intake) { create :intake }
+
+    context "with no user" do
+      it "returns nil" do
+        expect(intake.primary_user).to be_nil
+      end
+    end
+
+    context "with a couple filing jointly" do
+      let!(:primary) { create :user, intake: intake }
+      let!(:spouse) { create :spouse_user, intake: intake }
+
+      it "returns the first non-spouse user" do
+        expect(intake.primary_user).to eq primary
+      end
+    end
+
+    context "with one user" do
+      let!(:primary) { create :user, intake: intake }
+
+      it "returns that one user" do
+        expect(intake.primary_user).to eq primary
+      end
+    end
+  end
+
   describe "#referrer_domain" do
     let(:intake) { build :intake, referrer: referrer }
 
