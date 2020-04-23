1b68d9823999502d857f7c0cb713e2c7ad5e6bea
Jenny Heath <jheath@codeforamerica.org>
2020-04-22 17:15:13 -0700

    Add a test for the intake/user backfill
    Co-authored-by: Ben Golder <bgolder@codeforamerica.org>

--- app/lib/user_data_to_intake_backfill.rb
+++ app/lib/user_data_to_intake_backfill.rb
@@ -27,53 +27,55 @@ SPOUSE_COLUMN_MAP = {
 }
 
 class UserDataToIntakeBackfill
+  def self.print_output(message)
+    puts(message) unless Rails.env.test?
+  end
+
   def self.run
-    puts "~~~~~~~~~~~~ BEGIN BACKFILL ~~~~~~~~~~~~"
+    print_output "~~~~~~~~~~~~ BEGIN BACKFILL ~~~~~~~~~~~~"
 
     Intake.all.each do |intake|
       if intake.primary_user.present?
-        puts "~~~~~~~~~~~~ UPDATING PRIMARY USER COLUMNS ~~~~~~~~~~~~"
+        print_output "~~~~~~~~~~~~ UPDATING PRIMARY USER COLUMNS ~~~~~~~~~~~~"
 
-        if intake.primary_birth_date.blank? && intake.primary_user.birth_date.present?
-          puts "============ updating primary_birth_date"
+        if intake.primary_user.birth_date.present?
+          print_output "============ updating primary_birth_date"
           intake.update(primary_birth_date: intake.primary_user.parsed_birth_date)
         end
 
-        if intake.primary_last_four_ssn.blank? && intake.primary_user.ssn.present?
-          puts "============ updating encrypted_primary_last_four_ssn"
+        if intake.primary_user.ssn.present?
+          print_output "============ updating encrypted_primary_last_four_ssn"
           intake.update(primary_last_four_ssn: intake.primary_user.ssn_last_four)
         end
 
         PRIMARY_COLUMN_MAP.each do |intake_column, user_column|
-          if intake[intake_column].blank? && intake.primary_user[user_column].present?
-            puts "============ updating #{intake_column}"
-            intake.update(intake_column => intake.primary_user[user_column])
-          end
+          print_output "============ updating #{intake_column}"
+          intake.update(intake_column => intake.primary_user[user_column])
         end
       end
 
       if intake.spouse.present?
-        puts "~~~~~~~~~~~~ UPDATING SPOUSE USER COLUMNS ~~~~~~~~~~~~"
+        print_output "~~~~~~~~~~~~ UPDATING SPOUSE USER COLUMNS ~~~~~~~~~~~~"
 
-        if intake.spouse_birth_date.blank? && intake.spouse.birth_date.present?
-          puts "============ updating spouse_birth_date"
+        if intake.spouse.birth_date.present?
+          print_output "============ updating spouse_birth_date"
           intake.update(spouse_birth_date: intake.spouse.parsed_birth_date)
         end
 
-        if intake.spouse_last_four_ssn.blank? && intake.spouse.ssn.present?
-          puts "============ updating encrypted_spouse_last_four_ssn"
+        if intake.spouse.ssn.present?
+          print_output "============ updating encrypted_spouse_last_four_ssn"
           intake.update(spouse_last_four_ssn: intake.spouse.ssn_last_four)
         end
 
         SPOUSE_COLUMN_MAP.each do |intake_column, user_column|
-          if intake[intake_column].blank? && intake.spouse[user_column].present?
-            puts "============ updating #{intake_column}"
+          if intake.spouse[user_column].present?
+            print_output "============ updating #{intake_column}"
             intake.update(intake_column => intake.spouse[user_column])
           end
         end
       end
     end
 
-    puts "~~~~~~~~~~~~ COMPLETE BACKFILL ~~~~~~~~~~~~"
+    print_output "~~~~~~~~~~~~ COMPLETE BACKFILL ~~~~~~~~~~~~"
   end
 end
