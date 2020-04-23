b296a8065082bfab9d7e572eb90a7a7e1a15e142
Ben Golder <bgolder@codeforamerica.org>
2020-04-22 22:03:10 -0700

    Make ZD ticket after consent, with preferred name

--- app/controllers/questions/consent_controller.rb
+++ app/controllers/questions/consent_controller.rb
@@ -7,5 +7,9 @@ module Questions
         primary_consented_to_service_ip: request.remote_ip,
       )
     end
+
+    def after_update_success
+      CreateZendeskIntakeTicketJob.perform_later(current_intake.id) if current_intake.intake_ticket_id.blank?
+    end
   end
 end
