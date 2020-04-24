c39962848fcfcbd6812d088ea319639ac0742493
Jenny Heath <jheath@codeforamerica.org>
2020-04-24 15:52:06 -0700

    Add test

--- spec/lib/zendesk_phone_number_backfill_spec.rb
+++ spec/lib/zendesk_phone_number_backfill_spec.rb
@@ -1,13 +1,10 @@
 require "rails_helper"
 
-RSpec.describe ZendeskPhoneNumberBackfill do
-  describe '.update_zendesk!' do
-    let(:fake_zendesk_user) do
-      double(ZendeskAPI::User, id: 1234569879, phone: "5105551234", email: "testy@example.com", name: "Testy McTestuser")
-    end
-    let(:user_properties) { {} }
+RSpec.describe ZendeskRequestedDocsLinkBackfill do
+  describe '.update' do
+    let(:fake_zendesk_intake_service) { instance_double(ZendeskIntakeService) }
+    let(:fake_zendesk_ticket) { double(ZendeskAPI::Ticket) }
     let(:intake_properties) { {} }
-    let!(:user) { create(:user, intake: intakes[0], **user_properties) }
     let!(:intakes) do
       [
         create(:intake, **intake_properties),
@@ -15,57 +12,54 @@ RSpec.describe ZendeskPhoneNumberBackfill do
     end
 
     before do
-      allow(fake_zendesk_user).to receive(:save)
-      allow(fake_zendesk_user).to receive(:phone=)
-      allow_any_instance_of(ZendeskIntakeService)
-        .to receive(:get_end_user)
-        .with(user_id: fake_zendesk_user.id)
-        .and_return(fake_zendesk_user)
-      allow_any_instance_of(ZendeskDropOffService)
-        .to receive(:get_end_user)
-        .with(user_id: fake_zendesk_user.id)
-        .and_return(fake_zendesk_user)
-      allow_any_instance_of(ZendeskDropOffService)
-        .to receive(:find_end_user)
-        .with(fake_zendesk_user.name, fake_zendesk_user.email, include(fake_zendesk_user.phone))
-        .and_return(fake_zendesk_user.id)
+      allow(ZendeskIntakeService).to receive(:new).and_return fake_zendesk_intake_service
+      allow(fake_zendesk_intake_service)
+        .to receive(:attach_requested_docs_link)
+        .with(fake_zendesk_ticket)
+      allow(fake_zendesk_intake_service)
+        .to receive(:get_ticket)
+        .and_return(fake_zendesk_ticket)
     end
 
-    describe "for an intake with a non-E164 phone number" do
+    describe "for an intake with an existing requested docs token" do
       let(:intake_properties) do
         {
-          intake_ticket_requester_id: fake_zendesk_user.id,
+          requested_docs_token: "t0k3n",
+          intake_ticket_id: "123",
         }
       end
 
-      let(:user_properties) do
-        {
-          phone_number: "5105551234"
-        }
-      end
-
-      it "backfills the fields as we expect" do
-        described_class.update_zendesk!
-        expect(fake_zendesk_user).to have_received(:save)
-        expect(fake_zendesk_user).to have_received(:phone=)
-          .with("+15105551234")
+      it "doesn't do anything" do
+        described_class.update
+        expect(fake_zendesk_intake_service).not_to have_received(:get_ticket)
       end
     end
 
-    describe "for a drop-off" do
-      let!(:drop_off) do
-        create(:intake_site_drop_off,
-               name: fake_zendesk_user.name,
-               phone_number: fake_zendesk_user.phone,
-               email: fake_zendesk_user.email
-              )
+    describe "for an intake with no intake ticket id" do
+      let(:intake_properties) do
+        {
+          intake_ticket_id: nil,
+        }
       end
 
-      it "backfills the fields as we expect" do
-        described_class.update_zendesk!
-        expect(fake_zendesk_user).to have_received(:save)
-        expect(fake_zendesk_user).to have_received(:phone=)
-          .with("+15105551234")
+      it "doesn't do anything" do
+        described_class.update
+        expect(fake_zendesk_intake_service).not_to have_received(:get_ticket)
+      end
+    end
+
+    describe "for an intake with no requested docs token" do
+      let(:intake_properties) do
+        {
+          intake_ticket_id: "123",
+        }
+      end
+
+      it "backfills the field as we expect" do
+        described_class.update
+        expect(fake_zendesk_intake_service).to have_received(:get_ticket)
+        expect(fake_zendesk_intake_service).to have_received(:attach_requested_docs_link)
+          .with(fake_zendesk_ticket)
       end
     end
   end
