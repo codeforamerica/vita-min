e89c256992414dc69cad8aebb7124b349ba9a694
Ben Golder <bgolder@codeforamerica.org>
2020-04-24 21:11:41 -0700

    Create improved Zendesk SMS webhook
    - Also configures development to allow for ngrok connections

--- app/controllers/zendesk_webhook_controller.rb
+++ app/controllers/zendesk_webhook_controller.rb
@@ -3,10 +3,29 @@ class ZendeskWebhookController < ApplicationController
   before_action :authenticate
 
   def incoming
+    if ["new_sms", "updated_sms"].include? json_payload[:method]
+      incoming_sms
+    end
     head :ok if params[:zendesk_webhook].present?
   end
 
+  def incoming_sms
+    phone_number = json_payload["requester_phone_number"].tr("+", "")
+    sms_ticket_id = json_payload["ticket_id"].to_i
+    message_body = json_payload["message_body"]
+
+    ZendeskInboundSmsJob.perform_later(
+      sms_ticket_id: sms_ticket_id,
+      phone_number: phone_number,
+      message_body: message_body
+    )
+  end
+
   private
+
+  def json_payload
+    params[:zendesk_webhook]
+  end
 
   def authenticate
     authenticate_or_request_with_http_basic do |name, password|
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -65,4 +65,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # Allow incoming connections over ngrok
+  config.hosts << /[a-z0-9]+\.ngrok\.io/
 end
