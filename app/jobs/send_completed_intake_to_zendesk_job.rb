6868ed4931396cc6227bad01bcba63fbd96d9d9a
Jonathan Greenberg <jonathan.s.greenberg@gmail.com>
2020-04-29 19:33:05 -0400

    Rescue job errors and send explicitly to Sentry with intake_ticket_id
    - Create with_raven_context ApplicationJob helper for catching and
    sending errors to Sentry with extra context
    - Use shared examples for testing background jobs with errors in specs
    
    (#172583987) Improve Sentry error reporting context to include zendesk
    ticket ID

--- app/jobs/send_completed_intake_to_zendesk_job.rb
+++ app/jobs/send_completed_intake_to_zendesk_job.rb
@@ -3,11 +3,12 @@ class SendCompletedIntakeToZendeskJob < ApplicationJob
 
   def perform(intake_id)
     intake = Intake.find(intake_id)
+    with_raven_context({ticket_id: intake.intake_ticket_id}) do
+      service = ZendeskIntakeService.new(intake)
+      success = service.send_final_intake_pdf &&
+        service.send_all_docs
 
-    service = ZendeskIntakeService.new(intake)
-    success = service.send_final_intake_pdf &&
-      service.send_all_docs
-
-    intake.update(completed_intake_sent_to_zendesk: success)
+      intake.update(completed_intake_sent_to_zendesk: success)
+    end
   end
 end
