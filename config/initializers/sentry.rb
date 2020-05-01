b4c9e39e59e3c2fcf4adc0568324319c1805a27b
Ben Vandgrift <ben@vandgrift.com>
2020-05-01 15:37:27 -0400

    removes stash-pop artifacts

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,6 +1,6 @@
 Raven.configure do |config|
   config.dsn = Rails.application.credentials.dig(Rails.env.to_sym, :sentry_dsn)
-  config.environments = %w(production staging demo development)
+  config.environments = %w(production staging demo)
   config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
   config.excluded_exceptions = Raven::Configuration::IGNORE_DEFAULT + %w(
     ActionController::UnknownFormat
