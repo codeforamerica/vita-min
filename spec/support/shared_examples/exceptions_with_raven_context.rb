84e78017faaa801e353eedef99309ac48a4680d7
Jonathan Greenberg <jonathan.s.greenberg@gmail.com>
2020-05-01 20:06:21 -0400

    Use Raven.extra_context api to add details to exception
    - Adds FailingJob for testing purposes
    
    (#172583987) Improve Sentry error reporting context to include zendesk
    ticket ID

--- spec/support/shared_examples/exceptions_with_raven_context.rb
+++ spec/support/shared_examples/exceptions_with_raven_context.rb
@@ -3,9 +3,10 @@ shared_examples "catches exceptions with raven context" do |action|
     it "sends the intake ticket_id to Sentry" do
       allow(fake_zendesk_intake_service).to receive(action)
         .and_raise("Test Error")
-      expect(Raven).to receive(:capture_exception)
-        .with(instance_of(RuntimeError), extra: {ticket_id: intake.intake_ticket_id })
-      described_class.perform_now(intake.id)
+      expect(Raven).to receive(:extra_context)
+        .with({ticket_id: intake.intake_ticket_id })
+      expect { described_class.perform_now(intake.id) }
+        .to raise_error("Test Error")
     end
   end
 end
