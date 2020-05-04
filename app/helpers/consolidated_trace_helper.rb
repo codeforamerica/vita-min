f1a63c5eebbf3bbe2e9d54216e13dda7dd09dc5a
Ben Vandgrift <ben@vandgrift.com>
2020-05-04 17:51:31 -0400

    updates consolidated trace helper with with_raven_context

--- app/helpers/consolidated_trace_helper.rb
+++ app/helpers/consolidated_trace_helper.rb
@@ -16,60 +16,91 @@ end
 # to both the rails logger and to Raven
 module ConsolidatedTraceHelper
 
+  def unwind_extra_context(extra_context = {}, severity = Severity::UNKNOWN)
+    last_context = Raven.context.extra
+    Raven.extra_context(extra_context.merge(level: severity))
+
+    yield
+
+    Raven.context.extra = last_context
+  end
+
+  ## adds context to the raven extra context
+  def with_raven_context(extra_context={}, message = nil, severity = Severity::UNKNOWN)
+    yield
+  rescue => exception
+    trace_message = log_body(message || exception.message, extra_context)
+    logger&.send(severity, trace_message)
+
+    unwind_extra_context(extra_context, severity) do
+      raise exception, trace_message
+    end
+  end
+
   ## generic trace method providing severity options
-  def trace(message, attributes = {}, severity = Severity::UNKNOWN)
-    # send message to rails logger at the appropriate severity
-    stack_trace = Thread.current
-                        .backtrace
-                        .filter { |line| !line.include? 'consolidated_trace' }
-                        .slice(0, 5)
-                        .join("\n")
-    logger&.send(severity, log_body(message, attributes, stack_trace))
-    Raven.capture_message(log_body(message, attributes, stack_trace), { extra: attributes, level: severity })
+  def trace(message, extra_context = {}, severity = Severity::UNKNOWN)
+    trace_message = log_body(message, extra_context)
+    logger&.send(severity, trace_message)
+
+    unwind_extra_context(extra_context, severity) do
+      Raven.capture_message(trace_message)
+    end
   end
 
   # creates a log body
-  def log_body(msg, attrs, st)
+  def log_body(msg, attrs)
     <<~MSGBODY
       #{msg}
       MESSAGE INCLUDED:
       #{attrs.inspect}
       RELEVANT STACK:
-      #{st}
+      #{local_trace}
       ----
     MSGBODY
   end
-  
+
   def intake_context(intake)
     {
         name: intake.preferred_name,
         email: intake.email_address,
         phone: intake.phone_number,
-        intake_id: intake.id
+        intake_id: intake.id,
+        zendesk_ticket_id: intake.intake_ticket_id,
+        zendesk_requester_id: intake.intake_ticket_requester_id
     }
   end
 
-  def trace_debug(message, attributes = {})
-    trace(message, attributes, Severity::DEBUG)
+  def trace_debug(message, extra_context = {})
+    trace(message, extra_context, Severity::DEBUG)
   end
 
-  def trace_info(message, attributes = {})
-    trace(message, attributes, Severity::INFO)
+  def trace_info(message, extra_context = {})
+    trace(message, extra_context, Severity::INFO)
   end
 
-  def trace_warning(message, attributes = {})
-    trace(message, attributes, Severity::WARN)
+  def trace_warning(message, extra_context = {})
+    trace(message, extra_context, Severity::WARN)
   end
 
-  def trace_error(message, attributes = {})
-    trace(message, attributes, Severity::ERROR)
+  def trace_error(message, extra_context = {})
+    trace(message, extra_context, Severity::ERROR)
   end
 
-  def trace_fatal(message, attributes = {})
-    trace(message, attributes, Severity::FATAL)
+  def trace_fatal(message, extra_context = {})
+    trace(message, extra_context, Severity::FATAL)
   end
 
-  def trace_unknown(message, attributes = {})
-    trace(message, attributes, Severity::UNKNOWN)
+  def trace_unknown(message, extra_context = {})
+    trace(message, extra_context, Severity::UNKNOWN)
+  end
+
+  private
+
+  def local_trace
+    Thread.current
+        .backtrace
+        .filter { |line| !line.include? 'consolidated_trace' }
+        .slice(0, 5)
+        .join("\n")
   end
 end
