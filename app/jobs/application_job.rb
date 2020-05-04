4307aa8d8139e10faa34ebec5faf9777fdf4d368
Ben Vandgrift <ben@vandgrift.com>
2020-05-04 17:51:31 -0400

    updates consolidated trace helper with with_raven_context

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -1,10 +1,4 @@
 class ApplicationJob < ActiveJob::Base
-
-  def with_raven_context(extra_context={})
-    yield
-  rescue => exception
-    Raven.extra_context(extra_context)
-    raise exception
-  end
+  include ConsolidatedTraceHelper
 
 end
--- app/jobs/create_zendesk_intake_ticket_job.rb
+++ app/jobs/create_zendesk_intake_ticket_job.rb
@@ -7,10 +7,12 @@ class CreateZendeskIntakeTicketJob < ApplicationJob
     intake = Intake.find(intake_id)
 
     return unless intake.intake_ticket_id.blank?
-    service = ZendeskIntakeService.new(intake)
+    with_raven_context(intake_context(intake)) do
+      service = ZendeskIntakeService.new(intake)
 
-    service.assign_requester &&
-        service.assign_intake_ticket
+      service.assign_requester &&
+          service.assign_intake_ticket
+    end
   end
 
 end
