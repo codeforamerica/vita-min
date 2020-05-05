11f062afc20dd0eabb957cf7d97bf6a571d0c777
Jonathan Greenberg <jonathan.s.greenberg@gmail.com>
2020-05-05 15:00:27 -0400

    Use valid_email2 gem for a bit stricter email validation
    - (#172627155) Fix email validation

---
+++ spec/forms/email_address_form_spec.rb
@@ -0,0 +1,38 @@
+require "rails_helper"
+
+RSpec.describe EmailAddressForm do
+  let(:intake) { create :intake }
+
+  describe "validations" do
+    context "when the email is valid" do
+      it "is valid" do
+
+        form = EmailAddressForm.new(
+          intake,
+          {
+            email_address: "stuff@things.net",
+            email_address_confirmation: "stuff@things.net"
+          }
+        )
+
+        expect(form).to be_valid
+      end
+    end
+
+    context "when the email does not have a top level domain" do
+      it "is not valid" do
+
+        form = EmailAddressForm.new(
+          intake,
+          {
+            email_address: "stuff@things",
+            email_address_confirmation: "stuff@things"
+          }
+        )
+
+        expect(form).not_to be_valid
+        expect(form.errors[:email_address]).to be_present
+      end
+    end
+  end
+end
