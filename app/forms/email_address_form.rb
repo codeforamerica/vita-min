95e90d26b10f1f65a3ae92050699f41deceb71a0
Jonathan Greenberg <jonathan.s.greenberg@gmail.com>
2020-05-05 15:00:27 -0400

    Use valid_email2 gem for a bit stricter email validation
    - (#172627155) Fix email validation

--- app/forms/email_address_form.rb
+++ app/forms/email_address_form.rb
@@ -1,9 +1,6 @@
 class EmailAddressForm < QuestionsForm
   set_attributes_for :intake, :email_address, :email_address_confirmation
-  validates :email_address, format: {
-    with: URI::MailTo::EMAIL_REGEXP,
-    message: "Please enter a valid email address.",
-  }
+  validates :email_address, 'valid_email_2/email': { message: "Please enter a valid email address." }
   validates :email_address, confirmation: { message: "Please double check that the email addresses match." }
   validates :email_address_confirmation, presence: true
 
