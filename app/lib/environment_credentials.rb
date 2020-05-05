f9a90443992d68c1b4e6bf10a9321789469686f9
Jenny Heath <jheath@codeforamerica.org>
2020-05-05 11:57:03 -0700

    remove environment_credentials class

--- app/lib/environment_credentials.rb
+++
@@ -1,8 +0,0 @@
-class EnvironmentCredentials
-  class << self
-    def dig(*keys)
-      env_symbol = Rails.env.to_sym
-      Rails.application.credentials.dig(env_symbol, *keys)
-    end
-  end
-end
