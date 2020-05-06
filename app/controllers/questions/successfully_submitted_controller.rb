42b11360b56f6ca0f6616e51bbf07731931ad26a
Jenny Heath <jheath@codeforamerica.org>
2020-05-06 16:43:27 -0700

    Update copy and styles on successfully submitted page

--- app/controllers/questions/successfully_submitted_controller.rb
+++ app/controllers/questions/successfully_submitted_controller.rb
@@ -1,5 +1,6 @@
 module Questions
   class SuccessfullySubmittedController < QuestionsController
+    skip_before_action :require_intake
     append_after_action :reset_session, :track_page_view
 
     def edit
