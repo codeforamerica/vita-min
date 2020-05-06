c27c56aed8d1264a661be256527b37fa61504bf3
Molly T-M <mollyt@codeforamerica.org>
2020-05-06 16:31:49 -0700

    Only link sms messages to open ZD tickets
    - attach a comment to the sms ticket if there are no open related tickets

--- app/services/zendesk_sms_service.rb
+++ app/services/zendesk_sms_service.rb
@@ -44,22 +44,30 @@ class ZendeskSmsService
       EitcZendeskInstance::NEEDS_RESPONSE => true
     }
 
-    related_tickets = []
-    related_ticket_ids.each do |related_ticket_id|
-      related_tickets << get_ticket(ticket_id: related_ticket_id)
+    related_tickets = related_ticket_ids.map {|ticket_id| get_ticket(ticket_id: ticket_id)}
+    related_open_tickets = related_tickets.select{|ticket| ticket.status != "closed"}
+
+    if related_open_tickets.empty?
+      return append_comment_to_ticket(
+        ticket_id: sms_ticket_id,
+        comment: "This user has no associated open tickets.\ntext_user_has_no_other_open_ticket",
+        )
+    end
+
+    related_open_tickets.each do |related_ticket|
       append_comment_to_ticket(
-        ticket_id: related_ticket_id,
+        ticket_id: related_ticket.id,
         comment: related_ticket_comment_body,
         fields: related_ticket_fields
       )
     end
 
     # assign sms ticket to same group as most recently updated related ticket
-    most_recently_updated_ticket = related_tickets.sort_by(&:updated_at).last
+    most_recently_updated_ticket = related_open_tickets.sort_by(&:updated_at).last
 
     # comment on sms ticket with related ticket id's
     # update linked ticket field with related ticket id's
-    ticket_urls = related_ticket_ids.map { |id| ticket_url(id) }
+    ticket_urls = related_open_tickets.map { |ticket| ticket_url(ticket.id) }
     comment_body = ticket_urls.reduce("Linked to related tickets:\n") { |body, url| body + "• #{url}\n" }
     append_comment_to_ticket(
       ticket_id: sms_ticket_id,
