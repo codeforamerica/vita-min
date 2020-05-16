6666ca206e2381965cce1130779bc9fbd007c80f
Jonathan Greenberg <jonathan.s.greenberg@gmail.com>
2020-05-15 22:06:09 -0400

    Sends client to returning client page when duplicate intake found
    (#172730910) Add "returning client" page

--- app/controllers/questions/notification_preference_controller.rb
+++ app/controllers/questions/notification_preference_controller.rb
@@ -1,9 +1,16 @@
 module Questions
   class NotificationPreferenceController < QuestionsController
+    before_action :returning_client_guard
     private
 
     def tracking_data
       @form.attributes_for(:intake).reject { |k, _| k == :sms_phone_number }
+    end
+
+    def returning_client_guard
+      if DuplicateIntakeGuard.new(current_intake).has_duplicate?
+        redirect_to returning_client_path
+      end
     end
   end
 end
