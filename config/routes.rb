753b962437a115c3dc04b144299b1a6cca3a2986
Jonathan Greenberg <jonathan.s.greenberg@gmail.com>
2020-05-18 21:29:19 -0400

    Add ReturningClientController to QuestionNavigation flow  v (#172730910) Add "returning client" page

--- config/routes.rb
+++ config/routes.rb
@@ -62,8 +62,6 @@ Rails.application.routes.draw do
   post "/:organization/drop-offs", to: "intake_site_drop_offs#create", as: :create_drop_off
   get "/:organization/drop-off/:id", to: "intake_site_drop_offs#show", as: :show_drop_off
 
-  get "/returning-client", to: "questions/returning_client#show"
-
   get "/identity-needed", to: "offboarding#identity_needed"
   get "/other-options", to: "public_pages#other_options"
   get "/maybe-ineligible", to: "public_pages#maybe_ineligible"
