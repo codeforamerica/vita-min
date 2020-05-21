09761cdd83f56d85c3707fbf23913532174169a1
Ben Vandgrift <ben@vandgrift.com>
2020-05-21 16:05:59 -0400

    add token, index, and specs to diy_intake

--- app/models/diy_intake.rb
+++ app/models/diy_intake.rb
@@ -5,9 +5,38 @@
 #  id                 :bigint           not null, primary key
 #  preferred_name     :string
 #  state_of_residence :string
+#  token              :string
 #  created_at         :datetime         not null
 #  updated_at         :datetime         not null
 #
+# Indexes
+#
+#  index_diy_intakes_on_token  (token) UNIQUE
+#
 class DiyIntake < ApplicationRecord
-  
+  validates_presence_of(:preferred_name)
+  validates_presence_of(:state_of_residence)
+  validates_presence_of(:token)
+  validates_uniqueness_of(:token)
+  attr_readonly(:token)
+
+  before_validation :issue_token
+
+  ##
+  # issues a clean token prior to validation, if the model hasn't persisted.
+  # checks for duplicates.
+  def issue_token
+    return if persisted?
+
+    while DiyIntake.find_by(token: new_token = SecureRandom.urlsafe_base64(10)).present?
+      # nothing, just making sure this random number hasn't been used.
+    end
+
+    self.token = new_token
+  end
+
+  def token=(val)
+    return if persisted?
+    super
+  end
 end
---
+++ spec/models/diy_intake_spec.rb
@@ -0,0 +1,51 @@
+# == Schema Information
+#
+# Table name: diy_intakes
+#
+#  id                 :bigint           not null, primary key
+#  preferred_name     :string
+#  state_of_residence :string
+#  token              :string
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#
+# Indexes
+#
+#  index_diy_intakes_on_token  (token) UNIQUE
+#
+require 'rails_helper'
+
+describe DiyIntake do
+  describe 'validations' do
+    it { should validate_presence_of(:preferred_name) }
+    it { should validate_presence_of(:state_of_residence) }
+  end
+
+  describe 'token issuance' do
+    let(:diy_intake) { DiyIntake.new(preferred_name: "Someone Special", state_of_residence: "NC") }
+
+    it 'generates a token' do
+      token = diy_intake.issue_token()
+      expect(token).not_to be_nil
+      expect(token.length).to be >= 10
+    end
+
+    it 'assigns a token before persisting' do
+      expect(diy_intake.token).to be_nil
+      diy_intake.save!
+      expect(diy_intake.token).not_to be_nil
+    end
+
+    it 'ignores tokens submitted before persisting' do
+      diy_intake.token = "NONSENSE"
+      diy_intake.save!
+      expect(diy_intake.token).not_to eq("NONSENSE")
+    end
+
+    it 'ignores updates to tokens after creation' do
+      diy_intake.save!
+      diy_intake.token = "NONSENSE"
+      expect(diy_intake.token).not_to eq("NONSENSE")
+    end
+  end
+end
