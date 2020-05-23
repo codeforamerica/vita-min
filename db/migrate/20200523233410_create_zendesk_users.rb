c7a5b66c4e4d23c478f8c4c44801269e91ab6a47
Ben Golder <bgolder@codeforamerica.org>
2020-05-23 16:40:47 -0700

    Add new User model for ZD users
    Co-authored-by: Tom Dooner <tdooner@codeforamerica.org>

---
+++ db/migrate/20200523233410_create_zendesk_users.rb
@@ -0,0 +1,18 @@
+class CreateZendeskUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users do |t|
+      t.string :provider
+      t.string :uid
+      t.bigint :zendesk_user_id
+      t.string :name
+      t.string :email
+      t.string :role
+      t.string :ticket_restriction
+      t.boolean :two_factor_auth_enabled
+      t.boolean :active
+      t.boolean :suspended
+      t.boolean :verified
+      t.timestamps
+    end
+  end
+end
