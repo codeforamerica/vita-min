95e939bb9565ab0cfd3b27c6b7477e10c883d2ee
Ben Golder <bgolder@codeforamerica.org>
2020-05-24 12:51:45 -0700

    Add protected 13614-C and consent pdf pages

--- spec/controllers/zendesk/documents_controller_spec.rb
+++ spec/controllers/zendesk/documents_controller_spec.rb
@@ -6,44 +6,25 @@ RSpec.describe Zendesk::DocumentsController do
     let!(:document) { create :document, :with_upload, intake: intake }
     let(:user) { create :user, provider: "zendesk" }
 
-    context "when logged out" do
-      it "redirects to the sign in page" do
-        get :show, params: { id: document.id }
-        expect(response).to redirect_to(zendesk_sign_in_path)
+    it_behaves_like :a_protected_zendesk_ticket_page do
+      let(:valid_params) do
+        { id: document.id }
       end
     end
 
-    context "as an authenticated zendesk user" do
+    context "as an authenticated zendesk user with ticket access" do
+      let(:ticket) { instance_double(ZendeskAPI::Ticket) }
+
       before do
         allow(subject).to receive(:current_user).and_return(user)
+        allow(subject).to receive(:current_ticket).and_return(ticket)
       end
 
-      context "without access to the particular zendesk ticket" do
-        before do
-          allow(subject).to receive(:current_ticket).and_return nil
-        end
+      it "renders the document" do
+        get :show, params: { id: document.id }
 
-        it "renders not found" do
-          get :show, params: { id: document.id }
-
-          expect(response.status).to eq 404
-          expect(response).to render_template "public_pages/page_not_found"
-        end
-      end
-
-      context "with access to the ticket" do
-        let(:ticket) { instance_double(ZendeskAPI::Ticket) }
-
-        before do
-          allow(subject).to receive(:current_ticket).and_return(ticket)
-        end
-
-        it "renders the document" do
-          get :show, params: { id: document.id }
-
-          expect(response).to be_ok
-          expect(response.headers["Content-Type"]).to eq("image/jpeg")
-        end
+        expect(response).to be_ok
+        expect(response.headers["Content-Type"]).to eq("image/jpeg")
       end
     end
   end
