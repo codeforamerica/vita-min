78155c403ec20fa6bcec39fe44ca5d2511671ba1
loumoore <lourmoore@gmail.com>
2020-05-23 21:00:10 -0700

    i18n and locale switching setup

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,10 @@ module VitaMin
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    config.i18n.default_locale = :en
+    config.i18n.available_locales = [:en, :es]
+    config.i18n.fallbacks = [I18n.default_locale]
+
     config.active_job.queue_adapter = :delayed_job
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,88 @@
 Rails.application.routes.draw do
   root "public_pages#home"
 
+  # All routes in this scope will be prefixed with /locale if an available locale is set. See default_url_options in
+  # application_controller.rb and http://guides.rubyonrails.org/i18n.html for more info on this approach.
+  scope '(:locale)', locale: /#{I18n.available_locales.join('|')}/ do
+    get '/:locale' => 'public_pages#home'
+    root "public_pages#home"
+
+    resources :vita_providers, only: [:index, :show]
+    get "/vita_provider/map", to: "vita_providers#map"
+
+    resources :questions, controller: :questions do
+      collection do
+        QuestionNavigation.controllers.uniq.each do |controller_class|
+          { get: :edit, put: :update }.each do |method, action|
+            match "/#{controller_class.to_param}",
+                  action: action,
+                  controller: controller_class.controller_path,
+                  via: method
+          end
+        end
+      end
+    end
+
+    resources :documents, controller: :documents do
+      collection do
+        DocumentNavigation.controllers.uniq.each do |controller_class|
+          { get: :edit, put: :update }.each do |method, action|
+            match "/#{controller_class.to_param}",
+                  action: action,
+                  controller: controller_class.controller_path,
+                  via: method
+          end
+        end
+      end
+    end
+
+    namespace :documents do
+      delete "/requested-documents-later/remove", to: "requested_documents_later#destroy", as: :remove_requested_document
+      get "/requested-documents-later/not-found", to: "requested_documents_later#not_found", as: :requested_docs_not_found
+      get "/add/success", to: "send_requested_documents_later#success", as: :requested_documents_success
+      get "/add/:token", to: "requested_documents_later#edit", as: :add_requested_documents
+    end
+
+    resources :dependents, only: [:index, :new, :create, :edit, :update, :destroy]
+    resources :documents, only: [:destroy]
+    resources :ajax_mixpanel_events, only: [:create]
+
+    scope :diy, as: :diy do
+      DiyNavigation.controllers.uniq.each do |controller_class|
+        { get: :edit, put: :update }.each do |method, action|
+          match "/#{controller_class.to_param}",
+                action: action,
+                controller: controller_class.controller_path,
+                via: method
+        end
+      end
+      get "/:token", to: "diy/start_filing#start", as: :start_filing
+    end
+
+    get "/:organization/drop-off", to: "intake_site_drop_offs#new", as: :new_drop_off
+    post "/:organization/drop-offs", to: "intake_site_drop_offs#create", as: :create_drop_off
+    get "/:organization/drop-off/:id", to: "intake_site_drop_offs#show", as: :show_drop_off
+
+    get "/identity-needed", to: "offboarding#identity_needed"
+    get "/other-options", to: "public_pages#other_options"
+    get "/maybe-ineligible", to: "public_pages#maybe_ineligible"
+    get "/maintenance", to: "public_pages#maintenance"
+    get "/at-capacity", to: "public_pages#at_capacity"
+    get "/privacy", to: "public_pages#privacy_policy"
+    get "/about-us", to: "public_pages#about_us"
+    get "/tax-questions", to: "public_pages#tax_questions"
+    get "/faq", to: "public_pages#faq"
+    get "/stimulus", to: "public_pages#stimulus"
+    get "/500", to: "public_pages#internal_server_error"
+    get "/422", to: "public_pages#internal_server_error"
+    get "/404", to: "public_pages#page_not_found"
+
+    post "/zendesk-webhook/incoming", to: "zendesk_webhook#incoming", as: :incoming_zendesk_webhook
+    post "/email", to: "email#create"
+
+    # FSA routes
+    get '/diy/check-email', to: 'public_pages#check_email'
+  end
   mount Cfa::Styleguide::Engine => "/cfa"
 
   resources :vita_providers, only: [:index, :show]
