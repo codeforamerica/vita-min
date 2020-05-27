9d37fb9228ad0cdb8085d1a1bd7bb59f5201e91e
Molly T-M <mollyt@codeforamerica.org>
2020-05-27 10:50:34 -0700

    Queues up job to add comment to duplicate DIY intake ticket in ZD
    - adds resend_diy_confirmation message to trigger resending confirmation
    email in Zendesk
    - updates new DIY intake with requester_id and ticket_id from duplicate
    ticket to prevent multiple tickets from being created in Zendesk
    - sets current_diy_intake in StartFilingController so that id gets
    tracked in request payload for logs
    
    [#173016718] Prevent duplicate DIY tickets for the same requester.
    
    Co-authored-by: Jonathan Greenberg <jgreenberg@codeforamerica.org>

---
+++ spec/jobs/resend_diy_confirmation_email_job_spec.rb
@@ -0,0 +1,34 @@
+require "rails_helper"
+
+RSpec.describe ResendDiyConfirmationEmailJob, type: :job do
+  let(:fake_zendesk_intake_service) { double(ZendeskDiyIntakeService) }
+  let(:diy_intake) { create(:diy_intake) }
+
+  describe "#perform" do
+    context "without errors" do
+      it "appends a comment to the existing ticket" do
+        expect(ZendeskDiyIntakeService).to receive(:new).with(diy_intake)
+          .and_return(fake_zendesk_intake_service)
+        expect(fake_zendesk_intake_service).to receive(:append_resend_confirmation_email_comment)
+        described_class.perform_now(diy_intake.id)
+      end
+    end
+
+    context "with errors" do
+      context "when an error is raised while appending the comment" do
+        before do
+          expect(ZendeskDiyIntakeService).to receive(:new).with(diy_intake)
+            .and_return(fake_zendesk_intake_service)
+          expect(fake_zendesk_intake_service)
+            .to receive(:append_resend_confirmation_email_comment)
+            .and_raise(ZendeskServiceHelper::ZendeskServiceError)
+        end
+
+        it "raises the error" do
+          expect { described_class.perform_now(diy_intake.id) }
+            .to raise_error(ZendeskServiceHelper::ZendeskServiceError)
+        end
+      end
+    end
+  end
+end
