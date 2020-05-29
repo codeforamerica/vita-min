6eee96f98f4768293a4a3ff204e313b9b806709a
loumoore <lourmoore@gmail.com>
2020-05-28 21:18:51 -0700

    i18n documents views

--- app/controllers/documents/ids_controller.rb
+++ app/controllers/documents/ids_controller.rb
@@ -1,12 +1,8 @@
 module Documents
   class IdsController < DocumentUploadQuestionController
     def edit
-      @title = "Attach a photo of your ID card"
-      @help_text = "The IRS requires us to see a current drivers license, passport, or state ID."
       @names = [current_intake.primary_full_name]
       if current_intake.filing_joint_yes?
-        @title = "Attach photos of ID cards"
-        @help_text = "The IRS requires us to see a current drivers license, passport, or state ID for you and your spouse."
         @names << current_intake.spouse_name_or_placeholder
       end
       super
