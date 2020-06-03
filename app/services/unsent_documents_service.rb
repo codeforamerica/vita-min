05796d9232ab2c3406984abb2428b73c30a804c9
Jenny Heath <jheath@codeforamerica.org>
2020-06-03 11:54:52 -0700

    Datadog metrics
    Co-authored-by: Molly T-M <mollyt@codeforamerica.org>

--- app/services/unsent_documents_service.rb
+++ app/services/unsent_documents_service.rb
@@ -1,5 +1,3 @@
-
-
 class UnsentDocumentsService
   include ZendeskServiceHelper
   include Rails.application.routes.url_helpers
@@ -9,6 +7,7 @@ class UnsentDocumentsService
   end
 
   def detect_unsent_docs_and_notify
+    tickets_updated = 0
     Intake.where.not(intake_ticket_id: nil).find_each(batch_size: 100) do |intake|
       unsent_docs = intake.documents.where(zendesk_ticket_id: nil).where("created_at < ?", 15.minutes.ago)
       if unsent_docs.present?
@@ -23,7 +22,11 @@ class UnsentDocumentsService
           comment: comment_body
         )
         unsent_docs.each {|d| d.update(zendesk_ticket_id: intake.intake_ticket_id)}
+        tickets_updated += 1
+        DatadogApi.gauge("zendesk.ticket.docs.unsent.ticket_updated.document_count", unsent_docs.length)
       end
     end
+    DatadogApi.gauge("zendesk.ticket.docs.unsent.tickets_updated", tickets_updated)
+    DatadogApi.increment("cronjob.documents.unsent.detect_and_notify")
   end
 end
--- spec/services/unsent_documents_service_spec.rb
+++ spec/services/unsent_documents_service_spec.rb
@@ -1,9 +1,10 @@
 require "rails_helper"
 
 describe UnsentDocumentsService do
-  let(:service) { described_class.new }
+  let(:service) {described_class.new}
 
   describe "#detect_unsent_docs_and_notify" do
+    let(:fake_dogapi) {instance_double(Dogapi::Client, emit_point: nil)}
     let!(:intake_docs_sent) {create :intake, intake_ticket_id: 1}
     let!(:intake_docs_not_sent) {create :intake, intake_ticket_id: 2}
     let!(:intake_new_doc) {create :intake, intake_ticket_id: 3}
@@ -14,6 +15,16 @@ describe UnsentDocumentsService do
 
     before do
       allow(service).to receive(:append_comment_to_ticket)
+
+      DatadogApi.configure do |c|
+        c.enabled = true
+        c.namespace = "test.dogapi"
+      end
+      allow(Dogapi::Client).to receive(:new).and_return(fake_dogapi)
+    end
+
+    after do
+      DatadogApi.instance_variable_set("@dogapi_client", nil)
     end
 
     it "checks each intake for unsent documents and notifies Zendesk if any are found" do
@@ -44,6 +55,11 @@ describe UnsentDocumentsService do
 
       expect(document2.reload.zendesk_ticket_id).to eq intake_docs_not_sent.intake_ticket_id
       expect(document3.reload.zendesk_ticket_id).to eq intake_docs_not_sent.intake_ticket_id
+
+      expect(Dogapi::Client).to have_received(:new).once
+      expect(fake_dogapi).to have_received(:emit_point).once.with('test.dogapi.cronjob.documents.unsent.detect_and_notify', 1, {:tags => ["env:" + Rails.env], :type => "count"})
+      expect(fake_dogapi).to have_received(:emit_point).once.with('test.dogapi.zendesk.ticket.docs.unsent.tickets_updated', 1, {:tags => ["env:" + Rails.env], :type => "gauge"})
+      expect(fake_dogapi).to have_received(:emit_point).once.with('test.dogapi.zendesk.ticket.docs.unsent.ticket_updated.document_count', 2, {:tags => ["env:" + Rails.env], :type => "gauge"})
     end
 
     it "does not notify about or update unsent docs that are less than 15 minutes old" do
