91d2f979581da03f73b2267bdc62147a4e7840c1
Molly T-M <mollyt@codeforamerica.org>
2020-06-10 14:06:07 -0700

    Store locale on Intake when it is first created

--- app/controllers/questions/already_filed_controller.rb
+++ app/controllers/questions/already_filed_controller.rb
@@ -17,6 +17,7 @@ module Questions
       super.merge(
         source: current_intake.source || source,
         referrer: current_intake.referrer || referrer,
+        locale: current_intake.locale || I18n.locale,
         )
     end
 
