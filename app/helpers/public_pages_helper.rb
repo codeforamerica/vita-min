d34cef87c7e558a323a6a88cf9d44fbc888a9f72
Jonathan Greenberg <jgreenberg@codeforamerica.org>
2020-06-11 14:18:57 -0400

    Adds get started banner to bottom of stimulus page
    - refactors get started banner to shared partial
    - adds optional mixpanel data-click events to link in shared partial
    
    [#173198451] Add "get started" banner to bottom of the stimulus FAQ
    
    Signed-off-by: Ben Vandgrift <bvandgrift@codeforamerica.org>

--- app/helpers/public_pages_helper.rb
+++ app/helpers/public_pages_helper.rb
@@ -2,4 +2,14 @@ module PublicPagesHelper
   def enable_online_intake?
     true
   end
+
+  def mixpanel_info(locals)
+    mapping = {
+      mixpanel_label: 'data-track-click',
+      mixpanel_position: 'data-track-attribute-position',
+    }
+    mapping.reduce({}) do |acc, (k, v)|
+      locals[k].present? ? acc.merge("#{v}": locals[k]) : acc
+    end
+  end
 end
