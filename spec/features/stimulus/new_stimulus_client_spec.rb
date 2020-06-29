ee1ea9d06c4e72687c2ddb94bc97d77c9598e819
Ben Vandgrift <ben@vandgrift.com>
2020-06-29 17:17:38 -0400

    fixes misplacement of NullForm, ties up loose ends

--- spec/features/stimulus/new_stimulus_client_spec.rb
+++ spec/features/stimulus/new_stimulus_client_spec.rb
@@ -13,12 +13,28 @@ RSpec.feature "Stimulus Triage Flow" do
     # click_on("No") # => /filing-might-help
   end
 
-  scenario "new client has not filed this year" do
+  scenario "new stimulus triage client needs to correct" do
+    # TODO: finish flow and connect with welcome page as epic work continues
+    visit "/stimulus/filed-recently"
+    expect(page).to have_selector("h1", text: "Have you already filed for 2018 and 2019?")
+    click_on("Yes")
+    expect(page).to have_selector("h1", text: "Do you need to correct your 2019 taxes?")
+    click_on("Yes")
+    expect(page).to have_selector("h1", text: "We’ll help you collect your stimulus check by filing!")
+    click_on("Continue")
+    expect(page).to have_selector("h1", text: "What years do you need to file for?")
+  end
+
+  scenario "new stimulus triage client needs to file" do
+    # TODO: finish flow and connect with welcome page as epic work continues
     visit "/stimulus/filed-recently"
     expect(page).to have_selector("h1", text: "Have you already filed for 2018 and 2019?")
     click_on("No")
-
     expect(page).to have_selector("h1", text: "Do you need to file this year?")
+    click_on("Yes")
+    expect(page).to have_selector("h1", text: "We’ll help you collect your stimulus check by filing!")
+    click_on("Continue")
+    expect(page).to have_selector("h1", text: "What years do you need to file for?")
   end
 end
 
