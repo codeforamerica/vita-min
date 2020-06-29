8aede26c0a57a2d3755caace2c3bb84bd3e4c75f
Molly T-M <mollyt@codeforamerica.org>
2020-06-29 15:43:24 -0700

    Add Visit Stimulus FAQ page to triage workflow
    - add beginnings of FilingMightHelp page
    
    Co-authored-by: Ben Vandgrift <bvandgrift@codeforamerica.org>

--- spec/features/stimulus/new_stimulus_client_spec.rb
+++ spec/features/stimulus/new_stimulus_client_spec.rb
@@ -9,8 +9,21 @@ RSpec.feature "Stimulus Triage Flow" do
     expect(page).to have_selector("h1", text: "Do you need to correct your 2019 taxes?")
     click_on("No")
     expect(page).to have_selector("h1", text: "Have you already filed for 2016 and 2017?")
-    # click_on("Yes") # => /visit-stimulus-faq
     # click_on("No") # => /filing-might-help
+  end
+
+  scenario "new client visits the stimulus FAQ" do
+    # TODO: finish flow and connect with welcome page as epic work continues
+    visit "/stimulus/filed-recently"
+    expect(page).to have_selector("h1", text: "Have you already filed for 2018 and 2019?")
+    click_on("Yes")
+    expect(page).to have_selector("h1", text: "Do you need to correct your 2019 taxes?")
+    click_on("No")
+    expect(page).to have_selector("h1", text: "Have you already filed for 2016 and 2017?")
+    click_on("Yes")
+    expect(page).to have_selector("h1", text: "Get your stimulus check!")
+    click_on("Visit Stimulus FAQ")
+    expect(page).to have_selector("h1", text: "Get your Stimulus Payment (EIP)")
   end
 
   scenario "new stimulus triage client needs to correct" do
