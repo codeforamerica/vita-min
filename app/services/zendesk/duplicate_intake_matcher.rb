8db8d258848dfe2be9022c250d9d0675e88d7dd5
Jonathan Greenberg <jgreenberg@codeforamerica.org>
2020-06-29 14:41:31 -0400

    Adds DuplicateIntakeMatcher for identifying and merging strong matches
    - adds DuplicateDetection cli for running service
    
    [#173280533] run merging service on all strong matches
    
    Signed-off-by: Ben Vandgrift <bvandgrift@codeforamerica.org>

---
+++ app/services/zendesk/duplicate_intake_matcher.rb
@@ -0,0 +1,66 @@
+require "csv"
+
+module Zendesk
+  class DuplicateIntakeMatcher
+    AREL = Intake.arel_table
+
+    def strong_matches
+      return @strong_matches if @strong_matches
+      subquery = Intake.select(
+        Arel::Nodes::NamedFunction.new("ARRAY_AGG", [AREL[:id]]).as("intake_ids"),
+        Arel::Nodes::NamedFunction.new("ARRAY_AGG", [AREL[:intake_ticket_id]])
+          .as("ticket_ids"),
+        AREL[:id].count.as("match_count"),
+        AREL[:preferred_name].lower.as("lower_name"),
+        AREL[:email_address].lower.as("lower_email"),
+        :phone_number,
+        :needs_help_2019,
+        :needs_help_2018,
+        :needs_help_2017,
+        :needs_help_2016
+      ).group(:lower_name,
+              :lower_email,
+              :phone_number,
+              :needs_help_2019,
+              :needs_help_2018,
+              :needs_help_2017,
+              :needs_help_2016)
+
+      @strong_matches = Intake.from("(#{subquery.to_sql}) intakes")
+        .where(AREL[:match_count].gt(1))
+    end
+
+    def run(dry_run = true)
+      headers = ["name", "email", "phone", "intake ticket mapping", "primay ticket id"]
+      with_primaries = strong_matches.map do |matches|
+        mapping = matches.intake_ids.zip(matches.ticket_ids).to_h
+        primary = merging_service.find_primary_ticket(matches.ticket_ids.compact).id
+        [
+          matches.lower_name,
+          matches.lower_email,
+          matches.phone_number,
+          mapping,
+          primary,
+        ]
+      end
+      csv = ::CSV.generate do |csv|
+        csv << headers
+        with_primaries.each { |row| csv << row }
+      end
+      merge_duplicates unless dry_run
+      csv
+    end
+
+    private
+
+    def merge_duplicates
+      strong_matches.each do |matches|
+        merging_service.merge_duplicate_tickets(matches.intake_ids)
+      end
+    end
+
+    def merging_service
+      @merging_service ||= TicketMergingService.new
+    end
+  end
+end
