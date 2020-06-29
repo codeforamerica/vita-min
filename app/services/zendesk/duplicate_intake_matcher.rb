5f2e114ee9409ca8fc7a3b664e070fea763a0eaf
Jonathan Greenberg <jonathan.s.greenberg@gmail.com>
2020-06-29 18:43:16 -0400

    Trim whitespace from string fields for grouping
    - Use lazy enumeration when mapping over strong_matches
    
    [#173280533] run merging service on all strong matches

--- app/services/zendesk/duplicate_intake_matcher.rb
+++ app/services/zendesk/duplicate_intake_matcher.rb
@@ -6,38 +6,41 @@ module Zendesk
 
     def strong_matches
       return @strong_matches if @strong_matches
+      # ARRAY_AGG collects the intake ids and intake_ticket_ids for the matching
+      # intakes based on the grouping fields
       subquery = Intake.select(
         Arel::Nodes::NamedFunction.new("ARRAY_AGG", [AREL[:id]]).as("intake_ids"),
         Arel::Nodes::NamedFunction.new("ARRAY_AGG", [AREL[:intake_ticket_id]])
           .as("ticket_ids"),
         AREL[:id].count.as("match_count"),
-        AREL[:preferred_name].lower.as("lower_name"),
-        AREL[:email_address].lower.as("lower_email"),
-        :phone_number,
+        AREL[:preferred_name].lower.trim.as("lower_name"),
+        AREL[:email_address].lower.trim.as("lower_email"),
+        AREL[:phone_number].trim.as("phone"),
         :needs_help_2019,
         :needs_help_2018,
         :needs_help_2017,
         :needs_help_2016
-      ).group(:lower_name,
-              :lower_email,
-              :phone_number,
-              :needs_help_2019,
-              :needs_help_2018,
-              :needs_help_2017,
-              :needs_help_2016)
+      ).group(
+        :lower_name,
+        :lower_email,
+        :phone,
+        :needs_help_2019,
+        :needs_help_2018,
+        :needs_help_2017,
+        :needs_help_2016)
 
       @strong_matches = Intake.from(subquery, :intakes).where(AREL[:match_count].gt(1))
     end
 
     def run(dry_run = true)
       headers = ["name", "email", "phone", "filing years", "intake ticket mapping", "primary ticket id"]
-      with_primaries = strong_matches.map do |matches|
+      with_primaries = strong_matches.lazy.map do |matches|
         mapping = matches.intake_ids.zip(matches.ticket_ids).to_h
         primary = merging_service.find_primary_ticket(matches.ticket_ids.compact).id
         [
           matches.lower_name,
           matches.lower_email,
-          matches.phone_number,
+          matches.phone,
           matches.filing_years.join(", "),
           mapping,
           primary,
