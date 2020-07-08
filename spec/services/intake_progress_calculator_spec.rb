214832683c643b098ad4cb4f4dede985a2641e3e
Ben Golder <bgolder@codeforamerica.org>
2020-07-08 10:38:46 -0700

    Adjust scope of progress bar

--- spec/services/intake_progress_calculator_spec.rb
+++ spec/services/intake_progress_calculator_spec.rb
@@ -1,27 +1,16 @@
 require "rails_helper"
 
 describe IntakeProgressCalculator do
-  describe "::POSSIBLE_STEPS" do
-    it "inserts the document flow into the question flow after document overview" do
-      expect(IntakeProgressCalculator::POSSIBLE_STEPS.index(Questions::OverviewDocumentsController))
-        .to eq(IntakeProgressCalculator::POSSIBLE_STEPS.index(DocumentNavigation::FLOW.first) - 1)
-    end
-
-    it "starts with the backtaxes controller" do
-      expect(IntakeProgressCalculator::POSSIBLE_STEPS.first).to eq Questions::BacktaxesController
-    end
-  end
-
   describe "#get_progress" do
     let (:intake) { create :intake }
     it "returns 0 for the progress of the starting controller and intake" do
-      progress = IntakeProgressCalculator.get_progress(Questions::BacktaxesController, intake)
+      progress = IntakeProgressCalculator.get_progress(Questions::WasStudentController, intake)
 
       expect(progress).to eq 0
     end
 
     it "returns 100 for the progress of the final controller and intake" do
-      progress = IntakeProgressCalculator.get_progress(Questions::SuccessfullySubmittedController, intake)
+      progress = IntakeProgressCalculator.get_progress(Questions::AdditionalInfoController, intake)
 
       expect(progress).to eq 100
     end
@@ -31,12 +20,13 @@ describe IntakeProgressCalculator do
     end
 
     it "includes all possible future steps until you reach the question" do
-      intake_survey_yes = Intake.new(demographic_questions_opt_in: :yes)
-      intake_survey_no = Intake.new(demographic_questions_opt_in: :no)
-      intake_survey_unfilled = Intake.new
+      ever_married_yes = Intake.new(ever_married: :yes)
+      ever_married_no = Intake.new(ever_married: :no)
+      ever_married_unfilled = Intake.new
+      controller_before_question = Questions::IssuedIdentityPinController
 
-      expect(IntakeProgressCalculator.get_progress(Questions::MailingAddressController, intake_survey_yes)).to eq IntakeProgressCalculator.get_progress(Questions::MailingAddressController, intake_survey_no)
-      expect(IntakeProgressCalculator.get_progress(Questions::MailingAddressController, intake_survey_yes)).to eq IntakeProgressCalculator.get_progress(Questions::MailingAddressController, intake_survey_unfilled)
+      expect(IntakeProgressCalculator.get_progress(controller_before_question, ever_married_yes)).to eq IntakeProgressCalculator.get_progress(controller_before_question, ever_married_no)
+      expect(IntakeProgressCalculator.get_progress(controller_before_question, ever_married_yes)).to eq IntakeProgressCalculator.get_progress(controller_before_question, ever_married_unfilled)
     end
   end
 end
