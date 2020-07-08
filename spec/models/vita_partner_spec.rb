4f751f0484b79a267bc9b540c7f6f4f9c6610351
Ben Golder <bgolder@codeforamerica.org>
2020-07-08 13:29:15 -0700

    Annotate vita_partner_spec
    Co-authored-by: Asheesh Laroia <alaroia@codeforamerica.org>

--- spec/models/vita_partner_spec.rb
+++ spec/models/vita_partner_spec.rb
@@ -1,3 +1,19 @@
+# == Schema Information
+#
+# Table name: vita_partners
+#
+#  id                      :bigint           not null, primary key
+#  accepts_overflow        :boolean          default(FALSE)
+#  display_name            :string
+#  logo_path               :string
+#  name                    :string           not null
+#  source_parameter        :string
+#  weekly_capacity_limit   :integer
+#  zendesk_instance_domain :string           not null
+#  created_at              :datetime         not null
+#  updated_at              :datetime         not null
+#  zendesk_group_id        :string           not null
+#
 require "rails_helper"
 
 describe VitaPartner do
