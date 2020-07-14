6d887ecd9e4b6b5cefb630e59b05924d243a34b5
Jonathan Greenberg <jgreenberg@codeforamerica.org>
2020-07-14 11:32:11 -0400

    Removes at capacity exclusion for intakes started with partner sources
    [#173787113] Remove unique URL exclusion from at capacity page

--- app/controllers/questions/at_capacity_controller.rb
+++ app/controllers/questions/at_capacity_controller.rb
@@ -15,8 +15,7 @@ module Questions
       def show?(intake)
         return false unless intake.vita_partner
 
-        intake_from_partner_url = intake.vita_partner.source_parameters.pluck(:code).include?(intake.source)
-        intake.vita_partner.at_capacity? && !intake_from_partner_url
+        intake.vita_partner.at_capacity?
       end
     end
   end
