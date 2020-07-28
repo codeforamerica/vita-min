ab1831c795cdf61e020fb82417304f0556de899e
Asheesh Laroia <alaroia@codeforamerica.org>
2020-07-28 13:43:42 -0700

    Remove unnecessary change
    Co-authored-by: Ben Golder <bgolder@codeforamerica.org>

--- app/controllers/documents/additional_documents_controller.rb
+++ app/controllers/documents/additional_documents_controller.rb
@@ -1,22 +1,7 @@
 module Documents
   class AdditionalDocumentsController < DocumentUploadQuestionController
-
-    DocType = Struct.new(:doctype, :key)
-
-    DOC_HELP_MAPPING = [
-        []
-    ]
-
     def self.document_type
       "Other"
-    end
-
-    def likely(intake)
-      thing = [
-          [intake.had_interest_income_yes?, DocType.new('1099-DIV', 'thing.stuff.key') ]
-      ]
-
-      thing.filter { |t| t.first }
     end
   end
 end
