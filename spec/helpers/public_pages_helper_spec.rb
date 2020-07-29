c5e737556412db8eb793d8ef598810339cc61e9f
Jenny Heath <jheath@codeforamerica.org>
2020-07-29 11:55:57 -0700

    Remove enable_online_intake? feature flag

--- spec/helpers/public_pages_helper_spec.rb
+++
@@ -1,9 +0,0 @@
-require "rails_helper"
-
-RSpec.describe PublicPagesHelper do
-  describe "#enable_online_intake?" do
-    it "returns true" do
-      expect(helper.enable_online_intake?).to eq(true)
-    end
-  end
-end
