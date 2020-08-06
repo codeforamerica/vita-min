358d96fcfa753a43edde912396c8b3ee236a357f
Ben Golder <bgolder@codeforamerica.org>
2020-08-06 12:15:11 -0700

    Add environment specific credentials
    https://www.pivotaltracker.com/story/show/173402109
    
    Co-authored-by: Dimitri DeFigueiredo <ddefigueiredo@codeforamerica.org>

--- config/initializers/datadog.rb
+++ config/initializers/datadog.rb
@@ -5,12 +5,12 @@ Datadog.configure do |c|
   c.use :aws
   c.use :delayed_job
   c.tracer.enabled = Rails.env.staging? || Rails.env.demo? || Rails.env.production?
-  c.tracer hostname: Rails.application.credentials.dig(Rails.env.to_sym, :datadog_agent_host)
+  c.tracer hostname: Rails.application.credentials.dig(:datadog_agent_host)
 end
 
 DatadogApi.configure do |c|
   c.env = Rails.env
-  c.api_key = Rails.application.credentials.dig(Rails.env.to_sym, :datadog_api_key)
+  c.api_key = Rails.application.credentials.dig(:datadog_api_key)
   c.namespace = "vita-min.dogapi"
   c.enabled = Rails.env.staging? || Rails.env.demo? || Rails.env.production?
 end
--- spec/lib/environment_credentials_spec.rb
+++ spec/lib/environment_credentials_spec.rb
@@ -4,11 +4,8 @@ RSpec.describe EnvironmentCredentials do
   describe ".dig" do
     let(:mock_credentials) do
       {
-        test: {
+        service_key: {
           test_key: "test-value",
-        },
-        production: {
-          test_key: "other-value"
         }
       }
     end
@@ -18,14 +15,14 @@ RSpec.describe EnvironmentCredentials do
         allow(Rails.application).to receive(:credentials).and_return(mock_credentials)
       end
 
-      it "returns the value for the current environment" do
-        expect(EnvironmentCredentials.dig(:test_key)).to eq("test-value")
+      it "returns the value for the key" do
+        expect(EnvironmentCredentials.dig(:service_key, :test_key)).to eq("test-value")
       end
     end
 
     context "when the key does not exist" do
       it "returns nil" do
-        expect(EnvironmentCredentials.dig(:nonexistent_key)).to be_nil
+        expect(EnvironmentCredentials.dig(:service_key, :nonexistent_key)).to be_nil
       end
     end
 
