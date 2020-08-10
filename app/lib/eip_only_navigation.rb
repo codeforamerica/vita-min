0b72868e24437744ebc007811fd4feab1fa43daf
Ben Golder <bgolder@codeforamerica.org>
2020-08-10 14:41:43 -0700

    Add EipOnlyNavigation
    Co-authored-by: Jenny Heath <jheath@codeforamerica.org>

---
+++ app/lib/eip_only_navigation.rb
@@ -0,0 +1,70 @@
+class EipOnlyNavigation
+  FLOW = [
+    # Triage
+    Questions::EipOnlyController, # create new Intake with eip_only: true
+    Questions::EnvironmentWarningController,
+
+    # Overview
+    Questions::OverviewController,
+
+    # Contact information
+    Questions::PersonalInfoController,
+    Questions::AtCapacityController,
+    Questions::ChatWithUsController,
+    Questions::PhoneNumberController,
+    Questions::EmailAddressController,
+    Questions::ReturningClientController, # possible off-boarding from flow
+    Questions::NotificationPreferenceController,
+
+    # Consent
+    Questions::ConsentController, # create Zendesk ticket
+
+    # Primary filer personal information
+    Questions::IssuedIdentityPinController,
+
+    # Marital Status
+    Questions::EverMarriedController, # Begins requiring ZD ticket
+
+    # Filing status
+    Questions::FilingJointController,
+
+    # Spouse email
+    Questions::SpouseEmailAddressController,
+
+    # Spouse personal information
+    Questions::SpouseConsentController,
+    Questions::SpouseIssuedIdentityPinController,
+
+    # Dependents
+    Questions::HadDependentsController,
+
+    # Additional Information
+    Questions::AdditionalInfoController, # appends 13614-C & consent PDF to Zendesk ticket
+
+    # Documents --> See DocumentNavigation
+
+    # Interview time preferences
+    Questions::InterviewSchedulingController,
+
+    # Payment info
+    Questions::MailingAddressController,
+
+    # Optional Demographic Questions
+    Questions::DemographicQuestionsController,
+    Questions::DemographicEnglishConversationController,
+    Questions::DemographicEnglishReadingController,
+    Questions::DemographicDisabilityController,
+    Questions::DemographicVeteranController,
+    Questions::DemographicPrimaryRaceController,
+    Questions::DemographicSpouseRaceController,
+    Questions::DemographicPrimaryEthnicityController,
+    Questions::DemographicSpouseEthnicityController,
+
+    # Additional Information
+    Questions::FinalInfoController, # appends final 13614-C, bank details & docs to Zendesk
+    Questions::SuccessfullySubmittedController,
+    Questions::FeedbackController,
+  ].freeze
+
+  include ControllerNavigation
+end
---
+++ spec/lib/eip_only_navigation_spec.rb
@@ -0,0 +1,5 @@
+require "rails_helper"
+
+RSpec.describe EipOnlyNavigation do
+  it_behaves_like "controller navigation flow", EipOnlyNavigation
+end
