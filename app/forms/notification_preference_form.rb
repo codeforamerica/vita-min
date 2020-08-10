0b09da9971904ec495f6b1b64a2c16493d7f85e0
Ben Golder <bgolder@codeforamerica.org>
2020-08-10 10:11:20 -0700

    Require one method of contact
    Co-authored-by: Jenny Heath <jheath@codeforamerica.org>

--- app/forms/notification_preference_form.rb
+++ app/forms/notification_preference_form.rb
@@ -2,6 +2,7 @@ class NotificationPreferenceForm < QuestionsForm
   set_attributes_for :intake, :sms_phone_number, :sms_notification_opt_in, :email_notification_opt_in
   before_validation :parse_sms_phone_number
   validate :need_phone_number_for_sms_opt_in
+  validate :need_one_communication_method
 
   def save
     intake.update(attributes_for(:intake))
@@ -28,5 +29,10 @@ class NotificationPreferenceForm < QuestionsForm
   def need_phone_number_for_sms_opt_in
     has_phone_if_needed = sms_notification_opt_in == "yes" ? sms_phone_number.present? : true
     errors.add(:sms_phone_number, I18n.t("forms.errors.need_phone_number")) unless has_phone_if_needed
+  end
+
+  def need_one_communication_method
+    opted_out_of_both = sms_notification_opt_in == "no" && email_notification_opt_in =="no"
+    errors.add(:sms_notification_opt_in, I18n.t("forms.errors.need_one_communication_method")) if opted_out_of_both
   end
 end
---
+++ spec/forms/notification_preference_form_spec.rb
@@ -0,0 +1,51 @@
+require "rails_helper"
+
+RSpec.describe NotificationPreferenceForm do
+  let(:intake) { create :intake }
+
+  describe "#need_one_communication_method" do
+    context "opted into sms" do
+      it "is valid" do
+        form = NotificationPreferenceForm.new(
+          intake,
+          {
+            sms_phone_number: "1 (415) 553-7865",
+            sms_notification_opt_in: "yes",
+            email_notification_opt_in: "no",
+          }
+        )
+
+        expect(form).to be_valid
+      end
+    end
+
+    context "opted into email" do
+      it "is valid" do
+        form = NotificationPreferenceForm.new(
+          intake,
+          {
+            sms_notification_opt_in: "no",
+            email_notification_opt_in: "yes",
+          }
+        )
+
+        expect(form).to be_valid
+      end
+    end
+
+    context "opted into neither" do
+      it "is invalid and adds an error to sms_notification_opt_in" do
+        form = NotificationPreferenceForm.new(
+          intake,
+          {
+            sms_notification_opt_in: "no",
+            email_notification_opt_in: "no",
+          }
+        )
+
+        expect(form).not_to be_valid
+        expect(form.errors[:sms_notification_opt_in]).to be_present
+      end
+    end
+  end
+end
