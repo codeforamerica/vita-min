e0f150fd589bfcf3e7a70a8f87c7a91ba67134cb
loumoore <lourmoore@gmail.com>
2020-08-20 17:12:41 -0700

    full service landing page, so we can continue to do digital outreach for partners with capacity, in a more effective way than sending people to the homepage

--- config/routes.rb
+++ config/routes.rb
@@ -82,6 +82,7 @@ Rails.application.routes.draw do
     get "/tax-questions", to: "public_pages#tax_questions"
     get "/faq", to: "public_pages#faq"
     get "/stimulus", to: "public_pages#stimulus"
+    get "/full-service", to: "public_pages#full_service_home"
     get "/500", to: "public_pages#internal_server_error"
     get "/422", to: "public_pages#internal_server_error"
     get "/404", to: "public_pages#page_not_found"
