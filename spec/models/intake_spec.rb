86457e8ae37bf68675b52ade55ec8fbd9cd303a2
Asheesh Laroia <alaroia@codeforamerica.org>
2020-08-26 11:35:18 -0700

    Move FL statewide routing to United Way Suncoast

--- spec/models/intake_spec.rb
+++ spec/models/intake_spec.rb
@@ -838,7 +838,7 @@ describe Intake do
       it_behaves_like :state_level_routing, "NV", "Nevada Free Taxes Coalition", "eitc"
       it_behaves_like :state_level_routing, "TX", "Foundation Communities", "eitc"
       it_behaves_like :state_level_routing, "VA", "United Way of Greater Richmond and Petersburg", "eitc"
-      it_behaves_like :state_level_routing, "FL", "Tax Help Colorado (Piton Foundation)", "eitc"
+      it_behaves_like :state_level_routing, "FL", "United Way Suncoast", "eitc"
       it_behaves_like :state_level_routing, "MD", "CASH Campaign of MD", "eitc"
       it_behaves_like :state_level_routing, "NY", "Urban Upbound (NY)", "eitc"
       it_behaves_like :state_level_routing, "RI", "Urban Upbound (NY)", "eitc"
