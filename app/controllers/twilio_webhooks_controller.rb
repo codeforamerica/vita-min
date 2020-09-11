2374cdff91f215eac93239c7fab8c0a4c9f4169b
Ben Golder <bgolder@codeforamerica.org>
2020-09-11 07:40:18 -0700

    Webhooks inherit from ActionController::Base

--- app/controllers/twilio_webhooks_controller.rb
+++ app/controllers/twilio_webhooks_controller.rb
@@ -1,13 +1,4 @@
-class TwilioWebhooksController < ApplicationController
-  skip_before_action :redirect_to_getyourrefund
-  skip_before_action :set_visitor_id
-  skip_before_action :set_source
-  skip_before_action :set_referrer
-  skip_before_action :set_utm_state
-  skip_before_action :set_sentry_context
-  skip_before_action :check_maintenance_mode
-  skip_before_action :verify_authenticity_token
-  skip_after_action :track_page_view
+class TwilioWebhooksController < ActionController::Base
   before_action :validate_twilio_request
 
   def update_outgoing_text_message
