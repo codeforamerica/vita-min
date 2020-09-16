7318862a60520bd7bd3f8e5b01c87432803f12e9
Asheesh Laroia <alaroia@codeforamerica.org>
2020-09-16 11:56:18 -0700

    Stick to default path for logout (our customization was not working anyway)
    Co-authored-by: Ben Golder <bgolder@codeforamerica.org>

--- config/routes.rb
+++ config/routes.rb
@@ -106,9 +106,7 @@ Rails.application.routes.draw do
     resources :clients, only: [:show, :create]
     resources :outgoing_text_messages, only: [:create]
     resources :outgoing_emails, only: [:create]
-    devise_for :users, skip: :omniauth_callbacks do
-      delete "sign-out", :to => "users/sessions#destroy", as: :destroy_user_session
-    end
+    devise_for :users, skip: :omniauth_callbacks
     get "/users/profile" => "users#profile", as: :user_profile
 
     # Any other top level slash just goes to home as a source parameter
