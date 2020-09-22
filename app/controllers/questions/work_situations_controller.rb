521dc22993095dfca9f96ea119880f5a69d300cf
loumoore <lourmoore@gmail.com>
2020-09-21 19:10:08 -1000

    remove skip logic and corresponding test

--- app/controllers/questions/work_situations_controller.rb
+++ app/controllers/questions/work_situations_controller.rb
@@ -1,9 +1,5 @@
 module Questions
   class WorkSituationsController < QuestionsController
-    def self.show?(intake)
-      intake.had_a_job?
-    end
-
     def illustration_path
       "job-count.svg"
     end
