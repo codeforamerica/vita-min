98c2c4a3998afa35fd638765873eeec3f7d81c9e
Ben Golder <bgolder@codeforamerica.org>
2020-09-24 09:09:40 -0700

    Move TX to Baker Ripley
    Co-authored-by: Jenny Heath <jheath@codeforamerica.org>

--- spec/models/intake_spec.rb
+++ spec/models/intake_spec.rb
@@ -853,7 +853,7 @@ describe Intake do
       it_behaves_like :state_level_routing, "CO", "Tax Help Colorado (Piton Foundation)", "eitc"
       it_behaves_like :state_level_routing, "CA", "[United Way California] Online Intake", "eitc"
       it_behaves_like :state_level_routing, "FL", "[United Way California] Online Intake", "eitc"
-      it_behaves_like :state_level_routing, "TX", "[United Way California] Online Intake", "eitc"
+      it_behaves_like :state_level_routing, "TX", "BakerRipley Neighborhood Tax Centers", "eitc"
       it_behaves_like :state_level_routing, "WA", "United Way of King County", "eitc"
       it_behaves_like :state_level_routing, "PA", "Campaign for Working Families", "eitc"
       it_behaves_like :state_level_routing, "NV", "Nevada Free Taxes Coalition", "eitc"
