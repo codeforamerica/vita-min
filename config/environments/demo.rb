3aa79ab35bac8df543422b07333f07c127941f29
loumoore <lourmoore@gmail.com>
2020-09-28 20:48:19 -1000

    move setting to shared_deployment_config

--- config/environments/demo.rb
+++ config/environments/demo.rb
@@ -1,9 +1,6 @@
 require_relative "./shared_deployment_config"
 
 Rails.application.configure do
-  config.public_file_server.headers = {
-      'Cache-Control' => 'public, max-age=31536000'
-  }
   config.active_storage.service = :s3_demo
 
   config.action_mailer.default_options = { from: 'hello@mg-demo.getyourrefund-testing.org' }
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,11 +1,6 @@
 require_relative "./shared_deployment_config"
 
 Rails.application.configure do
-  # Set cache control headers
-  config.public_file_server.headers = {
-      'Cache-Control' => 'public, max-age=31536000'
-  }
-
   config.active_storage.service = :s3_prod
 
   config.action_mailer.default_options = { from: "hello@getyourrefund.org" }
