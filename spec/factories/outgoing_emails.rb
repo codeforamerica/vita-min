2480d7d40a31e8cdb70d8f1c15d6bdac617ffaf7
Asheesh Laroia <alaroia@codeforamerica.org>
2020-10-01 15:34:22 -0700

    Add required fields to factories
    Also rename outgoing_emails factory so it can be appropriately plural
    
    Co-authored-by: Jenny Heath <jheath@codeforamerica.org>

---
+++ spec/factories/outgoing_emails.rb
@@ -0,0 +1,34 @@
+# == Schema Information
+#
+# Table name: outgoing_emails
+#
+#  id         :bigint           not null, primary key
+#  body       :string           not null
+#  sent_at    :datetime         not null
+#  subject    :string           not null
+#  to         :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  client_id  :bigint           not null
+#  user_id    :bigint           not null
+#
+# Indexes
+#
+#  index_outgoing_emails_on_client_id  (client_id)
+#  index_outgoing_emails_on_user_id    (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (client_id => clients.id)
+#  fk_rails_...  (user_id => users.id)
+#
+FactoryBot.define do
+  factory :outgoing_email do
+    client
+    user
+    body { "nothin" }
+    subject { "Update from GetYourRefund" }
+    to { "outgoing@example.com" }
+    sequence(:sent_at) { |n| DateTime.new(2020, 9, 2, 15, 1, 30) + n.minutes }
+  end
+end
