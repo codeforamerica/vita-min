e66d63bb398be52055e3882030b435faa63c3c49
Asheesh Laroia <alaroia@codeforamerica.org>
2020-10-05 15:38:55 -0700

    Send email attachments
    Co-authored-by: Shannon Byrne <sbyrne@codeforamerica.org>

--- app/mailers/outgoing_email_mailer.rb
+++ app/mailers/outgoing_email_mailer.rb
@@ -1,6 +1,11 @@
 class OutgoingEmailMailer < ApplicationMailer
   def user_message(outgoing_email:)
     @outgoing_email = outgoing_email
+
+    outgoing_email.attachments.each do |attachment|
+      attachments[attachment.filename.to_s] = attachment.blob.download
+    end
+
     mail(
       to: outgoing_email.to,
       subject: outgoing_email.subject,
