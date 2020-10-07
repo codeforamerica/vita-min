52636bb200decf648283bae3db6d6b478a481487
Asheesh Laroia <alaroia@codeforamerica.org>
2020-10-07 09:41:34 -0700

    Add OutgoingTextMessage to db:seed
    Co-authored-by: Alex Sartan <asartan@vmware.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
 fake_vita_partner = VitaPartner.find_or_create_by(name: 'Fake Vita Partner', zendesk_group_id: 'foo', zendesk_instance_domain: 'eitc')
 
 # basic beta tester
-User.where(email: 'skywalker@example.com').first_or_initialize.update(
+user = User.where(email: 'skywalker@example.com').first_or_initialize
+user.update(
   name: 'Luke',
   password: 'theforcevita',
   vita_partner: fake_vita_partner,
@@ -14,3 +15,7 @@ intake = Intake.create
 
 Document.find_or_create_by(display_name: 'My Employment', document_type: 'Employment', client: client, created_at: 1.day.ago, intake: intake)
 Document.find_or_create_by(display_name: 'Identity Document', document_type: 'ID', client: client, created_at: 2.months.ago, intake: intake)
+
+OutgoingTextMessage.create!(client: client, body: "Hey client, how is it going", user: user, sent_at: 3.days.ago, to_phone_number: "+14155551212")
+OutgoingTextMessage.create!(client: client, body: "Hey client, how is it going", user: user, sent_at: 2.days.ago, to_phone_number: "+14155551212")
+OutgoingTextMessage.create!(client: client, body: "Hey client, how is it going", user: user, sent_at: DateTime.now, to_phone_number: "+14155551212")
