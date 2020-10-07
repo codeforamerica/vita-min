4d82f6a83764c24aac4890a4b74fb014807f5df0
Asheesh Laroia <alaroia@codeforamerica.org>
2020-10-07 08:25:07 -0700

    Add useful `rake db:seed` config
    Co-authored-by: Sarah Niemeyer <sniemeyer@vmware.com>
    Co-authored-by: Alex Sartan <asartan@vmware.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+fake_vita_partner = VitaPartner.find_or_create_by(name: 'Fake Vita Partner', zendesk_group_id: 'foo', zendesk_instance_domain: 'eitc')
+
+# basic beta tester
+User.where(email: 'skywalker@example.com').first_or_initialize.update(
+  name: 'Luke',
+  password: 'theforcevita',
+  vita_partner: fake_vita_partner,
+  is_beta_tester: true
+)
+
+client = Client.find_or_create_by(preferred_name: 'Captain', email_address: 'crunch@example.com', vita_partner: fake_vita_partner)
+
+intake = Intake.create
+
+Document.find_or_create_by(display_name: 'My Employment', document_type: 'Employment', client: client, created_at: 1.day.ago, intake: intake)
+Document.find_or_create_by(display_name: 'Identity Document', document_type: 'ID', client: client, created_at: 2.months.ago, intake: intake)
