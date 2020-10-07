754cea6de02c7406979980ef44cad86fb1d25817
Alex Sartan and Asheesh Laroia <asartan@codeforamerica.org>
2020-10-07 12:49:25 -0600

    Vary message bodies in seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,6 +16,6 @@ intake = Intake.create
 Document.find_or_create_by(display_name: 'My Employment', document_type: 'Employment', client: client, created_at: 1.day.ago, intake: intake)
 Document.find_or_create_by(display_name: 'Identity Document', document_type: 'ID', client: client, created_at: 2.months.ago, intake: intake)
 
-OutgoingTextMessage.create!(client: client, body: "Hey client, how is it going", user: user, sent_at: 3.days.ago, to_phone_number: "+14155551212")
-OutgoingTextMessage.create!(client: client, body: "Hey client, how is it going", user: user, sent_at: 2.days.ago, to_phone_number: "+14155551212")
-OutgoingTextMessage.create!(client: client, body: "Hey client, how is it going", user: user, sent_at: DateTime.now, to_phone_number: "+14155551212")
+OutgoingTextMessage.create!(client: client, body: "Hey client, nice to meet you", user: user, sent_at: 3.days.ago, to_phone_number: "+14155551212")
+OutgoingTextMessage.create!(client: client, body: "Hope you're having a good day", user: user, sent_at: 2.days.ago, to_phone_number: "+14155551212")
+OutgoingTextMessage.create!(client: client, body: "Thanks and have a good night!", user: user, sent_at: DateTime.now, to_phone_number: "+14155551212")
