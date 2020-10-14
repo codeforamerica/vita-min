213cc6bc7509d6ad51226c8687b12cad465644d1
Alex Sartan <asartan@codeforamerica.org>
2020-10-14 12:58:05 -0600

    Remove unused javascript

--- app/javascript/channels/index.js
+++
@@ -1,9 +0,0 @@
-// Load channel JS files within this directory and all subdirectories.
-// We require() them explicitly. By convention, they end in *_channel.js.
-require('./client_channel.js');
-
-function sayHello() {
-    return "hello!";
-}
-
-module.exports = sayHello;
