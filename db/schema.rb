d25ca3a6ff7bd9c899dca9e12a6f7a4cac70919b
Alex Sartan <asartan@codeforamerica.org>
2020-11-06 10:43:29 -0700

    Remove is_beta_tester column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_29_212035) do
+ActiveRecord::Schema.define(version: 2020_11_06_174103) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -527,7 +527,6 @@ ActiveRecord::Schema.define(version: 2020_10_29_212035) do
     t.integer "invitations_count", default: 0
     t.bigint "invited_by_id"
     t.boolean "is_admin", default: false, null: false
-    t.boolean "is_beta_tester", default: false, null: false
     t.datetime "last_sign_in_at"
     t.string "last_sign_in_ip"
     t.datetime "locked_at"
