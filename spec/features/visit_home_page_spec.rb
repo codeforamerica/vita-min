6185aff3af0e4c4d695b8c42c0ecfcfeebc8f295
loumoore <lourmoore@gmail.com>
2020-11-11 13:42:10 -0800

    update feature test to just use default test host instead of overriding it

--- spec/features/visit_home_page_spec.rb
+++ spec/features/visit_home_page_spec.rb
@@ -10,41 +10,35 @@ RSpec.feature "Visit home page" do
     expect(page).to have_text "sign up here"
   end
 
-  context "for the www.getyourrefund.org domain" do
-    before do
-      Capybara.default_host = "https://www.getyourrefund.org"
-    end
+  scenario "it has the correct SEO link tags in English" do
+    visit "/?new_locale=en"
+    # We are currently viewing the English homepage so the canonical URL is the English version, with no locale
+    # because English is the default
+    #
+    # Note: url_for appends a trailing slash by default to the root URL only, and this is ok because Google treats
+    # root URLs as the same regardless of whether they have a trailing slash, unlike URLs with additional path
+    # elements!
+    #
+    # i.e. https://www.getyourrefund.org == https://www.getyourrefund.org/ BUT https://www.getyourrefund.org/es != https://www.getyourrefund.org/es/
+    expect(page).to have_css 'link[rel="canonical"][href="http://www.example.com/"]', :visible => false
+    # The default language alternate is always English
+    expect(page).to have_css 'link[rel="alternate"][hreflang="x-default"][href="http://www.example.com/"]', :visible => false
+    # The English alternate doesn't include the locale because it's the default
+    expect(page).to have_css 'link[rel="alternate"][hreflang="en"][href="http://www.example.com/"]', :visible => false
+    # The Spanish alternate includes the locale
+    expect(page).to have_css 'link[rel="alternate"][hreflang="es"][href="http://www.example.com/es"]', :visible => false
+  end
 
-    scenario "it has the correct SEO link tags in English" do
-      visit "/?new_locale=en"
-      # We are currently viewing the English homepage so the canonical URL is the English version, with no locale
-      # because English is the default
-      #
-      # Note: url_for appends a trailing slash by default to the root URL only, and this is ok because Google treats
-      # root URLs as the same regardless of whether they have a trailing slash, unlike URLs with additional path
-      # elements!
-      #
-      # i.e. https://www.getyourrefund.org == https://www.getyourrefund.org/ BUT https://www.getyourrefund.org/es != https://www.getyourrefund.org/es/
-      expect(page).to have_css 'link[rel="canonical"][href="https://www.getyourrefund.org/"]', :visible => false
-      # The default language alternate is always English
-      expect(page).to have_css 'link[rel="alternate"][hreflang="x-default"][href="https://www.getyourrefund.org/"]', :visible => false
-      # The English alternate doesn't include the locale because it's the default
-      expect(page).to have_css 'link[rel="alternate"][hreflang="en"][href="https://www.getyourrefund.org/"]', :visible => false
-      # The Spanish alternate includes the locale
-      expect(page).to have_css 'link[rel="alternate"][hreflang="es"][href="https://www.getyourrefund.org/es"]', :visible => false
-    end
-
-    scenario "it has the correct SEO link tags in Spanish" do
-      visit "/?new_locale=es"
-      # We are currently viewing the Spanish homepage so the canonical URL is the Spanish version
-      expect(page).to have_css 'link[rel="canonical"][href="https://www.getyourrefund.org/es"]', :visible => false
-      # The default language alternate is always English
-      expect(page).to have_css 'link[rel="alternate"][hreflang="x-default"][href="https://www.getyourrefund.org/"]', :visible => false
-      # The English alternate doesn't include the locale because it's the default
-      expect(page).to have_css 'link[rel="alternate"][hreflang="en"][href="https://www.getyourrefund.org/"]', :visible => false
-      # The Spanish alternate includes the locale
-      expect(page).to have_css 'link[rel="alternate"][hreflang="es"][href="https://www.getyourrefund.org/es"]', :visible => false
-    end
+  scenario "it has the correct SEO link tags in Spanish" do
+    visit "/?new_locale=es"
+    # We are currently viewing the Spanish homepage so the canonical URL is the Spanish version
+    expect(page).to have_css 'link[rel="canonical"][href="http://www.example.com/es"]', :visible => false
+    # The default language alternate is always English
+    expect(page).to have_css 'link[rel="alternate"][hreflang="x-default"][href="http://www.example.com/"]', :visible => false
+    # The English alternate doesn't include the locale because it's the default
+    expect(page).to have_css 'link[rel="alternate"][hreflang="en"][href="http://www.example.com/"]', :visible => false
+    # The Spanish alternate includes the locale
+    expect(page).to have_css 'link[rel="alternate"][hreflang="es"][href="http://www.example.com/es"]', :visible => false
   end
 
   context "in non-production environments" do
