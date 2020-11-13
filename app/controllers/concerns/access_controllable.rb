1fd94cf6fac2891164ebbb46fdd1b4532a1d6aa4
Benjamin Golder <bgolder@codeforamerica.org>
2020-11-13 07:47:49 -0800

    stop redirecting to zendesk sign in (#587)

--- app/controllers/concerns/access_controllable.rb
+++ app/controllers/concerns/access_controllable.rb
@@ -4,7 +4,7 @@ module AccessControllable
   def require_sign_in(redirect_after_login: nil )
     unless current_user.present?
       session[:after_login_path] = redirect_after_login || request.path
-      redirect_to zendesk_sign_in_path
+      redirect_to new_user_session_path
     end
   end
 end
