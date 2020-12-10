60c16958753a4a1633ca6567f885bd8311e4bcc9
Shannon Byrne <sbyrne@codeforamerica.org>
2020-12-10 14:29:49 -0600

    Only anonymize non-null data [#176061608]
    Co-authored-by: Alex Sartan <asartan@vmware.com>

--- db/copy_anonymized_data.sql
+++ db/copy_anonymized_data.sql
@@ -75,115 +75,115 @@ create table anon2020.vita_providers as (select * from public.vita_providers);
 
 -- intake_site_drop_offs
 update anon2020.intake_site_drop_offs
-set additional_info = 'ANONYMIZED'
-  , name            = 'ANONYMIZED'
-  , phone_number    = 'ANONYMIZED'
-  , email           = 'ANONYMIZED';
+set additional_info = case when additional_info is null then additional_info else 'ANONYMIZED' end
+  , name            = case when name is null then name else 'ANONYMIZED' end
+  , phone_number    = case when phone_number is null then phone_number else 'ANONYMIZED' end
+  , email           = case when email is null then email else 'ANONYMIZED' end;
 
 -- diy_intakes
 update anon2020.diy_intakes
-set email_address  = 'ANONYMIZED'
-  , preferred_name = 'ANONYMIZED';
+set email_address  = case when email_address is null then email_address else 'ANONYMIZED' end
+  , preferred_name = case when preferred_name is null then preferred_name else 'ANONYMIZED' end;
 
 -- dependents
 update anon2020.dependents
-set birth_date = to_date(concat('1/1/', extract(year from birth_date)), 'MM/DD/YYYY')
-  , first_name = 'ANONYMIZED'
-  , last_name  = 'ANONYMIZED';
+set birth_date = case when birth_date is null then birth_date else to_date(concat('1/1/', extract(year from birth_date)), 'MM/DD/YYYY') end
+  , first_name = case when first_name is null then first_name else 'ANONYMIZED' end
+  , last_name  = case when last_name is null then last_name else 'ANONYMIZED' end;
 
 -- documents
 update anon2020.documents
-set display_name = 'ANONYMIZED';
+set display_name = case when display_name is null then display_name else 'ANONYMIZED' end;
 
 -- delayed_jobs
 truncate anon2020.idme_users;
 
 -- intakes
 update anon2020.intakes
-set additional_info                    = 'ANONYMIZED'
-  , email_address                      = 'ANONYMIZED'
-  , encrypted_bank_account_number      = 'ANONYMIZED'
-  , encrypted_bank_account_number_iv   = 'ANONYMIZED'
-  , encrypted_bank_name                = 'ANONYMIZED'
-  , encrypted_bank_name_iv             = 'ANONYMIZED'
-  , encrypted_bank_routing_number      = 'ANONYMIZED'
-  , encrypted_bank_routing_number_iv   = 'ANONYMIZED'
-  , encrypted_primary_last_four_ssn    = 'ANONYMIZED'
-  , encrypted_primary_last_four_ssn_iv = 'ANONYMIZED'
-  , encrypted_spouse_last_four_ssn     = 'ANONYMIZED'
-  , encrypted_spouse_last_four_ssn_iv  = 'ANONYMIZED'
-  , feedback                           = 'ANONYMIZED'
-  , final_info                         = 'ANONYMIZED'
-  , interview_timing_preference        = 'ANONYMIZED'
-  , other_income_types                 = 'ANONYMIZED'
-  , phone_number                       = 'ANONYMIZED'
-  , preferred_name                     = 'ANONYMIZED'
+set additional_info                    = case when additional_info is null then additional_info else 'ANONYMIZED' end
+  , email_address                      = case when email_address is null then email_address else 'ANONYMIZED' end
+  , encrypted_bank_account_number      = case when encrypted_bank_account_number is null then encrypted_bank_account_number else 'ANONYMIZED' end
+  , encrypted_bank_account_number_iv   = case when encrypted_bank_account_number_iv is null then encrypted_bank_account_number_iv else 'ANONYMIZED' end
+  , encrypted_bank_name                = case when encrypted_bank_name is null then encrypted_bank_name else 'ANONYMIZED' end
+  , encrypted_bank_name_iv             = case when encrypted_bank_name_iv is null then encrypted_bank_name_iv else 'ANONYMIZED' end
+  , encrypted_bank_routing_number      = case when encrypted_bank_routing_number is null then encrypted_bank_routing_number else 'ANONYMIZED' end
+  , encrypted_bank_routing_number_iv   = case when encrypted_bank_routing_number_iv is null then encrypted_bank_routing_number_iv else 'ANONYMIZED' end
+  , encrypted_primary_last_four_ssn    = case when encrypted_primary_last_four_ssn is null then encrypted_primary_last_four_ssn else 'ANONYMIZED' end
+  , encrypted_primary_last_four_ssn_iv = case when encrypted_primary_last_four_ssn_iv is null then encrypted_primary_last_four_ssn_iv else 'ANONYMIZED' end
+  , encrypted_spouse_last_four_ssn     = case when encrypted_spouse_last_four_ssn is null then encrypted_spouse_last_four_ssn else 'ANONYMIZED' end
+  , encrypted_spouse_last_four_ssn_iv  = case when encrypted_spouse_last_four_ssn_iv is null then encrypted_spouse_last_four_ssn_iv else 'ANONYMIZED' end
+  , feedback                           = case when feedback is null then feedback else 'ANONYMIZED' end
+  , final_info                         = case when final_info is null then final_info else 'ANONYMIZED' end
+  , interview_timing_preference        = case when interview_timing_preference is null then interview_timing_preference else 'ANONYMIZED' end
+  , other_income_types                 = case when other_income_types is null then other_income_types else 'ANONYMIZED' end
+  , phone_number                       = case when phone_number is null then phone_number else 'ANONYMIZED' end
+  , preferred_name                     = case when preferred_name is null then preferred_name else 'ANONYMIZED' end
   -- Anonymize birthday day and month
-  , primary_birth_date                 = to_date(concat('1/1/', extract(year from primary_birth_date)), 'MM/DD/YYYY')
-  , primary_consented_to_service_ip    = '127.0.0.1'
-  , primary_first_name                 = 'ANONYMIZED'
-  , primary_last_name                  = 'ANONYMIZED'
-  , referrer                           = 'ANONYMIZED'
-  , sms_phone_number                   = 'ANONYMIZED'
-  , spouse_birth_date                  = to_date(concat('1/1/', extract(year from spouse_birth_date)), 'MM/DD/YYYY')
-  , spouse_consented_to_service_ip     = '127.0.0.1'
-  , spouse_email_address               = 'ANONYMIZED'
-  , spouse_first_name                  = 'ANONYMIZED'
-  , spouse_last_name                   = 'ANONYMIZED'
-  , street_address                     = 'ANONYMIZED'
-  , zip_code                           = 'ANONYMIZED';
+  , primary_birth_date                 = case when primary_birth_date is null then primary_birth_date else to_date(concat('1/1/', extract(year from primary_birth_date)), 'MM/DD/YYYY') end
+  , primary_consented_to_service_ip    = case when primary_consented_to_service_ip is null then primary_consented_to_service_ip else '127.0.0.1' end
+  , primary_first_name                 = case when primary_first_name is null then primary_first_name else 'ANONYMIZED' end
+  , primary_last_name                  = case when primary_last_name is null then primary_last_name else 'ANONYMIZED' end
+  , referrer                           = case when referrer is null then referrer else 'ANONYMIZED' end
+  , sms_phone_number                   = case when sms_phone_number is null then sms_phone_number else 'ANONYMIZED' end
+  , spouse_birth_date                  = case when spouse_birth_date is null then spouse_birth_date else to_date(concat('1/1/', extract(year from spouse_birth_date)), 'MM/DD/YYYY') end
+  , spouse_consented_to_service_ip     = case when spouse_consented_to_service_ip is null then spouse_consented_to_service_ip else '127.0.0.1' end
+  , spouse_email_address               = case when spouse_email_address is null then spouse_email_address else 'ANONYMIZED' end
+  , spouse_first_name                  = case when spouse_first_name is null then spouse_first_name else 'ANONYMIZED' end
+  , spouse_last_name                   = case when spouse_last_name is null then spouse_last_name else 'ANONYMIZED' end
+  , street_address                     = case when street_address is null then street_address else 'ANONYMIZED' end
+  , zip_code                           = case when zip_code is null then zip_code else 'ANONYMIZED' end;
 
 -- active_storage_blobs
 update anon2020.active_storage_blobs
-set filename = 'ANONYMIZED';
+set filename = case when filename is null then filename else 'ANONYMIZED' end;
 
 -- delayed_jobs
 truncate anon2020.delayed_jobs;
 
 -- incoming_emails
 update anon2020.incoming_emails
-set stripped_signature = 'ANONYMIZED'
-  , stripped_text      = 'ANONYMIZED'
-  , stripped_html      = 'ANONYMIZED'
-  , body_plain         = 'ANONYMIZED'
-  , body_html          = 'ANONYMIZED'
-  , subject            = 'ANONYMIZED'
-  , recipient          = 'ANONYMIZED'
-  , sender             = 'ANONYMIZED'
-  , received           = 'ANONYMIZED'
-  , "from"             = 'ANONYMIZED'
-  , "to"               = 'ANONYMIZED';
+set stripped_signature = case when stripped_signature is null then stripped_signature else 'ANONYMIZED' end
+  , stripped_text      = case when stripped_text is null then stripped_text else 'ANONYMIZED' end
+  , stripped_html      = case when stripped_html is null then stripped_html else 'ANONYMIZED' end
+  , body_plain         = case when body_plain is null then body_plain else 'ANONYMIZED' end
+  , body_html          = case when body_html is null then body_html else 'ANONYMIZED' end
+  , subject            = case when subject is null then subject else 'ANONYMIZED' end
+  , recipient          = case when recipient is null then recipient else 'ANONYMIZED' end
+  , sender             = case when sender is null then sender else 'ANONYMIZED' end
+  , received           = case when received is null then received else 'ANONYMIZED' end
+  , "from"             = case when "from" is null then "from" else 'ANONYMIZED' end
+  , "to"               = case when "to" is null then "to" else 'ANONYMIZED' end;
 
 -- incoming_text_messages
 update anon2020.incoming_text_messages
-set body              = 'ANONYMIZED'
-  , from_phone_number = 'ANONYMIZED';
+set body              = case when body is null then body else 'ANONYMIZED' end
+  , from_phone_number = case when from_phone_number is null then from_phone_number else 'ANONYMIZED' end;
 
 -- notes
 update anon2020.notes
-set body              = 'ANONYMIZED';
+set body              = case when body is null then body else 'ANONYMIZED' end;
 
 -- outgoing_emails
 update anon2020.outgoing_emails
-set body    = 'ANONYMIZED'
-  , subject = 'ANONYMIZED'
-  , "to"    = 'ANONYMIZED';
+set body    = case when body is null then body else 'ANONYMIZED' end
+  , subject = case when subject is null then subject else 'ANONYMIZED' end
+  , "to"    = case when "to" is null then "to" else 'ANONYMIZED' end;
 
 -- system_notes
 update anon2020.system_notes
-set body = 'ANONYMIZED';
+set body = case when body is null then body else 'ANONYMIZED' end;
 
 -- stimulus_triages
 update anon2020.stimulus_triages
-set referrer = 'ANONYMIZED';
+set referrer = case when referrer is null then referrer else 'ANONYMIZED' end;
 
 -- users
 update anon2020.users
-set name                      = 'ANONYMIZED'
-  , email                     = 'ANONYMIZED'
-  , encrypted_access_token    = 'ANONYMIZED'
-  , encrypted_access_token_iv = 'ANONYMIZED'
-  , encrypted_password        = 'ANONYMIZED'
-  , current_sign_in_ip        = '127.0.0.1'
-  , last_sign_in_ip           = '127.0.0.1'
-  , timezone                  = 'ANONYMIZED';
+set name                      = case when name is null then name else 'ANONYMIZED' end
+  , email                     = case when email is null then email else 'ANONYMIZED' end
+  , encrypted_access_token    = case when encrypted_access_token is null then encrypted_access_token else 'ANONYMIZED' end
+  , encrypted_access_token_iv = case when encrypted_access_token_iv is null then encrypted_access_token_iv else 'ANONYMIZED' end
+  , encrypted_password        = case when encrypted_password is null then encrypted_password else 'ANONYMIZED' end
+  , current_sign_in_ip        = case when current_sign_in_ip is null then current_sign_in_ip else '127.0.0.1' end
+  , last_sign_in_ip           = case when last_sign_in_ip is null then last_sign_in_ip else '127.0.0.1' end
+  , timezone                  = case when timezone is null then timezone else 'ANONYMIZED' end;
