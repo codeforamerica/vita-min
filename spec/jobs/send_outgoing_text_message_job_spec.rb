30d3195605afa1e0cb6a20ae99d0fb9208f1bc8f
Jenny Heath <jheath@codeforamerica.org>
2020-12-14 17:06:15 -0800

    Change twilio credentials in all environments and update texting code to reflect new subaccounts and messaging services

--- spec/jobs/send_outgoing_text_message_job_spec.rb
+++ spec/jobs/send_outgoing_text_message_job_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe SendOutgoingTextMessageJob, type: :job do
     let(:outgoing_text_message) { create(:outgoing_text_message, client: client, user: user, to_phone_number: "+15855551212") }
 
     before do
-      allow(EnvironmentCredentials).to receive(:dig).with(:twilio, :phone_number).and_return("+15555551212")
+      allow(EnvironmentCredentials).to receive(:dig).with(:twilio, :messaging_service_sid).and_return("f@k3s!d")
       allow(Twilio::REST::Client).to receive(:new).and_return(fake_twilio_client)
       allow(fake_twilio_client).to receive(:messages).and_return(fake_twilio_messages)
       allow(fake_twilio_messages).to receive(:create).and_return(fake_twilio_message)
@@ -19,7 +19,7 @@ RSpec.describe SendOutgoingTextMessageJob, type: :job do
     it "send the message to Twilio along with a status callback URL and saves the status into the model" do
       SendOutgoingTextMessageJob.perform_now(outgoing_text_message.id)
       expect(fake_twilio_messages).to have_received(:create).with(
-        from: "+15555551212",
+        messaging_service_sid: "f@k3s!d",
         to: outgoing_text_message.to_phone_number,
         body: outgoing_text_message.body,
         status_callback: "http://test.host/outgoing_text_messages/#{outgoing_text_message.id}",
