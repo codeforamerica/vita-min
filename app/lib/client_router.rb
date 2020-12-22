7ce204ca366b0658764a35f94b3b9bb26a4da00d
Asheesh Laroia <alaroia@codeforamerica.org>
2020-12-21 16:27:11 -0800

    Use left join, so we always get a VitaPartner even if there are no OrganizationLeadRoles

--- app/lib/client_router.rb
+++ app/lib/client_router.rb
@@ -1,7 +1,8 @@
 class ClientRouter
   def self.route(client)
-    organization_with_most_users = OrganizationLeadRole.group(:id).group("vita_partner_id").order('COUNT(user) DESC').first.organization
-    client.update(vita_partner: organization_with_most_users)
-    client.intake.update(vita_partner: organization_with_most_users)
+    organization_id_with_most_leads = ActiveRecord::Base.connection.execute(
+      "SELECT vita_partners.id FROM vita_partners LEFT JOIN organization_lead_roles on vita_partners.id=organization_lead_roles.vita_partner_id GROUP BY vita_partners.id ORDER BY count(organization_lead_roles.id) DESC LIMIT 1").first["id"]
+    client.update(vita_partner_id: organization_id_with_most_leads)
+    client.intake.update(vita_partner_id: organization_id_with_most_leads)
   end
 end
