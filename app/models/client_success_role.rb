dcdaac909f37d19b6a5dbdf54cc83373751e27b6
Em Barnard-Shao <ebarnard@codeforamerica.org>
2021-01-07 11:23:52 -0500

    Add migration
    Co-authored-by: Jenny Heath <jheath@codeforamerica.org>

---
+++ app/models/client_success_role.rb
@@ -0,0 +1,12 @@
+# == Schema Information
+#
+# Table name: client_success_roles
+#
+#  id         :bigint           not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+class ClientSuccessRole < ApplicationRecord
+  TYPE = "ClientSuccessRole"
+  DISPLAY_NAME = I18n.t("general.client_success")
+end
