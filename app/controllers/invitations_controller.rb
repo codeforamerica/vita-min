4fc59ba6222abba7893f916c26e04940db3f9f49
Asheesh Laroia <alaroia@codeforamerica.org>
2021-01-11 17:17:15 -0800

    Maybe works
    Co-authored-by: Em Barnard-Shao <ebarnard@codeforamerica.org>

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -2,7 +2,7 @@ class InvitationsController < ApplicationController
   include AccessControllable
 
   before_action :require_sign_in
-  load_and_authorize_resource :user, only: :resend
+  load_and_authorize_resource :user, only: :resend, parent: false
 
   layout "admin"
 
@@ -11,6 +11,12 @@ class InvitationsController < ApplicationController
   end
 
   def resend
-    @user.invite!
+    if @user.invitation_accepted_at.present?
+      flash[:warning] = I18n.t("invitations.resend.cannot_because_accepted")
+    else
+      @user.invite!
+      flash[:notice] = I18n.t("devise.invitations.send_instructions", email: @user.email)
+    end
+    redirect_to invitations_path
   end
 end
--- spec/controllers/invitations_controller_spec.rb
+++ spec/controllers/invitations_controller_spec.rb
@@ -20,23 +20,29 @@ RSpec.describe InvitationsController do
   end
 
   describe "#resend" do
-    let(:params) { { user_id: invited_user.id } }
+    let(:params) { { id: invited_user.id } }
     let(:invited_user) { create(:invited_user) }
     it_behaves_like :a_post_action_for_admins_only, action: :resend
 
-    context "with a user id who has already been invited" do
+    context "with a logged-in admin user" do
       before { sign_in(create(:admin_user)) }
 
-      it "sends a fresh invitation email" do
-        expect { post :resend, params: { user_id: invited_user.id } }.to change(ActionMailer::Base.deliveries, :count).by 1
+      context "with a user id who has already been invited" do
+        it "sends a fresh invitation email" do
+          expect { post :resend, params: { id: invited_user.id } }.to change(ActionMailer::Base.deliveries, :count).by 1
+          expect(flash[:notice]).to eq "We sent an email invitation to #{invited_user.email}."
+          expect(response).to redirect_to(invitations_path)
+        end
       end
-    end
 
-    context "with a user id who has accepted their invitation" do
-      let(:accepted_invite_user) { create(:accepted_invite_user) }
+      context "with a user id who has accepted their invitation" do
+        let(:accepted_invite_user) { create(:accepted_invite_user) }
 
-      it "does not send an invitation email" do
-        expect { post :resend, params: { user_id: accepted_invite_user.id } }.not_to change(ActionMailer::Base.deliveries, :count)
+        it "does not send an invitation email" do
+          expect { post :resend, params: { id: accepted_invite_user.id } }.not_to change(ActionMailer::Base.deliveries, :count)
+          expect(flash[:warning]).to eq "Cannot re-invite a user who already accepted the invite."
+          expect(response).to redirect_to(invitations_path)
+        end
       end
     end
   end
