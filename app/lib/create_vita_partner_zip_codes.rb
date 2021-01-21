d6131bb59675b230024797b2a7c3b5825a256a97
Em Barnard-Shao <ebarnard@codeforamerica.org>
2021-01-21 18:47:43 -0500

    Fix csv parser
    Co-authored-by: Jenny Heath <jheath@codeforamerica.org>

--- app/lib/create_vita_partner_zip_codes.rb
+++ app/lib/create_vita_partner_zip_codes.rb
@@ -29,7 +29,7 @@ class CreateVitaPartnerZipCodes
 
       zip_codes.to_s.split(",").map do |zip_code|
         begin
-          VitaPartnerZipCode.create!(zip_code: zip_code&.strip, vita_partner: vita_partner&.strip)
+          VitaPartnerZipCode.create!(zip_code: zip_code&.strip, vita_partner: vita_partner)
           successes << "Created VitaPartnerZipCode with #{zip_code} for #{vita_partner&.name}"
         rescue => e
           problems << "SKIPPED Unable to create VitaPartnerZipCode with #{zip_code} for #{vita_partner&.name} because: #{ e.message }"
@@ -38,7 +38,7 @@ class CreateVitaPartnerZipCodes
     end
 
     problems << "SKIPPED Unable to find VitaPartner for organization names:\n#{unfound_vita_partners.join(", ")}"
-    puts "**** #{successes.length} SUCCESSES FOR THESE CODES ****"
+    puts "**** #{successes.length} SUCCESSES FOR THESE ZIP CODES ****"
     puts successes
     puts "**** LOOK INTO THESE #{problems.length} ISSUES ****"
     puts problems
