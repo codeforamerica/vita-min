d688d601bcdea78f5b7892a4cbb4b98bc3a58030
Sarah Niemeyer <sniemeyer@vmware.com>
2021-01-25 13:44:12 -0700

    Fix eip intake creation (#741)

--- app/controllers/questions/eip_overview_controller.rb
+++ app/controllers/questions/eip_overview_controller.rb
@@ -21,7 +21,7 @@ module Questions
       session[:intake_id] = @form.intake.id
       stimulus_triage_id = session.delete(:stimulus_triage_id)
       if stimulus_triage_id.present?
-        current_intake.update(triage_source: StimulusTriage.find(stimulus_triage_id), visitor_id: visitor_id)
+        @form.intake.update(triage_source: StimulusTriage.find(stimulus_triage_id))
       end
     end
   end
