6fd777a64701c60991c5179fd759d6af7171b519
Asheesh Laroia <alaroia@codeforamerica.org>
2021-01-29 10:28:18 -0800

    Move i18n test into one assertion; upgrade parser
    Co-authored-by: Jenny Heath <jheath@codeforamerica.org>

---
+++ spec/i18n_spec.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require 'i18n/tasks'
+
+RSpec.describe I18n do
+  let(:i18n) { I18n::Tasks::BaseTask.new }
+
+  it "does not have missing/unused keys or inconsistent interpolations" do
+    # This is all in one test since the I18n task computes some data which can be re-used by different validations.
+    expect(
+      i18n.unused_keys
+    ).to be_empty, "#{i18n.unused_keys.leaves.count} unused i18n keys, run `i18n-tasks health' to show them"
+    expect(
+      i18n.missing_keys
+    ).to be_empty, "#{i18n.missing_keys.leaves.count} i18n keys are missing from a language, run `i18n-tasks health' to show them"
+    expect(
+      i18n.inconsistent_interpolations
+    ).to be_empty, "#{i18n.inconsistent_interpolations.leaves.count} i18n keys have inconsistent interpolations, run `i18n-tasks health` to show them"
+  end
+end
