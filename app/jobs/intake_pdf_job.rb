83d40da73aea8ebd7b10584f487b2777f0840717
Shannon Byrne <sbyrne@codeforamerica.org>
2021-02-02 08:16:30 -0600

    Default value for IntakePDFJob

--- app/jobs/intake_pdf_job.rb
+++ app/jobs/intake_pdf_job.rb
@@ -1,5 +1,5 @@
 class IntakePdfJob < ApplicationJob
-  def perform(intake_id, filename)
+  def perform(intake_id, filename = nil)
     intake = Intake.find(intake_id)
     intake.create_intake_document(filename)
   end
