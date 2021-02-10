047f655c1ab0b1f1c27c40a9966cdfc526dafe70
Sarah Niemeyer <sniemeyer@vmware.com>
2021-02-10 16:20:59 -0700

    Update copy for first confirmation message (#806)

--- app/controllers/questions/consent_controller.rb
+++ app/controllers/questions/consent_controller.rb
@@ -11,12 +11,12 @@ module Questions
     def after_update_success
       current_intake.advance_tax_return_statuses_to("intake_in_progress")
       if current_intake.email_notification_opt_in_yes?
-        body = I18n.t("messages.getting_started.email_body", preferred_name: current_intake.preferred_name, requested_docs_link: current_intake.requested_docs_token_link, locale: current_intake.locale)
+        body = I18n.t("messages.getting_started.email_body", preferred_name: current_intake.preferred_name, requested_docs_link: current_intake.requested_docs_token_link, locale: current_intake.locale, client_id: current_intake.client_id)
         subject = I18n.t("messages.getting_started.email_subject", locale: current_intake.locale)
         ClientMessagingService.send_system_email(current_intake.client, body, subject)
       end
       if current_intake.sms_notification_opt_in_yes?
-        body = I18n.t("messages.getting_started.sms_body", preferred_name: current_intake.preferred_name, requested_docs_link: current_intake.requested_docs_token_link, locale: current_intake.locale)
+        body = I18n.t("messages.getting_started.sms_body", preferred_name: current_intake.preferred_name, requested_docs_link: current_intake.requested_docs_token_link, locale: current_intake.locale, client_id: current_intake.client_id)
         ClientMessagingService.send_system_text_message(current_intake.client, body)
       end
       Intake14446PdfJob.perform_later(current_intake, "Consent Form 14446.pdf")
