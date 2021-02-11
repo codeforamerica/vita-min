ac60ed6c08dda3ac5fb8849a914445d8064fb6ee
Ben Smith <besmith@vmware.com>
2021-02-11 08:48:50 -0700

    send MixPanel event when going to myfreetaxes.com
    Signed-off-by: Asheesh Laroia <alaroia@codeforamerica.org>

--- spec/features/diy/new_diy_client_spec.rb
+++ spec/features/diy/new_diy_client_spec.rb
@@ -1,12 +1,16 @@
 require "rails_helper"
 
 RSpec.feature "Web Intake New Client wants to file on their own" do
-  scenario "a new client files through My Free Taxes" do
+  scenario "a new client files through My Free Taxes", js: true do
+    allow(MixpanelService).to receive(:send_event)
+
     visit "/questions/welcome"
     click_on "File taxes myself"
 
     expect(page).to have_selector("h1", text: "File your taxes yourself!")
     click_on "Continue through MyFreeTaxes"
-    expect(current_url).to eq("https://www.myfreetaxes.com/en")
+    expect(current_url).to eq("https://www.myfreetaxes.com/")
+
+    expect(MixpanelService).to have_received(:send_event).with(hash_including({event_name: "click_mft-diy-referral"}))
   end
 end
