880e54fa96d6e7fb271a5b9d3ff15fa49300b540
Asheesh Laroia <alaroia@codeforamerica.org>
2021-02-11 06:47:09 -0800

    Try Knapsack Pro
    Co-authored-by: Jenny Heath <jheath@codeforamerica.org>

--- spec/support/webmock.rb
+++ spec/support/webmock.rb
@@ -2,4 +2,4 @@ require 'webmock/rspec'
 
 driver_urls = Webdrivers::Common.subclasses.map(&:base_url)
 
-WebMock.disable_net_connect!(allow_localhost: true, allow: driver_urls)
+WebMock.disable_net_connect!(allow_localhost: true, allow: driver_urls + ['api.knapsackpro.com'])
