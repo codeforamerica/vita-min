42a599de1f17d1f0db0179cff0a7cf0a699ee8d8
Asheesh Laroia <alaroia@codeforamerica.org>
2021-02-12 18:43:44 -0800

    Disable filesystem watching in CI

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -12,6 +12,9 @@ Rails.application.configure do
   # just for the purpose of running a single test. If you are using a tool that
   # preloads Rails for running tests, you may have to set it to true.
   config.eager_load = ENV['EAGER_LOAD'].present?
+  if ENV['EAGER_LOAD'].present?
+    config.Spring.watch_via = :polling
+  end
 
   # Configure public file server for tests with Cache-Control for performance.
   config.public_file_server.enabled = true
