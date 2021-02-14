2048540b311c63200f7ca4e5dbd0333d4e2df9a7
Ben Golder <bgolder@codeforamerica.org>
2021-02-13 20:14:57 -0800

    Avoid referencing unstable models in migrations

--- db/migrate/20210128180337_data_migration_create_gyr_national_org.rb
+++ db/migrate/20210128180337_data_migration_create_gyr_national_org.rb
@@ -1,4 +1,9 @@
 class DataMigrationCreateGyrNationalOrg < ActiveRecord::Migration[6.0]
+  # Avoid using models in migrations by embedding referenced models
+  # https://makandracards.com/makandra/15575-how-to-write-complex-migrations-in-rails
+  class VitaPartner < ActiveRecord::Base
+  end
+
   def up
     # Ensure this org exists
     VitaPartner.find_or_create_by!(name: "GYR National Organization")
