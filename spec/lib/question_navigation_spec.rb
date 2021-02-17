19f0451dc865c3ab0a289074d3f5c03ce110e8c7
Shannon Byrne <sbyrne@codeforamerica.org>
2021-02-17 13:49:56 -0800

    Fix removed tests

--- spec/lib/question_navigation_spec.rb
+++
@@ -1,5 +0,0 @@
-require "rails_helper"
-
-RSpec.describe QuestionNavigation do
-  it_behaves_like "controller navigation flow", QuestionNavigation
-end
--- spec/lib/stimulus_navigation_spec.rb
+++
@@ -1,5 +0,0 @@
-require "rails_helper"
-
-RSpec.describe StimulusNavigation do
-  it_behaves_like "controller navigation flow", StimulusNavigation
-end
