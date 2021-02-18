451ed3328a3d26538b0980100be000bacdc311d0
Ben Golder <bgolder@codeforamerica.org>
2021-02-17 17:30:20 -0800

    Move breach queries into model scopes

--- app/services/sla_breach_service.rb
+++ app/services/sla_breach_service.rb
@@ -12,28 +12,17 @@ class SLABreachService
   # attention_needed_since is present and is before breach threshold
   # returns data in the format { vita_partner_id: breach_count }
   def attention_needed_breaches
-    Client.sla_tracked
-      .where(Client.arel_table[:attention_needed_since].lteq(breach_threshold_date))
-      .group(:vita_partner_id)
-      .count(:id)
+    Client.attention_needed_breaches(breach_threshold_date).group(:vita_partner_id).count(:id)
   end
 
   # clients who messaged us and have not been responded to _with a message, call or email_ within the breach threshold
   def outgoing_communication_breaches
-    Client.sla_tracked
-      .where(Client.arel_table[:first_unanswered_incoming_interaction_at].lteq(breach_threshold_date))
-      .group(:vita_partner_id)
-      .count(:id)
+    Client.outgoing_communication_breaches(breach_threshold_date).group(:vita_partner_id).count(:id)
   end
 
   # clients who've messaged us _and we've not interacted with their profile at all_ within the breach threshold.
   def outgoing_interaction_breaches
-    clients = Client.arel_table
-    Client.sla_tracked
-      .where(clients[:first_unanswered_incoming_interaction_at].lteq(breach_threshold_date))
-      .where(clients[:last_internal_or_outgoing_interaction_at].lt(clients[:first_unanswered_incoming_interaction_at]).or(clients[:last_internal_or_outgoing_interaction_at].eq(nil)))
-      .group(:vita_partner_id)
-      .count(:id)
+    Client.outgoing_interaction_breaches(breach_threshold_date).group(:vita_partner_id).count(:id)
   end
 
   def self.generate_report
