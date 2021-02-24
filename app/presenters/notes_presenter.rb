c4e868ee56d3e70b4c8dfff132c6718e7827c623
em barnard-shao <49880002+embarnard@users.noreply.github.com>
2021-02-24 16:38:25 -0500

    Create synthetic note class and move client document note and outbound call notes to messages tab (#873)
    Co-authored-by: Shannon Byrne <sbyrne@codeforamerica.org>

--- app/presenters/notes_presenter.rb
+++ app/presenters/notes_presenter.rb
@@ -3,49 +3,8 @@ class NotesPresenter
     # Extract Note and SystemNote data from the database.
     # Generate notes from documents on-the-fly, so the summary can change as clients upload more documents.
     notes = (client.notes.includes(:user) + SystemNote.where(client: client))
-    synthetic_notes = synthetic_notes_from_documents(client.documents.where(uploaded_by: client).order(created_at: :asc))
-    synthetic_notes += synthetic_notes_from_outbound_calls(client.outbound_calls.order(created_at: :asc))
+    synthetic_notes = SyntheticNote.from_client_documents(client)
+    synthetic_notes += SyntheticNote.from_outbound_calls(client)
     (notes + synthetic_notes).sort_by(&:created_at).group_by { |note| note.created_at.beginning_of_day }
-  end
-
-  private
-
-  def self.synthetic_notes_from_documents(documents)
-    grouped = documents.group_by { |doc| doc.created_at.beginning_of_day }
-    grouped.map do |_, values|
-      # Use most recent Document created_at as note created_at
-      SyntheticNote.new(
-        values[-1].created_at,
-        I18n.t("hub.notes.index.document_note", count: values.length)
-      )
-    end
-  end
-
-  def self.synthetic_notes_from_outbound_calls(outbound_calls)
-    outbound_calls.map do |outbound_call|
-      next if outbound_call.twilio_status == "queued"
-
-      duration = ""
-      if outbound_call.twilio_call_duration
-        duration_minutes = outbound_call.twilio_call_duration / 60
-        duration_seconds = outbound_call.twilio_call_duration % 60
-        duration += "#{duration_minutes}m" if duration_minutes.positive?
-        duration += "#{duration_seconds}s"
-      end
-      body = I18n.t("hub.notes.index.outbound_call_synthetic_note", user_name: outbound_call.user.name, client_name: outbound_call.client.preferred_name, status: outbound_call.twilio_status, duration: duration)
-      body += "\n#{I18n.t("hub.notes.index.outbound_call_synthetic_note_body")}\n#{outbound_call.note}}" if outbound_call.note.present?
-      SyntheticNote.new(outbound_call.created_at, body)
-    end.compact
-  end
-
-  # A SyntheticNote has enough information to be displayed in the page.
-  class SyntheticNote
-    attr_accessor :created_at
-    attr_accessor :body
-
-    def initialize(created_at, body)
-      @created_at = created_at
-      @body = body
-    end
   end
 end
--- spec/presenters/notes_presenter_spec.rb
+++ spec/presenters/notes_presenter_spec.rb
@@ -13,80 +13,29 @@ RSpec.describe NotesPresenter do
     end
 
     context "with notes from different days" do
+      let(:document_note_double) { double(SyntheticNote) }
+      let(:outbound_call_note_double) { double(SyntheticNote) }
+      let(:day1){ DateTime.new(2019, 10, 5, 8, 1).utc }
+      let(:day2){ DateTime.new(2020, 10, 5, 5, 1).utc }
+
+      before do
+        allow(SyntheticNote).to receive(:from_client_documents).with(client).and_return [document_note_double]
+        allow(SyntheticNote).to receive(:from_outbound_calls).with(client).and_return [outbound_call_note_double]
+        allow(document_note_double).to receive(:created_at).and_return day1
+        allow(outbound_call_note_double).to receive(:created_at).and_return day2
+      end
+
       it "correctly groups notes by day created" do
-        day1 = DateTime.new(2019, 10, 5, 8, 1).utc
-        day2 = DateTime.new(2020, 10, 5, 5, 1).utc
         day1_client_note = create :note, client: client, created_at: day1 # UTC
         day2_client_note = create :note, client: client, created_at: day2
         day1_system_note = create :system_note, client: client, created_at: day1
-
         all_notes_by_day = NotesPresenter.grouped_notes(client)
 
-
         expect(all_notes_by_day.keys).to eq [day1.beginning_of_day, day2.beginning_of_day]
-
-        expect(all_notes_by_day[day1.beginning_of_day]).to eq [day1_client_note, day1_system_note]
-        expect(all_notes_by_day[day2.beginning_of_day]).to eq [day2_client_note]
+        expect(all_notes_by_day[day1.beginning_of_day]).to eq [day1_client_note, day1_system_note, document_note_double]
+        expect(all_notes_by_day[day2.beginning_of_day]).to eq [day2_client_note, outbound_call_note_double]
       end
     end
 
-    context "with documents on one day" do
-      let(:day1) { DateTime.new(2019, 10, 5, 8, 1).utc }
-      let!(:documents) { create_list(:document, 5, created_at: day1, client: client, uploaded_by: client) }
-
-      it "groups recently created documents into one message" do
-        result = NotesPresenter.grouped_notes(client)
-        expect(result[day1.beginning_of_day][0].created_at).to eq day1
-        expect(result[day1.beginning_of_day][0].body).to eq "Client added 5 documents."
-      end
-
-      context "when there are documents uploaded by the system or a user" do
-        before do
-          create_list(:document, 2, created_at: day1, client: client)
-          create_list(:document, 1, created_at: day1, client: client, uploaded_by: create(:user))
-        end
-
-        it "does not include docs that are not uploaded by the client" do
-          result = NotesPresenter.grouped_notes(client)
-          expect(result[day1.beginning_of_day][0].created_at).to eq day1
-          expect(result[day1.beginning_of_day][0].body).to eq "Client added 5 documents."
-        end
-      end
-
-    end
-
-    context "with documents on multiple days" do
-      let(:day1) { DateTime.new(2019, 10, 5, 1).utc }
-      let(:day2) { DateTime.new(2019, 10, 8, 1).utc }
-      let(:day2_noon) { DateTime.new(2019, 10, 8, 12).utc }
-
-      before do
-        create_list(:document, 1, created_at: day1, client: client, uploaded_by: client)
-        create_list(:document, 2, created_at: day2, client: client, uploaded_by: client)
-        create_list(:document, 2, created_at: day2_noon, client: client, uploaded_by: client)
-      end
-
-      it "groups recently created documents into one message" do
-        result = NotesPresenter.grouped_notes(client)
-        expect(result[day1.beginning_of_day][0].created_at).to eq day1
-        expect(result[day1.beginning_of_day][0].body).to eq "Client added 1 document."
-        expect(result[day2.beginning_of_day][0].created_at).to eq day2_noon
-        expect(result[day2.beginning_of_day][0].body).to eq "Client added 4 documents."
-      end
-    end
-
-    context "with outbound calls" do
-      let(:day1) { DateTime.new(2019, 10, 5, 8, 1).utc }
-      let(:day2) { DateTime.new(2020, 10, 5, 5, 1).utc }
-      let!(:outbound_call_completed) { create :outbound_call, twilio_status: "completed", created_at: day1, twilio_call_duration: 75, client: client, user: user, note: "I talked to them!" }
-      let!(:outbound_call_queued) { create :outbound_call, twilio_status: "queued", created_at: day2, client: client, user: user }
-      it "does not include outbound_calls in status queued" do
-        result = NotesPresenter.grouped_notes(client)
-        notes = result.values.flatten
-        expect(notes.length).to eq 1
-        expect(notes[0].body).to include "Called by #{user.name}. Call was completed and lasted 1m15s."
-        expect(notes[0].body).to include "I talked to them!"
-      end
-    end
   end
 end
