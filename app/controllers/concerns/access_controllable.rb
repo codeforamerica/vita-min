7635711f700ddc48f403430bbdcafaaea6b9844f
Ben Golder <bgolder@codeforamerica.org>
2021-02-23 20:30:57 -0800

    Allow users to share hub links with query params

--- app/controllers/concerns/access_controllable.rb
+++ app/controllers/concerns/access_controllable.rb
@@ -5,7 +5,7 @@ module AccessControllable
     unless current_user.present?
       respond_to do |format|
         format.html do
-          session[:after_login_path] = redirect_after_login || request.path
+          session[:after_login_path] = redirect_after_login || request.original_fullpath
           redirect_to new_user_session_path
         end
         format.js do
