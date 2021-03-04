51b9107a8b1891080a9730e6c08c1249351ab5b1
Jenny Heath <jheath@codeforamerica.org>
2021-03-03 17:28:15 -0800

    WIP
    Co-authored-by: Shannon Byrne <sbyrne@codeforamerica.org>

---
+++ app/controllers/questions/optional_consent_controller.rb
@@ -0,0 +1,29 @@
+module Questions
+  class OptionalConsentController < QuestionsController
+    layout "intake"
+
+    def illustration_path; end
+
+    def form_params
+      super.merge(
+        ip: request.remote_ip,
+        user_agent: request.user_agent
+      )
+    end
+
+    def after_update_success
+      # current_intake.advance_tax_return_statuses_to("intake_in_progress")
+      # if current_intake.email_notification_opt_in_yes?
+      #   body = I18n.t("messages.getting_started.email_body", preferred_name: current_intake.preferred_name, requested_docs_link: current_intake.requested_docs_token_link, locale: current_intake.locale, client_id: current_intake.client_id)
+      #   subject = I18n.t("messages.getting_started.email_subject", locale: current_intake.locale)
+      #   ClientMessagingService.send_system_email(current_intake.client, body, subject)
+      # end
+      # if current_intake.sms_notification_opt_in_yes?
+      #   body = I18n.t("messages.getting_started.sms_body", preferred_name: current_intake.preferred_name, requested_docs_link: current_intake.requested_docs_token_link, locale: current_intake.locale, client_id: current_intake.client_id)
+      #   ClientMessagingService.send_system_text_message(current_intake.client, body)
+      # end
+      # Intake14446PdfJob.perform_later(current_intake, "Consent Form 14446.pdf")
+      # IntakePdfJob.perform_later(current_intake.id, "Preliminary 13614-C with 15080.pdf")
+    end
+  end
+end
---
+++ spec/controllers/questions/optional_consent_controller_spec.rb
@@ -0,0 +1,50 @@
+require "rails_helper"
+
+RSpec.describe Questions::OptionalConsentController do
+  let(:intake) { create :intake, preferred_name: "Ruthie Rutabaga" }
+  let!(:tax_return) { create :tax_return, client: intake.client }
+
+  before do
+    allow(subject).to receive(:current_intake).and_return(intake)
+  end
+
+  describe "#update" do
+    context "with valid params" do
+      let (:params) do
+        {
+          optional_consent_form: {
+            disclose_consented: true,
+            global_carryforward_consented: true,
+            relational_efin_consented: true,
+            use_consented: true,
+          }
+        }
+      end
+      let(:ip_address) { "127.0.0.1" }
+      let(:user_agent) { "IceFerret" }
+      let(:current_time) { DateTime.new(2021, 2, 23) }
+
+      before do
+        allow(DateTime).to receive(:now).and_return current_time
+        request.remote_ip = ip_address
+        request.user_agent = user_agent
+      end
+
+      it "saves the answer, along with a timestamp and ip address" do
+        post :update, params: params
+
+        intake.reload
+        consent = intake.client.consent
+        expect(consent.disclose_consented_at).to eq current_time
+        expect(consent.global_carryforward_consented_at).to eq current_time
+        expect(consent.relational_efin_consented_at).to eq current_time
+        expect(consent.use_consented_at).to eq current_time
+        expect(consent.user_agent).to eq "IceFerret"
+        expect(consent.ip).to eq "127.0.0.1"
+      end
+    end
+
+    context "with invalid params" do
+    end
+  end
+end
