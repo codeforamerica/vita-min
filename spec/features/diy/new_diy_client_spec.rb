494d12f3b7c30b06c4970aa93f8457322542b886
Jenny Heath <jheath@codeforamerica.org>
2021-03-05 15:55:33 -0800

    Replace MFT links with MFT links that have tracking params
    Co-authored-by: Ben Golder <bgolder@codeforamerica.org>

--- spec/features/diy/new_diy_client_spec.rb
+++ spec/features/diy/new_diy_client_spec.rb
@@ -9,7 +9,7 @@ RSpec.feature "Web Intake New Client wants to file on their own" do
 
     expect(page).to have_selector("h1", text: "File your taxes yourself!")
     click_on "Continue through MyFreeTaxes"
-    expect(current_url).to eq("https://www.myfreetaxes.com/")
+    expect(current_url).to eq("https://www.myfreetaxes.com/?utm_source=GYR&utm_medum=web&utm_campaign=Partner-referral")
 
     expect(MixpanelService).to have_received(:send_event).with(hash_including({event_name: "click_mft-diy-referral"}))
   end
