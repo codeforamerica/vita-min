1f64d8d6350d33acab29228d6d95dc0a558c82cf
Jenny Heath <jheath@codeforamerica.org>
2021-03-08 14:30:11 -0800

    Open all MFT links in new tab

--- spec/features/diy/new_diy_client_spec.rb
+++ spec/features/diy/new_diy_client_spec.rb
@@ -8,8 +8,11 @@ RSpec.feature "Web Intake New Client wants to file on their own" do
     click_on "File taxes myself"
 
     expect(page).to have_selector("h1", text: "File your taxes yourself!")
-    click_on "Continue through MyFreeTaxes"
-    expect(current_url).to eq("https://www.myfreetaxes.com/?utm_source=GYR&utm_medum=web&utm_campaign=Partner-referral")
+
+    new_window = window_opened_by { click_on "Continue through MyFreeTaxes" }
+    within_window new_window do
+      expect(current_url).to eq("https://www.myfreetaxes.com/?utm_source=GYR&utm_medum=web&utm_campaign=Partner-referral")
+    end
 
     expect(MixpanelService).to have_received(:send_event).with(hash_including({event_name: "click_mft-diy-referral"}))
   end
