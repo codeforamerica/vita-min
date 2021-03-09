0b72179cff1c5aacb143f4656fe914f741e57c3f
Asheesh Laroia <alaroia@codeforamerica.org>
2021-03-08 16:39:05 -0800

    Use text message template; trivial re-organizing
    Co-authored-by: Ben Golder <bgolder@codeforamerica.org>

--- app/jobs/send_client_completion_survey_job.rb
+++ app/jobs/send_client_completion_survey_job.rb
@@ -1,11 +1,10 @@
 class SendClientCompletionSurveyJob < ApplicationJob
   def perform(client)
-    return if client.completion_survey_sent_at.present?
     best_contact_method = ClientMessagingService.contact_methods(client).keys.first
-    return if best_contact_method.blank?
+    return if best_contact_method.blank? || client.completion_survey_sent_at.present?
 
+    client.update!(completion_survey_sent_at: Time.current)
     survey_link = "https://codeforamerica.co1.qualtrics.com/jfe/form/SV_exiL2bLJx8GvjGC?ExternalDataReference=#{client.id}"
-
     case best_contact_method
     when :email
       ClientMessagingService.send_system_email(
@@ -16,10 +15,8 @@ class SendClientCompletionSurveyJob < ApplicationJob
     when :sms_phone_number
       ClientMessagingService.send_system_text_message(
         client,
-        I18n.t("messages.surveys.completion.email.body", locale: client.intake.locale, survey_link: survey_link),
+        I18n.t("messages.surveys.completion.text", locale: client.intake.locale, survey_link: survey_link),
       )
     end
-
-    client.update!(completion_survey_sent_at: Time.current)
   end
 end
