343a202a819dbd1852723f7b7a3751699858a34e
Asheesh Laroia <alaroia@codeforamerica.org>
2021-03-08 16:34:14 -0800

    Send completion survey by SMS if appropriate
    Co-authored-by: Ben Golder <bgolder@codeforamerica.org>

--- app/jobs/send_client_completion_survey_job.rb
+++ app/jobs/send_client_completion_survey_job.rb
@@ -1,12 +1,25 @@
 class SendClientCompletionSurveyJob < ApplicationJob
   def perform(client)
     return if client.completion_survey_sent_at.present?
+    best_contact_method = ClientMessagingService.contact_methods(client).keys.first
+    return if best_contact_method.blank?
 
-    ClientMessagingService.send_system_email(
-      client,
-      I18n.t("messages.surveys.completion.email.body", locale: client.intake.locale, client_id: client.id),
-      I18n.t("messages.surveys.completion.email.subject", locale: client.intake.locale)
-    )
+    survey_link = "https://codeforamerica.co1.qualtrics.com/jfe/form/SV_exiL2bLJx8GvjGC?ExternalDataReference=#{client.id}"
+
+    case best_contact_method
+    when :email
+      ClientMessagingService.send_system_email(
+        client,
+        I18n.t("messages.surveys.completion.email.body", locale: client.intake.locale, survey_link: survey_link),
+        I18n.t("messages.surveys.completion.email.subject", locale: client.intake.locale)
+      )
+    when :sms_phone_number
+      ClientMessagingService.send_system_text_message(
+        client,
+        I18n.t("messages.surveys.completion.email.body", locale: client.intake.locale, survey_link: survey_link),
+      )
+    end
+
     client.update!(completion_survey_sent_at: Time.current)
   end
 end
