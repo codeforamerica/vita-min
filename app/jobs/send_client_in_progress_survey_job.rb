b86ab826f3f36fe46317f021b69831430f4f443e
Asheesh Laroia <alaroia@codeforamerica.org>
2021-03-12 11:09:00 -0800

    Send in-progress survey from job [#177066379] (#922)
    Co-authored-by: Em Barnard-Shao <ebarnard@codeforamerica.org>

--- app/jobs/send_client_in_progress_survey_job.rb
+++ app/jobs/send_client_in_progress_survey_job.rb
@@ -1,5 +1,28 @@
 class SendClientInProgressSurveyJob < ApplicationJob
-  def perform
+  def perform(client)
+    best_contact_method = ClientMessagingService.contact_methods(client).keys.first
+    return if best_contact_method.blank?
 
+    # Avoid sending duplicate emails; use lock since there are multiple job workers
+    client.with_lock do
+      return if client.in_progress_survey_sent_at.present?
+
+      client.update!(in_progress_survey_sent_at: Time.current)
+    end
+
+    survey_link = "https://codeforamerica.co1.qualtrics.com/jfe/form/SV_6PDoi6ecHeQYiuq?ExternalDataReference=#{client.id}"
+    case best_contact_method
+    when :email
+      ClientMessagingService.send_system_email(
+        client,
+        I18n.t("messages.surveys.in_progress.email.body", locale: client.intake.locale, survey_link: survey_link, preferred_name: client.preferred_name, doc_upload_link: client.intake.requested_docs_token_link),
+        I18n.t("messages.surveys.in_progress.email.subject", locale: client.intake.locale)
+      )
+    when :sms_phone_number
+      ClientMessagingService.send_system_text_message(
+        client,
+        I18n.t("messages.surveys.in_progress.text", locale: client.intake.locale, survey_link: survey_link, preferred_name: client.preferred_name),
+      )
+    end
   end
 end
--- spec/jobs/send_client_in_progress_survey_job_spec.rb
+++ spec/jobs/send_client_in_progress_survey_job_spec.rb
@@ -2,5 +2,106 @@ require 'rails_helper'
 
 RSpec.describe SendClientInProgressSurveyJob, type: :job do
   describe "#perform" do
+    before do
+      allow(ClientMessagingService).to receive(:contact_methods).and_return({email: "example@example.com"})
+      allow(ClientMessagingService).to receive(:send_system_email)
+      allow(ClientMessagingService).to receive(:send_system_text_message)
+    end
+
+    let(:client) { create(:intake, locale: "es").client }
+
+    context "sending the survey" do
+      context "with a client who is opted-in to email notifications" do
+        before do
+          allow(ClientMessagingService).to receive(:contact_methods).and_return({email: "example@example.com"})
+        end
+
+        context "when the client has not received this survey" do
+          it "sends it by email" do
+            described_class.perform_now(client)
+
+            expect(ClientMessagingService).to have_received(:send_system_email).with(
+              client,
+              a_string_including("qualtrics.com"),
+              "Bienvenido a GetYourRefund.org. ¡Ya casi estás ahí!",
+              )
+            expect(client.reload.in_progress_survey_sent_at).to be_present
+          end
+        end
+      end
+
+      context "with a client who is opted-in to sms notifications" do
+        let(:client) { create(:intake, locale: "es").client }
+        before do
+          allow(ClientMessagingService).to receive(:contact_methods).and_return({sms_phone_number: "+14155551212"})
+        end
+
+        context "when the client has not received this survey" do
+          it "sends it by text" do
+            described_class.perform_now(client)
+
+            expect(ClientMessagingService).to have_received(:send_system_text_message).with(
+              client,
+              a_string_including("qualtrics.com"),
+              )
+            expect(ClientMessagingService).not_to have_received(:send_system_email)
+            expect(client.reload.in_progress_survey_sent_at).to be_present
+          end
+        end
+      end
+
+      context "with a client who is opted-in to email and sms notifications" do
+        before do
+          allow(ClientMessagingService).to receive(:contact_methods).and_return({email: "example@example.com", sms_phone_number: "+14155551212"})
+        end
+
+        context "when the client has not received this survey" do
+          it "sends it by email" do
+            described_class.perform_now(client)
+
+            expect(ClientMessagingService).to have_received(:send_system_email).with(
+              client,
+              a_string_including("qualtrics.com"),
+              "Bienvenido a GetYourRefund.org. ¡Ya casi estás ahí!"
+            )
+            expect(ClientMessagingService).not_to have_received(:send_system_text_message)
+            expect(client.reload.in_progress_survey_sent_at).to be_present
+          end
+        end
+      end
+    end
+
+    context "not sending the survey" do
+      context "with a client who has already received this survey with contact methods available" do
+        before do
+          allow(ClientMessagingService).to receive(:contact_methods).and_return({email: "example@example.com"})
+          client.update(in_progress_survey_sent_at: DateTime.new(2021, 1, 1))
+        end
+
+        it "does not send it" do
+          expect {
+            described_class.perform_now(client)
+          }.not_to change { client.reload.in_progress_survey_sent_at }
+
+          expect(ClientMessagingService).not_to have_received(:send_system_email)
+          expect(ClientMessagingService).not_to have_received(:send_system_text_message)
+        end
+      end
+
+      context "with a client with no contact methods available" do
+        before do
+          allow(ClientMessagingService).to receive(:contact_methods).and_return({})
+        end
+
+        it "does not send it" do
+          expect {
+            described_class.perform_now(client)
+          }.not_to change { client.reload.in_progress_survey_sent_at }
+
+          expect(ClientMessagingService).not_to have_received(:send_system_email)
+          expect(ClientMessagingService).not_to have_received(:send_system_text_message)
+        end
+      end
+    end
   end
 end
