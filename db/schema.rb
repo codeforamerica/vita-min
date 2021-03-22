061de4faf651a5c37a7030212d9d79f147ab7a22
Shannon Byrne <sbyrne@codeforamerica.org>
2021-03-22 08:43:02 -0500

    Add current_step migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2021_03_18_180556) do
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
   enable_extension "plpgsql"
@@ -289,6 +288,7 @@ ActiveRecord::Schema.define(version: 2021_03_18_180556) do
     t.datetime "completed_yes_no_questions_at"
     t.boolean "continued_at_capacity", default: false
     t.datetime "created_at"
+    t.string "current_step"
     t.integer "demographic_disability", default: 0, null: false
     t.integer "demographic_english_conversation", default: 0, null: false
     t.integer "demographic_english_reading", default: 0, null: false
