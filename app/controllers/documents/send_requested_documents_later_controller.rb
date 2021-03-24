cf8354aa27c77b6a73d4d100142eb2412905068c
Shannon Byrne <s.byrne2254@gmail.com>
2021-03-24 16:40:08 -0700

    Fix conditional touch of document request (#952)

--- app/controllers/documents/send_requested_documents_later_controller.rb
+++ app/controllers/documents/send_requested_documents_later_controller.rb
@@ -1,14 +1,15 @@
 module Documents
   class SendRequestedDocumentsLaterController < DocumentUploadQuestionController
-    append_after_action :reset_session, :track_page_view, :complete_documents_request, only: :edit
+    append_after_action :reset_session, :track_page_view, only: :edit
     skip_before_action :require_intake
 
     def edit
       @documents_request = DocumentsRequest.find_by(id: session[:documents_request_id])
       if @documents_request.nil?
-        flash[:warning] =  t("controllers.send_requested_documents_later_controller.not_found")
+        flash[:warning] = t("controllers.send_requested_documents_later_controller.not_found")
       else
-        flash[:notice] =  t("controllers.send_requested_documents_later_controller.success")
+        @documents_request.touch(:completed_at)
+        flash[:notice] = t("controllers.send_requested_documents_later_controller.success")
       end
       redirect_to(root_path)
     end
@@ -23,10 +24,6 @@ module Documents
 
     def self.document_type
       nil
-    end
-
-    def complete_documents_request
-      @documents_request.touch(:completed_at)
     end
   end
 end
