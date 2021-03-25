7fe6b6c4e2ead25999638f267945826fc859a87b
Ben Golder <bgolder@codeforamerica.org>
2021-03-25 16:23:18 -0700

    Add client sign in link to intake
    Co-authored-by: Colt Borg <cborg@codeforamerica.org>

--- app/controllers/questions/anonymous_intake_controller.rb
+++ app/controllers/questions/anonymous_intake_controller.rb
@@ -1,5 +1,11 @@
 module Questions
   class AnonymousIntakeController < QuestionsController
-    before_action :require_intake
+    before_action :require_intake, :set_show_client_sign_in_link
+
+    private
+
+    def set_show_client_sign_in_link
+      @show_client_sign_in_link = true
+    end
   end
 end
