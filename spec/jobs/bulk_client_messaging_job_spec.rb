04a0775689d4d1b980c1e5226df8c61f1fa8087b
Ben Golder <bgolder@codeforamerica.org>
2021-04-12 09:07:49 -0700

    Only message accessible clients

--- spec/jobs/bulk_client_messaging_job_spec.rb
+++ spec/jobs/bulk_client_messaging_job_spec.rb
@@ -4,7 +4,7 @@ describe BulkClientMessagingJob, type: :job do
   let(:message_body_en) { "Hey how's it going?" }
   let(:message_body_es) { "Oye como va?" }
   let!(:client_selection) { create :client_selection }
-  let(:user) { create :user }
+  let(:user) { create :admin_user }
   before do
     allow(ClientMessagingService).to receive(:send_message_to_all_opted_in_contact_methods)
   end
@@ -51,6 +51,24 @@ describe BulkClientMessagingJob, type: :job do
             described_class.perform_now(client_selection, user, es: message_body_es)
           end.to raise_error(ArgumentError)
         end
+      end
+    end
+
+    context "when the sender can't access some of the clients" do
+      let(:organization) { create :organization }
+      let(:other_org) { create :organization }
+      let(:user) { create :organization_lead_user, organization: organization }
+      let!(:accessible_client) { create(:intake, client: create(:client, client_selections: [client_selection], vita_partner: organization)).client }
+      let!(:inaccessible_client) { create(:intake, client: create(:client, client_selections: [client_selection], vita_partner: other_org)).client }
+
+      it "scopes down to only the accessible clients" do
+        described_class.perform_now(client_selection, user, en: message_body_en)
+        expect(ClientMessagingService).to have_received(:send_message_to_all_opted_in_contact_methods).with(
+          accessible_client, user, message_body_en
+        )
+        expect(ClientMessagingService).not_to have_received(:send_message_to_all_opted_in_contact_methods).with(
+          inaccessible_client, user, message_body_en
+        )
       end
     end
   end
