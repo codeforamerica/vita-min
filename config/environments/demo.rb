150a353ca77b21ff472f797175ce95df70457df5
Shannon Byrne <sbyrne@codeforamerica.org>
2021-04-13 16:41:59 -0500

    Configure asset hosts for mailing in prod and demo

--- config/environments/demo.rb
+++ config/environments/demo.rb
@@ -6,6 +6,7 @@ Rails.application.configure do
   config.action_mailer.default_options = { from: 'hello@mg-demo.getyourrefund-testing.org' }
   config.address_for_transactional_authentication_emails = 'no-reply@mg-demo.getyourrefund-testing.org'
   config.action_mailer.default_url_options = { host: 'demo.getyourrefund.org' }
+  config.action_mailer.asset_host = "https://demo.getyourrefund.org"
   config.offseason = false
   Rails.application.default_url_options = config.action_mailer.default_url_options
 end
