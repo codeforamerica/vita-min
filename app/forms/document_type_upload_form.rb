47f1a9bbd55db134e2833e9b6c719d95d1c50f6f
Asheesh Laroia <alaroia@codeforamerica.org>
2021-05-05 14:20:08 -0700

    Show document model validation errors to clients during intake [#177322945]
    Co-authored-by: Asheesh Laroia <alaroia@codeforamerica.org>

--- app/forms/document_type_upload_form.rb
+++ app/forms/document_type_upload_form.rb
@@ -1,6 +1,7 @@
 class DocumentTypeUploadForm < QuestionsForm
   set_attributes_for :intake, :document
   validates :document, file_type_allowed: true
+  validate :load_and_validate_document
 
   def initialize(document_type, *args, **kwargs)
     @document_type = document_type
@@ -8,22 +9,32 @@ class DocumentTypeUploadForm < QuestionsForm
   end
 
   def save
+    return false unless valid?
+
+    @doc.save!
+  end
+
+  private
+
+  def load_and_validate_document
     document_file_upload = attributes_for(:intake)[:document]
-    if document_file_upload.present?
-      doc = @intake.documents.new(
+    # Rewind to avoid IntegrityError
+    document_file_upload.tempfile.rewind
+    doc = @intake.documents.new(
         document_type: @document_type,
         client: @intake.client,
-        uploaded_by: @intake.client
-      )
-      # Rewind to avoid IntegrityError
-      document_file_upload.tempfile.rewind
-      # Remove non-utf-8 characters from the original filename
-      doc.upload.attach(
-        io: document_file_upload.tempfile,
-        filename: document_file_upload.original_filename.encode("UTF-8", invalid: :replace, replace: ""),
-        content_type: document_file_upload.content_type
-      )
-      doc.save!
+        uploaded_by: @intake.client,
+        upload: {
+          io: document_file_upload.tempfile,
+          # Remove non-utf-8 characters from the original filename
+          filename: document_file_upload.original_filename.encode("UTF-8", invalid: :replace, replace: ""),
+          content_type: document_file_upload.content_type
+        }
+    )
+    if doc.valid?
+      @doc = doc
+    else
+      doc.errors.values.flatten.each { |msg| errors[:document] << msg }
     end
   end
 end
