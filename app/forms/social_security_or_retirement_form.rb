9ab1a97f53be71009b3bc876dd948fea96297649
Shannon Byrne <s.byrne2254@gmail.com>
2021-05-14 17:42:40 -0500

    Update gated questions to no when someone answers the gated retirement questions to no (#1099)

--- app/forms/social_security_or_retirement_form.rb
+++ app/forms/social_security_or_retirement_form.rb
@@ -2,6 +2,20 @@ class SocialSecurityOrRetirementForm < QuestionsForm
   set_attributes_for :intake, :had_social_security_or_retirement
 
   def save
-    @intake.update(attributes_for(:intake))
+    if had_social_security_or_retirement == "no"
+      @intake.update(attributes_for(:intake).merge(gated_answers))
+    else
+      @intake.update(attributes_for(:intake))
+    end
+  end
+
+  private
+
+  def gated_answers
+    Hash[gated_questions.map { |question| [question, "no"] }]
+  end
+
+  def gated_questions
+    [:had_social_security_income, :had_retirement_income, :paid_retirement_contributions]
   end
 end
---
+++ spec/forms/social_security_or_retirement_form_spec.rb
@@ -0,0 +1,45 @@
+require "rails_helper"
+
+describe SocialSecurityOrRetirementForm do
+  let(:intake) { create :intake, client: (create :client) }
+  subject { described_class.new(intake, { had_social_security_or_retirement: had_social_security_or_retirement })}
+
+  context 'when had_social_security_or_retirement is yes' do
+    let(:had_social_security_or_retirement) { "yes" }
+    it 'updates had_social_security_or_retirement to yes and leaves gated questions unfilled' do
+      subject.save
+      intake.reload
+
+      expect(intake.had_social_security_or_retirement).to eq "yes"
+      expect(intake.had_social_security_income).to eq "unfilled"
+      expect(intake.had_retirement_income).to eq "unfilled"
+      expect(intake.paid_retirement_contributions).to eq "unfilled"
+    end
+  end
+
+  context 'when had_social_security_or_retirement is unsure' do
+    let(:had_social_security_or_retirement) { "unsure" }
+    it 'updates had_social_security_or_retirement to yes and leaves gated questions unfilled' do
+      subject.save
+      intake.reload
+
+      expect(intake.had_social_security_or_retirement).to eq "unsure"
+      expect(intake.had_social_security_income).to eq "unfilled"
+      expect(intake.had_retirement_income).to eq "unfilled"
+      expect(intake.paid_retirement_contributions).to eq "unfilled"
+    end
+  end
+
+  context 'when had social_security_or_retirement is no' do
+    let(:had_social_security_or_retirement) { "no" }
+    it 'updates had_social_security_or_retirement to no and makes gated questions no' do
+      subject.save
+      intake.reload
+
+      expect(intake.had_social_security_or_retirement).to eq "no"
+      expect(intake.had_social_security_income).to eq "no"
+      expect(intake.had_retirement_income).to eq "no"
+      expect(intake.paid_retirement_contributions).to eq "no"
+    end
+  end
+end
