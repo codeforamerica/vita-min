976eec4884e598e4907240d551e6a5c0a8ec59f0
Shannon Byrne <sbyrne@codeforamerica.org>
2021-05-14 11:50:52 -0500

    Update personal info controller to validate the form again and set up next_path to at_capacity if a vita partner is not returned

--- app/controllers/questions/personal_info_controller.rb
+++ app/controllers/questions/personal_info_controller.rb
@@ -6,23 +6,25 @@ module Questions
       {}
     end
 
-    def update
+    def after_update_success
       unless Client.after_consent.where(intake: current_intake).exists?
         routing_service = PartnerRoutingService.new(
           source_param: current_intake.source,
-          zip_code: form_params[:zip_code],
+          zip_code: current_intake.zip_code,
         )
-        vita_partner = routing_service.determine_partner
+        @vita_partner = routing_service.determine_partner
 
-        if vita_partner.present?
-          current_intake.client.update(vita_partner: vita_partner, routing_method: routing_service.routing_method)
-          current_intake.update(vita_partner: vita_partner)
-          super
+        if @vita_partner.present?
+          current_intake.client.update(vita_partner: @vita_partner, routing_method: routing_service.routing_method)
+          current_intake.update(vita_partner: @vita_partner)
         else
           current_intake.client.update(routing_method: routing_service.routing_method)
-          return redirect_to at_capacity_questions_path
         end
       end
+    end
+
+    def next_path
+      @vita_partner.present? ? super : at_capacity_questions_path
     end
   end
 end
