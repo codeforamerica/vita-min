8bdec625145945cceb04cd5cc3a48f5923424d43
Asheesh Laroia <alaroia@codeforamerica.org>
2021-05-18 11:41:09 -0400

    Alphabetize more
    Co-authored-by: Asheesh Laroia <alaroia@codeforamerica.org>

--- spec/services/recent_message_summary_service_spec.rb
+++ spec/services/recent_message_summary_service_spec.rb
@@ -17,6 +17,34 @@ describe RecentMessageSummaryService do
       let!(:old_outgoing_email) { create :outgoing_email, created_at: DateTime.new(2021, 3, 18, 0, 0, 0), user: user, client: client, body: "Hello this is an old email" }
       let!(:old_outgoing_text) { create :outgoing_text_message, created_at: DateTime.new(2021, 3, 17, 0, 0, 0), user: user, client: client, body: "Hello this is an old text" }
 
+      context "given a client ID with most recent message type: incoming email" do
+        let(:time) { DateTime.new(2021, 3, 21, 0, 0, 0) }
+        before do
+          Timecop.freeze(time) do
+            create(:incoming_email, client: client, body_plain: "Have a nice day")
+          end
+        end
+
+        it "returns the message body, author, and date" do
+          expect(RecentMessageSummaryService.messages([client.id])).to(
+            eq({client.id => {body: "Have a nice day", date: time, author: "Client Name"}}))
+        end
+      end
+
+      context "given a client ID with most recent message type: incoming text message" do
+        let(:time) { DateTime.new(2021, 3, 21, 0, 0, 0) }
+        before do
+          Timecop.freeze(time) do
+            create(:incoming_text_message, client: client, body: "Have a nice day")
+          end
+        end
+
+        it "returns the message body, author, and date" do
+          expect(RecentMessageSummaryService.messages([client.id])).to(
+            eq({client.id => {body: "Have a nice day", date: time, author: "Client Name"}}))
+        end
+      end
+
       context "given a client ID with most recent message type: outgoing email" do
         let(:time) { DateTime.new(2021, 3, 21, 0, 0, 0) }
 
@@ -46,33 +74,6 @@ describe RecentMessageSummaryService do
         end
       end
 
-      context "given a client ID with most recent message type: incoming email" do
-        let(:time) { DateTime.new(2021, 3, 21, 0, 0, 0) }
-        before do
-          Timecop.freeze(time) do
-            create(:incoming_email, client: client, body_plain: "Have a nice day")
-          end
-        end
-
-        it "returns the message body, author, and date" do
-          expect(RecentMessageSummaryService.messages([client.id])).to(
-            eq({client.id => {body: "Have a nice day", date: time, author: "Client Name"}}))
-        end
-      end
-
-      context "given a client ID with most recent message type: incoming text message" do
-        let(:time) { DateTime.new(2021, 3, 21, 0, 0, 0) }
-        before do
-          Timecop.freeze(time) do
-            create(:incoming_text_message, client: client, body: "Have a nice day")
-          end
-        end
-
-        it "returns the message body, author, and date" do
-          expect(RecentMessageSummaryService.messages([client.id])).to(
-            eq({client.id => {body: "Have a nice day", date: time, author: "Client Name"}}))
-        end
-      end
     end
   end
 end
