0973c12f9ce4a80fe198aad5a31633cb1835736c
Asheesh Laroia <alaroia@codeforamerica.org>
2021-05-18 11:42:13 -0400

    Alphabetize more more more
    Co-authored-by: Asheesh Laroia <alaroia@codeforamerica.org>

--- app/services/recent_message_summary_service.rb
+++ app/services/recent_message_summary_service.rb
@@ -18,6 +18,38 @@ class RecentMessageSummaryService
 
   SummarizedMessage = Struct.new(:created_at, :body, :author, :client_id)
 
+  def self.summarize_incoming_emails(client_ids)
+    summaries = {}
+    IncomingEmail.find_by_sql(
+      [
+        "select incoming_emails.id, incoming_emails.client_id, incoming_emails.body_plain, incoming_emails.created_at, intakes.preferred_name as prefetched_author from incoming_emails inner join intakes on intakes.client_id = incoming_emails.client_id where incoming_emails.created_at IN (select max(incoming_emails.created_at) from incoming_emails group by incoming_emails.client_id having incoming_emails.client_id in ( ? ))",
+        client_ids,
+      ]
+    ).each do |msg|
+      if summaries[msg.client_id].nil? || summaries[msg.client_id].created_at < msg.created_at
+        summaries[msg.client_id] = SummarizedMessage.new(msg.created_at, msg.body_plain, msg.prefetched_author)
+      end
+    end
+
+    summaries
+  end
+
+  def self.summarize_incoming_text_messages(client_ids)
+    summaries = {}
+    IncomingTextMessage.find_by_sql(
+      [
+        "select incoming_text_messages.id, incoming_text_messages.client_id, incoming_text_messages.body, incoming_text_messages.created_at, intakes.preferred_name as prefetched_author from incoming_text_messages inner join intakes on intakes.client_id = incoming_text_messages.client_id where incoming_text_messages.created_at IN (select max(incoming_text_messages.created_at) from incoming_text_messages group by incoming_text_messages.client_id having incoming_text_messages.client_id in ( ? ))",
+        client_ids,
+      ]
+    ).each do |msg|
+      if summaries[msg.client_id].nil? || summaries[msg.client_id].created_at < msg.created_at
+        summaries[msg.client_id] = SummarizedMessage.new(msg.created_at, msg.body, msg.prefetched_author)
+      end
+    end
+
+    summaries
+  end
+
   def self.summarize_outgoing_emails(client_ids)
     summaries = {}
     OutgoingEmail.find_by_sql(
@@ -50,35 +82,4 @@ class RecentMessageSummaryService
     summaries
   end
 
-  def self.summarize_incoming_emails(client_ids)
-    summaries = {}
-    IncomingEmail.find_by_sql(
-      [
-        "select incoming_emails.id, incoming_emails.client_id, incoming_emails.body_plain, incoming_emails.created_at, intakes.preferred_name as prefetched_author from incoming_emails inner join intakes on intakes.client_id = incoming_emails.client_id where incoming_emails.created_at IN (select max(incoming_emails.created_at) from incoming_emails group by incoming_emails.client_id having incoming_emails.client_id in ( ? ))",
-        client_ids,
-      ]
-    ).each do |msg|
-      if summaries[msg.client_id].nil? || summaries[msg.client_id].created_at < msg.created_at
-        summaries[msg.client_id] = SummarizedMessage.new(msg.created_at, msg.body_plain, msg.prefetched_author)
-      end
-    end
-
-    summaries
-  end
-
-  def self.summarize_incoming_text_messages(client_ids)
-    summaries = {}
-    IncomingTextMessage.find_by_sql(
-      [
-        "select incoming_text_messages.id, incoming_text_messages.client_id, incoming_text_messages.body, incoming_text_messages.created_at, intakes.preferred_name as prefetched_author from incoming_text_messages inner join intakes on intakes.client_id = incoming_text_messages.client_id where incoming_text_messages.created_at IN (select max(incoming_text_messages.created_at) from incoming_text_messages group by incoming_text_messages.client_id having incoming_text_messages.client_id in ( ? ))",
-        client_ids,
-      ]
-    ).each do |msg|
-      if summaries[msg.client_id].nil? || summaries[msg.client_id].created_at < msg.created_at
-        summaries[msg.client_id] = SummarizedMessage.new(msg.created_at, msg.body, msg.prefetched_author)
-      end
-    end
-
-    summaries
-  end
 end
