001da3bbd6b2d307ed3fc78f99f0384aea0beaab
Em Barnard-Shao <ebarnard@codeforamerica.org>
2021-05-24 10:49:57 -0400

    Add table name to order by
    Co-authored-by: Asheesh Laroia <alaroia@codeforamerica.org>

--- app/models/client.rb
+++ app/models/client.rb
@@ -114,9 +114,9 @@ class Client < ApplicationRecord
     else
       order = {column => direction}
       if column == "first_unanswered_incoming_interaction_at" && direction == "desc"
-        order = "first_unanswered_incoming_interaction_at DESC NULLS LAST"
+        order = "clients.first_unanswered_incoming_interaction_at DESC NULLS LAST"
       elsif column == "last_outgoing_communication_at" && direction == "desc"
-        order = "last_outgoing_communication_at DESC NULLS LAST"
+        order = "clients.last_outgoing_communication_at DESC NULLS LAST"
       end
 
       includes(:intake).order(order).distinct
