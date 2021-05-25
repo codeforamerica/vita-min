a4467e6817218a83e254e1956aded4d264cb92b5
Catie Talbot <ctalbot@codeforamerica.org>
2021-05-25 11:53:22 -0700

    Add tagging helper
    Co-authored-by: Colt Borg <cborg@codeforamerica.org>

---
+++ app/helpers/tagging_helper.rb
@@ -0,0 +1,12 @@
+module TaggingHelper
+  def taggable_vita_partners(vita_partners)
+    taggable_vita_partners = []
+    vita_partners.organizations.each do |vita_partner|
+      taggable_vita_partners << { id: vita_partner.id, name: vita_partner.name, value: vita_partner.id }
+      vita_partner.child_sites.each do |site|
+        taggable_vita_partners << { id: site.id, name: site.name, value: site.id }
+      end
+    end
+    taggable_vita_partners.to_json.to_s.html_safe
+  end
+end
--- app/javascript/lib/tagging/index.js
+++ app/javascript/lib/tagging/index.js
@@ -32,15 +32,10 @@ export function initTaggableNote() {
 export function initMultiSelectVitaPartner() {
     const input = document.querySelector('.multi-select-vita-partner');
 
-    const whitelist = window.taggableItems.map((i) => {
-        if (!i.value) { i.value = i.id } // whitelist items MUST have a value property
-        return i;
-    });
-
     new Tagify(input, {
         tagTextProp: 'name',  // <-- defines which attr is used as the tag display value
         // Array for initial interpolation, which allows only these tags to be used
-        whitelist: whitelist,
+        whitelist: window.taggableItems,
         enforceWhitelist: true,
         dropdown : {
             classname: "multi-select-dropdown",
@@ -49,7 +44,7 @@ export function initMultiSelectVitaPartner() {
             searchKeys: ['name'], // <-- defines the attr used to search for in dropdown
             highlightFirst: true,  // <-- automatically highlights first suggestion item in the dropdown
             closeOnSelect: false, // <-- keep dropdown open after selection
-            maxItems: whitelist.length, // <-- render all available items for the dropdown
+            maxItems: window.taggableItems.length, // <-- render all available items for the dropdown
         }
     });
 }
