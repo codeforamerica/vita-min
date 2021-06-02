7784f3124ae22c903dcb2242d4a4a183c110a802
Asheesh Laroia <alaroia@codeforamerica.org>
2021-06-02 13:26:09 -0400

    wip
    Co-authored-by: Asheesh Laroia <alaroia@codeforamerica.org>

--- app/controllers/portal/still_needs_helps_controller.rb
+++ app/controllers/portal/still_needs_helps_controller.rb
@@ -7,10 +7,17 @@ module Portal
     def edit; end
 
     def update
-      if update_params[:still_needs_help] == "yes" && current_client.triggered_still_needs_help_at.present?
-        current_client.update!(triggered_still_needs_help_at: nil, first_unanswered_incoming_interaction_at: Time.now)
+      if update_params[:still_needs_help] == "yes"
+        if current_client.triggered_still_needs_help_at.present?
+          current_client.update!(triggered_still_needs_help_at: nil, still_needs_help: "yes")
+          current_client.tax_returns.where(status: "file_not_filing").each { |tax_return| tax_return.update!(status: "file_hold") }
+          InteractionTrackingService.record_incoming_interaction(current_client)
+        end
+
         redirect_to portal_still_needs_help_yes_path
       else
+        current_client.system_notes.create!(body: "Client indicated that they no longer need tax help")
+        InteractionTrackingService.record_incoming_interaction(current_client)
         redirect_to portal_still_needs_help_no_path
       end
     end
--- spec/controllers/portal/still_needs_helps_controller_spec.rb
+++ spec/controllers/portal/still_needs_helps_controller_spec.rb
@@ -9,35 +9,40 @@ describe Portal::StillNeedsHelpsController do
     it_behaves_like :a_post_action_for_authenticated_clients_only, action: :update
 
     context "with an authenticated client" do
+      before do
+        allow(InteractionTrackingService).to receive(:record_incoming_interaction)
+        allow(MixpanelService).to receive(:send_status_change_event)
+      end
+
       context "when the client has triggered still needs help" do
         let(:tax_return_in_progress) { build(:tax_return, status: :intake_in_progress, year: 2018) }
         let(:tax_return_not_filing) { build(:tax_return, status: :file_not_filing, year: 2019) }
-        let(:client) { create :client, tax_returns: [tax_return_in_progress, tax_return_not_filing], triggered_still_needs_help_at: Time.now }
+        let(:client) { create :client, tax_returns: [tax_return_in_progress, tax_return_not_filing], triggered_still_needs_help_at: Time.now, intake: build(:intake) }
+        let(:fake_time) { DateTime.new(2021, 1, 1) }
         before { sign_in client }
 
         context "client indicates they still need help" do
-          let(:fake_time) { DateTime.new(2021, 1, 1) }
-
           it "saves answer, tax return statuses, first_unanswered_incoming_interaction_at, and clears triggered_still_needs_help_at" do
             Timecop.freeze(fake_time) { put :update, params: { still_needs_help: "yes" } }
 
             expect(tax_return_in_progress.reload.status).to eq "intake_in_progress"
-            expect(tax_return_not_filing.reload.status).to eq "file_not_filing"
+            expect(tax_return_not_filing.reload.status).to eq "file_hold"
             expect(client.reload.triggered_still_needs_help_at).to be_nil
-            # idea is to add a column still_needs_help with enum values "yes" "no"
-            # reason: yvonne/nicole want to preserve who has been through the flow
             expect(client.still_needs_help_yes?).to eq true
-            expect(client.first_unanswered_incoming_interaction_at).to eq fake_time
+            expect(InteractionTrackingService).to have_received(:record_incoming_interaction).with(client)
+            expect(MixpanelService).to have_received(:send_status_change_event).with(tax_return_not_filing)
           end
         end
 
         context "client indicates they longer needs help" do
           it "keeps their statuses as not filing and adds an internal note" do
             expect {
-              put :update, params: { still_needs_help: "no" }
-            }.to(change(Note, :count).by(1)).and.not_to(change(tax_return, :status))
+              Timecop.freeze(fake_time) { put :update, params: { still_needs_help: "no" } }
+            }.to(change(SystemNote, :count).by(1))
+            expect(client.reload.tax_returns.pluck(:status)).to match_array %w[file_not_filing intake_in_progress]
+            expect(InteractionTrackingService).to have_received(:record_incoming_interaction).with(client)
 
-            note = Note.last
+            note = SystemNote.last
             expect(note.body).to eq "Client indicated that they no longer need tax help"
           end
         end
