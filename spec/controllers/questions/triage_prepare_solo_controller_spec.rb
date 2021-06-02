05b1ab991b3344cb73c28824054c5a36078f5011
Catie Talbot <ctalbot@codeforamerica.org>
2021-06-02 11:25:29 -0700

    Code review spec updates

--- spec/controllers/questions/triage_prepare_solo_controller_spec.rb
+++ spec/controllers/questions/triage_prepare_solo_controller_spec.rb
@@ -37,7 +37,7 @@ RSpec.describe Questions::TriagePrepareSoloController do
 
     context "when no" do
       let(:will_prepare) { "no" }
-      it "redirects to the next triage question" do
+      it "redirects to the file with help page" do
         put :update, params: params
         expect(response).to redirect_to file_with_help_questions_path
         expect(MixpanelService).to have_received(:send_event).with(
