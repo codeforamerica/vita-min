05d69e8d3f0a152c4806f7426516fd9d2df54c54
Em Barnard-Shao <ebarnard@codeforamerica.org>
2021-06-08 12:26:01 -0400

    WIP: Add create_intercom_message_from_email method
    Co-authored-by: Jenny Heath <jheath@codeforamerica.org>

--- app/services/intercom_service.rb
+++ app/services/intercom_service.rb
@@ -1,52 +1,25 @@
 class IntercomService
-  # create an intercomlead model that stores lead ids
-  def create_message_from_user(body, lead)
-    message_properties = {
-      from: {
-        type: "user",
-        id: lead.id #user_id/lead_id
-      },
-      body: body
-    }
-    message = @intercom.messages.create(message_properties)
-
-    message.id
-  end
-
   def create_intercom_message_from_email(incoming_email)
     email = incoming_email.client.intake.email_address
-    lead_from_email = lead_from_email(email)
-    if lead_from_email.present?
-      intercom.messages.create(body: incoming_email.body, lead: lead_from_email)
-    else
-      new_lead = intercom.create_lead(email)
-      intercom.messages.create(body: incoming_email.body, lead: new_lead)
-    end
-    #  if intercom client can find a lead with this email
-    #   create new lead and new message
-    # else
-    #   create new message for lead
+    contact_id_from_email = contact_id_from_email(email)
+    contact_id = contact_id_from_email.present? ? contact_id_from_email : intercom.contacts.create(email: email, role: "contact").id
+    intercom.messages.create({
+                               from: {
+                                 type: "contact",
+                                 id: contact_id
+                               },
+                               body: incoming_email.body
+                             })
   end
 
-  def create_lead_by_email
-
-  end
-
-  def lead_from_email(email)
-    @intercom.contacts.search(
-      "query": {
-        "field": 'email',
-        "operator": '=',
-        "value": email
-      }
-    )
+  def contact_id_from_email(email)
+    contact = intercom.contacts.find(email: email).to_hash
+    contact["data"][0]["id"]
   end
 
   private
 
   def intercom
-
-      Intercom::Client.new(token: "dG9rOjNmYmU5NzhiXzBmMGRfNDc2Zl85NTU1XzRmMDdjODI5Yjg5MzoxOjA=")
-      @intercom ||= Intercom::Client.new(token: EnvironmentCredentials.dig(:intercom, :intercom_access_token))
+    @intercom ||= Intercom::Client.new(token: EnvironmentCredentials.dig(:intercom, :intercom_access_token)) #need to add access token
   end
 end
