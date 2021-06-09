24836b056869b7ce12c8c632625f0385d3f5f737
Catie Talbot <ctalbot@codeforamerica.org>
2021-06-09 10:54:52 -0700

    Very tiny syntax improvement

--- spec/controllers/concerns/client_sortable_spec.rb
+++ spec/controllers/concerns/client_sortable_spec.rb
@@ -117,7 +117,7 @@ RSpec.describe ClientSortable, type: :controller do
 
       it "creates a query for the search and scopes to vita partner" do
         expect(subject.filtered_and_sorted_clients).to eq clients_query_double
-        expect(clients_query_double).to have_received(:where).with({:vita_partner_id=>[vita_partner.id]})
+        expect(clients_query_double).to have_received(:where).with(vita_partner_id: [vita_partner.id])
       end
 
       context "more than one vita partner is selected" do
@@ -128,7 +128,7 @@ RSpec.describe ClientSortable, type: :controller do
 
         it "creates a query for the search and scopes to all selected vita partners" do
           expect(subject.filtered_and_sorted_clients).to eq clients_query_double
-          expect(clients_query_double).to have_received(:where).with({:vita_partner_id=>[vita_partner.id, site.id]})
+          expect(clients_query_double).to have_received(:where).with(vita_partner_id: [vita_partner.id, site.id])
         end
       end
     end
