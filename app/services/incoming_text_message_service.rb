7a2fc6a123479fe1dff4f25c1fbea3b67cb25f52
Em Barnard-Shao <ebarnard@codeforamerica.org>
2021-06-10 13:07:55 -0400

    Remove slash

--- app/services/incoming_text_message_service.rb
+++ app/services/incoming_text_message_service.rb
@@ -51,7 +51,7 @@ class IncomingTextMessageService
         documents: documents
       )
 
-      IntercomService.create_intercom_message_from_sms(contact_record, fw_from_hub: true) if (client.tax_returns.pluck(:status).map(&:to_sym) & TaxReturnStatus::FORWARD_TO_INTERCOM_STATUSES).any?\
+      IntercomService.create_intercom_message_from_sms(contact_record, fw_from_hub: true) if (client.tax_returns.pluck(:status).map(&:to_sym) & TaxReturnStatus::FORWARD_TO_INTERCOM_STATUSES).any?
 
       ClientChannel.broadcast_contact_record(contact_record)
     end
