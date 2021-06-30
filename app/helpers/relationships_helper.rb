5c8601174141c3e9d8914914e027272a490bfcab
Travis Grathwell <tgrathwell@codeforamerica.org>
2021-06-30 11:29:10 -0700

    CTC Valet form: Show a different, larger list of dependent relationship names
    Co-authored-by: Catie Talbot <ctalbot@codeforamerica.org>

--- app/helpers/relationships_helper.rb
+++ app/helpers/relationships_helper.rb
@@ -1,16 +1,28 @@
 module RelationshipsHelper
   # provides a list of relationship options for dependents, and pushes the client's provided free-form answer
   # to the list if necessary.
-  def dependent_relationship_options(current_relationship = nil)
-    options = I18n.t("general.dependent_relationships").map { |k, v| [v, k] }
+  def dependent_relationship_options(current_relationship: nil, is_ctc: false)
+    allowed_types = is_ctc ? [:ctc] : [:default]
+    relationships_hash = Hash[sorted_relationships(types: allowed_types)]
+    options = relationships_hash.map { |k, v| [v, k] }
     if current_relationship.present?
-      key = I18n.t("general.dependent_relationships")[current_relationship.to_sym]
+      key = relationships_hash[current_relationship.to_sym]
       options.push(["#{I18n.t("general.dependent_relationships.other")}: #{current_relationship}", current_relationship]) unless key.present?
     end
     options
   end
 
   def translated_relationship(relationship)
-    I18n.t("general.dependent_relationships.#{relationship}", default: relationship)
+    Hash[sorted_relationships(types: [:ctc, :default])][relationship.to_sym] || relationship
+  end
+
+  private
+
+  def sorted_relationships(types:)
+    yaml_relationships = {}
+    yaml_relationships.merge!(I18n.t("general.dependent_relationships")) if types.include?(:default)
+    yaml_relationships.merge!(I18n.t("general.ctc_dependent_relationships")) if types.include?(:ctc)
+
+    yaml_relationships.map { |k, v| [k.to_s.sub(/^\d+_/, '').to_sym, v] }
   end
 end
