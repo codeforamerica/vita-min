3ce3926705799c496af4a41a7038404671059f41
Mike Lawrence <contact@michaeltlawrence.com>
2021-07-14 16:54:05 -0500

    Creates child tax credit calculator

---
+++ spec/services/child_tax_credit_calculator_spec.rb
@@ -0,0 +1,21 @@
+require "rails_helper"
+
+describe ChildTaxCreditCalculator do
+  describe ".monthly_payment_due" do
+    it "should return the correct payment amount based on dependent counts" do
+      expected_payment = 850
+      monthly_payment_amount = described_class.monthly_payment_due(dependents_under_six_count: 1, dependents_over_six_count: 1)
+
+      expect(monthly_payment_amount).to eq(expected_payment)
+    end
+  end
+
+  describe ".total_payment_due" do
+    it "should return the correct payment amount based on dependent counts" do
+      expected_payment = 10200
+      total_payment_amount = described_class.total_payment_due(dependents_under_six_count: 1, dependents_over_six_count: 1)
+
+      expect(total_payment_amount).to eq(expected_payment)
+    end
+  end
+end
