9300bb5651ac35db654db69f4df84b092bb49150
Mike Lawrence <contact@michaeltlawrence.com>
2021-07-14 16:53:30 -0500

    Creates economic impact payment 3 calculator

---
+++ app/services/economic_impact_payment_three_calculator.rb
@@ -0,0 +1,11 @@
+class EconomicImpactPaymentThreeCalculator
+  PER_FILER_PAYMENT = 1400
+  PER_DEPENDENT_PAYMENT = 1400
+
+  def self.payment_due(filer_count:, dependent_count:)
+    filer_payment = PER_FILER_PAYMENT * filer_count
+    dependent_payment = PER_DEPENDENT_PAYMENT * dependent_count
+
+    filer_payment + dependent_payment
+  end
+end
