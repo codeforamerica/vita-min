1e929c1f33429a0eb38adf8263d0dad9e84a338b
Catie Talbot <ctalbot@codeforamerica.org>
2021-07-15 10:57:39 -0700

    Clean up controller and add a spec
    Co-authored-by: Molly T-M <mollyt@codeforamerica.org>

--- app/controllers/ctc/questions/home_controller.rb
+++ app/controllers/ctc/questions/home_controller.rb
@@ -4,27 +4,9 @@ module Ctc
       # TODO: Transition to Authenticated once we log in client
       include AnonymousIntakeConcern
 
-      def edit
-        @form = form_class.new
-      end
-
-      def update
-        @form = form_class.new(form_params)
-        render :edit and return unless @form.valid?
-
-        send_mixpanel_event(event_name: "answered_question", data: form_attributes, subject: "home")
-        redirect_to next_path
-      end
-
       private
 
       def illustration_path; end
-
-      def form_attributes
-        return {} unless @form.class.scoped_attributes.key?(:home)
-
-        @form.attributes_for(:home).except(*Rails.application.config.filter_parameters)
-      end
 
       def next_path
         @form.lived_in_territory_or_at_foreign_address? ? questions_use_gyr_path : super
--- app/forms/ctc/home_form.rb
+++ app/forms/ctc/home_form.rb
@@ -1,10 +1,8 @@
 module Ctc
   class HomeForm < QuestionsForm
-    set_attributes_for :home, :lived_in_fifty_states, :lived_at_military_facility, :lived_in_us_territory, :lived_at_foreign_address
+    set_attributes_for :intake, :lived_in_fifty_states, :lived_at_military_facility, :lived_in_us_territory, :lived_at_foreign_address
 
-    def initialize(form_params = {})
-      super(nil, form_params)
-    end
+    def save; end
 
     def lived_in_territory_or_at_foreign_address?
       [lived_in_us_territory, lived_at_foreign_address].any?("yes")
