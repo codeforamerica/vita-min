0c6e245448a99de1310c6cafb96238bccdec6ffe
Catie Talbot <ctalbot@codeforamerica.org>
2021-07-16 09:45:33 -0700

    Update e-file manifest builder
    Co-authored-by: Asheesh Laroia <alaroia@codeforamerica.org>

--- app/lib/submission_builder/manifest.rb
+++ app/lib/submission_builder/manifest.rb
@@ -1,17 +1,23 @@
 module SubmissionBuilder
   class Manifest < SubmissionBuilder::Base
+    include SubmissionBuilder::FormattingMethods
+
     @schema_file = File.join(Rails.root, "vendor", "irs", "unpacked", "2020v5.1", "Common", "efileAttachments.xsd")
     @root_node = "IRSSubmissionManifest"
 
     def document
       intake = @submission.intake
+      tax_return = @submission.tax_return
 
       Nokogiri::XML::Builder.new(encoding: 'UTF-8') do |xml|
         xml['efile'].IRSSubmissionManifest(root_node_attrs) {
           xml.SubmissionId @submission.irs_submission_id
           xml.EFIN EnvironmentCredentials.dig(:irs, :efin)
+          xml.TaxYr tax_return.year
           xml.GovernmentCd "IRS"
           xml.FederalSubmissionTypeCd "1040"
+          xml.TaxPeriodBeginDt date_type(Date.new(tax_return.year, 1, 1))
+          xml.TaxPeriodEndDt date_type(Date.new(tax_return.year, 12, 31))
           xml.TIN intake.primary_ssn
         }
       end.doc
