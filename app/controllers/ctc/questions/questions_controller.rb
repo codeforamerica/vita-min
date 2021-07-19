7ea586be4bc399852b539b473b476f68e73542ca
Travis Grathwell <tgrathwell@codeforamerica.org>
2021-07-18 23:15:30 -0700

    QuestionsController.to_path_helper uses url_for instead of path_helper_string
    Previously, path_helper_string was used to compute the appropriate
    url helper from a controller class (something like questions_ip_pin_path in CTC
    or irs_letter_questions_path in GYR)
    
    url_for can be used for this purpose and only requires a controller name
    and action (which in our form navigation framework is always :edit)

--- app/controllers/ctc/questions/questions_controller.rb
+++ app/controllers/ctc/questions/questions_controller.rb
@@ -21,14 +21,6 @@ module Ctc
         def form_key
           "ctc/" + controller_name + "_form"
         end
-
-        def path_helper_string
-          [
-            "questions",
-            controller_name,
-            "path"
-          ].join("_")
-        end
       end
     end
   end
