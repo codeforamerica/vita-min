5373aa192b1716d8a1d5599f40b4ed7548f84635
Catie Talbot <ctalbot@codeforamerica.org>
2021-07-20 12:30:46 -0700

    Message client when efile submission is accepted
    Co-authored-by: Asheesh Laroia <alaroia@codeforamerica.org>

---
+++ app/models/automated_message/efile_acceptance.rb
@@ -0,0 +1,20 @@
+module AutomatedMessage
+  class EfileAcceptance
+
+    def self.name
+      'messages.efile.acceptance'.freeze
+    end
+
+    def sms_body(*args)
+      I18n.t("messages.efile.acceptance.sms", *args)
+    end
+
+    def email_subject(*args)
+      I18n.t("messages.efile.acceptance.email.subject", *args)
+    end
+
+    def email_body(*args)
+      I18n.t("messages.efile.acceptance.email.body", *args)
+    end
+  end
+end
