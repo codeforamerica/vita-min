611b6499f9d81188b6c951edeeb15589476432ee
Travis Grathwell <tgrathwell@codeforamerica.org>
2021-07-23 14:46:32 -0700

    CTC intake flow: Make /confirm-dependents match the mock
    Add a /dependents/:id/temp-toggles path temporarily
    to allow toggling any of the properties that might make
    dependents hidden or shown on the review page
    
    [#178819620]
    
    Co-authored-by: Asheesh Laroia <alaroia@codeforamerica.org>

---
+++ app/forms/ctc/dependents/temp_toggles_form.rb
@@ -0,0 +1,27 @@
+module Ctc
+  module Dependents
+    class TempTogglesForm < DependentForm
+      ATTRIBUTES = [
+        :full_time_student,
+        :permanently_totally_disabled,
+        :provided_over_half_own_support,
+        :no_ssn_atin,
+        :filed_joint_return,
+        :lived_with_more_than_six_months,
+        :born_in_2020,
+        :passed_away_2020,
+        :placed_for_adoption,
+        :permanent_residence_with_client,
+        :can_be_claimed_by_other,
+        :claim_regardless,
+        :meets_misc_qualifying_relative_requirements,
+      ]
+      set_attributes_for(:dependent, *ATTRIBUTES)
+
+      def save
+        @dependent.assign_attributes(attributes_for(:dependent))
+        @dependent.save
+      end
+    end
+  end
+end
