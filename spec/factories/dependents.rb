f27423e8cb392cd56b60d8f879d22868bfe25477
Ben Golder <bgolder@codeforamerica.org>
2021-07-23 15:28:02 -0700

    resolving more test failures from merging
    Co-authored-by: Jenny Heath <jheath@codeforamerica.org>

--- spec/factories/dependents.rb
+++ spec/factories/dependents.rb
@@ -84,7 +84,7 @@ FactoryBot.define do
       provided_over_half_own_support { "no" }
       no_ssn_atin { "no" }
       filed_joint_return { "no" }
-      lived_with_less_than_six_months { "yes" }
+      lived_with_more_than_six_months { "no" }
       can_be_claimed_by_other { "yes" }
       claim_regardless { "yes" }
     end
