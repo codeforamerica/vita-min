fdb80d06ac153f288a638f9197f0baa9af952e8a
Travis Grathwell <tgrathwell@codeforamerica.org>
2021-07-26 00:21:06 -0700

    Fully I18n-ize the CTC feature spec
    incidentally fixes a bug with some links in error messages that
    weren't getting rendered in the current locale
    
    Co-authored-by: Asheesh Laroia <alaroia@codeforamerica.org>

--- app/forms/ctc/direct_deposit_form.rb
+++ app/forms/ctc/direct_deposit_form.rb
@@ -4,7 +4,7 @@ module Ctc
     set_attributes_for :confirmation, :my_bank_account
 
     validates_presence_of :account_type, :bank_name
-    validates :my_bank_account, acceptance: { accept: "yes", message: -> (_object, _data) { I18n.t("views.ctc.questions.direct_deposit.my_bank_account.error_message_html", link: Rails.application.routes.url_helpers.questions_refund_payment_path) } }
+    validates :my_bank_account, acceptance: { accept: "yes", message: -> (_object, _data) { I18n.t("views.ctc.questions.direct_deposit.my_bank_account.error_message_html", link: Ctc::Questions::RefundPaymentController.to_path_helper) } }
 
     def save
       @intake.update(bank_account_attributes: attributes_for(:bank_account))
--- spec/helpers/rrc_helper_spec.rb
+++ spec/helpers/rrc_helper_spec.rb
@@ -6,6 +6,16 @@ describe RrcHelper do
       expect(helper.calculated_or_provided_dollar_amount(1200)).to eq "$1,200"
     end
 
+    context "with Spanish language requested" do
+      around do |example|
+        I18n.with_locale(:es) { example.run }
+      end
+
+      it "return the US English formatted currency anyway" do
+        expect(helper.calculated_or_provided_dollar_amount(1200)).to eq "$1,200"
+      end
+    end
+
     it "returns a $TBD if it is not given a value" do
       expect(helper.calculated_or_provided_dollar_amount(nil)).to eq "$TBD"
     end
