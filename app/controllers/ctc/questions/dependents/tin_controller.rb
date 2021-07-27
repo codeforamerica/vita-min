a50f8276132c2e3c88735e90a601cf478b9a42e7
Travis Grathwell <tgrathwell@codeforamerica.org>
2021-07-27 15:26:23 -0700

    \n

--- app/controllers/ctc/questions/dependents/tin_controller.rb
+++ app/controllers/ctc/questions/dependents/tin_controller.rb
@@ -8,6 +8,7 @@ module Ctc
 
         def self.show?(dependent)
           return false unless dependent.intake.had_dependents_yes?
+
           dependent.qualifying_child_2020? || dependent.qualifying_relative_2020?
         end
 
