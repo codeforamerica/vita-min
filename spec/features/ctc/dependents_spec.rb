98252d0b0c211dd5e199192f23c1d7b07cddbedd
tgrathwell <30675659+tgrathwell@users.noreply.github.com>
2021-07-27 16:00:41 -0700

    Add two cases to dependents feature specs (#1354)

--- spec/features/ctc/dependents_spec.rb
+++ spec/features/ctc/dependents_spec.rb
@@ -5,8 +5,8 @@ def fill_in_dependent_info(dependent_birth_year)
   fill_in I18n.t('views.ctc.questions.dependents.info.first_name'), with: "Jessie"
   fill_in I18n.t('views.ctc.questions.dependents.info.middle_initial'), with: "M"
   fill_in I18n.t('views.ctc.questions.dependents.info.last_name'), with: "Pepper"
-  fill_in "ctc_dependents_info_form[birth_date_month]", with: "01"
-  fill_in "ctc_dependents_info_form[birth_date_day]", with: "11"
+  fill_in "ctc_dependents_info_form[birth_date_month]", with: "11"
+  fill_in "ctc_dependents_info_form[birth_date_day]", with: "01"
   fill_in "ctc_dependents_info_form[birth_date_year]", with: dependent_birth_year
 end
 
@@ -47,7 +47,7 @@ RSpec.feature "Dependents in CTC intake", :flow_explorer_screenshot, active_job:
       expect(page).to have_selector("h1", text: I18n.t('views.ctc.questions.dependents.confirm_dependents.title'))
       within "[data-automation='ctc-dependents']" do
         expect(page).to have_content("Jessie Pepper")
-        expect(page).to have_selector("div", text: "#{I18n.t('general.date_of_birth')}: 1/11/#{dependent_birth_year}")
+        expect(page).to have_selector("div", text: "#{I18n.t('general.date_of_birth')}: 11/1/#{dependent_birth_year}")
       end
     end
 
@@ -77,7 +77,7 @@ RSpec.feature "Dependents in CTC intake", :flow_explorer_screenshot, active_job:
       expect(page).to have_selector("h1", text: I18n.t('views.ctc.questions.dependents.confirm_dependents.title'))
       within "[data-automation='other-credits-dependents']" do
         expect(page).to have_content("Jessie Pepper")
-        expect(page).to have_selector("div", text: "#{I18n.t('general.date_of_birth')}: 1/11/#{dependent_birth_year}")
+        expect(page).to have_selector("div", text: "#{I18n.t('general.date_of_birth')}: 11/1/#{dependent_birth_year}")
       end
     end
 
@@ -100,7 +100,7 @@ RSpec.feature "Dependents in CTC intake", :flow_explorer_screenshot, active_job:
       expect(page).to have_selector("h1", text: I18n.t('views.ctc.questions.dependents.confirm_dependents.title'))
       within "[data-automation='other-credits-dependents']" do
         expect(page).to have_content("Jessie Pepper")
-        expect(page).to have_selector("div", text: "#{I18n.t('general.date_of_birth')}: 1/11/#{dependent_birth_year}")
+        expect(page).to have_selector("div", text: "#{I18n.t('general.date_of_birth')}: 11/1/#{dependent_birth_year}")
       end
     end
 
@@ -122,7 +122,70 @@ RSpec.feature "Dependents in CTC intake", :flow_explorer_screenshot, active_job:
       expect(page).to have_selector("h1", text: I18n.t('views.ctc.questions.dependents.confirm_dependents.title'))
       within "[data-automation='other-credits-dependents']" do
         expect(page).to have_content("Jessie Pepper")
-        expect(page).to have_selector("div", text: "#{I18n.t('general.date_of_birth')}: 1/11/#{dependent_birth_year}")
+        expect(page).to have_selector("div", text: "#{I18n.t('general.date_of_birth')}: 11/1/#{dependent_birth_year}")
+      end
+    end
+
+    scenario "an infant born in late 2020" do
+      fill_in_dependent_info(2020)
+      select I18n.t('general.dependent_relationships.00_daughter'), from: I18n.t('views.ctc.questions.dependents.info.relationship_to_you')
+      click_on I18n.t('general.continue')
+
+      expect(page).to have_selector("h1", text: I18n.t('views.ctc.questions.dependents.child_disqualifiers.title', name: 'Jessie'))
+      check I18n.t('general.none')
+      click_on I18n.t('general.continue')
+
+      expect(page).to have_selector("h1", text: I18n.t('views.ctc.questions.dependents.child_lived_with_you.title', name: 'Jessie', tax_year: '2020'))
+      click_on I18n.t('general.negative')
+
+      expect(page).to have_selector("h1", text: I18n.t('views.ctc.questions.dependents.child_residence_exceptions.title', name: 'Jessie'))
+      check I18n.t('views.ctc.questions.dependents.child_residence_exceptions.born_in_2020', name: "Jessie")
+      click_on I18n.t('general.continue')
+
+      expect(page).to have_selector("h1", text: I18n.t('views.ctc.questions.dependents.child_can_be_claimed_by_other.title', name: 'Jessie'))
+      click_on I18n.t('general.affirmative')
+
+      expect(page).to have_selector("h1", text: I18n.t('views.ctc.questions.dependents.tin.title', name: 'Jessie'))
+      select "Social Security Number (SSN)"
+      fill_in I18n.t('views.ctc.questions.dependents.tin.ssn_or_atin', name: "Jessie"), with: "222-33-4445"
+      fill_in I18n.t('views.ctc.questions.dependents.tin.ssn_or_atin_confirmation', name: "Jessie"), with: "222-33-4445"
+      click_on I18n.t('general.continue')
+
+      expect(page).to have_selector("h1", text: I18n.t('views.ctc.questions.dependents.confirm_dependents.title'))
+      within "[data-automation='ctc-dependents']" do
+        expect(page).to have_content("Jessie Pepper")
+        expect(page).to have_selector("div", text: "#{I18n.t('general.date_of_birth')}: 11/1/2020")
+      end
+    end
+
+    scenario "a child who could be claimed by another but we are claiming them" do
+      fill_in_dependent_info(2020)
+      select I18n.t('general.dependent_relationships.00_daughter'), from: I18n.t('views.ctc.questions.dependents.info.relationship_to_you')
+      click_on I18n.t('general.continue')
+
+      expect(page).to have_selector("h1", text: I18n.t('views.ctc.questions.dependents.child_disqualifiers.title', name: 'Jessie'))
+      check I18n.t('general.none')
+      click_on I18n.t('general.continue')
+
+      expect(page).to have_selector("h1", text: I18n.t('views.ctc.questions.dependents.child_lived_with_you.title', name: 'Jessie', tax_year: '2020'))
+      click_on I18n.t('general.affirmative')
+
+      expect(page).to have_selector("h1", text: I18n.t('views.ctc.questions.dependents.child_can_be_claimed_by_other.title', name: 'Jessie'))
+      click_on I18n.t('general.negative')
+
+      expect(page).to have_selector("h1", text: I18n.t('views.ctc.questions.dependents.claim_child_anyway.title', name: 'Jessie'))
+      click_on I18n.t('general.affirmative')
+
+      expect(page).to have_selector("h1", text: I18n.t('views.ctc.questions.dependents.tin.title', name: 'Jessie'))
+      select "Social Security Number (SSN)"
+      fill_in I18n.t('views.ctc.questions.dependents.tin.ssn_or_atin', name: "Jessie"), with: "222-33-4445"
+      fill_in I18n.t('views.ctc.questions.dependents.tin.ssn_or_atin_confirmation', name: "Jessie"), with: "222-33-4445"
+      click_on I18n.t('general.continue')
+
+      expect(page).to have_selector("h1", text: I18n.t('views.ctc.questions.dependents.confirm_dependents.title'))
+      within "[data-automation='ctc-dependents']" do
+        expect(page).to have_content("Jessie Pepper")
+        expect(page).to have_selector("div", text: "#{I18n.t('general.date_of_birth')}: 11/1/2020")
       end
     end
   end
