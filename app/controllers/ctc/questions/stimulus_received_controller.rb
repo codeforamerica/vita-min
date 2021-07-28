ba1446c9dc691d8728f98d16a982173a3e4d95b8
Shannon Byrne <sbyrne@codeforamerica.org>
2021-07-28 15:59:12 -0500

    Check for only 0 value (no negative possible) on guard for stimulus received

--- app/controllers/ctc/questions/stimulus_received_controller.rb
+++ app/controllers/ctc/questions/stimulus_received_controller.rb
@@ -8,12 +8,13 @@ module Ctc
       def self.show?(intake)
         return false if intake.eip1_amount_received.nil? || intake.eip2_amount_received.nil?
 
-        intake.tax_return(2020).outstanding_recovery_rebate_credit <= 0
+        intake.tax_return(2020).outstanding_recovery_rebate_credit.zero?
       end
 
       def edit
-        @first_stimulus_amount = current_intake.tax_return(2020).expected_recovery_rebate_credit_one
-        @second_stimulus_amount = current_intake.tax_return(2020).expected_recovery_rebate_credit_two
+        tax_return = current_intake.tax_return(2020)
+        @first_stimulus_amount = tax_return.expected_recovery_rebate_credit_one
+        @second_stimulus_amount = tax_return.expected_recovery_rebate_credit_two
         super
       end
 
