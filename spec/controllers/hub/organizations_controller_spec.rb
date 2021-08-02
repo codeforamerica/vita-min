5020447d0667b591d84b351c6f5aa49bd3c63846
Travis Grathwell <tgrathwell@codeforamerica.org>
2021-08-02 14:55:40 -0700

    Try more 'match_array' to deflake this sort sensitive test
    Co-authored-by: Asheesh Laroia <alaroia@codeforamerica.org>

--- spec/controllers/hub/organizations_controller_spec.rb
+++ spec/controllers/hub/organizations_controller_spec.rb
@@ -101,7 +101,7 @@ RSpec.describe Hub::OrganizationsController, type: :controller do
         it "shows my coalition and child organizations but no link to add or edit orgs" do
           get :index
           expect(response).to be_ok
-          expect(assigns(:organizations_by_coalition)).to match_array [[coalition, [organization, second_organization]]]
+          expect(assigns(:organizations_by_coalition)).to match([[coalition, match_array([organization, second_organization])]])
           expect(response.body).to include hub_organization_path(id: organization)
           expect(response.body).not_to include new_hub_organization_path
           expect(response.body).not_to include edit_hub_organization_path(id: organization)
