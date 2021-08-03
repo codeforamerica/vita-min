8c92872dfda752e27166a1e02066cbf410f81244
Em Barnard-Shao <ebarnard@codeforamerica.org>
2021-08-03 16:55:58 -0400

    Create file-full-return page in CTC intake flow

---
+++ app/controllers/ctc/questions/file_full_return_controller.rb
@@ -0,0 +1,18 @@
+module Ctc
+  module Questions
+    class FileFullReturnController < QuestionsController
+
+      layout "intake"
+
+      private
+
+      def form_class
+        NullForm
+      end
+
+      def illustration_path
+        "hand-holding-check.svg"
+      end
+    end
+  end
+end
