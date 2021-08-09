81fe31f1dbb1f45eadaa2138a587381b4e91a082
Shannon Byrne <s.byrne2254@gmail.com>
2021-08-09 16:54:17 -0500

    Do not expose "investigating" status in client portal. (#1423)

--- app/controllers/ctc/portal/portal_controller.rb
+++ app/controllers/ctc/portal/portal_controller.rb
@@ -5,8 +5,9 @@ class Ctc::Portal::PortalController < ApplicationController
   def home
     if current_client.efile_submissions.any?
       submission = current_client.efile_submissions.last
-      @status = submission.current_state
-      @exposed_error = submission.last_transition ? submission.last_transition.client_facing_errors.first : nil
+      latest_transition = submission.last_client_accessible_transition
+      @status = latest_transition.present? ? latest_transition.to_state : EfileSubmissionStateMachine.initial_state
+      @exposed_error = latest_transition.present? ? latest_transition.client_facing_errors.first : nil
       @current_step = nil
     else
       @status = "intake_in_progress"
