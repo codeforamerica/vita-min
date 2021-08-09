97673cd7d09349d30255f9e5a1d0674a5bed9b89
Travis Grathwell <tgrathwell@codeforamerica.org>
2021-08-09 16:50:03 -0700

    CTC emails: use a different API key when sending for getctc.org
    In Mailgun, API keys are bound to domains, so our existing one will
    always show a `mailed-by` of `mg.getyourrefund.org`. This works as
    long as the `From:` is set to `getyourrefund.org`, but if it is set
    to another domain like getctc it triggers Google's scary spam warning.
    
    Added a new domain `mg.getctc.org` to Mailgun, which is stored in
    the credentials as mailgun.ctc_api_key / mailgun.ctc_domain. Mailers
    that need to send as `getctc.org` can specify this alternate api key
    by providing `delivery_method_options` to `mail`.
    
    Co-authored-by: Jenny Heath <jheath@codeforamerica.org>

--- app/mailers/verification_code_mailer.rb
+++ app/mailers/verification_code_mailer.rb
@@ -6,7 +6,7 @@ class VerificationCodeMailer < ApplicationMailer
     @locale = params[:locale]
     @subject = I18n.t("messages.default_subject_with_service_name", service_name: @service_name, locale: @locale)
     @verification_code = params[:verification_code]
-    mail(to: params[:to], subject: @subject, from: service.from_email)
+    mail(to: params[:to], subject: @subject, from: service.from_email, delivery_method_options: service.delivery_method_options)
   end
 
   def no_match_found(to:, locale:, service_type:)
@@ -16,6 +16,6 @@ class VerificationCodeMailer < ApplicationMailer
     @service_type = service.service_type
     @url = service.url(locale: locale)
     @subject = I18n.t("verification_code_mailer.no_match.subject", service_name: @service_name, url: service.url(locale: locale), locale: @locale)
-    mail(to: to, subject: @subject, from: service.from_email)
+    mail(to: to, subject: @subject, from: service.from_email, delivery_method_options: service.delivery_method_options)
   end
 end
