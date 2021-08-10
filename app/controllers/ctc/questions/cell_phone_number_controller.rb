b4139c6d6665a70d80ba04d4c4a23cf80714250a
Em Barnard-Shao <ebarnard@codeforamerica.org>
2021-08-10 14:19:02 -0400

    Send sms opt-in message when someone selects text message for preferred contact info

--- app/controllers/ctc/questions/cell_phone_number_controller.rb
+++ app/controllers/ctc/questions/cell_phone_number_controller.rb
@@ -5,6 +5,16 @@ module Ctc
 
       layout "intake"
 
+      def after_update_success
+        if current_intake.sms_notification_opt_in_yes?
+          ClientMessagingService.send_system_text_message(
+            client: current_intake.client,
+            body: I18n.t("messages.ctc_sms_opt_in", locale: current_intake.locale),
+            locale: current_intake.locale
+          )
+        end
+      end
+
       private
 
       def illustration_path
