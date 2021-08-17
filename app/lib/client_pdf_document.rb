834527cb84c4eba1f21674ceceb7916a6b319a73
Shannon Byrne <sbyrne@codeforamerica.org>
2021-08-17 13:54:39 -0500

    Replace 1040 pdfs instead of creating new ones

--- app/lib/client_pdf_document.rb
+++ app/lib/client_pdf_document.rb
@@ -1,5 +1,5 @@
 class ClientPdfDocument
-  def self.create_or_update(output_file:, document_type: , client:, filename:)
+  def self.create_or_update(output_file:, document_type: , client:, filename:, tax_return: nil)
     tempfile = output_file
     tempfile.seek(0)
     document = client.documents.find_or_initialize_by(document_type: document_type.key)
@@ -12,6 +12,7 @@ class ClientPdfDocument
           content_type: "application/pdf",
           identify: false
       },
+      tax_return: tax_return
     )
     document
   end
