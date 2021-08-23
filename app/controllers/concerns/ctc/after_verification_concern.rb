befbc3b9e4715c9b71aac92ea18b925d062d678c
Shannon Byrne <s.byrne2254@gmail.com>
2021-08-23 15:20:23 -0500

    Advance to status in intake instead of direct update (#1527)
    Co-authored-by: Catie Talbot <ctalbot@codeforamerica.org>

--- app/controllers/concerns/ctc/after_verification_concern.rb
+++ app/controllers/concerns/ctc/after_verification_concern.rb
@@ -7,7 +7,7 @@ module Ctc
     def after_verification_actions
       send_mixpanel_event(event_name: "ctc_contact_verified")
       sign_in current_intake.client
-      current_intake.tax_returns.last.update(status: "intake_in_progress")
+      current_intake.tax_returns.last.advance_to("intake_in_progress")
 
       ClientMessagingService.send_system_message_to_all_opted_in_contact_methods(
         client: current_intake.client,
