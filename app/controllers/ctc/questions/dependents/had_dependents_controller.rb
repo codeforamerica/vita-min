19453f1c43915878c50e888e11066b3febc1569a
Shannon Byrne <sbyrne@codeforamerica.org>
2021-08-24 15:26:35 -0500

    When answering yes/no with existing dependents, redirect to confirm page to move forward
    Co-authored-by: Catie Talbot <ctalbot@codeforamerica.org>

--- app/controllers/ctc/questions/dependents/had_dependents_controller.rb
+++ app/controllers/ctc/questions/dependents/had_dependents_controller.rb
@@ -9,10 +9,12 @@ module Ctc
         private
 
         def next_path
-          if current_intake.had_dependents_yes?
-            Ctc::Questions::Dependents::InfoController.to_path_helper(id: current_intake.new_dependent_token)
-          else
+          if current_intake.dependents.count > 0 # If the client has already added dependents, take them to the confirmation page to add more or continue.
+            questions_confirm_dependents_path
+          elsif current_intake.had_dependents_no?
             super
+          else
+            Ctc::Questions::Dependents::InfoController.to_path_helper(id: current_intake.new_dependent_token)
           end
         end
 
