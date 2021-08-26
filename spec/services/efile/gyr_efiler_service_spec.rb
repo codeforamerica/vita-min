0dbe2db282075ead4cfc8d25084c0bbf88fd395d
Asheesh Laroia <alaroia@codeforamerica.org>
2021-08-25 23:55:37 -0700

    Stop removing Login Certificate line [#179291421]
    Co-authored-by: Asheesh Laroia <alaroia@codeforamerica.org>

--- spec/services/efile/gyr_efiler_service_spec.rb
+++ spec/services/efile/gyr_efiler_service_spec.rb
@@ -1,6 +1,12 @@
 require "rails_helper"
 
 RSpec.describe Efile::GyrEfilerService do
+  before do
+    # Skip preparing & downloading gyr-efiler since we mock `Process.spawn()` anyway.
+    allow(described_class).to receive(:ensure_config_dir_prepared)
+    allow(described_class).to receive(:ensure_gyr_efiler_downloaded).and_return("/tmp/hypothetical_classes.zip")
+  end
+
   describe ".run_efiler_command" do
     before do
       allow(EnvironmentCredentials).to receive(:dig).with(:irs, :app_sys_id).and_return "asystemidiguess"
@@ -8,7 +14,7 @@ RSpec.describe Efile::GyrEfilerService do
       allow(EnvironmentCredentials).to receive(:dig).with(:irs, :etin).and_return "electronictransmitteridentificationnarwhal"
     end
 
-    xcontext "success" do
+    context "success" do
       let(:zip_data) do
         buf = Zip::OutputStream.write_buffer do |zio|
           zio.put_next_entry("filename.txt")
@@ -37,11 +43,11 @@ RSpec.describe Efile::GyrEfilerService do
       end
     end
 
-    xcontext "command failure" do
+    context "command failure" do
       before do
         allow(Process).to receive(:spawn) do |_argv, chdir:, unsetenv_others:, in:|
           File.open("#{chdir}/output/log/audit_log.txt", 'wb') do |f|
-            f.write("Earlier line\nLogin Certificate: yikesdontsavethis\nLog output")
+            f.write("Earlier line\nLogin Certificate: blahBlahBlah\nLog output")
           end
 
           `false` # Run a command so that $? is set
@@ -52,10 +58,10 @@ RSpec.describe Efile::GyrEfilerService do
         allow(Process).to receive(:wait)
       end
 
-      it "raises an exception with the log output, with Login Certificate line removed" do
+      it "raises an exception with the log output" do
         expect {
           described_class.run_efiler_command
-        }.to raise_error(StandardError, "Earlier line\nLog output")
+        }.to raise_error(StandardError, "Earlier line\nLogin Certificate: blahBlahBlah\nLog output")
       end
     end
   end
