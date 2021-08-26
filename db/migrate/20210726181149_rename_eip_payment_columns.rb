024b77f8042244aa6a674006a7f1b0e89636f7f8
Shannon Byrne <sbyrne@codeforamerica.org>
2021-08-26 11:34:16 -0500

    Fix migration file inconsistencies

--- db/migrate/20210726181149_rename_eip_payment_columns.rb
+++ db/migrate/20210726181149_rename_eip_payment_columns.rb
@@ -2,6 +2,5 @@ class RenameEipPaymentColumns < ActiveRecord::Migration[6.0]
   def change
     rename_column :intakes, :recovery_rebate_credit_amount_1, :eip1_amount_received
     rename_column :intakes, :recovery_rebate_credit_amount_2, :eip2_amount_received
-    rename_column :intakes, :recovery_rebate_credit_amount_confidence, :eip1_and_2_amount_received_confidence
   end
 end
