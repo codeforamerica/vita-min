e6cd6f4eea1a30847568fdd28fd4b59840a77ec7
Asheesh Laroia <alaroia@codeforamerica.org>
2021-09-09 13:15:54 -0400

    Use CTC domain when sending emails from getctc.org [179186159]
    Co-authored-by: Asheesh Laroia <alaroia@codeforamerica.org>

--- app/mailers/ctc_signup_mailer.rb
+++ app/mailers/ctc_signup_mailer.rb
@@ -4,20 +4,24 @@ class CtcSignupMailer < ApplicationMailer
   def beta_navigation(email_address:, name:)
     @name = name
     @subject = "GetCTC Child Tax Credit Assistance now available"
-    @service_type = :ctc
+    ctc_service = MultiTenantService.new(:ctc)
     mail(
       to: email_address,
       subject: @subject,
+      from: ctc_service.default_email,
+      delivery_method_options: ctc_service.delivery_method_options,
     )
   end
 
   def launch_announcement(email_address:, name:)
     @name = name
     subject = "Thank you for signing up to receive updates regarding GetCTC! / ¡Gracias por registrarse para recibir las actualizaciones de GetCTC!"
-    @service_type = :ctc
+    ctc_service = MultiTenantService.new(:ctc)
     mail(
       to: email_address,
       subject: subject,
+      from: ctc_service.default_email,
+      delivery_method_options: ctc_service.delivery_method_options,
     )
   end
 end
