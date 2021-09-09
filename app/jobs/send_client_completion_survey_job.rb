d84d177f132403b65b79a01d54afca3c74e0afea
Travis Grathwell <tgrathwell@codeforamerica.org>
2021-09-09 11:20:25 -0700

    Use a SurveySender class to DRY up similar survey sending code
    because we're about to do it a third time for the CTC survey
    
    Co-authored-by: Em Barnard-Shao <ebarnard@codeforamerica.org>

--- app/jobs/send_client_completion_survey_job.rb
+++ app/jobs/send_client_completion_survey_job.rb
@@ -1,36 +1,9 @@
 class SendClientCompletionSurveyJob < ApplicationJob
   def perform(client)
-    best_contact_method = ClientMessagingService.contact_methods(client).keys.first
-    return if best_contact_method.blank?
-
-    # Avoid sending duplicate emails; use lock since there are multiple job workers
-    client.with_lock do
-      return if client.completion_survey_sent_at.present?
-
-      client.update!(completion_survey_sent_at: Time.current)
-    end
-
-    is_drop_off_client = client.tax_returns.pluck(:service_type).any? "drop_off"
-    survey_code = is_drop_off_client ? "SV_ebtml6MMfhf8Vsa" : "SV_exiL2bLJx8GvjGC"
-    survey_link = "https://codeforamerica.co1.qualtrics.com/jfe/form/#{survey_code}?ExternalDataReference=#{client.id}"
-
-    locale = client.intake.locale
-    message = AutomatedMessage::CompletionSurvey.new
-
-    case best_contact_method
-    when :email
-      ClientMessagingService.send_system_email(
-        client: client,
-        body: message.email_body(locale: locale, survey_link: survey_link),
-        subject: message.email_subject(locale: locale),
-        locale: locale
-      )
-    when :sms_phone_number
-      ClientMessagingService.send_system_text_message(
-        client: client,
-        body: message.sms_body(locale: locale, survey_link: survey_link),
-        locale: locale
-      )
-    end
+    SurveySender.send_survey(
+      client,
+      :completion_survey_sent_at,
+      AutomatedMessage::CompletionSurvey
+    )
   end
 end
--- app/models/automated_message/completion_survey.rb
+++ app/models/automated_message/completion_survey.rb
@@ -4,6 +4,12 @@ module AutomatedMessage
       'messages.surveys.completion'.freeze
     end
 
+    def self.survey_link(client)
+      is_drop_off_client = client.tax_returns.pluck(:service_type).any? "drop_off"
+      survey_code = is_drop_off_client ? "SV_ebtml6MMfhf8Vsa" : "SV_exiL2bLJx8GvjGC"
+      "https://codeforamerica.co1.qualtrics.com/jfe/form/#{survey_code}?ExternalDataReference=#{client.id}"
+    end
+
     def sms_body(*args)
       I18n.t("messages.surveys.completion.sms", *args)
     end
