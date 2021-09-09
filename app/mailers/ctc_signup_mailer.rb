9a9fa8b5d9efaa86096fef09dea00953c31f048b
Asheesh Laroia <alaroia@codeforamerica.org>
2021-09-09 13:33:42 -0400

    Delete old unused method; use noreply from address
    Co-authored-by: Asheesh Laroia <alaroia@codeforamerica.org>

--- app/mailers/ctc_signup_mailer.rb
+++ app/mailers/ctc_signup_mailer.rb
@@ -1,18 +1,4 @@
 class CtcSignupMailer < ApplicationMailer
-  default from: Rails.configuration.email_from[:noreply][:ctc]
-
-  def beta_navigation(email_address:, name:)
-    @name = name
-    @subject = "GetCTC Child Tax Credit Assistance now available"
-    ctc_service = MultiTenantService.new(:ctc)
-    mail(
-      to: email_address,
-      subject: @subject,
-      from: ctc_service.default_email,
-      delivery_method_options: ctc_service.delivery_method_options,
-    )
-  end
-
   def launch_announcement(email_address:, name:)
     @name = name
     subject = "Thank you for signing up to receive updates regarding GetCTC! / ¡Gracias por registrarse para recibir las actualizaciones de GetCTC!"
@@ -20,7 +6,7 @@ class CtcSignupMailer < ApplicationMailer
     mail(
       to: email_address,
       subject: subject,
-      from: ctc_service.default_email,
+      from: ctc_service.noreply_email,
       delivery_method_options: ctc_service.delivery_method_options,
     )
   end
