2e281cae0c3b2b3c94a2cc4389c3eda63cec484a
Catie Talbot <ctalbot@codeforamerica.org>
2021-09-20 17:07:32 +0200

    Get rid of with_options in update CTC form validations

--- app/validators/social_security_number_validator.rb
+++ app/validators/social_security_number_validator.rb
@@ -1,6 +1,6 @@
 class SocialSecurityNumberValidator < ActiveModel::EachValidator
-  REAL_SSN_REGEX = /\A(?!(000|666|9))\d{3}-?(?!00)\d{2}-?(?!0000)\d{4}\Z/ # Real SSNS cannot have 00 in 4th and 5th position
-  LOOSE_SSN_REGEX = /\A(?!(000|666|9))\d{3}-?\d{2}-?(?!0000)\d{4}\Z/
+  REAL_SSN_REGEX = /\A(?!(000|666|9))\d{3}-?(?!00)\d{2}-?(?!0000)\d{4}\z/ # Real SSNS cannot have 00 in 4th and 5th position
+  LOOSE_SSN_REGEX = /\A(?!(000|666|9))\d{3}-?\d{2}-?(?!0000)\d{4}\z/
 
   def validate_each(record, attr_name, value)
     regex = Rails.env.production? ? REAL_SSN_REGEX : LOOSE_SSN_REGEX
