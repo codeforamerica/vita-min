7667ad2cb45fed32a1e8a878967bb5f6aba02b25
Catie Talbot <ctalbot@codeforamerica.org>
2021-09-20 17:21:03 +0200

    Updates to Spouse SSN validations on update CTC client (#1651)
    * Get rid of with_options in update CTC form validations

--- app/validators/individual_taxpayer_identification_number_validator.rb
+++ app/validators/individual_taxpayer_identification_number_validator.rb
@@ -1,8 +1,8 @@
 class IndividualTaxpayerIdentificationNumberValidator < ActiveModel::EachValidator
-  ITIN_REGEX = /\A(?=(9))\d{3}-?([7]\d|8[0-8]|9([0-2]|[4-9]))-?\d{4}\Z/
+  ITIN_REGEX = /\A(?=(9))\d{3}-?([7]\d|8[0-8]|9([0-2]|[4-9]))-?\d{4}\z/
 
   def validate_each(record, attr_name, value)
-    unless value =~ ITIN_REGEX
+    unless ITIN_REGEX.match?(value)
       record.errors[attr_name] << I18n.t("validators.itin")
     end
   end
