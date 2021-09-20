9e7d93c15becd870945037579901f66248eade18
Catie Talbot <ctalbot@codeforamerica.org>
2021-09-20 17:15:41 +0200

    Small fix

--- app/validators/individual_taxpayer_identification_number_validator.rb
+++ app/validators/individual_taxpayer_identification_number_validator.rb
@@ -2,7 +2,7 @@ class IndividualTaxpayerIdentificationNumberValidator < ActiveModel::EachValidat
   ITIN_REGEX = /\A(?=(9))\d{3}-?([7]\d|8[0-8]|9([0-2]|[4-9]))-?\d{4}\z/
 
   def validate_each(record, attr_name, value)
-    unless regex.match?(value)
+    unless ITIN_REGEX.match?(value)
       record.errors[attr_name] << I18n.t("validators.itin")
     end
   end
