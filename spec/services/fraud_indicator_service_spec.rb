41f8c16c255aa165ccbaf02c480eb73119ad4252
Shannon Byrne <sbyrne@codeforamerica.org>
2021-09-24 16:08:32 -0500

    Revert premature change to fraud indicator service
    Co-authored-by: Maria Quadri <mquadri@codeforamerica.org>

--- spec/services/fraud_indicator_service_spec.rb
+++ spec/services/fraud_indicator_service_spec.rb
@@ -23,7 +23,7 @@ describe FraudIndicatorService do
         end
 
         it "the response array does not include recaptcha_score" do
-          expect(FraudIndicatorService.new(client).hold_indicators).to eq ['recaptcha_score']
+          expect(FraudIndicatorService.new(client).hold_indicators).to eq []
         end
       end
 
