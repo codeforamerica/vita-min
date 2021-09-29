e3c23f1502d857b04a41044b0a9a28c91137a6d5
Shannon Byrne <sbyrne@codeforamerica.org>
2021-09-29 09:40:18 -0500

    Do not flag ctc intakes just because they are completed

--- spec/models/intake_spec.rb
+++ spec/models/intake_spec.rb
@@ -952,21 +952,6 @@ describe Intake do
     end
   end
 
-  describe "after_save when the intake is completed" do
-    let(:intake) { create :intake }
-
-    it_behaves_like "an incoming interaction" do
-      let(:subject) { create :intake }
-      before { subject.completed_at = Time.now }
-    end
-  end
-
-  describe "after_save when the intake has already been completed" do
-    it_behaves_like "an internal interaction" do
-      let(:subject) { create :intake, completed_at: Time.now }
-    end
-  end
-
   describe "#update_or_create_13614c_document" do
     before do
       example_pdf = Tempfile.new("example.pdf")
