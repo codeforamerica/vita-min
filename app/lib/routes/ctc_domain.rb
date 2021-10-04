4787a337a3893c70c07431cde6d7b426bdca2f97
Shannon Byrne <s.byrne2254@gmail.com>
2021-10-04 16:35:23 -0700

    Revert "Revert "Add Heroku Review App configuration (#1611)" (#1693)"
    This reverts commit c4e32832f8f03dfe3afbd24c9f78818c9187ea6a.

--- app/lib/routes/ctc_domain.rb
+++ app/lib/routes/ctc_domain.rb
@@ -1,7 +1,9 @@
 module Routes
   class CtcDomain
     def matches?(request)
-      Rails.application.config.ctc_domains.values.include?(request.host)
+      Rails.application.config.ctc_domains.values.any? do |matcher|
+        matcher.is_a?(Regexp) ? matcher.match?(request.host) : matcher == request.host
+      end
     end
   end
 end
