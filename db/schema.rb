d2c36b1a1939491bc062dcbb9a65cc7147d887ed
Shannon Byrne <sbyrne@codeforamerica.org>
2021-10-08 14:15:44 -0700

    Not ready email updates

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,8 @@
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
-ActiveRecord::Schema.define(version: 2021_10_04_201212) do
+
+ActiveRecord::Schema.define(version: 2021_10_04_222000) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
