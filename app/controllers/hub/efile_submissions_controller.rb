0b27d5f83a0f0ea79aa28d66dc35b04af596369b
Travis Grathwell <tgrathwell@codeforamerica.org>
2021-10-18 18:46:55 -0700

    Try to speed up the EfileSubmission dashboard with some eager loading
    Make one query that counts all the states rather than a single query for every state
    this speeds up the amount of time to render the header by ~2x/5x but it's a bit variable.
    
    Eager loads tax_returns, client, intake, and efile_submission_transitions (to speed up current_state)
    
    Weirdly if you .includes(tax_return: :client) it speeds up submission.tax_return.client but not
    the delegated submission.client :'(

--- app/controllers/hub/efile_submissions_controller.rb
+++ app/controllers/hub/efile_submissions_controller.rb
@@ -9,7 +9,8 @@ module Hub
     layout "admin"
 
     def index
-      @efile_submissions = EfileSubmission.most_recent_by_tax_return.page(params[:page])
+      @efile_submission_state_counts = count_efile_submission_states
+      @efile_submissions = EfileSubmission.includes(:efile_submission_transitions, tax_return: [:client, :intake]).most_recent_by_tax_return.page(params[:page])
       @efile_submissions = @efile_submissions.in_state(params[:status]) if params[:status].present?
     end
 
@@ -59,6 +60,23 @@ module Hub
         user_agent: request.user_agent,
       )
       redirect_to transient_storage_url(@efile_submission.submission_bundle.blob, disposition: "attachment")
+    end
+
+    private
+
+    def count_efile_submission_states
+      result = {}
+      EfileSubmissionStateMachine.states.each { |state| result[state] = 0 }
+      ActiveRecord::Base.connection.execute(<<~SQL).each { |row| result[row['to_state']] = row['count'] }
+        SELECT to_state, COUNT(*) FROM "efile_submissions"
+        LEFT OUTER JOIN efile_submission_transitions AS most_recent_efile_submission_transition ON (
+          efile_submissions.id = most_recent_efile_submission_transition.efile_submission_id AND 
+          most_recent_efile_submission_transition.most_recent = TRUE
+        )
+        WHERE most_recent_efile_submission_transition.to_state IS NOT NULL
+        GROUP BY to_state
+      SQL
+      result
     end
   end
 end
