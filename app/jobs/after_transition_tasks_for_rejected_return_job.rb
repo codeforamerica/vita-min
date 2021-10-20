3f8f27572406fb6cece506000686cc636608627e
Catie Talbot <ctalbot@codeforamerica.org>
2021-10-20 11:15:51 -0700

    Jobs for running after transition tasks on rejected returns

---
+++ app/jobs/after_transition_tasks_for_rejected_return_job.rb
@@ -0,0 +1,31 @@
+# delete this later maybe - it should never happen again, inshallah
+
+class AfterTransitionTasksForRejectedReturnJob < ApplicationJob
+  def perform(submission)
+    transition = submission.last_transition
+
+    # copy-pasta the whole after transition block
+    submission.tax_return.update(status: "file_rejected")
+
+    Efile::SubmissionErrorParser.persist_errors(transition)
+
+    if transition.efile_errors.any?
+      ClientMessagingService.send_system_message_to_all_opted_in_contact_methods(
+        client: submission.client,
+        message: AutomatedMessage::EfileRejected,
+        locale: submission.client.intake.locale
+      )
+      submission.transition_to!(:cancelled) if transition.efile_errors.any?(&:auto_cancel)
+      submission.transition_to!(:waiting) if transition.efile_errors.all?(&:auto_wait)
+      if transition.efile_errors.all? { |efile_error| EfileError.error_codes_to_retry_once.include?(efile_error.code) }
+        already_auto_resubmitted = submission.previously_transmitted_submission && submission.previously_transmitted_submission.efile_submission_transitions.where(to_state: :resubmitted
+        ).any? { |transition| transition.metadata.dig("auto_resubmitted") }
+        unless already_auto_resubmitted
+          submission.transition_to!(:resubmitted, {auto_resubmitted: true})
+        end
+      end
+    end
+
+    EfileSubmissionStateMachine.send_mixpanel_event(submission, "ctc_efile_return_rejected")
+  end
+end
---
+++ app/jobs/run_after_transition_tasks_on_rejected_returns_job.rb
@@ -0,0 +1,16 @@
+# delete this later maybe - it should never happen again, inshallah
+
+class RunAfterTransitionTasksOnRejectedReturnsJob < ApplicationJob
+  def perform(limit)
+    # get rejected submissions where tax return status doesn't match
+    rejected_submissions = EfileSubmission
+      .in_state(:rejected)
+      .includes(:tax_return).where(tax_returns: {status: "file_efiled"})
+
+    rejected_submissions = rejected_submissions.limit(limit) if limit.present?
+
+    rejected_submissions.find_each(batch_size: 100) do |submission|
+      AfterTransitionTasksForRejectedReturnJob.perform(submission)
+    end
+  end
+end
