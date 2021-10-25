d7995c45d63f204e6ea33a9ca5d838908c268e7b
Asheesh Laroia <alaroia@codeforamerica.org>
2021-10-25 14:01:46 -0400

    cleanup
    Co-authored-by: Asheesh Laroia <alaroia@codeforamerica.org>

--- app/helpers/phone_number_helper.rb
+++ app/helpers/phone_number_helper.rb
@@ -2,6 +2,5 @@ module PhoneNumberHelper
   def local_phone_number(phone_number)
     phony_normalized = Phony.normalize(phone_number, cc: '1')
     Phony.format(phony_normalized, format: :national)
-    # Phony.parse(phone_number, "US").local_number
   end
 end
