b3d8728784b89ac019b14cb82c956b48dcb8431f
Shannon Byrne <sbyrne@codeforamerica.org>
2021-10-27 11:42:03 -0500

    With imported image

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,4 +1,4 @@
 // Support component names relative to this directory:
-var componentRequireContext = require.context("components", true);
+var componentRequireContext = require.context("react", true);
 var ReactRailsUJS = require("react_ujs");
 ReactRailsUJS.useContext(componentRequireContext);
