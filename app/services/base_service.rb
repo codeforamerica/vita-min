28693615de0f1383e0645c22b4f8199caa008eb8
Catie Talbot <ctalbot@codeforamerica.org>
2021-11-01 14:57:16 -0700

    Fix base service spec

--- app/services/base_service.rb
+++ app/services/base_service.rb
@@ -1,6 +1,8 @@
 class BaseService
   def self.ensure_transaction
-    raise StandardError, "Service requiring transaction was called without a transaction open" unless ActiveRecord::Base.connection.open_transactions.positive?
+    # require 2 open transactions in test, otherwise 1. this is due to the default rspec transaction.
+    required_open_transactions = Rails.env.test? ? ActiveRecord::Base.connection.open_transactions > 1 : ActiveRecord::Base.connection.open_transactions.positive?
+    raise StandardError, "Service requiring transaction was called without a transaction open" unless required_open_transactions
 
     yield
   end
--- spec/services/base_service_spec.rb
+++ spec/services/base_service_spec.rb
@@ -1,3 +1,5 @@
+require 'rails_helper'
+
 describe BaseService do
   describe '.ensure_transaction' do
     context "with a caller inside of a transaction" do
