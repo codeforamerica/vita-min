599be2a702b8655c2f97772da1e3f0ace4e2c2d6
Asheesh Laroia <alaroia@codeforamerica.org>
2021-11-04 10:50:45 -0400

    Select the right element for binding click
    Co-authored-by: Asheesh Laroia <alaroia@codeforamerica.org>

--- app/javascript/hub/MainMenuComponent.js
+++ app/javascript/hub/MainMenuComponent.js
@@ -1,6 +1,6 @@
 export default function MainMenuComponent() {
     document.querySelectorAll('[data-component="MainMenuComponent"]').forEach((element) => {
-        element.addEventListener("click", () => {
+        element.querySelector(".toggle").addEventListener("click", () => {
             const classes = element.classList;
             classes.toggle("expanded")
             classes.toggle("collapsed")
