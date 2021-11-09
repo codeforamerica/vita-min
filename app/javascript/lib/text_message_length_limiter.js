f5d5beaf082ac0f4b75553ad7a9a83e7d6a4e1df
Asheesh Laroia <alaroia@codeforamerica.org>
2021-11-09 17:51:49 -0500

    Fix styling on text message character limiter [#180196444] (#1794)
    Co-authored-by: Asheesh Laroia <alaroia@codeforamerica.org>

--- app/javascript/lib/text_message_length_limiter.js
+++ app/javascript/lib/text_message_length_limiter.js
@@ -1,21 +1,25 @@
 export function limitTextMessageLength() {
     const textMessageInput = document.querySelector("textarea.text-message-body");
-    const lengthSelector = document.querySelector("#text-message-length-counter");
+    const component = document.querySelector('.text-message-length-limiter');
     const textMessageFormButton = textMessageInput.form.querySelector("input[type='submit']");
 
-    setLengthProperties(textMessageInput.value.length, lengthSelector, textMessageFormButton)
+    setLengthProperties(textMessageInput.value.length, component, textMessageFormButton);
     textMessageInput.addEventListener('input', function() {
-        setLengthProperties(this.value.length, lengthSelector, textMessageFormButton)
+        setLengthProperties(textMessageInput.value.length, component, textMessageFormButton)
     });
 }
 
-function setLengthProperties(length, lengthSelector, textMessageFormButton) {
-    lengthSelector.innerHTML = `${length} / 1600`;
+function setLengthProperties(length, component, textMessageFormButton) {
+    const lengthElement = component.querySelector('[data-target="length-counter"]');
+    const errorMessageElement = component.querySelector('[data-target="error-message"]');
+    lengthElement.textContent = length;
     if (length >= 1600) {
-        lengthSelector.classList.add('text--error');
+        component.classList.add('text--error');
+        errorMessageElement.classList.remove("hidden");
         textMessageFormButton.disabled = true;
     } else {
-        lengthSelector.classList.remove('text--error');
+        component.classList.remove('text--error');
+        errorMessageElement.classList.add("hidden");
         textMessageFormButton.disabled = false;
     }
 }
