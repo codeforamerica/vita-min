02b116a7b2df0b69f3b2e5d0faa05bcc5058a385
Catie Talbot <ctalbot@codeforamerica.org>
2021-11-15 11:35:35 -0800

    Annotations

--- spec/models/site_spec.rb
+++ spec/models/site_spec.rb
@@ -1,3 +1,32 @@
+# == Schema Information
+#
+# Table name: vita_partners
+#
+#  id                         :bigint           not null, primary key
+#  allows_greeters            :boolean
+#  archived                   :boolean          default(FALSE)
+#  capacity_limit             :integer
+#  logo_path                  :string
+#  name                       :string           not null
+#  national_overflow_location :boolean          default(FALSE)
+#  processes_ctc              :boolean          default(FALSE)
+#  timezone                   :string           default("America/New_York")
+#  type                       :string           not null
+#  created_at                 :datetime         not null
+#  updated_at                 :datetime         not null
+#  coalition_id               :bigint
+#  parent_organization_id     :bigint
+#
+# Indexes
+#
+#  index_vita_partners_on_coalition_id               (coalition_id)
+#  index_vita_partners_on_parent_name_and_coalition  (parent_organization_id,name,coalition_id) UNIQUE
+#  index_vita_partners_on_parent_organization_id     (parent_organization_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (coalition_id => coalitions.id)
+#
 require "rails_helper"
 
 describe Site do
