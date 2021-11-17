f64461cb89e4bb34e3ec3a57522240947afcfd43
Em Barnard-Shao <ebarnard@codeforamerica.org>
2021-11-17 10:55:39 -0600

    Remove expand/collapse
    Co-authored-by: Maria Quadri <mquadri@codeforamerica.org>

--- app/javascript/hub/MainMenuComponent.js
+++ app/javascript/hub/MainMenuComponent.js
@@ -1,9 +1,2 @@
 export default function MainMenuComponent() {
-    document.querySelectorAll('[data-component="MainMenuComponent"]').forEach((element) => {
-        element.querySelector(".toggle").addEventListener("click", () => {
-            const classes = element.classList;
-            classes.toggle("expanded")
-            classes.toggle("collapsed")
-        });
-    });
 }
