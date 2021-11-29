cdb54bfee16e84d956a47b0f644bf194067db8c1
Catie Talbot <ctalbot@codeforamerica.org>
2021-11-29 14:04:43 -0800

    Small styling changes and always highlight the page you are on
    Co-authored-by: Maria Quadri <mquadri@codeforamerica.org>

--- app/javascript/hub/MainMenuComponent.js
+++ app/javascript/hub/MainMenuComponent.js
@@ -1,2 +1,8 @@
 export default function MainMenuComponent() {
+    const mainMenu = document.querySelector('[data-component="MainMenuComponent"]')
+    mainMenu.querySelectorAll('.menu-item').forEach((item) => {
+        if (item.href === window.location.href) {
+            item.classList.toggle('selected');
+        }
+    });
 }
