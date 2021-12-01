e16eca610401a0898bf83889cf2d5c73b2ab3f02
Catie Talbot <ctalbot@codeforamerica.org>
2021-11-30 18:20:44 -0800

    Toggle open and closed
    Co-authored-by: Maria Quadri <mquadri@codeforamerica.org>

--- app/javascript/hub/MainMenuComponent.js
+++ app/javascript/hub/MainMenuComponent.js
@@ -1,8 +1,21 @@
 export default function MainMenuComponent() {
-    const mainMenu = document.querySelector('[data-component="MainMenuComponent"]')
+    const mainMenu = document.querySelector('[data-component="MainMenuComponent"]');
+    highlightSelectedPageNavigation(mainMenu);
+    setSidebarExpandCollapse(mainMenu);
+}
+
+function highlightSelectedPageNavigation(mainMenu) {
     mainMenu.querySelectorAll('.menu-item').forEach((item) => {
         if (item.href === window.location.href) {
             item.classList.toggle('selected');
         }
+    });
+}
+
+function setSidebarExpandCollapse(mainMenu) {
+    mainMenu.querySelector('.toggle').addEventListener("click", () => {
+        const classes = mainMenu.classList;
+        classes.toggle("expanded");
+        classes.toggle("collapsed");
     });
 }
