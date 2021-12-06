01575a1e65f2c34bb87d4ac9679b22636a5ba470
Catie Talbot <ctalbot@codeforamerica.org>
2021-12-06 12:42:12 -0800

    Add a space back in the migration

--- db/migrate/20211206193923_create_state_routing_target.rb
+++ db/migrate/20211206193923_create_state_routing_target.rb
@@ -2,7 +2,7 @@ class CreateStateRoutingTarget < ActiveRecord::Migration[6.1]
   def change
     create_table :state_routing_targets do |t|
       t.string :state_abbreviation, null: false
-      t.references :target,null: false, polymorphic: true, index: true
+      t.references :target, null: false, polymorphic: true, index: true
       t.timestamps
     end
   end
