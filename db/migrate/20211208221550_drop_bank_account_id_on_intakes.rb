84245a1f9eeb7201a1386aaf16588b5b64ba0b33
Travis Grathwell <tgrathwell@codeforamerica.org>
2021-12-09 15:44:06 -0800

    Fix crash when running migrations because the migration code tried to index a since-deleted column
    Fix the migration that deletes bank_account_id to allow for multiple states
    depending on what migrations people have already run

--- db/migrate/20211208221550_drop_bank_account_id_on_intakes.rb
+++ db/migrate/20211208221550_drop_bank_account_id_on_intakes.rb
@@ -1,5 +1,10 @@
 class DropBankAccountIdOnIntakes < ActiveRecord::Migration[6.1]
   def change
-    remove_column :intakes, :bank_account_id
+    if column_exists?(:intakes, :bank_account_id)
+      remove_column :intakes, :bank_account_id
+    end
+    if table_exists?(:archived_intakes_2021) && column_exists?(:archived_intakes_2021, :bank_account_id)
+      remove_column :archived_intakes_2021, :bank_account_id
+    end
   end
 end
