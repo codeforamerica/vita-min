ae391ea32ee0d58ce9b736c11d172641e3ba553b
Shannon Byrne <sbyrne@codeforamerica.org>
2021-12-09 11:54:40 -0600

    Provide standard deduction for 2021

--- app/lib/standard_deduction.rb
+++ app/lib/standard_deduction.rb
@@ -5,7 +5,22 @@ class StandardDeduction
     @tax_year = tax_year
   end
 
-  def standard_deduction_2020(filing_status: nil)
+  def standard_deduction(filing_status)
+    return unless filing_status
+
+    send("standard_deduction_#{tax_year}", filing_status)
+  rescue NoMethodError
+    raise NotImplementedError, "Standard deduction rule not implemented for #{tax_year}"
+  end
+
+  def self.for(tax_year:, filing_status:)
+    obj = new(tax_year: tax_year)
+    obj.standard_deduction(filing_status)
+  end
+
+  private
+  
+  def standard_deduction_2020(filing_status)
     case filing_status.to_sym
     when :married_filing_jointly, :qualifying_widow
       24800
@@ -16,15 +31,14 @@ class StandardDeduction
     end
   end
 
-  def standard_deduction(filing_status)
-    return unless filing_status
-    send("standard_deduction_#{tax_year}", filing_status: filing_status)
-  rescue NoMethodError
-    raise NotImplementedError, "Standard deduction rule not implemented for #{tax_year}"
-  end
-
-  def self.for(tax_year:, filing_status:)
-    obj = new(tax_year: tax_year)
-    obj.standard_deduction(filing_status)
+  def standard_deduction_2021(filing_status)
+    case filing_status.to_sym
+    when :married_filing_jointly, :qualifying_widow
+      25100
+    when :head_of_household
+      18800
+    when :single, :married_filing_separately
+      12550
+    end
   end
 end
