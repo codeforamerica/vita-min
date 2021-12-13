90c31d5f7d05650f9b5185c79537ed72d0f9f145
Catie Talbot <ctalbot@codeforamerica.org>
2021-12-13 14:10:52 -0800

    More spec fixing

--- spec/features/hub/toggle_client_ssn_info_spec.rb
+++ spec/features/hub/toggle_client_ssn_info_spec.rb
@@ -28,47 +28,46 @@ RSpec.feature "Toggle ssn display" do
       visit hub_client_path(id: client)
 
       within(".primary-ssn") do
-        expect(page).to have_text "Last 4 of SSN/ITIN"
+        expect(page).to have_text "SSN/ITIN"
         expect(page).to have_text "View"
 
-        expect(page).not_to have_text client.intake.primary_last_four_ssn
+        expect(page).not_to have_text client.intake.primary_ssn
 
         expect do
           click_on "View"
 
-          expect(page).to have_text "Last 4 of SSN/ITIN"
+          expect(page).to have_text "SSN/ITIN"
           expect(page).to have_text "Hide"
 
-          expect(page).to have_text client.intake.primary_last_four_ssn
+          expect(page).to have_text client.intake.primary_ssn
 
         end.to change(AccessLog, :count).by(1)
 
         click_on "Hide"
 
-        expect(page).not_to have_text client.intake.primary_last_four_ssn
+        expect(page).not_to have_text client.intake.primary_ssn
       end
 
       within(".spouse-ssn") do
-        expect(page).to have_text "Last 4 of SSN/ITIN"
+        expect(page).to have_text "SSN/ITIN"
         expect(page).to have_text "View"
 
-        expect(page).not_to have_text client.intake.spouse_last_four_ssn
+        expect(page).not_to have_text client.intake.spouse_ssn
 
         expect do
           click_on "View"
 
-          expect(page).to have_text "Last 4 of SSN/ITIN"
+          expect(page).to have_text "SSN/ITIN"
           expect(page).to have_text "Hide"
 
-          expect(page).to have_text client.intake.spouse_last_four_ssn
+          expect(page).to have_text client.intake.spouse_ssn
 
         end.to change(AccessLog, :count).by(1)
 
         click_on "Hide"
 
-        expect(page).not_to have_text client.intake.spouse_last_four_ssn
+        expect(page).not_to have_text client.intake.spouse_ssn
       end
-
     end
   end
 end
