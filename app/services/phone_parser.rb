f8fb7267128eb42021a06b9aae821317c4402d49
Asheesh Laroia <alaroia@codeforamerica.org>
2021-12-22 11:34:21 -0500

    Simplify PhoneParser further
    Co-authored-by: Asheesh Laroia <alaroia@codeforamerica.org>

--- app/services/phone_parser.rb
+++ app/services/phone_parser.rb
@@ -1,22 +1,22 @@
 class PhoneParser
   def self.normalize(raw_phone_number)
-    valid, normalized = self.normalize_or_error(raw_phone_number)
-    valid ? Phony.format(normalized, format: :international, parentheses: false, spaces: '', local_spaces: '').to_s : raw_phone_number
+    valid, phony_normalized = self.phony_normalize_or_error(raw_phone_number)
+    valid ? self.e164(phony_normalized) : raw_phone_number
   end
 
   def self.formatted_phone_number(raw_phone_number)
-    valid, normalized = self.normalize_or_error(raw_phone_number)
-    valid ? Phony.format(normalized, format: :national) : raw_phone_number
+    valid, phony_normalized = self.phony_normalize_or_error(raw_phone_number)
+    valid ? Phony.format(phony_normalized, format: :national) : raw_phone_number
   end
 
   def self.phone_number_link(raw_phone_number)
-    valid, normalized = self.normalize_or_error(raw_phone_number)
-    valid ? "tel:+#{normalized}" : "tel:"
+    valid, phony_normalized = self.phony_normalize_or_error(raw_phone_number)
+    valid ? "tel:#{self.e164(phony_normalized)}" : "tel:"
   end
 
   private
 
-  def self.normalize_or_error(raw_phone_number)
+  def self.phony_normalize_or_error(raw_phone_number)
     return [false, nil] if raw_phone_number.nil?
     return [false, ""] if raw_phone_number == ""
 
@@ -26,5 +26,10 @@ class PhoneParser
     else
       [false, raw_phone_number]
     end
+  end
+
+  def self.e164(phony_normalized)
+    # Phony normalization results in a phone number with no punctuation and with a country calling code.
+    "+#{phony_normalized}"
   end
 end
