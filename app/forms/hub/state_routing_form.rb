3f57b8231344c8e6d029d78bdde4c299910dfe92
Catie Talbot <ctalbot@codeforamerica.org>
2022-01-05 13:25:48 -0800

    Begin to make the form do something

--- app/forms/hub/state_routing_form.rb
+++ app/forms/hub/state_routing_form.rb
@@ -11,6 +11,11 @@ module Hub
     end
 
     def save
+      state_routing_fraction_attributes.each do |_, v|
+        StateRoutingFraction.create(state_routing_target_id: v[:state_routing_target_id],
+                                    vita_partner_id: v[:vita_partner_id],
+                                    routing_fraction: routing_fraction_from_percentage(v[:routing_percentage]))
+      end
     end
 
     private
@@ -20,20 +25,24 @@ module Hub
     end
 
     def percentages_must_equal_100
-      # sum = 0
-      # vita_partner_states_attributes.each do |_, v|
-      #   sum += v[:routing_percentage].to_i
-      # end
-      # unless sum == 100
-      #   errors.add(:must_equal_100, I18n.t("forms.errors.state_routings.must_equal_100"))
-      # end
+      sum = 0
+      state_routing_fraction_attributes.each do |_, v|
+        sum += v[:routing_percentage].to_i
+      end
+      unless sum == 100
+        errors.add(:must_equal_100, I18n.t("forms.errors.state_routings.must_equal_100"))
+      end
     end
 
     def vita_partners_are_unique
-      # vps_ids = vita_partner_states_attributes.values.pluck(:vita_partner_id)
-      # unless vps_ids.uniq.length == vps_ids.length
-      #   errors.add(:duplicate_vita_partner, I18n.t("forms.errors.state_routings.duplicate_vita_partner"))
-      # end
+      vps_ids = state_routing_fraction_attributes.values.pluck(:vita_partner_id)
+      unless vps_ids.uniq.length == vps_ids.length
+        errors.add(:duplicate_vita_partner, I18n.t("forms.errors.state_routings.duplicate_vita_partner"))
+      end
+    end
+
+    def state_routing_fraction_attributes
+      @form_params.present? ? @form_params[:state_routing_fraction_attributes] : {}
     end
   end
 end
