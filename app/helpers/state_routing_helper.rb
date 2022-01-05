880febd3ad855dffb1704201dc85fa7a070ad389
Catie Talbot <ctalbot@codeforamerica.org>
2022-01-05 10:34:10 -0800

    Add routing percentage values to form

---
+++ app/helpers/state_routing_helper.rb
@@ -0,0 +1,6 @@
+module StateRoutingHelper
+  def current_routing_percentage(state_routing_target, vita_partner)
+    routing_fraction = state_routing_target.state_routing_fractions.find { |srf| srf.vita_partner = vita_partner }
+    routing_fraction ? routing_fraction.routing_percentage : 0
+  end
+end
