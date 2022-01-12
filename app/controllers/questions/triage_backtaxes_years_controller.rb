5865fb6ffb69c9df07ac8ce2227e1268c5d8979b
Travis Grathwell <tgrathwell@codeforamerica.org>
2022-01-12 12:05:47 -0800

    Only probably redirect to full service if have SSN/ITIN paperwork
    Co-authored-by: Asheesh Laroia <alaroia@codeforamerica.org>

--- app/controllers/questions/triage_backtaxes_years_controller.rb
+++ app/controllers/questions/triage_backtaxes_years_controller.rb
@@ -5,11 +5,19 @@ module Questions
     private
 
     def next_path
-      if [:backtaxes_2018, :backtaxes_2019, :backtaxes_2020].any? { |m| @form.triage.send(m) == "no" } && %w[all_copies some_copies].include?(@form.triage.doc_type)
+      if probably_full_service
         Questions::TriageIncomeTypesController.to_path_helper
       else
         super
       end
+    end
+
+    private
+
+    def probably_full_service
+      [:backtaxes_2018, :backtaxes_2019, :backtaxes_2020].any? { |m| @form.triage.send(m) == "no" } &&
+        %w[all_copies some_copies].include?(@form.triage.doc_type) &&
+        @form.triage.id_type == "have_paperwork"
     end
 
     def illustration_path; end
--- spec/controllers/questions/triage_backtaxes_years_controller_spec.rb
+++ spec/controllers/questions/triage_backtaxes_years_controller_spec.rb
@@ -4,8 +4,8 @@ RSpec.describe Questions::TriageBacktaxesYearsController do
   describe "#update" do
     context "with valid params" do
 
-      context "with triage answers that are within the filing limit and with at least some documents" do
-        let(:triage) { create(:triage, income_level: "hh_1_to_25100", doc_type: "some_copies") }
+      context "with triage answers that are within the filing limit and with at least some documents and with SSN/ITIN paperwork" do
+        let(:triage) { create(:triage, id_type: "have_paperwork", income_level: "hh_1_to_25100", doc_type: "some_copies") }
 
         before do
           session[:triage_id] = triage.id
@@ -33,6 +33,33 @@ RSpec.describe Questions::TriageBacktaxesYearsController do
 
       context "with triage answers that do not have any documents" do
         let(:triage) { create(:triage, income_level: "hh_1_to_25100", doc_type: "need_help") }
+
+        before do
+          session[:triage_id] = triage.id
+        end
+
+        context "when they need help with any non-2021 year" do
+          let(:params) do
+            {
+              triage_backtaxes_years_form: {
+                backtaxes_2018: "yes",
+                backtaxes_2019: "no",
+                backtaxes_2020: "no",
+                backtaxes_2021: "no",
+              }
+            }
+          end
+
+          it "redirects to the next page in the flow" do
+            post :update, params: params
+
+            expect(response).to redirect_to(Questions::TriageAssistanceController.to_path_helper)
+          end
+        end
+      end
+
+      context "with triage answers that do not have SSN/ITIN paperwork" do
+        let(:triage) { create(:triage, id_type: "know_number", income_level: "hh_1_to_25100", doc_type: "some_copies") }
 
         before do
           session[:triage_id] = triage.id
