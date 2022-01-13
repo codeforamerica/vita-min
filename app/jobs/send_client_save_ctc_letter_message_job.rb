7c216a41af1664e6d8095a873103b568fc79233e
Em Barnard-Shao <ebarnard@codeforamerica.org>
2022-01-13 13:13:19 -0500

    Add multitenantservice call

--- app/jobs/send_client_save_ctc_letter_message_job.rb
+++ app/jobs/send_client_save_ctc_letter_message_job.rb
@@ -2,7 +2,7 @@ class SendClientSaveCtcLetterMessageJob < ApplicationJob
   def perform(client)
     return if client.intake.email_notification_opt_in != "yes" && client.intake.sms_notification_opt_in != "yes"
 
-    service_name = client.intake.is_ctc? ? "GetCTC" : "GetYourRefund"
+    service_name = client.intake.is_ctc? ? MultiTenantService.new(:ctc).service_name : MultiTenantService.new(:gyr).service_name
 
     ClientMessagingService.send_system_message_to_all_opted_in_contact_methods(
       client: client,
