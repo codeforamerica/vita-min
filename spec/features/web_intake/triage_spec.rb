b9515dac4da40060ab540bdadfd909d5d43d81d9
Travis Grathwell <tgrathwell@codeforamerica.org>
2022-01-12 16:10:57 -0800

    fix it
    Co-authored-by: Asheesh Laroia <alaroia@codeforamerica.org>

--- spec/features/web_intake/triage_spec.rb
+++ spec/features/web_intake/triage_spec.rb
@@ -11,7 +11,7 @@ RSpec.feature "client is not eligible for VITA services", :flow_explorer_screens
     choose I18n.t('questions.triage_income_level.edit.levels.hh_over_73000')
     click_on I18n.t('general.continue')
 
-    expect(page).to have_selector("h1", text: I18n.t('views.public_pages.maybe_ineligible.title'))
+    expect(page).to have_selector("h1", text: I18n.t('questions.triage_do_not_qualify.edit.title'))
   end
 
   scenario "client is eligible for DIY but not full service" do
@@ -24,6 +24,6 @@ RSpec.feature "client is not eligible for VITA services", :flow_explorer_screens
     choose I18n.t('questions.triage_income_level.edit.levels.hh_66000_to_73000')
     click_on I18n.t('general.continue')
 
-    expect(page).to have_selector("h1", text: I18n.t('diy.file_yourself.edit.title'))
+    expect(page).to have_selector("h1", text: I18n.t('questions.triage_referral.edit.title'))
   end
 end
