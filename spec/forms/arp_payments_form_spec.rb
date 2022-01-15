125d6dca66929c6691198553cc7008d3399721bf
Shannon Byrne <sbyrne@codeforamerica.org>
2022-01-14 16:37:45 -0800

    Full form spec for new stimulus and ctc question behavior

--- spec/forms/arp_payments_form_spec.rb
+++ spec/forms/arp_payments_form_spec.rb
@@ -13,15 +13,19 @@ describe ArpPaymentsForm do
     })
   end
   let(:received_stimulus_payment) { "unfilled" }
-  let(:eip1_amount_received) { }
-  let(:eip2_amount_received) { }
-  let(:eip3_amount_received) { }
-  let(:advance_ctc_amount_received) { }
+  let(:eip1_amount_received) { 0 }
+  let(:eip2_amount_received) { 0 }
+  let(:eip3_amount_received) { 0 }
+  let(:advance_ctc_amount_received) { 0 }
   let(:received_advance_ctc_payment) { "unfilled" }
 
   describe "validations" do
     context "eip" do
       context "when eip amounts are all blank and not unsure" do
+        let(:received_stimulus_payment) { "unfilled" }
+        let(:eip1_amount_received) {  }
+        let(:eip2_amount_received) {  }
+        let(:eip3_amount_received) {  }
         it "adds an error" do
           form.valid?
           expect(form.errors).to include :received_stimulus_payment
@@ -39,6 +43,8 @@ describe ArpPaymentsForm do
 
     context "ctc" do
       context "when ctc amount is blank and not unsure" do
+        let(:advance_ctc_amount_received) {  }
+        let(:received_advance_ctc_payment) { "unfilled" }
         it "adds an error" do
           form.valid?
           expect(form.errors).to include :received_advance_ctc_payment
@@ -53,5 +59,67 @@ describe ArpPaymentsForm do
         end
       end
     end
+  end
+
+  describe "#save" do
+    context "when stimulus was received" do
+      let(:eip1_amount_received) { 100 }
+      it "sets received_stimulus_payment to yes" do
+        form.save
+        expect(intake.reload.received_stimulus_payment).to eq "yes"
+      end
+    end
+
+    context "when all stimulus payments are 0" do
+      let(:eip1_amount_received) { 0 }
+      let(:eip2_amount_received) { 0 }
+      let(:eip3_amount_received) { 0 }
+
+      it "sets received_stimulus_payment to yes" do
+        form.save
+        expect(intake.reload.received_stimulus_payment).to eq "no"
+      end
+    end
+
+    context "when stimulus payments are 0 but provided an unsure value" do
+      let(:eip1_amount_received) { 0 }
+      let(:eip2_amount_received) { 0 }
+      let(:eip3_amount_received) { 0 }
+      let(:received_stimulus_payment) { "unsure" }
+
+      it "leaves set to unsure" do
+        form.save
+        expect(intake.reload.received_stimulus_payment).to eq "unsure"
+      end
+    end
+
+    context "when advance ctc is set to 0" do
+      let(:advance_ctc_amount_received) { 0  }
+      let(:received_advance_ctc_payment) { "unfilled" }
+      it "sets received ctc to no" do
+        form.save
+        expect(intake.reload.received_advance_ctc_payment).to eq "no"
+      end
+    end
+
+    context "when advance ctc is set to a number other than 0" do
+      let(:advance_ctc_amount_received) { 10 }
+      let(:received_advance_ctc_payment) { "unfilled" }
+      it "sets received ctc to yes" do
+        form.save
+        expect(intake.reload.received_advance_ctc_payment).to eq "yes"
+      end
+    end
+
+    context "when advance ctc is set to a number other than 0 and unsure is checked" do
+      let(:advance_ctc_amount_received) { 10  }
+      let(:received_advance_ctc_payment) { "unsure" }
+
+      it "sets received ctc to unsure" do
+        form.save
+        expect(intake.reload.received_advance_ctc_payment).to eq "unsure"
+      end
+    end
+
   end
 end
