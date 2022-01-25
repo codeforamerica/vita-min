f75f06af1d7fd161d4624cf40bb901ae07342dd5
Travis Grathwell <tgrathwell@codeforamerica.org>
2022-01-24 16:16:38 -0800

    Fix crash for archived intakes with dependents
    the relationship between `Archived::Dependent2021` and their associated
    intake wasn't really set up right
    
    Co-authored-by: Em Barnard-Shao <ebarnard@codeforamerica.org>

--- spec/features/hub/show_client_spec.rb
+++ spec/features/hub/show_client_spec.rb
@@ -53,10 +53,12 @@ RSpec.describe "a user viewing a client" do
     context "for a client with an archived 2021 intake" do
       let(:intake) { nil }
       let!(:archived_intake) {  create(:archived_2021_gyr_intake, client: client) }
+      let!(:archived_dependent) {  create(:archived_2021_dependent, intake: archived_intake) }
 
       it "can view intake information" do
         visit hub_client_path(id: client.id)
         expect(page).to have_content(archived_intake.preferred_name)
+        expect(page).to have_content(archived_dependent.full_name)
       end
     end
   end
