2641ff27b9839d87dd14ef6cd1ac30f4dc6abaad
Travis Grathwell <tgrathwell@codeforamerica.org>
2022-01-28 10:43:30 -0800

    Hub: fix crashes in SecurityController for archived intake scenarios

--- app/controllers/hub/security_controller.rb
+++ app/controllers/hub/security_controller.rb
@@ -6,7 +6,7 @@ module Hub
     layout "hub"
 
     def show
-      @client = Client.find(params[:id])
+      @client = Hub::ClientsController::HubClientPresenter.new(Client.find(params[:id]))
       @duplicate_bank_client_ids = duplicate_bank_client_ids
       @security_events = (
         @client.efile_security_informations + @client.recaptcha_scores
@@ -16,7 +16,6 @@ module Hub
     private
 
     def duplicate_bank_client_ids
-      return [] unless @client.intake
       return [] unless @client.intake.bank_account.present?
       Intake.where(id: @client.intake.bank_account.duplicates.pluck(:intake_id)).pluck(:client_id)
     end
--- spec/controllers/hub/security_controller_spec.rb
+++ spec/controllers/hub/security_controller_spec.rb
@@ -2,15 +2,28 @@ require 'rails_helper'
 
 RSpec.describe Hub::SecurityController do
   context "when there is no current intake" do
-    let(:client) { create :client }
+    let(:client) { create :client, intake: intake }
+    let(:intake) { build(:intake, preferred_name: "Ivan Intake") }
+
+    render_views
 
     before do
       sign_in create(:admin_user)
     end
 
-    it "shows no duplicate bank client ids" do
+    it "shows security information" do
       get :show, params: { id: client.id }
-      expect(assigns(:duplicate_bank_client_ids)).to eq([])
+      expect(response.body).to include(intake.preferred_name)
+    end
+
+    context "for an archived intake" do
+      let(:intake) { nil }
+      let!(:archived_intake) { create :archived_2021_ctc_intake, client: client, preferred_name: "Andy Archive" }
+
+      it "shows security information" do
+        get :show, params: { id: client.id }
+        expect(response.body).to include(archived_intake.preferred_name)
+      end
     end
   end
 end
