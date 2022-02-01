9813bf894082d8df5b5e6050d3a7fbf5123b5418
Asheesh Laroia <alaroia@codeforamerica.org>
2022-02-01 10:43:11 -0500

    [#181102079] Ensure at-capacity clients aren't blocked by the duplicate guard
    Co-authored-by: Asheesh Laroia <alaroia@codeforamerica.org>

--- app/services/client_login_service.rb
+++ app/services/client_login_service.rb
@@ -12,10 +12,10 @@ class ClientLoginService
     # these might have multiple email addresses
     to_addresses = EmailAccessToken.lookup(raw_token).pluck(:email_address)
     emails = to_addresses.map { |to| to.split(",") }.flatten(1)
-    email_intake_matches = accessible_intakes.where(email_address: emails)
-    spouse_email_intake_matches = accessible_intakes.where(spouse_email_address: emails)
+    email_intake_matches = active_intake_accounts.where(email_address: emails)
+    spouse_email_intake_matches = active_intake_accounts.where(spouse_email_address: emails)
     phone_numbers = TextMessageAccessToken.lookup(raw_token).pluck(:sms_phone_number)
-    phone_intake_matches = accessible_intakes.where(sms_phone_number: phone_numbers)
+    phone_intake_matches = active_intake_accounts.where(sms_phone_number: phone_numbers)
     intake_matches = email_intake_matches.or(spouse_email_intake_matches).or(phone_intake_matches)
 
     # Client.by_raw_login_token supports login links generated from mid-Jan through early March 2021.
@@ -23,26 +23,26 @@ class ClientLoginService
   end
 
   def can_login_by_email_verification?(email_address, service_type: :gyr)
-    accessible_intakes.where(email_address: email_address).or(accessible_intakes.where(spouse_email_address: email_address)).exists?
+    active_intake_accounts.where(email_address: email_address).or(active_intake_accounts.where(spouse_email_address: email_address)).exists?
   end
 
   def can_login_by_sms_verification?(sms_phone_number, service_type: :gyr)
-    accessible_intakes.where(sms_phone_number: sms_phone_number, sms_notification_opt_in: "yes").exists?
+    active_intake_accounts.where(sms_phone_number: sms_phone_number, sms_notification_opt_in: "yes").exists?
+  end
+
+  def active_intake_accounts
+    service_type.to_sym == :gyr ? self.class.active_gyr_intake_accounts : self.class.active_ctc_intake_accounts
   end
 
   private
 
-  def accessible_intakes
-    service_type.to_sym == :gyr ? self.class.gyr_accessible_intakes : self.class.ctc_accessible_intakes
-  end
-
-  def self.gyr_accessible_intakes
+  def self.active_gyr_intake_accounts
     online_consented = Intake::GyrIntake.joins(:tax_returns).where({ tax_returns: { service_type: "online_intake" } }).where(primary_consented_to_service: "yes")
     drop_off = Intake::GyrIntake.joins(:tax_returns).where({ tax_returns: { service_type: "drop_off" } })
     online_consented.or(drop_off)
   end
 
-  def self.ctc_accessible_intakes
+  def self.active_ctc_intake_accounts
     sms_verified = Intake::CtcIntake.where.not(sms_phone_number_verified_at: nil)
     email_verified = Intake::CtcIntake.where.not(email_address_verified_at: nil)
     navigator_verified = Intake::CtcIntake.where.not(navigator_has_verified_client_identity: nil)
@@ -52,7 +52,7 @@ class ClientLoginService
 
   def self.has_ctc_duplicate?(intake)
     has_dupe = false
-    accessible_intakes = ctc_accessible_intakes.where.not(id: intake.id)
+    accessible_intakes = active_ctc_intake_accounts.where.not(id: intake.id)
     if intake.email_address.present?
       has_dupe = accessible_intakes.where(email_address: intake.email_address).exists?
     end
