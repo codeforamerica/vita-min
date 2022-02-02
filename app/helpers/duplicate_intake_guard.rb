4763cc4f4253d8f411377a5d1f40a08bb3fa4fb2
Em Barnard-Shao <ebarnard@codeforamerica.org>
2022-02-02 09:43:51 -0500

    WIP:  Add hash for primary_ssn on Intake

--- app/helpers/duplicate_intake_guard.rb
+++ app/helpers/duplicate_intake_guard.rb
@@ -1,17 +1,7 @@
 class DuplicateIntakeGuard < SimpleDelegator
   def has_duplicate?
-    ClientLoginService.new(:gyr).accessible_intakes
-      .where(
-         (arel_table[:email_address].eq(email_address)
-           .and(arel_table[:email_address].not_eq(nil)))
-         .or(arel_table[:phone_number].eq(phone_number)
-           .and(arel_table[:phone_number].not_eq(nil)))
-      ).exists?
-  end
-
-  private
-
-  def arel_table
-    Intake.arel_table
+    ClientLoginService.new(:gyr).accessible_intakes.each do |intake|
+      return true if DeduplificationService.duplicates(intake, :hashed_primary_ssn).exists?
+    end
   end
 end
--- spec/helpers/duplicate_intake_guard_spec.rb
+++ spec/helpers/duplicate_intake_guard_spec.rb
@@ -5,57 +5,57 @@ RSpec.describe DuplicateIntakeGuard do
     create(
       :intake,
       email_address: "existing@client.com",
-      primary_consented_to_service: "yes"
+      primary_consented_to_service: "yes",
+      phone_number: "+15005550006",
+      primary_ssn: "123456789",
+      client: build(:client, tax_returns: [build(:tax_return, service_type: "online_intake")])
     )
   end
 
   let(:subject) { DuplicateIntakeGuard.new(matching_intake) }
 
   describe "has_duplicate?" do
-    context "intake with matching email address exists" do
-      let!(:existing_intake) { create(:intake, email_address: "existing@client.com", client: build(:client, tax_returns: [build(:tax_return, service_type: "online_intake")])) }
-      let(:matching_intake) { create(:intake, email_address: "existing@client.com") }
+    context "intake with matching email address exists but different primary ssn" do
+      let!(:matching_intake) { create(:intake, email_address: "existing@client.com", primary_ssn: "923456789") }
 
-      it "returns false if the intake is not completed" do
+      it "returns false" do
+        expect(subject).not_to have_duplicate
+      end
+    end
+
+    context "intake with matching phone number exists but different primary ssn" do
+      let!(:matching_intake) { create(:intake, phone_number: "+15005550006", primary_ssn: "923456789") }
+
+      it "returns false if marching phone number but different intake" do
+        expect(subject).not_to have_duplicate
+      end
+    end
+
+    context "existing intake is missing primary ssn" do
+      let!(:existing_intake) { create(:intake, primary_consented_to_service: "yes", primary_ssn: nil) }
+      let(:matching_intake) { create(:intake, primary_ssn: nil) }
+
+      it "there is no match without primary ssn" do
+        expect(subject).not_to have_duplicate
+      end
+
+      it "there is no match with primary ssn" do
+        matching_intake.update(primary_ssn: "123456789")
+        expect(subject).not_to have_duplicate
+      end
+    end
+
+    context "intake with matching primary ssn exists" do
+      let!(:existing_intake) { create(:intake, primary_ssn: "123456789") }
+      let(:matching_intake) { create(:intake,  primary_ssn: "123456789") }
+
+      xit "returns false if the intake is not completed" do
         expect(subject).not_to have_duplicate
       end
 
       it "returns true if the primary filer has consented" do
         existing_intake.update(primary_consented_to_service: "yes")
         expect(subject).to have_duplicate
-      end
-    end
-
-    context "intake with matching phone number exists" do
-      let!(:existing_intake) { create(:intake, phone_number: "+15005550006", client: build(:client, tax_returns: [build(:tax_return, service_type: "online_intake")])) }
-      let(:matching_intake) { create(:intake, phone_number: "+15005550006") }
-
-      it "returns false if the intake is not completed" do
-        expect(subject).not_to have_duplicate
-      end
-
-      it "returns true if the primary filer has consented" do
-        existing_intake.update(primary_consented_to_service: "yes")
-        expect(subject).to have_duplicate
-      end
-    end
-
-    context "existing intake is missing email address and phone number" do
-      let!(:existing_intake) { create(:intake, primary_consented_to_service: "yes") }
-      let(:matching_intake) { create(:intake) }
-
-      it "there is no match without phone and email" do
-        expect(subject).not_to have_duplicate
-      end
-
-      it "there is no match with email address" do
-        matching_intake.update(email_address: "new@email.com")
-        expect(subject).not_to have_duplicate
-      end
-
-      it "there is no match with phone number" do
-        matching_intake.update(phone_number: "+15005550006")
-        expect(subject).not_to have_duplicate
       end
     end
   end
