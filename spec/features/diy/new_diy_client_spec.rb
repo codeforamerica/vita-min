cf9e095da27e6f7f1ee39c48884fffcd32f03184
Asheesh Laroia <alaroia@codeforamerica.org>
2022-02-01 20:10:23 -0500

    [#181077447] Update GYR.org/DIY copy (#2077)
    Change the HTML/CSS of the hero image on the /diy page to more closely
    resemble what's on the homepage. Previously it was trying to use `grid-flex`
    but that was causing the text to overlap the SVG unless you gave the <img>
    a specific width (or some other solution)
    
    the homepage has more eyes on it so they now use the same strategy
    so the two pages' destinies may be more likely to advance in tandem
    
    Co-authored-by: Asheesh Laroia <alaroia@codeforamerica.org>

--- spec/features/diy/new_diy_client_spec.rb
+++ spec/features/diy/new_diy_client_spec.rb
@@ -10,22 +10,21 @@ RSpec.feature "Web Intake New Client wants to file on their own" do
   scenario "a new client files through TaxSlayer" do
     allow(MixpanelService).to receive(:send_event)
     visit "/diy"
-    expect(page).to have_selector("h1", text: "File taxes on your own for free!")
-    expect(page).to have_selector("p", text: "We provide free access to an online tax prep site called TaxSlayer. We'll send you a link to create your own TaxSlayer login, so you can file on your own.")
-    click_on "Continue"
+    expect(page).to have_selector("h1", text: I18n.t('views.public_pages.diy_home.title'))
+    click_on I18n.t('general.continue')
 
-    expect(page).to have_selector("h1", text: "File your taxes yourself!")
-    click_on "Continue"
+    expect(page).to have_selector("h1", text: I18n.t('diy.file_yourself.edit.title'))
+    click_on I18n.t('general.continue')
 
-    expect(page).to have_selector("h1", text: "To access this site, please provide your e-mail address.")
-    fill_in "Email address", with: "example@example.com"
-    fill_in "Confirm email address", with: "example@example.com"
-    click_on "Continue"
+    expect(page).to have_selector("h1", text: I18n.t('diy.diy_intakes.new.title'))
+    fill_in I18n.t("views.questions.email_address.email_address"), with: "example@example.com"
+    fill_in I18n.t("views.questions.email_address.email_address_confirmation"), with: "example@example.com"
+    click_on I18n.t('general.continue')
 
-    expect(page).to have_selector("h1", text: "File taxes on your own!")
+    expect(page).to have_selector("h1", text: I18n.t('diy.tax_slayer.show.title'))
     # should have the button that links to tax slayer and tracks clicks in mixpanel
     expect(page).to have_selector(
                       "a.button[href=\"#{fake_taxslayer_link}\"][data-track-click=\"diy-cfa-taxslayer-link\"]",
-                      text: "Continue to TaxSlayer")
+                      text: I18n.t('diy.tax_slayer.show.continue_to_tax_slayer'))
   end
 end
