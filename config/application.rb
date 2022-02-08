c726f3432fb24dff9a163d9c6e38ad8e825b10e3
Travis Grathwell <tgrathwell@codeforamerica.org>
2022-02-08 11:16:29 -0800

    Use Rails 6.1 defaults instead of Rails 6.0 defaults
    so there's hopefully less steps to get to 7.x someday

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,7 @@ Bundler.require(*Rails.groups)
 
 module VitaMin
   class Application < Rails::Application
-    config.load_defaults 6.0
+    config.load_defaults 6.1
 
     config.i18n.default_locale = :en
     config.i18n.fallbacks = [I18n.default_locale]
