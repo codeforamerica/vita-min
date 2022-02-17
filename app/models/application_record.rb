47d1f66359c9435e97902c2f31e483ff863067ec
Asheesh Laroia <alaroia@codeforamerica.org>
2022-02-17 18:00:01 -0500

    Rename since it always does a query, to rhyme with ActiveRecord count

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -10,5 +10,5 @@ class ApplicationRecord < ActiveRecord::Base
   class_attribute :ignored_columns, default: [:__fake_column__]
 
   # Allow counting up to a max number; see https://alexcastano.com/the-hidden-cost-of-the-invisible-queries-in-rails/#how-far-do-you-plan-to-count
-  scope :size_greater_than?, ->(n) { limit(n + 1).count > n }
+  scope :count_greater_than?, ->(n) { limit(n + 1).count > n }
 end
