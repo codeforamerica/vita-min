75682c0e84907c80576c33b57d310fb7189328e4
Asheesh Laroia <alaroia@codeforamerica.org>
2022-02-17 17:25:13 -0500

    Add a limit to a count that was slow [#178761000]
    Co-authored-by: Asheesh Laroia <alaroia@codeforamerica.org>

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -8,4 +8,7 @@ class ApplicationRecord < ActiveRecord::Base
   # Migrate to enumerate_columns_in_select_statements when we switch to Rails 7
   # https://www.bigbinary.com/blog/rails-7-adds-setting-for-enumerating-columns-in-select-statements
   class_attribute :ignored_columns, default: [:__fake_column__]
+
+  # Allow counting up to a max number; see https://alexcastano.com/the-hidden-cost-of-the-invisible-queries-in-rails/#how-far-do-you-plan-to-count
+  scope :more_than, ->(n) { limit(n + 1).count > n }
 end
