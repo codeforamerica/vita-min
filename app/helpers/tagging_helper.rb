3b790617f180503949be9008c67f190ef971ab2d
Asheesh Laroia <alaroia@codeforamerica.org>
2022-02-18 09:40:41 -0500

    Preload :child_sites to optimize taggable_vita_partners [#178761000] (#2170)
    Co-authored-by: Asheesh Laroia <alaroia@codeforamerica.org>

--- app/helpers/tagging_helper.rb
+++ app/helpers/tagging_helper.rb
@@ -1,10 +1,11 @@
 module TaggingHelper
   def taggable_vita_partners(vita_partners)
     taggable_vita_partners = []
-    vita_partners.organizations.each do |vita_partner|
-      taggable_vita_partners << { id: vita_partner.id, name: vita_partner.name, value: vita_partner.id }
-      vita_partner.child_sites.each do |site|
-        taggable_vita_partners << { id: site.id, name: site.name, parentName: vita_partner.name, value: site.id }
+    # We query via the Organization model to trigger auto-loading of child_sites per Organization model's default scope.
+    Organization.where(id: vita_partners.organizations).each do |organization|
+      taggable_vita_partners << { id: organization.id, name: organization.name, value: organization.id }
+      organization.child_sites.each do |site|
+        taggable_vita_partners << { id: site.id, name: site.name, parentName: organization.name, value: site.id }
       end
     end
     taggable_vita_partners.to_json.to_s.html_safe
