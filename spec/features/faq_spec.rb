0808214048e0db107bb77812a98e776955789830
Travis Grathwell <tgrathwell@codeforamerica.org>
2022-03-08 12:03:47 -0800

    FAQ: style it
    Co-authored-by: Jenny Heath <jheath@codeforamerica.org>

--- spec/features/faq_spec.rb
+++ spec/features/faq_spec.rb
@@ -2,14 +2,14 @@ require "rails_helper"
 
 RSpec.feature "FAQ" do
   it "links to the most popular questions in each group" do
-    visit "/new-faq"
+    visit "/faq"
 
     click_on I18n.t('views.public_pages.faq.question_groups.stimulus.how_many_stimulus_payments_were_there.question')
     expect(strip_html_tags(page.body)).to include(strip_html_tags(I18n.t('views.public_pages.faq.question_groups.stimulus.how_many_stimulus_payments_were_there.answer_html')))
   end
 
   it "has a link within each section to show all questions in that section" do
-    visit "/new-faq"
+    visit "/faq"
 
     within '.faq-section-stimulus' do
       click_on "View all questions"
