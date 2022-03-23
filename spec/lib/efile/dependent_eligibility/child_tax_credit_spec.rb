b6c556b3a665dcb765f49dd4e6f959ba2e22ce54
Catie Talbot <ctalbot@codeforamerica.org>
2022-03-23 08:42:29 -0700

    Better specs

--- spec/lib/efile/dependent_eligibility/child_tax_credit_spec.rb
+++ spec/lib/efile/dependent_eligibility/child_tax_credit_spec.rb
@@ -1,9 +1,30 @@
 require "rails_helper"
 
 describe Efile::DependentEligibility::ChildTaxCredit do
-  context 'when condition' do
-    it 'succeeds' do
-      pending 'Not implemented'
+  context "when passing in an eligibility object" do
+    let(:child_eligibility) { double }
+    before do
+      allow(child_eligibility).to receive(:qualifies?).and_return true
+      allow(Efile::DependentEligibility::QualifyingChild).to receive(:new)
+    end
+
+    it "uses the passed in object instead of instantiating a new one" do
+      described_class.new((create :dependent), TaxReturn.current_tax_year, child_eligibility: child_eligibility)
+      expect(child_eligibility).to have_received(:qualifies?)
+    end
+  end
+
+  context "when not passing in an eligibility object" do
+    let(:dependent) { create :dependent }
+    let(:child_eligibility) { double }
+    before do
+      allow(child_eligibility).to receive(:qualifies?).and_return true
+    end
+
+    it "instantiates a new eligibility object" do
+      described_class.new(dependent, TaxReturn.current_tax_year)
+      expect(child_eligibility).not_to have_received(:qualifies?)
+
     end
   end
 end
