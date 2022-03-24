a8c8f7e9c8d274de8073e831941914813095ae03
Catie Talbot <ctalbot@codeforamerica.org>
2022-03-24 13:24:05 -0700

    Add StimulusThree page

--- app/controllers/ctc/questions/stimulus_three_controller.rb
+++ app/controllers/ctc/questions/stimulus_three_controller.rb
@@ -5,14 +5,6 @@ module Ctc
 
       layout "intake"
 
-      def edit
-        tax_return = current_intake.default_tax_return
-        benefits = Efile::BenefitsEligibility.new(tax_return: tax_return, dependents: current_intake.dependents)
-        @first_stimulus_amount = benefits.eip1_amount
-        @second_stimulus_amount = benefits.eip2_amount
-        super
-      end
-
       private
 
       def illustration_path
--- app/forms/ctc/stimulus_three_form.rb
+++ app/forms/ctc/stimulus_three_form.rb
@@ -1,11 +1,16 @@
 module Ctc
   class StimulusThreeForm < QuestionsForm
-    set_attributes_for :intake, :eip3_entry_method
+    set_attributes_for :intake, :eip3_amount_received
 
-    validates :eip3_entry_method, acceptance: { accept: 'did_not_receive' }
+    validates_presence_of :eip3_amount_received
+    validates :eip3_amount_received, gyr_numericality: { only_integer: true }, if: :not_blank?
 
     def save
-      @intake.update(attributes_for(:intake).merge(eip3_amount_received: 0))
+      @intake.update(attributes_for(:intake))
+    end
+
+    def not_blank?
+      attributes_for(:intake)[:eip3_amount_received].present?
     end
   end
 end
