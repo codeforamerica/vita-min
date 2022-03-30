3c85cb949d953f371f0319886c8e3aa371f8d174
Catie Talbot <ctalbot@codeforamerica.org>
2022-03-30 09:34:27 -0700

    Fix form specs

--- spec/forms/ctc/stimulus_payments_form_spec.rb
+++ spec/forms/ctc/stimulus_payments_form_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 describe Ctc::StimulusPaymentsForm do
-  let(:intake) { create :ctc_intake, client: client, eip1_amount_received: 0 }
+  let(:intake) { create :ctc_intake, client: client, eip3_amount_received: 123 }
   let(:client) { create :client, tax_returns: [build(:tax_return, year: 2021)] }
 
   let(:params) {
@@ -12,36 +12,39 @@ describe Ctc::StimulusPaymentsForm do
 
   describe '#save' do
     before do
-      allow_any_instance_of(Efile::BenefitsEligibility).to receive(:eip1_amount).and_return(2400)
-      allow_any_instance_of(Efile::BenefitsEligibility).to receive(:eip2_amount).and_return(1200)
+      allow_any_instance_of(Efile::BenefitsEligibility).to receive(:eip3_amount).and_return(2400)
     end
 
     context "when the client received the full amount" do
-      let(:eip_received_choice) { 'yes_received' }
+      let(:eip_received_choice) { 'this_amount' }
 
       it "persists the calculated eip values to the DB" do
         described_class.new(intake, params).save
 
         intake.reload
-        expect(intake.eip1_amount_received).to eq(2400)
-        expect(intake.eip2_amount_received).to eq(1200)
+        expect(intake.eip3_amount_received).to eq(2400)
       end
     end
 
     context "when the client received no amount" do
-      let(:eip_received_choice) { 'no_did_not_receive' }
-
-      before do
-        intake.update(eip1_amount_received: 123)
-        intake.update(eip1_amount_received: 456)
-      end
+      let(:eip_received_choice) { 'no_amount' }
 
       it "persists the calculated eip values to the DB" do
         described_class.new(intake, params).save
 
         intake.reload
-        expect(intake.eip1_amount_received).to be_nil
-        expect(intake.eip2_amount_received).to be_nil
+        expect(intake.eip3_amount_received).to eq(0)
+      end
+    end
+
+    context "when the client received a different amount" do
+      let(:eip_received_choice) { 'different_amount' }
+
+      it "persists the calculated eip values to the DB" do
+        described_class.new(intake, params).save
+
+        intake.reload
+        expect(intake.eip3_amount_received).to be_nil
       end
     end
   end
