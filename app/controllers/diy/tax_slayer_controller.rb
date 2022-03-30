de7824952ef84dc0b972d046661d37d22cb9cb70
jenny-heath <43800769+jenny-heath@users.noreply.github.com>
2022-03-30 12:27:34 -0700

    DIY flow: link to custom taxslayer URL for specific sets of sources (#2285)
    this is to facilitate an experiment where previous year filers
    will be invited to DIY, but we want GYR filers to have a different
    taxslayer URL than CTC filers
    
    Co-authored-by: Travis Grathwell <tgrathwell@codeforamerica.org>
    Co-authored-by: Jenny Heath <jheath@codeforamerica.org>

--- app/controllers/diy/tax_slayer_controller.rb
+++ app/controllers/diy/tax_slayer_controller.rb
@@ -2,7 +2,9 @@ module Diy
   class TaxSlayerController < ApplicationController
     before_action :redirect_in_offseason, :require_diy_intake
 
-    def show; end
+    def show
+      @taxslayer_link = taxslayer_link
+    end
 
     private
 
@@ -12,6 +14,18 @@ module Diy
 
     def require_diy_intake
       redirect_to diy_file_yourself_path unless session[:diy_intake_id].present?
+    end
+
+    def taxslayer_link
+      gyr_sources = %w[2022-taxes 2022_taxes]
+      ctc_sources = %w[taxes-2022 taxes_2022]
+      if gyr_sources.include?(source)
+        "https://www.taxslayer.com/v.aspx?rdr=/vitafsa&source=TSUSATY2021&sidn=34092122"
+      elsif ctc_sources.include?(source)
+        "https://www.taxslayer.com/v.aspx?rdr=/vitafsa&source=TSUSATY2021&sidn=31096682"
+      else
+        EnvironmentCredentials.dig(:tax_slayer_link)
+      end
     end
   end
 end
--- spec/controllers/diy/tax_slayer_controller_spec.rb
+++ spec/controllers/diy/tax_slayer_controller_spec.rb
@@ -2,11 +2,13 @@ require "rails_helper"
 
 RSpec.describe Diy::TaxSlayerController do
   describe "#show" do
-    context "with a diy intake id in the session" do
-      before do
-        session[:diy_intake_id] = 4
-      end
+    let(:diy_intake_id) { 4 }
 
+    before do
+      session[:diy_intake_id] = diy_intake_id
+    end
+
+    context "with a diy intake id in the session" do
       it "returns 200 OK" do
         get :show
 
@@ -15,10 +17,50 @@ RSpec.describe Diy::TaxSlayerController do
     end
 
     context "without a valid diy intake id in the session" do
+      let(:diy_intake_id) { nil }
+
       it "redirects to file yourself page" do
         get :show
 
         expect(response).to redirect_to diy_file_yourself_path
+      end
+    end
+
+    context "showing specific TS links to clients with specific source params" do
+      context "GYR cohort" do
+        before do
+          session[:source] = "2022-taxes"
+        end
+
+        it "shows the right TS link" do
+          get :show
+
+          expect(assigns(:taxslayer_link)).to eq "https://www.taxslayer.com/v.aspx?rdr=/vitafsa&source=TSUSATY2021&sidn=34092122"
+        end
+      end
+
+      context "GetCTC cohort" do
+        before do
+          session[:source] = "taxes-2022"
+        end
+
+        it "shows the right TS link" do
+          get :show
+
+          expect(assigns(:taxslayer_link)).to eq "https://www.taxslayer.com/v.aspx?rdr=/vitafsa&source=TSUSATY2021&sidn=31096682"
+        end
+      end
+
+      context "clients with no special source" do
+        before do
+          allow(EnvironmentCredentials).to receive(:dig).with(:tax_slayer_link).and_return "https://example.com/taxslayer"
+        end
+
+        it "shows the normal taxslayer link" do
+          get :show
+
+          expect(assigns(:taxslayer_link)).to eq "https://example.com/taxslayer"
+        end
       end
     end
   end
