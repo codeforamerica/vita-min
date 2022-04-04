1861a3ee44d59cafa21102eb584a7bb95afa8109
Catie Talbot <ctalbot@codeforamerica.org>
2022-04-04 13:27:08 -0700

    One more feature spec fix

--- spec/features/ctc/sign_out_spec.rb
+++ spec/features/ctc/sign_out_spec.rb
@@ -18,6 +18,7 @@ RSpec.feature "sign out during CTC Intake", active_job: true, efile_security_par
     click_on I18n.t('general.continue')
     click_on I18n.t('general.negative')
     click_on I18n.t("views.ctc.questions.file_full_return.simplified_btn")
+    click_on "Continue"
     click_on I18n.t('general.negative')
     choose I18n.t('views.ctc.questions.filed_prior_tax_year.did_not_file', prior_tax_year: prior_tax_year)
     click_on "Continue"
