30ae152670cadca18ad883e961029a55d9656035
Catie Talbot <ctalbot@codeforamerica.org>
2022-04-05 15:31:59 -0700

    Update home-page to use radio buttons (#2299)

--- app/forms/ctc/home_form.rb
+++ app/forms/ctc/home_form.rb
@@ -1,11 +1,11 @@
 module Ctc
   class HomeForm < QuestionsForm
-    set_attributes_for :misc, :lived_in_fifty_states, :lived_at_military_facility, :lived_in_us_territory, :lived_at_foreign_address
+    set_attributes_for :misc, :home_location
 
     def save; end
 
     def lived_in_territory_or_at_foreign_address?
-      [lived_in_us_territory, lived_at_foreign_address].any?("yes")
+      ["lived_in_us_territory", "lived_at_foreign_address"].include? home_location
     end
   end
 end
