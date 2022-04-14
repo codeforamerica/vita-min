4951c7cdaec74d2f35ef66d56a4e26846714fe6c
Jenny Heath <jheath@codeforamerica.org>
2022-04-14 15:40:31 -0700

    Use age_during_tax_year to check dependent age in schedule8812
    Co-authored-by: Whitney Schaefer <wschaefer@codeforamerica.org>

--- spec/lib/submission_builder/ty2021/documents/schedule8812_spec.rb
+++ spec/lib/submission_builder/ty2021/documents/schedule8812_spec.rb
@@ -3,9 +3,12 @@ require "rails_helper"
 describe SubmissionBuilder::Ty2021::Documents::Schedule8812 do
   let(:filing_status) { "married_filing_jointly" }
   let(:submission) { create :efile_submission, :ctc, filing_status: filing_status, tax_year: 2021 }
+  let!(:dependent) { create :qualifying_child, first_name: "Janis", intake: submission.intake }
 
   before do
     submission.intake.update(advance_ctc_amount_received: 900)
+    submission.transition_to(:preparing)
+    submission.reload
   end
 
   it "conforms to the eFileAttachments schema 2021v5.2" do
