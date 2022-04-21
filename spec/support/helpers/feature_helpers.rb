2dbbde6b8e5f48a139a45ebf4311de8eabe8f58f
Travis Grathwell <tgrathwell@codeforamerica.org>
2022-04-21 13:01:21 -0700

    Show a flash message and refresh current page if authenticity token is invalid
    Mostly this is just so we don't get a barrage of HTTP 500 errors when
    we get security scanned. Though it might also be nice for people on
    mobile devices whose cookies get cleared.
    
    Also increments a datadog metric when this happens, in case we want
    a graph of how often we're barraged with invalid authenticity tokens.

--- spec/support/helpers/feature_helpers.rb
+++ spec/support/helpers/feature_helpers.rb
@@ -100,6 +100,19 @@ module FeatureHelpers
     contents
   end
 
+  def clear_cookies
+    browser = Capybara.current_session.driver.browser
+    if browser.respond_to?(:clear_cookies)
+      # Rack::MockSession
+      browser.clear_cookies
+    elsif browser.respond_to?(:manage) && browser.manage.respond_to?(:delete_all_cookies)
+      # Selenium::WebDriver
+      browser.manage.delete_all_cookies
+    else
+      raise "Don't know how to clear cookies. Weird driver?"
+    end
+  end
+
   def strip_inner_newlines(text)
     text.gsub(/\n/, '')
   end
