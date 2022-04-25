53e007dc571750555fe4cdb14bc811d431a0646c
Em Barnard-Shao <ebarnard@codeforamerica.org>
2022-04-25 16:04:27 -0400

    Add xml
    Co-authored-by: Asheesh Laroia <alaroia@codeforamerica.org>

--- app/models/tax_return.rb
+++ app/models/tax_return.rb
@@ -100,17 +100,6 @@ class TaxReturn < ApplicationRecord
     standard_deduction + additional_blind_standard_deduction if standard_deduction.present?
   end
 
-  def additional_blind_standard_deduction
-    case filing_status
-    when "single", "head_of_household"
-      return 1700
-    when "married_filing_jointly"
-      return 2700 if intake.was_blind_yes? && intake.spouse_was_blind_yes?
-      return 1350 if intake.was_blind_yes? || intake.spouse_was_blind_yes?
-    end
-    0
-  end
-
   def has_submissions?
     efile_submissions.count.nonzero?
   end
@@ -262,6 +251,17 @@ class TaxReturn < ApplicationRecord
   def system_change_status(new_status)
     SystemNote::StatusChange.generate!(tax_return: self, old_status: current_state, new_status: new_status)
     transition_to(new_status)
+  end
+
+  def additional_blind_standard_deduction
+    case filing_status
+    when "single", "head_of_household"
+      return 1700
+    when "married_filing_jointly"
+      return 2700 if intake.was_blind_yes? && intake.spouse_was_blind_yes?
+      return 1350 if intake.was_blind_yes? || intake.spouse_was_blind_yes?
+    end
+    0
   end
 end
 
