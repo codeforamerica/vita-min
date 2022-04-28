e36c19a4abf3688d7ed962f2ddc0bb7299c74b18
Catie Talbot <ctalbot@codeforamerica.org>
2022-04-28 09:50:09 -0700

    Add drivers license information to XML

--- app/lib/submission_builder/ty2021/documents/irs1040.rb
+++ app/lib/submission_builder/ty2021/documents/irs1040.rb
@@ -12,6 +12,8 @@ module SubmissionBuilder
           intake = submission.intake
           tax_return = submission.tax_return
           bank_account = intake.bank_account
+          primary_drivers_license = intake.primary_drivers_license
+          spouse_drivers_license = intake.spouse_drivers_license
           qualifying_dependents = submission.qualifying_dependents
           benefits = Efile::BenefitsEligibility.new(tax_return: tax_return, dependents: qualifying_dependents)
           boxes_checked = boxes_checked(intake, tax_return)
@@ -55,6 +57,14 @@ module SubmissionBuilder
               xml.BankAccountTypeCd bank_account.account_type_code # 35c
               xml.DepositorAccountNum account_number_type(bank_account.account_number) # 35d
             end
+
+            if primary_drivers_license.present?
+              drivers_license_xml(xml, primary_drivers_license)
+            end
+            if spouse_drivers_license.present?
+              drivers_license_xml(xml, spouse_drivers_license)
+            end
+
             xml.RefundProductCd "NO FINANCIAL PRODUCT"
           end
         end
@@ -69,6 +79,15 @@ module SubmissionBuilder
             xml.DependentSSN dependent.ssn
             xml.DependentRelationshipCd dependent.irs_relationship_enum
             xml.EligibleForChildTaxCreditInd "X" if dependent.qualifying_ctc?
+          end
+        end
+
+        def drivers_license_xml(xml, drivers_license)
+          xml.DrvrLcnsOrStateIssdIdGrpType do
+            xml.DrvrLcnsOrStateIssdIdNum drivers_license.license_number
+            xml.DrvrLcnsOrStateIssdIdStCd drivers_license.state
+            xml.DrvrLcnsOrStateIssdIdExprDt date_type(drivers_license.expiration_date)
+            xml.DrvrLcnsOrStateIssdIdIssDt date_type(drivers_license.issue_date)
           end
         end
 
