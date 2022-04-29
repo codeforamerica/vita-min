627ab47c87f8eac660f3b7ff57f4e7c8c54c5e3c
Shannon Byrne <sbyrne@codeforamerica.org>
2022-04-28 17:19:55 -0700

    Create link when necessary for fraud indicators list page

---
+++ spec/helpers/fraud_indicators_helper_spec.rb
@@ -0,0 +1,35 @@
+require "rails_helper"
+
+describe FraudIndicatorsHelper do
+  describe 'link_to_indicator_list' do
+    context "when passed a fraud indicator without a list model type" do
+      let(:indicator) { create :fraud_indicator, list_model_name: nil }
+      it "returns nil" do
+        expect(link_to_indicator_list(indicator)).to eq nil
+      end
+    end
+
+    context "when the indicator type is not in the list" do
+      let(:indicator) { build :fraud_indicator, list_model_name: "Fraud::Indicators::Something", indicator_type: "duplicates"}
+      it "returns nil" do
+        expect(link_to_indicator_list(indicator)).to eq nil
+      end
+    end
+
+    context "when a route for the indicator does not exist" do
+      let(:indicator) { build :fraud_indicator, list_model_name: "Fraud::Indicators::Something", indicator_type: "not_in_safelist"}
+      it "returns nil" do
+        expect(link_to_indicator_list(indicator)).to eq nil
+      end
+    end
+
+    context "when a route for the indicator exists" do
+      context "hub_risky_domains" do
+        let(:indicator) { build :fraud_indicator, list_model_name: "Fraud::Indicators::Domain", indicator_type: "in_riskylist" }
+        it "returns a link to the page" do
+          expect(link_to_indicator_list(indicator)).to eq "<a href=\"/en/hub/fraud-indicators/risky-domains\"><i class=\"icon-\">list_alt</i></a>"
+        end
+      end
+    end
+  end
+end
