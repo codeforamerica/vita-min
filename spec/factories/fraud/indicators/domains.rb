6869f4b4458f52508a948730a018ed71af438c60
Travis Grathwell <tgrathwell@codeforamerica.org>
2022-04-29 10:27:36 -0700

    check in missing annotation for fraud_indicators_domains factory

--- spec/factories/fraud/indicators/domains.rb
+++ spec/factories/fraud/indicators/domains.rb
@@ -1,3 +1,20 @@
+# == Schema Information
+#
+# Table name: fraud_indicators_domains
+#
+#  id           :bigint           not null, primary key
+#  activated_at :datetime
+#  name         :string
+#  risky        :boolean
+#  safe         :boolean
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+# Indexes
+#
+#  index_fraud_indicators_domains_on_risky  (risky)
+#  index_fraud_indicators_domains_on_safe   (safe)
+#
 FactoryBot.define do
   factory :safe_domain, class: "Fraud::Indicators::Domain" do
     name { "gmail.com" }
