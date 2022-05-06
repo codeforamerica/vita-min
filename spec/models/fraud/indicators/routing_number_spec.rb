d092bc9c69c546e3e7009b924a3cc00b7b14db71
Shannon Byrne <sbyrne@codeforamerica.org>
2022-05-06 13:56:49 -0700

    Fix test

--- spec/models/fraud/indicators/routing_number_spec.rb
+++ spec/models/fraud/indicators/routing_number_spec.rb
@@ -16,12 +16,14 @@ describe Fraud::Indicators::RoutingNumber do
     before do
       described_class.create(routing_number: "123456789", bank_name: "Bank of Taxes", activated_at: DateTime.now)
       described_class.create(routing_number: "111111111", bank_name: "Bank of Money", activated_at: DateTime.now)
-      described_class.create(routing_number: "111111111", bank_name: "Bank of Things", activated_at: nil)
+      described_class.create(routing_number: "111111112", bank_name: "Bank of Things", activated_at: nil)
       allow(EnvironmentCredentials).to receive(:dig).with(:duplicate_hashing_key).and_return "1"
     end
 
-    it "converts entries into a list of their hashed versions" do
-      expect(described_class.riskylist).to eq ["4e1cbd2bad5ec1241a99af0ad298c080fc1358c9aba120e33d73a9c96d4445c6", "ebbcb9796f3f6e771f0599ea197d212c34050b8040eb565830514694eec035d0"]
+    it "returns a list of all activated routing_number entries" do
+      expect(described_class.riskylist).to include "123456789"
+      expect(described_class.riskylist).to include "111111111"
+      expect(described_class.riskylist).not_to include "111111112"
     end
   end
 end
