d020268503ec57224b51572a753b1c3bf10c6206
Catie Talbot <ctalbot@codeforamerica.org>
2022-05-09 15:39:45 -0700

    Rescue URI exceptions

--- app/controllers/ajax_mixpanel_events_controller.rb
+++ app/controllers/ajax_mixpanel_events_controller.rb
@@ -24,12 +24,17 @@ class AjaxMixpanelEventsController < ApplicationController
 
   def controller_and_path_data
     controller, action = event_params[:controller_action].split("#", 2)
+    begin
+      path = URI(event_params[:full_path]).path
+    rescue URI::InvalidURIError
+      path = ""
+    end
     {
-      full_path: event_params[:full_path],
-      path: URI(event_params[:full_path]).path,
-      controller_action: event_params[:controller_action],
-      controller_action_name: action,
-      controller_name: controller.sub("Controller", ""),
-    }
+        full_path: event_params[:full_path],
+        path: path,
+        controller_action: event_params[:controller_action],
+        controller_action_name: action,
+        controller_name: controller.sub("Controller", ""),
+      }
   end
 end
