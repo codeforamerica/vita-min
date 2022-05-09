a27d1bd0e92800ef6aa793b0f07a9fb8604e7d08
Catie Talbot <ctalbot@codeforamerica.org>
2022-05-09 15:20:20 -0700

    Update ddtracer, enabling log-trace correlation (#2485)

--- config/initializers/datadog.rb
+++ config/initializers/datadog.rb
@@ -1,9 +1,9 @@
 Datadog.configure do |c|
   c.service = 'getyourrefund-app'
   c.env = Rails.env
-  c.use :rails
-  c.use :aws
-  c.use :delayed_job
-  c.tracer.enabled = Rails.env.staging? || Rails.env.demo? || Rails.env.production?
-  c.tracer hostname: Rails.application.credentials.dig(:datadog_agent_host)
+  c.agent.host = Rails.application.credentials.dig(:datadog_agent_host)
+  c.tracing.enabled = Rails.env.staging? || Rails.env.demo? || Rails.env.production?
+  c.tracing.instrument :rails
+  c.tracing.instrument :aws
+  c.tracing.instrument :delayed_job
 end
