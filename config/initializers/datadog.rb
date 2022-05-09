a676da99a548d3ad62c8bec487d3e0dad372f541
Catie Talbot <ctalbot@codeforamerica.org>
2022-05-09 13:34:45 -0700

    Update some dd config

--- config/initializers/datadog.rb
+++ config/initializers/datadog.rb
@@ -1,9 +1,9 @@
 Datadog.configure do |c|
   c.service = 'getyourrefund-app'
   c.env = Rails.env
-  c.use :rails
-  c.use :aws
-  c.use :delayed_job
+  c.tracing.instrument :rails
+  c.tracing.instrument :aws
+  c.tracing.instrument :delayed_job
   c.tracing.enabled = Rails.env.staging? || Rails.env.demo? || Rails.env.production?
   c.tracing hostname: Rails.application.credentials.dig(:datadog_agent_host)
 end
