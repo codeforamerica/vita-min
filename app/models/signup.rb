a4777efe616b6c8b044fe3dece0ad81605c7b50f
Shannon Byrne <s.byrne2254@gmail.com>
2022-05-12 15:49:02 -0700

    Ignore column sent_followup on signup (#2555)
    Co-authored-by: Whitney Schaefer <wschaefer@codeforamerica.org>
    
    Co-authored-by: Whitney Schaefer <wschaefer@codeforamerica.org>

--- app/models/signup.rb
+++ app/models/signup.rb
@@ -12,35 +12,12 @@
 #  updated_at    :datetime         not null
 #
 class Signup < ApplicationRecord
+  self.ignored_columns = [:sent_followup]
   validates_presence_of :name
   validate :phone_number_or_email_address
   validates :zip_code, zip_code: true, allow_blank: true
   validates :phone_number, e164_phone: true, allow_blank: true
   validates :email_address, 'valid_email_2/email': true
-
-
-  def self.valid_emails_with_unsent_followups_count
-    distinct(:email_address).where(sent_followup: false).pluck(:email_address).filter do |email|
-      ValidEmail2::Address.new(email).valid?
-    end.count
-  end
-
-  def self.with_unsent_followups
-    distinct(:email_address).where(sent_followup: false)
-  end
-
-
-  def self.send_followup_emails(batch_size = nil)
-    valid_count = 0
-    with_unsent_followups.limit(batch_size).find_each do |signup|
-      next unless ValidEmail2::Address.new(signup.email_address).valid?
-
-      valid_count += 1
-      SignupFollowupMailer.followup(signup.email_address, signup.name).deliver_later
-      Signup.where(email_address: signup.email_address).update_all(sent_followup: true)
-    end
-    valid_count
-  end
 
   private
 
