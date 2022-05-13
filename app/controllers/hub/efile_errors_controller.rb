30ef3fc261cdd4ea184a9364d541534cb240668f
Catie Talbot <ctalbot@codeforamerica.org>
2022-05-13 10:51:06 -0700

    Add the ability to reprocess errors to the admin error index (#2556)

--- app/controllers/hub/efile_errors_controller.rb
+++ app/controllers/hub/efile_errors_controller.rb
@@ -22,6 +22,19 @@ module Hub
       redirect_to hub_efile_error_path(id: @efile_error.id)
     end
 
+    def reprocess
+      if @efile_error.present? && (@efile_error.auto_wait || @efile_error.auto_cancel)
+        auto_transition_to_state = @efile_error.auto_wait ? :waiting : :cancelled
+        submission_ids = EfileSubmissionTransitionError.includes(:efile_error, :efile_submission_transition).where(efile_error: @efile_error, efile_submission_transitions: { most_recent: true, to_state: ["rejected", "failed"] }).pluck(:efile_submission_id)
+        EfileSubmission.where(id: submission_ids).find_each { |submission| submission.transition_to(auto_transition_to_state) }
+
+        flash[:notice] = "Successfully reprocessed #{submission_ids.count} submission(s) with #{@efile_error.code} error!"
+      else
+        flash[:notice] = "Could not reprocess #{@efile_error.code}. Try again."
+      end
+      redirect_to hub_efile_error_path(id: @efile_error.id)
+    end
+
     def permitted_params
       params.require(:efile_error).permit(:expose, :auto_cancel, :auto_wait, :description_en, :description_es, :resolution_en, :resolution_es)
     end
