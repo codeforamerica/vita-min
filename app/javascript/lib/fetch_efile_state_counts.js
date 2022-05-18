4237954b3e71e723d89bb9faacdaebd13f9c9c7a
Catie Talbot <ctalbot@codeforamerica.org>
2022-05-18 14:39:03 -0700

    Remove unnecessary arguments

--- app/javascript/lib/fetch_efile_state_counts.js
+++ app/javascript/lib/fetch_efile_state_counts.js
@@ -2,8 +2,5 @@ export function fetchEfileStateCounts() {
     Rails.ajax({
         url: "/hub/efile/state-counts",
         type: "get",
-        data: "",
-        success: function(data) {},
-        error: function(data) {}
     })
 }
