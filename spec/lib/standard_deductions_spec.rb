e53e5d7c3ab74c77b999a9f569deac0d36651722
Catie Talbot <ctalbot@codeforamerica.org>
2022-05-25 10:09:39 -0700

    Another spec

--- spec/lib/standard_deductions_spec.rb
+++ spec/lib/standard_deductions_spec.rb
@@ -1,65 +1,39 @@
 require "rails_helper"
 
 describe StandardDeductions do
-  context 'initialization' do
-    context "when the passed in irs relationship category is not in the list of supported relationships" do
-      it "raises an error" do
-        expect {
-          described_class.new("daughter", :qualifying_friend, "DAUGHTER", true)
-        }.to raise_error RuntimeError
-      end
+  describe "#base_deductions" do
+    it "retrieves a hash of base deduction amounts for the give year" do
+      expect(described_class.base_deductions(tax_year: 2021)).to eq({
+        "single" => 12550,
+        "married_filing_separately" => 12550,
+        "head_of_household" => 18800,
+        "married_filing_jointly" => 25100,
+        "qualifying_widow" => 25100
+                                                                    })
+    end
+
+    it "allows access of the hash with string or symbol keys" do
+      expect(described_class.base_deductions(tax_year: 2021)[:single]).to eq described_class.base_deductions(tax_year: 2021)["single"]
     end
   end
 
-  describe "#irs_enum" do
-    it "retrieves the defined IRS enum for the relationship value" do
-      expect(described_class.find("in_law").irs_enum).to eq "OTHER"
-      expect(described_class.find("daughter").irs_enum).to eq "DAUGHTER"
+  describe "#blind_deductions" do
+    it "retrieves a hash of blind deduction amounts for the give year" do
+      expect(described_class.blind_deductions(tax_year: 2021)).to eq({
+                                                                      "single_filer" => 1700,
+                                                                      "primary_or_spouse" => 1350,
+                                                                      "primary_and_spouse" => 2700
+                                                                    })
     end
   end
 
-  describe "#qualifying_child_relationship?" do
-    context "when the value in the yaml file for the relationship is qualified_child" do
-      it "is true" do
-        expect(described_class.find("daughter").qualifying_child_relationship?).to eq true
-      end
-    end
-
-    context "when the value in the yaml file for the relationship is qualified_relative" do
-      it "is false" do
-        expect(described_class.find("aunt").qualifying_child_relationship?).to eq false
-      end
-    end
-  end
-
-  describe "#qualifying_relative_relationship?" do
-    context "when the value in the yaml file for the relationship is qualified_relative" do
-      it "is true" do
-        expect(described_class.find("uncle").qualifying_relative_relationship?).to eq true
-      end
-    end
-
-    context "when the value in the yaml file for the relationship is qualified_child" do
-      it "is false" do
-        expect(described_class.find("son").qualifying_relative_relationship?).to eq false
-      end
-    end
-  end
-
-  describe "qualifying_relative_requires_member_of_household_test?" do
-    context "when skip_relative_household_test is false" do
-      context "from a yml relationship" do
-        it "requires the household test" do
-          expect(described_class.find("other").qualifying_relative_requires_member_of_household_test?).to eq true
-        end
-      end
-
-      context "from instantiation" do
-        it "is true" do
-          instance = described_class.new("other", :qualifying_relative, "OTHER", false)
-          expect(instance.qualifying_relative_requires_member_of_household_test?).to eq true
-        end
-      end
+  describe "#older_than_65_deductions" do
+    it "retrieves a hash of age deduction amounts for the give year" do
+      expect(described_class.older_than_65_deductions(tax_year: 2021)).to eq({
+                                                                      "single_filer" => 1700,
+                                                                      "primary_or_spouse" => 1350,
+                                                                      "primary_and_spouse" => 2700
+                                                                    })
     end
   end
 end
