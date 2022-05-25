250ebaa13682fec1f3c25c3930aedf5e63899073
Shannon Byrne <sbyrne@codeforamerica.org>
2022-05-24 17:56:00 -0700

    Archive unused relationships instead of removing them

--- app/helpers/relationships_helper.rb
+++ app/helpers/relationships_helper.rb
@@ -2,7 +2,7 @@ module RelationshipsHelper
   # provides a list of relationship options for dependents, and pushes the client's provided free-form answer
   # to the list if necessary.
   def dependent_relationship_options(current_relationship: nil)
-    options = Efile::Relationship.all.map { |relationship| [I18n.t("general.dependent_relationships.#{relationship.id}"), relationship.id] }
+    options = Efile::Relationship.active.map { |relationship| [I18n.t("general.dependent_relationships.#{relationship.id}"), relationship.id] }
     if current_relationship.present? && Efile::Relationship.find(current_relationship).nil?
       options.push(["#{I18n.t("general.dependent_relationships.other_freefill")}: #{current_relationship}", current_relationship])
     end
--- app/lib/efile/relationship.rb
+++ app/lib/efile/relationship.rb
@@ -1,11 +1,12 @@
 module Efile
   class Relationship
-    attr_reader :id, :relationship, :irs_enum
+    attr_reader :id, :relationship, :irs_enum, :archived
+    alias_method :archived?, :archived
 
     def self.import(filename)
       @@relationships =
         YAML.load_file(filename)['relationships'].map do |relationship|
-          new(relationship["id"], relationship["relationship"].to_sym, relationship["irs_enum"], relationship["skip_relative_household_test"])
+          new(relationship["id"], relationship["relationship"].to_sym, relationship["irs_enum"], relationship["skip_relative_household_test"], relationship["archived"])
         end
     end
 
@@ -13,17 +14,22 @@ module Efile
       @@relationships
     end
 
+    def self.active
+      all.reject(&:archived?)
+    end
+
     def self.find(id)
       @@relationships.find { |relationship| id == relationship.id }
     end
 
-    def initialize(id, relationship, irs_enum, skip_relative_household_test)
+    def initialize(id, relationship, irs_enum, skip_relative_household_test, archived)
       @id = id
       relationships = [:qualifying_relative, :qualifying_child, :ineligible]
       raise "Invalid relationship. Relationship #{relationship} must be in: #{relationships}" unless relationship.in? relationships
       @relationship = relationship
       @irs_enum = irs_enum
       @skip_relative_household_test = ActiveModel::Type::Boolean.new.cast(skip_relative_household_test)
+      @archived = ActiveModel::Type::Boolean.new.cast(archived || false)
     end
 
     def qualifying_child_relationship?
