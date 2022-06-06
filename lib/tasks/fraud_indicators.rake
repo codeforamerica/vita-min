235cdbd72f98859ba0dd541a491e1e6208992b1a
Catie Talbot <ctalbot@codeforamerica.org>
2022-06-06 11:55:14 -0700

    Rake WIP
    Co-authored-by: Whitney Schaefer <wschaefer@codeforamerica.org>

--- lib/tasks/fraud_indicators.rake
+++ lib/tasks/fraud_indicators.rake
@@ -1,19 +1,23 @@
 namespace :fraud_indicators do
-  desc 'Adds new fraud indicators to the db from the encrypted file'
-  # rake fraud_indicators:add
-  task add: :environment do
+  desc 'Updates fraud indicators in the db to match the encrypted file'
+  # rake fraud_indicators:update --preview
+  task update: [:environment] do
+    preview = ARGV[1].to_s == "--preview"
     JSON.parse(Rails.application.encrypted('app/models/fraud/indicators.json.enc', key_path: 'config/fraud_indicators.key', env_key: 'FRAUD_INDICATORS_KEY').read).each do |indicator_attributes|
       indicator = Fraud::Indicator.find_or_initialize_by(name: indicator_attributes['name'])
 
-      next if indicator.persisted?
-
-      indicator.assign_attributes(
-        indicator_attributes.merge(
-          'activated_at' => Time.zone.now,
-          'query_model_name' => indicator_attributes['query_model_name']&.constantize
-        )
-      )
-      indicator.save!
+      indicator_attributes['activated_at'] = Time.zone.now unless indicator.persisted?
+      indicator_attributes['query_model_name'] = indicator_attributes['query_model_name']&.constantize
+      indicator.assign_attributes(indicator_attributes)
+      if preview
+        if indicator.persisted?
+          puts "updates: #{indicator.changes}" #if indicator.changed?
+        else
+          puts "adds: #{indicator.attributes.except("id", "activated_at", "created_at", "updated_at")}"
+        end
+      else
+        indicator.save!
+      end
     end
   end
 end
--- spec/tasks/fraud_indicators_spec.rb
+++ spec/tasks/fraud_indicators_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-describe "fraud_indicators:add" do
+describe "fraud_indicators:update" do
   include_context "rake"
 
   let(:encrypted_indicators) do
@@ -10,7 +10,7 @@ describe "fraud_indicators:add" do
        "threshold"=>nil,
        "reference"=>"intake",
        "list_model_name"=>nil,
-       "indicator_attributes"=>["attribute1"],
+       "indicator_attributes"=>%w[attribute1 attribute2 attribute3],
        "points"=>10,
        "multiplier"=>0,
        "description"=>nil
@@ -26,13 +26,50 @@ describe "fraud_indicators:add" do
       expect { task.invoke }.to change { Fraud::Indicator.count }.by(1)
       expect(Fraud::Indicator.last.name).to eq "pretend_indicator"
     end
+
+    context "with the variable --preview" do
+      before do
+        ARGV.replace ["fraud_indicators:update", "--preview"]
+      end
+
+      it "does not create a new indicator" do
+        expect { task.invoke }.to change { Fraud::Indicator.count }.by(0)
+      end
+
+      it "outputs the expected changes" do
+        expected_output = {"description"=>nil, "indicator_attributes"=>["attribute1", "attribute2", "attribute3"], "indicator_type"=>"gem", "list_model_name"=>nil, "multiplier"=>0.0, "name"=>"pretend_indicator", "points"=>10, "query_model_name"=>"Intake", "reference"=>"intake", "threshold"=>nil}
+        expect { task.invoke }.to output("adds: #{expected_output}").to_stdout
+      end
+    end
   end
 
-  context "with no new indicators" do
-    let!(:existing_indicator) { create(:fraud_indicator, name: "pretend_indicator") }
+  context "with the same indicator name but different attributes" do
+    let!(:existing_indicator) { create(:fraud_indicator, name: "pretend_indicator", points: 30) }
 
-    it "does not update the database" do
+    it "does not create a new indicator" do
       expect { task.invoke }.to change { Fraud::Indicator.count }.by(0)
+    end
+
+    it "updates the existing indicator" do
+      expect { task.invoke }.to change { Fraud::Indicator.last.points }.from(30).to(10)
+    end
+
+    it "does not update the activated_at" do
+      expect { task.invoke }.not_to change { Fraud::Indicator.last.activated_at }
+    end
+
+    context "with the variable --preview" do
+      before do
+        ARGV.replace ["fraud_indicators:update", "--preview"]
+      end
+
+      it "does not update the indicator" do
+        expect { task.invoke }.not_to change { Fraud::Indicator.last.points }
+      end
+
+      it "outputs the expected changes" do
+        expect { task.invoke }.to output('updates: { points => 10 }').to_stdout
+      end
     end
   end
 end
