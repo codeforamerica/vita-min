f9c6cedd1a58fb2b503dcdb3e34134a01f524833
Shannon Byrne <sbyrne@codeforamerica.org>
2022-06-15 14:27:28 -0700

    Drop intake primary consented to service at and index on the enum

--- db/migrate/20220615204444_add_index_to_primary_consented_to_service.rb
+++ db/migrate/20220615204444_add_index_to_primary_consented_to_service.rb
@@ -1,7 +1,7 @@
 class AddIndexToPrimaryConsentedToService < ActiveRecord::Migration[7.0]
   disable_ddl_transaction!
   def change
-    remove_index :intakes, :primary_consented_to_service_at
     add_index :intakes, :primary_consented_to_service, algorithm: :concurrently
+    remove_column :intakes, :primary_consented_to_service_at
   end
 end
