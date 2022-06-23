8526d7e139edb66a21ed69d51bc67fb42edf0ca6
Em Barnard-Shao <ebarnard@codeforamerica.org>
2022-06-23 12:12:50 -0400

    PR test fixes

--- spec/support/helpers/ctc_intake_feature_helper.rb
+++ spec/support/helpers/ctc_intake_feature_helper.rb
@@ -33,8 +33,8 @@ module CtcIntakeFeatureHelper
     end
     click_on I18n.t('general.continue')
 
-    expect(page).to have_selector("h1", text: I18n.t("views.ctc.questions.file_full_return.title"))
-    click_on I18n.t("views.ctc.questions.file_full_return.simplified_btn")
+    expect(page).to have_selector("h1", text: I18n.t("views.ctc.questions.file_full_return.puerto_rico_resident.title"))
+    click_on I18n.t("views.ctc.questions.file_full_return.puerto_rico_resident.simplified_btn")
     expect(page).to have_selector("h1", text: I18n.t('views.ctc.questions.restrictions.title'))
     click_on I18n.t('views.ctc.questions.restrictions.cannot_use_ctc')
     expect(page).to have_selector("h1", text: I18n.t('views.ctc.questions.use_gyr.title'))
