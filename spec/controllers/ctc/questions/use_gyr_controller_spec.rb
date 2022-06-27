891c1e0401efbb57e073071bcae6eb2688177f18
Whitney Schaefer <wschaefer@codeforamerica.org>
2022-06-27 11:34:47 -0700

    Use Gyr page redirects to start of flow if no current_intake
    We need it now to determine whether to show puerto rico specific content

--- spec/controllers/ctc/questions/use_gyr_controller_spec.rb
+++ spec/controllers/ctc/questions/use_gyr_controller_spec.rb
@@ -2,9 +2,25 @@ require "rails_helper"
 
 describe Ctc::Questions::UseGyrController do
   describe "#edit" do
-    it "renders edit template" do
-      get :edit, params: {}
-      expect(response).to render_template :edit
+    context 'with an intake in the session' do
+      let(:client) { create :client_with_ctc_intake_and_return}
+      let(:intake) { client.intake }
+
+      before do
+        session[:intake_id] = intake.id
+      end
+
+      it "renders edit template" do
+        get :edit, params: {}
+        expect(response).to render_template :edit
+      end
+    end
+
+    context 'without an intake in the session' do
+      it "redirects to the ctc homepage" do
+        get :edit, params: {}
+        expect(response).to redirect_to(question_path(id: CtcQuestionNavigation.first))
+      end
     end
   end
 end
