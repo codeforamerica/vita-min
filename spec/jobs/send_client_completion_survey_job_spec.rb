74a65d30c0d94da60f304e6ea6b7851bbe1a3fde
Catie Talbot <ctalbot@codeforamerica.org>
2022-07-12 16:50:11 -0700

    Update spec

--- spec/jobs/send_client_completion_survey_job_spec.rb
+++ spec/jobs/send_client_completion_survey_job_spec.rb
@@ -22,7 +22,7 @@ RSpec.describe SendClientCompletionSurveyJob, type: :job do
             expect(ClientMessagingService).to have_received(:send_system_email).with(
               client: client,
               body: a_string_including("qualtrics.com/jfe/form/SV_2uCOhUGqxJdG8Au"),
-              subject: "¡Gracias por declarar tus impuestos con GetYourRefund!",
+              subject: "¡Gracias por elegir GetYourRefund!",
               locale: "es"
             )
             expect(client.reload.completion_survey_sent_at).to be_present
@@ -63,7 +63,7 @@ RSpec.describe SendClientCompletionSurveyJob, type: :job do
             expect(ClientMessagingService).to have_received(:send_system_email).with(
               client: client,
               body: a_string_including("qualtrics.com/jfe/form/SV_2uCOhUGqxJdG8Au"),
-              subject: "¡Gracias por declarar tus impuestos con GetYourRefund!",
+              subject: "¡Gracias por elegir GetYourRefund!",
               locale: "es"
             )
             expect(ClientMessagingService).not_to have_received(:send_system_text_message)
@@ -85,7 +85,7 @@ RSpec.describe SendClientCompletionSurveyJob, type: :job do
             expect(ClientMessagingService).to have_received(:send_system_email).with(
                 client: client,
                 body: a_string_including("qualtrics.com/jfe/form/SV_1Ch7S3rTLOgzbFk"),
-                subject: "¡Gracias por declarar tus impuestos con GetYourRefund!",
+                subject: "¡Gracias por elegir GetYourRefund!",
                 locale: "es"
             )
             expect(ClientMessagingService).not_to have_received(:send_system_text_message)
