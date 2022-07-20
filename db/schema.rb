4a4ea5642fc48bf04f090e0e3728b5fc63d4cfed
Jenny Heath <jheath@codeforamerica.org>
2022-07-20 15:58:17 -0700

    Use the bigger number in schema file
    Co-authored-by: Travis Grathwell <tgrathwell@codeforamerica.org>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_18_220949) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_19_202248) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
   enable_extension "plpgsql"
