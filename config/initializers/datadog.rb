50000b2eff2e5852a1440b8a7e72fb6f27e36cb6
Catie Talbot <ctalbot@codeforamerica.org>
2022-07-21 16:14:04 -0700

    Only set up instrumentation for environments using ddtracer
    Co-authored-by: Travis Grathwell <tgrathwell@codeforamerica.org>

--- config/initializers/datadog.rb
+++ config/initializers/datadog.rb
@@ -2,9 +2,12 @@ Datadog.configure do |c|
   c.service = 'getyourrefund-app'
   c.env = Rails.env
   c.agent.host = Rails.application.credentials.dig(:datadog_agent_host)
-  c.tracing.enabled = Rails.env.staging? || Rails.env.demo? || Rails.env.production?
-  c.tracing.instrument :rails
-  c.tracing.instrument :aws
-  c.tracing.instrument :delayed_job
-  c.tracing.instrument :http
+  enable_tracing = Rails.env.staging? || Rails.env.demo? || Rails.env.production?
+  c.tracing.enabled = enable_tracing
+  if enable_tracing
+    c.tracing.instrument :rails
+    c.tracing.instrument :aws
+    c.tracing.instrument :delayed_job
+    c.tracing.instrument :http
+  end
 end
