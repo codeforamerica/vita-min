812ef790ea9d060bdc89980470481682005baad0
Travis Grathwell <tgrathwell@codeforamerica.org>
2022-07-21 14:13:44 -0700

    Allow magic verification code '000000' on Heroku for some flows

--- spec/forms/email_verification_form_spec.rb
+++ spec/forms/email_verification_form_spec.rb
@@ -1,19 +1,19 @@
 require "rails_helper"
 
-describe Ctc::PhoneVerificationForm do
+describe PhoneVerificationForm do
   let(:params) do
     {
-        verification_code: '000001'
+      verification_code: '000001'
     }
   end
 
   describe '#save' do
     before do
-      allow(TextMessageAccessToken).to receive(:lookup).and_return([TextMessageAccessToken.new])
+      allow(EmailAccessToken).to receive(:lookup).and_return([EmailAccessToken.new])
     end
 
     context 'when sms was the verification medium' do
-      let(:intake) { create :ctc_intake, sms_phone_number: '+15125551234', sms_notification_opt_in: 'yes' }
+      let(:intake) { create :intake, email_address: 'foo@example.com', email_notification_opt_in: 'yes' }
 
       it 'updates the verified_at timestamp for the verification medium used' do
         expect {
@@ -24,7 +24,7 @@ describe Ctc::PhoneVerificationForm do
   end
 
   describe "#valid?" do
-    let(:intake) { create :ctc_intake, sms_phone_number: '+15125551234', sms_notification_opt_in: 'yes' }
+    let(:intake) { create :intake, sms_phone_number: '+15125551234', sms_notification_opt_in: 'yes' }
     let(:form) { described_class.new(intake, params) }
 
     it_behaves_like :a_verification_form_that_accepts_the_magic_code
