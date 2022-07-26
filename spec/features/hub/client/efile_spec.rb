8cbc3098dacb7cedf35b9c99ce732eb333a12c14
tgrathwell <30675659+tgrathwell@users.noreply.github.com>
2022-07-26 15:49:53 -0700

    Hide resubmit button if archived intake on client efile submission page (#2957)
    Hide resubmit button if archived intake on client efile submission page
    
    also moves test files dealing with hub client tabs from
    /features/hub to /features/hub/client to better distinguish
    top-level hub features from client-specific ones
    
    Co-authored-by: Em Barnard-Shao <ebarnard@codeforamerica.org>

---
+++ spec/features/hub/client/efile_spec.rb
@@ -0,0 +1,37 @@
+require "rails_helper"
+
+RSpec.feature "View efile submissions for a client" do
+  context "As an authenticated user" do
+    let(:user) { create :admin_user }
+    let(:client) { intake.client }
+    let!(:intake) { create(:ctc_intake) }
+    let!(:tax_return) { create(:tax_return, :ctc, client: client, year: TaxReturn.current_tax_year) }
+    let!(:initial_efile_submission) { create :efile_submission, :failed, tax_return: tax_return }
+
+    let(:resubmit_button_text) { "Resubmit" }
+
+    before do
+      login_as user
+    end
+
+    context "when the last efile submission was a failure" do
+      it "allows resubmitting" do
+        visit efile_hub_client_path(id: client.id)
+
+        expect(page).to have_content(intake.preferred_name)
+        expect(page).to have_button(resubmit_button_text)
+      end
+
+      context "for an archived intake" do
+        let!(:intake) { create(:archived_2021_ctc_intake) }
+
+        it "does not allow resubmitting" do
+          visit efile_hub_client_path(id: client.id)
+
+          expect(page).to have_content(intake.preferred_name)
+          expect(page).not_to have_button(resubmit_button_text)
+        end
+      end
+    end
+  end
+end
