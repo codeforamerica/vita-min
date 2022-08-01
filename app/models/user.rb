b3188fbcf68f26a464fec24333803229a528e4d9
Shannon Byrne <sbyrne@codeforamerica.org>
2022-08-01 13:02:13 -0700

    Start to remove unused column
    Co-authored-by: Maria Quadri <mquadri@codeforamerica.org>

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,35 +2,33 @@
 #
 # Table name: users
 #
-#  id                        :bigint           not null, primary key
-#  current_sign_in_at        :datetime
-#  current_sign_in_ip        :string
-#  email                     :citext           not null
-#  encrypted_access_token    :string
-#  encrypted_access_token_iv :string
-#  encrypted_password        :string           default(""), not null
-#  failed_attempts           :integer          default(0), not null
-#  invitation_accepted_at    :datetime
-#  invitation_created_at     :datetime
-#  invitation_limit          :integer
-#  invitation_sent_at        :datetime
-#  invitation_token          :string
-#  invitations_count         :integer          default(0)
-#  last_sign_in_at           :datetime
-#  last_sign_in_ip           :string
-#  locked_at                 :datetime
-#  name                      :string
-#  phone_number              :string
-#  reset_password_sent_at    :datetime
-#  reset_password_token      :string
-#  role_type                 :string           not null
-#  sign_in_count             :integer          default(0), not null
-#  suspended_at              :datetime
-#  timezone                  :string           default("America/New_York"), not null
-#  created_at                :datetime         not null
-#  updated_at                :datetime         not null
-#  invited_by_id             :bigint
-#  role_id                   :bigint           not null
+#  id                     :bigint           not null, primary key
+#  current_sign_in_at     :datetime
+#  current_sign_in_ip     :string
+#  email                  :citext           not null
+#  encrypted_password     :string           default(""), not null
+#  failed_attempts        :integer          default(0), not null
+#  invitation_accepted_at :datetime
+#  invitation_created_at  :datetime
+#  invitation_limit       :integer
+#  invitation_sent_at     :datetime
+#  invitation_token       :string
+#  invitations_count      :integer          default(0)
+#  last_sign_in_at        :datetime
+#  last_sign_in_ip        :string
+#  locked_at              :datetime
+#  name                   :string
+#  phone_number           :string
+#  reset_password_sent_at :datetime
+#  reset_password_token   :string
+#  role_type              :string           not null
+#  sign_in_count          :integer          default(0), not null
+#  suspended_at           :datetime
+#  timezone               :string           default("America/New_York"), not null
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#  invited_by_id          :bigint
+#  role_id                :bigint           not null
 #
 # Indexes
 #
@@ -47,6 +45,7 @@
 #
 class User < ApplicationRecord
   include PgSearch::Model
+  self.ignored_columns = ["encrypted_access_token", "encrypted_access_token_iv"]
   devise :database_authenticatable, :lockable, :validatable, :timeoutable, :trackable, :invitable, :recoverable
 
   pg_search_scope :search, against: [
--- db/migrate/20220801194924_drop_unused_column_from_user.rb
+++
@@ -1,6 +0,0 @@
-class DropUnusedColumnFromUser < ActiveRecord::Migration[7.0]
-  def change
-    remove_column :users, :encrypted_access_token, :string
-    remove_column :users, :encrypted_access_token_iv, :string
-  end
-end
