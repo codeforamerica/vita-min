e72a1953988bd3a89ba3042fa9a435590c78ddf7
Jenny Heath <jheath@codeforamerica.org>
2022-08-08 12:38:44 -0700

    add test for intake#age_on_date
    Co-authored-by: Asheesh Laroia <alaroia@codeforamerica.org>

--- spec/models/intake_spec.rb
+++ spec/models/intake_spec.rb
@@ -525,6 +525,7 @@ describe Intake do
       end
     end
   end
+
   describe "#referrer_domain" do
     let(:intake) { build :intake, referrer: referrer }
 
@@ -1099,6 +1100,17 @@ describe Intake do
         it "is falsey" do
           expect(intake.itin_applicant?).to be_falsey
         end
+      end
+    end
+  end
+
+  describe "#age_on_date" do
+    let(:intake) { create(:intake) }
+
+    [[Date.parse("21-03-1993"), 29], [Date.parse("07-08-1992"), 30], [Date.parse("09-08-1992"), 29]].each do |dob_age_pair|
+      it "returns the correct age" do
+        intake.update(primary_birth_date: dob_age_pair[0])
+        expect(intake.age_on_date(Date.parse("08-08-2022"))).to eq dob_age_pair[1]
       end
     end
   end
