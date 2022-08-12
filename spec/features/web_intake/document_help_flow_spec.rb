169a4242f3ba3e83a5efa938204505cd46c3f044
Maria Quadri <mquadri@codeforamerica.org>
2022-08-12 14:24:00 -0500

    Add test

--- spec/features/web_intake/document_help_flow_spec.rb
+++ spec/features/web_intake/document_help_flow_spec.rb
@@ -5,8 +5,10 @@ RSpec.feature "Document Help Flow", :flow_explorer_screenshot, active_job: true 
     create :client,
            intake: (create :intake, bought_health_insurance: "yes", had_retirement_income: "yes", sms_notification_opt_in: "yes", sms_phone_number: "+15105551234")
   end
+  let(:user) { create(:admin_user) }
   before do
     login_as client, scope: :client
+    login_as user, scope: :user
   end
 
   scenario "getting through documents flow with help pages" do
@@ -43,5 +45,19 @@ RSpec.feature "Document Help Flow", :flow_explorer_screenshot, active_job: true 
     expect do
       click_on I18n.t('documents.documents_help.show.doesnt_apply')
     end.to change(SystemNote::DocumentHelp, :count).by(1)
+  end
+
+  scenario "need help finding ID card" do
+    visit "documents/ids"
+    expect(page).to have_text I18n.t('views.documents.ids.title.one')
+
+    click_on I18n.t('views.layouts.document_upload.dont_have')
+    expect(page).to have_text I18n.t('documents.documents_help.show.header')
+    expect do
+      click_on I18n.t("documents.documents_help.show.need_help_find")
+    end.to change(SystemNote::DocumentHelp, :count).by(1)
+    visit hub_client_notes_path(client_id: client.id)
+    expect(page).to have_text "Add a note"
+    expect(page).to have_text "I need help finding my ID card(s)"
   end
 end
