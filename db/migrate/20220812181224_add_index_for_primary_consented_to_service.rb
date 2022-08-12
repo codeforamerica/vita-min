60b907a00e866f9e3a1c8967b439c300b74f0c81
Shannon Byrne <sbyrne@codeforamerica.org>
2022-08-12 11:47:07 -0700

    Index primary_consented_to_service column on intake + drop datetime column

---
+++ db/migrate/20220812181224_add_index_for_primary_consented_to_service.rb
@@ -0,0 +1,7 @@
+class AddIndexForPrimaryConsentedToService < ActiveRecord::Migration[7.0]
+  disable_ddl_transaction!
+  def change
+    add_index :intakes, :primary_consented_to_service, algorithm: :concurrently
+    safety_assured { remove_column :intakes, :primary_consented_to_service_at }
+  end
+end
