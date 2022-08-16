f9ba6b80a4b25b49c84a7b6a310797eba7aa9c2e
mquadri23 <mquadri@codeforamerica.org>
2022-08-16 14:05:14 -0500

    Update system note  (#3003)
    * Update system note for identity cards
    
    * added translation keys
    
    * add comments and modify test
    
    * normalized
    
    * add spaces and update test format
    
    * add quotation marks and remove extra space

--- spec/features/web_intake/document_help_flow_spec.rb
+++ spec/features/web_intake/document_help_flow_spec.rb
@@ -5,8 +5,10 @@ RSpec.feature "Document Help Flow", :flow_explorer_screenshot, active_job: true 
     create :client,
            intake: (create :intake, bought_health_insurance: "yes", had_retirement_income: "yes", sms_notification_opt_in: "yes", sms_phone_number: "+15105551234")
   end
+  let(:user) { create(:admin_user) }
   before do
     login_as client, scope: :client
+    login_as user, scope: :user
   end
 
   scenario "getting through documents flow with help pages" do
@@ -43,5 +45,22 @@ RSpec.feature "Document Help Flow", :flow_explorer_screenshot, active_job: true 
     expect do
       click_on I18n.t('documents.documents_help.show.doesnt_apply')
     end.to change(SystemNote::DocumentHelp, :count).by(1)
+  end
+
+  scenario "need help finding ID card" do
+    # navigating as client
+    visit "documents/ids"
+    expect(page).to have_text I18n.t('views.documents.ids.title.one')
+
+    click_on I18n.t('views.layouts.document_upload.dont_have')
+    expect(page).to have_text I18n.t('documents.documents_help.show.header')
+    expect do
+      click_on I18n.t("documents.documents_help.show.need_help_find")
+    end.to change(SystemNote::DocumentHelp, :count).by(1)
+
+    # viewing notes in the hub
+    visit hub_client_notes_path(client_id: client.id)
+    expect(page).to have_text "Add a note"
+    expect(page).to have_text I18n.t("hub.system_notes.custom_help_message.ID")
   end
 end
