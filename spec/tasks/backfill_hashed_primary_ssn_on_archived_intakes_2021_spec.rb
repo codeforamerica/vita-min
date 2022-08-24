d2ac20bc208fd5d23566f09baf7b0a488214bc35
Jenny Heath <jheath@codeforamerica.org>
2022-08-24 14:50:22 -0700

    don't merge attributes, add more test cases

--- spec/tasks/backfill_hashed_primary_ssn_on_archived_intakes_2021_spec.rb
+++ spec/tasks/backfill_hashed_primary_ssn_on_archived_intakes_2021_spec.rb
@@ -6,9 +6,11 @@ describe "backfill_hashed_primary_ssn_on_archived_intakes_2021:backfill" do
   context "when there are some archived intakes with primary_ssn and some without" do
     before do
       5.times do
+        create(:archived_2021_gyr_intake, primary_ssn: rand.to_s[2..10], hashed_primary_ssn: nil)
         create(:archived_2021_ctc_intake, primary_ssn: rand.to_s[2..10], hashed_primary_ssn: nil)
       end
       4.times do
+        create(:archived_2021_gyr_intake, primary_ssn: nil)
         create(:archived_2021_ctc_intake, primary_ssn: nil)
       end
     end
@@ -17,13 +19,13 @@ describe "backfill_hashed_primary_ssn_on_archived_intakes_2021:backfill" do
       task.invoke
 
       backfilled = Archived::Intake2021.where.not(primary_ssn: nil)
-      expect(backfilled.count).to eq 5
+      expect(backfilled.count).to eq 10
       backfilled.each do |intake|
         expect(intake.hashed_primary_ssn).to eq DeduplificationService.sensitive_attribute_hashed(intake, :primary_ssn)
       end
 
       not_backfilled = Archived::Intake2021.where(primary_ssn: nil)
-      expect(not_backfilled.count).to eq 4
+      expect(not_backfilled.count).to eq 8
       not_backfilled.each do |intake|
         expect(intake.hashed_primary_ssn).to be_nil
       end
