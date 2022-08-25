21cecfd6f745306888d094100077b6fca877796e
Jenny Heath <jheath@codeforamerica.org>
2022-08-25 11:03:28 -0700

    Remove unused enums
    had_reportable_income on archived and current ctc intakes
    needs_help_2021 on archived gyr intake

--- app/models/archived/intake/gyr_intake_2021.rb
+++ app/models/archived/intake/gyr_intake_2021.rb
@@ -302,7 +302,6 @@ class Archived::Intake::GyrIntake2021 < Archived::Intake2021
   enum needs_help_2018: { unfilled: 0, yes: 1, no: 2 }, _prefix: :needs_help_2018
   enum needs_help_2019: { unfilled: 0, yes: 1, no: 2 }, _prefix: :needs_help_2019
   enum needs_help_2020: { unfilled: 0, yes: 1, no: 2 }, _prefix: :needs_help_2020
-  enum needs_help_2021: { unfilled: 0, yes: 1, no: 2 }, _prefix: :needs_help_2021
   enum no_eligibility_checks_apply: { unfilled: 0, yes: 1, no: 2 }, _prefix: :no_eligibility_checks_apply
   enum no_ssn: { unfilled: 0, yes: 1, no: 2 }, _prefix: :no_ssn
   enum paid_alimony: { unfilled: 0, yes: 1, no: 2, unsure: 3 }, _prefix: :paid_alimony
