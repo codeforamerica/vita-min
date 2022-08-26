40b0ba06b14ba9202e1e87d4c65da0db628e3aa2
Jenny Heath <jheath@codeforamerica.org>
2022-08-26 16:20:54 -0700

    slightly improve query, fix test set

--- spec/models/survey_messages/gyr_completion_survey_spec.rb
+++ spec/models/survey_messages/gyr_completion_survey_spec.rb
@@ -3,13 +3,17 @@ require 'rails_helper'
 RSpec.describe SurveyMessages::GyrCompletionSurvey do
   describe ".clients_to_survey" do
     let!(:client) do
-      (create :tax_return, status, client: build(:client, completion_survey_sent_at: completion_survey_sent_at, intake: build(:intake, primary_consented_to_service: "yes"))).client
+      (create :tax_return, :intake_in_progress, client: build(:client, completion_survey_sent_at: completion_survey_sent_at, intake: build(:intake, primary_consented_to_service: "yes"))).client
     end
     let(:completion_survey_sent_at) { nil }
     let(:status) { nil }
     let(:expected_send_time) { 25.hours.from_now }
     let(:too_early_dont_send_time) { expected_send_time - 1.day }
     let(:too_stale_dont_send_time) { expected_send_time + 31.days }
+
+    before do
+      client.tax_returns.last.transition_to(status)
+    end
 
     context "clients who should get the survey" do
       let(:status) { "file_accepted" }
@@ -22,9 +26,9 @@ RSpec.describe SurveyMessages::GyrCompletionSurvey do
     end
 
     context "clients who should not get the survey" do
-      context "a client whose tax returns are all in terminal statuses" do
-        let(:status) { "file_accepted" }
+      let(:status) { "file_accepted" }
 
+      context "a client whose tax returns are all in terminal statuses" do
         context "but has been in status for less than a day" do
           it "excludes the client" do
             expect(described_class.clients_to_survey(too_early_dont_send_time)).to be_empty
