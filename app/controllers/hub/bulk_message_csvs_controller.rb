bda7865aeb5947f3a14a50975ca7344764dd40aa
Travis Grathwell <tgrathwell@codeforamerica.org>
2022-09-01 14:04:22 -0700

    remove cruft and normalize i18n

--- app/controllers/hub/bulk_message_csvs_controller.rb
+++ app/controllers/hub/bulk_message_csvs_controller.rb
@@ -1,9 +1,7 @@
 module Hub
   class BulkMessageCsvsController < ApplicationController
-    include FilesConcern
     include AccessControllable
     before_action :require_sign_in
-    helper_method :transient_storage_url
     load_and_authorize_resource
 
     layout "hub"
