134a780329495c05515743c18b97c0ead666b371
Whitney Schaefer <wschaefer@codeforamerica.org>
2022-09-26 14:37:59 -0700

    Add all remaining W2 fields to the xml
    Re-add the irs test credentials to the development credentials
    because there isn't a convenient way to do dev irs testing with
    environment variables
    
    Co-authored-by: Travis Grathwell <tgrathwell@codeforamerica.org>

--- app/forms/ctc/w2s/misc_info_form.rb
+++ app/forms/ctc/w2s/misc_info_form.rb
@@ -22,10 +22,10 @@ module Ctc
       validates :box12b_code, allow_blank: true, inclusion: { in: W2::BOX12_OPTIONS }
       validates :box12c_code, allow_blank: true, inclusion: { in: W2::BOX12_OPTIONS }
       validates :box12d_code, allow_blank: true, inclusion: { in: W2::BOX12_OPTIONS }
-      validates :box12a_value, gyr_numericality: { message: ->(_object, _data) { I18n.t('views.ctc.questions.w2s.misc_info.box12_value_error') } }, allow_blank: true
-      validates :box12b_value, gyr_numericality: { message: ->(_object, _data) { I18n.t('views.ctc.questions.w2s.misc_info.box12_value_error') } }, allow_blank: true
-      validates :box12c_value, gyr_numericality: { message: ->(_object, _data) { I18n.t('views.ctc.questions.w2s.misc_info.box12_value_error') } }, allow_blank: true
-      validates :box12d_value, gyr_numericality: { message: ->(_object, _data) { I18n.t('views.ctc.questions.w2s.misc_info.box12_value_error') } }, allow_blank: true
+      validates :box12a_value, gyr_numericality: { greater_than_or_equal_to: 0, message: ->(_object, _data) { I18n.t('views.ctc.questions.w2s.misc_info.box12_value_error') } }, allow_blank: true
+      validates :box12b_value, gyr_numericality: { greater_than_or_equal_to: 0, message: ->(_object, _data) { I18n.t('views.ctc.questions.w2s.misc_info.box12_value_error') } }, allow_blank: true
+      validates :box12c_value, gyr_numericality: { greater_than_or_equal_to: 0, message: ->(_object, _data) { I18n.t('views.ctc.questions.w2s.misc_info.box12_value_error') } }, allow_blank: true
+      validates :box12d_value, gyr_numericality: { greater_than_or_equal_to: 0, message: ->(_object, _data) { I18n.t('views.ctc.questions.w2s.misc_info.box12_value_error') } }, allow_blank: true
       validate :box12s
 
       def box12s
