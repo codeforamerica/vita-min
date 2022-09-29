7f50ceb7168b3a25a792538460c01a6f0e1d121e
Asheesh Laroia <alaroia@codeforamerica.org>
2022-09-29 16:30:30 -0400

    [#183415708] Update soft launch EITC date to 9/30 on session toggles (#3103)
    Co-authored-by: Asheesh Laroia <alaroia@codeforamerica.org>

--- config/application.rb
+++ config/application.rb
@@ -70,7 +70,7 @@ module VitaMin
 
     config.ctc_soft_launch = Time.find_zone("America/New_York").parse("2022-05-04 09:00:00")
     config.ctc_full_launch = Time.find_zone("America/New_York").parse("2022-05-11 09:00:00")
-    config.eitc_soft_launch = Time.find_zone("America/New_York").parse("2022-10-03 09:00:00")
+    config.eitc_soft_launch = Time.find_zone("America/New_York").parse("2022-09-30 09:00:00")
     config.ctc_end_of_intake = Time.find_zone("America/New_York").parse("2022-11-15 17:00:00")
     config.ctc_end_of_login = Time.find_zone("America/New_York").parse("2022-11-19 17:00:00")
 
