7951e755105fc56bfdf18d0bb8249a483bc973e0
Travis Grathwell <tgrathwell@codeforamerica.org>
2022-10-06 10:13:13 -0700

    Decimal types (like w2s.wages_amount) accept commas
    the default rails implementation of decimal does "12,345".to_d,
    which just returns "12"
    
    this monkeypatches Decimal to always remove dollar signs and commas
    so we will get the full dollar value instead
    
    Co-authored-by: Asheesh Laroia <alaroia@codeforamerica.org>

--- config/initializers/types.rb
+++ config/initializers/types.rb
@@ -1,3 +1,14 @@
 Rails.application.reloader.to_prepare do
+  module DecimalCleaner
+    def cast(value)
+      if value.is_a?(String)
+        super(value.gsub(/[$,]/, ''))
+      else
+        super
+      end
+    end
+  end
+  ActiveRecord::Type::Decimal.prepend(DecimalCleaner)
+
   ActiveRecord::Type.register(:money, MoneyType)
 end
