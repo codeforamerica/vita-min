c8722e6904ad29dbd5f98a1bc8a0a2044fda3200
Travis Grathwell <tgrathwell@codeforamerica.org>
2022-10-26 16:30:41 -0700

    Clean up more rails-erb-loader files

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,8 +1,5 @@
 const { environment } = require('@rails/webpacker')
-const erb = require('./loaders/erb')
 const webpack = require('webpack');
-
-environment.loaders.prepend('erb', erb)
 
 environment.plugins.prepend(
     'Provide',
