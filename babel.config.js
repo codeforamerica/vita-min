adb2034d73ac319407fb28ece25d1092594a62d6
Travis Grathwell <tgrathwell@codeforamerica.org>
2022-10-27 15:17:56 -0700

    webpacking: bump webpacker and change babel config to silence a deprecation
    Co-authored-by: Asheesh Laroia <alaroia@codeforamerica.org>

--- babel.config.js
+++ babel.config.js
@@ -42,12 +42,6 @@ module.exports = function(api) {
       isTestEnv && 'babel-plugin-dynamic-import-node',
       '@babel/plugin-transform-destructuring',
       [
-        '@babel/plugin-proposal-class-properties',
-        {
-          "loose": true
-        }
-      ],
-      [
         '@babel/plugin-proposal-object-rest-spread',
         {
           useBuiltIns: true
