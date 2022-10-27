d052d720eabf498aed236d90424c967f9f00e3ec
Travis Grathwell <tgrathwell@codeforamerica.org>
2022-10-27 11:05:14 -0700

    Hub: micro-optimize the 'assigned to' filter options
    Co-authored-by: Asheesh Laroia <alaroia@codeforamerica.org>

--- app/helpers/tax_return_assignable_users_helper.rb
+++ app/helpers/tax_return_assignable_users_helper.rb
@@ -1,9 +1,7 @@
 module TaxReturnAssignableUsersHelper
   def assignable_user_options(assignable_users)
     assignable_users.pluck(:id, :role_type, :name, Arel.sql('suspended_at IS NOT NULL as suspended')).map do |id, role_type, name, suspended|
-      name = suspended ? I18n.t("hub.suspended_user_name", name: name) : name
-      role = (role_type || "").gsub("Role", "").underscore.humanize.titlecase
-      ["#{name} (#{role})", id]
+      ["#{name}#{suspended ? " (Suspended)" : ""} (#{role_name_from_role_type(role_type)})", id]
     end
   end
 end
