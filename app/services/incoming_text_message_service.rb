64fde795e41a7edba04ebeaf85334f8434f89a9b
Jenny Heath <jheath@codeforamerica.org>
2022-11-04 17:24:46 -0700

    fix test
    Co-authored-by: Jenny Heath <jheath@codeforamerica.org>

--- app/services/incoming_text_message_service.rb
+++ app/services/incoming_text_message_service.rb
@@ -9,10 +9,9 @@ class IncomingTextMessageService
 
     client_count = clients.count
     if client_count.zero?
-      DatadogApi.increment("twilio.incoming_text_messages.client_not_found")
       SendOutgoingTextMessageWithoutClientJob.perform_later(phone_number: phone_number, body: AutomatedMessage::UnmonitoredReplies.new.sms_body)
+      DatadogApi.increment("twilio.incoming_text_messages.client_not_found")
       DatadogApi.increment("twilio.outgoing_text_messages.sent_replies_not_monitored")
-      return head :ok
     end
 
     event_name = client_count > 1 ? "client_found_multiple" : "client_found"
