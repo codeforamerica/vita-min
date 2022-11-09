030f42455be2c7fd84eda9b44b4fe81ee4720337
Asheesh Laroia <alaroia@codeforamerica.org>
2022-11-09 12:17:09 -0500

    Add tests for bulk signup messaging for CtcSignup records [#183711250]

--- spec/controllers/hub/signup_selections_controller_spec.rb
+++ spec/controllers/hub/signup_selections_controller_spec.rb
@@ -15,25 +15,55 @@ describe Hub::SignupSelectionsController do
       File.unlink(@filename)
     end
 
-    let(:signup1) { create(:signup) }
-    let(:signup2) { create(:signup) }
+    let(:csv_content) { <<~CSV
+      id
+      #{signup1.id}
+      #{signup2.id}
+      -906
+    CSV
+    }
 
     context "with valid params" do
-      let(:csv_content) { <<~CSV
-        id
-        #{signup1.id}
-        #{signup2.id}
-        -906
-      CSV
-      }
+      context "for GYR signups" do
+        let(:signup1) { create(:signup) }
+        let(:signup2) { create(:signup) }
 
-      it 'stores an array of CSV data' do
-        expect {
-          put :create, params: { signup_selection: { upload: fixture_file_upload(@filename), signup_type: :GYR } }
-        }.to change(SignupSelection, :count).by(1)
-        record = SignupSelection.last
-        expect(record.id_array).to match_array [signup1.id, signup2.id]
-        expect(record.filename).to eq File.basename(@filename)
+        let(:csv_content) { <<~CSV
+          id
+          #{signup1.id}
+          #{signup2.id}
+          -906
+        CSV
+        }
+
+        let(:signup_type) { "GYR" }
+
+        it 'stores an array of CSV data' do
+          expect {
+            put :create, params: { signup_selection: { upload: fixture_file_upload(@filename), signup_type: signup_type } }
+          }.to change(SignupSelection, :count).by(1)
+          record = SignupSelection.last
+          expect(record.id_array).to match_array [signup1.id, signup2.id]
+          expect(record.signup_type).to eq signup_type
+          expect(record.filename).to eq File.basename(@filename)
+        end
+      end
+
+      context "for GetCTC signups" do
+        let(:signup1) { create(:ctc_signup) }
+        let(:signup2) { create(:ctc_signup) }
+
+        let(:signup_type) { "GetCTC" }
+
+        it 'stores an array of CSV data' do
+          expect {
+            put :create, params: { signup_selection: { upload: fixture_file_upload(@filename), signup_type: signup_type } }
+          }.to change(SignupSelection, :count).by(1)
+          record = SignupSelection.last
+          expect(record.id_array).to match_array [signup1.id, signup2.id]
+          expect(record.signup_type).to eq signup_type
+          expect(record.filename).to eq File.basename(@filename)
+        end
       end
     end
 
--- spec/features/hub/bulk_message_signups_csv_spec.rb
+++ spec/features/hub/bulk_message_signups_csv_spec.rb
@@ -3,65 +3,125 @@ require "rails_helper"
 RSpec.describe "Uploading a CSV for bulk messaging of signups", active_job: true do
   let(:user) { create :admin_user, name: "Admin the First" }
   let!(:gyr_email_and_phone_signup) { create :signup }
-  let!(:ctc_email_and_phone_signup) { create :ctc_signup }
+  let!(:ctc_email_and_phone_signup) { create :ctc_signup, email_address: "ctcfriend@example.com", phone_number: "+15855551212" }
 
   before do
     login_as user
   end
 
-  around do |example|
-    @filename = Rails.root.join("tmp", "bulk-signups-message-test-#{SecureRandom.hex}.csv")
-    File.write(@filename, <<~CSV)
-      id
-      #{gyr_email_and_phone_signup.id}
-    CSV
-    example.run
-    File.unlink(@filename)
+  context "for GYR signups" do
+    around do |example|
+      @filename = Rails.root.join("tmp", "bulk-signups-message-test-#{SecureRandom.hex}.csv")
+      File.write(@filename, <<~CSV)
+        id
+        #{gyr_email_and_phone_signup.id}
+      CSV
+      example.run
+      File.unlink(@filename)
+    end
+
+    scenario "bulk messaging signups by CSV" do
+      visit hub_signup_selections_path
+
+      attach_file "Select file", @filename
+      choose 'GYR'
+      click_on "Upload"
+
+      expect(page).to have_content(File.basename(@filename))
+
+      click_on I18n.t("hub.bulk_message_csvs.bulk_message_csv.send_email")
+      expect(page).to have_text "Sending email to 1 signup record(s)"
+      fill_in "Message", with: "Naranja es la mejor"
+      fill_in "Subject", with: "my great email subject"
+      click_on "Submit"
+
+      perform_enqueued_jobs
+      visit current_path
+      expect(page).to have_text "Contacting 1 signups over email (1 pending, 0 failed, 0 succeeded)"
+      perform_enqueued_jobs # to finish sending
+
+      OutgoingMessageStatus.last.update(delivery_status: 'delivered') # pretend we got a webhook status update from mailgun
+      visit current_path
+      expect(page).to have_text "Done contacting 1 signups over email (0 failed, 1 succeeded)"
+
+      expect(OutgoingMessageStatus.count).to eq 1
+      expect(OutgoingMessageStatus.last.parent.email_address).to eq(gyr_email_and_phone_signup.email_address)
+      expect(BulkSignupMessage.last.subject).to eq 'my great email subject'
+
+      click_on I18n.t("hub.bulk_message_csvs.bulk_message_csv.send_text")
+      expect(page).to have_text "Sending sms to 1 signup record(s)"
+      fill_in "Message", with: "Naranja es la mejor"
+      click_on "Submit"
+
+      perform_enqueued_jobs
+      visit current_path
+      expect(page).to have_text "Contacting 1 signups over sms (1 pending, 0 failed, 0 succeeded)"
+      perform_enqueued_jobs # to finish sending
+
+      OutgoingMessageStatus.last.update_status_if_further('sent') # pretend we got a webhook status update from twilio
+      visit current_path
+      expect(page).to have_text "Done contacting 1 signups over sms (0 failed, 1 succeeded)"
+
+      expect(OutgoingMessageStatus.count).to eq 2
+      expect(OutgoingMessageStatus.last.parent.phone_number).to eq(gyr_email_and_phone_signup.phone_number)
+    end
   end
 
-  scenario "bulk messaging signups by CSV" do
-    visit hub_signup_selections_path
+  context "for GetCTC signups" do
+    around do |example|
+      @filename = Rails.root.join("tmp", "bulk-signups-message-getctc-test-#{SecureRandom.hex}.csv")
+      File.write(@filename, <<~CSV)
+        id
+        #{ctc_email_and_phone_signup.id}
+      CSV
+      example.run
+      File.unlink(@filename)
+    end
 
-    attach_file "Select file", @filename
-    choose 'GYR'
-    click_on "Upload"
+    scenario "bulk messaging signups by signup CSV" do
+      visit hub_signup_selections_path
 
-    expect(page).to have_content(File.basename(@filename))
+      attach_file "Select file", @filename
+      choose 'GetCTC'
+      click_on "Upload"
 
-    click_on I18n.t("hub.bulk_message_csvs.bulk_message_csv.send_email")
-    expect(page).to have_text "Sending email to 1 signup record(s)"
-    fill_in "Message", with: "Naranja es la mejor"
-    fill_in "Subject", with: "my great email subject"
-    click_on "Submit"
+      expect(page).to have_content(File.basename(@filename))
 
-    perform_enqueued_jobs
-    visit current_path
-    expect(page).to have_text "Contacting 1 signups over email (1 pending, 0 failed, 0 succeeded)"
-    perform_enqueued_jobs # to finish sending
+      click_on I18n.t("hub.bulk_message_csvs.bulk_message_csv.send_email")
+      expect(page).to have_text "Sending email to 1 signup record(s)"
+      fill_in "Message", with: "Naranja es la mejor"
+      fill_in "Subject", with: "my great email subject"
+      click_on "Submit"
 
-    OutgoingMessageStatus.last.update(delivery_status: 'delivered') # pretend we got a webhook status update from mailgun
-    visit current_path
-    expect(page).to have_text "Done contacting 1 signups over email (0 failed, 1 succeeded)"
+      perform_enqueued_jobs
+      visit current_path
+      expect(page).to have_text "Contacting 1 signups over email (1 pending, 0 failed, 0 succeeded)"
+      perform_enqueued_jobs # to finish sending
 
-    expect(OutgoingMessageStatus.count).to eq 1
-    expect(OutgoingMessageStatus.last.parent.email_address).to eq(gyr_email_and_phone_signup.email_address)
-    expect(BulkSignupMessage.last.subject).to eq 'my great email subject'
+      OutgoingMessageStatus.last.update(delivery_status: 'delivered') # pretend we got a webhook status update from mailgun
+      visit current_path
+      expect(page).to have_text "Done contacting 1 signups over email (0 failed, 1 succeeded)"
 
-    click_on I18n.t("hub.bulk_message_csvs.bulk_message_csv.send_text")
-    expect(page).to have_text "Sending sms to 1 signup record(s)"
-    fill_in "Message", with: "Naranja es la mejor"
-    click_on "Submit"
+      expect(OutgoingMessageStatus.count).to eq 1
+      expect(OutgoingMessageStatus.last.parent.email_address).to eq(ctc_email_and_phone_signup.email_address)
+      expect(BulkSignupMessage.last.subject).to eq 'my great email subject'
 
-    perform_enqueued_jobs
-    visit current_path
-    expect(page).to have_text "Contacting 1 signups over sms (1 pending, 0 failed, 0 succeeded)"
-    perform_enqueued_jobs # to finish sending
+      click_on I18n.t("hub.bulk_message_csvs.bulk_message_csv.send_text")
+      expect(page).to have_text "Sending sms to 1 signup record(s)"
+      fill_in "Message", with: "Naranja es la mejor"
+      click_on "Submit"
 
-    OutgoingMessageStatus.last.update_status_if_further('sent') # pretend we got a webhook status update from twilio
-    visit current_path
-    expect(page).to have_text "Done contacting 1 signups over sms (0 failed, 1 succeeded)"
+      perform_enqueued_jobs
+      visit current_path
+      expect(page).to have_text "Contacting 1 signups over sms (1 pending, 0 failed, 0 succeeded)"
+      perform_enqueued_jobs # to finish sending
 
-    expect(OutgoingMessageStatus.count).to eq 2
-    expect(OutgoingMessageStatus.last.parent.phone_number).to eq(gyr_email_and_phone_signup.phone_number)
+      OutgoingMessageStatus.last.update_status_if_further('sent') # pretend we got a webhook status update from twilio
+      visit current_path
+      expect(page).to have_text "Done contacting 1 signups over sms (0 failed, 1 succeeded)"
+
+      expect(OutgoingMessageStatus.count).to eq 2
+      expect(OutgoingMessageStatus.last.parent.phone_number).to eq(ctc_email_and_phone_signup.phone_number)
+    end
   end
 end
