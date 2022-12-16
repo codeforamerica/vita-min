d14cc2c945604495dc4a8f6e6e7516f8ab16d34d
Asheesh Laroia <alaroia@codeforamerica.org>
2022-12-16 11:02:13 -0500

    Remove unnecessary change
    Co-authored-by: Asheesh Laroia <alaroia@codeforamerica.org>

--- app/controllers/ctc/portal/messages_controller.rb
+++ app/controllers/ctc/portal/messages_controller.rb
@@ -1,3 +1,1 @@
-class Ctc::Portal::MessagesController < Portal::MessagesController
-  skip_before_action :redirect_unless_open_for_logged_in_clients
-end
+class Ctc::Portal::MessagesController < Portal::MessagesController; end
