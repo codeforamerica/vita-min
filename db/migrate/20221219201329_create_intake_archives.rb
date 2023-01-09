1e7b9669445ac8023ceb8ccb755f1bdd1a0290ac
Travis Grathwell <tgrathwell@codeforamerica.org>
2023-01-09 14:41:14 -0800

    Remove old column
    Co-authored-by: Whitney Schaefer <wschaefer@codeforamerica.org>

--- db/migrate/20221219201329_create_intake_archives.rb
+++ db/migrate/20221219201329_create_intake_archives.rb
@@ -1,8 +1,6 @@
 class CreateIntakeArchives < ActiveRecord::Migration[7.0]
   def change
-    create_table :intake_archives do |t|
-      # t.integer :needs_help_2017
-    end
+    create_table :intake_archives
 
     # Adding a foreign key blocks writes on both tables. There are not many writes on these tables during December 2022.
     safety_assured {
