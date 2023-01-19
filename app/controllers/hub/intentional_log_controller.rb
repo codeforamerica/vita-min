e8184762946858cc39723cd9ea36ea0e8eaef262
Travis Grathwell <tgrathwell@codeforamerica.org>
2023-01-19 11:08:21 -0800

    intentional logging page includes info too
    Co-authored-by: Whitney Schaefer <wschaefer@codeforamerica.org>

--- app/controllers/hub/intentional_log_controller.rb
+++ app/controllers/hub/intentional_log_controller.rb
@@ -5,6 +5,7 @@ module Hub
     layout "hub"
 
     def index
+      Rails.logger.info("This an intentional info log line.")
       Rails.logger.warn("This an intentional warning log line.")
       Rails.logger.error("This an intentional error log line.")
 
