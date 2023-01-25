7bd7bf1304a44b889da82487022d85942e5b1eed
Jenny Heath <jheath@codeforamerica.org>
2023-01-25 15:46:50 -0800

    wip lookin good
    Co-authored-by: Asheesh Laroia <alaroia@codeforamerica.org>

--- app/jobs/faq_csv_import_job.rb
+++ app/jobs/faq_csv_import_job.rb
@@ -1,33 +1,74 @@
 require 'csv'
 
 # faq_csv = File.read("../../Downloads/faq.csv")
-# result = FaqCsvImportJob.perform_now(faq_csv)
+# result = FaqCsvImportJob.perform_now(File.read("../../Downloads/faq.csv"))
 # File.open('/tmp/test_faq.yml', 'w') {|f| f.write result.to_yaml }
 class FaqCsvImportJob < ApplicationJob
   def perform(faq_csv_content)
+    [:en, :es].each do |locale|
+      filename = "config/locales/#{locale}.yml"
+      new_translations = self.class.updated_translations(
+        YAML.safe_load(File.read("config/locales/#{locale}.yml")).with_indifferent_access,
+        "#{locale}.views.public_pages.faq.question_groups",
+        self.class.parse(faq_csv_content, locale)
+      )
+      File.write(filename, YAML.dump(new_translations))
+    end
+  end
+
+  def self.updated_translations(initial_data, path, new_content)
+    hash_to_modify = initial_data.dig(*path.split(".").map(&:to_sym))
+
+    hash_to_modify.each do |question_group_key, question_group|
+      question_group.each do |question_key, _|
+        new_question_content = new_content[question_group_key][question_key]
+
+        if new_question_content == { unchanged: true }
+          next
+        elsif new_question_content.nil?
+          question_group.delete(question_key)
+        else
+          question_group[question_key] = new_question_content
+        end
+      end
+    end
+
+    initial_data
+  end
+
+  def self.parse(faq_csv_content, locale)
+    lang_suffix = "(#{locale.to_s.upcase})"
     io = StringIO.new(faq_csv_content)
     io.set_encoding_by_bom
     new_copy = {}
-    CSV.parse(io, headers: true).map do |row|
-      section_key = row["Section Key"]
-      answer_content = row["Answer (EN)"]
-      if answer_content.split("\n").length > 1 &&
-        !answer_content.include?("<p>")
-        answer_content = answer_content.split("\n").map { |content| "<p>" + content + "</p>" }.reject{ |line| line == "<p></p>" }.join
-      end
-      q_a = {
-        question: row["Question (EN)"].strip,
-        answer_html: answer_content.strip
-      }
+    CSV.parse(io, headers: true).each do |row|
+      section_key = row["Section Key"].to_sym
+      question_key = row["Question Key"].to_sym
+      answer_content = row["Answer #{lang_suffix}"]
+
+      # find or create section key
       if !new_copy[section_key].present?
         new_copy[section_key] = {
-          title: row["Section Name (EN)"]
+          title: row["Section Name #{lang_suffix}"]
         }
       end
-      new_copy[section_key][row["Question Key"]] = q_a
-      I18n::Tasks
+
+      new_copy[section_key][question_key] =
+        if row["Updated"] == "No"
+          {
+            unchanged: true
+          }
+        else
+          if answer_content.split("\n").length > 1
+            answer_content = answer_content.split("\n").map { |content| "<p>" + content + "</p>" }.reject { |line| line == "<p></p>" }.join
+          end
+          {
+            question: row["Question #{lang_suffix}"].strip,
+            answer_html: answer_content.strip
+          }
+        end
     end
 
-    return new_copy
+    new_copy
   end
 end
--- spec/jobs/faq_csv_import_job_spec.rb
+++ spec/jobs/faq_csv_import_job_spec.rb
@@ -4,58 +4,114 @@ describe FaqCsvImportJob do
   describe ".parse" do
     let(:en_csv) do
       <<~CSV
-        Section Key,Updated,Question Key,Question (EN),Answer (EN)
-        stimulus,No,will_there_be_another_stimulus_payment,ignored,ignored
-        stimulus,Yes,how_do_i_get_the_stimulus_payments,How do I get it,This is how
+        Section Key,Updated,Section Name (EN),Question Key,Question (EN),Answer (EN)
+        stimulus,No,Stimulus,will_there_be_another_stimulus_payment,ignored,ignored
+        stimulus,Yes,Stimulus,how_do_i_get_the_stimulus_payments,How do I get it,"This is how
+        To get it"
+      CSV
+    end
+
+    let(:es_csv) do
+      <<~CSV
+        Section Key,Updated,Section Name (ES),Question Key,Question (ES),Answer (ES)
+        stimulus,No,Stimulus,will_there_be_another_stimulus_payment,ignored,ignored
+        stimulus,Yes,Stimulus,how_do_i_get_the_stimulus_payments,¿How do I get it?,"Este es
+        como you get it"
       CSV
     end
 
     it "parses English properly" do
       expect(
-        described_class.parse(en_csv)
+        described_class.parse(en_csv, :en)
       ).to(
         eq(
-          { stimulus:
-              {
-                will_there_be_another_stimulus_payment: { unchanged: true },
-                how_do_i_get_the_stimulus_payments: {
-                  question: "How do I get it",
-                  answer_html: "<p>This is how</p>",
-                }
+          {
+            stimulus: {
+              title: "Stimulus",
+              will_there_be_another_stimulus_payment: { unchanged: true },
+              how_do_i_get_the_stimulus_payments: {
+                question: "How do I get it",
+                answer_html: "<p>This is how</p><p>To get it</p>",
               }
+            }
+          }
+        )
+      )
+    end
+
+    it "parses Spanish properly" do
+      expect(
+        described_class.parse(es_csv, :es)
+      ).to(
+        eq(
+          {
+            stimulus: {
+              title: "Stimulus",
+              will_there_be_another_stimulus_payment: { unchanged: true },
+              how_do_i_get_the_stimulus_payments: {
+                question: "¿How do I get it?",
+                answer_html: "<p>Este es</p><p>como you get it</p>",
+              }
+            }
           }
         )
       )
     end
   end
 
-  describe ".update_translations" do
-    it "returns new translation data with the provided data" do
-      expect(described_class.update_yaml(
-        base_key: "views.public_pages.faq.question_groups",
-        initial_data: {
-          stimulus: {
-            will_there_be_another_stimulus_payment: {
-              question: "Well?",
-              answer_html: "Nah",
-            },
-            how_many_stimulus_payments_were_there: {
-              question: "How many",
-              answer_html: "5",
-            }
+  describe ".updated_translations" do
+    let(:new_content) do
+      {
+        stimulus: {
+          will_there_be_another_stimulus_payment: { unchanged: true },
+          how_many_stimulus_payments_were_there: {
+            question: "how many??",
+            answer_html: "500"
           }
-        },
-        new_data: {
-          stimulus: {
-            will_there_be_another_stimulus_payment: { unchanged: true },
-            how_many_stimulus_payments_were_there: {
-              question: "how many??",
-              answer_html: "500"
+        }
+      }
+    end
+    let(:initial_data) do
+      {
+        faq: {
+          question_groups: {
+            stimulus: {
+              will_there_be_another_stimulus_payment: {
+                question: "Well?",
+                answer_html: "Nah",
+              },
+              how_many_stimulus_payments_were_there: {
+                question: "How many",
+                answer_html: "5",
+              },
+              what_about_a_question_we_remove: {
+                question: "trash this question?",
+                answer_html: "yup",
+              }
             }
           }
         }
-      ).to eq({
+      }
+    end
 
+    it "returns new translation data with the provided data" do
+      expect(
+        described_class.updated_translations(initial_data, "faq.question_groups", new_content)
+      ).to eq({
+                faq: {
+                  question_groups: {
+                    stimulus: {
+                      how_many_stimulus_payments_were_there: {
+                        question: "how many??",
+                        answer_html: "500",
+                      },
+                      will_there_be_another_stimulus_payment: {
+                        question: "Well?",
+                        answer_html: "Nah",
+                      },
+                    }
+                  }
+                }
               })
     end
   end
