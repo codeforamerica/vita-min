c5bec16ac19da292946dd5bb86cbf83cc55f525d
Travis Grathwell <tgrathwell@codeforamerica.org>
2023-01-31 09:31:08 -0800

    RemoveUnconsentedClientsJob skips clients with attached incoming_text_messages or documents
    Non-consenting client records can accumulate incoming text messages because the
    we attach incoming text messages to all clients regardless of whether they have
    `consented_to_service_at`.
    
    Co-authored-by: Asheesh Laroia <alaroia@codeforamerica.org>

--- app/jobs/remove_unconsented_clients_job.rb
+++ app/jobs/remove_unconsented_clients_job.rb
@@ -1,6 +1,13 @@
 class RemoveUnconsentedClientsJob < ApplicationJob
   def perform(created_before: 14.days.ago)
-    Client.where("clients.created_at < ?", created_before).joins(:intake).where(consented_to_service_at: nil).find_in_batches do |clients|
+    clients_to_remove = Client
+      .where("clients.created_at < ?", created_before)
+      .joins(:intake)
+      .where(consented_to_service_at: nil)
+      .where.missing(:incoming_text_messages)
+      .where.missing(:documents)
+
+    clients_to_remove.find_in_batches do |clients|
       ActiveRecord::Base.connection.execute(ApplicationRecord.sanitize_sql([<<~SQL, clients.pluck('id')]))
         INSERT INTO abandoned_pre_consent_intakes(id, client_id, created_at, updated_at, source)
           (SELECT id, client_id, created_at, updated_at, source from intakes WHERE client_id IN (?))
