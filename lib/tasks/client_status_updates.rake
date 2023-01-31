0458d939181bb5508bf41b5c81936b10ce72fe41
Jacky Alcine <117868292+jalcine-cfa@users.noreply.github.com>
2023-01-31 16:19:58 -0500

    Add Automated Message to Hub for Newcomers  #184300083 (#3367)
    * Restored f4a5eefb251a32f9478ff044b5b24bc8d9e7b597 with changes.
    
    Co-authored-by: Nishi Nelson <nnelson@codeforamerica.org>
    Co-authored-by: Jacky Alcine <jalcine@codeforamerica.org>
    
    * Fix naming of message
    
    Co-authored-by: Jacky Alcine <jalcine@codeforamerica.org>
    
    * Update tests, cron and adjust spec for task.
    
    Co-authored-by: Nishi Nelson <nnelson@codeforamerica.org>
    
    * Allow for messages to send if no 'SENT_AT_COLUMN' is provided
    
    Co-authored-by: Nishi Nelson <nnelson@codeforamerica.org>
    
    * Remove survey
    
    Co-authored-by: Nishi Nelson <nnelson@codeforamerica.org>
    
    * Reset changes to translations
    
    Co-authored-by: Nishi Nelson <nnelson@codeforamerica.org>
    
    * Update crontab to send messages + surveys
    
    Co-authored-by: Nishi Nelson <nnelson@codeforamerica.org>
    
    * Update spec to mirror main
    
    Co-authored-by: Nishi Nelson <nnelson@codeforamerica.org>
    
    * My main was out of date, oops
    
    Co-authored-by: Nishi Nelson <nnelson@codeforamerica.org>
    
    * Remove irrelevant tests, update crontab, use existing column for idempotency of job running
    
    Co-authored-by: Nishi Nelson <nnelson@codeforamerica.org>
    
    * Bring back file, oops
    
    Co-authored-by: Nishi Nelson <nnelson@codeforamerica.org>
    
    * Update locale with new copy, tests, and survey sender args
    
    Co-authored-by: Nishi Nelson <nnelson@codeforamerica.org>
    
    * Remove unneeded field
    
    Co-authored-by: Nishi Nelson <nnelson@codeforamerica.org>
    
    * Fix the spacing (plz)
    
    Co-authored-by: Nishi Nelson <nnelson@codeforamerica.org>
    
    * Ran 'i18n-tasks normalize'
    
    Co-authored-by: Nishi Nelson <nnelson@codeforamerica.org>
    
    * Bring down interval to 20 minutes
    
    * Adjust spec and tests
    
    ---------
    
    Co-authored-by: Nishi Nelson <nnelson@codeforamerica.org>

--- lib/tasks/client_status_updates.rake
+++ lib/tasks/client_status_updates.rake
@@ -1,7 +1,12 @@
-namespace :client_surveys do
+namespace :client_status_updates do
   desc 'send completion surveys to eligible clients'
   task send_completion_surveys: [:environment] do
     SurveyMessages::CtcExperienceSurvey.enqueue_surveys(Time.current)
     SurveyMessages::GyrCompletionSurvey.enqueue_surveys(Time.current)
+  end
+
+  desc 'send client in progress an automated message'
+  task send_client_in_progress_automated_messages: [:environment] do
+    AutomatedMessage::InProgress.enqueue_messages(Time.now)
   end
 end
---
+++ spec/models/automated_message/in_progress_spec.rb
@@ -0,0 +1,58 @@
+require "rails_helper"
+
+RSpec.describe AutomatedMessage::InProgress do
+  describe ".clients_to_message" do
+    let(:expected_send_time) { 31.minutes.from_now }
+    let(:status) { :intake_in_progress }
+    let(:in_progress_survey_sent_at) { nil }
+    let!(:client) do
+      (
+        create :gyr_tax_return,
+               status,
+               client:
+                 create(
+                   :client,
+                   in_progress_survey_sent_at: in_progress_survey_sent_at,
+                   intake: create(:intake, primary_consented_to_service: "yes")
+                 )
+      ).client
+    end
+
+    context "clients who should get the message" do
+      context "who has had tax returns in 'intake_in_progress' and has been created at least half an hour ago" do
+        context "who has not received a message before" do
+          it "includes the client" do
+            expect(described_class.clients_to_message(expected_send_time)).to include(client)
+          end
+        end
+        
+        context "who has recevied a message before" do
+          let(:in_progress_survey_sent_at) { 7.minutes.ago }
+          it "does not includes the client" do
+            expect(described_class.clients_to_message(expected_send_time)).not_to include(client)
+          end
+        end
+      end
+    end
+
+    context "clients who should not get the message" do
+      context "who has no tax returns in 'intake_in_progress'" do
+        let(:status) { :intake_ready }
+        it "does not includes the client" do
+          expect(described_class.clients_to_message(expected_send_time)).to be_empty
+        end
+      end
+
+      context "who has had tax returns in 'intake_in_progress' and has been created at most half an hour ago" do
+        let(:expected_send_time) { 5.minutes.from_now }
+        context "who has received a message before" do
+          let(:in_progress_survey_sent_at) { 1.minute.ago }
+
+          it "does not includes the client" do
+            expect(described_class.clients_to_message(expected_send_time)).to be_empty
+          end
+        end
+      end
+    end
+  end
+end
