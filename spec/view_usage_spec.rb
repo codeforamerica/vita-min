6ae8e1c31c2786a7450df7081123320450daf14b
Whitney Schaefer <wschaefer@codeforamerica.org>
2023-02-07 14:20:43 -0800

    Delete view files associated with controllers deleted in c27dcbac9b963f22ae03c176cf22a0288e879cb4
    and one method that was only used in those views
    
    and move an i18n key associated with the ctc bank account page title that was in the wrong place
    
    and add a test to see if there are any view files with no associated controller
    
    if you're looking at this commit in the future and that test is causing you
    false positives, we're sorry
    
    Co-authored-by: Travis Grathwell <tgrathwell@codeforamerica.org>

---
+++ spec/view_usage_spec.rb
@@ -0,0 +1,23 @@
+RSpec.describe "view usage" do
+  it "should have an associated controller for any view with a resourcey name" do
+    view_paths = ActionView::ViewPaths.get_view_paths(ActionController::Base)
+    gyr_view_path = view_paths.select { |vp| vp.path == Rails.root.join('app', 'views').to_s }.first
+
+    # Sanity check these are the right paths to be fiddling with
+    expect(gyr_view_path.all_template_paths.select { |vp| vp.prefix == 'public_pages' }.first).to be
+
+    resourcey_names = ['edit', 'new', 'index']
+    missing_controllers = gyr_view_path.all_template_paths.select do |p|
+      resourcey_names.include?(p.name)
+    end.map do |path|
+      path.prefix.underscore.camelize + 'Controller'
+    end.select do |controller_name|
+      Object.const_get(controller_name)
+      false
+    rescue NameError
+      true
+    end
+
+    expect(missing_controllers).to be_empty, "Expected all views to have matching controller, these controllers were not found:\n#{missing_controllers.sort.join("\n")}"
+  end
+end
