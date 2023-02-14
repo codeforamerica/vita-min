fc53694e10578cef06398f64931495a3ecf7493b
Whitney Schaefer <wschaefer@codeforamerica.org>
2023-02-14 14:14:41 -0800

    Limit hub assigned to list of users to only active users
    Co-authored-by: Travis Grathwell <tgrathwell@codeforamerica.org>

--- app/helpers/tax_return_assignable_users_helper.rb
+++ app/helpers/tax_return_assignable_users_helper.rb
@@ -1,7 +1,7 @@
 module TaxReturnAssignableUsersHelper
   def assignable_user_options(assignable_users)
-    assignable_users.pluck(:id, :role_type, :name, Arel.sql('suspended_at IS NOT NULL as suspended')).map do |id, role_type, name, suspended|
-      ["#{name}#{suspended ? " (Suspended)" : ""} (#{role_name_from_role_type(role_type)})", id]
+    assignable_users.pluck(:id, :role_type, :name).map do |id, role_type, name|
+      ["#{name} (#{role_name_from_role_type(role_type)})", id]
     end
   end
 end
