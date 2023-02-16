4624ead418576e48b6dd2c0e1d681d0a55fb5bca
Em Barnard-Shao <ebarnard@codeforamerica.org>
2023-02-16 16:47:11 -0500

    bug

--- config/initializers/experiments.rb
+++ config/initializers/experiments.rb
@@ -1,5 +1,11 @@
-Rails.application.reloader.to_prepare do
-  if ActiveRecord::Base.connection && ActiveRecord::Base.connection.table_exists?(:experiments)
-    ExperimentService.ensure_experiments_exist_in_database
+begin
+  ActiveRecord::Base.connection
+rescue
+  exit 1
+else
+  Rails.application.reloader.to_prepare do
+    if ActiveRecord::Base.connection.table_exists?(:experiments)
+      ExperimentService.ensure_experiments_exist_in_database
+    end
   end
 end
