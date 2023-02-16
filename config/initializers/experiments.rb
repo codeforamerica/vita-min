688a3d44f100d50742a4a0fc53205632abb3ea23
Em Barnard-Shao <ebarnard@codeforamerica.org>
2023-02-16 16:32:56 -0500

    Check if db exists

--- config/initializers/experiments.rb
+++ config/initializers/experiments.rb
@@ -1,5 +1,5 @@
 Rails.application.reloader.to_prepare do
-  if ActiveRecord::Base.connection.table_exists?(:experiments)
+  if ActiveRecord::Base.connection && ActiveRecord::Base.connection.table_exists?(:experiments)
     ExperimentService.ensure_experiments_exist_in_database
   end
 end
