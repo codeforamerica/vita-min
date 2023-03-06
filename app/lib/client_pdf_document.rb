15775e2c1b02b0969344bb19d4f246e52eb5a5d1
Em Barnard-Shao <ebarnard@codeforamerica.org>
2023-03-06 16:31:52 -0500

    Do not create new F13614C if they have the old name

--- app/lib/client_pdf_document.rb
+++ app/lib/client_pdf_document.rb
@@ -2,7 +2,13 @@ class ClientPdfDocument
   def self.create_or_update(output_file:, document_type: , client:, filename:, tax_return: nil)
     tempfile = output_file
     tempfile.seek(0)
-    document = client.documents.find_or_initialize_by(document_type: document_type.key)
+    # TODO: after no document_types of "F13614C 2020" change back to => document = client.documents.find_or_initialize_by(document_type: document_type.key)
+    document =
+      if document_type == DocumentTypes::Form13614C && (client.documents.find_by(document_type: document_type.key) || client.documents.find_by(document_type: "F13614C 2020"))
+        client.documents.find_by(document_type: document_type.key) || client.documents.find_by(document_type: "F13614C 2020")
+      else
+        client.documents.find_or_initialize_by(document_type: document_type.key)
+      end
     document.update!(
       document_type: document_type.key,
       display_name: filename,
