4855604ab6dbadc9cc04dfe05957d783eac4e0d5
Asheesh Laroia <alaroia@codeforamerica.org>
2023-04-06 19:07:59 -0400

    Simplify migration and add test
    Co-authored-by: Jenny Heath <jheath@codeforamerica.org>

--- db/data/20230405203123_correct_demographic_data_on_intake.rb
+++ db/data/20230405203123_correct_demographic_data_on_intake.rb
@@ -2,6 +2,7 @@
 
 class CorrectDemographicDataOnIntake < ActiveRecord::Migration[7.0]
   def up
+    # Only about 800 matching records in prod, no big need for batches
     intakes_with_incorrect_primary_data = Intake::GyrIntake.where(
       demographic_primary_american_indian_alaska_native: true,
       demographic_primary_asian: true,
@@ -10,6 +11,17 @@ class CorrectDemographicDataOnIntake < ActiveRecord::Migration[7.0]
       demographic_primary_white: true,
       demographic_primary_prefer_not_to_answer_race: true,
     )
+    intakes_with_incorrect_primary_data.update_all(
+      {
+        demographic_primary_american_indian_alaska_native: nil,
+        demographic_primary_asian: nil,
+        demographic_primary_black_african_american: nil,
+        demographic_primary_native_hawaiian_pacific_islander: nil,
+        demographic_primary_white: nil,
+        demographic_primary_prefer_not_to_answer_race: nil,
+      }
+    )
+
     intakes_with_incorrect_spouse_data = Intake::GyrIntake.where(
       demographic_spouse_american_indian_alaska_native: true,
       demographic_spouse_asian: true,
@@ -18,32 +30,16 @@ class CorrectDemographicDataOnIntake < ActiveRecord::Migration[7.0]
       demographic_spouse_white: true,
       demographic_spouse_prefer_not_to_answer_race: true,
     )
-
-    intakes_to_backfill = intakes_with_incorrect_primary_data.or(intakes_with_incorrect_spouse_data)
-    intakes_to_backfill.find_in_batches do |batch|
-      batch.map do |intake|
-        if intakes_with_incorrect_primary_data.include?(intake)
-          intake.update(
-            demographic_primary_american_indian_alaska_native: nil,
-            demographic_primary_asian: nil,
-            demographic_primary_black_african_american: nil,
-            demographic_primary_native_hawaiian_pacific_islander: nil,
-            demographic_primary_white: nil,
-            demographic_primary_prefer_not_to_answer_race: nil,
-          )
-        end
-        if intakes_with_incorrect_spouse_data.include?(intake)
-          intake.update(
-            demographic_spouse_american_indian_alaska_native: nil,
-            demographic_spouse_asian: nil,
-            demographic_spouse_black_african_american: nil,
-            demographic_spouse_native_hawaiian_pacific_islander: nil,
-            demographic_spouse_white: nil,
-            demographic_spouse_prefer_not_to_answer_race: nil,
-          )
-        end
-      end
-    end
+    intakes_with_incorrect_spouse_data.update_all(
+      {
+        demographic_spouse_american_indian_alaska_native: nil,
+        demographic_spouse_asian: nil,
+        demographic_spouse_black_african_american: nil,
+        demographic_spouse_native_hawaiian_pacific_islander: nil,
+        demographic_spouse_white: nil,
+        demographic_spouse_prefer_not_to_answer_race: nil,
+      }
+    )
   end
 
   def down
