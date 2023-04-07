06c529e21ba1544aa20e505af642f99c12d736d6
Jacky Alcine <jalcine@codeforamerica.org>
2023-04-07 17:11:35 -0400

    Update formatting of spec, remove unneeded spec
    Co-authored-by: Nishi Nelson <nnelson@codeforamerica.org>
    Co-authored-by: Asheesh Laroia <alaroia@codeforamerica.org>

--- spec/validators/password_strength_validator_spec.rb
+++ spec/validators/password_strength_validator_spec.rb
@@ -17,17 +17,23 @@ describe PasswordStrengthValidator do
   subject { @validatable.new }
 
   describe "#validate_each" do
-    context "with an invalid password" do
-      it "is not valid" do
-        subject.password = "InvalidPha"
-        expect(subject).not_to be_valid
-        expect(subject.errors[:password]).to include(I18n.t("errors.attributes.password.insecure"))
+    context "with a non-admin user" do
+      context "with a valid password" do
+        before do
+          subject.password = "Strong_Passphrase3"
+        end
+
+        it "is valid" do
+          expect(subject).to be_valid
+        end
       end
 
-      it "is not valid if it uses a commonly used password" do
-        subject.password = "password123"
-        expect(subject).not_to be_valid
-        expect(subject.errors[:password]).to include(I18n.t("errors.attributes.password.insecure"))
+      context "with an invalid password" do
+        it "is not valid if it uses a commonly used password" do
+          subject.password = "password123"
+          expect(subject).not_to be_valid
+          expect(subject.errors[:password]).to include(I18n.t("errors.attributes.password.insecure"))
+        end
       end
     end
 
@@ -40,16 +46,6 @@ describe PasswordStrengthValidator do
         subject.password = "forceV"
         expect(subject).to be_valid
         expect(subject.errors[:password].length).to eq(0)
-      end
-    end
-
-    context "with a valid password" do
-      before do
-        subject.password = "Strong_Passphrase3"
-      end
-
-      it "is valid" do
-        expect(subject).to be_valid
       end
     end
   end
