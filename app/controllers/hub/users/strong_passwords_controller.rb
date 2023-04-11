a8e18ebffdcda65b1e02db4bc7bafafde19fe52a
Asheesh Laroia <alaroia@codeforamerica.org>
2023-04-11 11:37:37 -0400

    Shrink lines of code in StrongPasswordsController
    Co-authored-by: Asheesh Laroia <alaroia@codeforamerica.org>

--- app/controllers/hub/users/strong_passwords_controller.rb
+++ app/controllers/hub/users/strong_passwords_controller.rb
@@ -1,26 +1,21 @@
 module Hub
   module Users
     class StrongPasswordsController < ApplicationController
-
-      before_action :redirect_if_signed_out
-      before_action :redirect_if_is_admin_user
-      before_action :redirect_if_password_is_already_strong
+      before_action :redirect_away_if_needed
 
       layout "hub"
 
-      def edit
-        @user = current_user
-      end
+      def edit; end
 
       def update
-        @user = current_user
-        @user.assign_attributes(user_params)
-        @user.high_quality_password_as_of = DateTime.now
+        user = current_user
+        user.assign_attributes(user_params)
+        user.high_quality_password_as_of = DateTime.now
 
-        if @user.save
-          bypass_sign_in(@user) # Devise signs out after a password change, don't do that
-          @user.after_database_authentication
-          return redirect_to after_sign_in_path_for(@user)
+        if user.save
+          bypass_sign_in(user) # Devise signs out after a password change, don't do that
+          user.after_database_authentication
+          return redirect_to after_sign_in_path_for(user)
         end
 
         render :edit
@@ -29,21 +24,14 @@ module Hub
       private
 
       def user_params
-        params.require(:user).permit(:password, :password_confirmation)
+        params.require(:user).permit(:password, :password_confirmation).merge(high_quality_password_as_of: DateTime.now)
       end
 
-      def redirect_if_is_admin_user
-        redirect_to after_sign_in_path_for(current_user) if current_user.present? and current_user.admin?
-      end
+      private
 
-      def redirect_if_password_is_already_strong
-        redirect_to after_sign_in_path_for(current_user) if current_user.high_quality_password_as_of.present?
-      end
-
-      def redirect_if_signed_out
-        if current_user.blank?
-          redirect_to new_user_session_path
-        end
+      def redirect_away_if_needed
+        return redirect_to new_user_session_path if current_user.nil?
+        return redirect_to after_sign_in_path_for(current_user) if (current_user.high_quality_password_as_of.present? || current_user.admin?)
       end
     end
   end
