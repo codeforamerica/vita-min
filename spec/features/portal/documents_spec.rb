74a71f9bf25e15bd5102cfd8a69eaf23b93f9119
Jenny Heath <jheath@codeforamerica.org>
2023-04-12 17:14:48 -0700

    WIP add other doc type dropdown to upload page
    Co-authored-by: Whitney Schaefer <wschaefer@codeforamerica.org>

--- spec/features/portal/documents_spec.rb
+++ spec/features/portal/documents_spec.rb
@@ -49,5 +49,24 @@ RSpec.feature "a client on their portal" do
     within '#final-tax-return-docs' do
       expect(page).to have_content "2022 Final Tax Document"
     end
+
+    within "#other-docs" do
+      expect(page).to have_content "Other docs"
+      click_on "add"
+    end
+
+    expect(page).to have_content "Add a document"
+    # TODO find out list of common "Other" docs
+    select "TODO", from: "Document type"
+    upload_file("portal_document_upload_form[upload]", Rails.root.join("spec", "fixtures", "files", "test-pattern.png"))
+    expect(page).to have_content "test-pattern.png"
+    click_on 'Continue'
+
+    expect(page).to have_content "Here's a list of your documents"
+    within '#other-docs' do
+      expect(page).to have_content "test-pattern.png"
+    end
+
+    expect(client.documents.reload.last.document_type).to eq "TODO"
   end
 end
