56a595536f0aeda22590b190a0d7cf88a6b05d43
Travis Grathwell <tgrathwell@codeforamerica.org>
2023-04-17 10:16:12 -0700

    Fix flow explorer screenshot crash on DIY page
    needed to wait for the window to open before running enqueued jobs,
    and make sure the host on a link was set correctly
    
    make the fsa_link `example.com` in test so we aren't visiting a third-party
    website every testrun

--- spec/support/locale_helper.rb
+++ spec/support/locale_helper.rb
@@ -6,6 +6,10 @@ end
 
 class ActionDispatch::Routing::RouteSet
   def default_url_options(_options = {})
-    { locale: I18n.default_locale, host: "test.host" }
+    if Capybara.current_driver == Capybara.javascript_driver
+      { locale: I18n.default_locale, host: "#{Capybara.server_host}:#{Capybara.server_port}" }
+    else
+      { locale: I18n.default_locale, host: "test.host" }
+    end
   end
 end
