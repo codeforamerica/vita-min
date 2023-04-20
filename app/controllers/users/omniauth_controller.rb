a3106b54392c4f293fcb03a87058aba05bad3bc3
Asheesh Laroia <alaroia@codeforamerica.org>
2023-04-20 12:44:20 -0400

    Fix eager load issue
    Co-authored-by: Asheesh Laroia <alaroia@codeforamerica.org>

--- app/controllers/users/omniauth_controller.rb
+++ app/controllers/users/omniauth_controller.rb
@@ -1,4 +1,4 @@
-class OmniauthController < ApplicationController
+class Users::OmniauthController < ApplicationController
   def localized
     # Saves the current locale in the session and redirect to the unscoped path as before b/c devise callbacks cannot deal with dynamic routes
     session[:omniauth_login_locale] = I18n.locale
