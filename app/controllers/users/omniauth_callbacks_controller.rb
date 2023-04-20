ce7eb00e970d27e1295c4a92361398bf589fc45e
Asheesh Laroia <alaroia@codeforamerica.org>
2023-04-20 13:02:04 -0400

    Follow Em's advice that .present? is good enough
    Co-authored-by: Asheesh Laroia <alaroia@codeforamerica.org>

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -6,8 +6,8 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def handle_auth(kind)
     @user = User.from_omniauth(request.env['omniauth.auth'])
 
-    if @user.persisted?
-      flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: kind
+    if @user.present?
+      flash[:notice] = I18n.t('devise.omniauth_callbacks.success', kind: kind)
       sign_in_and_redirect @user, event: :authentication
     else
       flash[:alert] = I18n.t('devise.omniauth_callbacks.failure', kind: kind)
