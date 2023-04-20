01496dc4812944b54417a2def04f2d6a6f5f155b
Jenny Heath <jheath@codeforamerica.org>
2023-04-20 11:54:06 -0700

    Fix date in banner: should be end of docs not end of login
    Co-authored-by: Nishi Nelson <nnelson@codeforamerica.org>

--- spec/features/visit_home_page_spec.rb
+++ spec/features/visit_home_page_spec.rb
@@ -20,6 +20,43 @@ RSpec.feature "Visit home page" do
     expect(page).to have_text I18n.t('views.questions.welcome.title')
   end
 
+  context "shows the correct date-dependent banners" do
+    xcontext "when closed for filing" do
+      scenario "shows the closed banner" do; end
+    end
+
+    xcontext "when open for filing and before the tax deadline" do
+      scenario "shows the document deadline banner?" do; end
+    end
+
+    context "when open for filing and after the deadline" do
+      around do |example|
+        Timecop.freeze(DateTime.new(2023, 4, 20))
+        example.run
+        Timecop.return
+      end
+
+      before do
+        allow(Rails.configuration).to receive(:start_of_open_intake).and_return(DateTime.new(2023, 1, 31))
+        allow(Rails.configuration).to receive(:tax_deadline).and_return(DateTime.new(2023, 4, 18))
+        allow(Rails.configuration).to receive(:end_of_intake).and_return(DateTime.new(2023, 10, 1))
+        allow(Rails.configuration).to receive(:end_of_login).and_return(DateTime.new(2023, 10, 15))
+      end
+
+      scenario "shows the banner with closing date and document submission deadline" do
+        visit "/"
+
+        expect(page).to have_text "Get started with GetYourRefund by October 1st if you want to file with us in 2023. If your return is in progress, log in and submit your documents by October 8th."
+      end
+
+      scenario "shows the banner with closing date and document submission deadline with correctly formatted spanish dates" do
+        visit "/es"
+
+        expect(page).to have_text "Comience con GetYourRefund antes del 1 de octubre si desea presentar su declaración con nosotros en 2023. Si su declaración está en curso, inicie sesión y envíe sus documentos antes del 8 de octubre."
+      end
+    end
+  end
+
   scenario "it has the correct SEO link tags in English" do
     visit "/en?source=test"
     # We are currently viewing the English homepage so the canonical URL is the English version.
