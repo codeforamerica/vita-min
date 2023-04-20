b597125f7b3422f78ecd8b8044b7aec948d31225
Whitney Schaefer <wschaefer@codeforamerica.org>
2023-04-20 16:41:46 -0700

    more states

--- app/helpers/tax_return_card_helper.rb
+++ app/helpers/tax_return_card_helper.rb
@@ -17,9 +17,10 @@ module TaxReturnCardHelper
         button_type: :complete_intake_documents,
         call_to_action_text: t('portal.portal.home.calls_to_action.add_missing_documents')
       }
-    elsif state == :file_hold
+    elsif [:file_hold, :file_fraud_hold].include?(state)
       {
         help_text: t("portal.portal.home.waiting_state.tax_return.file_hold")
+        # TODO: percentage and CTA based on previous state
       }
     elsif state == :file_not_filing
       {
@@ -35,18 +36,36 @@ module TaxReturnCardHelper
       }
     elsif [:file_efiled, :file_mailed].include?(state)
       {
-
+        help_text: t("portal.portal.home.help_text.filing"),
+        percent_complete: 95,
+        button_type: :view_documents,
       }
-    elsif [:intake_info_requested, :prep_info_requested, :review_info_requested].include?(state)
+    elsif [:file_needs_review, :file_ready_to_file].include?(state)
+      {
+        help_text: t("portal.portal.home.help_text.filing"),
+        percent_complete: 90,
+        button_type: :view_documents,
+      }
+    elsif state == :review_signature_requested && tax_return.ready_for_8879_signature?(TaxReturn::PRIMARY_SIGNATURE)
+      {
+        help_text: t("portal.portal.home.progress_state.tax_return.review_signature_requested_primary"),
+        percent_complete: 90,
+        button_type: :add_signature_primary,
+        call_to_action_text: t("portal.portal.home.calls_to_action.add_signature_primary")
+      }
+    elsif state == :review_signature_requested && tax_return.ready_for_8879_signature?(TaxReturn::SPOUSE_SIGNATURE)
+      {
+        help_text: t("portal.portal.home.progress_state.tax_return.review_signature_requested_spouse"),
+        percent_complete: 90,
+        button_type: :add_signature_spouse,
+        call_to_action_text: t("portal.portal.home.calls_to_action.add_signature_spouse")
+      }
+    elsif [:intake_needs_doc_help, :intake_info_requested, :prep_info_requested, :review_info_requested].include?(state)
       {
         help_text: t('portal.portal.home.help_text.info_requested'),
-        percent_complete: {intake_info_requested: 45, prep_info_requested: 65, review_info_requested: 85}[state],
+        percent_complete: {intake_needs_doc_help: 45, intake_info_requested: 45, prep_info_requested: 65, review_info_requested: 85}[state],
         button_type: :add_missing_documents,
         call_to_action_text: t('portal.portal.home.calls_to_action.add_missing_documents')
-      }
-    elsif state_index >= TaxReturnStateMachine.states.index("review_signature_requested")
-      {
-        help_text: t("portal.portal.home.progress_state.tax_return.completed_qr", year: tax_return.year),
       }
     elsif state == :review_ready_for_call
       {
