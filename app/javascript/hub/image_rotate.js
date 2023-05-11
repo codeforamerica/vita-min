0a5bdb45433fa8c4a483bc8385ce5ebab4d4a369
Nishi Nelson <nnelson@codeforamerica.org>
2023-05-10 17:40:30 -0700

    Image now rotates. WIP

---
+++ app/javascript/hub/image_rotate.js
@@ -0,0 +1,38 @@
+// import $ from 'jquery';
+//
+// $(document).ready(function() {
+//     $('#rotate-button').click(function(e) {
+//         e.preventDefault();
+//
+//         // Get the image element
+//         let image = $(this).data('image');
+//
+//         // Increment the rotation angle by 90 degrees
+//         let rotation = (parseFloat(image.dataset.rotation) || 0) + 90;
+//
+//         // Apply the rotation using CSS transform
+//         image.style.transform = 'rotate(' + rotation + 'deg)';
+//
+//         // Update the rotation angle in the dataset attribute
+//         image.dataset.rotation = rotation;
+//     });
+// });
+export default function imageRotate() {
+    const rotateButton = document.getElementById("rotate-button");
+
+    rotateButton.addEventListener("click", function(e) {
+        e.preventDefault();
+        // Get the image element
+        const image = document.getElementById("image");
+
+        // Increment the rotation angle by 90 degrees
+        let rotation = parseFloat(image.dataset.rotation) || 0;
+        rotation += 90;
+
+        // Apply the rotation using CSS transform
+        image.style.transform = `rotate(${rotation}deg)`;
+
+        // Update the rotation angle in the dataset attribute
+        image.dataset.rotation = rotation;
+    });
+};
--- app/javascript/packs/hub.js
+++ app/javascript/packs/hub.js
@@ -1,6 +1,8 @@
 // See `app/javascript/hub/README.md`
 import MainMenuComponent from "../hub/MainMenuComponent";
+import imageRotate from "../hub/image_rotate";
 
 window.addEventListener("load", function() {
   MainMenuComponent();
+  imageRotate();
 })
