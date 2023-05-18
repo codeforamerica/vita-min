84423859a62fad8693a6baeb66d794f0fcc9b684
Nishi Nelson <73559203+nishinelson@users.noreply.github.com>
2023-05-18 09:43:23 -0700

    Add spouse doc handling copy to doc overview in client portal and intake flow for joint filers (#3540)
    Co-authored-by: Nishi Nelson <nnelson@codeforamerica.org>

--- spec/features/portal/documents_spec.rb
+++ spec/features/portal/documents_spec.rb
@@ -1,53 +1,73 @@
 require "rails_helper"
 
 RSpec.feature "a client on their portal" do
-  let(:tax_return) { create(:tax_return, :intake_ready, year: 2022) }
-  let(:client) do
-    create :client,
-           intake: (create :intake, preferred_name: "Randall", completed_at: 10.minutes.ago),
-           tax_returns: [tax_return]
-  end
-
-  before do
-    create :document, client: client, document_type: DocumentTypes::Identity.key, upload_path: Rails.root.join("spec", "fixtures", "files", "picture_id.jpg")
-    create :document, client: client, tax_return: tax_return, document_type: DocumentTypes::FinalTaxDocument.key, upload_path: Rails.root.join("spec", "fixtures", "files", "picture_id.jpg")
-    login_as client, scope: :client
-  end
-
-  scenario "linking to next step" do
-    visit portal_root_path
-
-    click_on I18n.t('portal.portal.home.document_link.view_documents')
-
-    expect(page).to have_content "Here's a list of your documents"
-
-    within '#id-docs' do
-      expect(page).to have_content "Photo ID"
-      expect(page).to have_content "picture_id.jpg"
-      expect(page).to have_link "add"
+  context "joint filer" do
+    let(:tax_return) { create(:tax_return, :intake_ready, year: 2022) }
+    let(:client) do
+      create :client,
+             intake: (create :intake, preferred_name: "Randall", completed_at: 10.minutes.ago, filing_joint: "yes"),
+             tax_returns: [tax_return]
     end
 
-    within '#selfie-docs' do
-      expect(page).to have_content "Photo Holding ID"
-      expect(page).to have_content "Please add document."
-      click_on "add"
+    before do
+      create :document, client: client, document_type: DocumentTypes::Identity.key, upload_path: Rails.root.join("spec", "fixtures", "files", "picture_id.jpg")
+      create :document, client: client, tax_return: tax_return, document_type: DocumentTypes::FinalTaxDocument.key, upload_path: Rails.root.join("spec", "fixtures", "files", "picture_id.jpg")
+      login_as client, scope: :client
     end
 
-    expect(page).to have_content "Add a document"
-    upload_file("portal_document_upload_form[upload]", Rails.root.join("spec", "fixtures", "files", "test-pattern.png"))
+    scenario "linking to next step" do
+      visit portal_root_path
 
-    expect(page).to have_content "test-pattern.png"
-    click_on 'Continue'
+      click_on I18n.t('portal.portal.home.document_link.view_documents')
 
-    expect(page).to have_content "Here's a list of your documents"
+      expect(page).to have_content "Here's a list of your documents"
 
-    within '#selfie-docs' do
-      expect(page).to have_content "Photo Holding ID"
+      within '#id-docs' do
+        expect(page).to have_content "Photo ID"
+        expect(page).to have_content "picture_id.jpg"
+        expect(page).to have_link "add"
+        expect(page).to have_content "Please add documents for you and your spouse."
+        click_on "add"
+      end
+
+      expect(page).to have_content "Add a document"
+      upload_file("portal_document_upload_form[upload]", Rails.root.join("spec", "fixtures", "files", "combined-test-pdf.pdf"))
+
+      expect(page).to have_content "combined-test-pdf.pdf"
+      click_on 'Continue'
+
+      expect(page).to have_content "Here's a list of your documents"
+
+      within '#id-docs' do
+        expect(page).to have_content "Photo ID"
+        expect(page).to have_content "picture_id.jpg"
+        expect(page).to have_content "combined-test-pdf.pdf"
+        expect(page).not_to have_content "Please add documents for you and your spouse."
+      end
+
+      within '#selfie-docs' do
+        expect(page).to have_content "Photo Holding ID"
+        expect(page).to have_content "Please add document."
+        click_on "add"
+      end
+
+      expect(page).to have_content "Add a document"
+      upload_file("portal_document_upload_form[upload]", Rails.root.join("spec", "fixtures", "files", "test-pattern.png"))
+
       expect(page).to have_content "test-pattern.png"
-    end
+      click_on 'Continue'
 
-    within '#final-tax-return-docs' do
-      expect(page).to have_content "2022 Final Tax Document"
+      expect(page).to have_content "Here's a list of your documents"
+
+      within '#selfie-docs' do
+        expect(page).to have_content "Photo Holding ID"
+        expect(page).to have_content "test-pattern.png"
+        expect(page).to have_content "Please add documents for you and your spouse."
+      end
+
+      within '#final-tax-return-docs' do
+        expect(page).to have_content "2022 Final Tax Document"
+      end
     end
   end
 end
