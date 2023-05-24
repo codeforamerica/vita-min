728b2eae97a1ba10039f89275f98aa3a48a10358
Nishi Nelson <nnelson@codeforamerica.org>
2023-05-23 17:38:42 -0700

    Rotate functionality on frontend works with various types of images and edit page looks like figma -WIP
    Co-authored-by: Travis Grathwell <tgrathwell@codeforamerica.org>

--- app/javascript/hub/image_rotate.js
+++ app/javascript/hub/image_rotate.js
@@ -1,24 +1,3 @@
-// import $ from 'jquery';
-//
-// $(document).ready(function() {
-//     $('#rotate-button').click(function(e) {
-//         e.preventDefault();
-//
-//         // Get the image element
-//         let image = $(this).data('image');
-//
-//         // Increment the rotation angle by 90 degrees
-//         let rotation = (parseFloat(image.dataset.rotation) || 0) + 90;
-//
-//         // Apply the rotation using CSS transform
-//         image.style.transform = 'rotate(' + rotation + 'deg)';
-//
-//         // Update the rotation angle in the dataset attribute
-//         image.dataset.rotation = rotation;
-//     });
-// });
-import adjustImageSize from "./adjust-image-size";
-
 export default function imageRotate() {
     const rotateButton = document.getElementById("rotate-button");
 
@@ -27,38 +6,42 @@ export default function imageRotate() {
         // Get the image element
         const image = document.getElementById("image");
         const container = document.getElementById("image-container");
-        console.log(container.offsetHeight);
 
         // Increment the rotation angle by 90 degrees
         let rotation = parseFloat(image.dataset.rotation) || 0;
         rotation += 90;
 
-        image.style.transform = `rotate(${rotation}deg)`;
-
-        const containerOffset = 12;
+        function fitImageForRotation(image, container) {
+            if (image.naturalWidth < image.naturalHeight) {
+                let newHeight = container.offsetWidth < image.naturalHeight ? `${container.offsetWidth}px` : 'auto';
+                image.style.width = `auto`;
+                image.style.height = newHeight;
+            } else {
+                let newWidth = container.offsetHeight < image.naturalWidth ? `${container.offsetHeight}px` : 'auto';
+                image.style.width = newWidth;
+                image.style.height = `auto`;
+            }
+        }
 
         // // Apply the rotation using CSS transform
         if ((rotation % 360) == 90) {
             // For a 90 degree rotation, choose top left rotation corner, push left by the image's height (which becomes its width after rotation), rotate
-            const translateX = Math.round(container.offsetHeight / 2) + containerOffset;
-            image.style.transform = `translateX(${translateX}px) rotate(${rotation}deg)`;
+            fitImageForRotation(image, container);
+            image.style.transform = `translateX(${image.height}px) rotate(${rotation}deg)`;
             image.style.transformOrigin = `top left`;
-            image.style.width = `${container.offsetHeight}px`;
-            image.style.height = 'auto';
         } else if ((rotation % 360) == 270) {
-            // For a 270 degree rotation, choose top left rotation corner, push left by the image's height (which becomes its width after rotation), push down by the images width (which will become its height), rotate
-            image.style.transform = `translateY(${container.offsetHeight}px) rotate(${rotation}deg)`;
+            // For a 270 degree rotation, choose top left rotation corner, push down by the image's width (which becomes its height after rotation), rotate
+            fitImageForRotation(image, container);
+            image.style.transform = `translateY(${image.width}px) rotate(${rotation}deg)`;
             image.style.transformOrigin = `top left`;
-            image.style.width = `${container.offsetHeight}px`;
         } else {
             image.style.transform = `rotate(${rotation}deg)`;
             image.style.transformOrigin = `center`;
-            image.style.width = '100%';
+            image.style.width = 'auto';
+            image.style.height = 'auto';
         }
 
         // Update the rotation angle in the dataset attribute
         image.dataset.rotation = rotation;
-
-        adjustImageSize();
     });
 };
