d3646a56d9bed347fda0341dd27d410b33cd8c81
Nishi Nelson <nnelson@codeforamerica.org>
2023-06-08 14:01:09 -0700

    wip
    Co-authored-by: Travis Grathwell <tgrathwell@codeforamerica.org>

--- app/controllers/users/invitations_controller.rb
+++ app/controllers/users/invitations_controller.rb
@@ -70,13 +70,13 @@ class Users::InvitationsController < Devise::InvitationsController
       when AdminRole::TYPE
         AdminRole.new
       when SiteCoordinatorRole::TYPE
-        SiteCoordinatorRole.new(sites: @vita_partners.sites.where(id: JSON.parse(params.require(:sites)).pluck('id')))
+        SiteCoordinatorRole.new(sites: @vita_partners.sites.where(id: JSON.parse(params[:sites].presence || '[]').pluck('id')))
       when ClientSuccessRole::TYPE
         ClientSuccessRole.new
       when GreeterRole::TYPE
         GreeterRole.new
       when TeamMemberRole::TYPE
-        TeamMemberRole.new(sites: @vita_partners.sites.where(id: JSON.parse(params.require(:sites)).pluck('id')))
+        TeamMemberRole.new(sites: @vita_partners.sites.where(id: JSON.parse(params[:sites].presence || '[]').pluck('id')))
       end
 
     authorize!(:create, @role)
