1279b91e1ad283642eff31629401a6e2dd61a859
Jacky Alcine <jalcine@codeforamerica.org>
2023-06-13 14:27:42 -0400

    Add more potential logic flowing

--- app/jobs/detect_blur_in_document_job.rb
+++ app/jobs/detect_blur_in_document_job.rb
@@ -2,27 +2,28 @@
 
 class DetectBlurInDocumentJob < ApplicationJob
   def perform(document:)
-    # TODO: Obtain the path to the root JavaScript directory that'll handle this job.
-    # TODO: Open up the process and wait for it to complete.
-
     downloaded_document = Tempfile.create('blurcheck')
     File.write(document.upload.download)
 
     temp_file = Tempfile.new
     argv = ["node", full_path_to_script, downloaded_document.path]
-    pid = Process.spawn(*argv, chdir: javascript_working_directory, in: temp_file.path)
+    pid = Process.spawn(*argv, chdir: javascript_working_directory, in: "/dev/null", out: temp_file.path, err: :out)
     Process.wait(pid)
+
     raise Error.new("Process failed to exit?") unless $?.exited?
-    output = temp_file.path
+    output_body = JSON.parse(File.readlines(temp_file.path).join)
+    blurriness_score = output_body[:blur_score]
+
+    document.update(computed_blurriness: blurriness_score)
   end
 
   private
 
   def full_path_to_script
-    "blur_check.js"
+    File.expand_path(File.expand_path(File.dirname(__FILE__)) + "/vendor/opencv-blur/index.js")
   end
 
   def javascript_working_directory
-    File.dirname(FILE)
+    File.dirname(full_path_to_script, 2)
   end
 end
