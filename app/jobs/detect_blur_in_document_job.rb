af7bb4499c95a8c90a81594c0ba609e49b667288
Jacky Alcine <jalcine@codeforamerica.org>
2023-06-14 15:36:37 -0400

    Update job to use pycall versus spawning a new process
    Co-authored-by: Em Barnard-Shao <ebarnard@codeforamerica.org>

--- app/jobs/detect_blur_in_document_job.rb
+++ app/jobs/detect_blur_in_document_job.rb
@@ -3,34 +3,21 @@
 class DetectBlurInDocumentJob < ApplicationJob
   def perform(document:)
     # Download the file
-    downloaded_document = Tempfile.create('blurcheck-result')
-    downloaded_document.write(document.upload.download)
+    image_bytes = document.upload.download
+
+    downloaded_document = Tempfile.create('blurcheck-result', binmode: true)
+    downloaded_document.write(image_bytes)
     downloaded_document.close
 
-    shell_output_file = Tempfile.create("blurcheck-output")
-    argv = ["node", full_path_to_script, downloaded_document.path]
-    pid = Process.spawn(*argv, chdir: javascript_working_directory, in: "/dev/null", out: shell_output_file.path, err: :out)
-    Process.wait(pid)
+    cv = PyCall.import_module("cv2")
+    image = cv.imread(downloaded_document.path)
+    grayscale_image = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+    fm = cv.Laplacian(grayscale_image, cv.CV_64F).var()
 
-    raise Error.new("Process failed to exit?") unless $?.exited?
-    downloaded_document.close!
-
-    output_body = JSON.parse(File.readlines(shell_output_file.path).join)
-    shell_output_file.close!
-    blurriness_score = output_body[:blur_score]
+    binding.pry
 
     # Update the record
-    # NOTE: Do we need to notify anything/aynone?
-    document.update(computed_blurriness: blurriness_score)
-  end
-
-  private
-
-  def full_path_to_script
-    File.expand_path(File.expand_path(File.dirname(__FILE__)) + "/vendor/opencv-blur/index.js")
-  end
-
-  def javascript_working_directory
-    File.dirname(full_path_to_script, 2)
+    # NOTE: Do we need to notify anything/anyone?
+    document.update(computed_blurriness: fm)
   end
 end
