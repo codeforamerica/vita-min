1b580a81803825589035475ce206c13504adde8e
Jacky Alcine <jalcine@codeforamerica.org>
2023-06-23 12:59:02 -0400

    Add negative test for PDF documents
    Co-authored-by: Em Barnard-Shao <ebarnard@codeforamerica.org>

--- spec/jobs/detect_blur_in_document_job_spec.rb
+++ spec/jobs/detect_blur_in_document_job_spec.rb
@@ -11,5 +11,15 @@ RSpec.describe DetectBlurInDocumentJob do
         }.to change(document, :blur_score)
       end
     end
+
+    context "when provided a PDF document" do
+      let(:document) { create(:document, :pdf) }
+
+      it "does not update score on document" do
+        expect {
+          described_class.perform_now(document: document)
+        }.not_to change(document, :blur_score)
+      end
+    end
   end
 end
