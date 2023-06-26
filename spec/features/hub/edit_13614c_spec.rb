61cfe8120572722aed7a8a38a8ac95abd76973bd
Nishi Nelson <nnelson@codeforamerica.org>
2023-06-26 15:12:02 -0700

    Add a feature test to lock in the behavior of the demographic_questions_hub_edit bit
    Co-authored-by: Whitney Schaefer <wschaefer@codeforamerica.org>

--- spec/features/hub/edit_13614c_spec.rb
+++ spec/features/hub/edit_13614c_spec.rb
@@ -361,5 +361,42 @@ RSpec.describe "a user editing a clients 13614c form" do
       expect(intake.demographic_primary_ethnicity).to eq "not_hispanic_latino"
       expect(intake.demographic_spouse_ethnicity).to eq "hispanic_latino"
     end
+
+    describe "demographic questions on page 3" do
+      before do
+        client.intake.update(
+          demographic_questions_opt_in: 'no',
+          demographic_spouse_native_hawaiian_pacific_islander: true, # somehow exists in the db even though demographic opt in is false
+        )
+      end
+
+      it "does not write the answers to the PDF unless the client opted in during intake or the hub user has saved page3" do
+        # generate pdf, prove spouse ethnicity is not filled in because demographic_questions_opt_in is false
+        form_fields = PdfForms.new.get_fields(F13614cPdf.new(client.intake).output_file)
+        expect(form_fields.find { |field| field.name == "form1[0].page3[0].q13[0].native_hawaiian[0]" }.value).to eq("Off")
+        expect(form_fields.find { |field| field.name == "form1[0].page3[0].q13[0].black_african[0]" }.value).to eq("Off")
+
+        visit hub_client_path(id: client.id)
+        within ".client-profile" do
+          click_on "Edit 13614-C"
+        end
+
+        within '.form_13614c-page-links', match: :first do
+          click_on "3"
+        end
+
+        within ".spouse-demographic-race" do
+          uncheck "Native Hawaiian or other Pacific Islander"
+          check "Black or African American"
+        end
+
+        click_on I18n.t("general.save")
+
+        # generate pdf, prove spouse ethnicity is filled in because demographic_questions_hub_edit is true
+        form_fields = PdfForms.new.get_fields(F13614cPdf.new(client.reload.intake).output_file)
+        expect(form_fields.find { |field| field.name == "form1[0].page3[0].q13[0].native_hawaiian[0]" }.value).to eq("")
+        expect(form_fields.find { |field| field.name == "form1[0].page3[0].q13[0].black_african[0]" }.value).to eq("1")
+      end
+    end
   end
 end
