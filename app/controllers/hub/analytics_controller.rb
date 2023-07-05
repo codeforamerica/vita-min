f92f15f227443b758337114da0c740d04f46ebcd
Em Barnard-Shao <ebarnard@codeforamerica.org>
2023-07-05 17:22:18 -0400

    tests

--- app/controllers/hub/analytics_controller.rb
+++ app/controllers/hub/analytics_controller.rb
@@ -3,6 +3,7 @@ module Hub
     include AccessControllable
     before_action :require_sign_in
     load_and_authorize_resource parent: false
+    before_action :require_admin
     load_and_authorize_resource :client
     load_and_authorize_resource through: :client, only: [:create]
     load_and_authorize_resource :user, parent: false, only: [:index]
--- spec/controllers/hub/analytics_controller_spec.rb
+++ spec/controllers/hub/analytics_controller_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe Hub::AnalyticsController, type: :controller do
   describe "#index" do
     let(:client) { create :client }
     let(:params) { { client_id: client.id } }
+    it_behaves_like :a_get_action_for_authenticated_users_only, action: :index
     it_behaves_like :a_get_action_for_admins_only, action: :index
 
     context "as an admin user loading a client's analytics" do
