770e7a090eb4822207fddc753239b53d9f3f33ab
Em Barnard-Shao <ebarnard@codeforamerica.org>
2023-07-05 17:37:47 -0400

    Tests

--- spec/controllers/hub/security_controller_spec.rb
+++ spec/controllers/hub/security_controller_spec.rb
@@ -9,17 +9,29 @@ RSpec.describe Hub::SecurityController do
     let(:other_client) { other_intake.client }
     let(:other_intake) { create(:ctc_intake, preferred_name: "Evan Entake") }
     let!(:other_bank_account) {  create(:bank_account, routing_number: "123456789", account_number: "87654321", intake: other_intake) }
+    let(:user) { create :admin_user }
 
     render_views
 
     before do
-      sign_in create(:admin_user)
+      sign_in user
     end
 
-    it "shows security information including IDs of other clients with the same bank account" do
-      get :show, params: { id: client.id }
-      expect(response.body).to include(intake.preferred_name)
-      expect(assigns(:duplicate_bank_client_ids)).to eq([other_client.id])
+    context "as an authenticated admin" do
+      it "shows security information including IDs of other clients with the same bank account" do
+        get :show, params: { id: client.id }
+        expect(response.body).to include(intake.preferred_name)
+        expect(assigns(:duplicate_bank_client_ids)).to eq([other_client.id])
+      end
+    end
+
+    context "with some other role that does not have access to client" do
+      let!(:user) { create :team_member_user }
+
+      it "does not allow access to the page" do
+        get :show, params: { id: client.id }
+        expect(response.status).to eq 403
+      end
     end
 
     context "for an archived intake" do
@@ -34,10 +46,21 @@ RSpec.describe Hub::SecurityController do
       let!(:other_archived_intake) { create :archived_2021_ctc_intake, client: other_archived_client, preferred_name: "Aiden Archive" }
       let!(:other_archived_bank_account) {  create(:archived_2021_bank_account, routing_number: "123456789", account_number: "87654321", intake: other_archived_intake) }
 
-      it "shows security information including IDs of archived clients with the same bank account" do
-        get :show, params: { id: client.id }
-        expect(response.body).to include(archived_intake.preferred_name)
-        expect(assigns(:duplicate_bank_client_ids)).to eq([other_archived_client.id])
+      context "as an authenticated admin" do
+        it "shows security information including IDs of archived clients with the same bank account" do
+          get :show, params: { id: client.id }
+          expect(response.body).to include(archived_intake.preferred_name)
+          expect(assigns(:duplicate_bank_client_ids)).to eq([other_archived_client.id])
+        end
+      end
+
+      context "with some other role that does not have access to client" do
+        let!(:user) { create :team_member_user }
+
+        it "does not allow access to the page" do
+          get :show, params: { id: client.id }
+          expect(response.status).to eq 403
+        end
       end
     end
   end
