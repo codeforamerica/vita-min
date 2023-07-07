8f4ed0580e1ffbb95a46f93897b33e049bda49fa
Jacky Alcine <jalcine@codeforamerica.org>
2023-07-07 15:19:49 -0400

    Fixed the index test but create table one fails

--- spec/db/data/metabase_user_restrictions_spec.rb
+++ spec/db/data/metabase_user_restrictions_spec.rb
@@ -1,10 +1,8 @@
 require "rails_helper"
 
 describe "RestrictiveAccessForMetabaseUser" do
-  before do
+  before(:all) do
     Rails.application.load_tasks
-    Rake::Task['analytics:drop_views'].invoke
-    Rake::Task['analytics:create_views'].invoke
   end
 
   context "when attempting intentional behavior" do
@@ -20,16 +18,24 @@ select count(*) from analytics.team_role_members (vita_partner_id, id);
   end
 
   context "when attempting unintentional behavior" do
+    before do
+      Rake::Task['analytics:create_views'].invoke
+
+    end
     it "can't create new indicies" do
+      #Rake::Task['analytics:create_views'].invoke
+
       expect {
         ActiveRecord::Base.connection.execute("
 set role metabase;
-create index no_way_vita_idx on analytics.team_role_members (vita_partner_id, id);
+create index no_way_vita_idx on analytics.team_member_roles (vita_partner_id, id);
       ")
-      }.to raise_exception(ActiveRecord::StatementInvalid, /permission denied/)
+      }.to raise_exception(ActiveRecord::StatementInvalid, /must be owner of view/)
     end
 
     it "can't create new tables" do
+      Rake::Task['analytics:create_views'].invoke
+
       expect {
         ActiveRecord::Base.connection.execute("
 set role metabase;
