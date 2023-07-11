20707658fa4729e7bd71bf7b77873f54d3f7462e
Jacky Alcine <117868292+jalcine-cfa@users.noreply.github.com>
2023-07-11 16:06:57 -0400

    Restrict and Assert Permissions of the Metabase Database User (#3593)
    * Revoke all privs for metabase user by default
    
    * Add test to SQL invocation tests
    
    * Fixed the index test but create table one fails
    
    * Make sure it's revoked
    
    * Use :all to do setup once
    
    * Reduce scope of permissions to only select
    
    * Correct tests to run as expected - fix query
    
    * Move SQL tests into spec
    
    * Fix whitespace
    
    * Only do the dropping once to prevent a potential conflict
    
    * Confirmed the failing tests for select
    
    * Adjust once more to defaults (sanity check)
    
    * Correct permissions and allow for referencial lookups
    
    * Confirm none can be removed
    
    * Confirm that public tables can't be selected

--- spec/tasks/analytics_spec.rb
+++ spec/tasks/analytics_spec.rb
@@ -1,9 +1,66 @@
 require 'rails_helper'
 
 describe 'creating database views' do
-  it 'runs without error' do
+  before(:all) do
     Rails.application.load_tasks
+  end
+
+  it 'runs without error' do
     Rake::Task['analytics:drop_views'].invoke
     Rake::Task['analytics:create_views'].invoke
+  end
+
+  context "metabase access" do
+    before(:all) do
+      Rake::Task['analytics:drop_views'].invoke
+      Rake::Task['analytics:create_views'].invoke
+    end
+
+    context "when attempting intentional behavior" do
+      it "can select tables from the analytics table" do
+        expect(ActiveRecord::Base.connection.query("
+set role metabase;
+select count(id) from analytics.users;
+").flatten).to eql([0])
+      end
+    end
+
+    context "when attempting unintentional behavior" do
+      it "can't create new indicies" do
+        expect {
+          ActiveRecord::Base.connection.execute("
+set role metabase;
+create index no_way_vita_idx on analytics.team_member_roles (vita_partner_id, id);
+")
+        }.to raise_exception(ActiveRecord::StatementInvalid, /must be owner/)
+      end
+
+      it "can't create new views" do
+        expect {
+          ActiveRecord::Base.connection.execute("
+set role metabase;
+create table analytics.phony_table (number INTEGER, name TEXT);
+      ")
+        }.to raise_exception(ActiveRecord::StatementInvalid, /permission denied/)
+      end
+
+      it "can't remove views" do
+        expect {
+          ActiveRecord::Base.connection.execute("
+set role metabase;
+drop view analytics.users;
+      ")
+        }.to raise_exception(ActiveRecord::StatementInvalid, /must be owner/)
+      end
+
+      it "can't use other tables" do
+        expect {
+          ActiveRecord::Base.connection.execute("
+set role metabase;
+select count(*) from public.users;
+      ")
+        }.to raise_exception(ActiveRecord::StatementInvalid, /permission denied/)
+      end
+    end
   end
 end
