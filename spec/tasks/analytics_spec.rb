687a5fa1dd5761a383555aa6c14c5b5624eb30ae
Jacky Alcine <jalcine@codeforamerica.org>
2023-07-11 09:33:20 -0400

    Move SQL tests into spec

--- spec/tasks/analytics_spec.rb
+++ spec/tasks/analytics_spec.rb
@@ -6,4 +6,49 @@ describe 'creating database views' do
     Rake::Task['analytics:drop_views'].invoke
     Rake::Task['analytics:create_views'].invoke
   end
+
+  context "metabase access" do
+    before(:all) do
+      Rails.application.load_tasks
+    end
+
+    before(:each) do
+      Rake::Task['analytics:drop_views'].invoke
+      Rake::Task['analytics:create_views'].invoke
+    end
+
+    context "when attempting intentional behavior" do
+      it "can select tables from the analytics table" do
+
+        expect {
+          ActiveRecord::Base.connection.execute("
+set role metabase;
+select count(*) from analytics.team_role_members;
+select * from analytics.users where failed_attempts == 0;
+")
+        }.to raise_exception(ActiveRecord::StatementInvalid, /permission denied/)
+
+      end
+    end
+
+    context "when attempting unintentional behavior" do
+      it "can't create new indicies" do
+        expect {
+          ActiveRecord::Base.connection.execute("
+set role metabase;
+create index no_way_vita_idx on analytics.team_member_roles (vita_partner_id, id);
+      ")
+        }.to raise_exception(ActiveRecord::StatementInvalid, /permission denied/)
+      end
+
+      it "can't create new tables" do
+        expect {
+          ActiveRecord::Base.connection.execute("
+set role metabase;
+create table analytics.phony_table (number INTEGER, name TEXT);
+      ")
+        }.to raise_exception(ActiveRecord::StatementInvalid, /permission denied/)
+      end
+    end
+  end
 end
