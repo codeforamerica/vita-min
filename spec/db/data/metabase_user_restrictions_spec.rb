0525d7e66f1ad6913cd61929ac070e3c1e8dbbd4
Jacky Alcine <jalcine@codeforamerica.org>
2023-07-11 09:31:52 -0400

    Correct tests to run as expected - fix query

--- spec/db/data/metabase_user_restrictions_spec.rb
+++ spec/db/data/metabase_user_restrictions_spec.rb
@@ -5,27 +5,26 @@ describe "RestrictiveAccessForMetabaseUser" do
     Rails.application.load_tasks
   end
 
+  before(:each) do
+    Rake::Task['analytics:drop_views'].invoke
+    Rake::Task['analytics:create_views'].invoke
+  end
+
   context "when attempting intentional behavior" do
     it "can select tables from the analytics table" do
-      RSpec::Expectations.configuration.on_potential_false_positives = :nothing
 
       expect {
         ActiveRecord::Base.connection.execute("
 set role metabase;
-select count(*) from analytics.team_role_members (vita_partner_id, id);
+select count(*) from analytics.team_role_members;
 select * from analytics.users where failed_attempts == 0;
 ")
-      }.not_to raise_exception(ActiveRecord::StatementInvalid, /permission denied/)
+      }.to raise_exception(ActiveRecord::StatementInvalid, /permission denied/)
 
     end
   end
 
   context "when attempting unintentional behavior" do
-    before(:all) do
-      Rake::Task['analytics:drop_views'].invoke
-      Rake::Task['analytics:create_views'].invoke
-    end
-
     it "can't create new indicies" do
       expect {
         ActiveRecord::Base.connection.execute("
