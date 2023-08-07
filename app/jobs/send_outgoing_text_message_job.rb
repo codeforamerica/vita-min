9b8f0f129409891ffd6f93c8970c787eec774698
Jacky Alcine <jalcine@codeforamerica.org>
2023-08-07 10:41:21 -0400

    Wrap jobs with failure catching of net timeouts

--- app/jobs/send_outgoing_text_message_job.rb
+++ app/jobs/send_outgoing_text_message_job.rb
@@ -3,19 +3,26 @@ class SendOutgoingTextMessageJob < ApplicationJob
 
   def perform(outgoing_text_message_id)
     outgoing_text_message = OutgoingTextMessage.find(outgoing_text_message_id)
-    message = TwilioService.send_text_message(
-      to: outgoing_text_message.to_phone_number,
-      body: outgoing_text_message.body,
-      status_callback: outgoing_text_message_url(outgoing_text_message, locale: nil),
-      outgoing_text_message: outgoing_text_message
-    )
-
-    if message
-      outgoing_text_message.update(
-        twilio_sid: message.sid,
-        sent_at: DateTime.now
+    begin
+      message = TwilioService.send_text_message(
+        to: outgoing_text_message.to_phone_number,
+        body: outgoing_text_message.body,
+        status_callback: outgoing_text_message_url(outgoing_text_message, locale: nil),
+        outgoing_text_message: outgoing_text_message
       )
-      outgoing_text_message.update_status_if_further(message.status, error_code: message.error_code)
+
+      if message
+        outgoing_text_message.update(
+          twilio_sid: message.sid,
+          sent_at: DateTime.now
+        )
+        outgoing_text_message.update_status_if_further(message.status, error_code: message.error_code)
+      end
+    rescue Twilio::REST::TwilioError => e
+      if e.cause == Net::OpenTimeout
+        DatadogApi.increment("twilio.outgoing_text_messages.failure.timeout")
+        retry_job(outgoing_text_message_id)
+      end
     end
   end
 
