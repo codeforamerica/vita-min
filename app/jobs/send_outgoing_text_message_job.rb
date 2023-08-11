e829ff61ad939ca4079b2a9d79d4fc708bc46fda
Jacky Alcine <117868292+jalcine-cfa@users.noreply.github.com>
2023-08-11 10:50:34 -0400

    Reenqueue Failed SMS Jobs to Twilio (#3617)
    * Wrap jobs with failure catching of net timeouts
    
    * Adjust block and update test
    
    * Correct spec to use status_code and not error_code
    
    * Add var to make status code 'shared'

--- app/jobs/send_outgoing_text_message_job.rb
+++ app/jobs/send_outgoing_text_message_job.rb
@@ -3,19 +3,25 @@ class SendOutgoingTextMessageJob < ApplicationJob
 
   def perform(outgoing_text_message_id)
     outgoing_text_message = OutgoingTextMessage.find(outgoing_text_message_id)
-    message = TwilioService.send_text_message(
-      to: outgoing_text_message.to_phone_number,
-      body: outgoing_text_message.body,
-      status_callback: outgoing_text_message_url(outgoing_text_message, locale: nil),
-      outgoing_text_message: outgoing_text_message
-    )
-
-    if message
-      outgoing_text_message.update(
-        twilio_sid: message.sid,
-        sent_at: DateTime.now
+    begin
+      message = TwilioService.send_text_message(
+        to: outgoing_text_message.to_phone_number,
+        body: outgoing_text_message.body,
+        status_callback: outgoing_text_message_url(outgoing_text_message, locale: nil),
+        outgoing_text_message: outgoing_text_message
       )
-      outgoing_text_message.update_status_if_further(message.status, error_code: message.error_code)
+
+      if message
+        outgoing_text_message.update(
+          twilio_sid: message.sid,
+          sent_at: DateTime.now
+        )
+        outgoing_text_message.update_status_if_further(message.status, error_code: message.error_code)
+      end
+    rescue Net::OpenTimeout
+      DatadogApi.increment("twilio.outgoing_text_messages.failure.timeout")
+      outgoing_text_message.update_status_if_further("twilio_error", error_code: nil)
+      retry_job
     end
   end
 
