955eb29df99503d1d957d0626580672695f44aa4
Tahsina Islam <tislam@codeforamerica.org>
2023-08-23 15:17:42 -0400

    Final config
    Co-authored-by: Jacky Alcine <jalcine@codeforamerica.org>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_17_185057) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_16_162603) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
   enable_extension "plpgsql"
