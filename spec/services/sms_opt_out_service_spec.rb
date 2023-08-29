f512f49dcbc80799518be4c37c68c8af25d9b295
Jacky Alcine <117868292+jalcine-cfa@users.noreply.github.com>
2023-08-29 13:41:06 -0400

    Add body with example opt out structure (#3643)

--- spec/services/sms_opt_out_service_spec.rb
+++ spec/services/sms_opt_out_service_spec.rb
@@ -23,7 +23,6 @@ RSpec.describe SmsOptOutService do
 
     context "with an incoming sms that requests an opt-out" do
 
-
       it "creates a system note" do
         expect {
           described_class.process(client: client, params: incoming_sms_params)
