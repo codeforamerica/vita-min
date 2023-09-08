036c57fa2a83ccdec219527560980e685042af46
Asheesh Laroia <alaroia@codeforamerica.org>
2023-09-08 16:04:36 -0400

    wip
    Co-authored-by: Asheesh Laroia <alaroia@codeforamerica.org>

--- app/lib/efile/ny/it201.rb
+++ app/lib/efile/ny/it201.rb
@@ -13,8 +13,8 @@ module Efile
 
         @filing_status = filing_status # single, mfj, that's all we support for now
         @lines = ActiveSupport::HashWithIndifferentAccess.new(lines)
+        @it227 = it227
         validate_lines
-        @lines["60E"] = it227.calculate()["part2_line1"]
       end
 
       def calculate
@@ -25,11 +25,55 @@ module Efile
         # that are now available, so there's the `.lines` which is the data
         # we got as input, and the output of `.calculate` which is also a hash
         # of line number/name => value
-        result = ActiveSupport::HashWithIndifferentAccess.new
-        result
+        @computed = ActiveSupport::HashWithIndifferentAccess.new
+        @computed[:AMT_60E] = @it227.calculate[:part2_line1]
+        # TODO: E_1_CBX
+        @computed[:AMT_17] = compute_line_17
+        @computed[:AMT_19] = compute_line_19
+        @computed[:AMT_24] = compute_line_24
+        @computed[:AMT_25] = @lines[:AMT_4]
+        @computed[:AMT_27] = @lines[:AMT_15]
+        @computed[:AMT_32] = compute_line_32
+        @computed[:AMT_33] = compute_line_33
+        @computed
       end
 
       private
+
+      def compute_line_17
+        result = 0
+        (1..16).each do |line_num|
+          next if line_num == 12
+          result += lines["AMT_#{line_num}"]
+        end
+
+        result
+      end
+
+      def compute_line_19
+        @computed[:AMT_17] - (@lines[:AMT_18]).abs
+      end
+
+      def compute_line_24
+        result = 0
+        result += @lines[:AMT_19A]
+        (20..23).each do |line_num|
+          result += lines["AMT_#{line_num}"]
+        end
+        result
+      end
+
+      def compute_line_32
+        result = 0
+        (25..31).each do |line_num|
+          result += lines["AMT_line_num"]
+        end
+        result
+      end
+
+      def compute_line_33
+        @computed[:AMT_24] - @computed[:AMT_32]
+      end
 
       def is_full_year_resident
         if filing_status_mfj?
