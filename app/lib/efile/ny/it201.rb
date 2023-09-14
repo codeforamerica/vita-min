85c0f239270c84d2913640c4525fadcfa134fa7c
Asheesh Laroia <alaroia@codeforamerica.org>
2023-09-14 17:39:55 -0400

    State file: beginning of NYS 2022 tax math [#185950068] (#3649)
    start using the it201 calculator class in the it201 xml generation
    
    Co-authored-by: Asheesh Laroia <alaroia@codeforamerica.org>
    Co-authored-by: Jenny Heath <jheath@codeforamerica.org>
    Co-authored-by: Travis Grathwell <tgrathwell@codeforamerica.org>

---
+++ app/lib/efile/ny/it201.rb
@@ -0,0 +1,129 @@
+module Efile
+  module Ny
+    class It201
+      def initialize(year:, filing_status:, claimed_as_dependent:, dependent_count:, lines:, it227:)
+        @year = year
+
+        @filing_status = filing_status # single, married_filing_jointly, that's all we support for now
+        @claimed_as_dependent = claimed_as_dependent # true/false
+        @dependent_count = dependent_count # number
+        @computed = ActiveSupport::HashWithIndifferentAccess.new(lines)
+        @it227 = it227
+      end
+
+      def calculate
+        @computed[:AMT_60E] = @it227.calculate[:part2_line1]
+        @computed[:AMT_17] = calculate_line_17
+        @computed[:AMT_19] = calculate_line_19
+        @computed[:AMT_24] = calculate_line_24
+        @computed[:AMT_25] = @computed[:AMT_4]
+        @computed[:AMT_27] = @computed[:AMT_15]
+        @computed[:AMT_32] = calculate_line_32
+        @computed[:AMT_33] = calculate_line_33
+        @computed[:AMT_34] = calculate_line_34
+        @computed[:AMT_35] = calculate_line_35
+        @computed[:AMT_36] = @dependent_count
+        @computed[:AMT_37] = calculate_line_37
+        @computed[:AMT_38] = @computed[:AMT_37]
+        @computed[:AMT_39] = calculate_line_39
+        @computed[:AMT_43] = calculate_line_43
+        @computed[:AMT_44] = calculate_line_44
+        @computed
+      end
+
+      private
+
+      def calculate_line_17
+        result = 0
+        (1..16).each do |line_num|
+          next if line_num == 12
+          result += line_or_zero("AMT_#{line_num}")
+        end
+
+        result
+      end
+
+      def calculate_line_19
+        line_or_zero(:AMT_17) - line_or_zero(:AMT_18).abs
+      end
+
+      def calculate_line_24
+        result = 0
+        result += line_or_zero(:AMT_19A)
+        (20..23).each do |line_num|
+          result += line_or_zero("AMT_#{line_num}")
+        end
+        result
+      end
+
+      def calculate_line_32
+        result = 0
+        (25..31).each do |line_num|
+          result += line_or_zero("AMT_#{line_num}")
+        end
+        result
+      end
+
+      def calculate_line_33
+        line_or_zero(:AMT_24) - line_or_zero(:AMT_32)
+      end
+
+      def calculate_line_34
+        if filing_status_single?
+          if @claimed_as_dependent
+            3100
+          else
+            8000
+          end
+        elsif filing_status_mfj?
+          16050
+        end
+      end
+
+      def calculate_line_35
+        result = line_or_zero(:AMT_33) - line_or_zero(:AMT_34)
+        [result, 0].max
+      end
+
+      def calculate_line_37
+        result = line_or_zero(:AMT_35) - (line_or_zero(:AMT_36) * 1000)
+        [result, 0].max
+      end
+
+      def calculate_line_39
+        # aka calctax
+        1 # TODO
+      end
+
+      def calculate_line_43
+        line_or_zero(:AMT_40) + line_or_zero(:AMT_41) + line_or_zero(:AMT_42)
+      end
+
+      def calculate_line_44
+        [line_or_zero(:AMT_39) - line_or_zero(:AMT_43), 0].max
+      end
+
+      def is_full_year_resident
+        if filing_status_mfj?
+          if lines["F_1_NBR"] == 12 && lines["F_2_NBR"] == 12
+            true
+          end
+        else
+          lines["F_1_NBR"] == 12
+        end
+      end
+
+      def filing_status_mfj?
+        @filing_status == :married_filing_jointly
+      end
+
+      def filing_status_single?
+        @filing_status == :single
+      end
+
+      def line_or_zero(line)
+        @computed[line] || 0
+      end
+    end
+  end
+end
--- app/lib/submission_builder/ty2022/states/ny/documents/it201.rb
+++ app/lib/submission_builder/ty2022/states/ny/documents/it201.rb
@@ -12,11 +12,32 @@ module SubmissionBuilder
                 xml.PR_DOB_DT claimed: @submission.data_source.primary.birth_date.strftime("%Y-%m-%d")
                 xml.FS_CD claimed: 1
                 xml.DEP_CLAIM_IND claimed: 2
+                xml.FEDAGI_B4_ADJ_AMT claimed: calculated_fields['AMT_17']
                 xml.STD_ITZ_IND claimed: 1
+                xml.DED_AMT claimed: calculated_fields['AMT_34']
                 xml.TXBL_INC_AMT claimed: 5000
                 xml.SALE_USE_AMT claimed: 0
                 xml.PR_SGN_IND claimed: 1
               end
+            end
+
+            private
+
+            def calculated_fields
+              @it201_fields ||=
+                begin
+                  it201 = Efile::Ny::It201.new(
+                    year: 2022,
+                    filing_status: @submission.data_source.filing_status.to_sym,
+                    claimed_as_dependent: false,
+                    dependent_count: 0,
+                    lines: {
+                      AMT_2: @submission.data_source.fed_taxable_income,
+                    },
+                    it227: Efile::Ny::It227.new
+                  )
+                  it201.calculate
+                end
             end
           end
         end
