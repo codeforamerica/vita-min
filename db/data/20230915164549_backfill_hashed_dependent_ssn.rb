1967914ece11e6f850d83c6f34505f449cf93a8e
tgrathwell <30675659+tgrathwell@users.noreply.github.com>
2023-09-26 09:47:36 -0700

    Disable ddl transaction on data migration (#3663)
    * Disable ddl transaction on data migration
    * Fix hashed dependent backfill test
    
    Co-authored-by: Travis Grathwell <tgrathwell@codeforamerica.org>
    Co-authored-by: Em Barnard-Shao <ebarnard@codeforamerica.org>

--- db/data/20230915164549_backfill_hashed_dependent_ssn.rb
+++ db/data/20230915164549_backfill_hashed_dependent_ssn.rb
@@ -1,12 +1,14 @@
 # frozen_string_literal: true
 
 class BackfillHashedDependentSsn < ActiveRecord::Migration[7.0]
+  disable_ddl_transaction!
+
   def up
     dependents_left_to_backfill = Dependent.where(hashed_ssn: nil).where.not(ssn: nil)
     dependents_left_to_backfill.find_in_batches do |batch|
       Dependent.upsert_all(
         batch.map { |dependent|
-          { id: dependent.id, hashed_ssn: DeduplicationService.sensitive_attribute_hashed(dependent, :ssn), birth_date: dependent.birth_date, intake_id: dependent.intake.id }
+          { id: dependent.id, hashed_ssn: DeduplicationService.sensitive_attribute_hashed(dependent, :ssn), birth_date: dependent.birth_date, intake_id: dependent.intake_id }
         },
         update_only: [:hashed_ssn]
       )
