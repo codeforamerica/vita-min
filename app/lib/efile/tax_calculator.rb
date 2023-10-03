4066b3aa57afef7c620ad809161412cde96a378a
Travis Grathwell <tgrathwell@codeforamerica.org>
2023-10-03 16:15:02 -0700

    State file: tax calculators: set_line no longer tries to look up source via string messiness
    now every calculate_x invocation uses set_line with a symbol

--- app/lib/efile/tax_calculator.rb
+++ app/lib/efile/tax_calculator.rb
@@ -5,20 +5,10 @@ module Efile
     private
 
     def set_line(line_id, value_fn)
-      source_description = nil
       if value_fn.is_a?(Symbol)
         value_fn = method(value_fn)
-      else
-        method_name = "calculate_#{line_id.to_s.sub('AMT_', 'line_').downcase}".to_sym
-        method =
-          begin
-            self.class.instance_method(method_name)
-          rescue NameError
-            nil
-          end
-        source_description = method&.source&.strip_heredoc
       end
-      source_description ||= value_fn.source.strip_heredoc
+      source_description = value_fn.source.strip_heredoc
 
       value, accesses = @value_access_tracker.with_tracking { value_fn.call }
       @lines[line_id] = TaxFormLine.new(line_id, value, source_description, accesses)
