91fa7cc97f2d37d53bb9bc906c0e3ddff3fedcd6
Em Barnard-Shao <ebarnard@codeforamerica.org>
2023-10-06 12:17:20 -0400

    Fix tests

--- spec/support/shared_examples/offseason.rb
+++ spec/support/shared_examples/offseason.rb
@@ -13,6 +13,7 @@ shared_examples :a_normal_page_when_intake_is_open do |controller, action: :edit
   end
 end
 
+
 shared_examples :a_normal_page_when_only_in_progress_intakes_open do |controller, action: :edit|
   describe "when only in-progress intakes open" do
     before do
@@ -28,6 +29,7 @@ shared_examples :a_normal_page_when_only_in_progress_intakes_open do |controller
     end
   end
 end
+
 
 shared_examples :a_normal_page_when_intake_is_closed do |controller, action: :edit|
   describe "when intake is closed" do
@@ -92,6 +94,24 @@ shared_examples :a_redirect_home_when_login_is_closed do |controller, action: :e
     it "redirects to home" do
       get controller.to_path_helper(action: action)
       expect(response).to redirect_to root_path
+    end
+  end
+end
+
+
+shared_examples :a_redirect_to_closed_login_when_login_is_closed do |controller, action: :edit|
+  describe "when login is closed" do
+    before do
+      allow(Rails.configuration).to receive(:start_of_unique_links_only_intake).and_return(1.minute.ago)
+      allow(Rails.configuration).to receive(:start_of_open_intake).and_return(1.minute.ago)
+      allow(Rails.configuration).to receive(:end_of_intake).and_return(1.minute.ago)
+      allow(Rails.configuration).to receive(:end_of_in_progress_intake).and_return(30.seconds.ago)
+      allow(Rails.configuration).to receive(:end_of_login).and_return(1.second.ago)
+    end
+
+    it "redirects to closed login page" do
+      get controller.to_path_helper(action: action)
+      expect(response).to redirect_to portal_closed_login_path
     end
   end
 end
