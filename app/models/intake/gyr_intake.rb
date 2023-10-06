0320a1c2919c378708f2001a2d72383373650030
Travis Grathwell <tgrathwell@codeforamerica.org>
2023-10-06 16:40:38 -0700

    Remove schema.rb content and annotations related to Intake#hashed_spouse_ssn and Dependent#hashed_ssn
    they aren't represented in the migrations and didn't make it to prod,
    so they should not be in the schema file

--- app/models/intake/gyr_intake.rb
+++ app/models/intake/gyr_intake.rb
@@ -289,7 +289,6 @@
 #  index_intakes_on_email_address                          (email_address)
 #  index_intakes_on_email_domain                           (email_domain)
 #  index_intakes_on_hashed_primary_ssn                     (hashed_primary_ssn)
-#  index_intakes_on_hashed_spouse_ssn                      (hashed_spouse_ssn)
 #  index_intakes_on_matching_previous_year_intake_id       (matching_previous_year_intake_id)
 #  index_intakes_on_needs_to_flush_searchable_data_set_at  (needs_to_flush_searchable_data_set_at) WHERE (needs_to_flush_searchable_data_set_at IS NOT NULL)
 #  index_intakes_on_phone_number                           (phone_number)
