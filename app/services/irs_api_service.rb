41532f07fb29f54f88dc6250935ce59e196bc452
Ivan Priego <itrujillopriego@codeforamerica.org>
2023-10-13 15:50:49 -0700

    adding jwt encoding into http request header

--- app/services/irs_api_service.rb
+++ app/services/irs_api_service.rb
@@ -1,6 +1,7 @@
 require 'net/http'
 require 'net/https'
 require 'uri'
+require 'jwt'
 class IrsApiService
   def self.import_federal_data(token)
     unless ENV['USE_FAKE_API_SERVER']
@@ -21,6 +22,40 @@ class IrsApiService
     client_cert = OpenSSL::X509::Certificate.new(File.read(client_cert_path))
     client_key = OpenSSL::PKey::RSA.new(File.read(client_key_path))
 
+    # Adding JWT
+    # requirtement : JWT header with a JWT signed with a private key from the state.
+    # A valid authorization code must be included with the HTTP request in order
+    #  each authorization code may only be used one time and expire within 15 minutes.
+    ## JWT Structure
+    # header = {
+    #   "alg": "RS256",
+    #   "kid": private_key.thumbprint(),
+    # }
+    # claim = {
+    #   "iss": state_account_id, # State identifier provided by the IRS
+    #   "iat": time.time(), # Issued at time
+    #   "nonce": secrets.token_hex(8), # In combination with iat, verify this is a unique transaction
+    #   "sub": authorization_code, # User authorization code from Direct File
+    # }
+
+
+    claim = {
+        "iss": "MD_ID", # State identifier provided by the IRS
+        "iat": 0, # Issued at time, add ruby time.time()
+        "nonce": "cool_nonce", # In combination with iat, verify this is a unique transaction :secrets.token_hex(8)
+        "sub": "cool_auth_code", # User authorization code from Direct File
+      }
+
+    puts claim
+
+    token = JWT.encode claim, client_key, 'RS256'
+
+    puts token
+    # verifying that JWT was actually sent
+    decoded_token = JWT.decode token, client_cert.public_key, true, { algorithm: 'RS256' }
+
+    puts decoded_token
+
     http = Net::HTTP.new(server_url.host, server_url.port)
     http.use_ssl = true
     http.cert = client_cert
@@ -30,11 +65,39 @@ class IrsApiService
     http.ca_file = server_ca_cert_path
 
     request = Net::HTTP::Get.new(server_url.request_uri)
+    request.initialize_http_header({'Authorization' => "JWT #{token}"})
+
     response = http.request(request)
 
     puts "Response Code: #{response.code}"
     puts "Response Body: #{response.body}"
 
     response.body
+
+
+
+
+
+
+
+
+  end
+end
+
+
+class JsonWebToken # todo : move class to its own file
+  def self.encode(payload, exp = 15.min.from_now)
+    payload[:exp] = exp.to_i
+    JWT.encode(payload, Rails.application.secrets.secret_key_base.to_s)
+  end
+
+  def auth_header
+    # { Authorization: 'Bearer <token>' }
+    request.headers['Authorization']
+  end
+
+  def self.decode(token)
+    decoded = JWT.decode(token, Rails.application.secrets.secret_key_base.to_s)[0]
+    HashWithIndifferentAccess.new decoded
   end
 end
