03538ba900fe264f3251233d24730b0dad94470c
Tahsina Islam <tislam@codeforamerica.org>
2023-10-18 11:36:26 -0400

    PDF
    Co-authored-by: Tahsina Islam <tislam@codeforamerica.org>

--- app/lib/pdf_filler/az140_pdf.rb
+++ app/lib/pdf_filler/az140_pdf.rb
@@ -28,14 +28,93 @@ module PdfFiller
         "State" => "AZ",
         "ZIP Code" => @submission.data_source.direct_file_data.mailing_zip,
         "Filing Status" => filing_status,
+        "8" => calculated_fields.fetch(:AMT_8),
+        "9" => calculated_fields.fetch(:AMT_9),
+        "10a" => "TODO",
+        "10b" => "TODO",
+        "11a" => "TODO",
+        "10d First" => "TODO",
+        "10e First" => "TODO",
+        "10d Last" => "TODO",
+        "10e Last" => "TODO",
+        "10d SSN" => "TODO",
+        "10e SSN" => "TODO",
+        "10d Relationship" => "TODO",
+        "10e Relationship" => "TODO",
+        "10d Mo in Home" => "TODO",
+        "10e Mo in Home" => "TODO",
+        "10d_10a check box" => "TODO",
+        "10e_10a check box" => "TODO",
         "12" => @xml_document.at('FedAdjGrossIncome')&.text,
         "14" => @xml_document.at('ModFedAdjGrossInc')&.text,
       }
       answers
     end
 
-    private
 
+
+    # FieldType: Button
+    # FieldName: 10d_10b check box
+    # FieldFlags: 0
+    # FieldJustification: Left
+    # FieldStateOption: Yes
+    # FieldStateOption: Off
+    # ---
+    # FieldType: Button
+    # FieldName: 10e_10b check box
+    # FieldFlags: 0
+    # FieldJustification: Left
+    # FieldStateOption: Yes
+    # FieldStateOption: Off
+    # ---
+    # FieldType: Button
+    # FieldName: 10d educ
+    # FieldFlags: 0
+    # FieldJustification: Left
+    # FieldStateOption: Yes
+    # FieldStateOption: Off
+    # ---
+    # FieldType: Button
+    # FieldName: 10e educ
+    # FieldFlags: 0
+    # FieldJustification: Left
+    # FieldStateOption: Yes
+    # FieldStateOption: Off
+    # ---
+    # FieldType: Text
+    # FieldName: 11c First
+    # FieldFlags: 12582912
+    # FieldJustification: Left
+    # ---
+    # FieldType: Text
+    # FieldName: 11c Last
+    # FieldFlags: 12582912
+    # FieldJustification: Left
+    # ---
+    # FieldType: Text
+    # FieldName: 11c SSN
+    # FieldFlags: 12582912
+    # FieldJustification: Center
+    # ---
+    # FieldType: Text
+    # FieldName: 11c Relationship
+    # FieldFlags: 12582912
+    # FieldJustification: Center
+    # ---
+    # FieldType: Button
+    # FieldName: 11c died
+    # FieldFlags: 0
+    # FieldJustification: Left
+    # FieldStateOption: Yes
+    # FieldStateOption: Off
+    # ---
+    # FieldType: Button
+    # FieldName: 11a check box
+    private
+    def calculated_fields
+      @calculated_fields ||= @submission.data_source.tax_calculator.calculate
+    end
+    
     FILING_STATUS_OPTIONS = {
       "MarriedJoint" => 'Choice1',
       "HeadHousehold" => 'Choice2',
