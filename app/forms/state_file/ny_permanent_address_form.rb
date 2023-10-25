00e2b67f1f6626d706c315be7eeca300730e488b
Tahsina Islam <tislam@codeforamerica.org>
2023-10-25 17:20:48 -0400

    copy over address 1040 fields to intake
    Co-authored-by: Jenny Heath <jheath@codeforamerica.org>

--- app/forms/state_file/ny_permanent_address_form.rb
+++ app/forms/state_file/ny_permanent_address_form.rb
@@ -3,7 +3,15 @@ module StateFile
     set_attributes_for :intake, :confirmed_permanent_address
 
     def save
-      @intake.update(attributes_for(:intake))
+      additional_attributes = confirmed_permanent_address == "yes" ?
+        {
+          permanent_apartment: @intake.direct_file_data.mailing_apartment,
+          permanent_city: @intake.direct_file_data.mailing_city,
+          permanent_street: @intake.direct_file_data.mailing_street,
+          permanent_zip: @intake.direct_file_data.mailing_zip,
+        } : {}
+
+      @intake.update(attributes_for(:intake).merge(additional_attributes))
     end
   end
 end
