8d0835c8fd30e3892db549a47405b2297a4d5a3e
Em Barnard-Shao <ebarnard@codeforamerica.org>
2023-10-27 11:17:58 -0400

    Add test for ny_dob_form

---
+++ spec/factories/state_file_dependents.rb
@@ -0,0 +1,31 @@
+# == Schema Information
+#
+# Table name: state_file_dependents
+#
+#  id             :bigint           not null, primary key
+#  dob            :date
+#  first_name     :string
+#  intake_type    :string           not null
+#  last_name      :string
+#  middle_initial :string
+#  months_in_home :integer
+#  relationship   :string
+#  ssn            :string
+#  suffix         :string
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#  intake_id      :bigint           not null
+#
+# Indexes
+#
+#  index_state_file_dependents_on_intake  (intake_type,intake_id)
+#
+FactoryBot.define do
+  factory :state_file_dependent do
+    intake
+    first_name { "Ali" }
+    middle_initial {"U"}
+    last_name { "Poppyseed" }
+    relationship { "DAUGHTER" }
+  end
+end
