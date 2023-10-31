42725a9479cd0fbb43516a34f870e35be8808803
Travis Grathwell <tgrathwell@codeforamerica.org>
2023-10-31 10:35:48 -0700

    Session toggles page has links to set the date quicker

--- app/controllers/session_toggles_controller.rb
+++ app/controllers/session_toggles_controller.rb
@@ -4,22 +4,32 @@ class SessionTogglesController < ApplicationController
   before_action :require_sign_in, if: -> { Rails.env.production? }
 
   def index
-    @gyr_times = [
-      SessionToggleTime.new(name: 'Start of unique-links only intake', property: :start_of_unique_links_only_intake),
-      SessionToggleTime.new(name: 'Start of open intake', property: :start_of_open_intake),
-      SessionToggleTime.new(name: 'End of intake', property: :end_of_intake),
-      SessionToggleTime.new(name: 'End of documents upload', property: :end_of_docs),
-      SessionToggleTime.new(name: 'End of finishing in-progress intakes', property: :end_of_in_progress_intake),
-      SessionToggleTime.new(name: 'End of login', property: :end_of_login),
-    ]
-    @ctc_times = [
-      SessionToggleTime.new(name: 'Soft launch', property: :ctc_soft_launch),
-      SessionToggleTime.new(name: 'Full launch', property: :ctc_full_launch),
-      SessionToggleTime.new(name: 'EITC soft launch', property: :eitc_soft_launch),
-      SessionToggleTime.new(name: 'EITC full launch', property: :eitc_full_launch),
-      SessionToggleTime.new(name: 'End of intake', property: :ctc_end_of_intake),
-      SessionToggleTime.new(name: 'End of read-write access', property: :ctc_end_of_read_write),
-      SessionToggleTime.new(name: 'End of login', property: :ctc_end_of_login),
+    @toggle_times = [
+      {
+        service_name: 'Get Your Refund',
+        service_url: url_for(host: MultiTenantService.new(:gyr).host, controller: :session_toggles),
+        times: [
+          SessionToggleTime.new(name: 'Start of unique-links only intake', property: :start_of_unique_links_only_intake),
+          SessionToggleTime.new(name: 'Start of open intake', property: :start_of_open_intake),
+          SessionToggleTime.new(name: 'End of intake', property: :end_of_intake),
+          SessionToggleTime.new(name: 'End of documents upload', property: :end_of_docs),
+          SessionToggleTime.new(name: 'End of finishing in-progress intakes', property: :end_of_in_progress_intake),
+          SessionToggleTime.new(name: 'End of login', property: :end_of_login),
+        ]
+      },
+      {
+        service_name: 'GetCTC',
+        service_url: url_for(host: MultiTenantService.new(:ctc).host, controller: :session_toggles),
+        times: [
+          SessionToggleTime.new(name: 'Soft launch', property: :ctc_soft_launch),
+          SessionToggleTime.new(name: 'Full launch', property: :ctc_full_launch),
+          SessionToggleTime.new(name: 'EITC soft launch', property: :eitc_soft_launch),
+          SessionToggleTime.new(name: 'EITC full launch', property: :eitc_full_launch),
+          SessionToggleTime.new(name: 'End of intake', property: :ctc_end_of_intake),
+          SessionToggleTime.new(name: 'End of read-write access', property: :ctc_end_of_read_write),
+          SessionToggleTime.new(name: 'End of login', property: :ctc_end_of_login),
+        ]
+      }
     ]
     @toggle = SessionToggle.new(session, 'app_time')
   end
