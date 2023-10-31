0f1e2b049c059b734bf69ff8d658b1d082a3e478
Ben Golder <benjamin.j.golder@gmail.com>
2023-10-31 14:27:34 -0700

    small refactors

--- app/controllers/state_file/questions/verification_code_controller.rb
+++ app/controllers/state_file/questions/verification_code_controller.rb
@@ -4,9 +4,9 @@ module StateFile
       def edit
         case current_intake.contact_preference
         when "text"
-          @contact_method = PhoneParser.formatted_phone_number(current_intake.phone_number)
+          @contact_info = PhoneParser.formatted_phone_number(current_intake.phone_number)
         when "email"
-          @contact_method = current_intake.email_address
+          @contact_info = current_intake.email_address
         end
 
         super
