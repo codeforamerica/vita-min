6be3c4bfdff6c7594122c16600133fc821516c5c
Ben Golder <benjamin.j.golder@gmail.com>
2023-10-30 17:13:46 -0700

    Save the state intake to the session from the concern
    Co-authored-by: Em Barnard-Shao <ebarnard@codeforamerica.org>

--- app/controllers/state_file/questions/contact_preference_controller.rb
+++ app/controllers/state_file/questions/contact_preference_controller.rb
@@ -2,12 +2,6 @@ module StateFile
   module Questions
     class ContactPreferenceController < QuestionsController
       include StartIntakeConcern
-
-      private
-
-      def after_update_success
-        session[:state_file_intake] = current_intake.to_global_id
-      end
     end
   end
 end
