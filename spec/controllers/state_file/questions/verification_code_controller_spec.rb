5825e983083fbb57ad1574ce4b14b55b94ce5843
Ben Golder <benjamin.j.golder@gmail.com>
2023-10-31 15:16:35 -0700

    Fix flow controller spec

--- spec/controllers/state_file/questions/verification_code_controller_spec.rb
+++ spec/controllers/state_file/questions/verification_code_controller_spec.rb
@@ -12,19 +12,21 @@ RSpec.describe StateFile::Questions::VerificationCodeController do
       it "sets @contact_method to a pretty phone number" do
         get :edit, params: { us_state: "az" }
 
-        expect(assigns(:contact_method)).to eq "(415) 333-4444"
+        expect(assigns(:contact_info)).to eq "(415) 333-4444"
       end
     end
+
     context "with an intake that prefers email" do
       let(:intake) { create(:state_file_az_intake, contact_preference: "email", email_address: "someone@example.com") }
 
       it "sets @contact_method to an email address" do
         get :edit, params: { us_state: "az" }
 
-        expect(assigns(:contact_method)).to eq "someone@example.com"
+        expect(assigns(:contact_info)).to eq "someone@example.com"
       end
     end
   end
+
   describe "#update" do
     xit "validates the verification code" do
     end
