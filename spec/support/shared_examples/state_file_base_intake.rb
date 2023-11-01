759d6c7e52d0d22b640053744e226b012b2b2e0f
Ben Golder <benjamin.j.golder@gmail.com>
2023-11-01 12:35:32 -0700

    Validate email & phone #s on state file intakes

---
+++ spec/support/shared_examples/state_file_base_intake.rb
@@ -0,0 +1,39 @@
+require "rails_helper"
+
+shared_examples :state_file_base_intake do |factory:|
+  describe "validations" do
+    describe "uses a 3rd party library to validate emails" do
+      context "with an invalid email" do
+        it "is not valid" do
+          intake = build(factory, email_address: "someone@nowhere")
+          expect(intake).not_to be_valid
+          expect(intake.errors).to include :email_address
+        end
+      end
+
+      context "with a valid email" do
+        it "is valid" do
+          intake = build(factory, email_address: "someone@example.com")
+          expect(intake).to be_valid
+        end
+      end
+    end
+
+    describe "uses a 3rd party library to validate phone numbers" do
+      context "with an invalid phone number" do
+        it "is not valid" do
+          intake = build(factory, phone_number: "55555")
+          expect(intake).not_to be_valid
+          expect(intake.errors).to include :phone_number
+        end
+      end
+
+      context "with a valid phone number" do
+        it "is valid" do
+          intake = build(factory, phone_number: "+14153334444")
+          expect(intake).to be_valid
+        end
+      end
+    end
+  end
+end
