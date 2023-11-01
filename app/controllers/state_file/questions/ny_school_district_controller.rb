c9e6ce6c9facbc5cfeefa2d2f6c610abf305e62b
Jenny Heath <jheath@codeforamerica.org>
2023-11-01 13:28:19 -0700

    extract csv stuff into class

--- app/controllers/state_file/questions/ny_school_district_controller.rb
+++ app/controllers/state_file/questions/ny_school_district_controller.rb
@@ -4,50 +4,20 @@ module StateFile
   module Questions
     class NySchoolDistrictController < QuestionsController
       layout "state_file/question"
-      before_action :create_district_and_combined_name_mappings_from_csv, only: [:update]
       before_action :get_county_school_districts_from_csv, only: [:edit]
 
       private
 
       def get_county_school_districts_from_csv
-        @school_districts = county_rows_from_csv.map do |row|
-          combined_name = [row['School District'], row['Use Elementary School District']].join(" ").strip
-          [combined_name, combined_name]
-        end.uniq
-      end
-
-      def create_district_and_combined_name_mappings_from_csv
-        @combined_district_name_mapping = Hash.new
-        @district_code_mapping = Hash.new
-        county_rows_from_csv.each do |row|
-          combined_name = [row['School District'], row['Use Elementary School District']].join(" ").strip
-          @district_code_mapping[combined_name] = row['Code Number']
-
-          if combined_name != row['School District']
-            @combined_district_name_mapping[combined_name] = row['School District']
-          end
-        end
-      end
-
-      def county_rows_from_csv
-        csv_file_path = Rails.root.join('docs', 'ny_school_districts.csv')
-        csv_content = File.read(csv_file_path)
-        io = StringIO.new(csv_content)
-        CSV.parse(io, headers: true).filter { |row| row["County"] == current_intake.residence_county }
+        @school_districts = NySchoolDistricts.county_school_districts_labels_for_select(current_intake.residence_county)
       end
 
       def form_params
         school_district_from_params = params[:state_file_ny_school_district_form][:school_district]
-        school_district_number = @district_code_mapping[school_district_from_params]
-        original_name = @combined_district_name_mapping[school_district_from_params]
-        if original_name.present?
-          school_district = original_name
-        else
-          school_district = school_district_from_params
-        end
-
+        school_district_number = NySchoolDistricts.combined_name_to_code_number_map(current_intake.residence_county)[school_district_from_params]
+        original_name = NySchoolDistricts.combined_name_to_original_name_map(current_intake.residence_county)[school_district_from_params]
         super.merge(
-          school_district: school_district,
+          school_district: original_name,
           school_district_number: school_district_number.to_i
         )
       end
---
+++ app/lib/ny_school_districts.rb
@@ -0,0 +1,33 @@
+class NySchoolDistricts
+  def self.county_school_districts_labels_for_select(county)
+    @school_districts ||= self.county_rows_from_csv(county).map do |row|
+      combined_name = [row['School District'], row['Use Elementary School District']].join(" ").strip
+      [combined_name, combined_name]
+    end.uniq
+  end
+
+  def self.combined_name_to_code_number_map(county)
+    @name_to_code ||= self.county_rows_from_csv(county).reduce({}) do |hash, row|
+      combined_name = [row['School District'], row['Use Elementary School District']].join(" ").strip
+      hash[combined_name] = row['Code Number']
+      hash
+    end
+  end
+
+  # includes districts where combined name is same as original
+  def self.combined_name_to_original_name_map(county)
+    @combined_name_to_original_name ||= self.county_rows_from_csv(county).reduce({}) do |hash, row|
+      combined_name = [row['School District'], row['Use Elementary School District']].join(" ").strip
+      hash[combined_name] = row['School District']
+      hash
+    end
+  end
+
+  def self.county_rows_from_csv(county)
+    csv_file_path = Rails.root.join('docs', 'ny_school_districts.csv')
+    csv_content = File.read(csv_file_path)
+    io = StringIO.new(csv_content)
+    @county_rows ||= CSV.parse(io, headers: true).filter { |row| row["County"] == county }
+  end
+end
+
