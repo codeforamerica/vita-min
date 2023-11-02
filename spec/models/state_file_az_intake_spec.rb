81dcee1aadf20314266bf83115090d3a5ea772e8
Lou Moore <lmoore@codeforamerica.org>
2023-11-02 00:51:57 -0700

    persist a has_prior_last_names field on AZ intakes so we can differentiate between unfilled vs set to no if the user goes back in the flow

--- spec/models/state_file_az_intake_spec.rb
+++ spec/models/state_file_az_intake_spec.rb
@@ -12,6 +12,7 @@
 #  contact_preference     :integer          default("unfilled"), not null
 #  current_step           :string
 #  email_address          :citext
+#  has_prior_last_names   :integer          default("unfilled"), not null
 #  phone_number           :string
 #  primary_first_name     :string
 #  primary_last_name      :string
