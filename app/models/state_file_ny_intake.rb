3012d6cdca26d703303e3a17e050a658e3e45527
Ben Golder <benjamin.j.golder@gmail.com>
2023-11-03 09:23:17 -0700

    Timestamp contact verification for state intakes

--- app/models/state_file_ny_intake.rb
+++ app/models/state_file_ny_intake.rb
@@ -13,6 +13,7 @@
 #  current_step                     :string
 #  date_electronic_withdrawal       :date
 #  email_address                    :citext
+#  email_address_verified_at        :datetime
 #  household_cash_assistance        :integer
 #  household_fed_agi                :integer
 #  household_ny_additions           :integer
@@ -39,6 +40,7 @@
 #  permanent_street                 :string
 #  permanent_zip                    :string
 #  phone_number                     :string
+#  phone_number_verified_at         :datetime
 #  primary_birth_date               :date
 #  primary_email                    :string
 #  primary_first_name               :string
