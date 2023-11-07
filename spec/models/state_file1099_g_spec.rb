f134b03851d748e4edb4979c0fddd3db5cb6dc32
Travis Grathwell <tgrathwell@codeforamerica.org>
2023-11-06 16:52:59 -0800

    Missing annotation
    Co-authored-by: Jenny Heath <jheath@codeforamerica.org>

--- spec/models/state_file1099_g_spec.rb
+++ spec/models/state_file1099_g_spec.rb
@@ -1,3 +1,29 @@
+# == Schema Information
+#
+# Table name: state_file1099_gs
+#
+#  id                          :bigint           not null, primary key
+#  address_confirmation        :integer          default("unfilled"), not null
+#  federal_income_tax_withheld :integer
+#  had_box_11                  :integer          default("unfilled"), not null
+#  intake_type                 :string           not null
+#  payer_name                  :string
+#  payer_name_is_default       :integer          default("unfilled"), not null
+#  recipient                   :integer          default("unfilled"), not null
+#  recipient_city              :string
+#  recipient_state             :string
+#  recipient_street_address    :string
+#  recipient_zip               :string
+#  state_income_tax_withheld   :integer
+#  unemployment_compensation   :integer
+#  created_at                  :datetime         not null
+#  updated_at                  :datetime         not null
+#  intake_id                   :bigint           not null
+#
+# Indexes
+#
+#  index_state_file1099_gs_on_intake  (intake_type,intake_id)
+#
 require 'rails_helper'
 
 RSpec.describe StateFile1099G do
