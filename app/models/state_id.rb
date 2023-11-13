5d89c45bbbc2eb56fb6d5eef5d9b36e93f67e3c5
Em Barnard-Shao <ebarnard@codeforamerica.org>
2023-11-12 20:15:15 -0500

    clear fields if no_id

--- app/models/state_id.rb
+++ app/models/state_id.rb
@@ -16,4 +16,15 @@ class StateId < ApplicationRecord
   has_one :intake_as_primary, class_name: "StateFileNyIntake", foreign_key: "primary_state_id"
   has_one :intake_as_spouse, class_name: "StateFileNyIntake", foreign_key: "spouse_state_id"
   enum id_type: { unfilled: 0, driver_license: 1, dmv_bmv: 2, no_id: 3 }, _prefix: :id_type
+
+  before_save do
+    if id_type_changed?(to: "no_id") || id_type_changed?(to: "unfilled")
+      self.expiration_date = nil
+      self.first_three_doc_num = nil
+      self.id_number = nil
+      self.issue_date = nil
+      self.state = nil
+    end
+  end
+
 end
