d7e13dc1f78d929e7d7bafcc747b1cef9ba1b75d
Jenny Heath <jheath@codeforamerica.org>
2023-11-13 14:39:56 -0800

    update copy
    Co-authored-by: Ben Golder <bgolder2@codeforamerica.org>

--- app/controllers/state_file/questions/eligibility_offboarding_controller.rb
+++ app/controllers/state_file/questions/eligibility_offboarding_controller.rb
@@ -6,7 +6,7 @@ module StateFile
 
       def ineligible_reason
         key = current_intake.disqualifying_eligibility_answer
-        I18n.t("state_file.questions.eligibility_offboarding.edit.ineligible_reason.#{key}")
+        I18n.t("state_file.questions.eligibility_offboarding.edit.ineligible_reason.#{key}", state: States.name_for_key(params[:us_state].upcase))
       end
 
       def set_prev_path
