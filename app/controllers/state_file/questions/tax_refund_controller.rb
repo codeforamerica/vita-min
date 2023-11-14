f4c64bed5827e452e5984a816ba372946be62ea3
Ricardo Kreyhsig <rkreyhsig@codeforamerica.org>
2023-11-14 17:22:55 -0500

    Add jQuery for Tax refund page

--- app/controllers/state_file/questions/tax_refund_controller.rb
+++ app/controllers/state_file/questions/tax_refund_controller.rb
@@ -2,6 +2,13 @@ module StateFile
   module Questions
     class TaxRefundController < QuestionsController
 
+      def refund_amount
+        calculator = current_intake.tax_calculator
+        calculator.calculate
+        calculator.refund_or_owed_amount
+      end
+      helper_method :refund_amount
+
       def form_class
         DepositTypeForm
       end
--- app/lib/navigation/state_file_az_question_navigation.rb
+++ app/lib/navigation/state_file_az_question_navigation.rb
@@ -25,7 +25,7 @@ module Navigation
       StateFile::Questions::AzStateCreditsController,
       StateFile::Questions::AzCharitableContributionsController,
       StateFile::Questions::AzReviewController,
-      StateFile::Questions::TaxesOwedController,
+      # StateFile::Questions::TaxesOwedController,
       StateFile::Questions::TaxRefundController,
       StateFile::Questions::EsignDeclarationController,
       StateFile::Questions::SubmissionConfirmationController,
