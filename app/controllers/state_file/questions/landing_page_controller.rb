c4ee36165f6fa1a7c0e2c68eddc1bf54f5cf6c75
jenny-heath <43800769+jenny-heath@users.noreply.github.com>
2023-11-15 14:35:05 -0800

    State file: move intake creation to landing page (#3790)
    Co-authored-by: Ben Golder <bgolder2@codeforamerica.org>

--- app/controllers/state_file/questions/landing_page_controller.rb
+++ app/controllers/state_file/questions/landing_page_controller.rb
@@ -1,6 +1,7 @@
 module StateFile
   module Questions
     class LandingPageController < QuestionsController
+      include StartIntakeConcern
     end
   end
 end
--- spec/controllers/state_file/questions/eligibility_residence_controller_spec.rb
+++ spec/controllers/state_file/questions/eligibility_residence_controller_spec.rb
@@ -2,34 +2,6 @@ require 'rails_helper'
 
 RSpec.describe StateFile::Questions::EligibilityResidenceController do
   describe "#update" do
-    # use the shared example to test functionality for creating the intake
-    # This can be moved to a different controller spec but the valid params
-    # will need to be defined for the new controller
-
-    it_behaves_like :start_intake_concern, intake_class: StateFileAzIntake, intake_factory: :state_file_az_intake do
-      let(:valid_params) do
-        {
-          us_state: "az",
-          state_file_az_eligibility_residence_form: {
-            eligibility_lived_in_state: "yes",
-            eligibility_married_filing_separately: "no"
-          }
-        }
-      end
-    end
-
-    it_behaves_like :start_intake_concern, intake_class: StateFileNyIntake, intake_factory: :state_file_ny_intake do
-      let(:valid_params) do
-        {
-          us_state: "ny",
-          state_file_ny_eligibility_residence_form: {
-            eligibility_lived_in_state: "yes",
-            eligibility_yonkers: "yes",
-          }
-        }
-      end
-    end
-
     # use the eligibility_offboarding_concern shared example if the page
     # should redirect to the state file eligibility offboarding page
     # when it receives a disqualifying answer.
