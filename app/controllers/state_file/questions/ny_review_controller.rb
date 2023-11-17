3782d4f9f56acdfef6eb5a90117fad6c4d4c258f
Lou Moore <lmoore@codeforamerica.org>
2023-11-17 12:48:43 -0800

    move abs call to view so it doesn't break logic in controllers that tests for positive

--- app/controllers/state_file/questions/ny_review_controller.rb
+++ app/controllers/state_file/questions/ny_review_controller.rb
@@ -2,7 +2,7 @@ module StateFile
   module Questions
     class NyReviewController < QuestionsController
       def edit
-        @refund_or_owed_amount = current_intake.calculated_refund_or_owed_amount.abs
+        @refund_or_owed_amount = current_intake.calculated_refund_or_owed_amount
         @refund_or_owed_label = @refund_or_owed_amount.positive? ? I18n.t("state_file.questions.ny_review.edit.your_refund") : I18n.t("state_file.questions.ny_review.edit.your_tax_owed")
       end
 
