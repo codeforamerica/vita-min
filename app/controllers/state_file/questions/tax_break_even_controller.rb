0d7753f66bdf8e4dc89bdb440c294c3adb1ca7d3
Ricardo Kreyhsig <rkreyhsig@codeforamerica.org>
2023-11-21 15:18:34 -0500

    Rm BreakEven controller
    Co-authored-by: Em Barnard-Shao <ebarnard@codeforamerica.org>

--- app/controllers/state_file/questions/tax_break_even_controller.rb
+++
@@ -1,9 +0,0 @@
-module StateFile
-  module Questions
-    class TaxBreakEvenController < QuestionsController
-      def self.show?(intake)
-        intake.calculated_refund_or_owed_amount.zero?
-      end
-    end
-  end
-end
