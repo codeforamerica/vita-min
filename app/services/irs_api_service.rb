5edeb635886adf44eb9c9662ddeebaad440c7329
Jenny Heath <jheath@codeforamerica.org>
2023-11-21 12:37:36 -0800

    DF Integration: convert iss to string before creating JWT
    Co-authored-by: Ben Golder <bgolder2@codeforamerica.org>

--- app/services/irs_api_service.rb
+++ app/services/irs_api_service.rb
@@ -18,13 +18,12 @@ class IrsApiService
     cert_finder = CertificateFinder.new(server_url, state_code)
 
     claim = {
-      "iss": account_id, # State identifier provided by the IRS
+      "iss": account_id.to_s, # State identifier provided by the IRS
       "iat": Time.now.to_i, # Issued at time
       "sub": authorization_code, # User authorization code from Direct File
     }
 
     token = JWT.encode claim, cert_finder.client_key, 'RS256'
-    
     # puts token
     # # verifying that JWT was actually sent
     # decoded_token = JWT.decode token, cert_finder.client_cert.public_key, true, { algorithm: 'RS256' }
