437da8988fcb26c3291cd4c587ceb023375bb216
Ricardo Kreyhsig <2385700+rickreyhsig@users.noreply.github.com>
2023-11-21 11:03:14 -0500

    Update app/controllers/state_file/questions/tax_break_even_controller.rb

--- app/controllers/state_file/questions/tax_break_even_controller.rb
+++ app/controllers/state_file/questions/tax_break_even_controller.rb
@@ -4,10 +4,7 @@ module StateFile
       def self.show?(intake)
         intake.calculated_refund_or_owed_amount.zero?
       end
-      def refund_amount
-        current_intake.calculated_refund_or_owed_amount
-      end
-      helper_method :refund_amount
+
     end
   end
 end
