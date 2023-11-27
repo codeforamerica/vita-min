0bc07212ce9fdfda3ea2a860d7e1857f26477f0b
Ricardo Kreyhsig <rkreyhsig@codeforamerica.org>
2023-11-27 15:48:09 -0500

    Add spec

--- app/controllers/state_file/questions/data_transfer_offboarding_controller.rb
+++ app/controllers/state_file/questions/data_transfer_offboarding_controller.rb
@@ -5,7 +5,11 @@ module StateFile
 
       def ineligible_reason
         key = current_intake.disqualifying_eligibility_answer
-        I18n.t("state_file.questions.eligibility_offboarding.edit.ineligible_reason.#{key}", state: States.name_for_key(params[:us_state].upcase))
+        return key.present? ?
+          I18n.t(
+            "state_file.questions.eligibility_offboarding.edit.ineligible_reason.#{key}",
+            state: States.name_for_key(params[:us_state].upcase)
+          ) : nil
       end
 
       def self.show?(intake)
