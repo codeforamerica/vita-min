cf8244573ade61425cdd0d4c1fd3253c93aaa65e
Ricardo Kreyhsig <rkreyhsig@codeforamerica.org>
2023-11-27 15:57:53 -0500

    Clean up specs

--- spec/controllers/state_file/questions/data_transfer_offboarding_controller_spec.rb
+++ spec/controllers/state_file/questions/data_transfer_offboarding_controller_spec.rb
@@ -2,30 +2,27 @@ require "rails_helper"
 
 RSpec.describe StateFile::Questions::DataTransferOffboardingController do
   describe "#edit" do
+    let(:intake) { create :state_file_az_intake }
+
     context "when the client is ineligible" do
-      let(:intake) { create :state_file_az_intake }
       before do
         session[:state_file_intake] = intake.to_global_id
         allow(subject).to receive(:params).and_return({us_state: 'az'})
+        allow_any_instance_of(StateFileAzIntake).to receive(:disqualifying_eligibility_answer).and_return(:eligibility_lived_in_state)
       end
 
       it "gets the correct values for ineligible_reason" do
-        allow(subject).to receive(:params).and_return({us_state: 'az'})
-        allow_any_instance_of(StateFileAzIntake).to receive(:disqualifying_eligibility_answer).and_return(:eligibility_lived_in_state)
-
         expect(subject.ineligible_reason).to eq('did not live in Arizona for all of 2023')
       end
     end
 
     context "when the client is eligible" do
-      let(:intake) { create :state_file_az_intake }
       before do
         session[:state_file_intake] = intake.to_global_id
+        allow(subject).to receive(:params).and_return({us_state: 'az'})
       end
 
       it "gets the correct values for ineligible_reason" do
-        allow(subject).to receive(:params).and_return({us_state: 'az'})
-
         expect(subject.ineligible_reason).to be_nil
       end
     end
