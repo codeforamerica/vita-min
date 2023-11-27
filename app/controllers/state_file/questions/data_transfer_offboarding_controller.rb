04f80f28ed64846d135ea7863f6fd9dc7f90f9b6
Ricardo Kreyhsig <rkreyhsig@codeforamerica.org>
2023-11-27 12:07:56 -0500

    i18n
    Co-authored-by: Em Barnard-Shao <ebarnard@codeforamerica.org>

--- app/controllers/state_file/questions/data_transfer_offboarding_controller.rb
+++ app/controllers/state_file/questions/data_transfer_offboarding_controller.rb
@@ -1,12 +1,10 @@
 module StateFile
   module Questions
     class DataTransferOffboardingController < QuestionsController
-      # before_action :set_prev_path, only: [:edit]
       helper_method :ineligible_reason
 
       def ineligible_reason
         key = current_intake.disqualifying_eligibility_answer
-        binding.pry
         I18n.t("state_file.questions.eligibility_offboarding.edit.ineligible_reason.#{key}", state: States.name_for_key(params[:us_state].upcase))
       end
 
