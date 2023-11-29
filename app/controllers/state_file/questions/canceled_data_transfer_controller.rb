a033294b729a5901e4f5dce5dd726453f3bdedbf
Jenny Heath <jheath@codeforamerica.org>
2023-11-29 15:51:57 -0800

    Add route for redirecting to state-specific page

--- app/controllers/state_file/questions/canceled_data_transfer_controller.rb
+++ app/controllers/state_file/questions/canceled_data_transfer_controller.rb
@@ -2,6 +2,7 @@ module StateFile
   module Questions
     class CanceledDataTransferController < QuestionsController
       include IrsDataTransferLinksConcern
+      before_action :redirect_if_no_us_state_in_params
 
       def self.show?(_intake)
         false
@@ -17,8 +18,27 @@ module StateFile
 
       private
 
-      def form_class
-        NullForm
+      # this page sometimes doesn't have us_state in the params so can't use the default current_intake
+      def current_intake
+        GlobalID.find(session[:state_file_intake])
+      end
+
+      # in order to give the IRS team one cancel link, this controller will redirect to the path with the us_state
+      # based on the intake in the session; part of the reason for this is so we can leverage the default implementation
+      # of
+      def redirect_if_no_us_state_in_params
+        return if params[:us_state]
+
+        if session[:state_file_intake].present?
+          if current_intake.class == StateFileAzIntake
+            redirect_link = az_questions_canceled_data_transfer_path(us_state: "az")
+          elsif current_intake.class == StateFileNyIntake
+            redirect_link = ny_questions_canceled_data_transfer_path(us_state: "ny")
+          end
+        else          # TODO: is there something else we should do if they have no intake in the session?
+          redirect_link = root_path
+        end
+        redirect_to redirect_link
       end
 
       def prev_path
