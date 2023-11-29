9becef04e9d6fc3e8bbce85726b66a2f0f6fdcf3
Jenny Heath <jheath@codeforamerica.org>
2023-11-29 15:58:07 -0800

    link to page from fake data transfer, fix error

--- app/controllers/state_file/questions/pending_federal_return_controller.rb
+++ app/controllers/state_file/questions/pending_federal_return_controller.rb
@@ -4,6 +4,13 @@ module StateFile
       def prev_path
         nil
       end
+
+      private
+
+      # edit page has no us_state in the params so default implementation of current_intake doesn't work
+      def current_intake
+        GlobalID.find(session[:state_file_intake])
+      end
     end
   end
 end
