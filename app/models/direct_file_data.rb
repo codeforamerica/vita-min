4a9a7ba33ef2b6d30609b8479f675ba1f04983f0
Tahsina Islam <79296603+tahsinaislam@users.noreply.github.com>
2023-12-01 09:58:08 -0500

    State Tax Withholdings (#3829)

--- app/models/direct_file_data.rb
+++ app/models/direct_file_data.rb
@@ -247,7 +247,21 @@ class DirectFileData
   end
 
   def total_state_tax_withheld
-    0 # TODO
+    total = 0
+    parsed_xml.css('IRSW2').map do |w2|
+      amt = w2.at('StateIncomeTaxAmt')&.text.to_i
+      total += amt
+    end
+    total
+  end
+
+  def total_local_tax_withheld
+    total = 0
+    parsed_xml.css('IRSW2').map do |w2|
+      amt = w2.at('LocalIncomeTaxAmt')&.text.to_i
+      total += amt
+    end
+    total
   end
 
   def fed_ctc_claimed
@@ -606,6 +620,7 @@ class DirectFileData
       :mailing_zip,
       :cell_phone_number,
       :tax_payer_email,
+      :total_state_tax_withheld,
       :fed_tax,
       :fed_agi,
       :fed_wages,
