e094e8aa03d61a57abc84032d764121e0ac11a6a
Jenny Heath <jheath@codeforamerica.org>
2023-12-01 14:58:02 -0800

    spouse inherits from primary
    Co-authored-by: Martha Pidcock <mpidcock@codeforamerica.org>

--- app/forms/state_file/ny_spouse_state_id_form.rb
+++ app/forms/state_file/ny_spouse_state_id_form.rb
@@ -1,5 +1,5 @@
 module StateFile
-  class NySpouseStateIdForm < StateIdForm
+  class NySpouseStateIdForm < NyPrimaryStateIdForm
     set_attributes_for :state_id, :id_type, :id_number, :state, :first_three_doc_num
 
     set_attributes_for :dates,
@@ -10,18 +10,10 @@ module StateFile
                        :expiration_date_month,
                        :expiration_date_year
 
-    validates :first_three_doc_num, alphanumeric: true, length: {is: 3}, unless: -> { id_type == "no_id" }
-
     private
 
     def self.record_type
       :spouse_state_id
-    end
-
-    def self.existing_state_id_attrs(state_id)
-      super.merge(
-        first_three_doc_num: state_id.first_three_doc_num,
-        )
     end
   end
 end
