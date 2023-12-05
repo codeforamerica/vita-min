40e9568e9ff9234346a4ad25774f70f979ad69ef
Lou Moore <lmoore@codeforamerica.org>
2023-12-05 11:06:03 -0800

    reviewed every MFS use case outlined in the ticket, fixed some issues, added lots of test coverage (#3848)

--- app/lib/efile/tax_calculator.rb
+++ app/lib/efile/tax_calculator.rb
@@ -26,12 +26,20 @@ module Efile
       @filing_status == :married_filing_jointly
     end
 
+    def filing_status_mfs?
+      @filing_status == :married_filing_separately
+    end
+
     def filing_status_single?
       @filing_status == :single
     end
 
     def filing_status_hoh?
       @filing_status == :head_of_household
+    end
+
+    def filing_status_qw?
+      @filing_status == :qualifying_widow
     end
 
     def line_or_zero(line)
