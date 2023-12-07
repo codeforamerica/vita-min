89c1b6e6d321c41f8083a71d77b09b825d91e3fe
Ricardo Kreyhsig <rkreyhsig@codeforamerica.org>
2023-12-07 19:51:37 -0300

    Add pending template

--- app/controllers/state_file/questions/return_status_controller.rb
+++ app/controllers/state_file/questions/return_status_controller.rb
@@ -2,8 +2,20 @@ module StateFile
   module Questions
     class ReturnStatusController < AuthenticatedQuestionsController
       helper_method :return_status
+      helper_method :title
 
       def edit; end
+
+      def title
+        case return_status
+        when 'accepted'
+          'Your 2023 Arizona state tax return is accepted'
+        when 'rejected'
+          'Unfortunately, your 2023 Arizona state tax return was rejected'
+        else
+          'You have submitted your 2023 Arizona tax return, and it is still waiting to be accepted.'
+        end
+      end
 
       def return_status
         case current_intake.efile_submissions.last.current_state
