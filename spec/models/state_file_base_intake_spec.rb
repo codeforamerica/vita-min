d451e50273b24df9796541bf639659ca8a5dea6f
Ricardo Kreyhsig <rkreyhsig@codeforamerica.org>
2023-12-11 15:38:47 -0500

    Add model spec

---
+++ spec/models/state_file_base_intake_spec.rb
@@ -0,0 +1,31 @@
+require "rails_helper"
+
+describe StateFileBaseIntake do
+
+  describe ".return_status" do
+    let!(:intake) { create :state_file_az_intake }
+    let!(:efile_submission) { create :efile_submission, data_source_id: intake.id, data_source_type: "StateFileAzIntake" }
+
+    context "with an accepted return" do
+      before { allow_any_instance_of(EfileSubmission).to receive(:current_state).and_return('accepted') }
+
+      it "it is accepted" do
+        expect(intake.return_status).to eq('accepted')
+      end
+    end
+
+    context "with a rejected return" do
+      before { allow_any_instance_of(EfileSubmission).to receive(:current_state).and_return('rejected') }
+      it "it is rejected" do
+        expect(intake.return_status).to eq('rejected')
+      end
+    end
+
+    context "with a pending return" do
+      before { allow_any_instance_of(EfileSubmission).to receive(:current_state).and_return('pending') }
+      it "it is pending" do
+        expect(intake.return_status).to eq('pending')
+      end
+    end
+  end
+end
