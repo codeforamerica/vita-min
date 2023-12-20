67427e0805e5b823af79093a614e853625931803
Tim O'Farrell <tofarrell@codeforamerica.org>
2023-12-20 10:06:41 -0700

    WIP

--- app/lib/submission_builder/ty2022/states/ny/individual_return.rb
+++ app/lib/submission_builder/ty2022/states/ny/individual_return.rb
@@ -112,7 +112,7 @@ module SubmissionBuilder
           end
 
           def schema_file
-            File.join(Rails.root, "vendor", "us_states", "unpacked", "NYSIndividual2023V4.0", "Common", "NysReturnState.xsd")
+            File.join(Rails.root, "vendor", "us_states", "unpacked", "NYSIndividual2023V3.0", "Common", "NysReturnState.xsd")
           end
 
           def attached_documents
