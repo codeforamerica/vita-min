0f95f8a18629087a1e9512233f68e96e85018408
Martha Pidcock <122047394+mpidcock@users.noreply.github.com>
2023-12-21 12:20:32 -0800

    Remove pre-population for `months-in-home` from the direct file data (#3914)
    Co-authored-by: Em Barnard-Shao <ebarnard@codeforamerica.org>

--- app/models/state_file_dependent.rb
+++ app/models/state_file_dependent.rb
@@ -34,6 +34,7 @@ class StateFileDependent < ApplicationRecord
   # Create birth_date_* accessor methods for Honeycrisp's cfa_date_select
   delegate :month, :day, :year, to: :dob, prefix: :dob, allow_nil: true
   validates_presence_of :first_name, :last_name, :dob, on: :dob_form
+  validates_presence_of :months_in_home, on: :dob_form, if: -> { self.intake_type == 'StateFileAzIntake' }
   validates :passed_away, :needed_assistance, inclusion: { in: %w[yes no], message: I18n.t("errors.messages.blank") }, on: :az_senior_form
 
   scope :az_qualifying_senior, -> do
