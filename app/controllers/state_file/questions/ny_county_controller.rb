ff3a71b01a458468cc1d470f386a89a5f8685ae6
Tim O'Farrell <tofarrell@codeforamerica.org>
2023-12-22 14:03:30 -0700

    When county changes, we make sure the school district is valid

--- app/controllers/state_file/questions/ny_county_controller.rb
+++ app/controllers/state_file/questions/ny_county_controller.rb
@@ -10,6 +10,23 @@ module StateFile
         NySchoolDistricts.county_labels_for_select
       end
 
+      def next_path
+        # If the current school district is not in the chosen county, the next step is school district selection
+        residence_county = current_intake.residence_county
+        school_district_options = NySchoolDistricts.county_school_districts_labels_for_select(residence_county)
+        school_district_names = school_district_options.pluck(1)
+        if school_district_names.include?(current_intake.school_district)
+          super
+        else
+          step_for_next_path = StateFile::Questions::NySchoolDistrictController
+          options = { us_state: params[:us_state], action: step_for_next_path.navigation_actions.first }
+          if params[:return_to_review]
+            options[:return_to_review] = 'y'
+          end
+          step_for_next_path.to_path_helper(options)
+        end
+      end
+
       private
 
       def illustration_path; end
