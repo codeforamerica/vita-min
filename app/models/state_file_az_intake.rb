117a201c31e696efe76404afa812a1891afc8b2e
Ricardo Kreyhsig <2385700+rickreyhsig@users.noreply.github.com>
2023-12-28 14:50:08 -0500

    AZ bug: Dependents over 17 not being counted in AZ-140 10b (#3937)
    * AZ bug: Dependents over 17 not being counted in AZ-140 10b

--- app/models/state_file_az_intake.rb
+++ app/models/state_file_az_intake.rb
@@ -110,12 +110,12 @@ class StateFileAzIntake < StateFileBaseIntake
     self.dependents.select{ |dependent| dependent.age < 17 }.length
   end
 
-  def federal_dependent_count_over_17
-    self.dependents.select{ |dependent| dependent.age >= 17 }.length
+  def federal_dependent_count_over_17_non_senior
+    self.dependents.select{ |dependent| dependent.age >= 17 && !dependent.ask_senior_questions? }.length
   end
 
   def qualifying_parents_and_grandparents
-    dependents.select(&:ask_senior_questions?).length
+    self.dependents.select(&:ask_senior_questions?).length
   end
 
   def ask_months_in_home?
