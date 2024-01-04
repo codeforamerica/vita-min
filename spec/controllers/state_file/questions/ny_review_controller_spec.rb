4491d427bbdeb0ab1acf969f8b480aa4854b572d
tofarr <tofarrell@codeforamerica.org>
2024-01-04 16:49:59 -0700

    Only Displaying Months in Home if Present #186728244 (#3957)
    * Only Displaying Months in Home if Present #186728244
    * Added specs

--- spec/controllers/state_file/questions/ny_review_controller_spec.rb
+++ spec/controllers/state_file/questions/ny_review_controller_spec.rb
@@ -29,5 +29,18 @@ RSpec.describe StateFile::Questions::NyReviewController do
         expect(refund_or_owed_label).to eq I18n.t("state_file.questions.shared.review_header.your_refund")
       end
     end
+
+    context "when a dependent is present" do
+      render_views
+      let(:intake) { create :state_file_ny_refund_intake }
+      let!(:dependent) { intake.dependents.create(dob: 7.years.ago, first_name: "Bobby", last_name: "Tables", relationship: "Son") }
+
+      it "displayed dependent details" do
+        get :edit, params: { us_state: "ny" }
+        expect(response.body).to include I18n.t("state_file.questions.shared.review_header.dependent_dob")
+        expect(response.body).to include "Bobby Tables"
+        expect(response.body).not_to include I18n.t("state_file.questions.shared.review_header.dependent_months_in_home")
+      end
+    end
   end
 end
