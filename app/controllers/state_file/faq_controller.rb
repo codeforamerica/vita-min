a2fd53f87d2a05439b2e4c15444c86aec71384f6
Em Barnard-Shao <ebarnard@codeforamerica.org>
2024-01-08 15:35:31 -0500

    pr changes

--- app/controllers/state_file/faq_controller.rb
+++ app/controllers/state_file/faq_controller.rb
@@ -17,25 +17,5 @@ class StateFile::FaqController < ApplicationController
     @question_key = params[:question_key].underscore
     @faq_item = FaqCategory.find_by(slug: @section_key)&.faq_items&.find_by(slug: @question_key)
     raise ActionController::RoutingError.new('Not found') unless @faq_item
-
-    @survey = FaqSurvey.find_or_initialize_by(visitor_id: visitor_id, question_key: @question_key)
-
-    if params[:diy_chat_with_us_token].present?
-      DiyIntake.find_by(token: params[:diy_chat_with_us_token], clicked_chat_with_us_at: nil)&.touch(:clicked_chat_with_us_at)
-    end
-  end
-
-  def answer_survey
-    @question_key = params[:question_key].underscore
-    @survey = FaqSurvey.find_or_initialize_by(visitor_id: visitor_id, question_key: @question_key)
-
-    @survey.update(params.require(:faq_survey).permit(:answer))
-    redirect_to faq_question_path(section_key: params[:section_key], question_key: @question_key)
-  end
-
-  private
-
-  def include_analytics?
-    true
   end
 end
