324407f69bf8b101d01eaadaebd6913b96608469
Em Barnard-Shao <ebarnard@codeforamerica.org>
2024-01-08 18:16:03 -0500

    Replace FAQ links with real FAQ link on state file pages

--- app/controllers/state_file/questions/data_transfer_offboarding_controller.rb
+++ app/controllers/state_file/questions/data_transfer_offboarding_controller.rb
@@ -3,6 +3,12 @@ module StateFile
     class DataTransferOffboardingController < StateFile::Questions::QuestionsController
       helper_method :ineligible_reason
 
+      def edit
+        super
+        product_type = params[:us_state] == 'az' ? "state_file_az" : "state_file_ny"
+        @learn_more_link = FaqCategory.where(slug: "other_state_filing_options", product_type: product_type).present? ? state_faq_section_path(section_key: "other_state_filing_options") : state_faq_path
+      end
+
       def ineligible_reason
         key = current_intake.disqualifying_df_data_reason
         if key.present?
