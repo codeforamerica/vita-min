2a0248553317c17b6f4b7dfb543694a11162470c
Mike Rotondo <mrotondo@codeforamerica.org>
2024-01-17 19:11:24 -0500

    barcodes in NY form PDFs (#3998)
    Draw properly sized and positioned barcodes on NY form PDFs that includes the CfA NACTP vendor code, and occlude the baked-in barcodes

--- app/lib/pdf_filler/ny214_pdf.rb
+++ app/lib/pdf_filler/ny214_pdf.rb
@@ -6,6 +6,16 @@ module PdfFiller
       "it214-TY2023"
     end
 
+    def nys_form_type
+      "214"
+    end
+
+    delegate :tax_year, to: :@submission
+
+    def barcode_overlay_rect
+      [[0, 26], 125, 29]
+    end
+
     def initialize(submission)
       @submission = submission
 
