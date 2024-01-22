eb8a1eee044f157e403a2c5642f18081355c7011
em barnard-shao <49880002+embarnard@users.noreply.github.com>
2024-01-22 17:22:20 -0500

    Remove production guard keeping statefile domain unlaunched (#4065)
    Co-authored-by: Mike Rotondo <mrotondo@codeforamerica.org>

--- app/lib/routes/state_file_domain.rb
+++ app/lib/routes/state_file_domain.rb
@@ -1,8 +1,6 @@
 module Routes
   class StateFileDomain
     def matches?(request)
-      return false if Rails.env.production?
-
       MultiTenantService.new(:statefile).host == request.host
     end
   end
