eccbbc08ae307d0b439519c7d990b298ff4e340e
Ricardo Kreyhsig <2385700+rickreyhsig@users.noreply.github.com>
2024-01-22 19:34:43 -0500

    NY: Hide "Numbers of months they lived with you" from Review page (#4066)

--- app/controllers/state_file/questions/az_review_controller.rb
+++ app/controllers/state_file/questions/az_review_controller.rb
@@ -4,6 +4,7 @@ module StateFile
       def edit
         @refund_or_owed_amount = current_intake.calculated_refund_or_owed_amount
         @refund_or_owed_label = @refund_or_owed_amount.positive? ? I18n.t("state_file.questions.shared.review_header.your_refund") : I18n.t("state_file.questions.shared.review_header.your_tax_owed")
+        @show_dependent_months_in_home = true
       end
 
       private
