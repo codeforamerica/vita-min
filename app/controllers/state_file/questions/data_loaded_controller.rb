d9ae337f6c3de63b21bb022346279f68c86152a7
Tim O'Farrell <tofarrell@codeforamerica.org>
2024-01-24 11:31:48 -0700

    Updated comments

--- app/controllers/state_file/questions/data_loaded_controller.rb
+++ app/controllers/state_file/questions/data_loaded_controller.rb
@@ -8,11 +8,6 @@ module StateFile
           current_intake&.disqualifying_df_data_reason.present?
           redirect_to next_path and return
         end
-        # Is there an existing intake with the same SSN?
-        # We have an intake.hashed_ssn
-        # current_intake.primary.ssn
-        # existing_intake = current_intake.class.where.not(id: current_intake.id).where()
-        # Do we need to somehow match existing here?
         StateFileEfileDeviceInfo.find_or_create_by!(
           event_type: "initial_creation",
           ip_address: ip_for_irs,
