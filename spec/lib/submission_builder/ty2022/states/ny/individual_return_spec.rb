d3633597bd26d98f01df9f88cbd48d8346c0eba3
Tahsina Islam <tislam@codeforamerica.org>
2024-01-26 17:53:40 -0500

    Fix test

--- spec/lib/submission_builder/ty2022/states/ny/individual_return_spec.rb
+++ spec/lib/submission_builder/ty2022/states/ny/individual_return_spec.rb
@@ -145,12 +145,12 @@ describe SubmissionBuilder::Ty2022::States::Ny::IndividualReturn do
       let(:intake) { create(:state_file_ny_intake) }
       let(:filing_status) { 'single' }
       before do
-        intake.direct_file_data.mailing_street = '211212 SUBDIVISION DRIVELINE #157'
+        intake.direct_file_data.mailing_street = '211212 SUBDIVISION DR POBOX #157'
       end
       it 'truncates under 30 characters' do
         xml = described_class.build(submission).document
         expect(xml.at("tiPrime MAIL_LN_2_ADR").text.length).to be <= 30
-        expect(xml.at("tiPrime MAIL_LN_2_ADR").text).to eq('211212 SUBDIVISION DRIVELINE')
+        expect(xml.at("tiPrime MAIL_LN_2_ADR").text).to eq('211212 SUBDIVISION DR POBOX')
         expect(xml.at("tiPrime MAIL_LN_1_ADR").text).to eq('#157')
       end
     end
