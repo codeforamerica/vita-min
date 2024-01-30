bc8877def4920ae6f8cff3e2a93b7be7cdc1bd06
Ricardo Kreyhsig <rkreyhsig@codeforamerica.org>
2024-01-30 16:57:47 -0500

    Fix terms & conditions
    Co-authored-by: Ricardo Kreyhsig <rkreyhsig@codeforamerica.org>

--- app/controllers/state_file/questions/terms_and_conditions_controller.rb
+++ app/controllers/state_file/questions/terms_and_conditions_controller.rb
@@ -1,6 +1,18 @@
 module StateFile
   module Questions
     class TermsAndConditionsController < QuestionsController
+
+      def edit
+        @li_items = I18n.t('state_file.questions.terms_and_conditions.edit.list_items_html').split('</li>')
+        unless Flipper.enabled?(:sms_notifications)
+          @li_items.each_with_index do |li_item, idx|
+            if li_item.include?('SMS')
+              @li_items[idx] = I18n.t('state_file.questions.terms_and_conditions.edit.list_items_no_sms_html')
+            end
+          end
+        end
+        # binding.pry
+      end
     end
   end
 end
