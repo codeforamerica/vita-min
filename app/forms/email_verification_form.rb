2abebbf4062973b676ff430d3f23bfa55e2fa993
Tim O'Farrell <tofarrell@codeforamerica.org>
2024-01-31 08:13:25 -0700

    Spec fix

--- app/forms/email_verification_form.rb
+++ app/forms/email_verification_form.rb
@@ -8,9 +8,12 @@
       # Magic codes provide a way of bypassing security in a development context.
       # The easiest way to do this was to update the last entry to actually have the magic code.
       if Rails.configuration.allow_magic_verification_code && verification_code == "000000"
-        EmailAccessToken.where(email_address: @intake.email_address).last.update(
-          token: Devise.token_generator.digest(EmailAccessToken, :token, hashed_verification_code),
-        )
+        token = EmailAccessToken.where(email_address: @intake.email_address).last
+        if token.present?
+          token.update(
+            token: Devise.token_generator.digest(EmailAccessToken, :token, hashed_verification_code),
+          )
+        end
         return true
       end
 
