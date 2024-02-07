32337c3e9ff61c3785d1213574cea211635c4ecb
Tim O'Farrell <tofarrell@codeforamerica.org>
2024-02-07 09:37:31 -0700

    WIP

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -15,8 +15,17 @@ module ApplicationCable
     end
 
     def current_state_file_intake
-      binding.pry
-      current_state_file_ny_intake || current_state_file_az_intake
+      return @current_state_file_intake if @current_state_file_intake
+      model_key = @request.session.keys.detect do |key|
+        key.starts_with?("warden.user.") && key.ends_with?(".key")
+      end
+      model_id = @request.session[model_key].first.first
+      model_name = model_key.delete_prefix("warden.user.").delete_suffix(".key")
+      model_class = model_name.camelize.constantize
+      @current_state_file_intake = model_class.find(model_id)
+      @current_state_file_intake
+    rescue
+      DatadogApi.increment "application_cable.uncaught_throw_warden_error"
     end
   end
 end
