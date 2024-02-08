46c540d95991ae7ef04826943ad78a060f4073ac
Tim O'Farrell <tofarrell@codeforamerica.org>
2024-02-08 08:40:55 -0700

    WIP

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -15,17 +15,14 @@ module ApplicationCable
     end
 
     def current_state_file_intake
-      return @current_state_file_intake if @current_state_file_intake
-      model_key = @request.session.keys.detect do |key|
-        key.starts_with?("warden.user.") && key.ends_with?(".key")
-      end
-      model_id = @request.session[model_key].first.first
-      model_name = model_key.delete_prefix("warden.user.").delete_suffix(".key")
-      model_class = model_name.camelize.constantize
-      @current_state_file_intake = model_class.find(model_id)
-      @current_state_file_intake
-    rescue
+      warden = env['warden']
+      @current_state_file_intake ||= warden.user(:state_file_az_intake) || warden.user(:state_file_ny_intake)
+    rescue UncaughtThrowError => e
+      raise unless e.tag == :warden
+
+      # 'uncaught throw :warden' is fired in certain circumstances, this here is to silence it
       DatadogApi.increment "application_cable.uncaught_throw_warden_error"
+      reject_unauthorized_connection
     end
   end
 end
