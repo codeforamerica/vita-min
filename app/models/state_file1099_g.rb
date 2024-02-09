de3c7391167a0cc95e59980c61296fe8aa0171f1
Tahsina Islam <tislam@codeforamerica.org>
2024-02-09 18:11:11 -0500

    fix zip validation

--- app/models/state_file1099_g.rb
+++ app/models/state_file1099_g.rb
@@ -40,7 +40,7 @@ class StateFile1099G < ApplicationRecord
   validates_presence_of :payer_name, :message => I18n.t("errors.attributes.payer_name.blank")
   validates_presence_of :payer_street_address, :message => I18n.t("errors.attributes.address.street_address.blank")
   validates_presence_of :payer_city, :message => I18n.t("errors.attributes.address.city.blank")
-  validates_presence_of :payer_zip, :message => I18n.t("errors.attributes.address.zip.blank")
+  validates :payer_zip, zip_code: true
   validates :payer_tin, format: { :with => /\d{9}/, :message => I18n.t("errors.attributes.payer_tin.blank")}
   validates_presence_of :state_identification_number, :message => I18n.t("errors.attributes.state_id_number.empty")
   validates_presence_of :recipient_city
