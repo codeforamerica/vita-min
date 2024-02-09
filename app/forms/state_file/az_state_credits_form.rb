94fdea1638f800170fdca53537b47a80be62deb8
Tim O'Farrell <tofarrell@codeforamerica.org>
2024-02-09 08:14:23 -0700

    Number validation #187003599

--- app/forms/state_file/az_state_credits_form.rb
+++ app/forms/state_file/az_state_credits_form.rb
@@ -2,8 +2,8 @@ module StateFile
   class AzStateCreditsForm < QuestionsForm
     set_attributes_for :intake, :tribal_member, :tribal_wages, :armed_forces_member, :armed_forces_wages
 
-    validates :tribal_wages, presence: true, allow_blank: false, if: -> { tribal_member == "yes" }
-    validates :armed_forces_wages, presence: true, allow_blank: false, if: -> { armed_forces_member == "yes" }
+    validates :tribal_wages, presence: true, allow_blank: false, gyr_numericality: { only_integer: true, greater_than_or_equal_to: 1 }, if: -> { tribal_member == "yes" }
+    validates :armed_forces_wages, presence: true, allow_blank: false, gyr_numericality: { only_integer: true, greater_than_or_equal_to: 1 }, if: -> { armed_forces_member == "yes" }
 
     def save
       attributes_to_save = attributes_for(:intake)
