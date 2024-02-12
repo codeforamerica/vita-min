04a8e96840a9e9253c92c69c943587896e995054
Ricardo Kreyhsig <rkreyhsig@codeforamerica.org>
2024-02-12 17:19:08 -0500

    WIP
    Co-authored-by: Ricardo Kreyhsig <rkreyhsig@codeforamerica.org>

--- app/controllers/state_file/questions/terms_and_conditions_controller.rb
+++ app/controllers/state_file/questions/terms_and_conditions_controller.rb
@@ -2,6 +2,12 @@ module StateFile
   module Questions
     class TermsAndConditionsController < QuestionsController
 
+
+      # def update
+      #   binding.pry
+      #   @li_items = I18n.t('state_file.questions.terms_and_conditions.edit.list_items_html', privacy_policy_link: state_file_privacy_policy_path).split('</li>')
+      # end
+
       def edit
         @li_items = I18n.t('state_file.questions.terms_and_conditions.edit.list_items_html', privacy_policy_link: state_file_privacy_policy_path).split('</li>')
         unless Flipper.enabled?(:sms_notifications)
