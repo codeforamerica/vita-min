aa22839f308c22e4bc50e04e625570cc0a5c0d7a
Ricardo Kreyhsig <rkreyhsig@codeforamerica.org>
2024-02-13 19:07:40 -0500

    Add validation and specs
    Co-authored-by: Ricardo Kreyhsig <rkreyhsig@codeforamerica.org>

--- app/forms/state_file/terms_and_conditions_form.rb
+++ app/forms/state_file/terms_and_conditions_form.rb
@@ -1,7 +1,7 @@
 module StateFile
   class TermsAndConditionsForm < QuestionsForm
     set_attributes_for :intake, :consented_to_terms_and_conditions
-    validates :consented_to_terms_and_conditions, presence: true
+    validates :consented_to_terms_and_conditions, inclusion: { in: ['yes', 'no'] }
 
     def save
       @intake.update(attributes_for(:intake))
