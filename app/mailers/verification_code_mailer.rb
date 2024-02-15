b98948fcd07366f8705a976e797f3cbd473ff293
Ricardo Kreyhsig <rkreyhsig@codeforamerica.org>
2024-02-15 16:54:49 -0500

    Singl quotes
    Co-authored-by: Ricardo Kreyhsig <rkreyhsig@codeforamerica.org>

--- app/mailers/verification_code_mailer.rb
+++ app/mailers/verification_code_mailer.rb
@@ -7,9 +7,9 @@ class VerificationCodeMailer < ApplicationMailer
     @verification_code = params[:verification_code]
     attachments.inline['logo.png'] = service.email_logo
     @subject = if @service_type == :statefile
-                 I18n.t("messages.verification_code_subject_with_service_name", service_name: @service_name, locale: @locale)
+                 I18n.t('messages.verification_code_subject_with_service_name', service_name: @service_name, locale: @locale)
                else
-                 I18n.t("messages.default_subject_with_service_name", service_name: @service_name, locale: @locale)
+                 I18n.t('messages.default_subject_with_service_name', service_name: @service_name, locale: @locale)
                end
     mail(to: params[:to], subject: @subject, from: service.noreply_email, delivery_method_options: service.delivery_method_options)
   end
