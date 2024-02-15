bacd4e0dcca361c51d246a0216e70a599c9a41e5
Em Barnard-Shao <ebarnard@codeforamerica.org>
2024-02-15 13:53:57 -0500

    Add tests

--- spec/services/state_file/messaging_service_spec.rb
+++ spec/services/state_file/messaging_service_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-describe StateFile::MessagingService, active_job: true do
+describe StateFile::MessagingService do
   let(:intake) { create :state_file_az_intake, primary_first_name: "Mona", email_address: "mona@example.com", email_address_verified_at: 1.minute.ago, message_tracker: {} }
   let(:message) { StateFile::AutomatedMessage::Welcome }
   let!(:messaging_service) { described_class.new(message: message, intake: intake) }
@@ -10,14 +10,12 @@ describe StateFile::MessagingService, active_job: true do
   end
 
   context "when has an email_address" do
-    it "returns a hash with the output of send_email as the value for outgoing_email" do
-      expect(messaging_service.send_message).to eq(message.name)
-    end
-
     it "creates an email" do
       expect do
         messaging_service.send_message
       end.to change(StateFileNotificationEmail, :count).by(1)
+
+      expect(intake.message_tracker).to include "messages.state_file.welcome"
     end
   end
 end
