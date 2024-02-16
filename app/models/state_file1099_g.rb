354ffbca2cb3ffe603599a202303c79e1a360633
Tahsina Islam <tislam@codeforamerica.org>
2024-02-16 18:01:42 -0500

    fix test

--- app/models/state_file1099_g.rb
+++ app/models/state_file1099_g.rb
@@ -44,7 +44,7 @@ class StateFile1099G < ApplicationRecord
   validates :payer_tin, format: { :with => /\A\d{9}\z/, :message => I18n.t("errors.attributes.payer_tin.invalid")}
   validates_presence_of :state_identification_number, :message => I18n.t("errors.attributes.state_id_number.empty")
   validates :recipient_street_address, presence: true, irs_street_address_type: true, format: { :with => /[a-zA-Z0-9\-\/ ]+/.freeze, message: I18n.t("errors.attributes.address.street_address.invalid")}
-  validates :recipient_street_address_apartment, format: { :with => /[a-zA-Z0-9\-\/ ]+/.freeze, message: I18n.t("errors.attributes.address.street_address.invalid")}
+  validates :recipient_street_address_apartment, format: { :with => /[a-zA-Z0-9\-\/ ]+/.freeze, message: I18n.t("errors.attributes.address.street_address.invalid"), allow_blank: true
   validates :recipient_city, presence: true, irs_street_address_type: true, format: { with: /[A-Za-z\s]/.freeze, message: I18n.t("errors.attributes.address.city.invalid")}
   validates :recipient_zip, zip_code: true
   validates :unemployment_compensation, numericality: { greater_than_or_equal_to: 1, only_integer: true }
