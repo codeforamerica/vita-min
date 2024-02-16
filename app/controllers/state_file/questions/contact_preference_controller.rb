c10548cf6fc9bcab8012ff6db85ff7e66995b09e
Ricardo Kreyhsig <rkreyhsig@codeforamerica.org>
2024-02-16 17:29:22 -0500

    WIP
    Co-authored-by: Ricardo Kreyhsig <rkreyhsig@codeforamerica.org>

--- app/controllers/state_file/questions/contact_preference_controller.rb
+++ app/controllers/state_file/questions/contact_preference_controller.rb
@@ -1,7 +1,10 @@
 module StateFile
   module Questions
     class ContactPreferenceController < QuestionsController
+=begin
       def update
+        # current_intake.update(permitted_params)
+        binding.pry
         current_intake.update(intake_params)
         redirect_to next_path
       end
@@ -10,15 +13,36 @@ module StateFile
       def next_path
         case params['us_state']
         when 'az'
-          az_questions_email_address_path
-        when 'ny'
-          ny_questions_email_address_path
+          case current_intake.contact_preference
+          when 'text'
+            az_questions_phone_number_path
+          else # 'email'
+            az_questions_email_address_path
+          end
+        else # 'ny'
+          case current_intake.contact_preference
+          when 'text_message'
+            ny_questions_phone_number_path
+          else
+            ny_questions_email_address_path
+          end
         end
       end
 
       def intake_params
         params.permit(:locale)
       end
+=end
+
+=begin
+      def state_file_contact_preference_params
+        params.require(:state_file_contact_preference_form).permit(:contact_preference)
+      end
+
+      def permitted_params
+        intake_params.merge(state_file_contact_preference_params)
+      end
+=end
     end
   end
 end
--- app/forms/state_file/contact_preference_form.rb
+++ app/forms/state_file/contact_preference_form.rb
@@ -1,7 +1,8 @@
 module StateFile
   class ContactPreferenceForm < QuestionsForm
-    set_attributes_for :intake, :contact_preference
+    set_attributes_for :intake, :contact_preference#, :locale
     def save
+      binding.pry
       @intake.update(attributes_for(:intake))
     end
   end
