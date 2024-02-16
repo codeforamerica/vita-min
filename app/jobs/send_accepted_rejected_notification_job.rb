02913d92566a6bb5db84fdef58c5668e1ac53ff9
em barnard-shao <49880002+embarnard@users.noreply.github.com>
2024-02-16 16:21:17 -0500

    Add job that sends notification messages to clients with accepted/rejected states (#4241)

---
+++ app/jobs/send_accepted_rejected_notification_job.rb
@@ -0,0 +1,35 @@
+class SendAcceptedRejectedNotificationJob < ApplicationJob
+  def perform(submission_ids)
+    submission_ids.each do |id|
+      send_notification(id)
+    end
+  end
+
+  def send_notification(efile_submission_id)
+    submission = EfileSubmission.for_state_filing.find(efile_submission_id)
+    if submission.nil?
+      Rails.logger.error("*****Error: No efile submission found with id #{efile_submission_id}")
+      return
+    end
+
+    messaging_service = StateFile::AfterTransitionMessagingService.new(submission)
+
+    case submission.current_state
+    when "accepted"
+      message = messaging_service.send_efile_submission_accepted_message
+      Rails.logger.info("*****Sent accepted message to EfileSubmission ##{efile_submission_id}") if message.present?
+    when "rejected"
+      message = messaging_service.send_efile_submission_rejected_message
+      Rails.logger.info("*****Sent rejected message to EfileSubmission ##{efile_submission_id}") if message.present?
+    else
+      Rails.logger.error("*****Current state '#{submission.current_state}' doesn't qualify")
+    end
+
+    Rails.logger.error("*********No message sent for EfileSubmission ##{efile_submission_id}") unless message
+    Rails.logger.info("*********StateFileNotificationEmail ##{message.id} created") if message.present?
+  end
+
+  def priority
+    PRIORITY_LOW
+  end
+end
