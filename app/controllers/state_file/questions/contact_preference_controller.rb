aadeedb3d5df779cbc413d00ee1ea296cca57ef2
Ricardo Kreyhsig <rkreyhsig@codeforamerica.org>
2024-02-16 15:36:25 -0500

    Save locale and redirect
    Co-authored-by: Ricardo Kreyhsig <rkreyhsig@codeforamerica.org>

--- app/controllers/state_file/questions/contact_preference_controller.rb
+++ app/controllers/state_file/questions/contact_preference_controller.rb
@@ -1,6 +1,24 @@
 module StateFile
   module Questions
     class ContactPreferenceController < QuestionsController
+      def update
+        current_intake.update(intake_params)
+        redirect_to next_path
+      end
+
+      private
+      def next_path
+        case params['us_state']
+        when 'az'
+          az_questions_email_address_path
+        when 'ny'
+          ny_questions_email_address_path
+        end
+      end
+
+      def intake_params
+        params.permit(:locale)
+      end
     end
   end
 end
