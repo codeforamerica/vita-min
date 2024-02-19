c8a219aa6797ae94d922637f41d73e5c5009dec5
Ricardo Kreyhsig <rkreyhsig@codeforamerica.org>
2024-02-19 16:21:55 -0500

    Add locale to messaging_service
    Co-authored-by: Ricardo Kreyhsig <rkreyhsig@codeforamerica.org>

--- app/services/state_file/messaging_service.rb
+++ app/services/state_file/messaging_service.rb
@@ -1,9 +1,10 @@
 module StateFile
   class MessagingService
+    DEFAULT_LOCALE = 'en'
     attr_accessor :locale, :message, :intake, :submission, :locale, :message_tracker, :sent_messages
 
     def initialize(message:, intake:, submission: nil, locale: nil, sms: true, email: true, body_args: {})
-      @locale = locale || "en" # TODO: add intake.locale once we start collecting it
+      @locale = intake.locale || DEFAULT_LOCALE
       @message = message
       @message_instance = message.new
       @intake = intake
