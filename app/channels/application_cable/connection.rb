f870f7ed63137d36f1220b541f7a528702a48442
Tim O'Farrell <tofarrell@codeforamerica.org>
2024-02-22 10:15:54 -0700

    WIP

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -16,7 +16,7 @@ module ApplicationCable
 
     def current_state_file_intake
       warden = env['warden']
-      @current_state_file_intake = StateFileBaseIntake::STATE_CODES.lazy.map{|c| warden.user("state_file_#{c}_intake") }.find(&:itself)
+      @current_state_file_intake = StateFileBaseIntake::STATE_CODES.lazy.map{|c| warden.user("state_file_#{c}_intake".to_sym) }.find(&:itself)
     rescue UncaughtThrowError => e
       raise unless e.tag == :warden
 
