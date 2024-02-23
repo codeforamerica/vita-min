cc03c66c2cb0aa83c01ac72e1812286b48369c4b
Ricardo Kreyhsig <rkreyhsig@codeforamerica.org>
2024-02-23 16:52:51 -0500

    REmove ap commment from experiment_service.rb
    Co-authored-by: Ricardo Kreyhsig <rkreyhsig@codeforamerica.org>

--- app/services/experiment_service.rb
+++ app/services/experiment_service.rb
@@ -68,7 +68,6 @@ class ExperimentService
   def self.ensure_experiments_exist_in_database
     CONFIG.each do |key, _details|
       experiment = Experiment.find_or_create_by(key: key)
-      ap experiment
       if experiment.name.blank?
         experiment.update(name: CONFIG[key][:name])
       end
