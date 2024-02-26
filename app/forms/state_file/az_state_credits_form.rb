6efd742a9c2cc4685bd5c6f33d7f6780e75f556c
Tahsina Islam <tislam@codeforamerica.org>
2024-02-26 13:35:39 -0600

    Fix error message display
    Co-authored-by: Jenny Heath <jheath@codeforamerica.org>

--- app/forms/state_file/az_state_credits_form.rb
+++ app/forms/state_file/az_state_credits_form.rb
@@ -4,10 +4,7 @@ module StateFile
 
     validates :tribal_wages, presence: true, allow_blank: false, numericality: { only_integer: true, greater_than_or_equal_to: 1 }, if: -> { tribal_member == "yes" }
     validates :armed_forces_wages, presence: true, allow_blank: false, numericality: { only_integer: true, greater_than_or_equal_to: 1 }, if: -> { armed_forces_member == "yes" }
-
-    with_options if: -> { tribal_wages.present? || armed_forces_wages.present? } do
-      validate :exceeds_1040_amount
-    end
+    validate :below_1040_amount, if: -> { tribal_wages.present? || armed_forces_wages.present? }
 
     def save
       attributes_to_save = attributes_for(:intake)
@@ -18,12 +15,13 @@ module StateFile
 
     private
 
-    def exceeds_1040_amount
+    def below_1040_amount
       amount_limit = @intake.direct_file_data.fed_wages_salaries_tips
       total = self.tribal_wages.to_i + self.armed_forces_wages.to_i
       if total > amount_limit
-        errors.add(:tribal_wages, I18n.t("forms.errors.state_credit.exceeds_limit", limit: amount_limit))
-        errors.add(:armed_forces_wages, I18n.t("forms.errors.state_credit.exceeds_limit", limit: amount_limit))
+        errors.add(:base, I18n.t("forms.errors.state_credit.exceeds_limit", limit: amount_limit))
+        errors.add(:tribal_wages, "")
+        errors.add(:armed_forces_wages, "")
       end
     end
   end
