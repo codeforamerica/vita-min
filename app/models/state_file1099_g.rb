51cdabfdab79fb6f464ebfc4b422188d1c2e4e1a
Tahsina Islam <tislam@codeforamerica.org>
2024-02-26 12:07:19 -0600

    Fix city'

--- app/models/state_file1099_g.rb
+++ app/models/state_file1099_g.rb
@@ -39,13 +39,13 @@ class StateFile1099G < ApplicationRecord
   validates_inclusion_of :had_box_11, in: ['yes', 'no'], message: ->(_object, _data) { I18n.t("errors.messages.blank") }
   validates :payer_name, :presence => {:message => I18n.t("errors.attributes.payer_name.blank") }, format: { with: /(([A-Za-z0-9#-()]|&|') ?)*([A-Za-z0-9#-()]|&|')/.freeze, message: I18n.t("errors.attributes.payer_name.invalid")}
   validates :payer_street_address, :presence => {:message => I18n.t("errors.attributes.address.street_address.blank") }, format: { with: /\A[a-zA-Z0-9\/\s-]+\z/.freeze, message: I18n.t("errors.attributes.address.street_address.invalid")}
-  validates :payer_city, :presence => {:message => I18n.t("errors.attributes.address.city.blank")}, format: { with: /[A-Za-z\s]/.freeze, message: I18n.t("errors.attributes.address.city.invalid")}
+  validates :payer_city, :presence => {:message => I18n.t("errors.attributes.address.city.blank")}, format: { with: /\A[a-zA-Z\s]+\z/.freeze, message: I18n.t("errors.attributes.address.city.invalid")}
   validates :payer_zip, zip_code: true
   validates :payer_tin, format: { :with => /\A\d{9}\z/, :message => I18n.t("errors.attributes.payer_tin.invalid")}
   validates_presence_of :state_identification_number, :message => I18n.t("errors.attributes.state_id_number.empty")
   validates :recipient_street_address, presence: true, format: { :with => /\A[a-zA-Z0-9\/\s-]+\z/.freeze, message: I18n.t("errors.attributes.address.street_address.invalid")}
   validates :recipient_street_address_apartment, format: { :with => /\A[a-zA-Z0-9\/\s-]+\z/.freeze, message: I18n.t("errors.attributes.address.street_address.invalid")}, allow_blank: true
-  validates :recipient_city, presence: true, format: { with: /[A-Za-z\s]/.freeze, message: I18n.t("errors.attributes.address.city.invalid")}
+  validates :recipient_city, presence: true, format: { with: /\A[a-zA-Z\s]+\z/.freeze, message: I18n.t("errors.attributes.address.city.invalid")}
   validates :recipient_zip, zip_code: true
   validates :unemployment_compensation, numericality: { greater_than_or_equal_to: 1, only_integer: true }
   validates :federal_income_tax_withheld, numericality: { greater_than_or_equal_to: 0, only_integer: true }
