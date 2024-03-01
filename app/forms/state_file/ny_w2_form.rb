7634bdaf2707deadfa01d15e6e980879f51266c6
Tim O'Farrell <tofarrell@codeforamerica.org>
2024-03-01 09:14:30 -0700

    WIP

--- app/forms/state_file/ny_w2_form.rb
+++ app/forms/state_file/ny_w2_form.rb
@@ -1,10 +1,12 @@
 module StateFile
   class NyW2Form < QuestionsForm
+    attr_accessor :w2s
     validate :validate_w2s
 
     def initialize(intake = nil, params = nil)
       super
-      assign_w2_attributes unless params.nil?
+      @w2s = intake.state_file_w2s
+      assign_w2_params(params) unless params.nil?
     end
 
     def self.from_intake(intake)
@@ -17,39 +19,37 @@ module StateFile
     end
 
     def save
-      assign_nested_attributes
-
       @w2s.each(&:save!)
     end
 
     # assigning the @w2s variable does seem a little janky to me but i was in get-the-test-to-pass mode
-    def assign_nested_attributes
-      if @intake.state_file_w2s.present?
-        @intake.state_file_w2s.each do |w2|
-          matching_attrs = w2s_attributes.find do |_, attrs|
-            w2.w2_index == attrs[:w2_index]
-          end
-          w2.assign_attributes(matching_attrs[1])
-        end
-        @w2s = @intake.state_file_w2s
-      else
-        @w2s = w2s_attributes.map do |_, attrs|
-          StateFileW2.new(state_file_intake: @intake, **attrs)
-        end
+    # TODO: Should we refactor this to override the assign_attributes method?
+    def assign_w2_params(params)
+      @w2s.each_with_index do |state_file_w2, index|
+        params.fetch()
       end
-    end
 
-    # thinking we shuold put validations on the model level (StateFileW2) and call valid on each instance
-    #def valid?
-    #  super
-    #end
+      #if @intake.state_file_w2s.present?
+      #  @intake.state_file_w2s.each do |w2|
+      #    matching_attrs = w2s_attributes.find do |_, attrs|
+      #      w2.w2_index == attrs[:w2_index]
+      #    end
+      #    w2.assign_attributes(matching_attrs[1])
+      #  end
+      #  @w2s = @intake.state_file_w2s
+      #else
+      #  @w2s = w2s_attributes.map do |_, attrs|
+      #    StateFileW2.new(state_file_intake: @intake, **attrs)
+      #  end
+      #end
+    end
 
     def validate_w2s
       # TODO: Check that the w2s either are new or already belong to the intake
-      intake.state_file_w2s.each_with_index do |state_file_w2, index|
+      @w2s.each_with_index do |state_file_w2, index|
         unless state_file_w2.valid?
           state_file_w2.errors.each do |error|
-            #TODO: I'm pretty sure "key" and "message" attributes are not what I actually need
+            # TODO: I'm pretty sure "key" and "message" attributes are not what I actually need
             errors.add("w2[#{index}].#{error.key}", error.message)
           end
         end
