e8e8cf308d517b0040e40111a73053e9fe7d4d6d
Tim O'Farrell <tofarrell@codeforamerica.org>
2024-03-01 10:55:28 -0700

    WIP

--- app/controllers/state_file/questions/ny_w2_controller.rb
+++ app/controllers/state_file/questions/ny_w2_controller.rb
@@ -1,6 +1,8 @@
 module StateFile
   module Questions
     class NyW2Controller < AuthenticatedQuestionsController
+      before_action :create_w2_list
+
       def self.show?(intake)
         invalid_w2s(intake).any?
       end
@@ -12,31 +14,25 @@ module StateFile
 
       def edit
         # Show a single W2 to edit
-        super
-      end
-
-      def update
-        # Update a single W2 record
-        super
+        @form = @w2s[params[:index].to_i]
       end
 
       def self.navigation_actions
         [:index, :edit]
       end
 
-      def self.state_file_w2s(intake)
+      def create_w2_list(intake)
         # Generate a new array of unsaved W2s based on direct file data
-        state_file_w2s = intake.direct_file_data.w2s.map do |df_w2|
+        @w2s = intake.direct_file_data.w2s.map do |df_w2|
           StateFileW2.from_df_w2(df_w2)
         end
-        state_file_w2s.each_with_index do |state_file_w2, index|
+        @w2s.each_with_index do |state_file_w2, index|
           state_file_w2.index = index
         end
         # Override values in the array with any that are already persisted
         intake.state_file_w2s.each do |state_file_w2|
-          state_file_w2s[state_file_w2.index] = state_file_w2
+          @w2s[state_file_w2.index] = state_file_w2
         end
-        state_file_w2s
       end
 
       def self.invalid_w2s(intake)
@@ -45,10 +41,24 @@ module StateFile
 
       private
 
-      def form_params
-        # We relax the form constraint here - we are gonna cherry pick the ones we want anyway
-        params.fetch(form_name, {}) #.permit(*form_class.attribute_names)
+      def next_step
+        # Edits on this redirect back to index
+        #@w2s.detect { |w2| !w2.valid? }.present? ? self.class : super
+        self.class
       end
+
+      def form_params
+        params.require(StateFileW2.name.underscore).permit(*StateFileW2.attribute_names)
+      end
+
+      def form_class
+        StateFileW2
+      end
+
+      #def form_params
+        # We relax the form constraint here - we are gonna cherry pick the ones we want anyway
+      #  params.fetch(form_name, {}) #.permit(*form_class.attribute_names)
+      #end
 
       def self.invalid_w2?(intake, w2)
         return true if w2.StateWagesAmt == 0
