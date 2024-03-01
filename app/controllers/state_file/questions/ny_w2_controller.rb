ea8bb50ca56d535fe6f7ba18403f26dd13c4a24c
Jenny Heath <jheath@codeforamerica.org>
2024-03-01 12:24:07 -0800

    style index a bit

--- app/controllers/state_file/questions/ny_w2_controller.rb
+++ app/controllers/state_file/questions/ny_w2_controller.rb
@@ -10,6 +10,10 @@ module StateFile
       def index
         # Show the list of W2s and whether they are valid - we build this list on the fly based on existing and
         # new entries...
+        @w2s_with_metadata = @w2s.map do |w2|
+          dfw2 = w2.state_file_intake.direct_file_data.w2s[w2.w2_index]
+          [w2, dfw2.EmployerName]
+        end
       end
 
       def edit
