d8487dd001a20739e94c83939c004f840a223e21
Tim O'Farrell <tofarrell@codeforamerica.org>
2024-03-04 13:53:08 -0700

    WIP

--- app/models/state_file_w2.rb
+++ app/models/state_file_w2.rb
@@ -25,13 +25,27 @@ class StateFileW2 < ApplicationRecord
   validates :w2_index, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   validates :employer_state_id_num, format: { with: /\A(\d{0,17})\z/ }
-  validates :state_wages_amt, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-  validates :state_income_tax_amt, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-  validates :local_wages_and_tips_amt, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-  validates :local_income_tax_amt, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :state_wages_amt, numericality: { only_integer: true, greater_than_or_equal_to: 0 }, if: -> { state_wages_amt.present? }
+  validates :state_income_tax_amt, numericality: { only_integer: true, greater_than_or_equal_to: 0 }, if: -> { state_wages_amt.present? }
+  validates :local_wages_and_tips_amt, numericality: { only_integer: true, greater_than_or_equal_to: 0 }, if: -> { state_wages_amt.present? }
+  validates :local_income_tax_amt, numericality: { only_integer: true, greater_than_or_equal_to: 0 }, if: -> { state_wages_amt.present? }
   validates :locality_nm, presence: true, if: -> { local_wages_and_tips_amt.present? && local_wages_and_tips_amt.positive? }
   validates :local_wages_and_tips_amt, numericality: { only_integer: true, greater_than_or_equal_to: 1 }, if: -> { local_income_tax_amt.present? && local_income_tax_amt.positive? }
   validates :state_wages_amt, numericality: { only_integer: true, greater_than_or_equal_to: 1 }, if: -> { state_income_tax_amt.present? && state_income_tax_amt.positive? }
   validates :employer_state_id_num, presence: true, if: -> { state_wages_amt.present? && state_wages_amt.positive? }
+  validate :locality_nm_validation
+  before_validation :locality_nm_to_upper_case
+
+  def locality_nm_validation
+    unless state_file_intake_type.constantize.locality_nm_valid?(locality_nm)
+      errors.add(:locality_nm, I18n.t("state_file.questions.ny_w2.edit.locality_nm_error"))
+    end
+  end
+
+  def locality_nm_to_upper_case
+    if locality_nm.present?
+      self.locality_nm = locality_nm.upcase
+    end
+  end
 
 end
