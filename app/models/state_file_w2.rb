91ab0c5a3dd95e5e8c33975230d2839264fe8e40
jenny-heath <43800769+jenny-heath@users.noreply.github.com>
2024-03-05 13:06:26 -0800

    Fix w2 xml overrides (specifically W2StateLocalTaxGrp) #187136591 (#4304)

--- app/models/state_file_w2.rb
+++ app/models/state_file_w2.rb
@@ -20,6 +20,19 @@
 #  index_state_file_w2s_on_state_file_intake  (state_file_intake_type,state_file_intake_id)
 #
 class StateFileW2 < ApplicationRecord
+  STATE_TAX_GRP_TEMPLATE = <<~XML
+  <W2StateTaxGrp>
+    <StateAbbreviationCd></StateAbbreviationCd>
+    <EmployerStateIdNum></EmployerStateIdNum>
+    <StateWagesAmt></StateWagesAmt>
+    <StateIncomeTaxAmt></StateIncomeTaxAmt>
+    <W2LocalTaxGrp>
+      <LocalWagesAndTipsAmt></LocalWagesAndTipsAmt>
+      <LocalIncomeTaxAmt></LocalIncomeTaxAmt>
+      <LocalityNm></LocalityNm>
+    </W2LocalTaxGrp>
+  </W2StateTaxGrp>
+  XML
   belongs_to :state_file_intake, polymorphic: true
 
   validates :w2_index, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
@@ -34,4 +47,16 @@ class StateFileW2 < ApplicationRecord
   validates :state_wages_amt, numericality: { only_integer: true, greater_than_or_equal_to: 1 }, if: -> { state_income_tax_amt.present? && state_income_tax_amt.positive? }
   validates :employer_state_id_num, presence: true, if: -> { state_wages_amt.present? && state_wages_amt.positive? }
 
+  def state_tax_group_xml_node
+    xml_template = Nokogiri::XML(STATE_TAX_GRP_TEMPLATE)
+    xml_template.at(:StateAbbreviationCd).content = "NY"
+    xml_template.at(:EmployerStateIdNum).content = employer_state_id_num
+    xml_template.at(:StateWagesAmt).content = state_wages_amt
+    xml_template.at(:StateIncomeTaxAmt).content = state_income_tax_amt
+    xml_template.at(:LocalWagesAndTipsAmt).content = local_wages_and_tips_amt
+    xml_template.at(:LocalIncomeTaxAmt).content = local_income_tax_amt
+    xml_template.at(:LocalityNm).content = locality_nm
+
+    xml_template.at(:W2StateTaxGrp).to_xml
+  end
 end
