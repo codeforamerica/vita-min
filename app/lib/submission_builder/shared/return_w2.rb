6cc95a28bcad78ebb171faf980cfd2d88af313ff
Jenny Heath <jheath@codeforamerica.org>
2024-03-04 20:23:56 -0800

    wip

--- app/lib/submission_builder/shared/return_w2.rb
+++ app/lib/submission_builder/shared/return_w2.rb
@@ -12,21 +12,21 @@ module SubmissionBuilder
         if intake_w2.present?
           # w2.create_or_destroy_df_xml_node(:W2StateTaxGrp, nil)
 
-          w2.create_or_destroy_df_xml_node(:StateAbbreviationCd, 1, "MedicareTaxWithheldAmt")
-          w2.create_or_destroy_df_xml_node(:EmployerStateIdNum, 1)
-          w2.create_or_destroy_df_xml_node(:StateWagesAmt, 1)
-          w2.create_or_destroy_df_xml_node(:StateIncomeTaxAmt, 1)
-          w2.create_or_destroy_df_xml_node(:LocalWagesAndTipsAmt, 1)
-          w2.create_or_destroy_df_xml_node(:LocalIncomeTaxAmt, 1)
-          w2.create_or_destroy_df_xml_node(:LocalityNm, 1)
+          # w2.create_or_destroy_df_xml_node(:StateAbbreviationCd, 1, "MedicareTaxWithheldAmt")
+          # w2.create_or_destroy_df_xml_node(:EmployerStateIdNum, 1)
+          # w2.create_or_destroy_df_xml_node(:StateWagesAmt, 1)
+          # w2.create_or_destroy_df_xml_node(:StateIncomeTaxAmt, 1)
+          # w2.create_or_destroy_df_xml_node(:LocalWagesAndTipsAmt, 1)
+          # w2.create_or_destroy_df_xml_node(:LocalIncomeTaxAmt, 1)
+          # w2.create_or_destroy_df_xml_node(:LocalityNm, 1)
 
-          # update_xml(w2, :StateAbbreviationCd, "NY")
-          # update_xml(w2, :EmployerStateIdNum, intake_w2.employer_state_id_num)
-          # update_xml(w2, :StateWagesAmt, intake_w2.state_wages_amt)
-          # update_xml(w2, :StateIncomeTaxAmt, intake_w2.state_income_tax_amt)
-          # update_xml(w2, :LocalWagesAndTipsAmt, intake_w2.local_wages_and_tips_amt)
-          # update_xml(w2, :LocalIncomeTaxAmt, intake_w2.local_income_tax_amt)
-          # update_xml(w2, :LocalityNm, intake_w2.locality_nm)
+          update_xml(w2, :StateAbbreviationCd, "NY")
+          update_xml(w2, :EmployerStateIdNum, intake_w2.employer_state_id_num)
+          update_xml(w2, :StateWagesAmt, intake_w2.state_wages_amt)
+          update_xml(w2, :StateIncomeTaxAmt, intake_w2.state_income_tax_amt)
+          update_xml(w2, :LocalWagesAndTipsAmt, intake_w2.local_wages_and_tips_amt)
+          update_xml(w2, :LocalIncomeTaxAmt, intake_w2.local_income_tax_amt)
+          update_xml(w2, :LocalityNm, intake_w2.locality_nm)
         end
         xml_node
       end
