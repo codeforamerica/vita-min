86d745e77517275cf04d7604d5e8d310db0c9d40
Tahsina Islam <79296603+tahsinaislam@users.noreply.github.com>
2024-03-06 17:39:08 -0600

    GYR email opt out (#4311)
    Co-authored-by: Em Barnard-Shao <ebarnard@codeforamerica.org>

--- config/routes.rb
+++ config/routes.rb
@@ -151,6 +151,8 @@ Rails.application.routes.draw do
       get "/consent-to-disclose", to: "consent_pages#consent_to_disclose"
       get "/relational-efin", to: "consent_pages#relational_efin"
       get "/global-carryforward", to: "consent_pages#global_carryforward"
+      get "/unsubscribe_from_emails", to: "notifications_settings#unsubscribe_from_emails", as: :unsubscribe_from_emails
+      post "/subscribe_to_emails", to: "notifications_settings#subscribe_to_emails", as: :subscribe_to_emails
 
       namespace :portal do
         root "portal#home"
