c99385b739b35e921809b106478a5fae7cd48509
Tim O'Farrell <tofarrell@codeforamerica.org>
2024-03-05 17:18:50 -0700

    Validation fix

--- app/models/state_file_w2.rb
+++ app/models/state_file_w2.rb
@@ -45,7 +45,7 @@ class StateFileW2 < ApplicationRecord
   validates :local_income_tax_amt, numericality: { only_integer: true, greater_than_or_equal_to: 0 }, if: -> { local_income_tax_amt.present? }
   validates :locality_nm, presence: { message: -> (_object, _data) { I18n.t('state_file.questions.ny_w2.edit.locality_nm_missing_error') } }, if: -> { local_wages_and_tips_amt.present? && local_wages_and_tips_amt.positive? }
   validates :employer_state_id_num, presence: true, if: -> { state_wages_amt.present? && state_wages_amt.positive? }
-  validates :locality_nm, format: { with: /\A[a-zA-Z]{1}([A-Za-z\-\s']{0,137})\z/ }, if: -> { locality_nm.present? }
+  validates :locality_nm, format: { with: /\A[a-zA-Z]{1}([A-Za-z\-\s']{0,19})\z/ }, if: -> { locality_nm.present? }
   validate :validate_tax_amts
   validate :locality_nm_validation
   before_validation :locality_nm_to_upper_case
