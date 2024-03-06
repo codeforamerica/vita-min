d1e8aa59c76b6a4b22f26886e9b86d55ef1ac53e
Tim O'Farrell <tofarrell@codeforamerica.org>
2024-03-05 18:21:20 -0700

    Auto fix locality name case

--- app/lib/submission_builder/shared/return_w2.rb
+++ app/lib/submission_builder/shared/return_w2.rb
@@ -8,6 +8,10 @@ module SubmissionBuilder
         if intake_w2.present?
           xml_node.at(:W2StateLocalTaxGrp).inner_html = intake_w2.state_tax_group_xml_node
         end
+        locality_nm = xml_node.at(:LocalityNm)
+        if locality_nm.present?
+          locality_nm.inner_html = locality_nm.inner_html.upcase
+        end
         xml_node
       end
     end
