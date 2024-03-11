6e86165fa8093d61cc92c0ca2d04bcc2e0aa2cc6
Tahsina Islam <tislam@codeforamerica.org>
2024-03-11 13:02:55 -0500

    fix test

--- app/forms/state_file/ny_sales_use_tax_form.rb
+++ app/forms/state_file/ny_sales_use_tax_form.rb
@@ -7,13 +7,13 @@ module StateFile
 
     validates :untaxed_out_of_state_purchases, presence: true
     validates :sales_use_tax_calculation_method, presence: true, if: -> { untaxed_out_of_state_purchases == "yes" }
-    validates_numericality_of :sales_use_tax, only_integer: true, message: I18n.t('errors.messages.whole_number'), if: -> { untaxed_out_of_state_purchases == "yes" }
     validates :sales_use_tax,
       presence: true,
       numericality: {
         message: I18n.t("state_file.questions.ny_sales_use_tax.edit.enter_valid_dollar_amount"),
         greater_than_or_equal_to: 0,
         less_than_or_equal_to: 1699,
+        only_integer: true,
       },
       if: -> { sales_use_tax_calculation_method == "manual" }
     
