639499e702561f04380dfb94694c9cf511d97c22
Tim O'Farrell <tofarrell@codeforamerica.org>
2024-03-13 14:05:53 -0600

    Reset schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2024_03_07_232948) do
-  create_schema "analytics"
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
   enable_extension "plpgsql"
