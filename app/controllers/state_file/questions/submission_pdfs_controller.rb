b493a046889f4070bf1f9a614cb523dbd62331f9
Em Barnard-Shao <ebarnard@codeforamerica.org>
2024-03-14 17:42:50 -0400

    Add error redirects for return downloads

--- app/controllers/state_file/questions/submission_pdfs_controller.rb
+++ app/controllers/state_file/questions/submission_pdfs_controller.rb
@@ -3,9 +3,16 @@ module StateFile
     class SubmissionPdfsController < QuestionsController
       def show
         @submission = current_intake.efile_submissions.find_by(id: params[:id])
-        error_redirect and return unless @submission.present?
+        error_redirect and return #unless @submission.present?
 
         send_data @submission.generate_filing_pdf.read, filename: "submission.pdf", disposition: 'inline'
+      end
+
+      private
+
+      def error_redirect
+        flash[:alert] = "We encountered a problem generating your tax return pdf. For assistance, please reach out to FileYourStateTaxes client support."
+        redirect_back(fallback_location: request.referer)
       end
     end
   end
