eeaddf31edbb123da497bdd18070a0b983e33c71
Em Barnard-Shao <ebarnard@codeforamerica.org>
2024-03-15 11:03:46 -0400

    Add back

--- app/controllers/state_file/questions/submission_pdfs_controller.rb
+++ app/controllers/state_file/questions/submission_pdfs_controller.rb
@@ -3,7 +3,7 @@ module StateFile
     class SubmissionPdfsController < QuestionsController
       def show
         @submission = current_intake.efile_submissions.find_by(id: params[:id])
-        error_redirect and return #unless @submission.present?
+        error_redirect and return unless @submission.present?
 
         send_data @submission.generate_filing_pdf.read, filename: "submission.pdf", disposition: 'inline'
       end
