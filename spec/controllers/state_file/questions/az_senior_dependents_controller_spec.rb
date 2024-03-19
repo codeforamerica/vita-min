3645803563312ab74fdb712fa3643721f15a92c1
Tim O'Farrell <tofarrell@codeforamerica.org>
2024-03-19 08:49:15 -0600

    Additional spec

--- spec/controllers/state_file/questions/az_senior_dependents_controller_spec.rb
+++ spec/controllers/state_file/questions/az_senior_dependents_controller_spec.rb
@@ -1,20 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe StateFile::Questions::AzSeniorDependentsController do
+
+  let(:dependent_name) { :az_senior_dependent }
+  let(:dependent) { create(dependent_name) }
+  let(:intake) { create(:state_file_az_intake, dependents: [dependent]) }
+  before { sign_in intake }
+
   describe ".show?" do
     context "with any senior dependents" do
       it "returns true" do
-        intake = create(:state_file_az_intake, dependents: [create(:az_senior_dependent)])
-        sign_in intake
         expect(described_class.show?(intake)).to eq true
       end
     end
 
     context "without senior dependents" do
+      let(:dependent_name) { :state_file_dependent }
       it "returns false" do
-        intake = create(:state_file_az_intake, dependents: [create(:state_file_dependent)])
-        sign_in intake
         expect(described_class.show?(intake)).to eq false
+      end
+    end
+
+    context "with the return to review parameter" do
+      it "returns to review" do
+        params = {
+          state_file_az_senior_dependents_form: {
+            dependents_attributes: {
+              "0": {
+                id: dependent.id,
+                needed_assistance: "yes",
+                passed_away: "no"
+              }
+            }
+          },
+          return_to_review: :y,
+          us_state: :az
+        }
+        post :update, params: params
+        expect(response).to redirect_to "/en/az/questions/az-review"
       end
     end
   end
