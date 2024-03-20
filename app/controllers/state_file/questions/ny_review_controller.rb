e63f007cbf41713ac845422ae691242fe1d6ec2f
Tim O'Farrell <tofarrell@codeforamerica.org>
2024-03-20 10:17:12 -0600

    Add W2 to Review Page #187222675

--- app/controllers/state_file/questions/ny_review_controller.rb
+++ app/controllers/state_file/questions/ny_review_controller.rb
@@ -5,6 +5,7 @@ module StateFile
         @refund_or_owed_amount = current_intake.calculated_refund_or_owed_amount
         @refund_or_owed_label = @refund_or_owed_amount.positive? ? I18n.t("state_file.questions.shared.review_header.your_refund") : I18n.t("state_file.questions.shared.review_header.your_tax_owed")
         @show_dependent_months_in_home = false
+        @show_w2s = StateFile::Questions::W2Controller.invalid_df_w2s(current_intake).present?
       end
 
       private
