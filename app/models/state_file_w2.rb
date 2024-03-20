2996752678f4992ac8c897700eb91e71f4ff928d
Tim O'Farrell <tofarrell@codeforamerica.org>
2024-03-20 09:27:00 -0600

    W2 page error copy updates #187269981

--- app/models/state_file_w2.rb
+++ app/models/state_file_w2.rb
@@ -38,18 +38,18 @@ class StateFileW2 < ApplicationRecord
 
   validates :w2_index, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
-  validates :employer_state_id_num, format: { with: /\A(\d{0,17})\z/ }, length: {maximum: 16}
-  validates_numericality_of :state_wages_amt, only_integer: true, message: I18n.t('errors.messages.whole_number'), if: -> { state_wages_amt.present? }
+  validates :employer_state_id_num, format: { with: /\A(\d{0,17})\z/, message: ->(_object, _data) { I18n.t('state_file.questions.w2.edit.employer_state_id_error') } }
+  validates_numericality_of :state_wages_amt, only_integer: true, message: :whole_number, if: -> { state_wages_amt.present? }
   validates :state_wages_amt, numericality: { greater_than_or_equal_to: 0 }, if: -> { state_wages_amt.present? }
-  validates_numericality_of :state_income_tax_amt, only_integer: true, message: I18n.t('errors.messages.whole_number'), if: -> { state_income_tax_amt.present? }
+  validates_numericality_of :state_income_tax_amt, only_integer: true, message: :whole_number, if: -> { state_income_tax_amt.present? }
   validates :state_income_tax_amt, numericality: { greater_than_or_equal_to: 0 }, if: -> { state_income_tax_amt.present? }
-  validates_numericality_of :local_wages_and_tips_amt, only_integer: true, message: I18n.t('errors.messages.whole_number'), if: -> { local_wages_and_tips_amt.present? }
+  validates_numericality_of :local_wages_and_tips_amt, only_integer: true, message: :whole_number, if: -> { local_wages_and_tips_amt.present? }
   validates :local_wages_and_tips_amt, numericality: { greater_than_or_equal_to: 0 }, if: -> { local_wages_and_tips_amt.present? }
-  validates_numericality_of :local_income_tax_amt, only_integer: true, message: I18n.t('errors.messages.whole_number'), if: -> { local_income_tax_amt.present? }
+  validates_numericality_of :local_income_tax_amt, only_integer: true, message: :whole_number, if: -> { local_income_tax_amt.present? }
   validates :local_income_tax_amt, numericality: { greater_than_or_equal_to: 0 }, if: -> { local_income_tax_amt.present? }
   validates :locality_nm, presence: { message: ->(_object, _data) { I18n.t('state_file.questions.w2.edit.locality_nm_missing_error') } }, if: -> { local_wages_and_tips_amt.present? && local_wages_and_tips_amt.positive? }
   validates :employer_state_id_num, presence: true, if: -> { state_wages_amt.present? && state_wages_amt.positive? }
-  validates :locality_nm, format: { with: /\A[a-zA-Z]{1}([A-Za-z\-\s']{0,19})\z/ }, if: -> { locality_nm.present? }
+  validates :locality_nm, format: { with: /\A[a-zA-Z]{1}([A-Za-z\-\s']{0,19})\z/, message: :only_letters }, if: -> { locality_nm.present? }
   validate :validate_tax_amts
   validate :state_specific_validation
   before_validation :locality_nm_to_upper_case
