1b91c3a481d08db60f345bba255ac7af2eb7a5cc
Ricardo Kreyhsig <rkreyhsig@codeforamerica.org>
2024-03-21 15:26:15 -0400

    Fix query
    Co-authored-by: Ricardo Kreyhsig <rkreyhsig@codeforamerica.org>

--- app/services/efile/reminder_to_finish_state_return_service.rb
+++ app/services/efile/reminder_to_finish_state_return_service.rb
@@ -1,13 +1,14 @@
 module Efile
   class ReminderToFinishStateReturnService
     def self.run
-      time_range = (Time.now - 24.hours)..Time.now
+      cutoff_time = 23.hours + 50.minutes
+      cutoff_time_ago = cutoff_time.ago
       msg = StateFile::AutomatedMessage::FinishReturn
       batch_size = 10
-      intakes_with_no_submission = StateFileAzIntake.where(df_data_imported_at: time_range)
+      intakes_with_no_submission = StateFileAzIntake.where("df_data_imported_at < ?", cutoff_time_ago)
                                                     .left_joins(:efile_submissions)
                                                     .where(efile_submissions: { id: nil })
-      intakes_with_no_submission += StateFileNyIntake.where(df_data_imported_at: time_range)
+      intakes_with_no_submission += StateFileNyIntake.where("df_data_imported_at < ?", cutoff_time_ago)
                                                      .left_joins(:efile_submissions)
                                                      .where(efile_submissions: { id: nil })
 
