2c34a49195d5e7c521c41fb028b6951339b01e31
Ricardo Kreyhsig <rkreyhsig@codeforamerica.org>
2024-03-22 14:34:16 -0400

    Update spec
    Co-authored-by: Ricardo Kreyhsig <rkreyhsig@codeforamerica.org>

--- spec/tasks/send_survey_notifications_spec.rb
+++ spec/tasks/send_survey_notifications_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe 'send_survey_notifications rake task' do
   let!(:intake) { create :state_file_az_intake, email_address: "test@example.com", email_address_verified_at: 1.minute.ago }
   let!(:efile_submission) { create :efile_submission, :for_state, :accepted, data_source: intake }
-  let!(:efile_submission_no_intake) { create :efile_submission, :for_state, :accepted, data_source: nil }
+  let!(:ctc_efile_submission) { create :efile_submission, :accepted}
   before(:all) do
     Rails.application.load_tasks
   end
