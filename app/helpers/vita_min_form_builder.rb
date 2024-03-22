52eec566cece7229749c2506f40352da74f8520a
Tim O'Farrell <tofarrell@codeforamerica.org>
2024-03-22 14:15:34 -0600

    Check required intake data #187270706
    Co-authored-by: Ricardo Kreyhsig <rkreyhsig@codeforamerica.org>

--- app/helpers/vita_min_form_builder.rb
+++ app/helpers/vita_min_form_builder.rb
@@ -368,10 +368,19 @@ class VitaMinFormBuilder < Cfa::Styleguide::CfaFormBuilder
   def submit(value, options = {})
     options[:data] ||= {}
     options[:data][:disable_with] = value
-    super(value, **options)
+    current_intake_input.html_safe + super(value, **options)
   end
 
   def continue(value = I18n.t("general.continue"))
     submit(value, class: "button button--primary button--wide")
   end
+
+  private
+
+  def current_intake_input
+    # A hidden element containing the identified for the current_intake
+    return "" unless @object.respond_to?(:intake) && @object.intake && @object.intake.id
+    @template.hidden_field_tag(:current_intake, @object.intake.to_global_id)
+  end
+
 end
