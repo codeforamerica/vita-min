b4501f57aa38495df585fe5118d1074853fcd163
Tim O'Farrell <tofarrell@codeforamerica.org>
2024-03-25 20:13:12 -0600

    Documentation update

--- app/helpers/questions_form_helper.rb
+++ app/helpers/questions_form_helper.rb
@@ -1,8 +1,9 @@
 module QuestionsFormHelper
 
   def questions_form(*args, **options, &block)
+    # Custom form with a hidden element containing the current intake id to match with the session
+    # in case multiple tabs are in use
     options[:builder] = VitaMinFormBuilder unless options[:builder].present?
-    #binding.pry
     result = form_with(*args, **options) do |f|
       if current_intake.present? && current_intake.persisted?
         output_buffer << "<input type=\"hidden\" name=\"current_intake\" value=\"#{current_intake.to_signed_global_id}\" />".html_safe
