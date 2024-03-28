3b446e4c8431adaae61ac0391c10c0c194a0ae37
Ricardo Kreyhsig <rkreyhsig@codeforamerica.org>
2024-03-28 13:41:08 -0400

    Filter intakes that already have finish_return message sent
    Co-authored-by: Ricardo Kreyhsig <rkreyhsig@codeforamerica.org>

--- app/services/state_file/reminder_to_finish_state_return_service.rb
+++ app/services/state_file/reminder_to_finish_state_return_service.rb
@@ -8,9 +8,12 @@ module StateFile
       intakes_with_no_submission = StateFileAzIntake.where("df_data_imported_at < ?", cutoff_time_ago)
                                                     .left_joins(:efile_submissions)
                                                     .where(efile_submissions: { id: nil })
+                                                    .where.not("state_file_az_intakes.message_tracker #> '{messages.state_file.finish_return}' IS NOT NULL")
+
       intakes_with_no_submission += StateFileNyIntake.where("df_data_imported_at < ?", cutoff_time_ago)
                                                      .left_joins(:efile_submissions)
                                                      .where(efile_submissions: { id: nil })
+                                                     .where.not("state_file_ny_intakes.message_tracker #> '{messages.state_file.finish_return}' IS NOT NULL")
 
       intakes_with_no_submission.each_slice(batch_size) do |batch|
         batch.each do |intake|
