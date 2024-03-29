12111795efe6fc67d4c0ddb72e53edd86c1ca07d
Tim O'Farrell <tofarrell@codeforamerica.org>
2024-03-29 08:00:12 -0600

    Launch updates

--- app/controllers/state_file/questions/landing_page_controller.rb
+++ app/controllers/state_file/questions/landing_page_controller.rb
@@ -1,14 +1,32 @@
 module StateFile
   module Questions
     class LandingPageController < QuestionsController
-      include StartIntakeConcern
-      before_action :before_update, only: :update
 
-      def before_update
+      def create
+        # Start a new session...
         StateFileBaseIntake::STATE_CODES.each do |state_code|
           intake = send("current_state_file_#{state_code}_intake")
           sign_out intake if intake
         end
+        super
+      end
+
+      def update
+        # Go to existing session
+        controller = intake.controller_for_current_step
+        to_path = controller.to_path_helper(
+          action: controller.navigation_actions.first,
+          us_state: intake.state_code
+        )
+        redirect_to to_path
+      end
+
+      def current_intake
+        @intake ||= question_navigator.intake_class.new(
+          visitor_id: cookies.encrypted[:visitor_id],
+          source: session[:source],
+          referrer: session[:referrer]
+        )
       end
     end
   end
