32ec5987cc23e196ba0e43276587b6fa49c72ef1
Tim O'Farrell <tofarrell@codeforamerica.org>
2024-03-28 22:26:18 -0600

    Fixed

--- app/controllers/state_file/questions/az_review_controller.rb
+++ app/controllers/state_file/questions/az_review_controller.rb
@@ -2,7 +2,7 @@ module StateFile
   module Questions
     class AzReviewController < AuthenticatedQuestionsController
       def edit
-        @invalid_df_w2s = StateFile::Questions::W2Controller.invalid_df_w2s(current_intake)
+        @invalid_w2s = StateFile::Questions::W2Controller.w2s_for_intake(current_intake)
         @refund_or_owed_amount = current_intake.calculated_refund_or_owed_amount
         @refund_or_owed_label = @refund_or_owed_amount.positive? ? I18n.t("state_file.questions.shared.review_header.your_refund") : I18n.t("state_file.questions.shared.review_header.your_tax_owed")
         @show_dependent_months_in_home = true
