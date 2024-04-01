2e03f327bcbaa92fd428d012895465a6f3203f43
Tahsina Islam <tislam@codeforamerica.org>
2024-04-01 13:09:26 -0500

    Add logic for automated messages

--- app/lib/send_automated_message.rb
+++ app/lib/send_automated_message.rb
@@ -18,7 +18,7 @@ class SendAutomatedMessage
   end
 
   def send_messages
-    return nil if @client.nil? && message.require_client_account?
+    return nil if @client.consented_to_service_at.nil? && message.require_client_account?
     return nil if message_tracker.already_sent? && message.send_only_once?
 
     send_email if @do_email
