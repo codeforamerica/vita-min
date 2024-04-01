85abad0801668e6b8b5381ca2e942ae0383f5d13
Tim O'Farrell <tofarrell@codeforamerica.org>
2024-04-01 10:59:44 -0600

    Only show first name

--- app/controllers/state_file/questions/landing_page_controller.rb
+++ app/controllers/state_file/questions/landing_page_controller.rb
@@ -7,13 +7,7 @@ module StateFile
         @state_name = StateFileBaseIntake::STATE_CODE_AND_NAMES[params[:us_state]]
         if current_intake.present?
           if current_intake.primary_first_name.present?
-            @user_name = "#{current_intake.primary_first_name} #{current_intake.primary_middle_initial} #{current_intake.primary_last_name}"
-          elsif current_intake.raw_direct_file_data.present?
-            begin
-              @user_name = current_intake.direct_file_data.node.at("Filer NameLine1Txt").text.gsub("&lt;", " ")
-            rescue => err
-              Rails.logger.error(err)
-            end
+            @user_name = current_intake.primary_first_name
           end
         end
       end
