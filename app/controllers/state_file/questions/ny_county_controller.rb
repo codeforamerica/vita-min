22c1bdacc146909eac8fe48cfdd6220740b1b176
Tim O'Farrell <tofarrell@codeforamerica.org>
2024-04-04 13:50:57 -0600

    Permitted fix

--- app/controllers/state_file/questions/ny_county_controller.rb
+++ app/controllers/state_file/questions/ny_county_controller.rb
@@ -17,8 +17,8 @@ module StateFile
       def edit
         @filing_year = Rails.configuration.statefile_current_tax_year
         @nyc_residency = current_intake.nyc_residency
-        nyc_residency_full_year = @nyc_residency == :full_year
-        @permitted_counties = NySchoolDistricts.county_labels.filter { |c| NYC_COUNTIES.include?(c) == nyc_residency_full_year }
+        nyc_residency_full_year = @nyc_residency == "full_year"
+        @permitted_counties = NySchoolDistricts.county_labels.filter { |c| NYC_COUNTIES.include?(c) != nyc_residency_full_year }
         super
       end
 
