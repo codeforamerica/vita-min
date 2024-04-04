0e2df98075a44ea2899b3b60b420ed778b70a020
Tim O'Farrell <tofarrell@codeforamerica.org>
2024-04-04 08:23:18 -0600

    Controller page cleanup

--- app/controllers/state_file/questions/ny_county_controller.rb
+++ app/controllers/state_file/questions/ny_county_controller.rb
@@ -1,6 +1,15 @@
 require 'csv'
 
 module StateFile
+  NYC_COUNTIES = [
+    "Bronx",
+    "Kings (Brooklyn)",
+    "Manhattan (see New York)",
+    "New York (Manhattan)",
+    "Queens",
+    "Richmond (Staten Isand)"
+  ].freeze
+
   module Questions
     class NyCountyController < AuthenticatedQuestionsController
       include ReturnToReviewConcern
@@ -8,6 +17,8 @@ module StateFile
       def edit
         @filing_year = Rails.configuration.statefile_current_tax_year
         @nyc_residency = current_intake.nyc_residency
+        nyc_residency_full_year = @nyc_residency == :full_year
+        @permitted_counties = NySchoolDistricts.county_labels.filter { |c| NYC_COUNTIES.include?(c) == nyc_residency_full_year }
         super
       end
 
