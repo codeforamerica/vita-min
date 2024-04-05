7b4b8173e461df54d5b4734d616feb40846bd186
Ricardo Kreyhsig <2385700+rickreyhsig@users.noreply.github.com>
2024-04-05 17:30:29 -0400

    Change logic for Reminder notification STAGING (#4449)
    * Change logic for Reminder notification
    
    Co-authored-by: Ricardo Kreyhsig <rkreyhsig@codeforamerica.org>
    
    * RM broken spec
    
    Co-authored-by: Ricardo Kreyhsig <rkreyhsig@codeforamerica.org>

--- app/services/state_file/reminder_to_finish_state_return_service.rb
+++ app/services/state_file/reminder_to_finish_state_return_service.rb
@@ -3,12 +3,12 @@ module StateFile
     def self.run
       cutoff_time_ago = 12.hours.ago
       batch_size = 10
-      intakes_with_no_submission = StateFileAzIntake.where("df_data_imported_at < ?", cutoff_time_ago)
+      intakes_with_no_submission = StateFileAzIntake.where('state_file_az_intakes.created_at < ?', cutoff_time_ago)
                                                     .left_joins(:efile_submissions)
                                                     .where(efile_submissions: { id: nil })
                                                     .where.not("state_file_az_intakes.message_tracker #> '{messages.state_file.finish_return}' IS NOT NULL")
 
-      intakes_with_no_submission += StateFileNyIntake.where("df_data_imported_at < ?", cutoff_time_ago)
+      intakes_with_no_submission += StateFileNyIntake.where('state_file_ny_intakes.created_at < ?', cutoff_time_ago)
                                                      .left_joins(:efile_submissions)
                                                      .where(efile_submissions: { id: nil })
                                                      .where.not("state_file_ny_intakes.message_tracker #> '{messages.state_file.finish_return}' IS NOT NULL")
