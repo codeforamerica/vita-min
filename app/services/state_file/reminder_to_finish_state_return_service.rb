231b2c71d032729f4159276f9859682300b365a7
Ricardo Kreyhsig <rkreyhsig@codeforamerica.org>
2024-04-08 14:40:31 -0300

    Update filtering logic on staging
    Co-authored-by: Ricardo Kreyhsig <rkreyhsig@codeforamerica.org>

--- app/services/state_file/reminder_to_finish_state_return_service.rb
+++ app/services/state_file/reminder_to_finish_state_return_service.rb
@@ -6,14 +6,17 @@ module StateFile
       intakes_with_no_submission = StateFileAzIntake.where('state_file_az_intakes.created_at < ?', cutoff_time_ago)
                                                     .left_joins(:efile_submissions)
                                                     .where(efile_submissions: { id: nil })
+                                                    .where.not(email_address: nil).where.not(email_address_verified_at: nil)
                                                     .where.not("state_file_az_intakes.message_tracker #> '{messages.state_file.finish_return}' IS NOT NULL")
 
       intakes_with_no_submission += StateFileNyIntake.where('state_file_ny_intakes.created_at < ?', cutoff_time_ago)
                                                      .left_joins(:efile_submissions)
                                                      .where(efile_submissions: { id: nil })
+                                                     .where.not(email_address: nil).where.not(email_address_verified_at: nil)
                                                      .where.not("state_file_ny_intakes.message_tracker #> '{messages.state_file.finish_return}' IS NOT NULL")
 
-      intakes_with_no_submission.select!{|n| n.email_address&.include? 'rkreyhsig' }
+      # intakes_with_no_submission.each {|i| p i.id, i.email_address, i.email_address_verified_at, i.message_tracker, '---'};nil
+
       intakes_with_no_submission.each_slice(batch_size) do |batch|
         batch.each do |intake|
           StateFile::MessagingService.new(message: StateFile::AutomatedMessage::FinishReturn, intake: intake).send_message
