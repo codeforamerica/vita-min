bf6068b6590200781476855430b0f993cae19c88
Tim O'Farrell <tofarrell@codeforamerica.org>
2024-04-09 07:58:43 -0600

    WIP

--- app/javascript/components/WarningForDateComponent.js
+++ app/javascript/components/WarningForDateComponent.js
@@ -2,18 +2,24 @@
 export default function WarningForDateComponent() {
     const warningElements = document.querySelectorAll("[data-warning-for-date]");
     warningElements.forEach((warningElement) => {
-        let { minDate, warningForDate } = warningElement.dataset;
+        let { minDate, warningForDate, requiredYear } = warningElement.dataset;
         minDate = new Date(minDate);
         const fields = ["year", "month", "day"].map((f) => {
             return document.querySelector(`[name="${warningForDate}[dob_${f}]"]`);
         });
 
-        function render(){
+        function show(){
+            if (requiredYear) {
+                const selectedYear = fields[0].value;
+                return selectedYear && selectedYear !== requiredYear;
+            }
             const date = new Date(...fields.map((s) => {
                 return parseInt(s.value);
             }))
-            const show = date.getTime() < minDate.getTime();
-            $(warningElement)[show ? "show" : "hide"]("slow");
+            return date.getTime() < minDate.getTime();
+        }
+        function render(){
+            $(warningElement)[show() ? "show" : "hide"]("slow");
         }
 
         fields.forEach((f) => {
