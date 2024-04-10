e5cd4d011f9c296445e459ebc274401bb1fa8756
Ricardo Kreyhsig <rkreyhsig@codeforamerica.org>
2024-04-10 13:52:24 -0300

    Add template for service
    Co-authored-by: Ricardo Kreyhsig <rkreyhsig@codeforamerica.org>

---
+++ app/services/state_file/pre_deadline_reminder_service.rb
@@ -0,0 +1,23 @@
+module StateFile
+  class PostDeadlineReminderService
+    BATCH_SIZE = 10
+    HOURS_AGO = 24
+
+    def self.run
+      # cutoff_time_ago = HOURS_AGO.hours.ago
+
+      intakes_to_notify = StateFileBaseIntake::STATE_CODES.map do |code|
+        # TODO
+      end
+
+      intakes_to_notify.each_slice(BATCH_SIZE) do |batch|
+        batch.each do |intake|
+          StateFile::MessagingService.new(
+            message: StateFile::AutomatedMessage::PreDeadlineReminder,
+            intake: intake
+          ).send_message
+        end
+      end
+    end
+  end
+end
