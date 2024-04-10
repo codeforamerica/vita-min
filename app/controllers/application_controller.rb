d28a8ae9b758a78421d21c69779b8ed1120bd0e8
Em Barnard-Shao <ebarnard@codeforamerica.org>
2024-04-10 17:25:35 -0400

    Update GYR banner for closing season

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -337,6 +337,9 @@ class ApplicationController < ActionController::Base
     if app_time <= Rails.configuration.tax_deadline && open_for_gyr_intake?
       # after open intake, before tax_deadline
       :open_intake
+    elsif app_time.between?(Rails.configuration.tax_deadline, Rails.configuration.end_of_intake)
+      # after tax_deadline, before end_of_intake
+      :open_intake_post_tax_deadline
     elsif app_time.between?(Rails.configuration.tax_deadline, Rails.configuration.end_of_in_progress_intake)
       # after tax_deadline, before end_of_in_progress_intake
       :in_progress_intake_only
