5ca8d834accd61cda474a99b2f29067b99dde6ca
Ricardo Kreyhsig <rkreyhsig@codeforamerica.org>
2024-04-12 11:39:49 -0300

    Scratch work
    Co-authored-by: Ricardo Kreyhsig <rkreyhsig@codeforamerica.org>

--- app/services/state_file/reminder_to_finish_state_return_service.rb
+++ app/services/state_file/reminder_to_finish_state_return_service.rb
@@ -1,17 +1,30 @@
 module StateFile
   class ReminderToFinishStateReturnService
+    BATCH_SIZE = 10
+    HOURS_AGO = 12
     def self.run
-      cutoff_time_ago = 12.hours.ago
-      batch_size = 10
-      intakes_with_no_submission = StateFileAzIntake.where('state_file_az_intakes.created_at < ?', cutoff_time_ago)
-                                                    .where.not(email_address: nil).where.not(email_address_verified_at: nil)
-                                                    .where(unsubscribed_from_email: false)
-                                                    .where("state_file_az_intakes.message_tracker #> '{messages.state_file.finish_return}' IS NULL")
+      cutoff_time_ago = HOURS_AGO.hours.ago
+      intakes_to_notify = []
 
-      intakes_with_no_submission += StateFileNyIntake.where('state_file_ny_intakes.created_at < ?', cutoff_time_ago)
-                                                     .where.not(email_address: nil).where.not(email_address_verified_at: nil)
-                                                     .where(unsubscribed_from_email: false)
-                                                     .where("state_file_ny_intakes.message_tracker #> '{messages.state_file.finish_return}' IS NULL")
+      ApplicationRecord::STATE_INTAKE_CLASS_NAMES.each do |base_class|
+        class_object = base_class.constantize
+        intakes_to_notify += class_object.where("#{base_class.underscore.pluralize}.created_at < ?", cutoff_time_ago)
+                                         .where.not(email_address: nil).where.not(email_address_verified_at: nil)
+                                         .where(unsubscribed_from_email: false)
+                                         .where("#{base_class.underscore.pluralize}.message_tracker #> '{messages.state_file.finish_return}' IS NULL")
+      end
+
+      # cutoff_time_ago = 12.hours.ago
+      # batch_size = 10
+      # intakes_with_no_submission = StateFileAzIntake.where('state_file_az_intakes.created_at < ?', cutoff_time_ago)
+      #                                               .where.not(email_address: nil).where.not(email_address_verified_at: nil)
+      #                                               .where(unsubscribed_from_email: false)
+      #                                               .where("state_file_az_intakes.message_tracker #> '{messages.state_file.finish_return}' IS NULL")
+      #
+      # intakes_with_no_submission += StateFileNyIntake.where('state_file_ny_intakes.created_at < ?', cutoff_time_ago)
+      #                                                .where.not(email_address: nil).where.not(email_address_verified_at: nil)
+      #                                                .where(unsubscribed_from_email: false)
+      #                                                .where("state_file_ny_intakes.message_tracker #> '{messages.state_file.finish_return}' IS NULL")
 
       intakes_with_no_submission.each_slice(batch_size) do |batch|
         batch.each do |intake|
