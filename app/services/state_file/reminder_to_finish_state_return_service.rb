6e6f19c9e2e8382fe0ac0e95948f07320fdf9713
Ricardo Kreyhsig <rkreyhsig@codeforamerica.org>
2024-04-12 11:42:02 -0300

    Merge main & fix service
    Co-authored-by: Ricardo Kreyhsig <rkreyhsig@codeforamerica.org>

--- app/services/state_file/reminder_to_finish_state_return_service.rb
+++ app/services/state_file/reminder_to_finish_state_return_service.rb
@@ -14,21 +14,11 @@ module StateFile
                                          .where("#{base_class.underscore.pluralize}.message_tracker #> '{messages.state_file.finish_return}' IS NULL")
       end
 
-      # cutoff_time_ago = 12.hours.ago
-      # batch_size = 10
-      # intakes_with_no_submission = StateFileAzIntake.where('state_file_az_intakes.created_at < ?', cutoff_time_ago)
-      #                                               .where.not(email_address: nil).where.not(email_address_verified_at: nil)
-      #                                               .where(unsubscribed_from_email: false)
-      #                                               .where("state_file_az_intakes.message_tracker #> '{messages.state_file.finish_return}' IS NULL")
-      #
-      # intakes_with_no_submission += StateFileNyIntake.where('state_file_ny_intakes.created_at < ?', cutoff_time_ago)
-      #                                                .where.not(email_address: nil).where.not(email_address_verified_at: nil)
-      #                                                .where(unsubscribed_from_email: false)
-      #                                                .where("state_file_ny_intakes.message_tracker #> '{messages.state_file.finish_return}' IS NULL")
-
-      intakes_with_no_submission.each_slice(batch_size) do |batch|
+      intakes_to_notify.each_slice(BATCH_SIZE) do |batch|
         batch.each do |intake|
-          StateFile::MessagingService.new(message: StateFile::AutomatedMessage::FinishReturn, intake: intake).send_message
+          StateFile::MessagingService.new(
+            message: StateFile::AutomatedMessage::FinishReturn, intake: intake
+          ).send_message
         end
       end
     end
