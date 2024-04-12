7c210239ade4344c3b27ddef73e0214ae471c73a
Tahsina Islam <tislam@codeforamerica.org>
2024-04-12 10:57:40 -0500

    charitable contributions errors

--- app/forms/state_file/az_charitable_contributions_form.rb
+++ app/forms/state_file/az_charitable_contributions_form.rb
@@ -7,7 +7,7 @@ module StateFile
     validates :charitable_cash, numericality: { greater_than_or_equal_to: 1, message: ->(_object, _data) { I18n.t('errors.messages.greater_than_or_equal_to', count: 1)}  }, if: -> { charitable_contributions == "yes" }
     validates_numericality_of :charitable_noncash, only_integer: true, message: :whole_number, if: -> { charitable_contributions == "yes" && charitable_noncash.present? }
     validates :charitable_noncash, numericality: { greater_than_or_equal_to: 1, message: ->(_object, _data) { I18n.t('errors.messages.greater_than_or_equal_to', count: 1)} }, if: -> { charitable_contributions == "yes" }
-    # validates :charitable_noncash, numericality: { less_than_or_equal_to: 500 }, if: -> { charitable_contributions == "yes" }
+    validates :charitable_noncash, numericality: { less_than_or_equal_to: 500, message: ->(_object, _data) { I18n.t('errors.messages.less_than_or_equal_to', count: 500)} }, if: -> { charitable_contributions == "yes" }
 
 
     def save
