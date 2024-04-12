be2d8692d4eb454e8f2cd366dd2afad1400b5a68
Em Barnard-Shao <ebarnard@codeforamerica.org>
2024-04-12 14:13:51 -0400

    fix tests

--- spec/services/replacement_parameters_service_spec.rb
+++ spec/services/replacement_parameters_service_spec.rb
@@ -383,7 +383,7 @@ describe ReplacementParametersService do
 
     context "successfully submitted text message" do
       context "in english" do
-        let(:body) { AutomatedMessage::SuccessfulSubmissionOnlineIntake.new.sms_body(locale: "en") }
+        let(:body) { AutomatedMessage::SuccessfulSubmissionOnlineIntake.new.sms_body(locale: "en", body_args: {end_of_docs_date: "October 8th"})}
 
         it "replaces the replacement strings in the template" do
           result = subject.process
@@ -393,7 +393,7 @@ describe ReplacementParametersService do
       end
 
       context "in spanish" do
-        let(:body) { I18n.t("messages.successful_submission_drop_off.sms", locale: "es") }
+        let(:body) { I18n.t("messages.successful_submission_drop_off.sms", locale: locale, body_args: {end_of_docs_date: "8 de octubre"})}
         let(:locale) { "es" }
         it "replaces the replacement strings in the template" do
           result = subject.process
