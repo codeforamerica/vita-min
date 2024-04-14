b798e38a2f63f71b06a96211e3827ba11dd3f710
Jenny Heath <jheath@codeforamerica.org>
2024-04-13 18:37:42 -0700

    fix test

--- spec/mailers/verification_code_mailer_spec.rb
+++ spec/mailers/verification_code_mailer_spec.rb
@@ -39,7 +39,7 @@ describe VerificationCodeMailer, type: :mailer do
     end
 
     context "for a state file client" do
-      it "delivers an email with state file branding and a no-reply@ address" do
+      it "delivers an email with state file branding and a no-reply@ address FROM GYR" do
         email = described_class.with(
           to: "example@example.com",
           locale: :en,
@@ -50,7 +50,7 @@ describe VerificationCodeMailer, type: :mailer do
           email.deliver_now
         end.to change(ActionMailer::Base.deliveries, :count).by 1
         expect(email.subject).to eq "Six-Digit Code from FileYourStateTaxes"
-        expect(email.from).to eq ["no-reply@statefile.test.localhost"]
+        expect(email.from).to eq ["no-reply@test.localhost"]
         expect(email.to).to eq ["example@example.com"]
         expect(email.text_part.decoded.strip).to include "FileYourStateTaxes"
       end
