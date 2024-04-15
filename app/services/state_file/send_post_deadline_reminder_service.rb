126f91e73390fb48edd7bfbc4f3f87c2d255ef9b
Martha Pidcock <mpidcock@codeforamerica.org>
2024-04-15 12:49:28 -0700

    WIP
    Co-authored-by: Tahsina Islam <tislam@codeforamerica.org>

--- app/services/state_file/send_post_deadline_reminder_service.rb
+++ app/services/state_file/send_post_deadline_reminder_service.rb
@@ -16,6 +16,8 @@ module StateFile
                                          .where(unsubscribed_from_email: false)
                                          .where("#{base_class.underscore.pluralize}.message_tracker #> '{messages.state_file.post_deadline_reminder}' IS NULL")
                                          .select do |intake|
+
+          # The finish_return message is a different reminder to finish your return. If it was sent recently, don't send this one.
           if intake.message_tracker.present? && intake.message_tracker["messages.state_file.finish_return"]
             finish_return_msg_sent_time = Date.parse(intake.message_tracker["messages.state_file.finish_return"])
             finish_return_msg_sent_time < cutoff_time_ago
