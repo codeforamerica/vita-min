7a62310ad547d4c3ae401d8f83e49f4f2702297b
Martha Pidcock <mpidcock@codeforamerica.org>
2024-04-16 17:01:24 -0700

    Update logic
    Co-authored-by: Tahsina Islam <tislam@codeforamerica.org>

--- app/services/state_file/send_post_deadline_reminder_service.rb
+++ app/services/state_file/send_post_deadline_reminder_service.rb
@@ -4,50 +4,46 @@ module StateFile
     HOURS_AGO = 24
 
     def self.run
-      cutoff_time_ago = 24.hours.ago
-      intakes_to_notify = []
-
-      # TODO: run in development to view the sql run here
-      ApplicationRecord::STATE_INTAKE_CLASS_NAMES.each do |base_class|
+      cutoff_time_ago = HOURS_AGO.hours.ago
+      intakes_to_notify = ApplicationRecord::STATE_INTAKE_CLASS_NAMES.map do |base_class|
         class_object = base_class.constantize
-        intakes_to_notify += class_object.left_joins(:efile_submissions)
-                                         .where(efile_submissions: { id: nil })
-                                         .where.not(email_address: nil)
-                                         # .where.not(email_address_verified_at: nil)
-                                         .where(unsubscribed_from_email: false)
-                                         .where("#{base_class.underscore.pluralize}.message_tracker #> '{messages.state_file.post_deadline_reminder}' IS NULL")
-                                         .select do |intake|
 
-          # The finish_return message is a different reminder to finish your return. If it was sent recently, don't send this one.
+        # First we get all intake ids by email address
+        intake_ids_by_email = class_object.select(:id, :email_address).where.not(email_address: nil).each_with_object({}) do |intake, result|
+          ids = result[intake.email_address]
+          ids = [] unless ids.present?
+          ids.append(intake.id)
+        end
+
+        # Next we get all intake ids by hashed SSN
+        intake_ids_by_hashed_ssn = class_object.select(:id, :hashed_ssn).where.not(hashed_ssn: nil).each_with_object({}) do |intake, result|
+          ids = result[intake.hashed_ssn]
+          ids = [] unless ids.present?
+          ids.append(intake.id)
+        end
+
+        class_object.left_joins(:efile_submissions)
+                    .where(efile_submissions: { id: nil })
+                    .where.not(email_address: nil)
+                    .where.not(email_address_verified_at: nil)
+                    .where(unsubscribed_from_email: false)
+                    .where("#{base_class.underscore.pluralize}.message_tracker #> '{messages.state_file.post_deadline_reminder}' IS NULL")
+                    .select do |intake|
           if intake.message_tracker.present? && intake.message_tracker["messages.state_file.finish_return"]
             finish_return_msg_sent_time = Date.parse(intake.message_tracker["messages.state_file.finish_return"])
             finish_return_msg_sent_time < cutoff_time_ago
           else
             true
           end
+        end.select do |intake|
+          # New criteria - gonna see if any associated intakes have submissions
+          intake_ids = intake_ids_by_email[intake.email_address] + (intake_ids_by_hashed_ssn[intake.hashed_ssn] || [])
+          intake_ids = intake_ids.to_set
+          (intake_ids.length == 1) || EfileSubmission.where(data_source_id: intake_ids, data_source_type: base_class).none?
         end
-      end
+      end.flatten
 
-      # Check if any submitted intakes have a matching email with intakes_to_notify
-      accepted_intakes = EfileSubmission.joins(:efile_submission_transitions)
-                                        .for_state_filing
-                                        .where("efile_submission_transitions.to_state = 'accepted'")
-                                        .extract_associated(:data_source)
-
-      accepted_intakes = accepted_intakes.reject { |intake| intake.email_address.nil? }
-
-
-      intakes_without_matching_accepted_intake = intakes_to_notify.reject do |intake|
-        accepted_intakes.any? { |accepted_intake|
-          if accepted_intake.email_address.nil?
-            false
-          else
-            intake.email_address.casecmp(accepted_intake&.email_address).zero?
-          end
-        }
-      end
-
-      intakes_without_matching_accepted_intake.each_slice(BATCH_SIZE) do |batch|
+      intakes_to_notify.each_slice(BATCH_SIZE) do |batch|
         batch.each do |intake|
           StateFile::MessagingService.new(
             message: StateFile::AutomatedMessage::PostDeadlineReminder,
