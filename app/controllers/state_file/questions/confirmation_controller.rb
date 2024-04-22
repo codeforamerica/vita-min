e14456189c25d762816b2fe7d5cb6f875580578b
Jenny Heath <jheath@codeforamerica.org>
2024-04-22 13:54:54 -0700

    Add refactor comment

--- app/controllers/state_file/questions/confirmation_controller.rb
+++ app/controllers/state_file/questions/confirmation_controller.rb
@@ -12,6 +12,15 @@ module StateFile
         builder_response.errors.present? ? render(plain: builder_response.errors.join("\n") + "\n\n" + builder_response.document.to_xml) : render(xml: builder_response.document)
       end
 
+      # possible TODO (from a chore written by Travis): Rewrite source display in Explain Calculations page to use the parser gem
+      # Currently we rely on method(...).source which comes from the method-source gem and is somewhat shady.
+      # It relies on .source_location, a core ruby method that returns the line number a given method is defined on. Then it uses eval on progressively longer chunks of code starting from that line until it finds a statement that doesn't crash. Probably slow?
+      #
+      # It might be better for us to use the parser gem and use source_location to find the right text from the AST. This would bypass the sketchy use of eval and give us finer control over the output.
+      #
+      # Right now for a line like this:
+      # set_line(:LINE1, -> { @some_field * 2 })
+      # asking .source of the lambda returns the whole line, set_line and all. This could be improved if we had finer grained control of the code.
       def explain_calculations
         # fixes Rails hot reload, see method source gem PR #73
         if Rails.env.development?
