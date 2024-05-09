2d39c739841acad83559567b9a2d638711b144bf
Tahsina Islam <tislam@codeforamerica.org>
2024-05-09 13:49:51 -0500

    Fix method for tax return status
    Co-authored-by: Mike Rotondo <mrotondo@codeforamerica.org>

--- app/helpers/tax_return_card_helper.rb
+++ app/helpers/tax_return_card_helper.rb
@@ -7,7 +7,7 @@ module TaxReturnCardHelper
 
     if ask_for_answers
       current_step = intake.current_step
-      if current_step.in?(Questions::AtCapacityController.all_localized_paths)
+      if intake.client.routing_method_at_capacity?
         # check if the appropriate partner is still at capacity
         routing_service = PartnerRoutingService.new(
           intake: intake,
@@ -16,7 +16,9 @@ module TaxReturnCardHelper
           )
         intake.client.update(vita_partner: routing_service.determine_partner, routing_method: routing_service.routing_method)
 
-        unless intake.client.routing_method_at_capacity?
+        if intake.client.routing_method_at_capacity?
+          current_step = Questions::AtCapacityController.to_path_helper
+        else
           current_step = Questions::ConsentController.to_path_helper
         end
       end
