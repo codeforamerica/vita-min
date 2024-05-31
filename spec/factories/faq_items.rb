3758a2cb46c10e5c98d94d33199ee812bc4d8779
Tim O'Farrell <tofarrell@codeforamerica.org>
2024-05-31 15:46:47 -0600

    Models and migrations set up for full text search

--- spec/factories/faq_items.rb
+++ spec/factories/faq_items.rb
@@ -2,20 +2,24 @@
 #
 # Table name: faq_items
 #
-#  id              :bigint           not null, primary key
-#  answer_en       :text
-#  answer_es       :text
-#  position        :integer
-#  question_en     :text
-#  question_es     :text
-#  slug            :string
-#  created_at      :datetime         not null
-#  updated_at      :datetime         not null
-#  faq_category_id :bigint           not null
+#  id                 :bigint           not null, primary key
+#  answer_en          :text
+#  answer_es          :text
+#  position           :integer
+#  question_en        :text
+#  question_es        :text
+#  searchable_data_en :tsvector
+#  searchable_data_es :tsvector
+#  slug               :string
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#  faq_category_id    :bigint           not null
 #
 # Indexes
 #
-#  index_faq_items_on_faq_category_id  (faq_category_id)
+#  index_faq_items_on_faq_category_id     (faq_category_id)
+#  index_faq_items_on_searchable_data_en  (searchable_data_en) USING gin
+#  index_faq_items_on_searchable_data_es  (searchable_data_es) USING gin
 #
 # Foreign Keys
 #
