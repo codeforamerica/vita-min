9188de978489704fea8f01d1733b2386aadbd15a
Mike Rotondo <mrotondo@codeforamerica.org>
2024-06-03 17:15:01 -0700

    drop the unused customization params

--- app/controllers/hub/automated_messages_controller.rb
+++ app/controllers/hub/automated_messages_controller.rb
@@ -13,16 +13,6 @@ module Hub
       Rails.application.eager_load!
       message_classes = AutomatedMessage::AutomatedMessage.descendants + [SurveyMessages::GyrCompletionSurvey, SurveyMessages::CtcExperienceSurvey]
 
-      # TODO: These were not being used in the previous implementation of this code, so either figure out how to use them or delete them
-      #   The issue is that different message classes take their args differently,
-      message_params = {
-        SurveyMessages::GyrCompletionSurvey => { survey_link: "https://fakecodeforamerica.co1.qualtrics.com" },
-        SurveyMessages::CtcExperienceSurvey => { survey_link: "https://fakecodeforamerica.co1.qualtrics.com" },
-        AutomatedMessage::DocumentsReminderLink => { body_args: { doc_type: "ID" } },
-        AutomatedMessage::SaveCtcLetter => { body_args: { service_name: MultiTenantService.new(:ctc).service_name } },
-        AutomatedMessage::UnmonitoredReplies => { support_email: Rails.configuration.email_from[:support][:gyr] },
-      }
-
       automated_messages_and_mailers = message_classes.to_h do |klass|
         message = klass.new
         replaced_body = message.email_body.gsub('<<', '&lt;&lt;').gsub('>>', '&gt;&gt;')
