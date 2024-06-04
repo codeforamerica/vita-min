9365846ad950a4cab50547bd489935dfc15bdad4
Mike Rotondo <mrotondo@codeforamerica.org>
2024-06-04 15:22:24 -0700

    add another couple mailers

--- app/controllers/hub/automated_messages_controller.rb
+++ app/controllers/hub/automated_messages_controller.rb
@@ -11,8 +11,10 @@ module Hub
 
     def messages_preview
       Rails.application.eager_load!
-      message_classes = AutomatedMessage::AutomatedMessage.descendants + [SurveyMessages::GyrCompletionSurvey, SurveyMessages::CtcExperienceSurvey]
+      automated_message_subclasses = AutomatedMessage::AutomatedMessage.descendants
+      survey_message_classes = [SurveyMessages::GyrCompletionSurvey, SurveyMessages::CtcExperienceSurvey]
 
+      message_classes = automated_message_subclasses + survey_message_classes
       automated_messages_and_mailers = message_classes.to_h do |klass|
         replaced_body = klass.new.email_body.gsub('<<', '&lt;&lt;').gsub('>>', '&gt;&gt;')
         email = OutgoingEmail.new(to: "example@example.com", body: replaced_body, subject: klass.new.email_subject, client: Client.new(intake: Intake::GyrIntake.new))
@@ -23,7 +25,8 @@ module Hub
         "UserMailer.assignment_email" => UserMailer.assignment_email(assigned_user: User.last, assigning_user: User.first, tax_return: TaxReturn.last, assigned_at: TaxReturn.last.updated_at),
         "VerificationCodeMailer.with_code" => VerificationCodeMailer.with(to: "example@example.com", locale: :en, service_type: :gyr, verification_code: '000000').with_code,
         "VerificationCodeMailer.no_match_found" => VerificationCodeMailer.no_match_found(to: "example@example.com", locale: :en, service_type: :gyr),
-        "DiyIntakeEmailMailer.high_support_message" => DiyIntakeEmailMailer.high_support_message(diy_intake: DiyIntake.new(email_address: 'example@example.com', preferred_first_name: "Preferredfirstname"))
+        "DiyIntakeEmailMailer.high_support_message" => DiyIntakeEmailMailer.high_support_message(diy_intake: DiyIntake.new(email_address: 'example@example.com', preferred_first_name: "Preferredfirstname")),
+        "CtcSignupMailer.launch_announcement" => CtcSignupMailer.launch_announcement(email_address: "example@example.com", name: "Preferredfirstname")
       }
 
       emails.merge(automated_messages_and_mailers).transform_values do |message|
