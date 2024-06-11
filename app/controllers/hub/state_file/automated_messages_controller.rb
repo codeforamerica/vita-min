20e9772ac855011e08e0638521671843e88a380b
Tim O'Farrell <tofarrell@codeforamerica.org>
2024-06-11 15:40:14 -0600

    WIP

--- app/controllers/hub/state_file/automated_messages_controller.rb
+++ app/controllers/hub/state_file/automated_messages_controller.rb
@@ -1,6 +1,17 @@
 module Hub::StateFile
   class AutomatedMessagesController < Hub::StateFile::BaseController
-    helper_method :render_message
+    MESSAGE_PARAMS = {
+      primary_first_name: "Cornelius",
+      intake_id: 1234,
+      survey_link: "/survey_link",
+      submitted_or_resubmitted: true,
+      state_name: "East Dakota",
+      return_status_link: "/return_status",
+      login_link: "/login",
+      state_pay_taxes_link: "/pay_taxes",
+    }.freeze
+    helper_method :email_message
+    helper_method :sms_body
 
     def index
       Rails.application.eager_load!
@@ -10,16 +21,27 @@ module Hub::StateFile
 
     private
 
-    def render_message(klass, locale)
-      replaced_body = klass.new.email_body(locale: locale).gsub('<<', '&lt;&lt;').gsub('>>', '&gt;&gt;')
+    def email_message(message_class, locale)
+      replaced_body = message_class.new.email_body(
+        **{locale: locale}.update(MESSAGE_PARAMS)
+      ).gsub('<<', '&lt;&lt;').gsub('>>', '&gt;&gt;')
+      subject = message_class.new.email_subject(
+        **{locale: locale}.update(MESSAGE_PARAMS)
+      )
       email = StateFileNotificationEmail.new(to: "example@example.com",
                                              body: replaced_body,
-                                             subject: klass.new.email_subject)
+                                             subject: subject)
       message = StateFile::NotificationMailer.user_message(notification_email: email)
       ActionMailer::Base.preview_interceptors.each do |interceptor|
         interceptor.previewing_email(message)
       end
       message
+    end
+
+    def sms_body(message_class, locale)
+      message_class.new.sms_body(
+        **{locale: locale}.update(MESSAGE_PARAMS)
+      )
     end
   end
 end
