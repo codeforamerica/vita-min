96757af9a281ba4ed8c57abe722d8d7f4d29421d
Jenny Heath <jheath@codeforamerica.org>
2024-06-11 15:46:56 -0700

    fix state map

--- app/services/state_file/state_information_service.rb
+++ app/services/state_file/state_information_service.rb
@@ -2,9 +2,13 @@ module StateFile
   class StateInformationService
     ACTIVE_STATES = ["az", "ny"].freeze
 
-    def state_code_to_name_map
-      ACTIVE_STATES.reduce({}) do |acc, state_abbrev|
-        acc[state_abbrev] = States.name_for_key(state_abbrev.upcase)
+    class << self
+      def state_code_to_name_map
+        ACTIVE_STATES.reduce({}) do |acc, state_abbrev|
+          state_name = States.name_for_key(state_abbrev.upcase)
+          acc[state_abbrev] = state_name if state_name
+          acc
+        end
       end
     end
   end
