eca7ea86ba96e24968b4b1bee7516b4b487481ee
Tim O'Farrell <tofarrell@codeforamerica.org>
2024-06-11 14:58:11 -0600

    Automated messages now displays different locales

--- app/controllers/hub/state_file/automated_messages_controller.rb
+++ app/controllers/hub/state_file/automated_messages_controller.rb
@@ -1,26 +1,25 @@
 module Hub::StateFile
   class AutomatedMessagesController < Hub::StateFile::BaseController
+    helper_method :render_message
 
     def index
-      @messages = messages_preview
+      Rails.application.eager_load!
+      @messages = StateFile::AutomatedMessage::BaseAutomatedMessage.descendants
+      @locales = [:en, :es]
     end
 
     private
 
-    def messages_preview
-      Rails.application.eager_load!
-      message_classes = StateFile::AutomatedMessage::BaseAutomatedMessage.descendants
-      message_classes.to_h do |klass|
-        replaced_body = klass.new.email_body.gsub('<<', '&lt;&lt;').gsub('>>', '&gt;&gt;')
-        email = StateFileNotificationEmail.new(to: "example@example.com",
-                                               body: replaced_body,
-                                               subject: klass.new.email_subject)
-        message = StateFile::NotificationMailer.user_message(notification_email: email)
-        ActionMailer::Base.preview_interceptors.each do |interceptor|
-          interceptor.previewing_email(message)
-        end
-        [klass, message]
+    def render_message(klass, locale)
+      replaced_body = klass.new.email_body(locale: locale).gsub('<<', '&lt;&lt;').gsub('>>', '&gt;&gt;')
+      email = StateFileNotificationEmail.new(to: "example@example.com",
+                                             body: replaced_body,
+                                             subject: klass.new.email_subject)
+      message = StateFile::NotificationMailer.user_message(notification_email: email)
+      ActionMailer::Base.preview_interceptors.each do |interceptor|
+        interceptor.previewing_email(message)
       end
+      message
     end
   end
 end
