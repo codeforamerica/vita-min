6b0d8e89def46abea63cc2ffd571081fdc082231
Mike Rotondo <mrotondo@codeforamerica.org>
2024-06-12 14:34:04 -0700

    fix flaky notes test

--- spec/features/hub/client/notes_spec.rb
+++ spec/features/hub/client/notes_spec.rb
@@ -16,7 +16,7 @@ RSpec.feature "View and add internal notes for a client" do
 
       expect(page).to have_selector("h1", text: "Bart Simpson")
       expect(page).to have_text("4:00 PM") # 4:00 PM Pacific is midnight UTC; the note is created at midnight UTC
-      expect(page).to have_text("Client added 5 documents.")
+      expect(page).to have_text("Client added 3 documents.")
       fill_in "Add a note", with: "Some pertinent info, presumably"
       click_on "Save"
 
