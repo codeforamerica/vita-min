d94de97094f0d7fe362d0f2db057e282c52ac926
Jenny Heath <jheath@codeforamerica.org>
2024-06-24 16:14:02 -0700

    fix namespace

--- app/lib/state_file/ny/efile/constants.rb
+++ app/lib/state_file/ny/efile/constants.rb
@@ -1,12 +1,16 @@
 module StateFile
-  class Constants
-    def self.import(filename)
-      # To compute this JSON, copy the JS function (call it f) that generates it into a Chrome console, then run
-      # console.log(JSON.stringify(f()))
-      # then copy to the desired file.
-      self.counties = JSON.parse(File.read(filename))
-    end
+  module Ny
+    module Efile
+      class Constants
+        def self.import(filename)
+          # To compute this JSON, copy the JS function (call it f) that generates it into a Chrome console, then run
+          # console.log(JSON.stringify(f()))
+          # then copy to the desired file.
+          self.counties = JSON.parse(File.read(filename))
+        end
 
-    class_attribute :counties
+        class_attribute :counties
+      end
+    end
   end
 end
