cd42faa6d739d3b04c8ab35b9c9ce11231209860
Jenny Heath <jheath@codeforamerica.org>
2024-06-24 15:52:10 -0700

    remove extra end

--- app/lib/state_file/ny/efile/constants.rb
+++ app/lib/state_file/ny/efile/constants.rb
@@ -1,13 +1,12 @@
 module StateFile
-    class Constants
-      def self.import(filename)
-        # To compute this JSON, copy the JS function (call it f) that generates it into a Chrome console, then run
-        # console.log(JSON.stringify(f()))
-        # then copy to the desired file.
-        self.counties = JSON.parse(File.read(filename))
-      end
-
-      class_attribute :counties
+  class Constants
+    def self.import(filename)
+      # To compute this JSON, copy the JS function (call it f) that generates it into a Chrome console, then run
+      # console.log(JSON.stringify(f()))
+      # then copy to the desired file.
+      self.counties = JSON.parse(File.read(filename))
     end
+
+    class_attribute :counties
   end
 end
