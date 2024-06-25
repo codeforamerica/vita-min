2f2f5923a2b02c4c849088d141e04f0f9417b5a3
Mike Rotondo <mrotondo@codeforamerica.org>
2024-06-25 15:05:18 -0700

    added javier test case

--- spec/factories/state_file/personas/2023/az.rb
+++ spec/factories/state_file/personas/2023/az.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
-  factory :johnny, class: StateFileAzIntake do
-    raw_direct_file_data { StateFile::XmlReturnSampleService.new.read('az_johnny_mfj_8_deps') }
+  factory "johnny", class: StateFileAzIntake do
+    raw_direct_file_data { StateFile::XmlReturnSampleService.new.read("az_johnny_mfj_8_deps") }
     primary_first_name { "Johnny" }
     primary_middle_initial { "L" }
     primary_last_name { "Rose" }
--- spec/personas/persona_spec.rb
+++ spec/personas/persona_spec.rb
@@ -1,21 +1,21 @@
-require 'rails_helper'
+require "rails_helper"
 
-describe 'personas' do
-  context '2023' do
+describe "personas" do
+  context "2023" do
     let(:tax_year) { 2023 }
-    context 'az' do
+    context "az" do
       let(:state_code) { :az }
-      context 'johnny' do
-        let(:persona_name) { 'johnny' }
-        let(:submission_id) { '1234562024165nly30yy' }
+      context "johnny" do
+        let(:persona_name) { "johnny" }
+        let(:submission_id) { "1234562024165nly30yy" }
         it_behaves_like :persona
       end
     end
-    context 'ny' do
+    context "ny" do
       let(:state_code) { :ny }
-      context 'javier' do
-        let(:persona_name) { '414h_test' }
-
+      context "javier" do
+        let(:persona_name) { "414h_test" }
+        let(:submission_id) { "1234562024177c2kg88t" }
         it_behaves_like :persona
       end
     end
