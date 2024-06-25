04ca0c9d6e6433750ac2434acfa10ff51286ec76
Tim O'Farrell <tofarrell@codeforamerica.org>
2024-06-25 14:54:04 -0600

    Updates from code review

--- app/lib/submission_builder/state_file.rb
+++ app/lib/submission_builder/state_file.rb
@@ -2,14 +2,14 @@ module SubmissionBuilder
   module StateFile
     def from_state_code_and_year(state_code, year)
       unless ::StateFile::StateInformationService::STATES_INFO.key?(state_code)
-        raise StandardError state_code
+        raise InvalidStateCodeError, state_code
       end
       builder = "SubmissionBuilder::Ty#{year.to_i}::States::#{state_code.to_s.titleize}::IndividualReturn"
       builder.constantize
     end
 
     def from_state_code(state_code)
-      # TODO: I am hard coding 2022 as the current year here because we don't have anything else defined yet
+      # TODO: I am hard coding 2022 as the current year here because we don't have any other years defined yet
       from_state_code_and_year(state_code, 2022)
     end
 
