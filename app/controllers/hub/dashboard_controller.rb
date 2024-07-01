09c0dd9dfd80542c77d6aac4151f5b6349baf78f
Tim O'Farrell <tofarrell@codeforamerica.org>
2024-07-01 15:16:07 -0600

    WIP

--- app/controllers/hub/dashboard_controller.rb
+++ app/controllers/hub/dashboard_controller.rb
@@ -1,10 +1,16 @@
 module Hub
   class DashboardController < Hub::BaseController
     layout "hub"
+    before_action :load_vita_partners, only: [:index, :show]
     # TODO: Require specific roles
 
     def index
-      puts "TODO: Implement Index"
+      redirect_to action: :show, id: @vita_partners.first.id
+    end
+
+    def show
+      @selected_id = params[:id].to_i
+      @vita_partner = @vita_partners.find { |vita_partner| vita_partner.id == @selected_id }
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -310,9 +310,7 @@ Rails.application.routes.draw do
         end
         resources :ctc_clients, only: [:edit, :update]
 
-        resources :dashboard do
-
-        end
+        resources :dashboard, only: [:index, :show]
 
         resources :tax_return_selections, path: "tax-return-selections", only: [:create, :show, :new]
 
