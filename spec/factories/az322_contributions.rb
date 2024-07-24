b19bfb4f371c69b97ca60877a5549a5f8b26443e
Jenny Heath <jheath@codeforamerica.org>
2024-07-24 15:33:31 -0700

    fix factory attr

--- spec/factories/az322_contributions.rb
+++ spec/factories/az322_contributions.rb
@@ -18,7 +18,7 @@
 #
 FactoryBot.define do
   factory :az322_contribution do
-    date_of { Date.new(2023, 3, 4) }
+    date_of_contribution { Date.new(2023, 3, 4) }
     ctds_code { "100206038" }
     school_name { "Schublic Pool" }
     district_name { "Dool Schistrict" }
