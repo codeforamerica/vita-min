a2a5ed6c241070989c16f1d9c692da45206a70fa
Tim O'Farrell <tofarrell@codeforamerica.org>
2024-08-01 12:46:04 -0600

    WIP

--- app/jobs/state_file/build_submission_pdf_job.rb
+++ app/jobs/state_file/build_submission_pdf_job.rb
@@ -2,7 +2,7 @@ module StateFile
   class BuildSubmissionPdfJob < ApplicationJob
     def perform(submission_id)
       submission = EfileSubmission.find(submission_id)
-      submission.submission_bundle.attach(
+      submission.data_source.submission_pdf.attach(
         io: submission.generate_filing_pdf,
         filename: "#{submission.irs_submission_id}.pdf",
         content_type: 'application/pdf'
