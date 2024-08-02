116d07a47a21cea2254da8e33e45784082bf844d
Tim O'Farrell <tofarrell@codeforamerica.org>
2024-08-02 09:27:24 -0600

    Send submission pdf

--- app/controllers/state_file/questions/submission_pdfs_controller.rb
+++ app/controllers/state_file/questions/submission_pdfs_controller.rb
@@ -4,6 +4,11 @@ module StateFile
       skip_before_action :redirect_if_in_progress_intakes_ended
 
       def show
+        if current_intake.submission_pdf.attached?
+          send_data current_intake.submission_pdf, filename: "submission.pdf", disposition: 'inline'
+          return
+        end
+        # This is a fallback for legacy reasons - all submitted intakes should have an attached submission pdf
         @submission = current_intake.efile_submissions.find_by(id: params[:id])
         error_redirect and return unless @submission.present?
 
