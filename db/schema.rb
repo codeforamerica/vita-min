48d02c86630a1693bef7995e368abb75fc664309
Mike Rotondo <mrotondo@codeforamerica.org>
2024-08-06 17:09:23 -0700

    more fix tests, more columns

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_06_223558) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_06_234752) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
   enable_extension "plpgsql"
@@ -1766,7 +1766,9 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_06_223558) do
   end
 
   create_table "state_file_nc_intakes", force: :cascade do |t|
+    t.string "account_number"
     t.integer "account_type", default: 0, null: false
+    t.string "bank_name"
     t.string "city"
     t.integer "consented_to_terms_and_conditions", default: 0, null: false
     t.integer "contact_preference", default: 0, null: false
@@ -1774,6 +1776,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_06_223558) do
     t.datetime "current_sign_in_at"
     t.inet "current_sign_in_ip"
     t.string "current_step"
+    t.date "date_electronic_withdrawal"
     t.datetime "df_data_import_failed_at"
     t.datetime "df_data_imported_at"
     t.integer "eligibility_lived_in_state", default: 0, null: false
@@ -1797,6 +1800,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_06_223558) do
     t.string "primary_last_name"
     t.text "raw_direct_file_data"
     t.string "referrer"
+    t.integer "routing_number"
     t.integer "sign_in_count", default: 0, null: false
     t.string "source"
     t.integer "spouse_esigned", default: 0, null: false
@@ -1806,6 +1810,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_06_223558) do
     t.boolean "unsubscribed_from_email", default: false, null: false
     t.datetime "updated_at", null: false
     t.string "visitor_id"
+    t.integer "withdraw_amount"
     t.string "zip_code"
     t.index ["hashed_ssn"], name: "index_state_file_nc_intakes_on_hashed_ssn"
   end
--- spec/models/state_file_nc_intake_spec.rb
+++ spec/models/state_file_nc_intake_spec.rb
@@ -3,13 +3,16 @@
 # Table name: state_file_nc_intakes
 #
 #  id                                :bigint           not null, primary key
+#  account_number                    :string
 #  account_type                      :integer          default("unfilled"), not null
+#  bank_name                         :string
 #  city                              :string
 #  consented_to_terms_and_conditions :integer          default("unfilled"), not null
 #  contact_preference                :integer          default("unfilled"), not null
 #  current_sign_in_at                :datetime
 #  current_sign_in_ip                :inet
 #  current_step                      :string
+#  date_electronic_withdrawal        :date
 #  df_data_import_failed_at          :datetime
 #  df_data_imported_at               :datetime
 #  eligibility_lived_in_state        :integer          default("unfilled"), not null
@@ -33,6 +36,7 @@
 #  primary_last_name                 :string
 #  raw_direct_file_data              :text
 #  referrer                          :string
+#  routing_number                    :integer
 #  sign_in_count                     :integer          default(0), not null
 #  source                            :string
 #  spouse_esigned                    :integer          default("unfilled"), not null
@@ -40,6 +44,7 @@
 #  street_address                    :string
 #  tax_return_year                   :integer
 #  unsubscribed_from_email           :boolean          default(FALSE), not null
+#  withdraw_amount                   :integer
 #  zip_code                          :string
 #  created_at                        :datetime         not null
 #  updated_at                        :datetime         not null
