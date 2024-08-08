522416b3b70dc7bd9e2e263eff94784387bc2480
Mike Rotondo <mrotondo@codeforamerica.org>
2024-08-08 10:55:44 -0700

    removed unnecessary constructor from D400 Calculator

--- app/lib/efile/nc/d400_calculator.rb
+++ app/lib/efile/nc/d400_calculator.rb
@@ -3,19 +3,6 @@ module Efile
     class D400Calculator < ::Efile::TaxCalculator
       attr_reader :lines
 
-      def initialize(year:, intake:, include_source: false)
-        @year = year
-        @intake = intake
-        @filing_status = intake.filing_status.to_sym
-        @direct_file_data = intake.direct_file_data
-        # intake.state_file_w2s.each do |w2|
-        #   dest_w2 = @direct_file_data.w2s[w2.w2_index]
-        #   dest_w2.node.at("W2StateLocalTaxGrp").inner_html = w2.state_tax_group_xml_node
-        # end
-        @value_access_tracker = Efile::ValueAccessTracker.new(include_source: include_source)
-        @lines = HashWithIndifferentAccess.new
-      end
-
       def calculate
         @lines.transform_values(&:value)
       end
