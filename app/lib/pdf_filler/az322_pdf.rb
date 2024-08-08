9fba35bc409c06791fc054ca114ba32f34ab06c1
Tahsina Islam <tislam@codeforamerica.org@Tahsinas-MBP.attlocal.net>
2024-08-08 17:31:42 -0500

    Add fields for xml and pdf

--- app/lib/pdf_filler/az322_pdf.rb
+++ app/lib/pdf_filler/az322_pdf.rb
@@ -20,8 +20,32 @@ module PdfFiller
         "TP_SSN" => @xml_document.at('Primary TaxpayerSSN')&.text,
         "Spouse_NAME" => [@xml_document.at('Secondary TaxpayerName FirstName')&.text, @xml_document.at('Secondary TaxpayerName MiddleInitial')&.text, @xml_document.at('Secondary TaxpayerName LastName')&.text, @xml_document.at('Secondary TaxpayerName NameSuffix')&.text].join(' '),
         "Spouse_SSN" => @xml_document.at('Secondary TaxpayerSSN')&.text,
-
+        "4" => @xml_document.at('TotalContributionsContSheet')&.text,
+        "5" => @xml_document.at('TotalContributions')&.text,
+        "9" => '0',
+        "10" => '0',
+        "11" => @xml_document.at('SubTotalAmt')&.text,
+        "12" => @xml_document.at('SingleHOH')&.text,
+        "13" => @xml_document.at('CurrentYrCr')&.text,
+        "20" => @xml_document.at('CurrentYrCr')&.text,
+        "22" => @xml_document.at('TotalAvailCr')&.text,
+        "4h" => @xml_document.at('TotalContributionsContSheet')&.text,
       }
+      @submission.data_source.az322_contributions.each_with_index do |contribution, index|
+        if index < 3
+          prefix = (index + 1).to_s
+        else
+          letter = ('a'..'g').to_a[index - 3]
+          prefix = "4#{letter}"
+        end
+        answers["#{prefix}a"] = contribution.date_of_contribution.strftime("%m%d")
+        answers["#{prefix}b"] = contribution.ctds_code
+        answers["#{prefix}c"] = contribution.school_name
+        answers["#{prefix}d"] = contribution.district_name
+        answers["#{prefix}e"] = contribution.amount.round
+      end
+
+      answers
     end
   end
 end
--- app/lib/submission_builder/ty2022/states/az/documents/az322.rb
+++ app/lib/submission_builder/ty2022/states/az/documents/az322.rb
@@ -8,17 +8,43 @@ module SubmissionBuilder
 
             def document
               build_xml_doc("Form322") do |xml|
-                @submission.data_source.az322_contributions.each_with_index do |contribution, index|
-                  break if index >= 3
+                @submission.data_source.az322_contributions.first(3).each do |contribution|
                   xml.SContribMadeTo do
                     xml.SchoolContrDate contribution.date_of_contribution
                     xml.CTDSCode contribution.ctds_code
                     xml.SchoolName contribution.school_name
                     xml.SchoolDist contribution.district_name
-                    xml.Contributions contribution.amount
+                    xml.Contributions contribution.amount.round
+                  end
+                end
+                xml.TotalContributionsContSheet calculated_fields.fetch(:AZ322_LINE_4)
+                xml.TotalContributions calculated_fields.fetch(:AZ322_LINE_5)
+                xml.SubTotalAmt calculated_fields.fetch(:AZ322_LINE_11)
+                xml.SingleHOH calculated_fields.fetch(:AZ322_LINE_12)
+                xml.CurrentYrCr calculated_fields.fetch(:AZ322_LINE_13)
+                xml.TotalAvailCr calculated_fields.fetch(:AZ322_LINE_22)
+                if @submission.data_source.az322_contributions.count > 3
+                  xml.ContinuationPages do
+                    @submission.data_source.az322_contributions.drop(3).each do |contribution|
+                      xml.SContribMadeTo do
+                        xml.SchoolContrDate contribution.date_of_contribution
+                        xml.CTDSCode contribution.ctds_code
+                        xml.SchoolName contribution.school_name
+                        xml.SchoolDist contribution.district_name
+                        xml.Contributions contribution.amount.round
+                      end
+                      xml.TotalContributions calculated_fields.fetch(:AZ322_LINE_4)
+                      xml.TotalContributionsAfter 0
+                    end
                   end
                 end
               end
+            end
+
+            private
+
+            def calculated_fields
+              @calculated_fields ||= @submission.data_source.tax_calculator.calculate
             end
           end
         end
