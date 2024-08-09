3ef9b524ce2e641925233ba928742cb0870da854
jenny-heath <43800769+jenny-heath@users.noreply.github.com>
2024-08-09 14:54:09 -0700

    FYST 393: Refactor direct file data, fed info form (#4695)
    * Dynamically define methods for modifying df xml nodes
    * Add parent class for DfW2 and DfIrsW2Form
    * Add parent class for Df1099R and DfIrs1099RForm
    * Add parent class for df xml accessors
    
    Co-authored-by: Mike Rotondo <mrotondo@codeforamerica.org>

--- app/forms/state_file/federal_info_form.rb
+++ app/forms/state_file/federal_info_form.rb
@@ -117,7 +117,7 @@ module StateFile
           @intake.direct_file_data.build_new_w2_node
         end
         w2 = w2s[index.to_i]
-        DfIrsW2Form::SELECTORS.each_key do |field|
+        DfIrsW2Form.selectors.each_key do |field|
           w2.send(:"#{field}=", w2_attributes[field.to_s])
         end
         index += 1
@@ -131,7 +131,7 @@ module StateFile
           @intake.direct_file_data.build_new_1099r_node
         end
         form1099r = form1099rs[index.to_i]
-        DfIrs1099RForm::SELECTORS.each_key do |field|
+        DfIrs1099RForm.selectors.each_key do |field|
           form1099r.send(:"#{field}=", form1099r_attributes[field.to_s])
         end
         index += 1
@@ -292,105 +292,12 @@ module StateFile
       end
     end
 
-    class DfIrsW2Form
-      include DfXmlCrudMethods
-
-      SELECTORS = {
-        WagesAmt: 'WagesAmt',
-        WithholdingAmt: 'WithholdingAmt',
-        StateWagesAmt: 'W2StateLocalTaxGrp W2StateTaxGrp StateWagesAmt',
-        StateIncomeTaxAmt: 'W2StateLocalTaxGrp W2StateTaxGrp StateIncomeTaxAmt',
-        LocalWagesAndTipsAmt: 'W2StateLocalTaxGrp W2StateTaxGrp W2LocalTaxGrp LocalWagesAndTipsAmt',
-        LocalIncomeTaxAmt: 'W2StateLocalTaxGrp W2StateTaxGrp W2LocalTaxGrp LocalIncomeTaxAmt',
-        LocalityNm: 'W2StateLocalTaxGrp W2StateTaxGrp W2LocalTaxGrp LocalityNm',
-      }
-
-      attr_reader :node
+    class DfIrsW2Form < DfW2Accessor
       attr_accessor :id
-      attr_accessor *SELECTORS.keys
       attr_accessor :_destroy
-
-      def selectors
-        SELECTORS
-      end
-
-      def initialize(node = nil)
-        @node = if node
-          node
-        else
-          Nokogiri::XML(IrsApiService.df_return_sample).at('IRSW2')
-        end
-      end
 
       def id
         @node['documentId']
-      end
-
-      def WagesAmt
-        df_xml_value(__method__)&.to_i
-      end
-
-      def WagesAmt=(value)
-        write_df_xml_value(__method__, value)
-      end
-
-      def WithholdingAmt
-        df_xml_value(__method__)&.to_i
-      end
-
-      def WithholdingAmt=(value)
-        write_df_xml_value(__method__, value)
-      end
-
-      def StateWagesAmt
-        df_xml_value(__method__)&.to_i
-      end
-
-      def StateWagesAmt=(value)
-        create_or_destroy_df_xml_node(__method__, value)
-        write_df_xml_value(__method__, value)
-      end
-
-      def StateIncomeTaxAmt
-        df_xml_value(__method__)&.to_i
-      end
-
-      def StateIncomeTaxAmt=(value)
-        create_or_destroy_df_xml_node(__method__, value)
-        write_df_xml_value(__method__, value)
-      end
-
-      def LocalWagesAndTipsAmt
-        df_xml_value(__method__)&.to_i
-      end
-
-      def LocalWagesAndTipsAmt=(value)
-        create_or_destroy_df_xml_node(__method__, value)
-        if value.present?
-          write_df_xml_value(__method__, value)
-        end
-      end
-
-      def LocalIncomeTaxAmt
-        df_xml_value(__method__)&.to_i
-      end
-
-      def LocalIncomeTaxAmt=(value)
-        create_or_destroy_df_xml_node(__method__, value)
-        if value.present?
-          write_df_xml_value(__method__, value)
-        end
-      end
-
-      def LocalityNm
-        df_xml_value(__method__)
-      end
-
-      def LocalityNm=(value)
-        create_or_destroy_df_xml_node(__method__, value)
-        if value.present?
-          write_df_xml_value(__method__, value)
-        end
       end
 
       def persisted?
@@ -402,148 +309,12 @@ module StateFile
       end
     end
 
-    class DfIrs1099RForm
-      include DfXmlCrudMethods
-
-      SELECTORS = {
-        PayerNameControlTxt: "PayerNameControlTxt",
-        PayerName: "PayerName BusinessNameLine1Txt",
-        AddressLine1Txt: "PayerUSAddress AddressLine1Txt",
-        CityNm: "PayerUSAddress CityNm",
-        StateAbbreviationCd: "PayerUSAddress StateAbbreviationCd",
-        ZIPCd: "PayerUSAddress ZIPCd",
-        PayerEIN: "PayerEIN",
-        PhoneNum: "PhoneNum",
-        GrossDistributionAmt: "GrossDistributionAmt",
-        TaxableAmt: "TaxableAmt",
-        FederalIncomeTaxWithheldAmt: "FederalIncomeTaxWithheldAmt",
-        F1099RDistributionCd: "F1099RDistributionCd",
-        StandardOrNonStandardCd: "StandardOrNonStandardCd",
-      }
-
-      attr_reader :node
+    class DfIrs1099RForm < Df1099rAccessor
       attr_accessor :id
-      attr_accessor *SELECTORS.keys
       attr_accessor :_destroy
-
-      def selectors
-        SELECTORS
-      end
-
-      def initialize(node = nil)
-        @node = if node
-                  node
-                else
-                  Nokogiri::XML(StateFile::XmlReturnSampleService.new.read("az_richard_retirement_1099r")).at('IRS1099R')
-                end
-      end
 
       def id
         @node['documentId']
-      end
-
-      def PayerNameControlTxt
-        df_xml_value(__method__)
-      end
-
-      def PayerNameControlTxt=(value)
-        write_df_xml_value(__method__, value)
-      end
-
-      def PayerName
-        df_xml_value(__method__)
-      end
-
-      def PayerName=(value)
-        write_df_xml_value(__method__, value)
-      end
-
-      def AddressLine1Txt
-        df_xml_value(__method__)
-      end
-
-      def AddressLine1Txt=(value)
-        write_df_xml_value(__method__, value)
-      end
-
-      def CityNm
-        df_xml_value(__method__)
-      end
-
-      def CityNm=(value)
-        write_df_xml_value(__method__, value)
-      end
-
-      def StateAbbreviationCd
-        df_xml_value(__method__)
-      end
-
-      def StateAbbreviationCd=(value)
-        write_df_xml_value(__method__, value)
-      end
-
-      def ZIPCd
-        df_xml_value(__method__)
-      end
-
-      def ZIPCd=(value)
-        write_df_xml_value(__method__, value)
-      end
-
-      def PayerEIN
-        df_xml_value(__method__)
-      end
-
-      def PayerEIN=(value)
-        write_df_xml_value(__method__, value)
-      end
-
-      def PhoneNum
-        df_xml_value(__method__)
-      end
-
-      def PhoneNum=(value)
-        write_df_xml_value(__method__, value)
-      end
-
-      def GrossDistributionAmt
-        df_xml_value(__method__)
-      end
-
-      def GrossDistributionAmt=(value)
-        write_df_xml_value(__method__, value)
-      end
-
-      def TaxableAmt
-        df_xml_value(__method__)
-      end
-
-      def TaxableAmt=(value)
-        write_df_xml_value(__method__, value)
-      end
-
-      def FederalIncomeTaxWithheldAmt
-        df_xml_value(__method__)
-      end
-
-      def FederalIncomeTaxWithheldAmt=(value)
-        write_df_xml_value(__method__, value)
-      end
-
-      def F1099RDistributionCd
-        df_xml_value(__method__)
-      end
-
-      def F1099RDistributionCd=(value)
-        write_df_xml_value(__method__, value)
-      end
-
-      def StandardOrNonStandardCd
-        df_xml_value(__method__)
-      end
-
-      def StandardOrNonStandardCd=(value)
-        write_df_xml_value(__method__, value)
       end
 
       def persisted?
