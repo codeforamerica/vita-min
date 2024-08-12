12bb2d9ee8547acb047cd4fb685ccfc907553f73
Kevin Diale <powersurge360@gmail.com>
2024-08-12 10:45:05 -0400

    Controller & views

---
+++ app/controllers/state_file/questions/az_qualifying_organization_contributions_controller.rb
@@ -0,0 +1,72 @@
+module StateFile
+  module Questions
+    class AzQualifyingOrganizationContributionsController < QuestionsController
+      include ReturnToReviewConcern
+
+      before_action -> { @filing_year = Rails.configuration.statefile_current_tax_year }
+
+      def index
+        redirect_to action: :new unless contributions.present?
+      end
+
+      def edit = contribution
+
+      def update
+        contribution.assign_attributes(az321_contribution_params)
+
+        if contribution.valid?
+          contribution.save
+
+          redirect_to action: :index, return_to_review: params[:return_to_review]
+        end
+      end
+
+      def new = contributions.build
+
+      def create
+        @contribution = contributions.build(az321_contribution_params)
+
+
+        if @contribution.valid?
+          return redirect_to next_path unless @contribution.made_contributions == "yes"
+
+          @contribution.save
+
+          redirect_to action: :index, return_to_review: params[:return_to_review]
+        else
+          render :new
+        end
+      end
+
+      def destroy
+        if contribution.destroy
+          flash[:notice] = "Form destroyed"
+        end
+
+        redirect_to action: :index, return_to_review: params[:return_to_review]
+      end
+
+      def self.navigation_actions
+        [:index, :new]
+      end
+
+      private
+
+      def contributions
+        @contributions ||= current_intake.az321_contributions
+      end
+
+      def contribution
+        @contribution ||= contributions.find(params[:id])
+      end
+
+      def az321_contribution_params
+        params.require(:az321_contribution).permit(
+            :charity_name, :charity_code, :amount,
+            :date_of_contribution_day, :date_of_contribution_month,
+            :date_of_contribution_year, :made_contributions
+          )
+      end
+    end
+  end
+end
