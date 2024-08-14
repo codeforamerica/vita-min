73f5ef015b22a540d5e8131ac72dbc681dd104ce
Kevin Diale <powersurge360@gmail.com>
2024-08-14 17:50:48 -0400

    Annotations

--- spec/models/az321_contribution_spec.rb
+++ spec/models/az321_contribution_spec.rb
@@ -1,3 +1,20 @@
+# == Schema Information
+#
+# Table name: az321_contributions
+#
+#  id                      :bigint           not null, primary key
+#  amount                  :decimal(12, 2)
+#  charity_code            :string
+#  charity_name            :string
+#  date_of_contribution    :date
+#  created_at              :datetime         not null
+#  updated_at              :datetime         not null
+#  state_file_az_intake_id :bigint
+#
+# Indexes
+#
+#  index_az321_contributions_on_state_file_az_intake_id  (state_file_az_intake_id)
+#
 require 'rails_helper'
 
 describe Az321Contribution do
