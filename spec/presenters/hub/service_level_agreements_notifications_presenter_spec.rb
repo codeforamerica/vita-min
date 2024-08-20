ea2f78985b7299dd959e6273313389aefa39a635
Tahsina Islam <tislam@codeforamerica.org@Tahsinas-MacBook-Pro.local>
2024-08-20 15:50:40 -0500

    Make tests more clear

--- spec/presenters/hub/service_level_agreements_notifications_presenter_spec.rb
+++ spec/presenters/hub/service_level_agreements_notifications_presenter_spec.rb
@@ -12,6 +12,8 @@ describe Hub::Dashboard::ServiceLevelAgreementsNotificationsPresenter do
   before do
     create :client, vita_partner: oregano_org, last_outgoing_communication_at: 5.business_days.ago, filterable_tax_return_properties: [{active: true }]
     create :client, vita_partner: oregano_org, last_outgoing_communication_at: 7.business_days.ago, filterable_tax_return_properties: [{active: true }]
+    create :client, vita_partner: oregano_org, last_outgoing_communication_at: 7.business_days.ago, filterable_tax_return_properties: [{active: false }]
+    create :client, vita_partner: orangutan_organization, last_outgoing_communication_at: 5.business_days.ago, filterable_tax_return_properties: [{active: false }]
     create :client, vita_partner: orangutan_organization, last_outgoing_communication_at: 5.business_days.ago, filterable_tax_return_properties: [{active: true }]
     create :client, vita_partner: orangutan_organization, last_outgoing_communication_at: 8.business_days.ago, filterable_tax_return_properties: [{active: true }]
     create :client, vita_partner: orangutan_organization, last_outgoing_communication_at: 9.business_days.ago, filterable_tax_return_properties: [{active: true }]
