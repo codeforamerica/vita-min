f98b7ed5d2472d24d23ff7f217da628808fa7e2f
jenny-heath <43800769+jenny-heath@users.noreply.github.com>
2024-08-21 15:58:02 -0700

    Add initial content to NC submission builder (#4711)

--- app/lib/submission_builder/ty2024/states/nc/nc_return_xml.rb
+++ app/lib/submission_builder/ty2024/states/nc/nc_return_xml.rb
@@ -4,6 +4,24 @@ module SubmissionBuilder
     module States
       module Nc
         class NcReturnXml < SubmissionBuilder::StateReturn
+          private
+
+          def build_xml_doc_tag
+            "efile:ReturnState"
+          end
+
+          def schema_file
+            SchemaFileLoader.load_file("us_states", "unpacked", "NCIndividual2023v1.0", "NCIndividual", "IndividualReturnNCD400.xsd")
+          end
+
+          def documents_wrapper
+            xml_doc = build_xml_doc("FormNCD400")
+            xml_doc.at('*')
+          end
+
+          def supported_documents
+            []
+          end
         end
       end
     end
