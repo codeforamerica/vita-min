844e0db87951953ac9ce22f32f086ae8b83176d8
em barnard-shao <49880002+embarnard@users.noreply.github.com>
2024-08-22 11:55:44 -0400

    Don't show suspended users and don't count tax returns from archived clients in the team assignments module (#4710)

--- app/presenters/hub/dashboard/team_assignment_presenter.rb
+++ app/presenters/hub/dashboard/team_assignment_presenter.rb
@@ -18,16 +18,16 @@ module Hub
                                      organizations = @selected_model.instance_of?(Organization) ? @selected_model : @current_user.role.coalition.organizations
                                      sites = VitaPartner.sites.where(parent_organization: organizations)
                                      roles = OrganizationLeadRole.where(organization: organizations) + SiteCoordinatorRole.assignable_to_sites(sites) + TeamMemberRole.assignable_to_sites(sites)
-                                     User.where(role: roles)
+                                     User.active.where(role: roles)
                                    elsif @current_user.org_lead?
                                      organization = @current_user.role.organization
                                      sites = @selected_model.instance_of?(Site) ? @selected_model : VitaPartner.sites.where(parent_organization: organization)
                                      roles = OrganizationLeadRole.where(organization: organization) + SiteCoordinatorRole.assignable_to_sites(sites) + TeamMemberRole.assignable_to_sites(sites)
-                                     User.where(role: roles)
+                                     User.active.where(role: roles)
                                    elsif @current_user.site_coordinator?
                                      sites = @selected_model.instance_of?(Site) ? @selected_model : @current_user.role.sites
                                      roles = SiteCoordinatorRole.assignable_to_sites(sites) + TeamMemberRole.assignable_to_sites(sites)
-                                     User.where(role: roles)
+                                     User.active.where(role: roles)
                                    end
 
         @user_count = accessible_users_by_role.count
@@ -38,10 +38,12 @@ module Hub
       def ordered_by_tr_count_users
         return unless team_assignment_users.present?
 
-        team_assignment_users.select('users.*, COUNT(tax_returns.id) AS tax_returns_count')
-                             .left_joins(:assigned_tax_returns)
-                             .group('users.id, users.name, users.role_type')
-                             .order('tax_returns_count DESC')
+        team_assignment_users
+          .select('users.*, COUNT(tax_returns.id) AS tax_returns_count')
+          .left_joins(assigned_tax_returns: :client)
+          .group('users.id, users.name, users.role_type')
+          .order('tax_returns_count DESC')
+          .where('clients.filterable_product_year = :product_year OR clients.id IS NULL', product_year: Rails.configuration.product_year)
       end
 
     end
