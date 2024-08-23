648a8f4d93f61defd4cd2698371c3e0ed15630ed
Kevin Diale <powersurge360@gmail.com>
2024-08-23 10:54:47 -0400

    Annotations added

--- spec/models/state_file_az_intake_spec.rb
+++ spec/models/state_file_az_intake_spec.rb
@@ -35,6 +35,7 @@
 #  last_sign_in_ip                             :inet
 #  locale                                      :string           default("en")
 #  locked_at                                   :datetime
+#  made_az321_contributions                    :integer          default("unfilled"), not null
 #  message_tracker                             :jsonb
 #  payment_or_deposit_type                     :integer          default("unfilled"), not null
 #  phone_number                                :string
