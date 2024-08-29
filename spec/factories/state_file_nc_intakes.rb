7ea2b1ec63c5b620bfc84b5b60722f6641b1642c
Tahsina Islam <tislam@codeforamerica.org@Tahsinas-MacBook-Pro.local>
2024-08-29 11:21:19 -0400

    Add fixtures

--- spec/factories/state_file_nc_intakes.rb
+++ spec/factories/state_file_nc_intakes.rb
@@ -91,5 +91,21 @@ FactoryBot.define do
     trait :df_data_many_w2s do
       raw_direct_file_data { StateFile::XmlReturnSampleService.new.read('nc_cookiemonster') }
     end
+
+    trait :single do
+      raw_direct_file_data { StateFile::XmlReturnSampleService.new.read('tucker_single') }
+    end
+
+    trait :head_of_household do
+      raw_direct_file_data { StateFile::XmlReturnSampleService.new.read('shiloh_mfs') }
+    end
+
+    trait :married_filing_separately do
+      raw_direct_file_data { StateFile::XmlReturnSampleService.new.read('sheldon_mfs') }
+    end
+
+    trait :qualified_widow do
+      raw_direct_file_data { StateFile::XmlReturnSampleService.new.read('laney_qss') }
+    end
   end
 end
