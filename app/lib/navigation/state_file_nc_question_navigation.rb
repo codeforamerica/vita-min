a35da0b620643c1db57d021095e3c4153f8f2b9b
Tahsina Islam <tislam@codeforamerica.org>
2024-09-03 15:21:07 -0400

    FYST-483 Add review page for NC flow

--- app/lib/navigation/state_file_nc_question_navigation.rb
+++ app/lib/navigation/state_file_nc_question_navigation.rb
@@ -33,6 +33,7 @@ module Navigation
         Navigation::NavigationStep.new(StateFile::Questions::NameDobController),
         Navigation::NavigationStep.new(StateFile::Questions::W2Controller),
         Navigation::NavigationStep.new(StateFile::Questions::UnemploymentController),
+        Navigation::NavigationStep.new(StateFile::Questions::NcReviewController),
         Navigation::NavigationStep.new(StateFile::Questions::TaxesOwedController),
         Navigation::NavigationStep.new(StateFile::Questions::TaxRefundController),
         Navigation::NavigationStep.new(StateFile::Questions::EsignDeclarationController), # creates EfileSubmission and transitions to preparing
