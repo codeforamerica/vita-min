cd6ee111e86b96d9e406c65b8d8d6e06abbb6339
Em Barnard-Shao <ebarnard@codeforamerica.org>
2024-09-04 17:31:43 -0400

    Add reload

--- spec/lib/pdf_filler/az321_pdf_spec.rb
+++ spec/lib/pdf_filler/az321_pdf_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe PdfFiller::Az321Pdf do
 
   describe '#hash_for_pdf' do
     it 'uses field names that exist in the pdf' do
-      pdf_fields = filled_in_values(submission.generate_filing_pdf.path)
+      pdf_fields = filled_in_values(submission.reload.generate_filing_pdf.path)
       missing_fields = pdf.hash_for_pdf.keys.map(&:to_s) - pdf_fields.keys
       expect(missing_fields).to eq([])
     end
