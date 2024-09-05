ff7a08d3e20846848d426d50f60a96e747580a94
Anne LoVerso <anne.loverso@gmail.com>
2024-09-05 16:09:13 -0400

    nj 34 - add municipality/county selectors for CountyCode

--- spec/lib/submission_builder/ty2024/states/nj/nj_return_xml_spec.rb
+++ spec/lib/submission_builder/ty2024/states/nj/nj_return_xml_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe SubmissionBuilder::Ty2024::States::Nj::NjReturnXml, required_schema: "nj" do
   describe '.build' do
-    let(:intake) { create(:state_file_nj_intake) }
+    let(:intake) { create(:state_file_nj_intake, municipality_code: "0101") }
     let(:submission) { create(:efile_submission, data_source: intake) }
     let!(:initial_efile_device_info) { create :state_file_efile_device_info, :initial_creation, :filled, intake: intake }
     let!(:submission_efile_device_info) { create :state_file_efile_device_info, :submission, :filled, intake: intake }
@@ -12,6 +12,10 @@ describe SubmissionBuilder::Ty2024::States::Nj::NjReturnXml, required_schema: "n
       expect(xml.document.root.namespaces).to include({ "xmlns:efile" => "http://www.irs.gov/efile", "xmlns" => "http://www.irs.gov/efile" })
       expect(xml.document.at('AuthenticationHeader').to_s).to include('xmlns="http://www.irs.gov/efile"')
       expect(xml.document.at('ReturnHeaderState').to_s).to include('xmlns="http://www.irs.gov/efile"')
+    end
+
+    it "includes municipality code with a prepending 0" do
+      expect(xml.document.at("CountyCode").to_s).to include("00101")
     end
   end
 end
