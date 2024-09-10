c17a56f358189531b3c7bd608c12ba6432f4fcf8
Mike Rotondo <mrotondo@codeforamerica.org>
2024-09-09 17:25:47 -0700

    decided not to rename the column that stores the df xml in this scope. we can do it later if we want.

--- db/migrate/20240909220319_update_intake_to_store_xml_and_json_from_df_api.rb
+++ db/migrate/20240909220319_update_intake_to_store_xml_and_json_from_df_api.rb
@@ -4,10 +4,5 @@ class UpdateIntakeToStoreXmlAndJsonFromDfApi < ActiveRecord::Migration[7.1]
     add_column :state_file_nc_intakes, :raw_direct_file_intake_data, :jsonb
     add_column :state_file_nj_intakes, :raw_direct_file_intake_data, :jsonb
     add_column :state_file_ny_intakes, :raw_direct_file_intake_data, :jsonb
-
-    add_column :state_file_az_intakes, :raw_direct_file_tax_return_data, :text
-    add_column :state_file_nc_intakes, :raw_direct_file_tax_return_data, :text
-    add_column :state_file_nj_intakes, :raw_direct_file_tax_return_data, :text
-    add_column :state_file_ny_intakes, :raw_direct_file_tax_return_data, :text
   end
 end
