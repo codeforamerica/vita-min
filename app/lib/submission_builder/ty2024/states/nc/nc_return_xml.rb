502e6d810a74ce8d826dd5793df68450976d9cad
Tahsina Islam <tislam@codeforamerica.org>
2024-09-10 18:11:29 -0500

    Add review and pdf

--- app/lib/submission_builder/ty2024/states/nc/nc_return_xml.rb
+++ app/lib/submission_builder/ty2024/states/nc/nc_return_xml.rb
@@ -22,28 +22,6 @@ module SubmissionBuilder
             SchemaFileLoader.load_file("us_states", "unpacked", "NCIndividual2023v1.0", "NCIndividual", "IndividualReturnNCD400.xsd")
           end
 
-          def documents_wrapper
-            xml_doc = build_xml_doc("FormNCD400") do |xml|
-              xml.ResidencyStatusPrimary true
-              xml.ResidencyStatusSpouse true if @submission.data_source.filing_status_mfj?
-              xml.VeteranInfoPrimary @submission.data_source.primary_veteran_yes? ? 1 : 2
-              if @submission.data_source.filing_status_mfj?
-                xml.VeteranInfoSpouse @submission.data_source.spouse_veteran_yes? ? 1 : 2
-              end
-              xml.FilingStatus filing_status
-              if @submission.data_source.filing_status_mfs?
-                xml.MFSSpouseName @submission.data_source.direct_file_data.spouse_name
-                xml.MFSSpouseSSN @submission.data_source.direct_file_data.spouse_ssn
-              end
-              if @submission.data_source.filing_status_qw?
-                xml.QWYearSpouseDied Date.parse(@submission.data_source.direct_file_data.spouse_date_of_death).year
-              end
-              xml.FAGI @submission.data_source.direct_file_data.fed_agi
-              xml.NCStandardDeduction standard_deduction
-            end
-            xml_doc.at('*')
-          end
-
           def supported_documents
             supported_docs = [
               {
