d654a81b47572a9b2bf836f54b654694e32dce20
Tahsina Islam <tislam@codeforamerica.org>
2024-09-16 13:24:59 -0500

    Add AZ322 and AZ321 pdf specs

--- spec/models/state_file_nc_intake_spec.rb
+++ spec/models/state_file_nc_intake_spec.rb
@@ -37,9 +37,12 @@
 #  primary_last_name                 :string
 #  primary_middle_initial            :string
 #  primary_suffix                    :string
+#  primary_veteran                   :integer          default(0), not null
 #  raw_direct_file_data              :text
 #  referrer                          :string
 #  routing_number                    :integer
+#  sales_use_tax                     :decimal(12, 2)
+#  sales_use_tax_calculation_method  :integer          default(0), not null
 #  sign_in_count                     :integer          default(0), not null
 #  source                            :string
 #  spouse_birth_date                 :date
@@ -49,10 +52,12 @@
 #  spouse_last_name                  :string
 #  spouse_middle_initial             :string
 #  spouse_suffix                     :string
+#  spouse_veteran                    :integer          default(0), not null
 #  ssn                               :string
 #  street_address                    :string
 #  tax_return_year                   :integer
 #  unsubscribed_from_email           :boolean          default(FALSE), not null
+#  untaxed_out_of_state_purchases    :integer          default(0), not null
 #  withdraw_amount                   :integer
 #  zip_code                          :string
 #  created_at                        :datetime         not null
