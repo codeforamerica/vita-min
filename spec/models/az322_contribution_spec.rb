71828dbd004742507ebb1537cdc8c6b85b886cc0
Arin Choi <achoi@codeforamerica.org>
2024-09-17 15:45:23 -0700

    Add missing specs for az322

---
+++ spec/models/az322_contribution_spec.rb
@@ -0,0 +1,104 @@
+# == Schema Information
+#
+# Table name: az322_contributions
+#
+#  id                      :bigint           not null, primary key
+#  amount                  :decimal(12, 2)
+#  ctds_code               :string
+#  date_of_contribution    :date
+#  district_name           :string
+#  made_contribution       :integer          default("unfilled"), not null
+#  school_name             :string
+#  created_at              :datetime         not null
+#  updated_at              :datetime         not null
+#  state_file_az_intake_id :bigint
+#
+# Indexes
+#
+#  index_az322_contributions_on_state_file_az_intake_id  (state_file_az_intake_id)
+#
+require 'rails_helper'
+
+describe 'Az322Contribution' do
+  let(:intake) { create(:state_file_az_intake) }
+
+  describe "#made_contributions" do
+    it 'should validate presence' do
+      az = Az322Contribution.new(state_file_az_intake: intake, made_contribution: nil)
+      az.valid?
+      expect(az.errors[:made_contribution]).not_to be_empty
+
+      az.made_contribution = 'yes'
+      az.valid?
+      expect(az.errors[:made_contribution]).to be_empty
+
+      az.made_contribution = 'gobble'
+      az.valid?
+      expect(az.errors[:made_contribution]).not_to be_empty
+    end
+  end
+
+  context "made_contributions is true" do
+    let(:az) { Az322Contribution.new(state_file_az_intake: intake, made_contribution: 'yes') }
+
+    describe "#school_name" do
+      it 'should validate presence' do
+        az.school_name = nil
+        az.valid?
+        expect(az.errors[:school_name]).not_to be_empty
+
+        az.school_name = 'Hopscotch School'
+        az.valid?
+        expect(az.errors[:school_name]).to be_empty
+      end
+    end
+
+    describe "#ctds_code" do
+      it 'should validate presence' do
+        az.ctds_code = nil
+        az.valid?
+        expect(az.errors[:ctds_code]).not_to be_empty
+
+        az.ctds_code = '123456789'
+        az.valid?
+        expect(az.errors[:ctds_code]).to be_empty
+
+        az.ctds_code = '12345678'
+        az.valid?
+        expect(az.errors[:ctds_code]).not_to be_empty
+
+        az.ctds_code = 'fffffffff'
+        az.valid?
+        expect(az.errors[:ctds_code]).not_to be_empty
+      end
+    end
+
+    describe "#district_name" do
+      it 'should validate presence' do
+        az.district_name = nil
+        az.valid?
+        expect(az.errors[:district_name]).not_to be_empty
+
+        az.district_name = 'Sesame District'
+        az.valid?
+        expect(az.errors[:district_name]).to be_empty
+      end
+    end
+
+    describe "#amount" do
+      it 'should validate presence' do
+        az.amount = nil
+        az.valid?
+        expect(az.errors[:amount]).not_to be_empty
+
+        az.amount = 0
+        az.valid?
+        expect(az.errors[:amount]).not_to be_empty
+
+        az.amount = 1
+        az.valid?
+        expect(az.errors[:amount]).to be_empty
+      end
+    end
+  end
+end
