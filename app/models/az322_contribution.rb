a114fd5717b43a0393e9bc84fe2302696c520c41
Yejee Arin Choi <achoi@codeforamerica.org>
2024-09-20 15:29:06 -0700

    Follow patterns for date selection and utilize DateAccessible module to validate date inputs (#4783)

--- app/models/az322_contribution.rb
+++ app/models/az322_contribution.rb
@@ -18,6 +18,8 @@
 #  index_az322_contributions_on_state_file_az_intake_id  (state_file_az_intake_id)
 #
 class Az322Contribution < ApplicationRecord
+  date_accessor :date_of_contribution
+
   include DateHelper
   belongs_to :state_file_az_intake
 
@@ -28,30 +30,12 @@ class Az322Contribution < ApplicationRecord
   validates :ctds_code, presence: true, format: { with: /\A\d{9}\z/, message: -> (_object, _data) { I18n.t("validators.ctds_code") }}, if: -> { made_contribution == "yes" }
   validates :district_name, presence: true, if: -> { made_contribution == "yes" }
   validates :amount, presence: true, numericality: { greater_than: 0 }, if: -> { made_contribution == "yes" }
-  validate :date_of_contribution_is_valid_date, if: -> { made_contribution == "yes" }
 
-  attr_accessor :date_of_contribution_day, :date_of_contribution_month, :date_of_contribution_year
-
-  before_validation :set_date_of_contribution, if: -> { made_contribution == "yes" }
+  validates :date_of_contribution,
+            inclusion: {
+              in: TAX_YEAR.beginning_of_year..TAX_YEAR.end_of_year
+            },
+            presence: true
 
   private
-
-  def set_date_of_contribution
-    if date_of_contribution_year.present? || date_of_contribution_month.present? || date_of_contribution_day.present?
-      self.date_of_contribution = parse_date_params(date_of_contribution_year, date_of_contribution_month, date_of_contribution_day)
-    end
-  end
-
-  def date_of_contribution_is_valid_date
-    if date_of_contribution_year.present? || date_of_contribution_month.present? || date_of_contribution_day.present?
-      valid_text_date(date_of_contribution_year, date_of_contribution_month, date_of_contribution_day, :date_of_contribution)
-    else
-      if ((DateTime.parse(date_of_contribution) rescue ArgumentError) == ArgumentError)
-        errors.add(:date_of_contribution, I18n.t('errors.attributes.birth_date.blank'))
-        return false
-      end
-
-      true
-    end
-  end
 end
