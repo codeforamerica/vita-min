acbeadff131d5bc7c003a660bb5c4036f1bdf919
Jenny Heath <jheath@codeforamerica.org>
2024-09-30 14:50:31 -0700

    small refactor of state code enums

--- app/models/efile_error.rb
+++ app/models/efile_error.rb
@@ -22,10 +22,9 @@ class EfileError < ApplicationRecord
   has_rich_text :resolution_en
   has_rich_text :resolution_es
 
-  state_enum_options = StateFile::StateInformationService.active_state_codes.reduce({}) do |acc, state_code|
-    int_value = state_code.bytes.map(&:to_s).join.to_i
-    acc["state_file_#{state_code}"] = int_value
-    acc
+  state_enum_options = StateFile::StateInformationService.active_state_codes.to_h do |state_code, _|
+    int_value = state_code.bytes.join.to_i
+    ["state_file_#{state_code}", int_value]
   end
   enum service_type: { unfilled: 0, ctc: 1, state_file: 2 }.merge(state_enum_options), _prefix: :service_type
 
