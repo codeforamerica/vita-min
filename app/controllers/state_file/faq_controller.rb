a2ed2b88404338f8572e464c696f15a4f25e2c47
Jenny Heath <jheath@codeforamerica.org>
2024-09-30 17:09:06 -0700

    FYST FAQ: in the off season, only show states that will reopen

--- app/controllers/state_file/faq_controller.rb
+++ app/controllers/state_file/faq_controller.rb
@@ -3,13 +3,23 @@ class StateFile::FaqController < ApplicationController
 
   def index
     intake_start = Rails.configuration.state_file_start_of_open_intake
-    active_tax_year = if (app_time.year == intake_start.year) && app_time < intake_start
-                        # if the start of intake is set to this year and it is currently before that date, we should still show the previous year's states
-                        Rails.configuration.statefile_current_tax_year - 1
-                      else
-                        Rails.configuration.statefile_current_tax_year
-                      end
-    visible_state_code_names = StateFile::StateInformationService.state_code_to_name_map.filter { |code, name| StateFile::StateInformationService.filing_years(code).include?(active_tax_year) }
+    intake_end = Rails.configuration.state_file_end_of_in_progress_intakes
+    tax_year = Rails.configuration.statefile_current_tax_year
+
+    filter_years = if intake_start < app_time && app_time < intake_end # intake is open
+                     # show currently open states
+                     [tax_year]
+                   elsif app_time.year == intake_start.year && app_time < intake_start # before intake opens this year
+                     # show states that were open the previous year and will open this year
+                     [tax_year - 1, tax_year]
+                   else # intake is closed for the year
+                     # show states that were open this year and will open next year
+                     [tax_year, tax_year + 1]
+                   end
+
+    visible_state_code_names = StateFile::StateInformationService.state_code_to_name_map.filter do |state_code, _|
+      filter_years.all? { |year| StateFile::StateInformationService.filing_years(state_code).include?(year) }
+    end
     @state_code_names = if params[:us_state] == 'us'
                           visible_state_code_names
                         else
--- spec/controllers/state_file/faq_controller_spec.rb
+++ spec/controllers/state_file/faq_controller_spec.rb
@@ -16,11 +16,31 @@ RSpec.describe StateFile::FaqController do
     end
 
     context "showing the right states" do
+      shared_examples :showing_only_states_that_were_open_and_will_reopen do
+        it "shows the right states" do
+          get :index, params: { us_state: "us" }
+
+          expect(response.body).to have_text I18n.t("state_file.faq.index.title", state: "state that was open last season and will reopen next season")
+          expect(response.body).not_to have_text I18n.t("state_file.faq.index.title", state: "new state that will be open this season but not yet")
+          expect(response.body).not_to have_text I18n.t("state_file.faq.index.title", state: "state that was open last season but will not reopen this season")
+        end
+      end
+
+      before do
+        allow(StateFile::StateInformationService).to receive(:state_code_to_name_map).and_return(
+          {
+            state_abbrev_1: "state that was open last season and will reopen next season",
+            state_abbrev_2: "new state that will be open this season but not yet",
+            state_abbrev_3: "state that was open last season but will not reopen this season"
+          }
+        )
+      end
+
       context "when FYST has not opened yet for the year" do
         before do
           allow(StateFile::StateInformationService).to receive(:filing_years).with(:state_abbrev_1).and_return([current_year - 1, current_year - 2])
           allow(StateFile::StateInformationService).to receive(:filing_years).with(:state_abbrev_2).and_return([current_year - 1])
-          allow(StateFile::StateInformationService).to receive(:state_code_to_name_map).and_return({ state_abbrev_1: "state that was open last year", state_abbrev_2: "state that will be open this year but not yet" })
+          allow(StateFile::StateInformationService).to receive(:filing_years).with(:state_abbrev_3).and_return([current_year - 2])
         end
 
         around do |example|
@@ -29,23 +49,25 @@ RSpec.describe StateFile::FaqController do
           end
         end
 
-        it "shows states that were active the last time the app was open" do
-          get :index, params: { us_state: "us" }
-
-          expect(response.body).to have_text I18n.t("state_file.faq.index.title", state: "state that was open last year")
-          expect(response.body).not_to have_text I18n.t("state_file.faq.index.title", state: "state that will be open this year but not yet")
-        end
+        it_behaves_like :showing_only_states_that_were_open_and_will_reopen
       end
 
       context "when FYST is either open or has closed for the year" do
         before do
-          allow(StateFile::StateInformationService).to receive(:filing_years).with(:state_abbrev_1).and_return([current_year - 1, current_year - 2])
-          allow(StateFile::StateInformationService).to receive(:filing_years).with(:state_abbrev_2).and_return([current_year - 2])
+          allow(StateFile::StateInformationService).to receive(:filing_years).with(:state_abbrev_1).and_return([current_year, current_year - 1, current_year - 2])
+          allow(StateFile::StateInformationService).to receive(:filing_years).with(:state_abbrev_2).and_return([current_year - 1])
+          allow(StateFile::StateInformationService).to receive(:filing_years).with(:state_abbrev_3).and_return([current_year - 2])
         end
 
         context "open" do
           before do
-            allow(StateFile::StateInformationService).to receive(:state_code_to_name_map).and_return({ state_abbrev_1: "state that is open this year", state_abbrev_2: "state that was open last year but not this year" })
+            allow(StateFile::StateInformationService).to receive(:state_code_to_name_map).and_return(
+              {
+                state_abbrev_1: "state that is open this season",
+                state_abbrev_2: "new state that just opened this season",
+                state_abbrev_3: "state that was open last season but not this season"
+              }
+            )
           end
           around do |example|
             Timecop.freeze(Rails.configuration.state_file_end_of_in_progress_intakes - 1.day) do
@@ -53,30 +75,23 @@ RSpec.describe StateFile::FaqController do
             end
           end
 
-          it "shows states that are currently active" do
+          it "shows states that are currently open" do
             get :index, params: { us_state: "us" }
 
-            expect(response.body).to have_text I18n.t("state_file.faq.index.title", state: "state that is open this year")
-            expect(response.body).not_to have_text I18n.t("state_file.faq.index.title", state: "state that was open last year but not this year")
+            expect(response.body).to have_text I18n.t("state_file.faq.index.title", state: "state that is open this season")
+            expect(response.body).to have_text I18n.t("state_file.faq.index.title", state: "new state that just opened this season")
+            expect(response.body).not_to have_text I18n.t("state_file.faq.index.title", state: "state that was open last season but not this season")
           end
         end
 
         context "after close" do
-          before do
-            allow(StateFile::StateInformationService).to receive(:state_code_to_name_map).and_return({ state_abbrev_1: "state that was open this year", state_abbrev_2: "state that was open last year but was not this year" })
-          end
           around do |example|
             Timecop.freeze(Rails.configuration.state_file_end_of_in_progress_intakes + 1.day) do
               example.run
             end
           end
 
-          it "shows states that were active during the season" do
-            get :index, params: { us_state: "us" }
-
-            expect(response.body).to have_text I18n.t("state_file.faq.index.title", state: "state that was open this year")
-            expect(response.body).not_to have_text I18n.t("state_file.faq.index.title", state: "state that was open last year but was not this year")
-          end
+          it_behaves_like :showing_only_states_that_were_open_and_will_reopen
         end
       end
     end
