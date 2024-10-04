b5931a7787561eb1ca460d9db314bf4303794a14
jenny-heath <43800769+jenny-heath@users.noreply.github.com>
2024-10-04 13:26:30 -0700

    Fyst 524 add MD, ID, and NJ FAQ (#4818)
    * FYST FAQ: in the off season, only show states that will reopen

--- app/controllers/state_file/faq_controller.rb
+++ app/controllers/state_file/faq_controller.rb
@@ -2,14 +2,9 @@ class StateFile::FaqController < ApplicationController
   layout "state_file"
 
   def index
-    intake_start = Rails.configuration.state_file_start_of_open_intake
-    active_tax_year = if (app_time.year == intake_start.year) && app_time < intake_start
-                        # if the start of intake is set to this year and it is currently before that date, we should still show the previous year's states
-                        Rails.configuration.statefile_current_tax_year - 1
-                      else
-                        Rails.configuration.statefile_current_tax_year
-                      end
-    visible_state_code_names = StateFile::StateInformationService.state_code_to_name_map.filter { |code, name| StateFile::StateInformationService.filing_years(code).include?(active_tax_year) }
+    visible_state_code_names = StateFile::StateInformationService.state_code_to_name_map.filter do |state_code, _|
+      filing_years_to_show.all? { |year| StateFile::StateInformationService.filing_years(state_code).include?(year) }
+    end
     @state_code_names = if params[:us_state] == 'us'
                           visible_state_code_names
                         else
@@ -22,5 +17,32 @@ class StateFile::FaqController < ApplicationController
     @faq_category = FaqCategory.find_by(slug: @section_key, product_type: FaqCategory.state_to_product_type(params[:us_state]))
 
     raise ActionController::RoutingError.new('Not found') unless @faq_category
+  end
+
+  private
+
+  def filing_years_to_show
+    intake_start = Rails.configuration.state_file_start_of_open_intake
+    intake_end = Rails.configuration.state_file_end_of_in_progress_intakes
+    tax_year = Rails.configuration.statefile_current_tax_year
+
+    if Rails.env.production?
+      if app_time.between?(intake_start, intake_end) # intake is open
+        # show currently open states
+        [tax_year]
+      elsif app_time.year == intake_start.year && app_time < intake_start # before intake opens this year
+        # show states that were open the previous year and will open this year
+        [tax_year - 1, tax_year]
+      else # intake is closed for the year
+        # show states that were open this year and will open next year
+        [tax_year, tax_year + 1]
+      end
+    else
+      if app_time > intake_end
+        [tax_year + 1]
+      else
+        [tax_year]
+      end
+    end
   end
 end
