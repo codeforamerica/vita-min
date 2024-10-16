8fcc8a7937f341cfdaec322257582e4390c6d1ee
Tahsina Islam <tislam@codeforamerica.org>
2024-10-16 11:05:39 -0500

    Trying js
    Co-authored-by: Kevin Diale <kdiale@codeforamerica.org>

--- app/controllers/state_file/questions/md_county_controller.rb
+++ app/controllers/state_file/questions/md_county_controller.rb
@@ -8,19 +8,6 @@ module StateFile
         @subdivisions = current_intake.subdivisions_for_select
         super
       end
-
-      def update
-        # Make sure to re-initialize the dropdowns here in case of an error
-        @counties = current_intake.counties_for_select
-        @subdivisions = current_intake.subdivisions_for_select
-
-        # Continue with the usual update logic
-        if @form.update(form_params)
-          redirect_to next_path
-        else
-          render :edit
-        end
-      end
     end
   end
 end
