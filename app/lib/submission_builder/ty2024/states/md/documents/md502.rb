cd0e5fb71db692f114e6709fb9366053b0fb99dc
Em Barnard-Shao <ebarnard@codeforamerica.org>
2024-10-18 12:48:39 -0400

    reorder xml

--- app/lib/submission_builder/ty2024/states/md/documents/md502.rb
+++ app/lib/submission_builder/ty2024/states/md/documents/md502.rb
@@ -34,14 +34,11 @@ module SubmissionBuilder
                   end
                 end
                 income_section(xml)
-                xml.DaytimePhoneNumber @submission.data_source.direct_file_data.phone_number if @submission.data_source.direct_file_data.phone_number.present?
-                if @submission.data_source.filing_status_mfs?
-                  xml.MFSSpouseSSN @submission.data_source.direct_file_data.spouse_ssn
-                end
                 xml.Subtractions do
                   xml.ChildAndDependentCareExpenses @submission.data_source.direct_file_data.dependent_care_expenses
                   xml.SocialSecurityRailRoadBenefits  @submission.data_source.direct_file_data.fed_taxable_ssb
                 end
+                xml.DaytimePhoneNumber @submission.data_source.direct_file_data.phone_number if @submission.data_source.direct_file_data.phone_number.present?
               end
             end
 
