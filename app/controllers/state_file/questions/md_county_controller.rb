e116d863b999dc2caa884f2ee71919356ee122e3
Tahsina Islam <tislam@codeforamerica.org>
2024-10-18 12:51:09 -0500

    fix controller to pass in data to update as well

--- app/controllers/state_file/questions/md_county_controller.rb
+++ app/controllers/state_file/questions/md_county_controller.rb
@@ -2,12 +2,12 @@ module StateFile
   module Questions
     class MdCountyController < QuestionsController
       include ReturnToReviewConcern
+      before_action :set_ivars, only: [:edit, :update]
 
-      def edit
+      def set_ivars
         @filing_year = Rails.configuration.statefile_current_tax_year
         @counties = current_intake.counties_for_select
         @subdivisions_by_county = current_intake.counties_and_subdivisions_array
-        super
       end
     end
   end
