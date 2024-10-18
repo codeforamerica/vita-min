419123c59c81e7c3cc1ee6591cdd72f9efdbf00c
Afia Kyalo <akyalo@codeforamerica.org>
2024-10-18 17:08:11 -0400

    format

--- app/forms/state_file/md_primary_state_id_form.rb
+++ app/forms/state_file/md_primary_state_id_form.rb
@@ -1,19 +1,19 @@
 module StateFile
-    class MdPrimaryStateIdForm < StateIdForm
-      set_attributes_for :state_id, :id_type, :id_number, :state, :non_expiring
-  
-      set_attributes_for :dates,
-                         :issue_date_day,
-                         :issue_date_month,
-                         :issue_date_year,
-                         :expiration_date_day,
-                         :expiration_date_month,
-                         :expiration_date_year
-  
-      private
-  
-      def self.record_type
-        :primary_state_id
-      end
-    end
-  end
+	class MdPrimaryStateIdForm < StateIdForm
+		set_attributes_for :state_id, :id_type, :id_number, :state, :non_expiring
+
+    set_attributes_for :dates,
+                       :issue_date_day,
+                       :issue_date_month,
+                       :issue_date_year,
+                       :expiration_date_day,
+                       :expiration_date_month,
+                       :expiration_date_year
+
+		private
+
+		def self.record_type
+			:primary_state_id
+		end
+	end
+end
