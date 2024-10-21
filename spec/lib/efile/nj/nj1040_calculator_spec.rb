e22d7c9a006a91e6a896dc970d84a909a232715d
Jenny Heath <jheath@codeforamerica.org>
2024-10-21 13:58:58 -0700

    wip start to refactor NJ test
    Co-authored-by: Anisha Ramnani <aramnani@codeforamerica.org>

--- spec/lib/efile/nj/nj1040_calculator_spec.rb
+++ spec/lib/efile/nj/nj1040_calculator_spec.rb
@@ -336,20 +336,22 @@ describe Efile::Nj::Nj1040Calculator do
   end
 
   describe 'line 27 - total income' do
-    let(:intake) { create(:state_file_nj_intake, :df_data_2_w2s) }
+    let(:intake) { create(:state_file_nj_intake) }
+
     it 'sets line 27 to the sum of all state wage amounts' do
-      line_15_w2_wages = 12345 + 50000
-      expect(instance.lines[:NJ1040_LINE_15].value).to eq(line_15_w2_wages)
-      expect(instance.lines[:NJ1040_LINE_27].value).to eq(line_15_w2_wages)
+      allow(instance).to receive(:calculate_line_15).and_return 50000
+      instance.calculate
+      expect(instance.lines[:NJ1040_LINE_27].value).to eq(50000)
     end
   end
 
   describe 'line 29 - gross income' do
-    let(:intake) { create(:state_file_nj_intake, :df_data_2_w2s) }
+    let(:intake) { create(:state_file_nj_intake) }
+
     it 'sets line 29 to the sum of all state wage amounts' do
-      line_15_w2_wages = 12345 + 50000
-      expect(instance.lines[:NJ1040_LINE_15].value).to eq(line_15_w2_wages)
-      expect(instance.lines[:NJ1040_LINE_29].value).to eq(line_15_w2_wages)
+      allow(instance).to receive(:calculate_line_15).and_return 50000
+      instance.calculate
+      expect(instance.lines[:NJ1040_LINE_29].value).to eq(50000)
     end
   end
 
