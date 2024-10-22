c5ceb82a3cde9ed383e33d05084d13f09ca9df01
Tahsina Islam <tislam@codeforamerica.org>
2024-10-22 16:45:05 -0500

    fix e-declaration form

--- app/forms/state_file/esign_declaration_form.rb
+++ app/forms/state_file/esign_declaration_form.rb
@@ -54,7 +54,7 @@ module StateFile
     end
 
     def intake_already_submitted?
-      if Rails.env.development? || Rails.env.heroku?
+      if Rails.env.development? || Rails.env.heroku? || Rails.env.demo?
         return false
       end
       if Flipper.enabled?(:prevent_duplicate_accepted_statefile_submissions)
