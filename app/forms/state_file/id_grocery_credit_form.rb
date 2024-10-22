036e43db6be501c8d5e059cf2568291004b4f632
Anisha Ramnani <aramnani@codeforamerica.org>
2024-10-22 16:58:04 -0400

    adds primary and spouse to IdGroceryCreditForm
    Co-authored-by: Hugo Melo <hmelo@codeforamerica.org>

--- app/forms/state_file/id_grocery_credit_form.rb
+++ app/forms/state_file/id_grocery_credit_form.rb
@@ -1,5 +1,11 @@
 module StateFile
   class IdGroceryCreditForm < QuestionsForm
+    attr_reader :intake
+    set_attributes_for :intake,:primary_months_eligible_for_grocery_credit, :spouse_months_eligible_for_grocery_credit
 
+    def save
+      attributes_to_save = attributes_for(:intake).delete_if { |k, v| v.nil? }
+      @intake.update(attributes_to_save)
+    end
   end
 end
