0da1f60e7ee84ba59f8cd16a2ca05cb1bb8f9378
Kevin Diale <powersurge360@gmail.com>
2024-10-22 11:23:18 -0400

    Added PDF support

--- app/lib/pdf_filler/id_39r_pdf.rb
+++ app/lib/pdf_filler/id_39r_pdf.rb
@@ -1,10 +1,19 @@
 module PdfFiller
   class Id39rPdf
     include PdfHelper
-    def source_pdf_name = "idform394-TY2023.pdf"
+    def source_pdf_name = "idform39r-TY2023"
 
     def initialize(submission)
       @submission = submission
+
+      builder = StateFile::StateInformationService.submission_builder_class(:id)
+      @xml_document = builder.new(submission).document
+    end
+
+    def hash_for_pdf
+      {
+        "BL6" => @xml_document.at('Form39R ChildCareCreditAmt')&.text
+      }
     end
   end
 end
