9eec5aa2a2b988beb9421ca135b1cbfe7d51426c
Melanie <melanie.mazanec@gmail.com>
2024-10-23 13:59:48 -0400

    Remove confusing conditional in medical expenses form

--- app/forms/state_file/nj_medical_expenses_form.rb
+++ app/forms/state_file/nj_medical_expenses_form.rb
@@ -3,8 +3,8 @@ module StateFile
     set_attributes_for :intake,
                        :medical_expenses
 
-    validates_numericality_of :medical_expenses, only_integer: true, message: :round_to_whole_number, if: -> { medical_expenses.present? }
-    validates :medical_expenses, presence: true, allow_blank: false, numericality: { greater_than_or_equal_to: 0 }, if: -> { medical_expenses.present? }
+    validates_numericality_of :medical_expenses, only_integer: true, message: :round_to_whole_number
+    validates :medical_expenses, presence: true, allow_blank: false, numericality: { greater_than_or_equal_to: 0 }
                    
     def save
       unless medical_expenses.nil?
