610fae24c1fe8a566f9fb332f02505dbcbb9b3b6
Anisha Ramnani <aramnani@codeforamerica.org>
2024-10-23 15:08:12 -0400

    small cleanup to form
    Co-authored-by: Hugo Melo <hmelo@codeforamerica.org>

--- app/forms/state_file/id_grocery_credit_form.rb
+++ app/forms/state_file/id_grocery_credit_form.rb
@@ -1,10 +1,9 @@
 module StateFile
   class IdGroceryCreditForm < QuestionsForm
-    attr_reader :intake
     set_attributes_for :intake,:primary_months_eligible_for_grocery_credit, :spouse_months_eligible_for_grocery_credit
 
     def save
-      attributes_to_save = attributes_for(:intake).delete_if { |k, v| v.nil? }
+      attributes_to_save = attributes_for(:intake).compact
       @intake.update(attributes_to_save)
     end
   end
