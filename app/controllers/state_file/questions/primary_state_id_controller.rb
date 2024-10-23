a54801003dd84b810d64b02defb1ca7cb00f7476
Tahsina Islam <tislam@codeforamerica.org>
2024-10-23 18:41:14 -0500

    change primary id controller

--- app/controllers/state_file/questions/primary_state_id_controller.rb
+++ app/controllers/state_file/questions/primary_state_id_controller.rb
@@ -2,6 +2,11 @@ module StateFile
   module Questions
     class PrimaryStateIdController < QuestionsController
       include ReturnToReviewConcern
+
+      def edit
+        super
+        render "state_file/questions/#{current_state_code}_primary_state_id/_#{current_state_code}_primary"
+      end
     end
   end
 end
