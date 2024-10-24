fcfd01b5e7d5d38c41a2614ef19d916ed1d6690b
Anne LoVerso <anne.loverso@gmail.com>
2024-10-24 11:27:46 -0400

    NJ 129 - refactor direct string comparisons to use convenience methods

--- app/helpers/state_file/nj_tenant_eligibility_helper.rb
+++ app/helpers/state_file/nj_tenant_eligibility_helper.rb
@@ -6,8 +6,8 @@ module StateFile
 
     class << self
       def determine_eligibility(intake)
-        is_ineligible = intake.tenant_home_subject_to_property_taxes == 'no' || (intake.tenant_building_multi_unit == 'yes' && intake.tenant_access_kitchen_bath == 'no')
-        is_unsupported = intake.tenant_more_than_one_main_home_in_nj == 'yes' || intake.tenant_shared_rent_not_spouse == 'yes'
+        is_ineligible = intake.tenant_home_subject_to_property_taxes_no? || (intake.tenant_building_multi_unit_yes? && intake.tenant_access_kitchen_bath_no?)
+        is_unsupported = intake.tenant_more_than_one_main_home_in_nj_yes? || intake.tenant_shared_rent_not_spouse_yes?
 
         if is_ineligible
           INELIGIBLE
