ea134694a0f1f5ef171f655eab1b407724617f66
Melanie <melanie.mazanec@gmail.com>
2024-10-24 11:09:03 -0400

    Update axe gems and start to set up nj test for axe

---
+++ spec/features/state_file/nj_spec.rb
@@ -0,0 +1,41 @@
+require "rails_helper"
+
+RSpec.feature "NJ minimal", active_job: true do
+  include MockTwilio
+  include StateFileIntakeHelper
+
+  before do
+    allow_any_instance_of(Routes::StateFileDomain).to receive(:matches?).and_return(true)
+    Flipper.enable :sms_notifications
+  end
+
+  it "advances past the loading screen by listening for an actioncable broadcast", js: true do
+    visit "/"
+    click_on "Start Test NJ"
+
+    expect(page).to have_text "File your New Jersey taxes for free"
+    click_on "Get Started", id: "firstCta"
+
+    # step_through_eligibility_screener(us_state: "nj") to do after eligibility
+    click_on "Continue"
+
+    step_through_initial_authentication(contact_preference: :email)
+
+    expect(page).to have_text I18n.t('state_file.questions.terms_and_conditions.edit.title')
+    click_on I18n.t("state_file.questions.terms_and_conditions.edit.accept")
+
+    step_through_df_data_transfer("Transfer Minimal")
+
+    expect(page).to have_text "Your federal tax return is now transferred."
+    click_on "Continue"
+
+    # County
+    # Municipality
+    # Medical expenses
+    # Homeowner tenant
+    # Disabled
+    # Review
+    # Esign
+    # Confirmation
+  end
+end
