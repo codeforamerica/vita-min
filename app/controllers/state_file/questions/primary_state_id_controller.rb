21f91297ee9ce492682ae4b7f0cb9126d8d866de
Tahsina Islam <tislam@codeforamerica.org>
2024-10-24 16:33:09 -0500

    make render method

--- app/controllers/state_file/questions/primary_state_id_controller.rb
+++ app/controllers/state_file/questions/primary_state_id_controller.rb
@@ -5,7 +5,7 @@ module StateFile
 
       def edit
         super
-        render "state_file/questions/#{current_state_code}_primary_state_id/_#{current_state_code}_primary"
+        render state_specific_view
       end
 
       def update
@@ -18,8 +18,14 @@ module StateFile
         else
           after_update_failure
           track_validation_error
-          render "state_file/questions/#{current_state_code}_primary_state_id/_#{current_state_code}_primary"
+          render state_specific_view
         end
+      end
+
+      private
+
+      def state_specific_view
+        "state_file/questions/#{current_state_code}_primary_state_id/_#{current_state_code}_primary"
       end
     end
   end
