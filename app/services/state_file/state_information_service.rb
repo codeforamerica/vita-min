100385f3418e59f2f2069ef49c41634c749ae01c
Tahsina Islam <tislam@codeforamerica.org>
2024-10-29 17:01:04 -0500

    FYST-909 Update W2 UI

--- app/services/state_file/state_information_service.rb
+++ app/services/state_file/state_information_service.rb
@@ -20,6 +20,8 @@ module StateFile
       :vita_link,
       :voucher_form_name,
       :voucher_path,
+      :w2_supported_box_14_codes,
+      :w2_include_local_income_boxes,
     ].freeze
 
     class << self
@@ -74,6 +76,8 @@ module StateFile
         vita_link: "https://airtable.com/appnKuyQXMMCPSvVw/pag0hcyC6juDxamHo/form",
         voucher_form_name: "Form AZ-140V",
         voucher_path: "/pdfs/AZ-140V.pdf",
+        w2_supported_box_14_codes: [],
+        w2_include_local_income_boxes: false
       },
       id: {
         intake_class: StateFileIdIntake,
@@ -94,6 +98,8 @@ module StateFile
         vita_link: "",
         voucher_form_name: "",
         voucher_path: "",
+        w2_supported_box_14_codes: [],
+        w2_include_local_income_boxes: false
       },
       md: {
         intake_class: StateFileMdIntake,
@@ -114,6 +120,8 @@ module StateFile
         vita_link: "",
         voucher_form_name: "",
         voucher_path: "",
+        w2_supported_box_14_codes: ["STPICKUP"],
+        w2_include_local_income_boxes: true
       },
       nc: {
         intake_class: StateFileNcIntake,
@@ -136,6 +144,8 @@ module StateFile
         vita_link: "",
         voucher_form_name: "Form D-400V",
         voucher_path: "/pdfs/d400v-TY2023.pdf",
+        w2_supported_box_14_codes: [],
+        w2_include_local_income_boxes: false
       },
       nj: {
         intake_class: StateFileNjIntake,
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_10_21_190718) do
+ActiveRecord::Schema[7.1].define(version: 2024_10_28_204525) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
   enable_extension "plpgsql"
@@ -2250,6 +2250,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_10_21_190718) do
   end
 
   create_table "state_file_w2s", force: :cascade do |t|
+    t.decimal "box_14_stpickup", precision: 12, scale: 2
     t.datetime "created_at", null: false
     t.string "employee_name", null: false
     t.string "employee_ssn"
