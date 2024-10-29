4ead478de841aa2e5fcc1d20b4371b6ac11d39be
jenny-heath <43800769+jenny-heath@users.noreply.github.com>
2024-10-28 17:07:26 -0700

    Remove old add_columns_to_state_file_w2, replace with one that doesn't have not-null constraint (#4922)
    Co-authored-by: Mike Rotondo <mrotondo@codeforamerica.org>

--- db/migrate/20241017215227_add_columns_to_state_file_w2.rb
+++
@@ -1,6 +0,0 @@
-class AddColumnsToStateFileW2 < ActiveRecord::Migration[7.1]
-  def change
-    add_column :state_file_w2s, :employer_name, :string, null: false
-    add_column :state_file_w2s, :employee_name, :string, null: false
-  end
-end
