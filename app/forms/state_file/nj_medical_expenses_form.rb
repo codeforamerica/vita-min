06c4dd99ecdf11dcd3a5a32931264565002a088a
Anne LoVerso <anne.loverso@gmail.com>
2024-10-31 11:52:31 -0400

    update validations

--- app/forms/state_file/nj_medical_expenses_form.rb
+++ app/forms/state_file/nj_medical_expenses_form.rb
@@ -3,7 +3,7 @@ module StateFile
     set_attributes_for :intake,
                        :medical_expenses
 
-    validates_numericality_of :medical_expenses, message: I18n.t("validators.not_a_number")
+    validates_numericality_of :medical_expenses, message: I18n.t("validators.not_a_number"), if: -> { medical_expenses.present? }
     validates :medical_expenses, presence: true, allow_blank: false, numericality: { greater_than_or_equal_to: 0 }
                    
     def save
