3f9fa177d05e4efc88919e744d8e2014923da42f
Arin Choi <achoi@codeforamerica.org>
2024-11-07 15:18:25 -0800

    Fill in Interest Income From Obligations of the United States or United States’ Possessions

--- spec/lib/submission_builder/ty2024/states/nc/documents/d400_schedule_s_spec.rb
+++ spec/lib/submission_builder/ty2024/states/nc/documents/d400_schedule_s_spec.rb
@@ -7,7 +7,17 @@ describe SubmissionBuilder::Ty2024::States::Nc::Documents::D400ScheduleS, requir
     let(:build_response) { described_class.build(submission, validate: false) }
     let(:xml) { Nokogiri::XML::Document.parse(build_response.document.to_xml) }
 
-   context "return contain tribal wages amounts " do
+    context "USInterestInc" do
+      before do
+        intake.direct_file_data.fed_taxable_income = 323
+      end
+
+      it "correctly fills answers" do
+        expect(xml.document.at('DedFedAGI USInterestInc').text).to eq "323"
+      end
+    end
+
+    context "return contain tribal wages amounts " do
       before do
         intake.tribal_member = "yes"
         intake.tribal_wages_amount = 100.00
