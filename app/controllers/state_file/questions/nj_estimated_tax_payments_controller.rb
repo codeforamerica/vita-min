591c31b0408f9077760230f927d4f6b2a96a8329
Anne LoVerso <anne.loverso@gmail.com>
2024-11-07 11:31:37 -0500

    add year interpolated variables

--- app/controllers/state_file/questions/nj_estimated_tax_payments_controller.rb
+++ app/controllers/state_file/questions/nj_estimated_tax_payments_controller.rb
@@ -2,6 +2,9 @@ module StateFile
   module Questions
     class NjEstimatedTaxPaymentsController < QuestionsController
       include ReturnToReviewConcern
+
+      before_action -> { @filing_year = Rails.configuration.statefile_current_tax_year }
+      before_action -> { @prior_year = Rails.configuration.statefile_current_tax_year - 1 }
     end
   end
 end
