a281e87fb86cda8e5d6640724b7c124f9067c4e2
Hugo Melo <hmelo@codeforamerica.org>
2024-11-08 11:28:31 -0500

    Add two income sub form specs, fix validation converstion
    Co-authored-by: Mike Rotondo <mrotondo@codeforamerica.org>

--- app/forms/state_file/md_two_income_subtractions_form.rb
+++ app/forms/state_file/md_two_income_subtractions_form.rb
@@ -12,7 +12,7 @@ module StateFile
 
     def valid_amounts
       if @intake.direct_file_data.fed_student_loan_interest.present?
-        if (primary_student_loan_interest_ded_amount.to_i + spouse_student_loan_interest_ded_amount.to_i) != @intake.direct_file_data.fed_student_loan_interest.to_i
+        if (primary_student_loan_interest_ded_amount.to_f + spouse_student_loan_interest_ded_amount.to_f).to_i != @intake.direct_file_data.fed_student_loan_interest.to_i
           errors.add(
             :primary_student_loan_interest_ded_amount,
             I18n.t("state_file.questions.md_two_income_subtractions.edit.sum_form_error", total_deduction: @intake.direct_file_data.fed_student_loan_interest)
