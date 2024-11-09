d6e98d95536b268bab3504b82d2747ed0699abf3
Martha Pidcock <mpidcock@codeforamerica.org>
2024-11-08 17:24:40 -0800

    Remove unneeded changes to MD supported documents

--- app/lib/submission_builder/ty2024/states/md/md_return_xml.rb
+++ app/lib/submission_builder/ty2024/states/md/md_return_xml.rb
@@ -37,39 +37,40 @@ module SubmissionBuilder
             has_income_from_taxable_pensions_iras_annuities = calculated_fields.fetch(:MD502_LINE_1D)&.to_i.positive?
             has_md_su_subtractions = calculated_fields.fetch(:MD502_SU_LINE_1).positive?
 
-            supported_docs = []
-            supported_docs << {
-              xml: SubmissionBuilder::Ty2024::States::Md::Documents::Md502,
-              pdf: PdfFiller::Md502Pdf,
-              include: true
-            }
-            supported_docs << {
-              xml: SubmissionBuilder::Ty2024::States::Md::Documents::Md502b,
-              pdf: PdfFiller::Md502bPdf,
-              include: @submission.data_source.dependents.count.positive?
-            }
-            supported_docs << {
-              xml: SubmissionBuilder::Ty2024::States::Md::Documents::Md502Su,
-              pdf: PdfFiller::Md502SuPdf,
-              include: has_md_su_subtractions,
-            }
-            supported_docs << {
-              xml: SubmissionBuilder::Ty2024::States::Md::Documents::Md502R,
-              pdf: PdfFiller::Md502RPdf,
-              include: has_income_from_taxable_pensions_iras_annuities
-            }
-            supported_docs << {
-              xml: SubmissionBuilder::Ty2024::States::Md::Documents::Md502Cr,
-              pdf: PdfFiller::Md502CrPdf,
-              include: true,
-            }
-            supported_docs += form1099gs
-            supported_docs << {
-              xml: nil,
-              pdf: PdfFiller::MdEl101Pdf,
-              include: true
-            }
+            supported_docs = [
+              {
+                xml: SubmissionBuilder::Ty2024::States::Md::Documents::Md502,
+                pdf: PdfFiller::Md502Pdf,
+                include: true
+              },
+              {
+                xml: SubmissionBuilder::Ty2024::States::Md::Documents::Md502b,
+                pdf: PdfFiller::Md502bPdf,
+                include: @submission.data_source.dependents.count.positive?
+              },
+              {
+                xml: SubmissionBuilder::Ty2024::States::Md::Documents::Md502Su,
+                pdf: PdfFiller::Md502SuPdf,
+                include: has_md_su_subtractions,
+              },
+              {
+                xml: SubmissionBuilder::Ty2024::States::Md::Documents::Md502Cr,
+                pdf: PdfFiller::Md502CrPdf,
+                include: true,
+              },
+              {
+                xml: SubmissionBuilder::Ty2024::States::Md::Documents::Md502R,
+                pdf: PdfFiller::Md502RPdf,
+                include: has_income_from_taxable_pensions_iras_annuities
+              },
+              {
+                xml: nil,
+                pdf: PdfFiller::MdEl101Pdf,
+                include: true
+              },
+            ]
 
+            supported_docs += form1099gs # must be sequenced here
             supported_docs += combined_w2s
             supported_docs += form1099rs
             supported_docs += form1099ints
