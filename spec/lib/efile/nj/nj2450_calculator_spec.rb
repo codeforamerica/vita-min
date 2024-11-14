87b4fe9e91b08fd3014c98b8d690537db44b12be
Melanie <melanie.mazanec@gmail.com>
2024-11-14 16:35:10 -0500

    Add w2 fields in 2450 pdf
    Add pdf fields for totals

--- spec/lib/efile/nj/nj2450_calculator_spec.rb
+++ spec/lib/efile/nj/nj2450_calculator_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 describe Efile::Nj::Nj2450Calculator do
   let(:intake) { create(:state_file_nj_intake, :df_data_mfj) }
   let(:primary_ssn_from_fixture) { intake.primary.ssn }
+  let(:spouse_ssn_from_fixture) { intake.spouse.ssn }
   let(:nj1040_calculator) do
     Efile::Nj::Nj1040Calculator.new(
       year: MultiTenantService.statefile.current_tax_year,
@@ -15,44 +16,92 @@ describe Efile::Nj::Nj2450Calculator do
     instance.calculate
   end
 
-  context "columnn a" do
-    context "multiple w2s that individually do not exceed #{Efile::Nj::Nj1040Calculator::EXCESS_UI_WF_SWF_MAX} and total more than #{Efile::Nj::Nj1040Calculator::EXCESS_UI_WF_SWF_MAX}" do 
-      let!(:w2_1) { create(:state_file_w2, state_file_intake: intake, employee_ssn: primary_ssn_from_fixture, box14_ui_hc_wd: 100) }
-      let!(:w2_2) { create(:state_file_w2, state_file_intake: intake, employee_ssn: primary_ssn_from_fixture, box14_ui_hc_wd: 134) }
+  context "primary" do
+    context "columnn a" do
+      context "multiple w2s that individually do not exceed #{Efile::Nj::Nj1040Calculator::EXCESS_UI_WF_SWF_MAX} and total more than #{Efile::Nj::Nj1040Calculator::EXCESS_UI_WF_SWF_MAX}" do 
+        let!(:w2_1) { create(:state_file_w2, state_file_intake: intake, employee_ssn: primary_ssn_from_fixture, box14_ui_hc_wd: 100) }
+        let!(:w2_2) { create(:state_file_w2, state_file_intake: intake, employee_ssn: primary_ssn_from_fixture, box14_ui_hc_wd: 134) }
 
-      before do
-        instance.calculate
+        before do
+          instance.calculate
+        end
+
+        it "sums ui/wf/swf and ui/hc/wd" do
+          expected_sum = 234
+          expect(instance.lines[:NJ2450_COLUMN_A_TOTAL_PRIMARY].value).to eq(expected_sum)
+        end
+
+        it "subtracts max contribution amount" do
+          expected_difference = 54
+          expect(instance.lines[:NJ2450_COLUMN_A_EXCESS_PRIMARY].value).to eq(expected_difference)
+        end
       end
+    end
 
-      it "sums ui/wf/swf and ui/hc/wd" do
-        expected_sum = 234
-        expect(instance.lines[:NJ2450_COLUMN_A_TOTAL_PRIMARY].value).to eq(expected_sum)
-      end
+    context "column c" do
+      context "multiple w2s that individually do not exceed #{Efile::Nj::Nj1040Calculator::EXCESS_FLI_MAX} and total more than #{Efile::Nj::Nj1040Calculator::EXCESS_FLI_MAX}" do 
+        let!(:w2_1) { create(:state_file_w2, state_file_intake: intake, employee_ssn: primary_ssn_from_fixture, box14_fli: 100) }
+        let!(:w2_2) { create(:state_file_w2, state_file_intake: intake, employee_ssn: primary_ssn_from_fixture, box14_fli: 134) }
 
-      it "subtracts max contribution amount" do
-        expected_difference = 54
-        expect(instance.lines[:NJ2450_COLUMN_A_EXCESS_PRIMARY].value).to eq(expected_difference)
+        before do
+          instance.calculate
+        end
+
+        it "sums fli" do
+          expected_sum = 234
+          expect(instance.lines[:NJ2450_COLUMN_C_TOTAL_PRIMARY].value).to eq(expected_sum)
+        end
+
+        it "subtracts max contribution amount" do
+          expected_difference = 89
+          expect(instance.lines[:NJ2450_COLUMN_C_EXCESS_PRIMARY].value).to eq(expected_difference)
+        end
       end
     end
   end
 
-  context "column c" do
-    context "multiple w2s that individually do not exceed #{Efile::Nj::Nj1040Calculator::EXCESS_FLI_MAX} and total more than #{Efile::Nj::Nj1040Calculator::EXCESS_FLI_MAX}" do 
-      let!(:w2_1) { create(:state_file_w2, state_file_intake: intake, employee_ssn: primary_ssn_from_fixture, box14_fli: 100) }
-      let!(:w2_2) { create(:state_file_w2, state_file_intake: intake, employee_ssn: primary_ssn_from_fixture, box14_fli: 134) }
 
-      before do
-        instance.calculate
+  context "mfj spouse" do
+    let(:instance) { nj1040_calculator.instance_variable_get(:@nj2450_spouse) }
+    context "columnn a" do
+      context "multiple w2s that individually do not exceed #{Efile::Nj::Nj1040Calculator::EXCESS_UI_WF_SWF_MAX} and total more than #{Efile::Nj::Nj1040Calculator::EXCESS_UI_WF_SWF_MAX}" do 
+        let!(:w2_1) { create(:state_file_w2, state_file_intake: intake, employee_ssn: spouse_ssn_from_fixture, box14_ui_hc_wd: 100) }
+        let!(:w2_2) { create(:state_file_w2, state_file_intake: intake, employee_ssn: spouse_ssn_from_fixture, box14_ui_hc_wd: 134) }
+
+        before do
+          instance.calculate
+        end
+
+        it "sums ui/wf/swf and ui/hc/wd" do
+          expected_sum = 234
+          expect(instance.lines[:NJ2450_COLUMN_A_TOTAL_SPOUSE].value).to eq(expected_sum)
+        end
+
+        it "subtracts max contribution amount" do
+          expected_difference = 54
+          expect(instance.lines[:NJ2450_COLUMN_A_EXCESS_SPOUSE].value).to eq(expected_difference)
+        end
       end
+    end
 
-      it "sums fli" do
-        expected_sum = 234
-        expect(instance.lines[:NJ2450_COLUMN_C_TOTAL_PRIMARY].value).to eq(expected_sum)
-      end
+    context "column c" do
+      context "multiple w2s that individually do not exceed #{Efile::Nj::Nj1040Calculator::EXCESS_FLI_MAX} and total more than #{Efile::Nj::Nj1040Calculator::EXCESS_FLI_MAX}" do 
+        let!(:w2_1) { create(:state_file_w2, state_file_intake: intake, employee_ssn: spouse_ssn_from_fixture, box14_fli: 100) }
+        let!(:w2_2) { create(:state_file_w2, state_file_intake: intake, employee_ssn: spouse_ssn_from_fixture, box14_fli: 134) }
 
-      it "subtracts max contribution amount" do
-        expected_difference = 89
-        expect(instance.lines[:NJ2450_COLUMN_C_EXCESS_PRIMARY].value).to eq(expected_difference)
+        before do
+          instance.calculate
+        end
+
+        it "sums fli" do
+          expected_sum = 234
+          expect(instance.lines[:NJ2450_COLUMN_C_TOTAL_SPOUSE].value).to eq(expected_sum)
+        end
+
+        it "subtracts max contribution amount" do
+          expected_difference = 89
+          expect(instance.lines[:NJ2450_COLUMN_C_EXCESS_SPOUSE].value).to eq(expected_difference)
+        end
       end
     end
   end
