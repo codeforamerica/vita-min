64dc0a8e68bb4acd9327a58ffb39b12cec36e7e2
Mike Rotondo <mrotondo@codeforamerica.org>
2024-11-15 13:55:12 -0800

    create jsons for returns used by az and ny persona tests, and remove name/dob/relationship specifications from those tests

--- app/models/state_file_base_intake.rb
+++ app/models/state_file_base_intake.rb
@@ -66,6 +66,7 @@ class StateFileBaseIntake < ApplicationRecord
         primary_first_name: direct_file_json_data.primary_filer.first_name,
         primary_middle_initial: direct_file_json_data.primary_filer.middle_initial,
         primary_last_name: direct_file_json_data.primary_filer.last_name,
+        primary_suffix: direct_file_json_data.primary_filer.suffix,
         primary_birth_date: direct_file_json_data.primary_filer.dob
       )
     end
@@ -75,6 +76,7 @@ class StateFileBaseIntake < ApplicationRecord
         spouse_first_name: direct_file_json_data.spouse_filer.first_name,
         spouse_middle_initial: direct_file_json_data.spouse_filer.middle_initial,
         spouse_last_name: direct_file_json_data.spouse_filer.last_name,
+        spouse_suffix: direct_file_json_data.spouse_filer.suffix,
         spouse_birth_date: direct_file_json_data.spouse_filer.dob
       )
     end
