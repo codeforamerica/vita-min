139eef3273b14f7286584c40edee4a8de5aad9c8
Tahsina Islam <tislam@codeforamerica.org>
2024-11-15 12:39:22 -0600

    fix test

--- spec/lib/submission_builder/ty2024/states/md/documents/md502_spec.rb
+++ spec/lib/submission_builder/ty2024/states/md/documents/md502_spec.rb
@@ -361,9 +361,9 @@ describe SubmissionBuilder::Ty2024::States::Md::Documents::Md502, required_schem
           expect(xml.at("Form502 ExemptionAmount")).to be_nil
           expect(xml.at("Form502 StateTaxComputation TaxableNetIncome")).to be_nil
           expect(xml.at("Form502 StateTaxComputation StateIncomeTax")).to be_nil
-          expect(xml.at("Form502 StateTaxComputation PovertyLevelCredit").text).to be_nil
-          expect(xml.at("Form502 StateTaxComputation TotalCredits").text).to be_nil
-          expect(xml.at("Form502 StateTaxComputation StateTaxAfterCredits").text).to be_nil
+          expect(xml.at("Form502 StateTaxComputation PovertyLevelCredit")).to be_nil
+          expect(xml.at("Form502 StateTaxComputation TotalCredits")).to be_nil
+          expect(xml.at("Form502 StateTaxComputation StateTaxAfterCredits")).to be_nil
         end
       end
 
