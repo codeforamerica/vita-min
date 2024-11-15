aa974becb8653ab12131a122fecc5477389d845a
Tahsina Islam <79296603+tahsinaislam@users.noreply.github.com>
2024-11-15 13:32:47 -0600

    Fyst 659 implement form 502 xml pdf poverty level credit (#4984)

--- app/lib/submission_builder/ty2024/states/md/documents/md502.rb
+++ app/lib/submission_builder/ty2024/states/md/documents/md502.rb
@@ -47,13 +47,13 @@ class SubmissionBuilder::Ty2024::States::Md::Documents::Md502 < SubmissionBuilde
       xml.MarylandAddress do
         if @intake.confirmed_permanent_address_yes?
           xml.AddressLine1Txt sanitize_for_xml(@intake.direct_file_data.mailing_street, 35)
-          xml.AddressLine2Txt sanitize_for_xml(@intake.direct_file_data.mailing_apartment, 35)
+          xml.AddressLine2Txt sanitize_for_xml(@intake.direct_file_data.mailing_apartment, 35) if @intake.direct_file_data.mailing_apartment.present?
           xml.CityNm sanitize_for_xml(@intake.direct_file_data.mailing_city, 22)
           xml.StateAbbreviationCd @intake.state_code.upcase
           xml.ZIPCd @intake.direct_file_data.mailing_zip
         elsif @intake.confirmed_permanent_address_no?
           xml.AddressLine1Txt sanitize_for_xml(@intake.permanent_street, 35)
-          xml.AddressLine2Txt sanitize_for_xml(@intake.permanent_apartment, 35)
+          xml.AddressLine2Txt sanitize_for_xml(@intake.permanent_apartment, 35) if @intake.direct_file_data.mailing_apartment.present?
           xml.CityNm sanitize_for_xml(@intake.permanent_city, 22)
           xml.StateAbbreviationCd @intake.state_code.upcase
           xml.ZIPCd @intake.permanent_zip
@@ -132,6 +132,9 @@ class SubmissionBuilder::Ty2024::States::Md::Documents::Md502 < SubmissionBuilde
           add_element_if_present(xml, "StateIncomeTax", :MD502_LINE_21)
           add_element_if_present(xml, "EarnedIncomeCredit", :MD502_LINE_22)
           add_element_if_present(xml, "MDEICWithQualChildInd", :MD502_LINE_22B)
+          xml.PovertyLevelCredit calculated_fields.fetch(:MD502_LINE_23) if @deduction_method_is_standard
+          xml.TotalCredits calculated_fields.fetch(:MD502_LINE_26) if @deduction_method_is_standard
+          xml.StateTaxAfterCredits calculated_fields.fetch(:MD502_LINE_27) if @deduction_method_is_standard
         end
       end
       xml.TaxWithheld calculated_fields.fetch(:MD502_LINE_40)
