0df5c45132edd8b5bf5174b9a3e6a8e170d1e788
Drew Proebstel <d.proebstel@gmail.com>
2024-11-18 12:14:19 -0600

    Fyst 1049 update tax logic to use 2024 numbers (#4972)
    * update az-321 line 12
    
    * increase rate to 33
    
    * update line 43
    
    * update az return xml spec
    
    * tests updated
    
    * skip xml validation on complete survey spec
    
    * add missing new line
    
    * formating
    
    * Update persona pdfs
    
    ---------
    
    Co-authored-by: Drew Proebstel <dproebstel@codeforamerica.org>
    Co-authored-by: Arin Choi <achoi@codeforamerica.org>

--- app/lib/efile/az/az140_calculator.rb
+++ app/lib/efile/az/az140_calculator.rb
@@ -173,11 +173,11 @@ module Efile
       def calculate_line_43
         # AZ Standard Deductions for 2023
         if filing_status_single?
-          13_850
+          14_600
         elsif filing_status_mfj?
-          27_700
+          29_200
         elsif filing_status_hoh?
-          20_800
+          21_900
         end
       end
 
@@ -324,7 +324,7 @@ module Efile
       end
 
       def calculate_ccws_line_7c
-        (line_or_zero(:AZ140_CCWS_LINE_6c) * 0.31).round
+        (line_or_zero(:AZ140_CCWS_LINE_6c) * 0.33).round
       end
     end
   end
