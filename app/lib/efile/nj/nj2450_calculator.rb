3b68b5b5d369be35ff96a422e47520360aa980f9
Melanie <melanie.mazanec@gmail.com>
2024-11-19 17:00:39 -0500

    Make w2s a method

--- app/lib/efile/nj/nj2450_calculator.rb
+++ app/lib/efile/nj/nj2450_calculator.rb
@@ -8,7 +8,6 @@ module Efile
         @lines = lines
         @intake = intake
         @primary_or_spouse = primary_or_spouse
-        @w2s = get_persons_w2s(intake, @primary_or_spouse)
       end
 
       def calculate
@@ -22,26 +21,30 @@ module Efile
       
       private
 
+      def w2s
+        get_persons_w2s(@intake, @primary_or_spouse)
+      end
+
       def column_a_total
-        total = @w2s.reduce(0) do |sum, w2|
+        total = w2s.reduce(0) do |sum, w2|
           sum + (w2.box14_ui_hc_wd || 0) + (w2.box14_ui_wf_swf || 0)
         end
         total.round
       end
 
       def column_c_total
-        total = @w2s.reduce(0) { |sum, w2| sum + (w2.box14_fli || 0) }
+        total = w2s.reduce(0) { |sum, w2| sum + (w2.box14_fli || 0) }
         total.round
       end
 
       def column_a_excess
         difference = (column_a_total - Nj1040Calculator::EXCESS_UI_WF_SWF_MAX).round
-        difference.positive? ? difference.round : 0
+        difference.positive? ? difference : 0
       end
 
       def column_c_excess 
         difference = (column_c_total - Nj1040Calculator::EXCESS_FLI_MAX).round
-        difference.positive? ? difference.round : 0
+        difference.positive? ? difference : 0
       end
     end
   end
