941ddc5f384d7478928b25ed9236fa7247a2a461
Hugo Melo <hmelo@codeforamerica.org>
2024-11-19 09:34:14 -0500

    Fix redirect back for review
    Co-authored-by: Mike Rotondo <mrotondo@codeforamerica.org>

--- app/controllers/state_file/questions/id_grocery_credit_controller.rb
+++ app/controllers/state_file/questions/id_grocery_credit_controller.rb
@@ -1,6 +1,8 @@
 module StateFile
   module Questions
     class IdGroceryCreditController < QuestionsController
+      include ReturnToReviewConcern
+
       def self.show?(intake)
         !intake.direct_file_data.claimed_as_dependent?
       end
