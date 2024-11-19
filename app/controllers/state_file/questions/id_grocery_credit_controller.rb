134f85e4357b2bed6769d2893f818d9ffd65528a
Hugo Melo <hmelo@codeforamerica.org>
2024-11-19 16:18:22 -0500

    Fix redirect to go to review first
    Co-authored-by: Mike Rotondo <mrotondo@codeforamerica.org>

--- app/controllers/state_file/questions/id_grocery_credit_controller.rb
+++ app/controllers/state_file/questions/id_grocery_credit_controller.rb
@@ -1,13 +1,22 @@
 module StateFile
   module Questions
     class IdGroceryCreditController < QuestionsController
-      include ReturnToReviewConcern
-
       def self.show?(intake)
         !intake.direct_file_data.claimed_as_dependent?
       end
 
       private
+
+      def next_path
+        prev = super
+        if params[:return_to_review].present?
+          uri = URI.parse(prev)
+          new_params = URI.decode_www_form(uri.query || '') << ['return_to_review', params[:return_to_review]]
+          uri.query = URI.encode_www_form(new_params)
+          prev = uri.to_s
+        end
+        prev
+      end
 
       def form_params
         params
