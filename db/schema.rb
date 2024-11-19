2abb23010741cdbdd8b3a0f26fba08d94deb3026
Anne LoVerso <anne.loverso@gmail.com>
2024-11-19 13:43:26 -0500

    review fixes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_11_18_194009) do
+ActiveRecord::Schema[7.1].define(version: 2024_11_19_183818) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
   enable_extension "plpgsql"
