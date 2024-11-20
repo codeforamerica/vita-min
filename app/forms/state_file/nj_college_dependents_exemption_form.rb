1337fa36874eb13435209066fa4f4dd0654aa50f
Anne LoVerso <anne.loverso@gmail.com>
2024-11-20 16:28:29 -0500

    review comments

--- app/forms/state_file/nj_college_dependents_exemption_form.rb
+++ app/forms/state_file/nj_college_dependents_exemption_form.rb
@@ -3,15 +3,8 @@ module StateFile
     attr_accessor :dependents_attributes
     delegate :dependents, to: :intake
 
-    set_attributes_for(:intake)
-
     def save
-      @intake.update!(attributes_to_save)
-    end
-
-    def attributes_to_save
-      base_attrs = attributes_for(:intake)
-      base_attrs.merge({ dependents_attributes: dependents_attributes.to_h })
+      @intake.update!(dependents_attributes: dependents_attributes.to_h)
     end
   end
 end
