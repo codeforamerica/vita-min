068d2a7c88ad3bd86360b184080d1a90af35b8ef
Drew Proebstel <dproebstel@codeforamerica.org>
2024-11-21 15:36:09 -0800

    acceptance fixes

--- app/controllers/state_file/questions/md_had_health_insurance_controller.rb
+++ app/controllers/state_file/questions/md_had_health_insurance_controller.rb
@@ -2,11 +2,13 @@ module StateFile
   module Questions
     class MdHadHealthInsuranceController < QuestionsController
       def form_params
-        params
+        if params[:state_file_md_had_health_insurance_form]
+          params
           .require(:state_file_md_had_health_insurance_form)
           .permit(
             form_class.attribute_names +
               [{ dependents_attributes: [:id, :md_did_not_have_health_insurance] }])
+        end
       end
     end
   end
