01afd2e77e1ac216c8c2c07b1a1c7de90968e402
Jenny Heath <jheath@codeforamerica.org>
2024-11-22 14:08:11 -0800

    delete more old files

--- db/migrate/20241114212616_add_authorize_sharing_of_health_insurance_info_to_state_file_md_intakes.rb
+++
@@ -1,5 +0,0 @@
-class AddAuthorizeSharingOfHealthInsuranceInfoToStateFileMdIntakes < ActiveRecord::Migration[7.1]
-  def change
-    add_column :state_file_md_intakes, :authorize_sharing_of_health_insurance_info, :integer, null: false, default: 0
-  end
-end
