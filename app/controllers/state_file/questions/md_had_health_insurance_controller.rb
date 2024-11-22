7e0fd76c795ce51bcadb6d625bb2c8b63ab5fd1a
Drew Proebstel <dproebstel@codeforamerica.org>
2024-11-21 17:06:37 -0800

    fix issue missing param issue in controller

--- app/controllers/state_file/questions/md_had_health_insurance_controller.rb
+++ app/controllers/state_file/questions/md_had_health_insurance_controller.rb
@@ -3,8 +3,8 @@ module StateFile
     class MdHadHealthInsuranceController < QuestionsController
       def form_params
         if params[:state_file_md_had_health_insurance_form]
-          params
-          .require(:state_file_md_had_health_insurance_form)
+        params
+          .fetch(:state_file_md_had_health_insurance_form, {})
           .permit(
             form_class.attribute_names +
               [{ dependents_attributes: [:id, :md_did_not_have_health_insurance] }])
