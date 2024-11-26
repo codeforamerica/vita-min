dce5d6b9a049a210aeb48cbe21560a89e2cdbf5a
Hugo Melo <hmelo@codeforamerica.org>
2024-11-26 11:36:47 -0500

    Add specs for form
    Co-authored-by: Mike Rotondo <mrotondo@codeforamerica.org>

--- app/forms/state_file/notification_preferences_form.rb
+++ app/forms/state_file/notification_preferences_form.rb
@@ -2,7 +2,7 @@ module StateFile
   class NotificationPreferencesForm < QuestionsForm
     set_attributes_for :intake, :sms_notification_opt_in, :email_notification_opt_in, :phone_number
 
-    validates :phone_number, allow_blank: true, e164_phone: true, if: -> { attributes_for(:intake)[:sms_notification_opt_in].present? }
+    validates :phone_number, allow_blank: true, e164_phone: true, if: -> { attributes_for(:intake)[:sms_notification_opt_in] == "yes" }
 
     def save
       @intake.update(attributes_for(:intake))
