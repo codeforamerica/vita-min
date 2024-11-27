fd6e3a1e29235014ae8d8e1c3bb5473cdb03d8f7
Arin Choi <achoi@codeforamerica.org>
2024-11-27 13:40:22 -0800

    Drop account_holder_name from StateFileMdIntakes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_11_21_222604) do
+ActiveRecord::Schema[7.1].define(version: 2024_11_27_213848) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
   enable_extension "plpgsql"
@@ -1898,7 +1898,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_21_222604) do
     t.string "account_holder_first_name"
     t.string "account_holder_last_name"
     t.string "account_holder_middle_initial"
-    t.string "account_holder_name"
     t.string "account_holder_suffix"
     t.string "account_number"
     t.integer "account_type", default: 0, null: false
