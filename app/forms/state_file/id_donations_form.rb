69872f23933b291d795685f05d9a40977f0bcb32
Drew Proebstel <dproebstel@codeforamerica.org>
2024-12-02 12:16:29 -0800

    review suggestions

--- app/forms/state_file/id_donations_form.rb
+++ app/forms/state_file/id_donations_form.rb
@@ -12,15 +12,18 @@ module StateFile
       :opportunity_scholarship_program_donation
     )
 
-    validates :nongame_wildlife_fund_donation, numericality: { greater_than_or_equal_to: 0 }, allow_blank: true
-    validates :childrens_trust_fund_donation, numericality: { greater_than_or_equal_to: 0 }, allow_blank: true
-    validates :special_olympics_donation, numericality: { greater_than_or_equal_to: 0 }, allow_blank: true
-    validates :guard_reserve_family_donation, numericality: { greater_than_or_equal_to: 0 }, allow_blank: true
-    validates :american_red_cross_fund_donation, numericality: { greater_than_or_equal_to: 0 }, allow_blank: true
-    validates :veterans_support_fund_donation, numericality: { greater_than_or_equal_to: 0 }, allow_blank: true
-    validates :food_bank_fund_donation, numericality: { greater_than_or_equal_to: 0 }, allow_blank: true
-    validates :opportunity_scholarship_program_donation, numericality: { greater_than_or_equal_to: 0 }, allow_blank: true
+    NON_ZERO_REQUIRED_FIELDS = [
+      :nongame_wildlife_fund_donation,
+      :childrens_trust_fund_donation,
+      :special_olympics_donation,
+      :guard_reserve_family_donation,
+      :american_red_cross_fund_donation,
+      :veterans_support_fund_donation,
+      :food_bank_fund_donation,
+      :opportunity_scholarship_program_donation
+    ]
 
+    validates *NON_ZERO_REQUIRED_FIELDS, numericality: { greater_than_or_equal_to: 0 }, allow_blank: true
 
     def save
       attributes_to_save = attributes_for(:intake)
