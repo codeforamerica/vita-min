9745d8d9ea651a7985cf9afb98e74ed45af6ae07
Hugo Melo <hmelo@codeforamerica.org>
2024-12-02 11:46:12 -0800

    Fix failing specs
    Co-authored-by: Hugo Melo <hmelo@codeforamerica.org>

--- spec/forms/state_file/nc_eligibility_form_spec.rb
+++ spec/forms/state_file/nc_eligibility_form_spec.rb
@@ -3,32 +3,32 @@ require 'rails_helper'
 RSpec.describe StateFile::NcEligibilityForm do
   let(:intake) {
     build :state_file_nc_intake,
-          eligibility_out_of_state_income: "unfilled",
-          eligibility_withdrew_529: "unfilled"
+          eligibility_ed_loan_emp_payment: "unfilled",
+          eligibility_ed_loan_cancelled: "unfilled"
   }
 
   describe "validations" do
     let(:invalid_params) do
       {
-        eligibility_out_of_state_income: nil,
-        eligibility_withdrew_529: nil,
+        eligibility_ed_loan_emp_payment: nil,
+        eligibility_ed_loan_cancelled: nil,
       }
     end
 
-    it "requires both params" do
+    it "does not requires both params" do
       form = described_class.new(intake, invalid_params)
       form.valid?
 
-      expect(form.errors[:eligibility_out_of_state_income]).to include "Can't be blank."
-      expect(form.errors[:eligibility_withdrew_529]).to include "Can't be blank."
+      expect(form.errors[:eligibility_ed_loan_emp_payment]).to be_empty
+      expect(form.errors[:eligibility_ed_loan_cancelled]).to be_empty
     end
   end
 
   describe "#save" do
     let(:valid_params) do
       {
-        eligibility_out_of_state_income: "yes",
-        eligibility_withdrew_529: "no",
+        eligibility_ed_loan_emp_payment: "yes",
+        eligibility_ed_loan_cancelled: "no",
       }
     end
 
@@ -36,8 +36,8 @@ RSpec.describe StateFile::NcEligibilityForm do
       form = described_class.new(intake, valid_params)
       form.save
       intake.reload
-      expect(intake.eligibility_out_of_state_income_yes?).to eq true
-      expect(intake.eligibility_withdrew_529_no?).to eq true
+      expect(intake.eligibility_ed_loan_emp_payment_yes?).to eq true
+      expect(intake.eligibility_ed_loan_cancelled_no?).to eq true
     end
   end
 end
