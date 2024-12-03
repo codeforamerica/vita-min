97afe198271e06e5f047b7a0116e4b43591c397c
Arin Choi <achoi@codeforamerica.org>
2024-12-03 10:59:29 -0800

    Address more spec failures

--- spec/support/helpers/state_file_intake_helper.rb
+++ spec/support/helpers/state_file_intake_helper.rb
@@ -93,8 +93,8 @@ module StateFileIntakeHelper
       click_on "Send code"
 
 
-      expect(page).to have_text I18n.t("state_file.questions.verification_code.edit.title")
-      expect(page).to have_text "We’ve sent your code to (415) 333-4444."
+      expect(strip_html_tags(page.body)).to have_text strip_html_tags(I18n.t("state_file.questions.verification_code.edit.title_html", contact_info: '(415) 333-4444'))
+      expect(page).to have_text "We’ve sent your code to (415) 333-4444"
 
       perform_enqueued_jobs
       sms = FakeTwilioClient.messages.last
