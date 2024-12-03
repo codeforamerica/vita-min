21c1ee7de3bf5fb11d32685280621d742fbb4cb9
Hugo Melo <hmelo@codeforamerica.org>
2024-12-03 10:48:33 -0800

    Remove income form from flow
    Co-authored-by: Hugo Melo <hmelo@codeforamerica.org>

--- app/lib/navigation/state_file_nc_question_navigation.rb
+++ app/lib/navigation/state_file_nc_question_navigation.rb
@@ -34,7 +34,6 @@ module Navigation
         Navigation::NavigationStep.new(StateFile::Questions::NcCountyController),
         Navigation::NavigationStep.new(StateFile::Questions::NcVeteranStatusController),
         Navigation::NavigationStep.new(StateFile::Questions::NcSalesUseTaxController),
-        Navigation::NavigationStep.new(StateFile::Questions::IncomeReviewController),
         Navigation::NavigationStep.new(StateFile::Questions::UnemploymentController),
         Navigation::NavigationStep.new(StateFile::Questions::NcSubtractionsController),
         Navigation::NavigationStep.new(StateFile::Questions::PrimaryStateIdController),
