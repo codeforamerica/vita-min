d5172ba0f85635b7e57f40c909da0b15ba5287d3
Arin Choi <achoi@codeforamerica.org>
2024-12-03 11:59:31 -0800

    Address failures related to period removal from validations and refer to service instead of tool

--- spec/features/state_file/eligibility_spec.rb
+++ spec/features/state_file/eligibility_spec.rb
@@ -15,12 +15,12 @@ RSpec.feature "Going through the eligibility screener" do
       expect(page).to have_text I18n.t("state_file.landing_page.edit.ny.title")
       click_on "Get Started", id: "firstCta"
 
-      expect(page).to have_text "First, let's see if you can use this tool to file your taxes"
+      expect(page).to have_text "First, let's see if you can use this service to file your taxes"
       choose "state_file_ny_eligibility_residence_form_eligibility_lived_in_state_no"
       choose "state_file_ny_eligibility_residence_form_eligibility_yonkers_no"
       click_on "Continue"
 
-      expect(page).to have_text "Unfortunately, you can't use this tool this year. Don't worry, there are other filing options."
+      expect(page).to have_text "Unfortunately, you can't use this service this year. Don't worry, there are other filing options."
     end
 
     it "AZ: shows them the offboarding page if they don't meet the residency requirement" do
@@ -30,12 +30,12 @@ RSpec.feature "Going through the eligibility screener" do
       expect(page).to have_text I18n.t("state_file.landing_page.edit.az.title")
       click_on "Get Started", id: "firstCta"
 
-      expect(page).to have_text "First, let's see if you can use this tool to file your taxes"
+      expect(page).to have_text "First, let's see if you can use this service to file your taxes"
       choose "state_file_az_eligibility_residence_form_eligibility_lived_in_state_no"
       choose "state_file_az_eligibility_residence_form_eligibility_married_filing_separately_no"
       click_on "Continue"
 
-      expect(page).to have_text "Unfortunately, you can't use this tool this year. Don't worry, there are other filing options."
+      expect(page).to have_text "Unfortunately, you can't use this service this year. Don't worry, there are other filing options."
     end
   end
 end
