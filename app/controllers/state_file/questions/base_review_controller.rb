6e0df3701803047e2e99e7af40af2393b64aa60f
Tahsina Islam <79296603+tahsinaislam@users.noreply.github.com>
2024-12-03 10:42:15 -0800

    FYST-1213 ID review items (#5055)
    Co-authored-by: Kevin Diale <kdiale@codeforamerica.org>

--- app/controllers/state_file/questions/base_review_controller.rb
+++ app/controllers/state_file/questions/base_review_controller.rb
@@ -4,9 +4,19 @@ module StateFile
       def edit
         @refund_or_owed_amount = current_intake.calculated_refund_or_owed_amount
         @refund_or_owed_label = @refund_or_owed_amount.positive? ? I18n.t("state_file.questions.shared.review_header.your_refund") : I18n.t("state_file.questions.shared.review_header.your_tax_owed")
+        @income_documents_present = income_documents_present?
       end
 
       private
+
+      def income_documents_present?
+        current_intake.state_file1099_rs.present? ||
+          current_intake.state_file_w2s.present? ||
+          current_intake.direct_file_json_data.interest_reports.count.positive? ||
+          current_intake.direct_file_data.fed_unemployment.positive? ||
+          current_intake.direct_file_data.fed_ssb.positive? || 
+          current_intake.direct_file_data.fed_taxable_ssb.positive?
+      end
 
       def form_class
         NullForm
