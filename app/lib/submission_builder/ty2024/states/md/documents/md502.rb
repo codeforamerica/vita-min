fb2f100eea19de2356aa7c6cf4fc475a4ae34db8
jenny-heath <43800769+jenny-heath@users.noreply.github.com>
2024-12-04 09:35:50 -0800

    FYST-551 MD form 502 xml & pdf healthcare coverage (#4986)
    Co-authored-by: Drew Proebstel <dproebstel@codeforamerica.org>
    Co-authored-by: Martha Pidcock <mpidcock@codeforamerica.org>

--- app/lib/submission_builder/ty2024/states/md/documents/md502.rb
+++ app/lib/submission_builder/ty2024/states/md/documents/md502.rb
@@ -129,6 +129,22 @@ class SubmissionBuilder::Ty2024::States::Md::Documents::Md502 < SubmissionBuilde
         xml.NetIncome calculated_fields.fetch(:MD502_LINE_18)
         xml.ExemptionAmount calculated_fields.fetch(:MD502_LINE_19)
       end
+      if has_healthcare_coverage_section?
+        xml.MDHealthCareCoverage do
+          if @intake.primary_did_not_have_health_insurance_yes?
+            xml.PriWithoutHealthCoverageInd "X"
+            xml.PriDOB date_type(@intake.primary_birth_date)
+          end
+          if @intake.spouse_did_not_have_health_insurance_yes?
+            xml.SecWithoutHealthCoverageInd "X"
+            xml.SecDOB date_type(@intake.spouse_birth_date)
+          end
+          if @intake.authorize_sharing_of_health_insurance_info_yes?
+            xml.AuthorToShareInfoHealthExchInd "X"
+            xml.TaxpayerEmailAddress email_from_intake_or_df
+          end
+        end
+      end
       if has_state_tax_computation?
         xml.StateTaxComputation do
           xml.TaxableNetIncome calculated_fields.fetch(:MD502_LINE_20) if @deduction_method_is_standard
@@ -219,6 +235,12 @@ class SubmissionBuilder::Ty2024::States::Md::Documents::Md502 < SubmissionBuilde
       calculated_fields.fetch(line) > 0
     end
     has_dependent_exemption? || has_line_a_or_b_exemptions
+  end
+
+  def has_healthcare_coverage_section?
+    @intake.primary_did_not_have_health_insurance_yes? ||
+      @intake.spouse_did_not_have_health_insurance_yes? ||
+      @intake.authorize_sharing_of_health_insurance_info_yes?
   end
 
   def has_state_tax_computation?
