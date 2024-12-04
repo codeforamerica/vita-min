51c88be4a787d358afeb3a7877b0666042a0b087
Tahsina Islam <tislam@codeforamerica.org>
2024-12-03 16:28:10 -0800

    fix 502 xml

--- app/lib/submission_builder/ty2024/states/md/documents/md502.rb
+++ app/lib/submission_builder/ty2024/states/md/documents/md502.rb
@@ -53,7 +53,7 @@ class SubmissionBuilder::Ty2024::States::Md::Documents::Md502 < SubmissionBuilde
           xml.ZIPCd @intake.direct_file_data.mailing_zip
         elsif @intake.confirmed_permanent_address_no?
           xml.AddressLine1Txt sanitize_for_xml(@intake.permanent_street, 35)
-          xml.AddressLine2Txt sanitize_for_xml(@intake.permanent_apartment, 35) if @intake.direct_file_data.mailing_apartment.present?
+          xml.AddressLine2Txt sanitize_for_xml(@intake.permanent_apartment, 35) if @intake.permanent_apartment.present?
           xml.CityNm sanitize_for_xml(@intake.permanent_city, 22)
           xml.StateAbbreviationCd @intake.state_code.upcase
           xml.ZIPCd @intake.permanent_zip
