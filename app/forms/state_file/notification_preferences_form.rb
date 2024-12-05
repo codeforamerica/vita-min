ee198c72dc93ddd6f6a63077a2264c6c7b2df1d5
Kevin Diale <powersurge360@gmail.com>
2024-12-05 11:52:31 -0800

    Progress on required field ticket

--- app/forms/state_file/notification_preferences_form.rb
+++ app/forms/state_file/notification_preferences_form.rb
@@ -3,7 +3,7 @@ module StateFile
     set_attributes_for :intake, :sms_notification_opt_in, :email_notification_opt_in, :phone_number
     before_validation :normalize_phone_numbers
 
-    validates :phone_number, allow_blank: true, e164_phone: true, if: -> { attributes_for(:intake)[:sms_notification_opt_in] == "yes" }
+    validates :phone_number, allow_blank: true, e164_phone: true, if: -> { sms_notification_opt_in == "yes" }
     validate :at_least_one_selected
 
     def save
@@ -16,6 +16,10 @@ module StateFile
       unless attributes_for(:intake).values_at(:sms_notification_opt_in, :email_notification_opt_in).any? { |value| value == "yes" }
         errors.add(:sms_notification_opt_in, I18n.t("state_file.questions.notification_preferences.form.at_least_one"))
       end
+    end
+
+    def phone_number_required
+      sms_notification_opt_in == "yes"
     end
 
     def normalize_phone_numbers
