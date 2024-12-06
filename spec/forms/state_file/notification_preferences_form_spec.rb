baa86295986ca5e463814205ffac0737dc358093
Tahsina Islam <79296603+tahsinaislam@users.noreply.github.com>
2024-12-05 16:13:32 -0800

    FYST-1258 Fix email notifications preference (#5108)

--- spec/forms/state_file/notification_preferences_form_spec.rb
+++ spec/forms/state_file/notification_preferences_form_spec.rb
@@ -39,10 +39,72 @@ RSpec.describe StateFile::NotificationPreferencesForm do
       end
     end
 
+    context "when email_notification_opt_in is present" do
+      let(:valid_params) do
+        {
+          email_notification_opt_in: "yes",
+          email_address: "test@example.com"
+        }
+      end
+
+      subject(:form) { described_class.new(intake, valid_params) }
+
+      it "is valid" do
+        expect(form).to be_valid
+      end
+
+      context "when email address is invalid" do
+        let(:invalid_params) do
+          {
+            email_notification_opt_in: "yes",
+            email_address: "invalid-email"
+          }
+        end
+        subject(:form) { described_class.new(intake, invalid_params) }
+
+        it "is invalid" do
+          form.valid?
+          expect(form).not_to be_valid
+        end
+
+        it "adds an error to the email_address attribute" do
+          form.valid?
+          expect(form.errors[:email_address]).to be_present
+        end
+      end
+
+      context "when email address is missing" do
+        let(:invalid_params) do
+          {
+            email_notification_opt_in: "yes",
+            email_address: ""
+          }
+        end
+        subject(:form) { described_class.new(intake, invalid_params) }
+
+        it "is invalid" do
+          form.valid?
+          expect(form).not_to be_valid
+        end
+
+        it "adds a presence error to the email_address attribute" do
+          form.valid?
+          expect(form.errors[:email_address]).to include "Can't be blank."
+        end
+      end
+
+      it "updates the intake with the provided attributes" do
+        form = described_class.new(intake, valid_params)
+        expect { form.save }.to change(intake, :email_notification_opt_in).from("unfilled").to("yes")
+                                                                          .and change(intake, :email_address).to("test@example.com")
+      end
+    end
+
     context "when sms_notification_opt_in is not present" do
       let(:invalid_params) do
         {
           email_notification_opt_in: "yes",
+          email_address: "test@example.com",
           phone_number: "415555121"
         }
       end
