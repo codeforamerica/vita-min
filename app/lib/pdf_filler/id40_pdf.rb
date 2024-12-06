1c2882302c8cfe2440d20143b38fe5cbdd3bfb99
Drew Proebstel <dproebstel@codeforamerica.org>
2024-12-06 14:10:55 -0800

    merge main

--- app/lib/pdf_filler/id40_pdf.rb
+++ app/lib/pdf_filler/id40_pdf.rb
@@ -90,7 +90,7 @@ module PdfFiller
                          'DirectDepositL57Route' => @xml_document.at('RoutingTransitNumber')&.text,
                          'DirectDepositL57Acct' => @xml_document.at('BankAccountNumber')&.text,
                          'DirectDepositChecking' => @xml_document.at('Checking')&.text == "X" ? 'Yes' : 'Off',
-                         'DirectDepositSavings' => @xml_document.at('Savings')&.text == "X" ? 'Yes' : 'Off',
+                         'DirectDepositSavings' => 'Yes'
                        })
       end
       if @xml_document.at('WildlifeDonation').present?
