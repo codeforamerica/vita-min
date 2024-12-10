752002555e05207a614020ea06ba573e6c3ea46f
Melanie <melanie.mazanec@gmail.com>
2024-12-10 17:20:19 -0500

    Add dependents to view and style

--- app/forms/state_file/nj_eligibility_health_insurance_form.rb
+++ app/forms/state_file/nj_eligibility_health_insurance_form.rb
@@ -1,6 +1,8 @@
 module StateFile
   class NjEligibilityHealthInsuranceForm < QuestionsForm
     set_attributes_for :intake, :eligibility_all_members_health_insurance
+    attr_accessor :dependents_attributes
+    delegate :dependents, to: :intake
 
     validates :eligibility_all_members_health_insurance, presence: true
 
