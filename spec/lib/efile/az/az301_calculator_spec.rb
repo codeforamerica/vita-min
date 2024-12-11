771a33ed8f34b533d1f087204f8aa2a6c0109084
Arin Choi <achoi@codeforamerica.org>
2024-12-11 12:47:21 -0800

    Add logic for calculating lines 40 and 41

--- spec/lib/efile/az/az301_calculator_spec.rb
+++ spec/lib/efile/az/az301_calculator_spec.rb
@@ -47,5 +47,144 @@ describe Efile::Az::Az301Calculator do
       expect(instance.lines[:AZ301_LINE_60].value).to eq(1338)
       expect(instance.lines[:AZ301_LINE_62].value).to eq(1338)
     end
+
+    context "line 40" do
+      context "with 6c greater than line 34 value" do
+        before do
+          allow(instance).to receive(:calculate_line_6c).and_return 1_000
+          allow(instance).to receive(:calculate_line_34).and_return 500
+        end
+
+        it "line 40 value is equal to line 34 value" do
+          instance.calculate
+          expect(instance.lines[:AZ301_LINE_40].value).to eq(500)
+        end
+      end
+
+      context "with line 6c less than line 34" do
+        before do
+          allow(instance).to receive(:calculate_line_6c).and_return 500
+          allow(instance).to receive(:calculate_line_34).and_return 1000
+        end
+
+        it "line 40 value is equal to line 6c value" do
+          instance.calculate
+          expect(instance.lines[:AZ301_LINE_40].value).to eq(500)
+        end
+      end
+
+      context "with line 6c blank" do
+        before do
+          allow(instance).to receive(:calculate_line_6c).and_return nil
+          allow(instance).to receive(:calculate_line_34).and_return 1000
+        end
+
+        it "line 40 value is equal to zero" do
+          instance.calculate
+          expect(instance.lines[:AZ301_LINE_40].value).to eq(0)
+        end
+      end
+
+      context "with line 34 blank" do
+        before do
+          allow(instance).to receive(:calculate_line_6c).and_return 10
+          allow(instance).to receive(:calculate_line_34).and_return nil
+        end
+
+        it "line 40 value is equal to zero" do
+          instance.calculate
+          expect(instance.lines[:AZ301_LINE_40].value).to eq(0)
+        end
+      end
+    end
+
+    context "line 41" do
+      context "line 34 greater than line 40" do
+        before do
+          allow(instance).to receive(:calculate_line_34).and_return 120
+          allow(instance).to receive(:calculate_line_40).and_return 100
+        end
+
+        context "line 7c greater than subtraction" do
+          before do
+            allow(instance).to receive(:calculate_line_7c).and_return 30
+          end
+
+          it "should return the subtraction amount" do
+            instance.calculate
+            expect(instance.lines[:AZ301_LINE_41].value).to eq(20)
+          end
+        end
+
+        context "line 7c less than subtraction" do
+          before do
+            allow(instance).to receive(:calculate_line_7c).and_return 10
+          end
+
+          it "should return the 7c amount" do
+            instance.calculate
+            expect(instance.lines[:AZ301_LINE_41].value).to eq(10)
+          end
+        end
+      end
+
+      context "line 34 less than line 40" do
+        before do
+          allow(instance).to receive(:calculate_line_34).and_return 90
+          allow(instance).to receive(:calculate_line_40).and_return 100
+        end
+
+        context "line 7c greater than subtraction" do
+          before do
+            allow(instance).to receive(:calculate_line_7c).and_return 10
+          end
+
+          it "returns 0" do
+            instance.calculate
+            expect(instance.lines[:AZ301_LINE_41].value).to eq(0)
+          end
+        end
+
+        context "line 7c equal to subtraction" do
+          before do
+            allow(instance).to receive(:calculate_line_7c).and_return 0
+          end
+
+          it "returns 0" do
+            instance.calculate
+            expect(instance.lines[:AZ301_LINE_41].value).to eq(0)
+          end
+        end
+      end
+
+      context "line 34 equal to line 40" do
+        before do
+          allow(instance).to receive(:calculate_line_34).and_return 90
+          allow(instance).to receive(:calculate_line_40).and_return 90
+        end
+
+        context "line 7c greater than subtraction" do
+          before do
+            allow(instance).to receive(:calculate_line_7c).and_return 100
+          end
+
+          it "returns 0" do
+            instance.calculate
+            expect(instance.lines[:AZ301_LINE_41].value).to eq(0)
+          end
+        end
+
+        context "line 7c equal to subtraction which is 0" do
+          before do
+            allow(instance).to receive(:calculate_line_7c).and_return 0
+          end
+
+          it "returns 0" do
+            instance.calculate
+            expect(instance.lines[:AZ301_LINE_41].value).to eq(0)
+          end
+        end
+      end
+    end
   end
 end
