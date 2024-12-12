cc01c12bb2e225df83e70947c7fe44e841dcfb49
John Chan <john@innovation.nj.gov>
2024-12-12 16:43:28 -0500

    required worksheet value to be greater than 0 to prevent bug

--- app/forms/state_file/nj_tenant_property_tax_worksheet_form.rb
+++ app/forms/state_file/nj_tenant_property_tax_worksheet_form.rb
@@ -4,7 +4,7 @@ module StateFile
                        :rent_paid
 
     validates_numericality_of :rent_paid, message: I18n.t("validators.not_a_number"), if: -> { rent_paid.present? }
-    validates :rent_paid, presence: true, numericality: { greater_than_or_equal_to: 0 }, if: -> { rent_paid.present? }
+    validates :rent_paid, presence: true, numericality: { greater_than_or_equal_to: 1 }, if: -> { rent_paid.present? }
 
     def save
       @intake.update(attributes_for(:intake))
