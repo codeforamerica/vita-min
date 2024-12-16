fddfc37243adec1829207a9286f6a29cd0736b1b
Arin Choi <achoi@codeforamerica.org>
2024-12-16 12:02:32 -0800

    Allow MD return xml spec to pass with hard-coded intake info that should be present at bundling

--- spec/factories/state_file_md_intakes.rb
+++ spec/factories/state_file_md_intakes.rb
@@ -130,6 +130,12 @@ FactoryBot.define do
     primary_middle_initial { "A" }
     primary_last_name { "Lando" }
     primary_birth_date { Date.new(1950, 01, 01) } # matches the bday in md_minimal.json
+    primary_signature_pin { '12345' }
+    primary_esigned_at { DateTime.now }
+    subdivision_code { "0111" }
+    political_subdivision { "Mt Savage" }
+    confirmed_permanent_address { "yes" }
+    residence_county { "Allegany" }
 
     after(:build) do |intake, evaluator|
       numeric_status = {
@@ -187,16 +193,14 @@ FactoryBot.define do
       spouse_middle_initial { "B" }
       spouse_last_name { "Lando" }
       spouse_birth_date { MultiTenantService.statefile.end_of_current_tax_year - 40 }
+      spouse_signature_pin { '54321' }
+      spouse_esigned_at { DateTime.now }
     end
 
     trait :with_senior_spouse do
+      with_spouse
       raw_direct_file_data { StateFile::DirectFileApiResponseSampleService.new.read_xml("md_minimal_with_spouse") }
       raw_direct_file_intake_data { StateFile::DirectFileApiResponseSampleService.new.read_json('md_minimal_with_spouse') }
-      filing_status { 'married_filing_jointly' }
-
-      spouse_first_name { "Marty" }
-      spouse_middle_initial { "B" }
-      spouse_last_name { "Lando" }
       spouse_birth_date { MultiTenantService.statefile.end_of_current_tax_year - 70 }
     end
 
