293176ae59641fff3d1d5409d045a05402638a59
spompea-cfa <spompea@codeforamerica.org>
2024-12-17 12:49:41 -0500

    Add end-of-flow behavior to race screens + fix new_single_filer_spec.rb

--- app/controllers/questions/demographic_spouse_race_controller.rb
+++ app/controllers/questions/demographic_spouse_race_controller.rb
@@ -5,9 +5,22 @@ module Questions
     layout "intake"
 
     def self.show?(intake)
-      intake.demographic_questions_opt_in_yes? && intake.filing_joint_yes?
+      intake&.demographic_questions_opt_in_yes? && intake&.filing_joint_yes?
     end
 
     def illustration_path; end
+
+    private
+
+    def next_path
+      root_path
+    end
+
+    def after_update_success
+      super
+      if next_path == root_path
+        clear_intake_session
+      end
+    end
   end
 end
