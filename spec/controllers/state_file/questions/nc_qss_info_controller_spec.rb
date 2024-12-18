274f9513f51934d663c8cf8e5683d0e4d89dc4d8
Mike Rotondo <mrotondo@codeforamerica.org>
2024-12-18 12:39:58 -0800

    generate list of years instead of hardcoding

--- spec/controllers/state_file/questions/nc_qss_info_controller_spec.rb
+++ spec/controllers/state_file/questions/nc_qss_info_controller_spec.rb
@@ -36,8 +36,52 @@ RSpec.describe StateFile::Questions::NcQssInfoController do
       }
     end
 
-    it_behaves_like :return_to_review_concern do
 
+    it_behaves_like :return_to_review_concern
+
+    context "when no year is selected" do
+      let(:empty_params) do
+        {
+        }
+      end
+
+      it "fails & shows an error" do
+        post :update, params: empty_params
+        expect(assigns(:form)).not_to be_valid
+        expect(assigns(:form).errors[:spouse_death_year]).not_to be_blank
+      end
+    end
+
+    context "when some invalid year is somehow provided" do
+      let(:invalid_params) do
+        {
+          state_file_nc_qss_info_form: {
+            spouse_death_year: 1900
+          }
+        }
+      end
+
+      it "fails & shows an error" do
+        post :update, params: invalid_params
+        expect(assigns(:form)).not_to be_valid
+        expect(assigns(:form).errors[:spouse_death_year]).not_to be_blank
+      end
+    end
+
+    context "when a valid year is provided" do
+      let(:valid_params) do
+        {
+          state_file_nc_qss_info_form: {
+            spouse_death_year: MultiTenantService.statefile.current_tax_year - 1
+          }
+        }
+      end
+
+      it "succeeds" do
+        post :update, params: valid_params
+        expect(assigns(:form)).to be_valid
+        expect(assigns(:form).errors[:spouse_death_year]).to be_blank
+      end
     end
   end
 end
