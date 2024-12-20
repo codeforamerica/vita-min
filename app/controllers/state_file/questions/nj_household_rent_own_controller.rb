fc9ae57a00f055f8018e817d822f9a0b0c307310
Anne LoVerso <anne.loverso@gmail.com>
2024-12-20 16:49:43 -0500

    Nj 192 property tax both (#5224)

--- app/controllers/state_file/questions/nj_household_rent_own_controller.rb
+++ app/controllers/state_file/questions/nj_household_rent_own_controller.rb
@@ -8,20 +8,18 @@ module StateFile
         options[:return_to_review] = params[:return_to_review] if params[:return_to_review].present?
 
         if Efile::Nj::NjPropertyTaxEligibility.ineligible?(current_intake)
-          return super # skip remaining property tax questions and go to whichever is next by default
+          return StateFile::NjPropertyTaxFlowOffRamp.next_controller(options)
         end
         
         case current_intake.household_rent_own
         when 'rent'
           NjTenantEligibilityController.to_path_helper(options)
-        when 'own'
+        when 'own', 'both'
           NjHomeownerEligibilityController.to_path_helper(options)
-        when 'both'
-          NjIneligiblePropertyTaxController.to_path_helper(options)
         when 'neither'
           NjIneligiblePropertyTaxController.to_path_helper(options)
         else
-          super
+          StateFile::NjPropertyTaxFlowOffRamp.next_controller(options)
         end
       end
     end
