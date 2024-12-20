8162fb92d9f5b6f49ec5b702d9900609e72ab5ee
Anne LoVerso <anne.loverso@gmail.com>
2024-12-20 17:15:57 -0500

    NJ 161 - include veteran doc requirements on submission page

--- spec/controllers/state_file/questions/submission_confirmation_controller_spec.rb
+++ spec/controllers/state_file/questions/submission_confirmation_controller_spec.rb
@@ -15,4 +15,33 @@ describe StateFile::Questions::SubmissionConfirmationController do
       expect(response_html).to have_text "state tax return is now submitted"
     end
   end
+
+  describe "nj veteran content" do
+    context "when nj and primary veteran" do
+      let(:intake) { create :state_file_nj_intake, :married_filing_jointly, primary_veteran: "yes", spouse_veteran: "no" }
+      render_views
+      it "shows veteran documentation requirements" do
+        get :edit
+        expect(response_html).to have_text "You said you or your spouse are filing for the veteran exemption"
+      end
+    end
+
+    context "when nj and spouse veteran" do
+      let(:intake) { create :state_file_nj_intake, :married_filing_jointly, primary_veteran: "no", spouse_veteran: "yes" }
+      render_views
+      it "shows veteran documentation requirements" do
+        get :edit
+        expect(response_html).to have_text "You said you or your spouse are filing for the veteran exemption"
+      end
+    end
+
+    context "when nj and neither primary nor spouse are veteran" do
+      let(:intake) { create :state_file_nj_intake, :married_filing_jointly, primary_veteran: "no", spouse_veteran: "no" }
+      render_views
+      it "does not show veteran documentation requirements" do
+        get :edit
+        expect(response_html).not_to have_text "You said you or your spouse are filing for the veteran exemption"
+      end
+    end
+  end
 end
