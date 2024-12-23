4c1a953e763dca3a8c658f6d9d18db73818d2d1b
Arin Choi <achoi@codeforamerica.org>
2024-12-23 11:52:13 -0800

    update to using NC daffy single as base for StateFileNcIntakes

--- spec/factories/state_file_nc_intakes.rb
+++ spec/factories/state_file_nc_intakes.rb
@@ -87,7 +87,7 @@
 FactoryBot.define do
   factory :state_file_nc_intake do
     transient do
-      filing_status { 'married_filing_jointly' }
+      filing_status { 'single' }
     end
 
     factory :state_file_nc_refund_intake do
@@ -101,8 +101,8 @@ FactoryBot.define do
       end
     end
 
-    raw_direct_file_data { StateFile::DirectFileApiResponseSampleService.new.read_xml('nc_nick') }
-    raw_direct_file_intake_data { StateFile::DirectFileApiResponseSampleService.new.read_json('nc_nick') }
+    raw_direct_file_data { StateFile::DirectFileApiResponseSampleService.new.read_xml('nc_daffy_single') }
+    raw_direct_file_intake_data { StateFile::DirectFileApiResponseSampleService.new.read_json('nc_daffy_single') }
     df_data_import_succeeded_at { DateTime.now }
 
     primary_first_name { "North" }
