1dec652e4bf258a4763c5d55f6f472f8c9142c8e
Melanie <melanie.mazanec@gmail.com>
2024-12-26 10:46:49 -0500

    Account for single veteran case

--- spec/controllers/state_file/questions/submission_confirmation_controller_spec.rb
+++ spec/controllers/state_file/questions/submission_confirmation_controller_spec.rb
@@ -5,6 +5,7 @@ describe StateFile::Questions::SubmissionConfirmationController do
   let!(:submission) { create :efile_submission, :for_state, data_source: intake }
   before do
     sign_in intake
+    allow(subject).to receive(:current_intake).and_return(intake)
   end
 
   describe '#edit' do
@@ -17,9 +18,19 @@ describe StateFile::Questions::SubmissionConfirmationController do
   end
 
   describe "nj veteran content" do
+    let(:or_spouse) { I18n.t('state_file.questions.submission_confirmation.nj_additional_content.or_spouse') }
+    let(:body_substring) { I18n.t('state_file.questions.submission_confirmation.nj_additional_content.body_html', or_spouse: or_spouse)[0..30] }
     render_views
-    let(:body_substring) { I18n.t('state_file.questions.submission_confirmation.nj_additional_content.body_html')[0..30] }
-    # Searching for html as text in the document throws an error
+
+    context "single veteran" do
+      let(:intake) { create :state_file_nj_intake, primary_veteran: "yes", filing_status: :single }
+      let(:or_spouse) { '' }
+      
+      it "shows veteran documentation requirements" do
+        get :edit
+        expect(response_html).to have_text body_substring
+      end
+    end
 
     context "when nj and primary veteran" do
       let(:intake) { create :state_file_nj_intake, :married_filing_jointly, primary_veteran: "yes", spouse_veteran: "no" }
