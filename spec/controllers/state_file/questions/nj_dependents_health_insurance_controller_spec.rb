79b1e630dfdf08f124a0381af6af41b0b8e952e4
Melanie <melanie.mazanec@gmail.com>
2024-12-27 16:18:46 -0500

    Test dependents health insurance show

--- spec/controllers/state_file/questions/nj_dependents_health_insurance_controller_spec.rb
+++ spec/controllers/state_file/questions/nj_dependents_health_insurance_controller_spec.rb
@@ -1,6 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe StateFile::Questions::NjDependentsHealthInsuranceController do
+  describe ".show?" do
+    context "when intake has no dependents" do
+      let(:intake) { create :state_file_nj_intake}
+      it "does not show" do
+        expect(described_class.show?(intake)).to eq false
+      end
+    end
+
+    context "when intake has depedents" do
+      let(:intake) { create :state_file_nj_intake, :df_data_two_deps }
+      
+      context "and did not have a health insurance requirement exception and all members had health insurance" do
+        it "does not show" do
+          allow_any_instance_of(StateFileNjIntake).to receive(:has_health_insurance_requirement_exception?).and_return(false)
+          allow_any_instance_of(StateFileNjIntake).to receive(:eligibility_all_members_health_insurance_no?).and_return(false)
+          expect(described_class.show?(intake)).to eq false
+        end
+      end
+  
+      context "and had a health insurance requirement exception, and all members had health insurance" do
+        it "shows" do
+          allow_any_instance_of(StateFileNjIntake).to receive(:has_health_insurance_requirement_exception?).and_return(true)
+          allow_any_instance_of(StateFileNjIntake).to receive(:eligibility_all_members_health_insurance_no?).and_return(false)
+          expect(described_class.show?(intake)).to eq true
+        end
+      end
+     
+      context "and did not have a health insurance requirement exception, but all members did not have health insurance" do  
+        it "shows" do
+          allow_any_instance_of(StateFileNjIntake).to receive(:has_health_insurance_requirement_exception?).and_return(false)
+          allow_any_instance_of(StateFileNjIntake).to receive(:eligibility_all_members_health_insurance_no?).and_return(true)
+          expect(described_class.show?(intake)).to eq true
+        end
+      end
+    end
+  end
+
   describe "#update" do
     context "taxpayer with dependents" do
       let(:intake) { create :state_file_nj_intake, :df_data_two_deps }
