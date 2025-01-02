fe460342fa58bd2c25c5d6fe8abdf066cc2eeb79
Melanie <melanie.mazanec@gmail.com>
2025-01-02 13:52:31 -0500

    Fix controller tests

--- spec/controllers/state_file/questions/nj_dependents_health_insurance_controller_spec.rb
+++ spec/controllers/state_file/questions/nj_dependents_health_insurance_controller_spec.rb
@@ -46,7 +46,7 @@ RSpec.describe StateFile::Questions::NjDependentsHealthInsuranceController do
 
       let(:form_params) do
         {
-          state_file_nj_dependent_health_insurance_form: {
+          state_file_nj_dependents_health_insurance_form: {
             dependents_attributes: {
               '0': {
                 id: first_dependent.id,
@@ -81,45 +81,6 @@ RSpec.describe StateFile::Questions::NjDependentsHealthInsuranceController do
         post :update, params: form_params
         intake.reload
         expect(intake.dependents[0].nj_did_not_have_health_insurance).to eq "yes"
-        expect(intake.dependents[1].nj_did_not_have_health_insurance).to eq "no"
-      end
-    end
-
-
-    context "when taxpayer with dependents affirms coverage when answer was previously no and they had indicated dependents without coverage" do
-      let(:intake) { create :state_file_nj_intake, :df_data_two_deps, eligibility_all_members_health_insurance: 'no' }
-      let(:first_dependent) { intake.dependents[0] }
-      let(:second_dependent) { intake.dependents[1] }
-
-      let(:form_params) do
-        {
-          state_file_nj_dependent_health_insurance_form: {
-            eligibility_all_members_health_insurance: "yes",
-            dependents_attributes: {
-              '0': {
-                id: first_dependent.id,
-                nj_did_not_have_health_insurance: 'yes'
-              },
-              '1': {
-                id: second_dependent.id,
-                nj_did_not_have_health_insurance: 'yes'
-              }
-            }
-          }
-        }
-      end
-
-      before do
-        sign_in intake
-      end
-      
-      it "sets all dependent values to no to indicate that they did indeed have coverage" do
-        first_dependent.nj_did_not_have_health_insurance = 'no'
-        second_dependent.nj_did_not_have_health_insurance = 'yes'
-        intake.reload
-        post :update, params: form_params
-        intake.reload
-        expect(intake.dependents[0].nj_did_not_have_health_insurance).to eq "no"
         expect(intake.dependents[1].nj_did_not_have_health_insurance).to eq "no"
       end
     end
