234b1e303c7d7f0b9b2e5e345bc6d9e876b95662
Drew Proebstel <dproebstel@codeforamerica.org>
2025-01-02 11:07:23 -0800

    wip

--- app/controllers/state_file/questions/email_address_validation_controller.rb
+++ app/controllers/state_file/questions/email_address_validation_controller.rb
@@ -3,11 +3,11 @@ module StateFile
     class EmailAddressValidationController < QuestionsController
       def edit
         super
-        State_file_archived_intake_access_logs.create(
-          event_type: "submission",
-          ip_address: ip_for_irs,
-          intake: current_intake,
-          )
+        # ::StateFileArchivedIntakeAccessLog.create(
+        #   event_type: "submission",
+        #   ip_address: ip_for_irs,
+        #   intake: current_intake,
+        #   )
       end
 
     end
--- app/forms/state_file/email_address_validation_form.rb
+++ app/forms/state_file/email_address_validation_form.rb
@@ -1,8 +1,14 @@
 module StateFile
   class EmailAddressValidationForm < QuestionsForm
-    set_attributes_for :state_file_archived_intake_access_logs, :ip_address, :event_type, :updated_at
+    set_attributes_for :state_file_archived_intake, :email_address
+    validates :email_address, 'valid_email_2/email': true
+    validates :email_address, presence: true
+    validates :email_address_attached_to_an_intake
+
     def save
       @intake.update(attributes_for(:state_file_archived_intake_access_logs))
     end
+
+
   end
 end
