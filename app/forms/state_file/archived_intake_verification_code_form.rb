4181a66f40bed591a386b76efa725ab970c588af
Drew Proebstel <dproebstel@codeforamerica.org>
2025-01-06 10:05:10 -0800

    update unused file

--- app/forms/state_file/archived_intake_verification_code_form.rb
+++
@@ -1,7 +0,0 @@
-module Portal
-  class ArchivedIntakeVerificationCodeForm < Form
-    include PhoneNumberHelper
-    attr_accessor :contact_info, :verification_code
-    validates :verification_code, format: { with: /\A[0-9]{6}\z/ }
-  end
-end
