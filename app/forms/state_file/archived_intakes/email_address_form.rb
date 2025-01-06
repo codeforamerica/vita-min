5162bf6e9961bb3a1de1d3e59f2d9284db251c15
Drew Proebstel <dproebstel@codeforamerica.org>
2025-01-06 10:11:08 -0800

    remove unused prys

--- app/forms/state_file/archived_intakes/email_address_form.rb
+++ app/forms/state_file/archived_intakes/email_address_form.rb
@@ -7,16 +7,13 @@ module StateFile
       validates :email_address, presence: true
 
       def initialize(attributes = {})
-        binding.pry
         super
         assign_attributes(attributes)
       end
 
       def save
-        binding.pry
         run_callbacks :save do
           if valid?
-            # Logic to persist the form data or perform actions
             true
           else
             false
