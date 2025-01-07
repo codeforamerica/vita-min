18aafe63ccc749f40c3f7771a3317da961e18c2c
Anne LoVerso <anne.loverso@gmail.com>
2025-01-07 12:06:40 -0500

    pm review updates

--- app/controllers/state_file/questions/nj_dependents_health_insurance_controller.rb
+++ app/controllers/state_file/questions/nj_dependents_health_insurance_controller.rb
@@ -1,6 +1,7 @@
 module StateFile
   module Questions
     class NjDependentsHealthInsuranceController < QuestionsController
+      include ReturnToReviewConcern
 
       before_action -> { @filing_year = Rails.configuration.statefile_current_tax_year }
 
