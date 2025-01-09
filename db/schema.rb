ec3acd06ed139199b92fc43e164948d8815c4cfc
spompea-cfa <spompea@codeforamerica.org>
2025-01-08 19:59:59 -0500

    rebase madness redux

--- db/schema.rb
+++ db/schema.rb
@@ -1178,6 +1178,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_01_13_222716) do
     t.boolean "demographic_primary_asian"
     t.boolean "demographic_primary_black_african_american"
     t.integer "demographic_primary_ethnicity", default: 0, null: false
+    t.boolean "demographic_primary_mena"
     t.boolean "demographic_primary_native_hawaiian_pacific_islander"
     t.boolean "demographic_primary_prefer_not_to_answer_race"
     t.boolean "demographic_primary_white"
@@ -1187,6 +1188,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_01_13_222716) do
     t.boolean "demographic_spouse_asian"
     t.boolean "demographic_spouse_black_african_american"
     t.integer "demographic_spouse_ethnicity", default: 0, null: false
+    t.boolean "demographic_spouse_mena"
     t.boolean "demographic_spouse_native_hawaiian_pacific_islander"
     t.boolean "demographic_spouse_prefer_not_to_answer_race"
     t.boolean "demographic_spouse_white"
