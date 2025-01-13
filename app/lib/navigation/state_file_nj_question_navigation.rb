6289934d1889f11667e4c391228f7847acf5a061
Matt Luedke <matthew@innovation.nj.gov>
2025-01-13 10:12:29 -0800

    adjust order in question navigation

--- app/lib/navigation/state_file_nj_question_navigation.rb
+++ app/lib/navigation/state_file_nj_question_navigation.rb
@@ -30,34 +30,34 @@ module Navigation
                                           # Federal info does not show to users
                                           Navigation::NavigationStep.new(StateFile::Questions::FederalInfoController),
                                           Navigation::NavigationStep.new(StateFile::Questions::DataTransferOffboardingController, false),
-                                          Navigation::NavigationStep.new(StateFile::Questions::IncomeReviewController),
+                                          Navigation::NavigationStep.new(StateFile::Questions::IncomeReviewController), # line 15-27, but intentionally moved up for eligibility checks
                                           Navigation::NavigationStep.new(StateFile::Questions::NjEligibilityHealthInsuranceController),
                                           Navigation::NavigationStep.new(StateFile::Questions::EligibilityOffboardingController, false),
-                                          Navigation::NavigationStep.new(StateFile::Questions::NjYearOfDeathController),
-                                          Navigation::NavigationStep.new(StateFile::Questions::NjCountyController),
-                                          Navigation::NavigationStep.new(StateFile::Questions::NjMunicipalityController),
-                                          Navigation::NavigationStep.new(StateFile::Questions::NjDisabledExemptionController),
-                                          Navigation::NavigationStep.new(StateFile::Questions::NjVeteransExemptionController),
-                                          Navigation::NavigationStep.new(StateFile::Questions::NjCollegeDependentsExemptionController),
-                                          Navigation::NavigationStep.new(StateFile::Questions::NjDependentsHealthInsuranceController),
-                                          Navigation::NavigationStep.new(StateFile::Questions::NjMedicalExpensesController),
-                                          Navigation::NavigationStep.new(StateFile::Questions::NjHouseholdRentOwnController),
-                                          Navigation::NavigationStep.new(StateFile::Questions::NjHomeownerEligibilityController),
-                                          Navigation::NavigationStep.new(StateFile::Questions::NjHomeownerPropertyTaxController),
-                                          Navigation::NavigationStep.new(StateFile::Questions::NjHomeownerPropertyTaxWorksheetController),
-                                          Navigation::NavigationStep.new(StateFile::Questions::NjTenantEligibilityController),
-                                          Navigation::NavigationStep.new(StateFile::Questions::NjTenantRentPaidController),
-                                          Navigation::NavigationStep.new(StateFile::Questions::NjTenantPropertyTaxWorksheetController),
-                                          Navigation::NavigationStep.new(StateFile::Questions::NjIneligiblePropertyTaxController),
-                                          Navigation::NavigationStep.new(StateFile::Questions::NjEstimatedTaxPaymentsController),
-                                          Navigation::NavigationStep.new(StateFile::Questions::NjEitcQualifyingChildController),
-                                          Navigation::NavigationStep.new(StateFile::Questions::NjSalesUseTaxController),
-                                          Navigation::NavigationStep.new(StateFile::Questions::NjGubernatorialElectionsController),
-                                          Navigation::NavigationStep.new(StateFile::Questions::PrimaryStateIdController),
-                                          Navigation::NavigationStep.new(StateFile::Questions::SpouseStateIdController),
+                                          Navigation::NavigationStep.new(StateFile::Questions::NjYearOfDeathController), # Line 5
+                                          Navigation::NavigationStep.new(StateFile::Questions::NjCountyController), # header
+                                          Navigation::NavigationStep.new(StateFile::Questions::NjMunicipalityController), # header
+                                          Navigation::NavigationStep.new(StateFile::Questions::NjDisabledExemptionController), # Line 8
+                                          Navigation::NavigationStep.new(StateFile::Questions::NjVeteransExemptionController), # Line 9
+                                          Navigation::NavigationStep.new(StateFile::Questions::NjCollegeDependentsExemptionController), # Line 13
+                                          Navigation::NavigationStep.new(StateFile::Questions::NjDependentsHealthInsuranceController), # Line 14
+                                          Navigation::NavigationStep.new(StateFile::Questions::NjMedicalExpensesController), # Line 31
+                                          Navigation::NavigationStep.new(StateFile::Questions::NjEitcQualifyingChildController), # Line 58, intentionally moved up to be in the context of other credits and deductions, and to ensure there is a consistent page after the property taxes section.
+                                          Navigation::NavigationStep.new(StateFile::Questions::NjHouseholdRentOwnController), # Line 40b
+                                          Navigation::NavigationStep.new(StateFile::Questions::NjHomeownerEligibilityController), # Line 40a
+                                          Navigation::NavigationStep.new(StateFile::Questions::NjTenantEligibilityController), # Line 40a
+                                          Navigation::NavigationStep.new(StateFile::Questions::NjIneligiblePropertyTaxController), # Line 40a
+                                          Navigation::NavigationStep.new(StateFile::Questions::NjHomeownerPropertyTaxWorksheetController), # Line 40a
+                                          Navigation::NavigationStep.new(StateFile::Questions::NjHomeownerPropertyTaxController), # Line 40a
+                                          Navigation::NavigationStep.new(StateFile::Questions::NjTenantPropertyTaxWorksheetController), # Line 40a
+                                          Navigation::NavigationStep.new(StateFile::Questions::NjTenantRentPaidController), # Line 40a
+                                          Navigation::NavigationStep.new(StateFile::Questions::NjSalesUseTaxController), # Line 51
+                                          Navigation::NavigationStep.new(StateFile::Questions::NjEstimatedTaxPaymentsController), # Line 57
+                                          Navigation::NavigationStep.new(StateFile::Questions::PrimaryStateIdController), # Footer
+                                          Navigation::NavigationStep.new(StateFile::Questions::SpouseStateIdController), # Footer
+                                          Navigation::NavigationStep.new(StateFile::Questions::NjReviewController), # review should come before taxes owed / refund due screens
                                           Navigation::NavigationStep.new(StateFile::Questions::TaxesOwedController),
                                           Navigation::NavigationStep.new(StateFile::Questions::TaxRefundController),
-                                          Navigation::NavigationStep.new(StateFile::Questions::NjReviewController),
+                                          Navigation::NavigationStep.new(StateFile::Questions::NjGubernatorialElectionsController),
                                           Navigation::NavigationStep.new(StateFile::Questions::EsignDeclarationController), # creates EfileSubmission and transitions to preparing
                                         ]),
       Navigation::NavigationSection.new("state_file.navigation.section_6", [
