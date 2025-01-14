f0e4926f1f7cc4d0851f4e6d70b35e0f86f9d790
Jenny Heath <jheath@codeforamerica.org>
2025-01-14 15:32:18 -0800

    Fix Sentry exception where twilio response doesn't exist; persist twilio error code from response

--- spec/jobs/state_file/send_notification_text_message_job_spec.rb
+++ spec/jobs/state_file/send_notification_text_message_job_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe StateFile::SendNotificationTextMessageJob, type: :job do
   describe "#perform" do
     let(:text_message) { create :state_file_notification_text_message }
     let(:twilio_double) { instance_double(TwilioService) }
-    let(:fake_twilio_response) { double(sid: "123", status: "sending") }
+    let(:fake_twilio_response) { double(sid: "123", status: "sending", error_code: nil) }
 
     before do
       allow(TwilioService).to receive(:new).and_return(twilio_double)
@@ -20,11 +20,33 @@ RSpec.describe StateFile::SendNotificationTextMessageJob, type: :job do
         expect(twilio_double).to have_received(:send_text_message).with(
           to: text_message.to_phone_number,
           body: text_message.body,
+          outgoing_text_message: text_message,
         )
         text_message.reload
         expect(text_message.sent_at).to eq fake_time
         expect(text_message.twilio_sid).to eq "123"
         expect(text_message.twilio_status).to eq "sending"
+      end
+    end
+
+    context "when the message has an error code" do
+      let(:fake_twilio_response) { double(sid: "123", status: "sending", error_code: "30007") }
+
+      it "persists the error code" do
+        described_class.perform_now(text_message.id)
+
+        text_message.reload
+        expect(text_message.error_code).to eq "30007"
+      end
+    end
+
+    context "when Twilio doesn't return a response in time" do
+      let(:fake_twilio_response) { nil }
+
+      it "exits gracefully" do
+        expect {
+          described_class.perform_now(text_message.id)
+        }.not_to raise_error
       end
     end
   end
