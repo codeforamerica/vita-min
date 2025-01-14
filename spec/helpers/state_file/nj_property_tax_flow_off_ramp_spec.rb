ca92b7dd2a821e923e073c93e5896339e5ba0133
Matt Luedke <matthew@innovation.nj.gov>
2025-01-14 09:46:55 -0800

    adjust helper test

--- spec/helpers/state_file/nj_property_tax_flow_off_ramp_spec.rb
+++ spec/helpers/state_file/nj_property_tax_flow_off_ramp_spec.rb
@@ -12,7 +12,7 @@ describe StateFile::NjPropertyTaxFlowOffRamp do
     context "when not return to review" do
       it "returns Nj Estimated Tax Payments path" do
         options = {}
-        expect(described_class.next_controller(options)).to eq("/en/questions/nj-estimated-tax-payments")
+        expect(described_class.next_controller(options)).to eq("/en/questions/nj-sales-use-tax")
       end
     end
   end
