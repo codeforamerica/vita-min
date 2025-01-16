6145d0d25ef00871394bd4da73671fcbf010d498
Tahsina Islam <tislam@codeforamerica.org>
2025-01-16 15:14:36 -0800

    Fix reference
    Co-authored-by: Anisha Ramnani <aramnani@codeforamerica.org>

--- app/models/state_file_archived_intake_request.rb
+++ app/models/state_file_archived_intake_request.rb
@@ -18,7 +18,7 @@
 class StateFileArchivedIntakeRequest < ApplicationRecord
   devise :lockable, unlock_in: 60.minutes, unlock_strategy: :time
   has_many :state_file_archived_intake_access_logs, class_name: 'StateFileArchivedIntakeAccessLog'
-  belongs_to :state_file_archived_intake, class_name: 'StateFileArchivedIntake'
+  belongs_to :state_file_archived_intake, class_name: 'StateFileArchivedIntake', optional: true
 
   def self.maximum_attempts
     2
