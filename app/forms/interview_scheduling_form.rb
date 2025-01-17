d211a0adebda1b5d49130be85050218f814199d9
Em Barnard-Shao <ebarnard@codeforamerica.org>
2025-01-17 10:45:55 -0500

    Add written langauge question to the interview scheduling page

--- app/forms/interview_scheduling_form.rb
+++ app/forms/interview_scheduling_form.rb
@@ -1,5 +1,8 @@
 class InterviewSchedulingForm < QuestionsForm
-  set_attributes_for :intake, :interview_timing_preference, :preferred_interview_language
+  set_attributes_for :intake,
+                     :interview_timing_preference,
+                     :preferred_interview_language,
+                     :preferred_written_language
 
   def save
     @intake.update(attributes_for(:intake))
@@ -9,8 +12,13 @@ class InterviewSchedulingForm < QuestionsForm
     I18n.backend.translations[I18n.locale][:general][:language_options].invert
   end
 
+  def written_language_options
+    I18n.backend.translations[I18n.locale][:general][:written_language_options].invert
+  end
+
   def self.existing_attributes(intake)
     intake.preferred_interview_language ||= I18n.locale
+    intake.preferred_written_language ||= I18n.locale
     HashWithIndifferentAccess.new(intake.attributes)
   end
 end
