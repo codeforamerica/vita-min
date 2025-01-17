994e02d12eacf8533cc02b76e624a47c0d1d4331
Arin Choi <achoi@codeforamerica.org>
2025-01-17 14:33:21 -0800

    Make sure request record is persisted before moving on to verification page
    Co-authored-by: Jenny Heath <jheath@codeforamerica.org>

--- app/controllers/state_file/archived_intakes/email_address_controller.rb
+++ app/controllers/state_file/archived_intakes/email_address_controller.rb
@@ -11,8 +11,8 @@ module StateFile
 
         if @form.valid?
           archived_intake = StateFileArchivedIntake.find_by(email_address: @form.email_address)
-          session[:email_address] = @form.email_address
-          StateFileArchivedIntakeRequest.find_or_create_by(email_address: @form.email_address, ip_address: ip_for_irs, state_file_archived_intake_id: archived_intake&.id )
+          request = StateFileArchivedIntakeRequest.find_or_create_by(email_address: @form.email_address, ip_address: ip_for_irs, state_file_archived_intake_id: archived_intake&.id)
+          session[:email_address] = request.email_address
           create_state_file_access_log("issued_email_challenge")
 
           redirect_to state_file_archived_intakes_edit_verification_code_path
