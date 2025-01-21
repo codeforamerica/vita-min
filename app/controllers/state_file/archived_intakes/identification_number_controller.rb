cced8717ccf3552c3e66739be28ba8a411b991f4
Drew Proebstel <dproebstel@codeforamerica.org>
2025-01-21 15:43:22 -0800

    route id number controller to the mailing address controller

--- app/controllers/state_file/archived_intakes/identification_number_controller.rb
+++ app/controllers/state_file/archived_intakes/identification_number_controller.rb
@@ -16,9 +16,7 @@ module StateFile
           create_state_file_access_log("correct_ssn_challenge")
           current_request.reset_failed_attempts!
           session[:ssn_verified] = true
-          redirect_to root_path
-          # TODO: https://codeforamerica.atlassian.net/browse/FYST-1518
-          # need to change to address controller
+          redirect_to state_file_archived_intakes_edit_mailing_address_validation_path
         else
           create_state_file_access_log("incorrect_ssn_challenge")
           current_request.increment_failed_attempts
