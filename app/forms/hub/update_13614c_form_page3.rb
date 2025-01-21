9af291c43a3a712ff1bb2f4eb4ff5d527388cba8
spompea-cfa <spompea@codeforamerica.org>
2025-01-21 12:32:16 -0500

    remove old code

--- app/forms/hub/update_13614c_form_page3.rb
+++ app/forms/hub/update_13614c_form_page3.rb
@@ -2,36 +2,7 @@ module Hub
   class Update13614cFormPage3 < Form
     include FormAttributes
 
-    set_attributes_for :intake,
-                       :receive_written_communication,
-                       :preferred_written_language,
-                       :refund_payment_method,
-                       :savings_purchase_bond,
-                       :savings_split_refund,
-                       :balance_pay_from_bank,
-                       :had_disaster_loss,
-                       :received_irs_letter,
-                       :presidential_campaign_fund_donation,
-                       :had_disaster_loss_where,
-                       :register_to_vote,
-                       :demographic_english_conversation,
-                       :demographic_english_reading,
-                       :demographic_disability,
-                       :demographic_veteran,
-                       :demographic_primary_american_indian_alaska_native,
-                       :demographic_primary_asian,
-                       :demographic_primary_black_african_american,
-                       :demographic_primary_native_hawaiian_pacific_islander,
-                       :demographic_primary_white,
-                       :demographic_primary_prefer_not_to_answer_race,
-                       :demographic_spouse_american_indian_alaska_native,
-                       :demographic_spouse_asian,
-                       :demographic_spouse_black_african_american,
-                       :demographic_spouse_native_hawaiian_pacific_islander,
-                       :demographic_spouse_white,
-                       :demographic_spouse_prefer_not_to_answer_race,
-                       :demographic_primary_ethnicity,
-                       :demographic_spouse_ethnicity
+    set_attributes_for :intake #,
 
     attr_accessor :client
 
