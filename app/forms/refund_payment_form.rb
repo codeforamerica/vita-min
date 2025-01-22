26ffbbf4952abbb65d9aaaccb38772a23acb9c2f
spompea-cfa <spompea@codeforamerica.org>
2025-01-22 15:04:42 -0500

    tweak RefundPaymentForm save logic

--- app/forms/refund_payment_form.rb
+++ app/forms/refund_payment_form.rb
@@ -8,9 +8,9 @@ class RefundPaymentForm < QuestionsForm
 
     if payment_method == 'direct_deposit'
       intake.update(refund_direct_deposit: 'yes')
-      intake.update(refund_check_by_mail: 'unfilled')
+      intake.update(refund_check_by_mail: 'no')
     else
-      intake.update(refund_direct_deposit: 'unfilled')
+      intake.update(refund_direct_deposit: 'no')
       intake.update(refund_check_by_mail: 'yes')
     end
 
