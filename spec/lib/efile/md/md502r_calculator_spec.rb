5c9c488638261337e9b2c3dd6416f68b417baa94
Tahsina Islam <tislam@codeforamerica.org>
2025-01-23 14:32:56 -0600

    fix calculator and test

--- spec/lib/efile/md/md502r_calculator_spec.rb
+++ spec/lib/efile/md/md502r_calculator_spec.rb
@@ -12,54 +12,45 @@ describe Efile::Md::Md502rCalculator do
   let(:instance) { main_calculator.instance_variable_get(:@md502r) }
 
   describe '#calculate_9a' do
-    context 'when filing MFJ with positive federal SSB' do
-      let(:filing_status) { "married_filing_jointly" }
+    context 'when filing MFJ with positive federal social security benefits' do
+      let(:intake) { create(:state_file_md_intake, :df_data_2_w2s, filing_status: "married_filing_jointly") } # df_data_2_w2s has $8000 in federal social security benefits
       before do
-        intake.direct_file_data.fed_ssb = 1000
         intake.primary_ssb_amount = 600
-        intake.update!(raw_direct_file_data: intake.direct_file_data.to_s)
         main_calculator.calculate
       end
 
       it 'returns primary SSB amount' do
-
         expect(instance.lines[:MD502R_LINE_9A].value).to eq 600
       end
     end
 
-    context 'when not filing MFJ or no federal SSB' do
-      before do
-        intake.direct_file_data.fed_ssb = 1000
-      end
-
+    context 'when not filing MFJ' do
+      let(:intake) { create(:state_file_md_intake, :df_data_2_w2s) }
       it 'returns federal SSB amount' do
         main_calculator.calculate
-        expect(instance.lines[:MD502R_LINE_9A].value).to eq 1000
+        expect(instance.lines[:MD502R_LINE_9A].value).to eq 8000
       end
     end
   end
 
   describe '#calculate_line_9b' do
-    context 'when filing MFJ with positive federal SSB' do
+    context 'when filing MFJ with positive federal social security benefits' do
+      let(:intake) { create(:state_file_md_intake, :df_data_2_w2s, filing_status: "married_filing_jointly") } # df_data_2_w2s has $8000 in federal social security benefits
       before do
-        allow(intake).to receive(:filing_status_mfj?).and_return(true)
-        allow(intake).to receive_message_chain(:direct_file_data, :fed_ssb).and_return(1000)
-        allow(intake).to receive(:spouse_ssb_amount).and_return(400)
+        intake.spouse_ssb_amount = 400
+        main_calculator.calculate
       end
 
       it 'returns spouse SSB amount' do
-        expect(calculator.send(:calculate_line_9b)).to eq(400)
+        expect(instance.lines[:MD502R_LINE_9B].value).to eq 400
       end
     end
 
-    context 'when not filing MFJ or no federal SSB' do
-      before do
-        allow(intake).to receive(:filing_status_mfj?).and_return(false)
-        allow(intake).to receive_message_chain(:direct_file_data, :fed_ssb).and_return(0)
-      end
-
+    context 'when not filing MFJ' do
+      let(:intake) { create(:state_file_md_intake, :df_data_2_w2s) }
       it 'returns nil' do
-        expect(calculator.send(:calculate_line_9b)).to be_nil
+        main_calculator.calculate
+        expect(instance.lines[:MD502R_LINE_9B].value).to eq nil
       end
     end
   end
