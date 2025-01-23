a8075e49baaa74a800e8e990a54adaac3968cf22
Drew Proebstel <dproebstel@codeforamerica.org>
2025-01-23 13:16:38 -0800

    fix prior_year_access_spec

--- spec/features/state_file/prior_year_access_spec.rb
+++ spec/features/state_file/prior_year_access_spec.rb
@@ -30,9 +30,9 @@ RSpec.feature "accessing a prior year PDF", active_job: true do
       fill_in I18n.t("state_file.archived_intakes.identification_number.edit.ssn_label"), with: "123456789"
       click_on I18n.t("general.continue")
 
-      # TODO: https://codeforamerica.atlassian.net/browse/FYST-1518
-      # need to change to address path
-      expect(current_path).to eq(root_path)
+      expect(current_path).to eq(state_file_archived_intakes_edit_mailing_address_validation_path)
+
+
     end
   end
 
