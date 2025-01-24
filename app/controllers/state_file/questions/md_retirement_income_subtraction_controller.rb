ebb5cdb6cc9b60a0c570b740a062b7db76273f4f
Mike Rotondo <mrotondo@codeforamerica.org>
2025-01-24 13:05:19 -0800

    refactor 1099r controller out into shared controller

--- app/controllers/state_file/questions/md_retirement_income_subtraction_controller.rb
+++ app/controllers/state_file/questions/md_retirement_income_subtraction_controller.rb
@@ -1,44 +1,9 @@
 module StateFile
   module Questions
-    class MdRetirementIncomeSubtractionController < QuestionsController
-      include ReturnToReviewConcern
-      include RepeatedQuestionConcern
+    class MdRetirementIncomeSubtractionController < RetirementIncomeSubtractionController
 
-      attr_reader :state_file_1099r
+      def followup_class = StateFileMd1099RFollowup
 
-      def initialized_edit_form
-        attribute_keys = Attributes.new(form_class.attribute_names).to_sym
-        state_specific_followup = find_or_create_state_specific_followup
-        form_class.new(state_specific_followup, form_class.existing_attributes(state_specific_followup).slice(*attribute_keys))
-      end
-
-      def initialized_update_form
-        form_class.new(find_or_create_state_specific_followup, form_params)
-      end
-
-      def self.show?(intake)
-        Flipper.enabled?(:show_retirement_ui) && intake.state_file1099_rs.length.positive?
-      end
-
-      private
-
-      def num_items
-        current_intake.state_file1099_rs.count
-      end
-
-      def load_item(index)
-        @state_file_1099r = current_intake.state_file1099_rs[index]
-        if @state_file_1099r.nil?
-          render "public_pages/page_not_found", status: 404
-        end
-      end
-
-      def find_or_create_state_specific_followup
-        unless @state_file_1099r.state_specific_followup.present?
-          @state_file_1099r.update(state_specific_followup: StateFileMd1099RFollowup.create)
-        end
-        @state_file_1099r.state_specific_followup
-      end
     end
   end
 end
---
+++ app/controllers/state_file/questions/retirement_income_subtraction_controller.rb
@@ -0,0 +1,48 @@
+module StateFile
+  module Questions
+    class RetirementIncomeSubtractionController < QuestionsController
+      include ReturnToReviewConcern
+      include RepeatedQuestionConcern
+
+      attr_reader :state_file_1099r
+
+      def initialized_edit_form
+        attribute_keys = Attributes.new(form_class.attribute_names).to_sym
+        state_specific_followup = find_or_create_state_specific_followup
+        form_class.new(state_specific_followup, form_class.existing_attributes(state_specific_followup).slice(*attribute_keys))
+      end
+
+      def initialized_update_form
+        form_class.new(find_or_create_state_specific_followup, form_params)
+      end
+
+      def self.show?(intake)
+        Flipper.enabled?(:show_retirement_ui) && intake.state_file1099_rs.length.positive?
+      end
+
+      private
+
+      def num_items
+        current_intake.state_file1099_rs.count
+      end
+
+      def load_item(index)
+        @state_file_1099r = current_intake.state_file1099_rs[index]
+        if @state_file_1099r.nil?
+          render "public_pages/page_not_found", status: 404
+        end
+      end
+
+      def find_or_create_state_specific_followup
+        unless @state_file_1099r.state_specific_followup.present?
+          @state_file_1099r.update(state_specific_followup: followup_class.create)
+        end
+        @state_file_1099r.state_specific_followup
+      end
+
+      def followup_class
+        raise NotImplementedError, "Implement Subclass#followup_class to return the state-specific 1099R followup model class"
+      end
+    end
+  end
+end
