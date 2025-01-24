2fef5f6b73712c000bfe28d713e7c6cb3cf0ae77
Hugo Melo <hmelo@codeforamerica.org>
2025-01-24 12:00:53 -0500

    Guard mailing address verified
    Co-authored-by: Hugo Melo <hmelo@codeforamerica.org>

--- app/controllers/state_file/archived_intakes/pdfs_controller.rb
+++ app/controllers/state_file/archived_intakes/pdfs_controller.rb
@@ -4,13 +4,14 @@ module StateFile
       before_action :check_feature_flag
       before_action :require_archived_intake_email
       before_action :require_archived_intake_verified
+      before_action :require_mailig_address_verified
       before_action do
-        ActiveStorage::Current.url_options = { protocol: request.protocol, host: request.host, port: request.port }
+        if Rails.env.development? || Rails.env.test?
+          ActiveStorage::Current.url_options = { protocol: request.protocol, host: request.host, port: request.port }
+        end
       end
 
-
       def index
-        # @prior_year_intake = StateFileArchivedIntake.last
         @prior_year_intake = StateFileArchivedIntake.find_by!(email_address: current_request.email_address)
         @pdf_url = @prior_year_intake.submission_pdf.url(expires_in: pdf_expiration_time, disposition: "inline")
         create_state_file_access_log("issued_pdf_download_link")
@@ -34,6 +35,12 @@ module StateFile
 
       def require_archived_intake_verified
         return if session[:ssn_verified].present?
+
+        redirect_to root_path
+      end
+
+      def require_mailig_address_verified
+        return if session[:mailing_verified].present?
 
         redirect_to root_path
       end
