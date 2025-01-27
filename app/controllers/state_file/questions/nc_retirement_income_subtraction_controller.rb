e56204966c044c1a8e51c5bcd8bbf2eb075f2a8a
Anisha Ramnani <aramnani@codeforamerica.org>
2025-01-27 17:53:59 -0500

    add view for nc retirement income controller
    Co-authored-by: Jenny Heath <jheath@codeforamerica.org>

--- app/controllers/state_file/questions/nc_retirement_income_subtraction_controller.rb
+++ app/controllers/state_file/questions/nc_retirement_income_subtraction_controller.rb
@@ -1,6 +1,7 @@
 module StateFile
   module Questions
     class NcRetirementIncomeSubtractionController < QuestionsController
+      skip_before_action :redirect_if_no_intake, :require_state_file_intake_login
 
       def edit
         @state_file_1099r = current_intake.state_file1099_rs[0]
