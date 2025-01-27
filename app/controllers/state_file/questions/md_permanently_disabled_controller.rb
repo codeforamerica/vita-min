39417aa8b712c3cc1aa4662dbfb10af483606605
Tahsina Islam <tislam@codeforamerica.org>
2025-01-27 12:24:31 -0600

    add controller test

--- app/controllers/state_file/questions/md_permanently_disabled_controller.rb
+++ app/controllers/state_file/questions/md_permanently_disabled_controller.rb
@@ -1,9 +1,12 @@
 module StateFile
   module Questions
     class MdPermanentlyDisabledController < QuestionsController
-      # def self.show?(intake)
-      #   # Flipper.enabled?(:show_retirement_ui) && intake.state_file1099_rs.length.positive?
-      # end
+      def self.show?(intake)
+        Flipper.enabled?(:show_retirement_ui) && intake.state_file1099_rs.length.positive?
+      end
+
+      private
+
       def form_params
         params.require(:state_file_md_permanently_disabled_form).permit(:mfj_disability, :primary_disabled, :spouse_disabled, :proof_of_disability_submitted)
       end
