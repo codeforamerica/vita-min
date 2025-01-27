abfa45edc54f93be89b049e5fe6a584b5dedbd16
Anisha Ramnani <aramnani@codeforamerica.org>
2025-01-27 15:44:12 -0500

    --wip-- [skip ci]
    Co-authored-by: Em Barnard-Shao <ebarnard@codeforamerica.org>

--- app/controllers/state_file/questions/nc_retirement_income_subtraction_controller.rb
+++ app/controllers/state_file/questions/nc_retirement_income_subtraction_controller.rb
@@ -1,6 +1,11 @@
 module StateFile
   module Questions
     class NcRetirementIncomeSubtractionController < QuestionsController
+
+      def edit
+        @state_file_1099r = current_intake.state_file1099_rs[0]
+        @form = NcRetirementIncomeSubtractionForm.new()
+      end
     end
   end
 end
