fb82ba59c9a10345f13ced0f011a7a65d97ce3ec
Jenny Heath <jheath@codeforamerica.org>
2025-01-28 16:50:22 -0800

    Adapt apology reminder service/job to a new message

--- app/services/state_file/send_reminder_apology_service.rb
+++ app/services/state_file/send_reminder_apology_service.rb
@@ -1,15 +1,38 @@
 module StateFile
-  class SendReminderApologyService
-    def self.run
-
-      intakes = EfileSubmission.joins(:efile_submission_transitions)
-                     .for_state_filing
-                     .where("efile_submission_transitions.to_state = 'accepted'")
-                     .extract_associated(:data_source)
-
-      intakes.each do |intake|
-        SendReminderApologyMessageJob.perform_later(intake)
+  class SendDfTransferIssueMessageService
+    # assuming a data structure that looks like this:
+    # [
+    #   {
+    #     email: false,
+    #     sms: true,
+    #     contact_info: "5551112222",
+    #     state_code: "az"
+    #   }
+    # ]
+    # and that the client verified and opted in to each of these contact methods
+    def self.run(contact_list)
+      contact_list.each_with_index do |contact, i|
+        puts "."
+        if !StateFile::StateInformationService.active_state_codes.not.include?(state_code)
+          puts "state code missing or invalid; index #{i}; #{contact.contact_info}"
+          return
+        end
+        if contact.contact_info.blank?
+          puts "no contact info; index #{i}; #{state_code}"
+          return
+        end
+        if !contact.email && !contact.sms
+          puts "no contact method; index #{i}; #{contact.contact_info} #{contact.state_code}"
+          return
+        end
+        SendReminderApologyMessageJob.perform_later(
+          email: contact.email,
+          sms: contact.sms,
+          contact_info: contact.contact_info,
+          state_code: contact.state_code
+        )
       end
     end
   end
 end
+
