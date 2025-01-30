fbb491d167f48bce268d1bb1dc23f59f7c0140d4
Yejee Arin Choi <achoi@codeforamerica.org>
2025-01-30 10:36:26 -0800

    Fyst 1730 strip any values that are not allowed from middle initial field (#5478)

--- app/lib/submission_builder/ty2022/states/az/az_return_xml.rb
+++ app/lib/submission_builder/ty2022/states/az/az_return_xml.rb
@@ -63,7 +63,7 @@ module SubmissionBuilder
                   xml.DependentDetails do
                     xml.Name do
                       xml.FirstName sanitize_for_xml(dependent.first_name, 16)
-                      xml.MiddleInitial sanitize_for_xml(dependent.middle_initial, 1) if dependent.middle_initial.present?
+                      xml.MiddleInitial sanitize_middle_initial(dependent.middle_initial) if sanitize_middle_initial(dependent.middle_initial).present?
                       xml.LastName sanitize_for_xml(dependent.last_name, 32)
                     end
                     unless dependent.ssn.nil?
@@ -82,7 +82,7 @@ module SubmissionBuilder
                   xml.QualParentsAncestors do
                     xml.Name do
                       xml.FirstName sanitize_for_xml(dependent.first_name, 16)
-                      xml.MiddleInitial sanitize_for_xml(dependent.middle_initial, 1) if dependent.middle_initial.present?
+                      xml.MiddleInitial sanitize_middle_initial(dependent.middle_initial) if sanitize_middle_initial(dependent.middle_initial).present?
                       xml.LastName sanitize_for_xml(dependent.last_name, 32)
                     end
                     unless dependent.ssn.nil?
