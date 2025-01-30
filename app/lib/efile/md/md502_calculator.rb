7b1d7438f8887ae0d0ea539110f1e286b23ce5da
Mike Rotondo <mrotondo@codeforamerica.org>
2025-01-30 14:25:05 -0800

    remove redundant filing_status_dependent? method, fix and rename tax calculator filing_status_dependent? to indicate that it is MD specific, and fix test for 502CR calculator to accurately represent a persona whose MD filing status will be dependent

--- app/lib/efile/md/md502_calculator.rb
+++ app/lib/efile/md/md502_calculator.rb
@@ -374,7 +374,7 @@ module Efile
                        [1_000..2_000, 20, 0.03],
                        [2_000..3_000, 50, 0.04],
                      ]
-                   elsif filing_status_single? || filing_status_mfs? || filing_status_dependent?
+                   elsif filing_status_single? || filing_status_mfs? || md_filing_status_dependent?
                      [
                        [3_000..100_000, 90, 0.0475],
                        [100_000..125_000, 4_697.5, 0.05],
@@ -404,7 +404,7 @@ module Efile
 
       def calculate_line_22
         # Earned Income Credit (EIC)
-        return if filing_status_dependent?
+        return if md_filing_status_dependent?
 
         if filing_status_mfj? || filing_status_mfs? || @direct_file_data.fed_eic_qc_claimed
           (@direct_file_data.fed_eic * 0.50).round
@@ -418,7 +418,7 @@ module Efile
       end
 
       def calculate_line_23
-        return 0 if filing_status_dependent? || @lines[:MD502_LINE_1B].value <= 0 || !deduction_method_is_standard?
+        return 0 if md_filing_status_dependent? || @lines[:MD502_LINE_1B].value <= 0 || !deduction_method_is_standard?
 
         comparison_amount = [@lines[:MD502_LINE_7].value, @lines[:MD502_LINE_1B].value].max
 
@@ -481,7 +481,7 @@ module Efile
                    when "Anne Arundel"
                      anne_arundel_local_tax_brackets.find { |bracket| taxable_net_income <= bracket[:threshold] }[:rate]
                    when "Frederick"
-                     if filing_status_dependent? || filing_status_single? || filing_status_mfs?
+                     if md_filing_status_dependent? || filing_status_single? || filing_status_mfs?
                        if taxable_net_income <= 25_000
                          0.0225
                        elsif taxable_net_income <= 50_000
@@ -516,7 +516,7 @@ module Efile
       end
 
       def anne_arundel_local_tax_brackets
-        if filing_status_dependent? || filing_status_single? || filing_status_mfs?
+        if md_filing_status_dependent? || filing_status_single? || filing_status_mfs?
           [
             { threshold: 50_000, rate: 0.0270 },
             { threshold: 400_000, rate: 0.0281 },
@@ -601,7 +601,7 @@ module Efile
 
       def calculate_line_42
         # Earned Income Credit (EIC)
-        return if filing_status_dependent?
+        return if md_filing_status_dependent?
 
         if filing_status_mfj? || filing_status_mfs? || @direct_file_data.fed_eic_qc_claimed
           [(@direct_file_data.fed_eic * 0.45).round - line_or_zero(:MD502_LINE_21), 0].max
@@ -636,10 +636,6 @@ module Efile
 
       def calculate_line_50
         line_or_zero(:MD502_LINE_45) + line_or_zero(:MD502_LINE_49)
-      end
-
-      def filing_status_dependent?
-        @intake.direct_file_data.claimed_as_dependent?
       end
 
       def deduction_method_is_standard?
