b2c25e8877b229d60da135cdd60ff53973c06c86
Arin Choi <achoi@codeforamerica.org>
2025-01-30 14:26:06 -0800

    Put TotalPermDisabledIndicator under show_retirement_ui flag

--- spec/lib/submission_builder/ty2024/states/md/documents/md502_r_spec.rb
+++ spec/lib/submission_builder/ty2024/states/md/documents/md502_r_spec.rb
@@ -11,7 +11,7 @@ describe SubmissionBuilder::Ty2024::States::Md::Documents::Md502R, required_sche
       expect(build_response.errors).to be_empty
     end
 
-    describe ".document" do
+    describe "Age section" do
       let(:primary_birth_date) { Date.new(MultiTenantService.statefile.current_tax_year - 65, 1, 1) }
       let(:secondary_birth_date) { Date.new(MultiTenantService.statefile.current_tax_year - 64, 1, 1) }
 
@@ -20,50 +20,57 @@ describe SubmissionBuilder::Ty2024::States::Md::Documents::Md502R, required_sche
         intake.spouse_birth_date = secondary_birth_date
       end
 
-      it "Age section" do
+      it "should show ages of the filers" do
         expect(xml.at("Form502R PrimaryAge").text.to_i).to eq(65)
         expect(xml.at("Form502R SecondaryAge").text.to_i).to eq(64)
       end
+    end
 
-      context "show_md_ssa" do
-        before do
-          allow(Flipper).to receive(:enabled?).and_call_original
-          allow(Flipper).to receive(:enabled?).with(:show_md_ssa).and_return(true)
-        end
+    context "show_retirement_ui" do
+      before do
+        allow(Flipper).to receive(:enabled?).and_call_original
+        allow(Flipper).to receive(:enabled?).with(:show_retirement_ui).and_return(true)
+      end
 
-        context "disability" do
-          context "with disabled filers" do
-            before do
-              allow_any_instance_of(Efile::Md::Md502RCalculator).to receive(:calculate_primary_disabled).and_return "X"
-              allow_any_instance_of(Efile::Md::Md502RCalculator).to receive(:calculate_spouse_disabled).and_return "X"
-            end
-
-            it "outputs all relevant values" do
-              expect(xml.at("Form502R PriTotalPermDisabledIndicator").text).to eq("X")
-              expect(xml.at("Form502R SecTotalPermDisabledIndicator").text).to eq("X")
-            end
-          end
-
-          context "with no disabled filers" do
-            it "outputs all relevant values" do
-              expect(xml.at("Form502R PriTotalPermDisabledIndicator")).to be_nil
-              expect(xml.at("Form502R SecTotalPermDisabledIndicator")).to be_nil
-            end
-          end
-        end
-
-        context "Line 9" do
+      context "disability" do
+        context "with disabled filers" do
           before do
-            allow_any_instance_of(Efile::Md::Md502RCalculator).to receive(:calculate_line_9a).and_return 100
-            allow_any_instance_of(Efile::Md::Md502RCalculator).to receive(:calculate_line_9b).and_return 200
+            allow_any_instance_of(Efile::Md::Md502RCalculator).to receive(:calculate_primary_disabled).and_return "X"
+            allow_any_instance_of(Efile::Md::Md502RCalculator).to receive(:calculate_spouse_disabled).and_return "X"
           end
+
           it "outputs all relevant values" do
-            expect(xml.at("Form502R PriSSecurityRailRoadBenefits").text.to_i).to eq(100)
-            expect(xml.at("Form502R SecSSecurityRailRoadBenefits").text.to_i).to eq(200)
+            expect(xml.at("Form502R PriTotalPermDisabledIndicator").text).to eq("X")
+            expect(xml.at("Form502R SecTotalPermDisabledIndicator").text).to eq("X")
+          end
+        end
+
+        context "with no disabled filers" do
+          it "outputs all relevant values" do
+            expect(xml.at("Form502R PriTotalPermDisabledIndicator")).to be_nil
+            expect(xml.at("Form502R SecTotalPermDisabledIndicator")).to be_nil
           end
         end
       end
-
     end
+
+    context "show_md_ssa" do
+      before do
+        allow(Flipper).to receive(:enabled?).and_call_original
+        allow(Flipper).to receive(:enabled?).with(:show_md_ssa).and_return(true)
+      end
+
+      context "Line 9" do
+        before do
+          allow_any_instance_of(Efile::Md::Md502RCalculator).to receive(:calculate_line_9a).and_return 100
+          allow_any_instance_of(Efile::Md::Md502RCalculator).to receive(:calculate_line_9b).and_return 200
+        end
+        it "outputs all relevant values" do
+          expect(xml.at("Form502R PriSSecurityRailRoadBenefits").text.to_i).to eq(100)
+          expect(xml.at("Form502R SecSSecurityRailRoadBenefits").text.to_i).to eq(200)
+        end
+      end
+    end
+
   end
 end
