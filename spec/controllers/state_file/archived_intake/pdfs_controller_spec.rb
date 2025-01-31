6180b23bda5ca8c6db70be08155bfebbe5718c66
Hugo Melo <hmelo@codeforamerica.org>
2025-01-31 15:36:05 -0500

    Fyst -520 archived intake pdf download screen (#5399)
    * WIP - archived page without full auth
    
    Co-authored-by: Hugo Melo <hmelo@codeforamerica.org>
    
    * Add gypdf to flipper
    
    Co-authored-by: Hugo Melo <hmelo@codeforamerica.org>
    
    * Add issue pdf event
    
    Co-authored-by: Hugo Melo <hmelo@codeforamerica.org>
    
    * update annotation
    
    Co-authored-by: Hugo Melo <hmelo@codeforamerica.org>
    
    * Set expiration time per env
    
    Co-authored-by: Hugo Melo <hmelo@codeforamerica.org>
    
    * Adjust button style
    
    Co-authored-by: Hugo Melo <hmelo@codeforamerica.org>
    
    * Normalize i18n
    
    Co-authored-by: Hugo Melo <hmelo@codeforamerica.org>
    
    * Guard mailing address verified
    
    Co-authored-by: Hugo Melo <hmelo@codeforamerica.org>
    
    * Clean anotations
    
    Co-authored-by: Hugo Melo <hmelo@codeforamerica.org>
    
    * Reset db to revert annotation
    
    Co-authored-by: Hugo Melo <hmelo@codeforamerica.org>
    
    * Fix typo
    
    Co-authored-by: Hugo Melo <hmelo@codeforamerica.org>
    
    * Redirect to pdf download after verify
    
    Co-authored-by: Hugo Melo <hmelo@codeforamerica.org>
    
    * Fix controller specs
    
    Co-authored-by: Hugo Melo <hmelo@codeforamerica.org>
    
    * Fix feature spec
    
    Co-authored-by: Hugo Melo <hmelo@codeforamerica.org>
    
    * remove duplicate title
    
    Co-authored-by: Hugo Melo <hmelo@codeforamerica.org>

--- spec/controllers/state_file/archived_intake/pdfs_controller_spec.rb
+++ spec/controllers/state_file/archived_intake/pdfs_controller_spec.rb
@@ -1,10 +1,10 @@
 require "rails_helper"
 
-RSpec.describe StateFile::ArchivedIntakes::MailingAddressValidationController, type: :controller do
-  let(:intake) { create(:state_file_archived_intake, mailing_state: "NY") }
-  let(:current_request) { create(:state_file_archived_intake_request, email_address:email_address, failed_attempts: 0, state_file_archived_intake: intake) }
-  let(:controller_instance) { described_class.new }
+RSpec.describe StateFile::ArchivedIntakes::PdfsController, type: :controller do
   let(:email_address) { "test@example.com" }
+  let!(:intake) { create(:state_file_archived_intake, mailing_state: "NY", email_address: email_address) }
+  let(:current_request) { create(:state_file_archived_intake_request, email_address: email_address, failed_attempts: 0, state_file_archived_intake: intake) }
+  let(:controller_instance) { described_class.new }
   let(:valid_verification_code) { "123456" }
   let(:invalid_verification_code) { "654321" }
 
@@ -12,23 +12,76 @@ RSpec.describe StateFile::ArchivedIntakes::MailingAddressValidationController, t
     Flipper.enable(:get_your_pdf)
     allow(controller).to receive(:current_request).and_return(current_request)
     allow(I18n).to receive(:locale).and_return(:en)
+    session[:email_address] = true
     session[:code_verified] = true
     session[:ssn_verified] = true
+    session[:mailing_verified] = true
   end
 
-  describe "GET #edit" do
-    context "when the request is locked" do
+  describe "GET #index" do
+    context "request is locked" do
       before do
         allow(current_request).to receive(:access_locked?).and_return(true)
       end
 
       it "redirects to error page" do
-        get :edit
+        get :index
 
         expect(response).to redirect_to(state_file_archived_intakes_verification_error_path)
       end
     end
 
+    context "email address is not verified" do
+      before do
+        session[:code_verified] = nil
+      end
+
+      it "redirects to the email verification page" do
+        get :index
+
+        expect(response).to redirect_to(state_file_archived_intakes_verification_error_path)
+      end
+    end
+
+    context "ssn is not verified" do
+      before do
+        session[:ssn_verified] = nil
+      end
+
+      it "redirects to the ssn verification page" do
+        get :index
+
+        expect(response).to redirect_to(state_file_archived_intakes_verification_error_path)
+      end
+    end
+
+    context "mailing address is not verified" do
+      before do
+        session[:mailing_verified] = nil
+      end
+
+      it "redirects to the ssn verification page" do
+        get :index
+
+        expect(response).to redirect_to(state_file_archived_intakes_verification_error_path)
+      end
+    end
+
+    context "by default" do
+      it "renders" do
+        get :index
+
+        expect(assigns(:prior_year_intake)).to eq(intake)
+        expect(response).to render_template(:index)
+      end
+    end
+  end
+end
+
+=begin
+
+RSpec.describe StateFile::ArchivedIntakes::MailingAddressValidationController, type: :controller do
+  describe "GET #edit" do
     context "when the request is not locked" do
       before do
         allow(current_request).to receive(:access_locked?).and_return(false)
@@ -105,3 +158,4 @@ RSpec.describe StateFile::ArchivedIntakes::MailingAddressValidationController, t
     end
   end
 end
+=end
