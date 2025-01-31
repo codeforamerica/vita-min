6beeac5b1c0ee8523e54558402a268bcad894fe8
Matt Luedke <matthew@innovation.nj.gov>
2025-01-31 11:59:39 -0800

    work in progress - make ownership dynamic in terms

--- app/controllers/state_file/questions/terms_and_conditions_controller.rb
+++ app/controllers/state_file/questions/terms_and_conditions_controller.rb
@@ -2,7 +2,8 @@ module StateFile
   module Questions
     class TermsAndConditionsController < QuestionsController
       def edit
-        @li_items = I18n.t('state_file.questions.terms_and_conditions.edit.list_items_html', privacy_policy_link: state_file_privacy_policy_path).split('</li>')
+        ownership = I18n.t('general.code_for_america')
+        @li_items = I18n.t('state_file.questions.terms_and_conditions.edit.list_items_html', ownership: ownership, privacy_policy_link: state_file_privacy_policy_path).split('</li>')
         unless Flipper.enabled?(:sms_notifications)
           @li_items.each_with_index do |li_item, idx|
             if li_item.include?('SMS')
