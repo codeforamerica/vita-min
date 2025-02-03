b56d26f33cfae3865920ef6c776706093a2b800c
Tahsina Islam <tislam@codeforamerica.org>
2025-02-03 16:02:59 -0600

    Revert "Try visible false"
    This reverts commit b5fc651f3df190b4613ec7410fe05e2a088dffbb.
    
    Co-authored-by: Anisha Ramnani <aramnani@codeforamerica.org>

--- spec/features/hub/cache_control_spec.rb
+++ spec/features/hub/cache_control_spec.rb
@@ -8,18 +8,18 @@ describe "Cache control" do
   end
 
   context "user on profile page, logs out and clicks back button", js: true do
-    it "cache is cleared and redirects to the login page" do
+    xit "cache is cleared and redirects to the login page" do
       visit hub_user_profile_path
-      click_on 'Sign out', visible: false
+      click_on 'Sign out'
       page.evaluate_script('window.history.back()')
       expect(page).to have_text "Sign in"
     end
   end
 
   context "user on client page, logs out and clicks back button", js: true do
-    it "cache is cleared and redirects to the login page" do
+    xit "cache is cleared and redirects to the login page" do
       visit hub_clients_path
-      click_on 'Sign out', visible: false
+      click_on 'Sign out'
       page.evaluate_script('window.history.back()')
       expect(page).to have_text "Sign in"
     end
