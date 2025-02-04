e05bcbfb8e8b7f19933cb8fb50162f752c4b4a65
Jenny Heath <jheath@codeforamerica.org>
2025-02-04 09:27:51 -0800

    fix model spec

--- spec/models/az322_contribution_spec.rb
+++ spec/models/az322_contribution_spec.rb
@@ -21,140 +21,122 @@ require 'rails_helper'
 describe 'Az322Contribution' do
   let(:intake) { create(:state_file_az_intake) }
 
-  describe "#made_contributions" do
+  let(:az) { Az322Contribution.new(state_file_az_intake: intake) }
+
+  describe "#school_name" do
     it 'should validate presence' do
-      az = Az322Contribution.new(state_file_az_intake: intake, made_contribution: nil)
+      az.school_name = nil
       az.valid?
-      expect(az.errors[:made_contribution]).not_to be_empty
+      expect(az.errors[:school_name]).not_to be_empty
 
-      az.made_contribution = 'yes'
+      az.school_name = 'Hopscotch School'
       az.valid?
-      expect(az.errors[:made_contribution]).to be_empty
-
-      az.made_contribution = 'gobble'
-      az.valid?
-      expect(az.errors[:made_contribution]).not_to be_empty
+      expect(az.errors[:school_name]).to be_empty
     end
   end
 
-  context "made_contributions is true" do
-    let(:az) { Az322Contribution.new(state_file_az_intake: intake, made_contribution: 'yes') }
+  describe "#ctds_code" do
+    it 'should validate presence' do
+      az.ctds_code = nil
+      az.valid?
+      expect(az.errors[:ctds_code]).not_to be_empty
 
-    describe "#school_name" do
-      it 'should validate presence' do
-        az.school_name = nil
-        az.valid?
-        expect(az.errors[:school_name]).not_to be_empty
+      az.ctds_code = '123456789'
+      az.valid?
+      expect(az.errors[:ctds_code]).to be_empty
 
-        az.school_name = 'Hopscotch School'
-        az.valid?
-        expect(az.errors[:school_name]).to be_empty
-      end
+      az.ctds_code = '12345678'
+      az.valid?
+      expect(az.errors[:ctds_code]).not_to be_empty
+
+      az.ctds_code = 'fffffffff'
+      az.valid?
+      expect(az.errors[:ctds_code]).not_to be_empty
+    end
+  end
+
+  describe "#district_name" do
+    it 'should validate presence' do
+      az.district_name = nil
+      az.valid?
+      expect(az.errors[:district_name]).not_to be_empty
+
+      az.district_name = 'Sesame District'
+      az.valid?
+      expect(az.errors[:district_name]).to be_empty
+    end
+  end
+
+  describe "#amount" do
+    it 'should validate presence' do
+      az.amount = nil
+      az.valid?
+      expect(az.errors[:amount]).to eq(["Can't be blank.", "is not a number"])
+
+      az.amount = 0
+      az.valid?
+      expect(az.errors[:amount]).to eq(["must be greater than 0"])
+
+      az.amount = 1
+      az.valid?
+      expect(az.errors[:amount]).to be_empty
+      expect(az.amount).to eq(1)
+    end
+  end
+
+  describe '#date_of_contribution' do
+    it 'should be valid in the current tax year' do
+      az.date_of_contribution_year = Rails.configuration.statefile_current_tax_year
+
+      az.valid?
+
+      expect(az.errors[:date_of_contribution]).to be_empty
     end
 
-    describe "#ctds_code" do
-      it 'should validate presence' do
-        az.ctds_code = nil
-        az.valid?
-        expect(az.errors[:ctds_code]).not_to be_empty
+    it 'should be invalid in the previous year' do
+      az.date_of_contribution_year = Rails.configuration.statefile_current_tax_year - 1
 
-        az.ctds_code = '123456789'
-        az.valid?
-        expect(az.errors[:ctds_code]).to be_empty
+      az.valid?
 
-        az.ctds_code = '12345678'
-        az.valid?
-        expect(az.errors[:ctds_code]).not_to be_empty
-
-        az.ctds_code = 'fffffffff'
-        az.valid?
-        expect(az.errors[:ctds_code]).not_to be_empty
-      end
+      expect(az.errors[:date_of_contribution]).not_to be_empty
     end
 
-    describe "#district_name" do
-      it 'should validate presence' do
-        az.district_name = nil
-        az.valid?
-        expect(az.errors[:district_name]).not_to be_empty
+    it 'should be invalid in the next year' do
+      az.date_of_contribution_year = Rails.configuration.statefile_current_tax_year + 1
 
-        az.district_name = 'Sesame District'
-        az.valid?
-        expect(az.errors[:district_name]).to be_empty
-      end
+      az.valid?
+
+      expect(az.errors[:date_of_contribution]).not_to be_empty
     end
 
-    describe "#amount" do
-      it 'should validate presence' do
-        az.amount = nil
-        az.valid?
-        expect(az.errors[:amount]).to eq(["Can't be blank.", "is not a number"])
+    it 'should be valid when a correct date is provided' do
+      current_tax_year = Rails.configuration.statefile_current_tax_year
 
-        az.amount = 0
-        az.valid?
-        expect(az.errors[:amount]).to eq(["must be greater than 0"])
+      az.date_of_contribution_year = current_tax_year
+      az.date_of_contribution_day = 12
+      az.date_of_contribution_month = 5
 
-        az.amount = 1
-        az.valid?
-        expect(az.errors[:amount]).to be_empty
-        expect(az.amount).to eq(1)
-      end
+      az.valid?
+
+      expect(az.errors[:date_of_contribution]).to be_empty
+
+      az.restore_attributes
+
+      expect(az.date_of_contribution).to be_nil
+
+      az.date_of_contribution = "#{current_tax_year}-05-12"
+
+      az.valid?
+
+      expect(az.errors[:date_of_contribution]).to be_empty
     end
 
-    describe '#date_of_contribution' do
-      it 'should be valid in the current tax year' do
-        az.date_of_contribution_year = Rails.configuration.statefile_current_tax_year
+    it 'should be invalid when nonsense is provided' do
+      az.date_of_contribution = "foo"
 
-        az.valid?
+      az.valid?
 
-        expect(az.errors[:date_of_contribution]).to be_empty
-      end
-
-      it 'should be invalid in the previous year' do
-        az.date_of_contribution_year = Rails.configuration.statefile_current_tax_year - 1
-
-        az.valid?
-
-        expect(az.errors[:date_of_contribution]).not_to be_empty
-      end
-
-      it 'should be invalid in the next year' do
-        az.date_of_contribution_year = Rails.configuration.statefile_current_tax_year + 1
-
-        az.valid?
-
-        expect(az.errors[:date_of_contribution]).not_to be_empty
-      end
-
-      it 'should be valid when a correct date is provided' do
-        current_tax_year = Rails.configuration.statefile_current_tax_year
-
-        az.date_of_contribution_year = current_tax_year
-        az.date_of_contribution_day = 12
-        az.date_of_contribution_month = 5
-
-        az.valid?
-
-        expect(az.errors[:date_of_contribution]).to be_empty
-
-        az.restore_attributes
-
-        expect(az.date_of_contribution).to be_nil
-
-        az.date_of_contribution = "#{current_tax_year}-05-12"
-
-        az.valid?
-
-        expect(az.errors[:date_of_contribution]).to be_empty
-      end
-
-      it 'should be invalid when nonsense is provided' do
-        az.date_of_contribution = "foo"
-
-        az.valid?
-
-        expect(az.errors[:date_of_contribution]).not_to be_empty
-      end
+      expect(az.errors[:date_of_contribution]).not_to be_empty
     end
   end
 end
