f322262d7ca45d92ce134f85ce2af6310516a70b
Matt Luedke <matthew@innovation.nj.gov>
2025-02-04 12:32:28 -0800

    vary ownership string when in nj

--- app/controllers/state_file/questions/terms_and_conditions_controller.rb
+++ app/controllers/state_file/questions/terms_and_conditions_controller.rb
@@ -2,7 +2,9 @@ module StateFile
   module Questions
     class TermsAndConditionsController < QuestionsController
       def edit
-        ownership = I18n.t('general.code_for_america')
+        ownership = current_state_code == "nj" ?
+          "#{I18n.t('general.code_for_america')} #{I18n.t('general.and')} #{I18n.t('general.nj_ooi')}" :
+          I18n.t('general.code_for_america')
         @li_items = I18n.t('state_file.questions.terms_and_conditions.edit.list_items_html', ownership: ownership, privacy_policy_link: state_file_privacy_policy_path).split('</li>')
         unless Flipper.enabled?(:sms_notifications)
           @li_items.each_with_index do |li_item, idx|
