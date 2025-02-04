c244b7c6dd0bf95cab28462639ffc87ac1dca28b
Mike Rotondo <mrotondo@codeforamerica.org>
2025-02-03 18:40:00 -0800

    big viewport

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -176,7 +176,7 @@ RSpec.configure do |config|
   end
 
   config.before(type: :feature) do |example|
-    # Capybara.page.current_window.resize_to(2000, 4000)
+    Capybara.page.current_window.resize_to(2000, 4000)
 
     if config.filter.rules[:flow_explorer_screenshot]
       example.metadata[:js] = true
