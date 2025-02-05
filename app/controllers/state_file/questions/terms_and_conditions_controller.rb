886debc2883c4fdc3686e7693ff99ee9e2d260b2
Matt Luedke <matthew@innovation.nj.gov>
2025-02-05 09:49:56 -0800

    use scan instead of split to keep proper HTML

--- app/controllers/state_file/questions/terms_and_conditions_controller.rb
+++ app/controllers/state_file/questions/terms_and_conditions_controller.rb
@@ -5,7 +5,11 @@ module StateFile
         ownership = current_state_code == "nj" ?
           "#{I18n.t('general.code_for_america')} #{I18n.t('general.and')} #{I18n.t('general.nj_ooi')}" :
           I18n.t('general.code_for_america')
-        @li_items = I18n.t('state_file.questions.terms_and_conditions.edit.list_items_html', ownership: ownership, privacy_policy_link: state_file_privacy_policy_path).split('</li>')
+          @li_items = I18n.t(
+            'state_file.questions.terms_and_conditions.edit.list_items_html',
+            ownership: ownership,
+            privacy_policy_link: state_file_privacy_policy_path
+          ).scan(/<li>.*?<\/li>/m) # elements of resulting array include <li> and </li> tags
         unless Flipper.enabled?(:sms_notifications)
           @li_items.each_with_index do |li_item, idx|
             if li_item.include?('SMS')
