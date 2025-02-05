ba3f79067c87372ff495fc604f969cbb25fc5e0e
Anne LoVerso <anne.loverso@gmail.com>
2025-02-05 14:47:32 -0500

    NJ 79 - add page to select incoome source for 1099-R retirement income

---
+++ spec/controllers/state_file/questions/nj_retirement_income_source_controller_spec.rb
@@ -0,0 +1,134 @@
+require "rails_helper"
+
+RSpec.describe StateFile::Questions::NjRetirementIncomeSourceController do
+  let(:intake) { create :state_file_nj_intake }
+  before do
+    sign_in intake
+  end
+
+  describe "#show?" do
+    context "when they have no 1099Rs in their DF XML" do
+      let(:intake) { create :state_file_nj_intake, :df_data_2_w2s }
+      it "does not show" do
+        expect(described_class.show?(intake)).to eq false
+      end
+    end
+
+    context "when they have at least one 1099R in their DF XML" do
+      let(:intake) { create :state_file_nj_intake, :df_data_2_1099r }
+      it "shows" do
+        expect(described_class.show?(intake)).to eq true
+      end
+    end
+  end
+
+  describe "#edit" do
+    let(:intake) { create :state_file_nj_intake, :df_data_2_1099r }
+    render_views
+
+    it 'succeeds' do
+      get :edit
+      expect(response).to be_successful
+    end
+
+    context "when an index is not provided" do
+      it "renders the data for the first 1099R" do
+        get :edit
+        expect(response.body).to include("Payer Name")
+        expect(response.body).to include("$1,000")
+        expect(response.body).to include("Zeus Thunder")
+
+        expect(response.body).not_to include("Payer 2 Name")
+        expect(response.body).not_to include("$3,000")
+        expect(response.body).not_to include("Hera Thunder")
+      end
+    end
+
+    context "when an index param is provided" do
+      it "renders the data for the 1099R at that index" do
+        get :edit, params: { index: 1 }
+        expect(response.body).to include("Payer 2 Name")
+        expect(response.body).to include("$3,000")
+        expect(response.body).to include("Hera Thunder")
+
+        expect(response.body).not_to include("Payer Name")
+        expect(response.body).not_to include("$1,000")
+      end
+    end
+  end
+
+  describe "#next_path" do
+    let(:intake) { create :state_file_nj_intake, :df_data_2_1099r }
+
+    context 'when not return_to_review' do
+      context "when there are additional 1099Rs to view" do
+        it "next path is NjRetirementIncomeSourceController with new index set" do
+          post :update
+          expect(subject.next_path).to eq("/en/questions/nj-retirement-income-source?index=1")
+        end
+      end
+
+      context "when there are no additional 1099Rs to view" do
+        it "next path is whichever is next overall" do
+          post :update, params: {index: "1"}
+          allow_any_instance_of(described_class.superclass).to receive(:next_path).and_return("/mocked/super/path")
+          expect(subject.next_path).to eq("/mocked/super/path")
+        end
+      end
+    end
+
+    context 'when return_to_review' do
+      context "when there are additional 1099Rs to view" do
+        it "next path is NjRetirementIncomeSourceController with new index set and review param" do
+          post :update, params: { return_to_review: "y" }
+          expect(subject.next_path).to eq("/en/questions/nj-retirement-income-source?index=1&return_to_review=y")
+        end
+      end
+
+      context "when there are no additional 1099Rs to view" do
+        it "next path is Review Controller" do
+          post :update, params: {index: "1", return_to_review: "y"}
+          expect(subject.next_path).to eq("/en/questions/#{intake.state_code}-review")
+        end
+      end
+    end
+  end
+
+  describe "#prev_path" do
+    let(:intake) { create :state_file_nj_intake, :df_data_2_1099r }
+
+    context 'when not return_to_review' do
+      context "when there are previous 1099Rs to view" do
+        it "prev path is NjRetirementIncomeSourceController with prev index set" do
+          get :edit, params: { index: "1" }
+          expect(subject.prev_path).to eq("/en/questions/nj-retirement-income-source?index=0")
+        end
+      end
+
+      context "when there are no previous 1099Rs to view" do
+        it "prev path is whichever is previous overall" do
+          get :edit
+          allow_any_instance_of(described_class.superclass).to receive(:prev_path).and_return("/mocked/super/path")
+          expect(subject.prev_path).to eq("/mocked/super/path")
+        end
+      end
+    end
+
+    context 'when return_to_review' do
+      context "when there are previous 1099Rs to view" do
+        it "prev path is NjRetirementIncomeSourceController with prev index and return to review set" do
+          get :edit, params: { index: "1", return_to_review: "y" }
+          expect(subject.prev_path).to eq("/en/questions/nj-retirement-income-source?index=0&return_to_review=y")
+        end
+      end
+
+      context "when there are no previous 1099Rs to view" do
+        it "prev path is review screen" do
+          get :edit, params: { return_to_review: "y" }
+          expect(subject.prev_path).to eq("/en/questions/nj-review")
+        end
+      end
+    end
+  end
+end
+
---
+++ spec/forms/state_file/nj_retirement_income_source_form_spec.rb
@@ -0,0 +1,44 @@
+require "rails_helper"
+
+RSpec.describe StateFile::NjRetirementIncomeSourceForm do
+  let(:intake) { create(:state_file_nj_intake) }
+  let(:state_file_1099r) { create :state_file1099_r, intake: intake }
+  let(:state_specific_followup) { create :state_file_nj1099_r_followup, state_file1099_r: state_file_1099r }
+  let(:form) { described_class.new(state_specific_followup, form_params) }
+
+  describe "validations" do
+    context "income_source is required" do
+      let(:form_params) do
+        { income_source: nil }
+      end
+
+      it "is invalid" do
+        expect(form.valid?).to eq false
+        expect(form.errors[:income_source]).to include "Can't be blank."
+      end
+    end
+
+    context "when income_source is present" do
+      let(:form_params) do
+        { income_source: :military_pension }
+      end
+
+      it "is valid" do
+        expect(form.valid?).to eq true
+      end
+    end
+  end
+
+  describe ".save" do
+    context "when saving a new county" do
+      let(:form_params) do
+        { income_source: :military_pension }
+      end
+
+      it "saves attributes" do
+        form.save
+        expect(intake.state_file1099_rs[0].state_specific_followup.income_source).to eq "military_pension"
+      end
+    end
+  end
+end
