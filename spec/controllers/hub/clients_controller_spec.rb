51d707b0b42981a1819d0a1a624c34942b497e4f
spompea-cfa <spompea@codeforamerica.org>
2025-02-05 21:58:44 -0500

    spec test updates

--- spec/controllers/hub/clients_controller_spec.rb
+++ spec/controllers/hub/clients_controller_spec.rb
@@ -1693,10 +1693,12 @@ RSpec.describe Hub::ClientsController do
             state: intake.state,
             zip_code: intake.zip_code,
             primary_us_citizen: "unfilled",
+            primary_visa: "unfilled",
             spouse_first_name: intake.spouse.first_name,
             spouse_last_name: intake.spouse.last_name,
             spouse_email_address: intake.spouse_email_address,
             spouse_us_citizen: "unfilled",
+            spouse_visa: "yes",
             preferred_written_language: "Greek",
             receive_written_communication: intake.receive_written_communication,
             refund_payment_method: intake.refund_payment_method,
@@ -1753,6 +1755,7 @@ RSpec.describe Hub::ClientsController do
           expect(system_note.data['changes']).to match({ "preferred_written_language" => [intake.preferred_written_language, "Greek"],
                                                          "primary_last_name" => [intake.primary.last_name, "Name"],
                                                          "primary_first_name" => [intake.primary.first_name, "Updated"],
+                                                         "spouse_visa" => ["unfilled", "yes"],
                                                        })
 
           expect(client.last_13614c_update_at).to be_within(1.second).of(DateTime.now)
