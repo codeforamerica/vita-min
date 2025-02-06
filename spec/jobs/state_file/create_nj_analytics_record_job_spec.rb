90801c6cac4abf10e124e90dbdce5ed6cbc35f15
John Chan <john@innovation.nj.gov>
2025-02-06 11:44:11 -0500

    Added additional tests

--- spec/jobs/state_file/create_nj_analytics_record_job_spec.rb
+++ spec/jobs/state_file/create_nj_analytics_record_job_spec.rb
@@ -5,7 +5,7 @@ describe StateFile::CreateNjAnalyticsRecordJob do
     let!(:submitted_intake) { create :state_file_nj_intake, email_address: 'test+01@example.com', email_address_verified_at: 1.minute.ago }
     let!(:submission) { create :efile_submission, :for_state, data_source: submitted_intake }
 
-    it "updates the NJ analytics record attached to intake" do
+    it "populates the NJ analytics record attached to intake" do
       expect(submitted_intake.state_file_nj_analytics).not_to be_present
       described_class.perform_now(submission.id)
       submitted_intake.reload
