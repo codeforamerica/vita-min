71832bc1bc44c19750d2a7fa55bb430e10ac47e7
spompea-cfa <spompea@codeforamerica.org>
2025-02-05 20:24:58 -0500

    Add new refund_other_cb col + set up revised refund_other bits etc

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_02_05_160115) do
+ActiveRecord::Schema[7.1].define(version: 2025_02_06_005534) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
   enable_extension "plpgsql"
@@ -1421,6 +1421,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_02_05_160115) do
     t.integer "refund_check_by_mail"
     t.integer "refund_direct_deposit"
     t.string "refund_other"
+    t.integer "refund_other_cb", default: 0, null: false
     t.integer "refund_payment_method", default: 0, null: false
     t.integer "register_to_vote", default: 0, null: false
     t.integer "reported_asset_sale_loss", default: 0, null: false
