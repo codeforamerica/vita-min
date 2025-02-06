d5a5897d9952a4998dfed0f081c5790714f71723
John Chan <john@innovation.nj.gov>
2025-02-06 17:04:20 -0500

    recreated DB and added schema files

--- db/schema.rb
+++ db/schema.rb
@@ -2276,26 +2276,6 @@ ActiveRecord::Schema[7.1].define(version: 2025_02_05_153246) do
   end
 
   create_table "state_file_nj_intakes", force: :cascade do |t|
-    t.integer "NJ1040_LINE_12_COUNT", default: 0, null: false
-    t.integer "NJ1040_LINE_15", default: 0, null: false
-    t.integer "NJ1040_LINE_16A", default: 0, null: false
-    t.integer "NJ1040_LINE_16B", default: 0, null: false
-    t.integer "NJ1040_LINE_29", default: 0, null: false
-    t.integer "NJ1040_LINE_31", default: 0, null: false
-    t.integer "NJ1040_LINE_41", default: 0, null: false
-    t.integer "NJ1040_LINE_42", default: 0, null: false
-    t.integer "NJ1040_LINE_43", default: 0, null: false
-    t.integer "NJ1040_LINE_51", default: 0, null: false
-    t.integer "NJ1040_LINE_56", default: 0, null: false
-    t.integer "NJ1040_LINE_58", default: 0, null: false
-    t.integer "NJ1040_LINE_58_IRS", default: 0, null: false
-    t.integer "NJ1040_LINE_59", default: 0, null: false
-    t.integer "NJ1040_LINE_61", default: 0, null: false
-    t.integer "NJ1040_LINE_64", default: 0, null: false
-    t.integer "NJ1040_LINE_65", default: 0, null: false
-    t.integer "NJ1040_LINE_65_DEPENDENTS", default: 0, null: false
-    t.integer "NJ1040_LINE_7_SELF", default: 0, null: false
-    t.integer "NJ1040_LINE_7_SPOUSE", default: 0, null: false
     t.string "account_number"
     t.integer "account_type", default: 0, null: false
     t.string "bank_name"
@@ -2980,6 +2960,8 @@ ActiveRecord::Schema[7.1].define(version: 2025_02_05_153246) do
   add_foreign_key "incoming_text_messages", "clients"
   add_foreign_key "intake_archives", "intakes", column: "id"
   add_foreign_key "intakes", "clients"
+  add_foreign_key "intakes", "drivers_licenses", column: "primary_drivers_license_id"
+  add_foreign_key "intakes", "drivers_licenses", column: "spouse_drivers_license_id"
   add_foreign_key "intakes", "intakes", column: "matching_previous_year_intake_id"
   add_foreign_key "intakes", "vita_partners"
   add_foreign_key "notes", "clients"
