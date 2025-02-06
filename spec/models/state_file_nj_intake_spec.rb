7ff21cf2a34eb45e010b3be70cf41f40c885f292
John Chan <john@innovation.nj.gov>
2025-02-06 17:04:20 -0500

    recreated DB and added schema files

--- spec/models/state_file_nj_intake_spec.rb
+++ spec/models/state_file_nj_intake_spec.rb
@@ -3,26 +3,6 @@
 # Table name: state_file_nj_intakes
 #
 #  id                                                     :bigint           not null, primary key
-#  NJ1040_LINE_12_COUNT                                   :integer          default(0), not null
-#  NJ1040_LINE_15                                         :integer          default(0), not null
-#  NJ1040_LINE_16A                                        :integer          default(0), not null
-#  NJ1040_LINE_16B                                        :integer          default(0), not null
-#  NJ1040_LINE_29                                         :integer          default(0), not null
-#  NJ1040_LINE_31                                         :integer          default(0), not null
-#  NJ1040_LINE_41                                         :integer          default(0), not null
-#  NJ1040_LINE_42                                         :integer          default(0), not null
-#  NJ1040_LINE_43                                         :integer          default(0), not null
-#  NJ1040_LINE_51                                         :integer          default(0), not null
-#  NJ1040_LINE_56                                         :integer          default(0), not null
-#  NJ1040_LINE_58                                         :integer          default(0), not null
-#  NJ1040_LINE_58_IRS                                     :integer          default(0), not null
-#  NJ1040_LINE_59                                         :integer          default(0), not null
-#  NJ1040_LINE_61                                         :integer          default(0), not null
-#  NJ1040_LINE_64                                         :integer          default(0), not null
-#  NJ1040_LINE_65                                         :integer          default(0), not null
-#  NJ1040_LINE_65_DEPENDENTS                              :integer          default(0), not null
-#  NJ1040_LINE_7_SELF                                     :integer          default(0), not null
-#  NJ1040_LINE_7_SPOUSE                                   :integer          default(0), not null
 #  account_number                                         :string
 #  account_type                                           :integer          default("unfilled"), not null
 #  claimed_as_dep                                         :integer
