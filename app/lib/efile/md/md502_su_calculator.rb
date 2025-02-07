9476749a947519a0e4f31d0038dc3c57c03a214e
Anisha Ramnani <anisha.ramnani@gmail.com>
2025-02-07 17:44:03 -0500

    [FYST-818] MD Military Subtraction (#5512)
    * Add Military Subtraction to MD502SU 
    * Add :with_spouse attribute for other states as well for more accurate testing data
    ---------
    
    Co-authored-by: Jenny Heath <jheath@codeforamerica.org>
    Co-authored-by: Arin Choi <achoi@codeforamerica.org>

--- app/lib/efile/md/md502_su_calculator.rb
+++ app/lib/efile/md/md502_su_calculator.rb
@@ -3,22 +3,37 @@ module Efile
     class Md502SuCalculator < ::Efile::TaxCalculator
       attr_reader :lines, :value_access_tracker
 
+      CALCULATED_FIELDS_AND_CODE_LETTERS = { MD502_SU_LINE_AB: "AB", MD502_SU_LINE_U: "U" }
+
       def initialize(value_access_tracker:, lines:, intake:)
         @value_access_tracker = value_access_tracker
         @lines = lines
-        intake = intake
+        @intake = intake
         @direct_file_json_data = intake.direct_file_json_data
       end
 
       def calculate
         set_line(:MD502_SU_LINE_AB, :calculate_line_ab)
-        set_line(:MD502_SU_LINE_U, :calculate_line_u) # STUBBED: PLEASE REPLACE, don't forget line_data.yml
+        set_line(:MD502_SU_LINE_U_PRIMARY, :calculate_line_u_primary)
+        set_line(:MD502_SU_LINE_U_SPOUSE, :calculate_line_u_spouse)
+        set_line(:MD502_SU_LINE_U, :calculate_line_u)
+        set_line(:MD502_SU_LINE_V_PRIMARY, :calculate_line_v_primary) # STUBBED: PLEASE REPLACE, don't forget line_data.yml
+        set_line(:MD502_SU_LINE_V_SPOUSE, :calculate_line_v_spouse) # STUBBED: PLEASE REPLACE, don't forget line_data.yml
         set_line(:MD502_SU_LINE_V, :calculate_line_v) # STUBBED: PLEASE REPLACE, don't forget line_data.yml
         set_line(:MD502_SU_LINE_1, :calculate_line_1)
-        set_line(:MD502_SU_LINE_U_PRIMARY, :calculate_line_u_primary) # STUBBED: PLEASE REPLACE, don't forget line_data.yml
-        set_line(:MD502_SU_LINE_V_PRIMARY, :calculate_line_v_primary) # STUBBED: PLEASE REPLACE, don't forget line_data.yml
-        set_line(:MD502_SU_LINE_U_SPOUSE, :calculate_line_u_spouse) # STUBBED: PLEASE REPLACE, don't forget line_data.yml
-        set_line(:MD502_SU_LINE_V_SPOUSE, :calculate_line_v_spouse) # STUBBED: PLEASE REPLACE, don't forget line_data.yml
+      end
+
+      def calculate_military_per_filer(filer)
+        age_benefits = @intake.is_filer_55_and_older?(filer) ? 20_000 : 12_500
+        [@intake.sum_1099_r_followup_type_for_filer(:primary, :service_type_military?), age_benefits].min
+      end
+
+      def calculate_line_u_primary
+        calculate_military_per_filer(:primary)
+      end
+
+      def calculate_line_u_spouse
+        @intake.filing_status_mfj? ? calculate_military_per_filer(:spouse) : 0
       end
 
       private
@@ -27,15 +42,13 @@ module Efile
         @direct_file_json_data.interest_reports.sum(&:interest_on_government_bonds).round
       end
 
-      def calculate_line_u_primary; end
+      def calculate_line_u
+        line_or_zero(:MD502_SU_LINE_U_PRIMARY) + line_or_zero(:MD502_SU_LINE_U_SPOUSE)
+      end
 
       def calculate_line_v_primary; end
 
-      def calculate_line_u_spouse; end
-
       def calculate_line_v_spouse; end
-
-      def calculate_line_u; end
 
       def calculate_line_v; end
 
