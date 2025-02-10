e84132836608da22ec49a658370aca3eb3cb277d
Tahsina Islam <tislam@codeforamerica.org>
2025-02-10 13:34:30 -0600

    Fix logic for controller
    Co-authored-by: Anisha Ramnani <aramnani@codeforamerica.org>

--- app/controllers/state_file/questions/id_disability_controller.rb
+++ app/controllers/state_file/questions/id_disability_controller.rb
@@ -2,7 +2,8 @@ module StateFile
   module Questions
     class IdDisabilityController < QuestionsController
       def self.show?(intake)
-        false
+        current_intake.state_file1099_rs.any? { |form1099r| form1099r.taxable_amount&.to_f&.positive? } &&
+          current_intake.primary_senior? && !current_intake.filing_status_mfs?
       end
     end
   end
