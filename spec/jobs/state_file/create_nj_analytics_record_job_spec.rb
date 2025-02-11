1158a5c86b82e8fbbd9f7bdb8ba0298a8e960d94
John Chan <john@innovation.nj.gov>
2025-02-11 09:52:03 -0500

    Added test for multiple submissions on one intake

--- spec/jobs/state_file/create_nj_analytics_record_job_spec.rb
+++ spec/jobs/state_file/create_nj_analytics_record_job_spec.rb
@@ -11,5 +11,16 @@ describe StateFile::CreateNjAnalyticsRecordJob do
       submitted_intake.reload
       expect(submitted_intake.state_file_nj_analytics).to be_present
     end
+
+    context "when multiple submissions exist" do
+      let!(:submission_2) { create :efile_submission, :for_state, data_source: submitted_intake }
+      it "allows multiple NJ analytics records to be attached to one intake" do
+        expect(submitted_intake.state_file_nj_analytics).not_to be_present
+        described_class.perform_now(submission.id)
+        submitted_intake.reload
+        described_class.perform_now(submission_2.id)
+        expect(submitted_intake.state_file_nj_analytics.size).to eq 2
+      end
+    end
   end
 end
