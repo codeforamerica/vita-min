6eb9dd16ad04369ada0f002b6dda651c6131c743
Jenny Heath <jheath@codeforamerica.org>
2025-02-11 15:59:31 -0800

    get rid of environment distinction bc code doesn't support multiple logged-in intakes

--- spec/support/shared_examples/start_intake_concern.rb
+++ spec/support/shared_examples/start_intake_concern.rb
@@ -20,49 +20,39 @@ shared_examples :start_intake_concern do |intake_class:|
     end
 
     context "with existing intakes in the session" do
-      let(:existing_intake) do
-        create(intake_class.name.underscore)
-      end
-      let(:existing_oos_intake) do
-        intake_classes = StateFile::StateInformationService.state_intake_classes.excluding(StateFileNyIntake)
-        other_intake_class_index = intake_classes.find_index(intake_class) - 1
-        other_intake_class = intake_classes[other_intake_class_index]
-        create(other_intake_class.name.underscore)
-      end
-
       before do
         sign_in existing_intake
-        sign_in existing_oos_intake
       end
 
-      context "in production" do
-        before do
-          allow(Rails).to receive(:env).and_return("production".inquiry)
+      context "with an in-state intake" do
+        let(:existing_intake) do
+          create(intake_class.name.underscore)
         end
 
-        it "replaces the existing in-state intake and does not clear the out of state intakes" do
+        it "replaces the existing in-state intake and clears other intakes" do
           post :update, params: valid_params
           logged_in_intakes = StateFile::StateInformationService.state_intake_classes.map do |klass|
             subject.send("current_#{klass.name.underscore}")
           end.compact
           expect(logged_in_intakes).not_to include existing_intake
-          expect(logged_in_intakes).to include existing_oos_intake
           expect(logged_in_intakes).to include intake_class.send(:last)
         end
       end
 
-      context "in any other environment" do
-        before do
-          allow(Rails).to receive(:env).and_return("demo".inquiry)
+      context "with an out-of-state intake" do
+        let(:existing_intake) do
+          intake_classes = StateFile::StateInformationService.state_intake_classes.excluding(StateFileNyIntake)
+          other_intake_class_index = intake_classes.find_index(intake_class) - 1
+          other_intake_class = intake_classes[other_intake_class_index]
+          create(other_intake_class.name.underscore)
         end
 
-        it "replaces the existing in-state intake and clears the out of state intakes" do
+        it "replaces the existing in-state intake and clears other intakes" do
           post :update, params: valid_params
           logged_in_intakes = StateFile::StateInformationService.state_intake_classes.map do |klass|
             subject.send("current_#{klass.name.underscore}")
           end.compact
           expect(logged_in_intakes).not_to include existing_intake
-          expect(logged_in_intakes).not_to include existing_oos_intake
           expect(logged_in_intakes).to include intake_class.send(:last)
         end
       end
