f48ba1b5033324aa207122e0268c45575c3f8593
Arin Choi <achoi@codeforamerica.org>
2025-02-12 15:08:49 -0800

    Add test for 29a pdf filling

--- spec/lib/pdf_filler/az140_pdf_spec.rb
+++ spec/lib/pdf_filler/az140_pdf_spec.rb
@@ -33,6 +33,16 @@ RSpec.describe PdfFiller::Az140Pdf do
       end
     end
 
+    context "with retirement income from qualified pensions" do
+      before do
+        allow_any_instance_of(Efile::Az::Az140Calculator).to receive(:calculate_line_29a).and_return 102
+      end
+
+      it "fills the fields correctly" do
+        expect(pdf.hash_for_pdf["29a"]).to eq "102"
+      end
+    end
+
     context 'Nonrefundable Credits from Arizona Form 301, Part 2, line 62' do
       before do
         allow_any_instance_of(Efile::Az::Az301Calculator).to receive(:calculate_line_60).and_return 100
