11de38b98ea53a81ff847fe29e07ae72b893890c
Tahsina Islam <tislam@codeforamerica.org>
2025-02-13 09:52:02 -0600

    Fix form logic
    Co-authored-by: Anisha Ramnani <aramnani@codeforamerica.org>

--- app/controllers/state_file/questions/id_retirement_and_pension_income_controller.rb
+++ app/controllers/state_file/questions/id_retirement_and_pension_income_controller.rb
@@ -1,6 +1,42 @@
 module StateFile
   module Questions
     class IdRetirementAndPensionIncomeController < RetirementIncomeSubtractionController
+
+      def self.show?(intake)
+        Flipper.enabled?(:show_retirement_ui) &&
+          intake.state_file1099_rs.any? { |form1099r| form1099r.taxable_amount&.to_f&.positive? } &&
+          !intake.filing_status_mfs?
+      end
+
+      def person_qualifies?(form1099r)
+        if form1099r.recipient_ssn == current_intake.direct_file_json_data.primary_filer
+          current_intake.primary_disabled_yes? || current_intake.primary_senior?
+        elsif form1099r.recipient_ssn == current_intake.direct_file_json_data.spouse_filer
+          current_intake.spouse_disabled_yes? || current_intake.spouse_senior?
+        else
+          false
+        end
+      end
+
+      def num_items
+        current_intake.state_file1099_rs.count { |form1099r|
+          form1099r.taxable_amount&.to_f&.positive? &&
+            person_qualifies?(form1099r)
+        }
+      end
+
+      def load_item(index)
+        eligible_1099rs = current_intake.state_file1099_rs.select { |form1099r|
+          form1099r.taxable_amount&.to_f&.positive? &&
+            person_qualifies?(form1099r)
+        }
+        @state_file_1099r = eligible_1099rs[index]
+
+        if @state_file_1099r.nil?
+          render "public_pages/page_not_found", status: 404
+        end
+      end
+
       def followup_class = StateFileId1099RFollowup
     end
   end
--- app/forms/state_file/id_disability_form.rb
+++ app/forms/state_file/id_disability_form.rb
@@ -8,22 +8,51 @@ module StateFile
 
     def save
       if intake.filing_status_mfj?
+        primary_eligible = eligible?(:primary)
+        spouse_eligible = eligible?(:spouse)
+
         case mfj_disability
         when "me"
-          @intake.update(primary_disabled: "yes", spouse_disabled: "no")
+          @intake.update(primary_disabled: primary_eligible ? "yes" : "no", spouse_disabled: "no")
         when "spouse"
-          @intake.update(primary_disabled: "no", spouse_disabled: "yes")
+          @intake.update(primary_disabled: "no", spouse_disabled: spouse_eligible ? "yes" : "no")
         when "both"
-          @intake.update(primary_disabled: "yes", spouse_disabled: "yes")
+          @intake.update(
+            primary_disabled: primary_eligible ? "yes" : "no",
+            spouse_disabled: spouse_eligible ? "yes" : "no"
+          )
         when "none"
           @intake.update(primary_disabled: "no", spouse_disabled: "no")
         end
       else
-        @intake.update(attributes_for(:intake))
+        @intake.update(
+          primary_disabled: eligible?(:primary) ? primary_disabled : "no",
+          spouse_disabled: "no"
+        )
       end
     end
 
     private
+
+    def eligible?(person)
+      age = intake.calculate_age(
+        person == :primary ? intake.primary_birth_date : intake.spouse_birth_date,
+        inclusive_of_jan_1: true
+      )
+
+      ssn = if person == :primary
+              intake.direct_file_json_data.primary_filer
+            else
+              intake.direct_file_json_data.spouse_filer
+            end
+
+      age_eligible = age >= 62 && age < 65
+      has_taxable_1099r = current_intake.forms_1099r.any? do |form|
+        form.recipient_ssn == ssn && form.taxable_amount&.to_f&.positive?
+      end
+
+      age_eligible && has_taxable_1099r
+    end
 
     def disability_selected?
       mfj_disability.in?(%w[me spouse both]) || primary_disabled == "yes"
