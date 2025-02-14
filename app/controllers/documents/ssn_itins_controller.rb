c19fcd201e31ecbe405202c475e3393dbc52f1c0
spompea-cfa <spompea@codeforamerica.org>
2025-02-14 18:16:33 -0500

    rework state transition logic around flow/docs

--- app/controllers/documents/ssn_itins_controller.rb
+++ app/controllers/documents/ssn_itins_controller.rb
@@ -24,12 +24,7 @@ module Documents
       end
     end
 
-    def after_update_success
-      transition_to = has_all_required_docs? ? :intake_ready : :intake_needs_doc_help
-      current_intake.tax_returns.each do |tax_return|
-        tax_return.transition_to(transition_to) if tax_return.current_state.to_sym != transition_to
-      end
-    end
+    def after_update_success; end
 
     def form_params
       if IdVerificationExperimentService.new(current_intake).show_expanded_id?
@@ -41,17 +36,6 @@ module Documents
 
     def illustration_path
       'ssn-itins.svg'
-    end
-
-    private
-
-    def has_all_required_docs?
-      intake_doc_types = current_intake.documents.pluck(:document_type)
-      required_docs = []
-      required_docs << DocumentTypes::Selfie.key unless IdVerificationExperimentService.new(current_intake).skip_selfies?
-      DocumentTypes::IDENTITY_TYPES.map(&:key).intersect?(intake_doc_types) &&
-        DocumentTypes::SECONDARY_IDENTITY_TYPES.map(&:key).intersect?(intake_doc_types) &&
-        required_docs.all? {|key| intake_doc_types.include?(key) }
     end
   end
 end
