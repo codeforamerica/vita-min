fd0f4dfbe54b791c61b02d5f8b110762cd3d7a3d
Hugo Melo <hmelo@codeforamerica.org>
2025-02-14 12:47:41 -0500

    test setting and fetching ivars
    Co-authored-by: Hugo Melo <hmelo@codeforamerica.org>

--- spec/models/concerns/date_accessible_spec.rb
+++ spec/models/concerns/date_accessible_spec.rb
@@ -2,7 +2,10 @@ require 'rails_helper'
 
 RSpec.describe DateAccessible do
   let(:year) { Rails.configuration.statefile_current_tax_year }
-  subject { create(:state_id) }
+  let(:month) { 2 }
+  let(:day) { 1 }
+  let(:expiration_date) { Date.new(year, month, day) }
+  subject { create(:state_id, expiration_date: expiration_date) }
 
   describe '#date_accessor' do
     it 'should create readers & writers for a single property' do
@@ -42,52 +45,55 @@ RSpec.describe DateAccessible do
       subject.expiration_date_year = year
       subject.valid?
       expect(subject.expiration_date).to eq(Date.new(year, 2, 1))
+    end
 
-      subject.expiration_date_day = 21
-      subject.expiration_date_year = nil
-      subject.valid?
-      expect(subject.expiration_date).to eq(Date.new(year, 2, 1))
+    context "with a date set" do
+      context "and an invalid submitted value to update" do
+        it "is not set" do
+          subject.expiration_date_day = 21
+          subject.expiration_date_year = nil
+          subject.valid?
+          expect(subject.expiration_date).to eq(expiration_date)
+        end
+      end
     end
   end
 
   describe "#date_reader" do
-    it 'should allow the use of the a _day reader' do
-      expect(subject).to respond_to(:expiration_date_day)
+    context "when the date is not set" do
+      let(:expiration_date) { nil }
 
-      expect(subject.expiration_date_day).to be_nil
+      it 'should allow the use of the a _day reader' do
+        expect(subject).to respond_to(:expiration_date_day)
+        expect(subject.expiration_date_day).to be_nil
+      end
 
-      subject.expiration_date_day = 12
-      subject.expiration_date_month = 2
-      subject.expiration_date_year = year
-      subject.valid?
+      it 'should allow the use of the a _month reader' do
+        expect(subject).to respond_to(:expiration_date_month)
+        expect(subject.expiration_date_month).to be_nil
+      end
 
-      expect(subject.expiration_date_day).to eq(12)
+      it 'should allow the use of the a _year reader' do
+        expect(subject).to respond_to(:expiration_date_year)
+        expect(subject.expiration_date_year).to be_nil
+      end
     end
 
-    it 'should allow the use of the a _month reader' do
-      expect(subject).to respond_to(:expiration_date_month)
+    context "when the date is set" do
+      it 'should allow the use of the a _day reader' do
+        expect(subject).to respond_to(:expiration_date_day)
+        expect(subject.expiration_date_day).to eq(day)
+      end
 
-      expect(subject.expiration_date_month).to be_nil
+      it 'should allow the use of the a _month reader' do
+        expect(subject).to respond_to(:expiration_date_month)
+        expect(subject.expiration_date_month).to eq(month)
+      end
 
-      subject.expiration_date_day = 1
-      subject.expiration_date_month = 5
-      subject.expiration_date_year = year
-      subject.valid?
-
-      expect(subject.expiration_date_month).to eq(5)
-    end
-
-    it 'should allow the use of the a _year reader' do
-      expect(subject).to respond_to(:expiration_date_year)
-
-      expect(subject.expiration_date_year).to be_nil
-
-      subject.expiration_date_day = 1
-      subject.expiration_date_month = 2
-      subject.expiration_date_year = year
-      subject.valid?
-
-      expect(subject.expiration_date_year).to eq(year)
+      it 'should allow the use of the a _year reader' do
+        expect(subject).to respond_to(:expiration_date_year)
+        expect(subject.expiration_date_year).to eq(year)
+      end
     end
   end
 end
