7e37781966b76fa542eca7dbcebeee7b67c96734
spompea-cfa <spompea@codeforamerica.org>
2025-02-14 18:16:33 -0500

    rework state transition logic around flow/docs

--- app/controllers/questions/successfully_submitted_controller.rb
+++ app/controllers/questions/successfully_submitted_controller.rb
@@ -1,6 +1,13 @@
 module Questions
   class SuccessfullySubmittedController < PostCompletionQuestionsController
-    include AuthenticatedClientConcern
+    include AuthenticatedClientConcern, GyrDocuments
+
+    before_action do
+      next_state = has_all_required_docs?(current_intake) ? :intake_ready : :intake_needs_doc_help
+      current_intake.tax_returns.each do |tax_return|
+        tax_return.transition_to(next_state) if tax_return.current_state.to_sym != next_state
+      end
+    end
 
     def include_analytics?
       true
