cabca0bdd18e68be2bd6e3f444433605dd5490a3
Martha Pidcock <mpidcock@codeforamerica.org>
2025-02-19 14:00:13 -0800

    FYST-1820 Remove duplicate w2s after importing

--- app/controllers/state_file/questions/post_data_transfer_controller.rb
+++ app/controllers/state_file/questions/post_data_transfer_controller.rb
@@ -13,6 +13,8 @@ module StateFile
            current_intake&.disqualifying_df_data_reason.present?
           redirect_to next_path and return
         end
+        # cleanup
+        remove_duplicate_w2s(:state_file_w2s)
         StateFileEfileDeviceInfo.find_or_create_by!(
           event_type: "initial_creation",
           ip_address: ip_for_irs,
@@ -21,6 +23,30 @@ module StateFile
       end
 
       private
+
+      def remove_duplicate_w2s(data_to_deduplicate)
+        indices = []
+        current_intake.send(data_to_deduplicate).each do |w2|
+          current_index = w2.w2_index
+          if indices.include(current_index)
+            w2.destroy!
+          else
+            indices.add(current_index)
+          end
+        end
+      end
+
+      def remove_duplicate_w2s(data_to_deduplicate)
+        indices = []
+        current_intake.send(data_to_deduplicate).each do |w2|
+          current_index = w2.w2_index
+          if indices.include(current_index)
+            w2.destroy!
+          else
+            indices.add(current_index)
+          end
+        end
+      end
 
       def prev_path
         nil
