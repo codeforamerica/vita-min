180ada1dadad02019315de22b648e3b1069c3b34
Drew Proebstel <dproebstel@codeforamerica.org>
2025-02-20 15:04:59 -0800

    bounce from page when there is no request

--- spec/controllers/state_file/archived_intake/archived_intake_controller_spec.rb
+++ spec/controllers/state_file/archived_intake/archived_intake_controller_spec.rb
@@ -88,5 +88,57 @@ describe StateFile::ArchivedIntakes::ArchivedIntakeController, type: :controller
         end
       end
     end
+
+    describe '#is_request_locked' do
+      before do
+        allow(controller).to receive(:current_request).and_return(request_instance)
+        allow(controller).to receive(:current_archived_intake).and_return(archived_intake)
+      end
+
+      context 'when the request is nil' do
+        before do
+          allow(controller).to receive(:current_request).and_return(nil)
+        end
+
+        it 'redirects to verification error page' do
+          expect(controller).to receive(:redirect_to).with(state_file_archived_intakes_verification_error_path)
+          controller.is_request_locked
+        end
+      end
+
+      context 'when the request is locked' do
+        before do
+          allow(request_instance).to receive(:access_locked?).and_return(true)
+        end
+
+        it 'redirects to verification error page' do
+          expect(controller).to receive(:redirect_to).with(state_file_archived_intakes_verification_error_path)
+          controller.is_request_locked
+        end
+      end
+
+      context 'when the archived intake is permanently locked' do
+        before do
+          allow(archived_intake).to receive(:permanently_locked_at).and_return(Time.current)
+        end
+
+        it 'redirects to verification error page' do
+          expect(controller).to receive(:redirect_to).with(state_file_archived_intakes_verification_error_path)
+          controller.is_request_locked
+        end
+      end
+
+      context 'when the request is valid and not locked' do
+        before do
+          allow(request_instance).to receive(:access_locked?).and_return(false)
+          allow(archived_intake).to receive(:permanently_locked_at).and_return(nil)
+        end
+
+        it 'does not redirect' do
+          expect(controller).not_to receive(:redirect_to)
+          controller.is_request_locked
+        end
+      end
+    end
   end
 end
