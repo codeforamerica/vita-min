8f0ec68f2bab14cf405d020e9acfa057045b5876
Mike Rotondo <mrotondo@codeforamerica.org>
2025-02-20 17:07:42 -0800

    remove diagnostic output from webhook controller

--- app/controllers/aws_ip_ranges_webhooks_controller.rb
+++ app/controllers/aws_ip_ranges_webhooks_controller.rb
@@ -1,12 +1,8 @@
 class AwsIpRangesWebhooksController < ApplicationController
 
   def update_aws_ip_ranges
-
-    puts "EHLLO WE ARE IN THE CONTROLLER"
-
     RemoteIpTrustedProxiesService.configure_trusted_proxies(RemoteIpTrustedProxiesService.load_current_aws_ip_ranges)
-
-    render text: "HELLO WE DID THE THING"
+    head :ok
   end
 
 end
