e5fa47cbea79dd37030e8e789fb9f4ada12a0da1
Mike Rotondo <mrotondo@codeforamerica.org>
2025-02-20 17:25:02 -0800

    add logging of params so that we can confirm the SNS subscription

--- app/controllers/aws_ip_ranges_webhooks_controller.rb
+++ app/controllers/aws_ip_ranges_webhooks_controller.rb
@@ -2,6 +2,8 @@ class AwsIpRangesWebhooksController < ApplicationController
 
   def update_aws_ip_ranges
     RemoteIpTrustedProxiesService.configure_trusted_proxies(RemoteIpTrustedProxiesService.load_current_aws_ip_ranges)
+    puts "LOGGING AWS SNS SUBSCRIPTION CONFIRMATION FOR UPDATE_AWS_IP_RANGES WEBHOOK"
+    puts params
     head :ok
   end
 
