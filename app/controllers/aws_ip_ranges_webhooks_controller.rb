f6f6b08e7ded6eca5be96c83dabd9c885e610ad9
Mike Rotondo <mrotondo@codeforamerica.org>
2025-02-25 14:46:33 -0800

    log out raw post request body instead of params, since apparently the latter doesn't work with AWS' subscription confirmation request

--- app/controllers/aws_ip_ranges_webhooks_controller.rb
+++ app/controllers/aws_ip_ranges_webhooks_controller.rb
@@ -3,7 +3,7 @@ class AwsIpRangesWebhooksController < ActionController::Base
 
   def update_aws_ip_ranges
     RemoteIpTrustedProxiesService.configure_trusted_proxies(RemoteIpTrustedProxiesService.load_current_aws_ip_ranges)
-    Rails.logger.info("LOGGING AWS SNS SUBSCRIPTION CONFIRMATION FOR UPDATE_AWS_IP_RANGES WEBHOOK: #{params}")
+    Rails.logger.info("LOGGING AWS SNS SUBSCRIPTION CONFIRMATION FOR UPDATE_AWS_IP_RANGES WEBHOOK: #{request.raw_post}")
     head :ok
   end
 
