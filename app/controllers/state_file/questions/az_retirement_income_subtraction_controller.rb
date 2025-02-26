bfdf607132de1196eb2c1f2ce535fdf01c006a3d
Drew Proebstel <dproebstel@codeforamerica.org>
2025-02-26 09:45:20 -0800

    add new show logic for md and az 1099-r questions

--- app/controllers/state_file/questions/az_retirement_income_subtraction_controller.rb
+++ app/controllers/state_file/questions/az_retirement_income_subtraction_controller.rb
@@ -1,6 +1,33 @@
 module StateFile
   module Questions
     class AzRetirementIncomeSubtractionController < RetirementIncomeSubtractionController
+
+      def self.show?(intake)
+        Flipper.enabled?(:show_retirement_ui) && has_eligible_1099rs?(intake)
+      end
+
+      def self.has_eligible_1099rs?(intake)
+        intake.state_file1099_rs.any? do |form1099r|
+          form1099r.taxable_amount&.to_f&.positive?
+        end
+      end
+
+      private
+
+      def eligible_1099rs
+        @eligible_1099rs ||= current_intake.state_file1099_rs.select do |form1099r|
+          form1099r.taxable_amount&.to_f&.positive?
+        end
+      end
+
+      def num_items
+        eligible_1099rs.count
+      end
+
+      def load_item(index)
+        @state_file_1099r = eligible_1099rs[index]
+        render "public_pages/page_not_found", status: 404 if @state_file_1099r.nil?
+      end
       def followup_class = StateFileAz1099RFollowup
     end
   end
