97aaaeab8d1865ce77309eb60cc87745d1597788
Arin Choi <achoi@codeforamerica.org>
2025-02-26 14:50:56 -0800

    Remove checking for eligible? when saving disability status - it should save the response regardless of their eligiblity

--- app/forms/state_file/id_disability_form.rb
+++ app/forms/state_file/id_disability_form.rb
@@ -8,19 +8,13 @@ module StateFile
 
     def save
       if intake.filing_status_mfj?
-        primary_eligible = eligible?(:primary)
-        spouse_eligible = eligible?(:spouse)
-
         case mfj_disability
         when "me"
-          @intake.update(primary_disabled: primary_eligible ? "yes" : "no", spouse_disabled: "no")
+          @intake.update(primary_disabled: "yes", spouse_disabled: "no")
         when "spouse"
-          @intake.update(primary_disabled: "no", spouse_disabled: spouse_eligible ? "yes" : "no")
+          @intake.update(primary_disabled: "no", spouse_disabled: "yes")
         when "both"
-          @intake.update(
-            primary_disabled: primary_eligible ? "yes" : "no",
-            spouse_disabled: spouse_eligible ? "yes" : "no"
-          )
+          @intake.update(primary_disabled: "yes", spouse_disabled: "yes")
         when "none"
           @intake.update(primary_disabled: "no", spouse_disabled: "no")
         end
@@ -43,21 +37,6 @@ module StateFile
         spouse_followups =  @intake.filer_1099_rs(:spouse).map(&:state_specific_followup).compact
         spouse_followups.each(&:destroy)
       end
-    end
-
-    def eligible?(primary_or_spouse)
-      person = intake.send(primary_or_spouse)
-      birth_date = person.birth_date
-      return false unless birth_date.present?
-
-      age = intake.calculate_age(birth_date, inclusive_of_jan_1: true)
-      age_eligible = age >= 62 && age < 65
-
-      has_taxable_1099r = intake.state_file1099_rs.any? do |form|
-        form.recipient_ssn == person.ssn && form.taxable_amount&.to_f&.positive?
-      end
-
-      age_eligible && has_taxable_1099r
     end
   end
 end
