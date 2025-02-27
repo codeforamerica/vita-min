1a333495ecff6b5fa18fd6e89b3f32e6d9824339
Arin Choi <achoi@codeforamerica.org>
2025-02-27 11:42:18 -0800

    Move checking eligibility method to the intake

--- app/controllers/state_file/questions/id_disability_controller.rb
+++ app/controllers/state_file/questions/id_disability_controller.rb
@@ -4,17 +4,7 @@ module StateFile
       def self.show?(intake)
         Flipper.enabled?(:show_retirement_ui) &&
           intake.state_file1099_rs.any? { |form1099r| form1099r.taxable_amount&.to_f&.positive? } &&
-          !intake.filing_status_mfs? && meets_age_requirements?(intake)
-      end
-
-      def self.meets_age_requirements?(intake)
-        primary_age = intake.calculate_age(intake.primary_birth_date, inclusive_of_jan_1: true)
-        if intake.filing_status_mfj? && intake.spouse_birth_date.present?
-          spouse_age = intake.calculate_age(intake.spouse_birth_date, inclusive_of_jan_1: true)
-          (primary_age >= 62 && primary_age < 65) || (spouse_age >= 62 && spouse_age < 65)
-        else
-          primary_age >= 62 && primary_age < 65
-        end
+          !intake.filing_status_mfs? && intake.meets_age_requirements_for_disability_check?
       end
 
       def next_path
--- spec/controllers/state_file/questions/id_disability_controller_spec.rb
+++ spec/controllers/state_file/questions/id_disability_controller_spec.rb
@@ -17,13 +17,10 @@ RSpec.describe StateFile::Questions::IdDisabilityController do
   end
 
   describe ".show?" do
-    context "when single" do
-      let(:intake) { create :state_file_id_intake}
-      let!(:state_file1099_r) { create(:state_file1099_r, intake: intake, taxable_amount: 25) }
-      before do
-        intake.primary_birth_date = Date.new(MultiTenantService.statefile.current_tax_year - 61, 1, 1)
-      end
+    let(:intake) { create :state_file_id_intake}
+    let!(:state_file1099_r) { create(:state_file1099_r, intake: intake, taxable_amount: 25) }
 
+    context "when single" do
       context "when feature flag is disabled" do
         before do
           allow(Flipper).to receive(:enabled?).with(:show_retirement_ui).and_return(false)
@@ -33,95 +30,30 @@ RSpec.describe StateFile::Questions::IdDisabilityController do
           expect(described_class.show?(intake)).to eq false
         end
       end
+    end
 
-      context "with a 1099R but zero taxable amount" do
-        let!(:state_file1099_r) { create(:state_file1099_r, intake: intake, taxable_amount: 0) }
+    context "with married filing separately status" do
+      let(:intake) { create :state_file_id_intake, filing_status: "married_filing_separately" }
 
-        it "does not show" do
-          expect(described_class.show?(intake)).to eq false
-        end
-      end
-
-      context "with married filing separately status" do
-        let(:intake) { create :state_file_id_intake, filing_status: "married_filing_separately" }
-
-        it "does not show" do
-          expect(described_class.show?(intake)).to eq false
-        end
-      end
-
-      context "when filer is under 62" do
-        before do
-          intake.primary_birth_date = Date.new(MultiTenantService.statefile.current_tax_year - 60, 1, 1)
-        end
-
-        it "does not show" do
-          expect(described_class.show?(intake)).to eq false
-        end
-      end
-
-      context "when filer is within the age range qualifications" do
-        before do
-          intake.primary_birth_date = Date.new(MultiTenantService.statefile.current_tax_year - 62, 1, 1)
-        end
-
-        it "shows" do
-          expect(described_class.show?(intake)).to eq true
-        end
-      end
-
-      context "when filer is above the age requirements" do
-        before do
-          intake.primary_birth_date = Date.new(MultiTenantService.statefile.current_tax_year - 65, 1, 1)
-        end
-
-        it "does not show" do
-          expect(described_class.show?(intake)).to eq false
-        end
+      it "does not show" do
+        expect(described_class.show?(intake)).to eq false
       end
     end
 
-    context "when married filing jointly" do
-      let(:intake) { create :state_file_id_intake, :mfj_filer_with_json}
-      let!(:state_file1099_r) { create(:state_file1099_r, intake: intake, taxable_amount: 25) }
-
-      context "when both spouses are under 62" do
+    context "meets_age_requirements_for_disability_check?" do
+      context "meets requirements" do
         before do
-          intake.primary_birth_date = Date.new(MultiTenantService.statefile.current_tax_year - 60, 1, 1)
-          intake.spouse_birth_date = Date.new(MultiTenantService.statefile.current_tax_year - 60, 1, 1)
+          allow(intake).to receive(:meets_age_requirements_for_disability_check?).and_return(true)
         end
 
         it "does not show" do
-          expect(described_class.show?(intake)).to eq false
-        end
-      end
-
-      context "when primary is 62 and spouse is under 62" do
-        before do
-          intake.primary_birth_date = Date.new(MultiTenantService.statefile.current_tax_year - 61, 1, 1)
-          intake.spouse_birth_date = Date.new(MultiTenantService.statefile.current_tax_year - 60, 1, 1)
-        end
-
-        it "shows" do
           expect(described_class.show?(intake)).to eq true
         end
       end
 
-      context "when primary is under 62 and spouse is 62" do
+      context "does not meet requirements" do
         before do
-          intake.primary_birth_date = Date.new(MultiTenantService.statefile.current_tax_year - 60, 1, 1)
-          intake.spouse_birth_date = Date.new(MultiTenantService.statefile.current_tax_year - 61, 1, 1)
-        end
-
-        it "shows" do
-          expect(described_class.show?(intake)).to eq true
-        end
-      end
-
-      context "when both spouses are 65" do
-        before do
-          intake.primary_birth_date = Date.new(MultiTenantService.statefile.current_tax_year - 65, 1, 1)
-          intake.spouse_birth_date = Date.new(MultiTenantService.statefile.current_tax_year - 65, 1, 1)
+          allow(intake).to receive(:meets_age_requirements_for_disability_check?).and_return(false)
         end
 
         it "does not show" do
