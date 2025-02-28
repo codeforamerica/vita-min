3caa4b4f1347f83592eaa8ac18a709a07b3899cb
John Chan <john@innovation.nj.gov>
2025-02-28 14:47:04 -0500

    Fix typo in line 15 and line 16a keys

--- app/controllers/state_file/questions/nj_retirement_warning_controller.rb
+++ app/controllers/state_file/questions/nj_retirement_warning_controller.rb
@@ -4,8 +4,8 @@ module StateFile
       include EligibilityOffboardingConcern
 
       def self.show?(intake)
-        line_15 = intake.calculator.line_or_zero(:NJ_1040_LINE_15)
-        line_16a = intake.calculator.line_or_zero(:NJ_1040_LINE_16A)
+        line_15 = intake.calculator.line_or_zero(:NJ1040_LINE_15)
+        line_16a = intake.calculator.line_or_zero(:NJ1040_LINE_16A)
         nj_retirement_income_helper = Efile::Nj::NjRetirementIncomeHelper.new(intake)
         nj_retirement_income_helper.show_retirement_income_warning?(line_15, line_16a) && Flipper.enabled?(:show_retirement_ui)
       end
