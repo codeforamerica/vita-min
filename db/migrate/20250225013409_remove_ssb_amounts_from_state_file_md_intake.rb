d0d5078984154bed16eede3407290510936338fd
Mike Rotondo <mrotondo@codeforamerica.org>
2025-02-27 17:02:40 -0800

    revert removal of ssb amount columns

--- db/migrate/20250225013409_remove_ssb_amounts_from_state_file_md_intake.rb
+++
@@ -1,8 +0,0 @@
-class RemoveSsbAmountsFromStateFileMdIntake < ActiveRecord::Migration[7.1]
-  def change
-    safety_assured do
-      remove_column :state_file_md_intakes, :primary_ssb_amount
-      remove_column :state_file_md_intakes, :spouse_ssb_amount
-    end
-  end
-end
