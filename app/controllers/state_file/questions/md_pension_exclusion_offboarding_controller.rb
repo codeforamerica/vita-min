83dc7f903dc44d6a071c04726991ea73be2c8278
Arin Choi <achoi@codeforamerica.org>
2025-03-03 14:35:25 -0800

    Refactor show logic into intake methods for clarity and better testing
    Co-authored-by: Anisha Ramnani <aramnani@codeforamerica.org>

--- app/controllers/state_file/questions/md_pension_exclusion_offboarding_controller.rb
+++ app/controllers/state_file/questions/md_pension_exclusion_offboarding_controller.rb
@@ -3,14 +3,7 @@ module StateFile
     class MdPensionExclusionOffboardingController < QuestionsController
       include OtherOptionsLinksConcern
       def self.show?(intake)
-        if intake.filing_status_mfj? && intake.state_file1099_rs.present?
-          if !intake.primary_senior? || !intake.spouse_senior?
-            if intake.primary_proof_of_disability_submitted_no? || intake.spouse_proof_of_disability_submitted_no?
-              return true
-            end
-          end
-        end
-        false
+        intake.filing_status_mfj? && intake.state_file1099_rs.present? && intake.has_filer_under_65? && intake.no_proof_of_disability_submitted?
       end
     end
   end
