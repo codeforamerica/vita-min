0d27256c9ac40182f7b2a4d63d9abaabdbfc46ec
Drew Proebstel <dproebstel@codeforamerica.org>
2025-03-03 11:25:45 -0800

    add sofi offboarding page

--- app/controllers/state_file/questions/md_pension_exclusion_offboarding_controller.rb
+++ app/controllers/state_file/questions/md_pension_exclusion_offboarding_controller.rb
@@ -1,9 +1,10 @@
 module StateFile
   module Questions
     class MdPensionExclusionOffboardingController < QuestionsController
+      include OtherOptionsLinksConcern
       def self.show?(intake)
-        if intake.filing_status_mfj?
-          if intake.primary_senior? || intake.spouse_senior?
+        if intake.filing_status_mfj? && intake.state_file1099_rs.present?
+          if !intake.primary_senior? || !intake.spouse_senior?
             if intake.primary_proof_of_disability_submitted_no? || intake.spouse_proof_of_disability_submitted_no?
               return true
             end
---
+++ spec/controllers/state_file/questions/md_pension_exclusion_offboarding_controller_spec.rb
@@ -0,0 +1,80 @@
+require "rails_helper"
+
+RSpec.describe StateFile::Questions::MdPensionExclusionOffboardingController do
+  let(:intake) { create(:state_file_md_intake) }
+  let!(:state_file1099_r) { create(:state_file1099_r, intake: intake) }
+
+  describe "#show?" do
+    context "when they have no 1099Rs in their DF XML" do
+      before { allow(intake).to receive(:state_file1099_rs).and_return([]) }
+
+      it "does not show" do
+        expect(described_class.show?(intake)).to eq false
+      end
+    end
+
+    context "when the primary is not a senior, has a 1099-r, has not submitted proof of disability but filing status is single" do
+      before do
+        allow(intake).to receive(:filing_status_mfj?).and_return(false)
+        allow(intake).to receive(:primary_senior?).and_return(false)
+        intake.primary_proof_of_disability_submitted = :no
+      end
+
+      it "does not show" do
+        expect(described_class.show?(intake)).to eq false
+      end
+    end
+
+    context "when the primary is not a senior, has a 1099-r, filing status is mfj but has submitted proof of disability" do
+      before do
+        allow(intake).to receive(:filing_status_mfj?).and_return(true)
+        allow(intake).to receive(:primary_senior?).and_return(false)
+        intake.primary_proof_of_disability_submitted = :yes
+      end
+
+      it "does not show" do
+        expect(described_class.show?(intake)).to eq false
+      end
+    end
+
+    context "when the primary has a 1099-r, filing status is mfj, has not submitted proof of disability but both filers are seniors" do
+      before do
+        allow(intake).to receive(:filing_status_mfj?).and_return(true)
+        allow(intake).to receive(:primary_senior?).and_return(true)
+        allow(intake).to receive(:spouse_senior?).and_return(true)
+        intake.primary_proof_of_disability_submitted = :no
+      end
+
+      it "does not show" do
+        expect(described_class.show?(intake)).to eq false
+      end
+    end
+
+    context "when the primary is a not senior, has a 1099-r, filing status is mfj and has not submitted proof of disability bout spouse is a senior" do
+      before do
+        allow(intake).to receive(:filing_status_mfj?).and_return(true)
+        allow(intake).to receive(:primary_senior?).and_return(false)
+        allow(intake).to receive(:spouse_senior?).and_return(true)
+        intake.primary_proof_of_disability_submitted = :no
+      end
+
+      it "shows" do
+        expect(described_class.show?(intake)).to eq true
+      end
+    end
+
+    context "when the primary is a not senior, has a 1099-r, filing status is mfj and has submitted proof of disability bout spouse is a senior and has not submitted proof of disability" do
+      before do
+        allow(intake).to receive(:filing_status_mfj?).and_return(true)
+        allow(intake).to receive(:primary_senior?).and_return(false)
+        allow(intake).to receive(:spouse_senior?).and_return(true)
+        intake.primary_proof_of_disability_submitted = :yes
+        intake.spouse_proof_of_disability_submitted = :no
+      end
+
+      it "shows" do
+        expect(described_class.show?(intake)).to eq true
+      end
+    end
+  end
+end
