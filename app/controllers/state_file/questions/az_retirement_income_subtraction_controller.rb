cd7e46eaec0ecbbe914d7452c90be01fabe39e18
Hugo Melo <hmelo@codeforamerica.org>
2025-03-04 16:19:11 -0500

    [FYST-902] Add az 1099r to final review (#5639)
    Co-authored-by: Yejee Arin Choi <achoi@codeforamerica.org>

--- app/controllers/state_file/questions/az_retirement_income_subtraction_controller.rb
+++ app/controllers/state_file/questions/az_retirement_income_subtraction_controller.rb
@@ -1,6 +1,7 @@
 module StateFile
   module Questions
     class AzRetirementIncomeSubtractionController < RetirementIncomeSubtractionController
+      include ReturnToReviewConcern
 
       def self.show?(intake)
         Flipper.enabled?(:show_retirement_ui) && intake.eligible_1099rs.present?
