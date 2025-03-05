36d3fe99a32130af722f62447a1db4879b443491
Jenny Heath <jheath@codeforamerica.org>
2025-03-05 15:13:31 -0800

    conditionally include callback argument

--- app/services/text_message_verification_code_service.rb
+++ app/services/text_message_verification_code_service.rb
@@ -12,15 +12,16 @@ class TextMessageVerificationCodeService
   def request_code
     verification_code, access_token = TextMessageAccessToken.generate!(sms_phone_number: @phone_number, client_id: @client_id)
     outgoing_message_status = OutgoingMessageStatus.find_or_create_by!(message_type: :sms, parent: access_token)
-    twilio_response = TwilioService.new(@service_data.service_type).send_text_message(
+    args = {
       to: @phone_number,
       body: I18n.t("verification_code_sms.with_code",
                    service_name: @service_data.service_name,
                    locale: @locale,
                    verification_code: verification_code
-      ).strip,
-      status_callback: twilio_update_status_url(outgoing_message_status.id, locale: nil),
-    )
+      ).strip
+    }
+    args = args.merge(status_callback: twilio_update_status_url(outgoing_message_status.id, locale: nil)) if @service_data.service_type != :statefile
+    twilio_response = TwilioService.new(@service_data.service_type).send_text_message(**args)
     VerificationTextMessage.create!(
       text_message_access_token: access_token,
       visitor_id: @visitor_id,
