ab7ca9589420250aff7b2a104df8335fb79f901b
Tahsina Islam <tislam@codeforamerica.org>
2025-03-07 14:16:35 -0600

    Fix schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2025_03_04_145229) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
   enable_extension "plpgsql"
