80f4eff5732a96a38115a6d3831219c52c4a4753
Tahsina Islam <tislam@codeforamerica.org>
2025-03-07 12:51:45 -0600

    Fix spacing between list items

--- app/controllers/state_file/questions/terms_and_conditions_controller.rb
+++ app/controllers/state_file/questions/terms_and_conditions_controller.rb
@@ -6,17 +6,17 @@ module StateFile
           "general.owner.#{current_state_code}",
           default: I18n.t("general.owner.default")
         )
-        @li_items = I18n.t(
-          'state_file.questions.terms_and_conditions.edit.list_items_html',
-          owner: owner,
-          privacy_policy_link: state_file_privacy_policy_path
-        ).scan(/<li>.*?<\/li>/m) # elements of the resulting array include <li> and </li> tags
-        unless Flipper.enabled?(:sms_notifications)
-          @li_items.each_with_index do |li_item, idx|
-            if li_item.include?('SMS')
-              @li_items[idx] = I18n.t('state_file.questions.terms_and_conditions.edit.list_items_no_sms_html')
-            end
-          end
+        
+        @li_items = []
+
+        (1..13).each do |i|
+          @li_items << if i == 8 && !Flipper.enabled?(:sms_notifications)
+            t('state_file.questions.terms_and_conditions.edit.list_item_no_sms_html')
+          else
+            t("state_file.questions.terms_and_conditions.edit.list_item_#{i}_html",
+                           owner: owner,
+                           privacy_policy_link: state_file_privacy_policy_path)
+                       end
         end
       end
     end
