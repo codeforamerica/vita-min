8f25f09b497d0670e23ac5d677390f8a5d93fba0
Tahsina Islam <tislam@codeforamerica.org>
2025-03-07 13:09:59 -0600

    Fix controller formatting

--- app/controllers/state_file/questions/terms_and_conditions_controller.rb
+++ app/controllers/state_file/questions/terms_and_conditions_controller.rb
@@ -11,11 +11,11 @@ module StateFile
 
         (1..13).each do |i|
           @li_items << if i == 8 && !Flipper.enabled?(:sms_notifications)
-            t('state_file.questions.terms_and_conditions.edit.list_item_no_sms_html')
-          else
-            t("state_file.questions.terms_and_conditions.edit.list_item_#{i}_html",
-                           owner: owner,
-                           privacy_policy_link: state_file_privacy_policy_path)
+                         t('state_file.questions.terms_and_conditions.edit.list_item_no_sms_html')
+                       else
+                         t("state_file.questions.terms_and_conditions.edit.list_item_#{i}_html",
+                                        owner: owner,
+                                        privacy_policy_link: state_file_privacy_policy_path)
                        end
         end
       end
