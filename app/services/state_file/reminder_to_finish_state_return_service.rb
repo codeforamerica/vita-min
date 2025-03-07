53b3fbcab506dbb2e1eeb3eb4d3c8d462eb08fa2
Em Barnard-Shao <ebarnard@codeforamerica.org>
2025-03-07 18:22:15 -0500

    Narrow the scope of the query for finish returns

--- app/services/state_file/reminder_to_finish_state_return_service.rb
+++ app/services/state_file/reminder_to_finish_state_return_service.rb
@@ -1,18 +1,36 @@
 module StateFile
   class ReminderToFinishStateReturnService
     def self.run
-      cutoff_time_ago = 6.hours.ago
-      batch_size = 10
-      intakes_with_no_submission = StateFile::StateInformationService.state_intake_classes.flat_map do |class_object|
-        class_object.where("df_data_imported_at < ?", cutoff_time_ago)
-                    .left_joins(:efile_submissions)
-                    .where(efile_submissions: { id: nil })
-                    .where.not("state_file_#{class_object.state_code}_intakes.message_tracker #> '{messages.state_file.finish_return}' IS NOT NULL")
+      intakes_with_no_submission = StateFile::StateInformationService.active_state_codes.flat_map do |state_code|
+        intake_class = StateFile::StateInformationService.intake_class(state_code)
+        intake_class
+          .left_joins(:efile_submissions)
+          .where(df_data_imported_at: Time.current.beginning_of_year..6.hours.ago)
+          .where(efile_submissions: { id: nil })
+          .where.not("state_file_#{state_code}_intakes.message_tracker #> '{messages.state_file.finish_return}' IS NOT NULL")
+          .where(<<~SQL)
+            (
+              phone_number IS NOT NULL
+              AND sms_notification_opt_in = 1
+              AND phone_number_verified_at IS NOT NULL
+            )
+            OR
+            (
+              email_address IS NOT NULL
+              AND email_notification_opt_in = 1
+              AND email_address_verified_at IS NOT NULL
+            )
+          SQL
       end
 
+      batch_size = 50
       intakes_with_no_submission.each_slice(batch_size) do |batch|
         batch.each do |intake|
-          StateFile::MessagingService.new(message: StateFile::AutomatedMessage::FinishReturn, intake: intake).send_message
+          begin
+            StateFile::MessagingService.new(message: StateFile::AutomatedMessage::FinishReturn, intake: intake).send_message
+          rescue => e
+            Sentry.capture_exception(e, extra: { intake_id: intake.id })
+          end
         end
       end
     end
