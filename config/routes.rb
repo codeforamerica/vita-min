767487465dcd62ea6c9dda565f7f122a862bc39a
Tahsina Islam <79296603+tahsinaislam@users.noreply.github.com>
2025-03-10 11:21:09 -0500

    FYST 1652 Re-direct specific 2024 AZ urls to homepage (#5707)

--- config/routes.rb
+++ config/routes.rb
@@ -599,6 +599,9 @@ Rails.application.routes.draw do
       get "/data-import-failed", to: "state_file/state_file_pages#data_import_failed"
       get "/initiate-data-transfer", to: "state_file/questions/initiate_data_transfer#initiate_data_transfer"
 
+      get '/az/questions/return-status', to: redirect('/')
+      get '/az/questions/submission-confirmation', to: redirect('/')
+
       resources :intake_logins, only: [:new, :create, :edit, :update], module: "state_file", path: "login" do
         put "check-verification-code", to: "intake_logins#check_verification_code", as: :check_verification_code, on: :collection
         get "locked", to: "intake_logins#account_locked", as: :account_locked, on: :collection
