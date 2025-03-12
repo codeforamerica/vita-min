cb12deefc640e5974f7aba476636e02e0d3892a9
Martha Pidcock <mpidcock@codeforamerica.org>
2025-03-11 19:53:54 -0700

    Clean up NC withdrawal_date validations

--- app/forms/state_file/nc_taxes_owed_form.rb
+++ app/forms/state_file/nc_taxes_owed_form.rb
@@ -2,46 +2,42 @@ module StateFile
   class NcTaxesOwedForm < TaxesOwedForm
 
     with_options unless: -> { payment_or_deposit_type == "mail" || !form_submitted_before_payment_deadline? } do
-      validate :withdrawal_date_validations
+      validate :withdrawal_date_is_at_least_two_business_days_in_the_future
+      validate :withdrawal_date_is_not_on_a_weekend
+      validate :withdrawal_date_is_not_a_federal_holiday
     end
 
-    def withdrawal_date_validations
-      five_pm_et = app_time_eastern.change(hour: 17, min: 0, sec: 0)
-      if (app_time_eastern >= five_pm_et) && less_than_two_business_days_in_future?
+    private
+
+    def withdrawal_date_is_at_least_two_business_days_in_the_future
+      # From the ticket (FYST-1061):
+      # I have just gotten confirmation that NC wants an additional twist to this logic: if you submit your bank draft
+      # payment after 5:00 pm EST, the earliest draft date available will be two business days in the future
+      after_5pm = @form_submitted_time.in_time_zone.hour >= 17
+      two_business_days_away = add_business_days_to_date(@form_submitted_time, 2)
+      if after_5pm && date_electronic_withdrawal.before?(two_business_days_away)
         errors.add(:date_electronic_withdrawal, I18n.t("errors.attributes.nc_withdrawal_date.post_five_pm"))
       end
-      if date_electronic_withdrawal <= app_time_eastern.to_date
-        errors.add(:date_electronic_withdrawal, I18n.t("errors.attributes.nc_withdrawal_date.past"))
-      end
+    end
+
+    def withdrawal_date_is_not_on_a_weekend
       if date_electronic_withdrawal.saturday? || date_electronic_withdrawal.sunday?
         errors.add(:date_electronic_withdrawal, I18n.t("errors.attributes.nc_withdrawal_date.weekend"))
       end
+    end
+
+    def withdrawal_date_is_not_a_federal_holiday
       if Holidays.on(date_electronic_withdrawal, :us, :federalreservebanks, :observed).any?
         errors.add(:date_electronic_withdrawal, I18n.t("errors.attributes.nc_withdrawal_date.holiday"))
       end
     end
 
-    private
-
-    def app_time_eastern
-      @form_submitted_time.in_time_zone('Eastern Time (US & Canada)')
-    end
-
-    def add_business_days(num_days)
-      current_date = app_time_eastern.to_date
+    def add_business_days_to_date(date, num_days)
       while num_days.positive?
-        current_date += 1.day
-        # Check if the current date is a business day (Mon-Fri)
-        if current_date.wday.between?(1, 5)  # wday 1-5 are Monday to Friday
-          num_days -= 1.day
-        end
+        date += 1.day
+        num_days -= 1.day if date.wday.between?(1, 5)
       end
-      current_date
-    end
-
-    def less_than_two_business_days_in_future?
-      two_business_days_later = add_business_days(2)
-      date_electronic_withdrawal <= two_business_days_later
+      date
     end
   end
 end
