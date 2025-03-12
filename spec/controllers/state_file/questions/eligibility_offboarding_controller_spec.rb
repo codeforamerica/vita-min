db4b0f5745de8ecf3bb9cd277103464c2f5b08b1
Arin Choi <achoi@codeforamerica.org>
2025-03-11 22:38:41 -0700

    Revert "Add spec that failed before this code change & highlight still-existing issue"
    This reverts commit ae9f1447b651bafbc36a7377126c574277db0c17.

--- spec/controllers/state_file/questions/eligibility_offboarding_controller_spec.rb
+++ spec/controllers/state_file/questions/eligibility_offboarding_controller_spec.rb
@@ -18,66 +18,59 @@ RSpec.describe StateFile::Questions::EligibilityOffboardingController do
   end
 
   describe "#edit" do
+    render_views
     before do
       sign_in intake
     end
 
-    context "NJ specific content" do
-      # az does not utilize eligiblity_offboarding_controller in its flow
-      StateFile::StateInformationService.active_state_codes.excluding("az", "ny", "nj").each do |state_code|
-        render_views
+    context "with offboarded_from set in the session" do
+      let(:intake) { create :state_file_id_intake }
+      let(:offboarded_from_path) do
+        StateFile::Questions::IdEligibilityResidenceController.to_path_helper
+      end
+      before do
+        session[:offboarded_from] = offboarded_from_path
+      end
 
-        let(:intake) { create "state_file_#{state_code}_intake".to_sym }
+      it "uses the correct prev_path for the Go back button" do
+        get :edit
 
-        it "#{state_code} does not show NJ-specific content" do
-          get :edit
-          expect(response.body).to include("Visit our FAQ")
-          expect(response.body).not_to include("Get connected now")
-        end
+        expect(Nokogiri::HTML.parse(response.body)).to have_link(href: offboarded_from_path)
+        expect(session[:offboarded_from]).to be_nil
       end
     end
 
-    # For NJ, two controllers can lead to the ineligible offboarding controller -- so we still have an issue where if the
-    # locale changes, session[:offboarded_from] will be `nil` and they will have an inactive Go back button.
-    [
-      ["id", StateFile::Questions::IdEligibilityResidenceController],
-      ["md", StateFile::Questions::MdEligibilityFilingStatusController],
-      ["nc", StateFile::Questions::NcEligibilityController],
-      ["nj", StateFile::Questions::NjEligibilityHealthInsuranceController],
-      ["nj", StateFile::Questions::NjRetirementWarningController, StateFile::Questions::NjEligibilityHealthInsuranceController]
-    ].each do |state_code, eligibility_controller, default_eligibility_controller|
-      context "#{state_code} with eligibility controller #{eligibility_controller}" do
-        render_views
+    context "AZ" do
+      let(:intake) { create :state_file_az_intake }
 
-        let(:intake) { create "state_file_#{state_code}_intake".to_sym }
-        let(:eligibility_controller_path) { eligibility_controller.to_path_helper }
+      it "does not show NJ-specific content" do
+        get :edit
+        expect(response.body).to include("Visit our FAQ")
+        expect(response.body).not_to include("Get connected now")
+      end
+    end
 
-        context "with offboarded_from set in the session" do
-          before do
-            session[:offboarded_from] = eligibility_controller_path
-          end
+    context "ID" do
+      let(:intake) { create :state_file_id_intake }
 
-          it "uses the correct prev_path for the Go back button" do
-            get :edit
+      it "does not show NJ-specific content" do
+        get :edit
+        expect(response.body).to include("Visit our FAQ")
+        expect(response.body).not_to include("Get connected now")
+      end
+    end
 
-            expect(Nokogiri::HTML.parse(response.body)).to have_link(href: eligibility_controller_path)
-          end
-        end
+    context "MD" do
+      let(:intake) { create :state_file_md_intake }
 
-        context "without offboarded_from set in the session" do
-          let(:default_controller_path) { default_eligibility_controller ? default_eligibility_controller.to_path_helper : eligibility_controller_path }
-
-          it "uses the default prev_path for the Go back button (controller right before offboarding controller in flow)" do
-            get :edit
-
-            expect(Nokogiri::HTML.parse(response.body)).to have_link(href: default_controller_path)
-          end
-        end
+      it "does not show NJ-specific content" do
+        get :edit
+        expect(response.body).to include("Visit our FAQ")
+        expect(response.body).not_to include("Get connected now")
       end
     end
 
     context "NJ" do
-      render_views
       let(:intake) { create :state_file_nj_intake }
   
       it "shows NJ-specific content" do
