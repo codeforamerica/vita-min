0842e0562a7297d5e0f4fa625fca7c6b63c4909d
Drew Proebstel <dproebstel@codeforamerica.org>
2025-03-17 15:36:32 -0700

    update test

--- spec/channels/state_file_submission_pdf_status_channel_spec.rb
+++ spec/channels/state_file_submission_pdf_status_channel_spec.rb
@@ -4,14 +4,16 @@ RSpec.describe StateFileSubmissionPdfStatusChannel, type: :channel do
   let(:intake) { create(:state_file_az_intake) }
 
   before do
-    allow(subject).to receive(:current_state_file_intake).and_return(intake)
+    stub_connection current_state_file_intake: intake
   end
 
-  context "before the bundle submission pdf job run" do
-    it 'does not broadcast intake ready' do
-      expect do
-        subscribe
-      end.to have_broadcasted_to(StateFileSubmissionPdfStatusChannel.broadcasting_for(intake))
+  context "before the bundle submission PDF job runs" do
+    it "broadcasts processing when the submission PDF is not attached" do
+      subscribe
+
+      expect {
+        perform(:status_update)
+      }.to have_broadcasted_to(intake).with(status: :processing)
     end
   end
 end
