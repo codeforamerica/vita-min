a7a3d87f71cc9d44d3d36d91d6b861e4efc3330c
Drew Proebstel <dproebstel@codeforamerica.org>
2025-03-17 15:01:46 -0700

    update to state name

--- spec/channels/state_file_submission_pdf_status_channel_spec.rb
+++ spec/channels/state_file_submission_pdf_status_channel_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe StateFileSubmissionPdfStatusChannel, type: :channel do
   let(:intake) { create(:state_file_az_intake) }
 
   before do
-    allow(subject).to receive(:current_intake).and_return(intake)
+    allow(subject).to receive(:current_state_file_intake).and_return(intake)
   end
 
   context "before the bundle submission pdf job run" do
@@ -12,17 +12,6 @@ RSpec.describe StateFileSubmissionPdfStatusChannel, type: :channel do
       expect do
         subscribe
       end.to have_broadcasted_to(StateFileSubmissionPdfStatusChannel.broadcasting_for(intake))
-    end
-  end
-
-  context "with direct file data" do
-    let(:direct_file_data) { StateFile::DirectFileApiResponseSampleService.new.old_xml_sample }
-
-    it 'broadcasts job complete' do
-      expect do
-        subscribe
-      end.to have_broadcasted_to(DfDataTransferJobChannel.broadcasting_for(intake)).with(["The job is complete"])
-      expect(subscription).to have_stream_for(intake)
     end
   end
 end
