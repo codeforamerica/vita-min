548447f34c5e49e2a8fff2dddbfb4cd5cca4ac63
Em Barnard-Shao <ebarnard@codeforamerica.org>
2025-03-20 16:17:43 -0400

    Add md address validation for PO boxes

--- app/forms/state_file/md_permanent_address_form.rb
+++ app/forms/state_file/md_permanent_address_form.rb
@@ -13,6 +13,8 @@ module StateFile
     validates :permanent_apartment, irs_street_address_type: { maximum: nil }
     validates :permanent_city, presence: true, irs_street_address_type: { maximum: nil }, if: -> { confirmed_permanent_address == "no" }
     validates :permanent_zip, presence: true, zip_code: { zip_code_lengths: [5, 9, 12].freeze }, if: -> { confirmed_permanent_address == "no" }
+    validate :permanent_street_is_not_po_box, if: -> { permanent_street.present? }
+    validate :permanent_apartment_is_not_po_box, if: -> { permanent_apartment.present? }
 
     def initialize(intake = nil, params = nil)
       if params[:confirmed_permanent_address] == "yes"
@@ -24,16 +26,37 @@ module StateFile
     end
 
     def save
-      attributes_from_direct_file = confirmed_permanent_address == "yes" ?
+      attributes_from_direct_file = if confirmed_permanent_address == "yes"
                                       {
                                         permanent_city: @intake.direct_file_data.mailing_city,
                                         permanent_street: @intake.direct_file_data.mailing_street,
                                         permanent_apartment: @intake.direct_file_data.mailing_apartment,
                                         permanent_zip: @intake.direct_file_data.mailing_zip,
-                                      } : {}
+                                      }
+                                    else
+                                      {}
+                                    end
       attributes_from_direct_file[:permanent_address_outside_md] = @intake.direct_file_data.mailing_state != 'MD' && confirmed_permanent_address == "yes" ? "yes" : "no"
       attributes_from_form = attributes_for(:intake).except(:permanent_zip).merge({ permanent_zip: permanent_zip&.delete('-') })
       @intake.update(attributes_from_form.merge(attributes_from_direct_file))
+    end
+
+    private
+
+    def permanent_street_is_not_po_box
+      errors.add(:permanent_street, I18n.t("forms.errors.address_is_not_po_box", tax_year: MultiTenantService.statefile.current_tax_year)) if contains_po_box(permanent_street)
+    end
+
+    def permanent_apartment_is_not_po_box
+      errors.add(:permanent_apartment, I18n.t("forms.errors.address_is_not_po_box", tax_year: MultiTenantService.statefile.current_tax_year)) if contains_po_box(permanent_apartment)
+    end
+
+    def contains_po_box(address)
+      return false if address.blank?
+
+      # Regex tested at https://regex101.com/r/4b22vr/1
+      pattern = /(?i)p(?:[.\s]*)o(?:[.\s]*)(?:b(?:[.\s]*)(?:o(?:[.\s]*)x)?)?/
+      address.match?(pattern)
     end
   end
 end
