3b39acfdd51e917185d1c348976dc88082d9950d
Drew Proebstel <dproebstel@codeforamerica.org>
2025-03-24 13:37:49 -0700

    add rails logs

--- app/controllers/state_file/questions/submission_confirmation_controller.rb
+++ app/controllers/state_file/questions/submission_confirmation_controller.rb
@@ -3,7 +3,9 @@ module StateFile
     class SubmissionConfirmationController < QuestionsController
       def edit
         raise ActiveRecord::RecordNotFound unless EfileSubmission.where(data_source: current_intake).present?
-        @show_download_button = current_intake.submission_pdf&.reload&.attached?
+        Rails.logger.info "PDF attached?: #{current_intake.submission_pdf.attached?}"
+        Rails.logger.info "PDF blob: #{current_intake.submission_pdf.blob.inspect}"
+        @show_download_button = current_intake.submission_pdf.attached?
       end
 
       def prev_path
