deba21e087edf46ea604128dcc7452dc53f12c10
Em Barnard-Shao <ebarnard@codeforamerica.org>
2025-03-24 15:41:33 -0400

    Update regex link

--- app/helpers/state_file/pattern_matching_helper.rb
+++ app/helpers/state_file/pattern_matching_helper.rb
@@ -3,7 +3,7 @@ module StateFile
     def contains_po_box(address)
       return false if address.blank?
 
-      # Regex tested at https://regex101.com/r/4b22vr/1
+      # Regex tested at https://regex101.com/r/SksZmO/1
       pattern = /(?i)p(?:[.\s]*)o(?:[.\s]*)(?:b(?:[.\s]*)(?:o(?:[.\s]*)x)?)?/
       address.match?(pattern)
     end
