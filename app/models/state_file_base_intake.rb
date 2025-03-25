7535342c4b137d028bbdcf9706ed9c4bc53d0ed1
Arin Choi <achoi@codeforamerica.org>
2025-03-25 12:14:10 -0700

    Fix comment

--- app/models/state_file_base_intake.rb
+++ app/models/state_file_base_intake.rb
@@ -414,7 +414,7 @@ class StateFileBaseIntake < ApplicationRecord
   def increment_failed_attempts
     # If unlock_in time has passed, unlock the intake. Otherwise, the intakes' failed_attempts will
     # remain unchanged until the verification code is answered correctly (see: unlock_for_login!)
-    # taken from https://github.com/heartcombo/devise/issues/4679#issuecomment-1453450974
+    # derived from https://github.com/heartcombo/devise/issues/4679#issuecomment-1453450974
     if lock_expired?
       unlock_access!
     end
