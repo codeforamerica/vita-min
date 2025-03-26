e1197b966fb9e60b6fa357297ad7193cfab8a407
Tahsina Islam <tislam@codeforamerica.org>
2025-03-26 13:50:38 -0500

    GYR-702 Reduce reliance on not ready status on GYR
    Co-authored-by: Anisha Ramnani <aramnani@codeforamerica.org>

--- spec/helpers/tax_return_status_helper_spec.rb
+++ spec/helpers/tax_return_status_helper_spec.rb
@@ -76,6 +76,61 @@ describe TaxReturnStatusHelper do
 
   end
 
+  describe "#grouped_status_options_for_partner" do
+    expected = [
+      ["Intake",
+       [
+         ["Needs doc help", "intake_needs_doc_help"],
+         ["Info requested", "intake_info_requested"],
+         ["Greeter - info requested", "intake_greeter_info_requested"],
+         ["Ready for review", "intake_ready"],
+         ["Reviewing", "intake_reviewing"],
+         ["Ready for call", "intake_ready_for_call"],
+       ]
+      ],
+      ["Tax prep",
+       [
+         ["Ready for prep", "prep_ready_for_prep"],
+         ["Preparing", "prep_preparing"],
+         ["Info requested", "prep_info_requested"]
+       ]
+      ],
+      ["Quality review",
+       [
+         ["Ready for QR", "review_ready_for_qr"],
+         ["Reviewing", "review_reviewing"],
+         ["Ready for call", "review_ready_for_call"],
+         ["Signature requested", "review_signature_requested"],
+         ["Info requested", "review_info_requested"]
+       ]
+      ],
+      [I18n.t("hub.tax_returns.stage.file"),
+       [
+         ["Needs review", "file_needs_review"],
+         ["Ready to file", "file_ready_to_file"],
+         ['E-filed', "file_efiled"],
+         ["Filed by mail", "file_mailed"],
+         ["Rejected", "file_rejected"],
+         ["Accepted", "file_accepted"],
+         ["Not filing", "file_not_filing"],
+         ["Hold", "file_hold"],
+         ["ID Check", "file_fraud_hold"]
+       ]
+      ]
+    ]
+
+    context "as a non-greeter" do
+      let(:user_double) { double(User) }
+      before do
+        allow(helper).to receive(:current_user).and_return user_double
+        allow(user_double).to receive(:role_type).and_return TeamMemberRole::TYPE
+      end
+      it "returns status options formatted to create select optgroups" do
+        expect(helper.grouped_status_options_for_partner).to eq(expected)
+      end
+    end
+  end
+
   describe "#language_options" do
     context "with a non default locale set" do
       before { allow(I18n).to receive(:locale).and_return(:es) }
