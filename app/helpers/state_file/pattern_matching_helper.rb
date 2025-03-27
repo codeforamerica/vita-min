2d229488fdb872770ae99e68e2478be7a80b026d
em barnard-shao <49880002+embarnard@users.noreply.github.com>
2025-03-27 17:06:00 -0400

    Fix MD address XML bug when DF address is PO Box (#5843)

--- app/helpers/state_file/pattern_matching_helper.rb
+++ app/helpers/state_file/pattern_matching_helper.rb
@@ -3,8 +3,8 @@ module StateFile
     def contains_po_box(address)
       return false if address.blank?
 
-      # Regex tested at https://regex101.com/r/SksZmO/1
-      pattern = /(?i)p(?:[.\s]*)o(?:[.\s]*)(?:b(?:[.\s]*)(?:o(?:[.\s]*)x)?)?/
+      # Regex tested at https://regex101.com/r/V64RgG/1
+      pattern = /(?i)\bp(?:[.\s]*)o(?:[.\s]*)(?:b(?:[.\s]*)(?:o(?:[.\s]*)x)?)?\b/
       address.match?(pattern)
     end
   end
