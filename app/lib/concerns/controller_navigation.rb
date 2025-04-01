3a724e76405bd630ca3f8cfe724a5b7e0e978e10
Mike Rotondo <mrotondo@codeforamerica.org>
2025-04-01 12:24:41 -0700

    refactor and improve return to review behavior, and add fixes

--- app/lib/concerns/controller_navigation.rb
+++ app/lib/concerns/controller_navigation.rb
@@ -23,65 +23,85 @@ module ControllerNavigation
     @item_index = item_index
   end
 
-  def return_to_review?(return_to_review_param, showable_pages)
-    return true if return_to_review_param == "y"
-    showable_pages.none? do |page_info|
-      page_controller_name = page_info[:controller].name.demodulize.underscore
-      possible_matches = [page_controller_name, "#{page_controller_name}_#{page_info[:item_index]}", page_info[:step], "#{page_info[:step]}_#{page_info[:item_index]}"]
-      possible_matches.include? return_to_review_param
-    end
-  end
-
   def next(controller_class = nil)
     all_pages = pages(current_controller.visitor_record)
     current_page_index = index(all_pages, controller_class)
     return unless current_page_index
-    pages_until_end = all_pages[current_page_index + 1..-1]
-    showable_pages_until_end = select_showable(pages_until_end)
-
+    pages_until_end = all_pages[current_page_index..-1]
     return_to_review_param = (current_controller.params[:return_to_review_after] ||
       current_controller.params[:return_to_review])
-    if return_to_review?(return_to_review_param, showable_pages_until_end)
-      { controller: current_controller.review_controller }
-    else
-      next_page_info = showable_pages_until_end.first
-      next_page_info[:params] = { return_to_review: current_controller.params[:return_to_review],
-                                  return_to_review_before: current_controller.params[:return_to_review_before],
-                                  return_to_review_after: current_controller.params[:return_to_review_after] }.compact
-      next_page_info
-    end
+
+    proceed(pages_until_end, return_to_review_param)
   end
 
   def prev
     all_pages = pages(current_controller.visitor_record)
     current_page_index = index(all_pages)
-    return unless current_page_index&.nonzero?
-    pages_until_beginning = all_pages[0..current_page_index - 1].reverse
-    showable_pages_until_beginning = select_showable(pages_until_beginning)
-
+    return unless current_page_index
+    pages_until_beginning = all_pages[0..current_page_index].reverse
     return_to_review_param = (current_controller.params[:return_to_review_before] ||
-      current_controller.params[:return_to_review_after] ||
       current_controller.params[:return_to_review])
-    if return_to_review?(return_to_review_param, showable_pages_until_beginning)
-      { controller: current_controller.review_controller }
-    else
-      prev_page_info = showable_pages_until_beginning.first
-      prev_page_info[:params] = { return_to_review: current_controller.params[:return_to_review],
-                                  return_to_review_before: current_controller.params[:return_to_review_before],
-                                  return_to_review_after: current_controller.params[:return_to_review_after] }.compact
-      prev_page_info
-    end
+
+    proceed(pages_until_beginning, return_to_review_param)
   end
 
   private
 
-  def index(list, controller_class = nil)
+  def proceed(remaining_pages, return_to_review_param)
+    current_page_info = remaining_pages[0]
+    showable_pages = select_showable(remaining_pages[1..])
+    if return_to_review?(return_to_review_param, current_page_info, showable_pages)
+      { controller: current_controller.review_controller }
+    elsif showable_pages.present?
+      next_page_info = showable_pages.first
+      preserve_return_to_review_params(next_page_info)
+      next_page_info
+    end
+  end
+
+  def preserve_return_to_review_params(page_info)
+    return_to_review_params = { return_to_review: current_controller.params[:return_to_review],
+                                return_to_review_before: current_controller.params[:return_to_review_before],
+                                return_to_review_after: current_controller.params[:return_to_review_after] }.compact
+    page_info[:params] = return_to_review_params if return_to_review_params.present?
+  end
+
+  def return_to_review?(return_to_review_param, current_page_info, showable_pages)
+    return false if return_to_review_param.blank?
+
+    # Case 1: Always return to review if return_to_review=y
+    return true if return_to_review_param == "y"
+
+    if showable_pages.present?
+      # Case 2: If there are more showable pages, and we don't see a page matching the return to review param, assume
+      # that we passed it and return to review
+      showable_pages.none? do |page_info|
+        ControllerNavigation.page_info_matches_return_to_review_param?(page_info, return_to_review_param)
+      end
+    else
+      # Case 3: If there are no more showable pages, only return to review if the current page matches the return to
+      # review param
+      ControllerNavigation.page_info_matches_return_to_review_param?(current_page_info, return_to_review_param)
+    end
+  end
+
+  def self.page_info_matches_return_to_review_param?(page_info, return_to_review_param)
+    page_controller_name = page_info[:controller].name.demodulize.underscore
+    possible_matches = [page_controller_name,
+                        "#{page_controller_name}_#{page_info[:item_index]}",
+                        page_info[:step],
+                        "#{page_info[:step]}_#{page_info[:item_index]}"]
+    possible_matches.include? return_to_review_param
+  end
+
+  def index(pages, controller_class = nil)
     controller_class ||= current_controller.class
-    index = list.index { |page_info| page_info[:controller] == controller_class && page_info[:item_index] == item_index }
+    index = pages.index { |page_info| page_info[:controller] == controller_class && page_info[:item_index] == item_index }
     if index.nil?
-      # we might be missing an item_index param - try looking for a page with item_index 0
+      # if we didn't find a match, try looking for a page with item_index 0
       # rubocop:disable Style/NumericPredicate
-      index = list.index { |page_info| page_info[:controller] == controller_class && page_info[:item_index] == 0 }
+      # rubcop is wrong, and `hash[:key] == 0` is not the same as `hash[:key].zero?` (which will raise if key is missing)
+      index = pages.index { |page_info| page_info[:controller] == controller_class && page_info[:item_index] == 0 }
       # rubocop:enable Style/NumericPredicate
     end
     index
@@ -97,7 +117,7 @@ module ControllerNavigation
           controller_class.model_for_show_check(current_controller),
           current_controller
         )
-      when 1
+      when -1, 1
         controller_class.show?(
           controller_class.model_for_show_check(current_controller)
         )
--- spec/lib/concerns/controller_navigation_spec.rb
+++ spec/lib/concerns/controller_navigation_spec.rb
@@ -16,12 +16,20 @@ RSpec.describe ControllerNavigation do
       end
 
       def visitor_record; end
+
+      def params
+        {}
+      end
+
+      def review_controller; end
     end
 
     class FirstController < BaseController; end
     class SecondController < BaseController; end
     class ThirdController < BaseController; end
     class FourthController < BaseController; end
+
+    class ReviewController < BaseController; end
 
     stub_const("#{navigation_class.name}::FLOW",
                [
@@ -30,8 +38,6 @@ RSpec.describe ControllerNavigation do
                    ThirdController,
                ]
     )
-
-
   end
 
   describe ".controllers" do
@@ -103,6 +109,111 @@ RSpec.describe ControllerNavigation do
         expect(navigation.next).to eq({ controller: FourthController, item_index: 1 })
       end
     end
+
+    context "when return to review params" do
+      before do
+        allow_any_instance_of(BaseController).to receive(:review_controller).and_return(ReviewController)
+      end
+
+      context "is return_to_review=y" do
+        before do
+          allow_any_instance_of(SecondController).to receive(:params).and_return({ return_to_review: "y"})
+        end
+
+        it "returns path for review controller" do
+          navigation = navigation_class.new(SecondController.new)
+          expect(navigation.next).to eq({ controller: ReviewController })
+        end
+      end
+
+      context "has return_to_review_after=controller_name" do
+        before do
+          allow_any_instance_of(FirstController).to receive(:params).and_return({ return_to_review_after: "first_controller"})
+          allow_any_instance_of(SecondController).to receive(:params).and_return({ return_to_review_after: "third_controller"})
+          allow_any_instance_of(ThirdController).to receive(:params).and_return({ return_to_review_after: "third_controller"})
+        end
+
+        it "returns review controller if we are proceeding past the specified controller" do
+          navigation = navigation_class.new(FirstController.new)
+          expect(navigation.next).to eq({ controller: ReviewController })
+        end
+
+        it "returns next controller in flow if we haven't proceeded far enough to return to review yet, and preserves params" do
+          navigation = navigation_class.new(SecondController.new)
+          expect(navigation.next).to eq({ controller: ThirdController, params: { return_to_review_after: "third_controller"} })
+        end
+
+        it "returns review controller if we are proceeding past the specified controller, even if it's the last in the flow" do
+          navigation = navigation_class.new(ThirdController.new)
+          expect(navigation.next).to eq({ controller: ReviewController })
+        end
+      end
+
+      context "has return_to_review_after=step_name" do
+        let(:pages) {
+          [
+            { controller: FirstController },
+            { controller: SecondController },
+            { controller: ThirdController, item_index: 0, step: "repeated_step" },
+            { controller: FourthController, item_index: 0, step: "repeated_step" },
+            { controller: ThirdController, item_index: 1, step: "repeated_step" },
+            { controller: FourthController, item_index: 1, step: "repeated_step" }
+          ]
+        }
+        before do
+          allow(navigation_class).to receive(:pages).and_return(pages)
+        end
+
+        context "without an item index" do
+          before do
+            allow_any_instance_of(BaseController).to receive(:params).and_return({ return_to_review_after: "repeated_step"})
+          end
+
+          it "returns the next page and preserves params when not done with all iterations of the named step" do
+            navigation = navigation_class.new(FourthController.new, item_index: 0)
+            expect(navigation.next).to eq({ controller: ThirdController, item_index: 1, step: "repeated_step", params: { return_to_review_after: "repeated_step"} })
+
+            navigation = navigation_class.new(ThirdController.new, item_index: 1)
+            expect(navigation.next).to eq({ controller: FourthController, item_index: 1, step: "repeated_step", params: { return_to_review_after: "repeated_step"} })
+          end
+
+          it "returns the review controller when done with all iterations of the named step" do
+            navigation = navigation_class.new(FourthController.new, item_index: 1)
+            expect(navigation.next).to eq({ controller: ReviewController })
+          end
+        end
+
+        context "with an item index" do
+          before do
+            allow_any_instance_of(BaseController).to receive(:params).and_return({ return_to_review_after: "repeated_step_0"})
+          end
+
+          it "returns the next page and preserves params when not done with the specified iterations of the named step" do
+            navigation = navigation_class.new(SecondController.new)
+            expect(navigation.next).to eq({ controller: ThirdController, item_index: 0, step: "repeated_step", params: { return_to_review_after: "repeated_step_0"} })
+
+            navigation = navigation_class.new(ThirdController.new, item_index: 0)
+            expect(navigation.next).to eq({ controller: FourthController, item_index: 0, step: "repeated_step", params: { return_to_review_after: "repeated_step_0"} })
+          end
+
+          it "returns the review controller when done with the specified iterations of the named step when it isn't the last step" do
+            navigation = navigation_class.new(FourthController.new, item_index: 0)
+            expect(navigation.next).to eq({ controller: ReviewController })
+          end
+
+          context "when the specified iteration of the named step is at the end of the flow" do
+            before do
+              allow_any_instance_of(BaseController).to receive(:params).and_return({ return_to_review_after: "repeated_step_1"})
+            end
+
+            it "returns the review controller when done with the specified iterations of the named step, even if it's the last step" do
+              navigation = navigation_class.new(FourthController.new, item_index: 1)
+              expect(navigation.next).to eq({ controller: ReviewController })
+            end
+          end
+        end
+      end
+    end
   end
 
   describe "#prev" do
@@ -110,7 +221,7 @@ RSpec.describe ControllerNavigation do
       allow(SecondController).to receive(:show?) { false }
     end
 
-    it "returns path for next non-skipped controller in main flow" do
+    it "returns path for prev non-skipped controller in main flow" do
       navigation = navigation_class.new(ThirdController.new)
       expect(navigation.prev).to eq({ controller: FirstController })
     end
@@ -140,6 +251,141 @@ RSpec.describe ControllerNavigation do
       it "returns the correct next page" do
         navigation = navigation_class.new(ThirdController.new, item_index: 1)
         expect(navigation.prev).to eq({ controller: FourthController, item_index: 0 })
+      end
+    end
+
+    context "when return to review params" do
+      before do
+        allow_any_instance_of(BaseController).to receive(:review_controller).and_return(ReviewController)
+      end
+
+      context "is return_to_review=y" do
+        before do
+          allow_any_instance_of(SecondController).to receive(:params).and_return({ return_to_review: "y"})
+        end
+
+        it "returns path for review controller" do
+          navigation = navigation_class.new(SecondController.new)
+          expect(navigation.prev).to eq({ controller: ReviewController })
+        end
+      end
+
+      context "has return_to_review_before=controller_name" do
+        before do
+          allow(SecondController).to receive(:show?).and_return(true)
+          allow_any_instance_of(FirstController).to receive(:params).and_return({ return_to_review_before: "first_controller"})
+          allow_any_instance_of(SecondController).to receive(:params).and_return({ return_to_review_before: "second_controller"})
+          allow_any_instance_of(ThirdController).to receive(:params).and_return({ return_to_review_before: "second_controller"})
+        end
+
+        it "returns review controller if we are proceeding past the specified controller" do
+          navigation = navigation_class.new(SecondController.new)
+          expect(navigation.prev).to eq({ controller: ReviewController })
+        end
+
+        it "returns previous controller in flow if we haven't proceeded far enough to return to review yet, and preserves params" do
+          navigation = navigation_class.new(ThirdController.new)
+          expect(navigation.prev).to eq({ controller: SecondController, params: { return_to_review_before: "second_controller"} })
+        end
+
+        it "returns review controller if we are proceeding past the specified controller, even if it's the first in the flow" do
+          navigation = navigation_class.new(FirstController.new)
+          expect(navigation.prev).to eq({ controller: ReviewController })
+        end
+      end
+
+      context "has return_to_review_before=step_name" do
+        let(:pages) {
+          [
+            { controller: FirstController },
+            { controller: SecondController },
+            { controller: ThirdController, item_index: 0, step: "repeated_step" },
+            { controller: FourthController, item_index: 0, step: "repeated_step" },
+            { controller: ThirdController, item_index: 1, step: "repeated_step" },
+            { controller: FourthController, item_index: 1, step: "repeated_step" }
+          ]
+        }
+        before do
+          allow(navigation_class).to receive(:pages).and_return(pages)
+        end
+
+        context "without an item index" do
+          before do
+            allow_any_instance_of(BaseController).to receive(:params).and_return({ return_to_review_before: "repeated_step"})
+          end
+
+          it "returns the prev page and preserves params when not done with all iterations of the named step" do
+            navigation = navigation_class.new(FourthController.new, item_index: 1)
+            expect(navigation.prev).to eq({ controller: ThirdController, item_index: 1, step: "repeated_step", params: { return_to_review_before: "repeated_step"} })
+
+            navigation = navigation_class.new(FourthController.new, item_index: 0)
+            expect(navigation.prev).to eq({ controller: ThirdController, item_index: 0, step: "repeated_step", params: { return_to_review_before: "repeated_step"} })
+          end
+
+          it "returns the review controller when done with all iterations of the named step" do
+            navigation = navigation_class.new(ThirdController.new, item_index: 0)
+            expect(navigation.prev).to eq({ controller: ReviewController })
+          end
+
+          context "even if there's nothing before the named step in the flow" do
+            let(:pages) {
+              [
+                { controller: ThirdController, item_index: 0, step: "repeated_step" },
+                { controller: FourthController, item_index: 0, step: "repeated_step" },
+                { controller: ThirdController, item_index: 1, step: "repeated_step" },
+                { controller: FourthController, item_index: 1, step: "repeated_step" },
+                { controller: FirstController },
+                { controller: SecondController }
+              ]
+            }
+
+            it "returns the review controller when done with all iterations of the named step" do
+              navigation = navigation_class.new(ThirdController.new, item_index: 0)
+              expect(navigation.prev).to eq({ controller: ReviewController })
+            end
+          end
+        end
+
+        context "with an item index" do
+          let(:pages) {
+            [
+              { controller: ThirdController, item_index: 0, step: "repeated_step" },
+              { controller: FourthController, item_index: 0, step: "repeated_step" },
+              { controller: ThirdController, item_index: 1, step: "repeated_step" },
+              { controller: FourthController, item_index: 1, step: "repeated_step" },
+              { controller: FirstController },
+              { controller: SecondController }
+            ]
+          }
+
+          before do
+            allow_any_instance_of(BaseController).to receive(:params).and_return({ return_to_review_before: "repeated_step_1"})
+          end
+
+          it "returns the prev page and preserves params when not done with the specified iterations of the named step" do
+            navigation = navigation_class.new(FourthController.new, item_index: 1)
+            expect(navigation.prev).to eq({ controller: ThirdController, item_index: 1, step: "repeated_step", params: { return_to_review_before: "repeated_step_1"} })
+
+            navigation = navigation_class.new(FirstController.new)
+            expect(navigation.prev).to eq({ controller: FourthController, item_index: 1, step: "repeated_step", params: { return_to_review_before: "repeated_step_1"} })
+          end
+
+          it "returns the review controller when done with the specified iterations of the named step when it isn't the first step" do
+            navigation = navigation_class.new(ThirdController.new, item_index: 1)
+            expect(navigation.prev).to eq({ controller: ReviewController })
+          end
+
+          context "when the specified iteration of the named step is at the beginning of the flow" do
+            before do
+              allow_any_instance_of(BaseController).to receive(:params).and_return({ return_to_review_before: "repeated_step_0"})
+            end
+
+            it "returns the review controller when done with the specified iterations of the named step, even if it's the first step" do
+              navigation = navigation_class.new(ThirdController.new, item_index: 0)
+              expect(navigation.prev).to eq({ controller: ReviewController })
+            end
+          end
+        end
       end
     end
 
