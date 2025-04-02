1ef014b68ca817c4c36efdb77bef1526f6dcef95
Arin Choi <achoi@codeforamerica.org>
2025-04-01 21:12:45 -0700

    Fix up az complete intake spec

--- spec/features/state_file/complete_intake_spec.rb
+++ spec/features/state_file/complete_intake_spec.rb
@@ -127,6 +127,7 @@ RSpec.feature "Completing a state file intake", active_job: true, js: true do
 
       expect(page).to have_text I18n.t("state_file.questions.federal_extension_payments.edit.title")
       choose I18n.t("general.affirmative")
+      click_on I18n.t("general.continue")
 
       expect(page).to have_text I18n.t('state_file.questions.primary_state_id.edit.title')
       choose I18n.t('state_file.questions.primary_state_id.state_id.id_type_question.dmv')
