3310b20982fb99a170429750c19307a1e2d8cf3f
Arin Choi <achoi@codeforamerica.org>
2025-04-02 12:16:57 -0700

    Add out_of_country field to NC intakes and front end

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_03_27_180616) do
+ActiveRecord::Schema[7.1].define(version: 2025_04_02_190355) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
   enable_extension "plpgsql"
@@ -2238,6 +2238,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_03_27_180616) do
     t.datetime "locked_at"
     t.jsonb "message_tracker", default: {}
     t.integer "moved_after_hurricane_helene", default: 0, null: false
+    t.integer "out_of_country", default: 0, null: false
     t.integer "payment_or_deposit_type", default: 0, null: false
     t.string "phone_number"
     t.datetime "phone_number_verified_at"
