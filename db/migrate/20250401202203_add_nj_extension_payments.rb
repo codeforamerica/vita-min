50bb58a066093612e0ad9c7738c67ec836b0875d
John Chan <john@innovation.nj.gov>
2025-04-02 10:29:52 -0400

    Added tests and changed schema to allow nil

--- db/migrate/20250401202203_add_nj_extension_payments.rb
+++ db/migrate/20250401202203_add_nj_extension_payments.rb
@@ -1,5 +1,5 @@
 class AddNjExtensionPayments < ActiveRecord::Migration[7.1]
   def change
-    add_column :state_file_nj_intakes, :extension_payments, :integer, default: 0, null: false
+    add_column :state_file_nj_intakes, :extension_payments, :decimal, precision: 12, scale: 2
   end
 end
