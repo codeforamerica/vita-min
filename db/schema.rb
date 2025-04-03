15b6b10c30a59ecd37aa37fc4a71b3059bd4482e
John Chan <john@innovation.nj.gov>
2025-04-03 13:57:25 -0400

    Nj 321 extension payments (#5857)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_03_27_180616) do
+ActiveRecord::Schema[7.1].define(version: 2025_04_01_202203) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
   enable_extension "plpgsql"
@@ -2354,6 +2354,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_03_27_180616) do
     t.datetime "email_address_verified_at"
     t.integer "email_notification_opt_in", default: 0, null: false
     t.decimal "estimated_tax_payments", precision: 12, scale: 2
+    t.decimal "extension_payments", precision: 12, scale: 2
     t.integer "failed_attempts", default: 0, null: false
     t.integer "fed_taxable_income"
     t.integer "fed_wages"
