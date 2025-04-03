b74a1fe9cf499e5985967dcab6767865a7e39f1a
Yejee Arin Choi <achoi@codeforamerica.org>
2025-04-03 12:24:37 -0700

    [FYST-1942] Federal Extension Payments frontend + AZ xml/pdf (#5856)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_04_01_202203) do
+ActiveRecord::Schema[7.1].define(version: 2025_04_01_214001) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
   enable_extension "plpgsql"
@@ -1898,6 +1898,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_04_01_202203) do
     t.integer "made_az322_contributions", default: 0, null: false
     t.jsonb "message_tracker", default: {}
     t.integer "paid_extension_payments", default: 0, null: false
+    t.integer "paid_federal_extension_payments", default: 0, null: false
     t.integer "payment_or_deposit_type", default: 0, null: false
     t.string "phone_number"
     t.datetime "phone_number_verified_at"
