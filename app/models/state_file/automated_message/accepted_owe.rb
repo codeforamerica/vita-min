6702e8a06176f71ff3f9792db2b1d1a55a2eef50
Kevin Diale <powersurge360@gmail.com>
2025-04-08 15:26:26 -0400

    Split into before, after, and during tax deadline for accepted_owe

--- app/models/state_file/automated_message/accepted_owe.rb
+++ app/models/state_file/automated_message/accepted_owe.rb
@@ -14,15 +14,45 @@ module StateFile::AutomatedMessage
     end
 
     def sms_body(**args)
-      I18n.t("messages.state_file.accepted_owe.sms", **args)
+      time = app_time(args)
+
+      i18n_msg = if time.between? tax_deadline.beginning_of_day, tax_deadline
+                   'messages.state_file.accepted_owe.during_deadline.sms'
+                 elsif time > tax_deadline
+                   'messages.state_file.accepted_owe.after_deadline.sms'
+                 else
+                   'messages.state_file.accepted_owe.before_deadline.sms'
+                 end
+
+      I18n.t(i18n_msg, **args)
     end
 
     def email_subject(**args)
-      I18n.t("messages.state_file.accepted_owe.email.subject", **args)
+      time = app_time(args)
+
+      i18n_msg = if time.between? tax_deadline.beginning_of_day, tax_deadline
+                   'messages.state_file.accepted_owe.during_deadline.email.subject'
+                 elsif time > tax_deadline
+                   'messages.state_file.accepted_owe.after_deadline.email.subject'
+                 else
+                   'messages.state_file.accepted_owe.before_deadline.email.subject'
+                 end
+
+      I18n.t(i18n_msg, **args)
     end
 
     def email_body(**args)
-      I18n.t("messages.state_file.accepted_owe.email.body", **args)
+      time = app_time(args)
+
+      i18n_msg = if time.between? tax_deadline.beginning_of_day, tax_deadline
+                   'messages.state_file.accepted_owe.during_deadline.email.body'
+                 elsif time > tax_deadline
+                   'messages.state_file.accepted_owe.after_deadline.email.body'
+                 else
+                   'messages.state_file.accepted_owe.before_deadline.email.body'
+                 end
+
+      I18n.t(i18n_msg, **args)
     end
   end
 end
--- app/models/state_file/automated_message/base_automated_message.rb
+++ app/models/state_file/automated_message/base_automated_message.rb
@@ -9,5 +9,12 @@ module StateFile::AutomatedMessage
     def self.after_transition_notification?
       false
     end
+
+    # Convenience methods for time
+    def tax_deadline = Rails.application.config.tax_deadline
+
+    def app_time(build_args)
+      build_args.fetch(:app_time, Time.current).to_datetime
+    end
   end
 end
