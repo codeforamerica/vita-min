751aa0db7895688a6fc9fe7166f1ef3c1325a8d6
Mike Rotondo <mrotondo@codeforamerica.org>
2025-04-08 11:47:03 -0700

    allow blank on state distribution as well, for the same reason

--- app/models/state_file1099_r.rb
+++ app/models/state_file1099_r.rb
@@ -57,7 +57,8 @@ class StateFile1099R < ApplicationRecord
     validates :state_distribution_amount,
               numericality: {
                 greater_than_or_equal_to: 0,
-                less_than_or_equal_to: BigDecimal(10**10)
+                less_than_or_equal_to: BigDecimal(10**10),
+                allow_blank: true
               },
               presence: {
                 message: proc { I18n.t('forms.errors.no_money_amount') }
