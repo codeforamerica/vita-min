98135c75c3f0ff5a0c873bb790120ca4ded3b604
Drew Proebstel <dproebstel@codeforamerica.org>
2025-04-09 16:07:29 -0700

    comment to get tests to run

--- app/controllers/state_file/questions/nc_taxes_owed_controller.rb
+++ app/controllers/state_file/questions/nc_taxes_owed_controller.rb
@@ -3,6 +3,7 @@ module StateFile
     class NcTaxesOwedController < TaxesOwedController
 
       def edit
+        # try to run tests
         super
         @allow_date_select = StateInformationService.before_payment_deadline?(app_time, current_intake.state_code)
       end
