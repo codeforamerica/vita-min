df0c5d95afbf71409dc444ffde74b63bef2f9313
Kevin Diale <powersurge360@gmail.com>
2025-04-09 18:27:44 -0400

    Updated i18n

--- app/models/state_file/automated_message/accepted_owe.rb
+++ app/models/state_file/automated_message/accepted_owe.rb
@@ -15,43 +15,37 @@ module StateFile::AutomatedMessage
     def sms_body(**args)
       time = app_time(args)
 
-      i18n_msg = if time.between? tax_deadline.beginning_of_day, tax_deadline
-                   'messages.state_file.accepted_owe.during_deadline.sms'
-                 elsif time > tax_deadline
-                   'messages.state_file.accepted_owe.after_deadline.sms'
-                 else
-                   'messages.state_file.accepted_owe.before_deadline.sms'
-                 end
-
-      I18n.t(i18n_msg, **args)
+      if time.between? tax_deadline.beginning_of_day, tax_deadline
+        I18n.t('messages.state_file.accepted_owe.during_deadline.sms', **args)
+      elsif time > tax_deadline
+        I18n.t('messages.state_file.accepted_owe.after_deadline.sms', **args)
+      else
+        I18n.t('messages.state_file.accepted_owe.before_deadline.sms', **args)
+      end
     end
 
     def email_subject(**args)
       time = app_time(args)
 
-      i18n_msg = if time.between? tax_deadline.beginning_of_day, tax_deadline
-                   'messages.state_file.accepted_owe.during_deadline.email.subject'
-                 elsif time > tax_deadline
-                   'messages.state_file.accepted_owe.after_deadline.email.subject'
-                 else
-                   'messages.state_file.accepted_owe.before_deadline.email.subject'
-                 end
-
-      I18n.t(i18n_msg, **args)
+      if time.between? tax_deadline.beginning_of_day, tax_deadline
+        I18n.t('messages.state_file.accepted_owe.during_deadline.email.subject', **args)
+      elsif time > tax_deadline
+        I18n.t('messages.state_file.accepted_owe.after_deadline.email.subject', **args)
+      else
+        I18n.t('messages.state_file.accepted_owe.before_deadline.email.subject', **args)
+      end
     end
 
     def email_body(**args)
       time = app_time(args)
 
-      i18n_msg = if time.between? tax_deadline.beginning_of_day, tax_deadline
-                   'messages.state_file.accepted_owe.during_deadline.email.body'
-                 elsif time > tax_deadline
-                   'messages.state_file.accepted_owe.after_deadline.email.body'
-                 else
-                   'messages.state_file.accepted_owe.before_deadline.email.body'
-                 end
-
-      I18n.t(i18n_msg, **args)
+      if time.between? tax_deadline.beginning_of_day, tax_deadline
+        I18n.t('messages.state_file.accepted_owe.during_deadline.email.body', **args)
+      elsif time > tax_deadline
+        I18n.t('messages.state_file.accepted_owe.after_deadline.email.body', **args)
+      else
+        I18n.t('messages.state_file.accepted_owe.before_deadline.email.body', **args)
+      end
     end
   end
 end
