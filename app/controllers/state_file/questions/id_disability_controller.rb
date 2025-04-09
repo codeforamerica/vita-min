9685666e9e72540889e5ed4eb39b1a879ee8a355
Mike Rotondo <mrotondo@codeforamerica.org>
2025-04-09 13:42:36 -0700

    fix a crash when disability question is left blank

--- app/controllers/state_file/questions/id_disability_controller.rb
+++ app/controllers/state_file/questions/id_disability_controller.rb
@@ -9,7 +9,7 @@ module StateFile
       private
 
       def form_params
-        params.require(:state_file_id_disability_form).permit(:mfj_disability, :primary_disabled, :spouse_disabled)
+        params.fetch(:state_file_id_disability_form, {}).permit(:mfj_disability, :primary_disabled, :spouse_disabled)
       end
     end
   end
