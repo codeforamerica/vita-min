c3b782f79fad2bb225e8e79e70fb6ba08e769af2
Mike Rotondo <mrotondo@codeforamerica.org>
2025-04-10 12:30:46 -0700

    fix continue to file with this service returning to the correct ineligible page and with the return to review params intact

--- app/controllers/state_file/questions/id_ineligible_retirement_and_pension_income_controller.rb
+++ app/controllers/state_file/questions/id_ineligible_retirement_and_pension_income_controller.rb
@@ -2,6 +2,7 @@ module StateFile
   module Questions
     class IdIneligibleRetirementAndPensionIncomeController < RetirementIncomeSubtractionController
       include OtherOptionsLinksConcern
+
       def self.show?(intake, item_index: nil)
         return false unless item_index.present?
         state_file_1099r = load_1099r(intake, item_index)
@@ -10,15 +11,28 @@ module StateFile
           state_file_1099r&.state_specific_followup&.civil_service_account_number_eight?
       end
 
+      def edit
+        current_intake.update(clicked_to_file_with_other_service_at: nil)
+        super
+      end
+
       def file_with_another_service
         load_links
         current_intake.touch(:clicked_to_file_with_other_service_at)
       end
 
-      def continue_filing
-        current_intake.update(clicked_to_file_with_other_service_at: nil)
-        redirect_to next_path
+      def continue_filing_path
+        url_options = { action: :edit, item_index: item_index }
+        url_options.merge!(
+          {
+            return_to_review: params[:return_to_review],
+            return_to_review_before: params[:return_to_review_before],
+            return_to_review_after: params[:return_to_review_after]
+          }.compact
+        )
+        self.class.to_path_helper(url_options)
       end
+      helper_method :continue_filing_path
 
       private
 
