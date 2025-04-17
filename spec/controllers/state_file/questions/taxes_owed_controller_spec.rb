ba1be0b16fb3e17a0fc9f1c81234e909d72e1eb8
John Chan <john@innovation.nj.gov>
2025-04-17 14:18:12 -0400

    Update unit test for taxes_owed_controller

--- spec/controllers/state_file/questions/taxes_owed_controller_spec.rb
+++ spec/controllers/state_file/questions/taxes_owed_controller_spec.rb
@@ -53,15 +53,6 @@ describe StateFile::Questions::TaxesOwedController do
         expect(response_html).to have_text "Routing Number"
       end
 
-      context 'when taxpayer owes more than 400$ in taxes' do
-        let(:intake) { create(:state_file_nj_intake, :df_data_taxes_owed) }
-        it 'displays the underpayment notice' do
-          get :edit
-          expect(response).to be_successful
-          expect(response_html).to have_text "This can happen for many reasons, but common ones are"
-        end
-      end
-
       context 'when taxpayer owes less than 400$ in taxes' do
         let(:intake) { create(:state_file_nj_intake, :df_data_minimal) }
         it 'does not display the underpayment notice' do
