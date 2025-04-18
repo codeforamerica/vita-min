2cc4272e7fcc4e9f1fb4558351501fc112adbc2a
Drew Proebstel <dproebstel@codeforamerica.org>
2025-04-18 12:46:33 -0700

    Hide nc-subtractions page for filers with negative FAGI

--- app/controllers/state_file/questions/nc_subtractions_controller.rb
+++ app/controllers/state_file/questions/nc_subtractions_controller.rb
@@ -3,6 +3,10 @@ module StateFile
     class NcSubtractionsController < QuestionsController
       before_action :set_ivars, only: [:edit, :update]
 
+      def self.show?(intake)
+        intake.direct_file_data.fed_agi.positive?
+      end
+
       def set_ivars
         @subtractions_limit = current_intake.calculator.subtractions_limit
       end
