a06d2d724d617e29c396762c9b16a5c4fbe5979c
Hugo Melo <hmelo@codeforamerica.org>
2025-04-22 14:45:52 -0400

    merge main db schema change
    Co-authored-by: Hugo Melo <hmelo@codeforamerica.org>

--- db/schema.rb
+++ db/schema.rb
@@ -1884,7 +1884,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_04_14_203545) do
     t.citext "email_address"
     t.datetime "email_address_verified_at"
     t.integer "email_notification_opt_in", default: 0, null: false
-    t.decimal "extension_payments_amount", precision: 12, scale: 2
+    t.decimal "extension_payments_amount", precision: 12, scale: 2, default: "0.0"
     t.integer "failed_attempts", default: 0, null: false
     t.string "federal_return_status"
     t.string "federal_submission_id"
