b658eaca0ca1eccb457cfb04b443f56a3b0c5087
Jenny Heath <jheath@codeforamerica.org>
2025-04-22 16:57:15 -0700

    strip whitespace from email in PYA flow

--- app/forms/state_file/archived_intakes/email_address_form.rb
+++ app/forms/state_file/archived_intakes/email_address_form.rb
@@ -1,7 +1,10 @@
 module StateFile
   module ArchivedIntakes
     class EmailAddressForm < Form
+      include FormAttributes
+
       attr_accessor :email_address
+      before_validation_squish :email_address
 
       validates :email_address, presence: true, 'valid_email_2/email': true
     end
