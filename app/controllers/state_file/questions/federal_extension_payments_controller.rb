7d0c6945a09ab44e94e699086a71200da4d3e5aa
Drew Proebstel <dproebstel@codeforamerica.org>
2025-04-22 09:22:43 -0700

    extra space

--- app/controllers/state_file/questions/federal_extension_payments_controller.rb
+++ app/controllers/state_file/questions/federal_extension_payments_controller.rb
@@ -1,6 +1,7 @@
 module StateFile
   module Questions
     class FederalExtensionPaymentsController < QuestionsController
+      
       def self.show?(intake)
         Flipper.enabled?(:extension_period)
       end
