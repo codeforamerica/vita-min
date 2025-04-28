e4c4c72d3871848e2d261c03b4b7574e3e6f49d5
Arin Choi <achoi@codeforamerica.org>
2025-04-28 11:42:27 -0700

    Do not set Rack::MiniProfiler configurations for prod

--- config/initializers/rack_profiler.rb
+++ config/initializers/rack_profiler.rb
@@ -3,7 +3,7 @@ if Rails.env.heroku? || Rails.env.demo?
   Rack::MiniProfiler.config.start_hidden = true
 end
 
-if ENV['REDIS_URL'].present?
+if ENV['REDIS_URL'].present? && (Rails.env.demo? || Rails.env.development? || Rails.env.heroku? || Rails.env.staging?)
   Rack::MiniProfiler.config.storage_options = { url: ENV["REDIS_URL"] }
   Rack::MiniProfiler.config.storage = Rack::MiniProfiler::RedisStore
 end
