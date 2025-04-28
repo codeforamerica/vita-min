3c1532aff7ef5fcc28bd900069706d9544d3556c
Hugo Melo <hmelo@codeforamerica.org>
2025-04-28 10:42:37 -0400

    disable bullet for after these fixes
    Co-authored-by: Hugo Melo <hmelo@codeforamerica.org>

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -7,7 +7,7 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   config.after_initialize do
-    Bullet.enable        = true
+    Bullet.enable        = false
     Bullet.bullet_logger = true
     # raise an error if n+1 query occurs
     Bullet.raise         = true
