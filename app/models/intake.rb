5ab84d879fb9ed31d1336ccab5a5ad5a9f84b8f8
Em Barnard-Shao <ebarnard@codeforamerica.org>
2025-05-19 20:53:33 -0400

    Change mixpanel service stuff

--- app/models/intake.rb
+++ app/models/intake.rb
@@ -616,7 +616,12 @@ class Intake < ApplicationRecord
   end
 
   def needs_help_with_backtaxes?
-    MultiTenantService.new(:gyr).backtax_years.any? { |year| send("needs_help_#{year}_yes?") }
+    if (product_year == 2022)
+      # TODO: eventually we will remove this when we remove the needs_help_2021, needs_help_2019, needs_help_2018, needs_help_2017 fields
+      MultiTenantService.new(:gyr).backtax_years.any? { |year| send("needs_help_#{year}_yes?") }
+    else
+      needs_help_previous_year_1_yes? || needs_help_previous_year_2_yes? || needs_help_previous_year_3_yes?
+    end
   end
 
   def update_or_create_13614c_document(filename)
