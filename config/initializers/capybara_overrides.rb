4cc7afa0f147af4ec67d00c2abfbd5e12c4240d3
Em Barnard-Shao <ebarnard@codeforamerica.org>
2025-05-19 21:09:06 -0400

    Ensure file is not loaded when capybara is not loaded

--- config/initializers/capybara_overrides.rb
+++ config/initializers/capybara_overrides.rb
@@ -1,7 +1,13 @@
-class Capybara::Window
-  def resize_to(width, height, override: false)
-    return super(width, height) unless override
+if Rails.env.test?
+  require 'capybara'
 
-    wait_for_stable_size(5.seconds) { @driver.resize_window_to(handle, width, height) }
+  if defined?(Capybara::Window)
+    class Capybara::Window
+      def resize_to(width, height, override: false)
+        return super(width, height) unless override
+
+        wait_for_stable_size(5.seconds) { @driver.resize_window_to(handle, width, height) }
+      end
+    end
   end
 end
