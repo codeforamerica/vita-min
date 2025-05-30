edba0238ceae427cba4a2d280e5ae74a20fbdf2d
Em Barnard-Shao <ebarnard@codeforamerica.org>
2025-05-30 15:28:57 -0400

    Fix

--- app/models/intake_archive.rb
+++ app/models/intake_archive.rb
@@ -15,7 +15,6 @@
 class IntakeArchive < ApplicationRecord
   belongs_to :intake, foreign_key: :id
   enum needs_help_2017: { unfilled: 0, yes: 1, no: 2 }, _prefix: :needs_help_2017
-  # TODO: do I need to add the other fields here?
   enum spouse_was_on_visa: { unfilled: 0, yes: 1, no: 2 }, _prefix: :spouse_was_on_visa
   enum was_on_visa: { unfilled: 0, yes: 1, no: 2, unsure: 3 }, _prefix: :was_on_visa
   enum had_student_in_family: { unfilled: 0, yes: 1, no: 2, unsure: 3 }, _prefix: :had_student_in_family
