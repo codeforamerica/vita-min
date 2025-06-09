60193ecde406fd23b36291c002002f892af608cc
Jenny Heath <jheath@codeforamerica.org>
2025-06-09 15:52:10 -0700

    Delete IdVerificationExperimentService
    Co-authored-by: Anisha Ramnani <aramnani@codeforamerica.org>

--- spec/controllers/documents/selfie_instructions_controller_spec.rb
+++ spec/controllers/documents/selfie_instructions_controller_spec.rb
@@ -5,38 +5,7 @@ RSpec.describe Documents::SelfieInstructionsController do
   let(:intake) { create :intake }
 
   before do
-    Experiment.update_all(enabled: true)
-
     sign_in intake.client
-  end
-
-  describe ".show?" do
-    context "they aren't in the experiment" do
-      it "returns true" do
-        expect(subject.class.show?(intake)).to eq true
-      end
-    end
-
-    context "they are in the experiment" do
-      before do
-        experiment = Experiment.find_by(key: ExperimentService::ID_VERIFICATION_EXPERIMENT)
-        ExperimentParticipant.create!(experiment: experiment, record: intake, treatment: treatment)
-      end
-
-      context "they aren't receiving the skip selfie experiment treatment" do
-        let(:treatment) { :control }
-        it "returns true" do
-          expect(subject.class.show?(intake)).to eq true
-        end
-      end
-
-      context "they are receiving the skip selfie experiment treatment" do
-        let(:treatment) { :no_selfie }
-        it "returns false" do
-          expect(subject.class.show?(intake)).to eq false
-        end
-      end
-    end
   end
 
   describe "#delete" do
