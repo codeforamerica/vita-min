6bb0ecce3464b0688f1330655b53305fab7492bd
Em Barnard-Shao <ebarnard@codeforamerica.org>
2025-06-12 15:33:08 -0400

    Add test for dashboard presenter

--- spec/presenters/hub/dashboard_presenter_spec.rb
+++ spec/presenters/hub/dashboard_presenter_spec.rb
@@ -5,6 +5,7 @@ describe Hub::Dashboard::DashboardPresenter do
   let(:ability) { Ability.new(user) }
   let(:coalition) { create :coalition }
   let!(:oregano_org) { create :organization, name: "Oregano Org", coalition: coalition }
+  let(:site) { create :site, name: "Shell Site", parent_organization: oregano_org }
   let!(:orangutan_organization) { create :organization, name: "Orangutan Organization", coalition: coalition }
 
   context "with a coalition lead user" do
@@ -39,6 +40,28 @@ describe Hub::Dashboard::DashboardPresenter do
 
     it "selects the correct organization" do
       expect(subject.selected_model).to eq oregano_org
+    end
+
+    context "with an invalid organization selection" do
+      let(:selected) { "coalition/#{coalition.id}" }
+
+      it "fails to select" do
+        expect(subject.selected_model).to be_nil
+      end
+    end
+  end
+
+  context "with a team member user" do
+    let(:user) { create :team_member_user, sites: [site] }
+    let(:selected) { "site/#{site.id}" }
+
+    it "presents filter options including only site" do
+      expect(subject.filter_options.length).to eq 1
+      expect(subject.filter_options.map(&:model).map(&:name)).to eq ["Shell Site"]
+    end
+
+    it "selects the correct site" do
+      expect(subject.selected_model).to eq site
     end
 
     context "with an invalid organization selection" do
