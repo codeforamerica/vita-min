71671f02aa74193502d172ea1ae1d3e0ebed4ff2
Em Barnard-Shao <ebarnard@codeforamerica.org>
2025-07-24 15:57:18 -0400

    Annotate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_07_03_191519) do
+ActiveRecord::Schema[7.1].define(version: 2025_07_21_202752) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
   enable_extension "plpgsql"
@@ -1374,6 +1374,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_07_03_191519) do
     t.integer "paid_school_supplies", default: 0, null: false
     t.integer "paid_self_employment_expenses", default: 0, null: false
     t.integer "paid_student_loan_interest", default: 0, null: false
+    t.integer "payment_in_installments", default: 0, null: false
     t.string "phone_carrier"
     t.string "phone_number"
     t.integer "phone_number_can_receive_texts", default: 0, null: false
@@ -1824,6 +1825,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_07_03_191519) do
   end
 
   create_table "state_file_archived_intakes", force: :cascade do |t|
+    t.integer "contact_preference", default: 0, null: false
     t.datetime "created_at", null: false
     t.string "email_address"
     t.integer "failed_attempts", default: 0, null: false
@@ -1837,6 +1839,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_07_03_191519) do
     t.string "mailing_street"
     t.string "mailing_zip"
     t.datetime "permanently_locked_at"
+    t.string "phone_number"
     t.string "state_code"
     t.integer "tax_year"
     t.boolean "unsubscribed_from_email", default: false, null: false
@@ -2901,7 +2904,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_07_03_191519) do
   create_table "vita_providers", force: :cascade do |t|
     t.string "appointment_info"
     t.boolean "archived", default: false, null: false
-    t.geography "coordinates", limit: {:srid=>4326, :type=>"st_point", :geographic=>true}
+    t.geography "coordinates", limit: {srid: 4326, type: "st_point", geographic: true}
     t.datetime "created_at", precision: nil
     t.string "dates"
     t.string "details"
