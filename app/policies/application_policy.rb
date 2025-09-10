2a53199e430d3af51f39a2c58cabd43071a1dcbc
em barnard-shao <49880002+embarnard@users.noreply.github.com>
2025-09-10 16:39:07 -0400

    Add pundit, application, efile-error and user policy (#6008)
    Co-authored-by: Kevin Diale <kdiale@codeforamerica.org>

---
+++ app/policies/application_policy.rb
@@ -0,0 +1,58 @@
+class ApplicationPolicy
+  attr_reader :user, :record
+
+  def initialize(user, record)
+    @user = user
+    @record = record
+  end
+
+  def index?
+    false
+  end
+
+  def show?
+    false
+  end
+
+  def create?
+    false
+  end
+
+  def new?
+    create?
+  end
+
+  def update?
+    false
+  end
+
+  def edit?
+    update?
+  end
+
+  def destroy?
+    false
+  end
+
+  private
+
+  def record_exists_in_scope
+    resolved = Pundit.policy_scope!(user, record.class)
+    resolved.where(id: record.id).exists?
+  end
+
+  class Scope
+    def initialize(user, scope)
+      @user = user
+      @scope = scope
+    end
+
+    def resolve
+      raise NoMethodError, "You must define #resolve in #{self.class}"
+    end
+
+    private
+
+    attr_reader :user, :scope
+  end
+end
