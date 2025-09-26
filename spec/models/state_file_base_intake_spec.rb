3f5442796011fd0f9085b242ec3276a47ed9ff5a
Em Barnard-Shao <ebarnard@codeforamerica.org>
2025-09-26 10:58:12 -0400

    clean up

--- spec/models/state_file_base_intake_spec.rb
+++ spec/models/state_file_base_intake_spec.rb
@@ -592,29 +592,26 @@ describe StateFileBaseIntake do
   describe ".selected_intakes_for_deadline_reminder_soon_notifications" do
     let!(:az_intake_with_email_notifications_and_df_import) {
       create :state_file_az_intake,
-             df_data_imported_at: 2.minutes.ago,
              email_address: 'test_1@example.com',
              email_address_verified_at: 5.minutes.ago,
              email_notification_opt_in: 1
     }
     let!(:az_intake_from_last_year) {
       create :state_file_az_intake,
-             df_data_imported_at: 2.minutes.ago,
              email_address: 'test_2@example.com',
              email_address_verified_at: 5.minutes.ago,
              email_notification_opt_in: 1,
-             created_at: (1.year.ago)
+             created_at: 1.year.ago
     }
     let!(:az_intake_with_email_notifications_without_df_import) {
       create :state_file_az_intake,
-             df_data_imported_at: nil,
+             raw_direct_file_data: nil,
              email_address: 'test_3@example.com',
              email_address_verified_at: 5.minutes.ago,
              email_notification_opt_in: 1
     }
     let!(:az_intake_with_text_notifications_and_df_import) {
       create :state_file_az_intake,
-             df_data_imported_at: 2.minutes.ago,
              email_address: 'test_4@example.com',
              phone_number: "+15551115511",
              sms_notification_opt_in: 1,
@@ -622,17 +619,14 @@ describe StateFileBaseIntake do
     }
     let!(:az_intake_with_unverified_text_notifications_and_df_import) {
       create :state_file_az_intake,
-             df_data_imported_at: 2.minutes.ago,
              email_address: 'test_5@example.com',
              phone_number: "+15551115511",
              sms_notification_opt_in: "yes",
-             email_address: 'test@example.com',
              email_address_verified_at: 5.minutes.ago,
              email_notification_opt_in: "no"
     }
     let!(:az_intake_submitted) {
       create :state_file_az_intake,
-             df_data_imported_at: 2.minutes.ago,
              email_address: 'test_6@example.com',
              email_address_verified_at: 5.minutes.ago,
              email_notification_opt_in: 1
@@ -643,8 +637,7 @@ describe StateFileBaseIntake do
              filing_status: :married_filing_separately,
              email_address: "test_7@example.com",
              email_address_verified_at: 1.hour.ago,
-             email_notification_opt_in: 1,
-             df_data_imported_at: 2.minutes.ago
+             email_notification_opt_in: 1
     }
     let!(:az_intake_submitted_ssn_duplicate) {
       create :state_file_az_intake,
@@ -652,26 +645,24 @@ describe StateFileBaseIntake do
              email_address_verified_at: 1.hour.ago,
              email_notification_opt_in: 1,
              phone_number: nil,
-             df_data_imported_at: 2.minutes.ago,
-             hashed_ssn: "111443333"
-    }
-    let!(:az_intake_submitted_ssn_duplicate_1) {
-      create :state_file_az_intake,
-             email_address: "test@example.com",
-             email_address_verified_at: 1.hour.ago,
-             email_notification_opt_in: 1,
-             phone_number: nil,
-             df_data_imported_at: 2.minutes.ago,
              hashed_ssn: "111443333"
     }
     let!(:efile_submission_for_duplicate) { create :efile_submission, :for_state, data_source: az_intake_submitted_ssn_duplicate }
+    let!(:az_intake_submitted_ssn_duplicate_1) {
+      create :state_file_az_intake,
+             email_address: "test_9@example.com",
+             email_address_verified_at: 1.hour.ago,
+             email_notification_opt_in: 1,
+             phone_number: nil,
+             hashed_ssn: "111443333"
+    }
 
     before do
       allow(Flipper).to receive(:enabled?).and_call_original
       allow(Flipper).to receive(:enabled?).with(:prevent_duplicate_ssn_messaging).and_return(true)
     end
 
-    it "returns intakes with verified contact info, with or without df data, and without efile submissions or duplicate (same hashed_ssn) intake with efile submission" do
+    it "returns intakes with verified contact info, with or without df data, and with efile submissions or duplicate (same hashed_ssn) intake with efile submission" do
       results = StateFileAzIntake.selected_intakes_for_deadline_reminder_soon_notifications
       intakes_to_message = [
         az_intake_with_email_notifications_and_df_import,
@@ -679,7 +670,7 @@ describe StateFileBaseIntake do
         az_intake_with_email_notifications_without_df_import,
         az_intake_with_unverified_text_notifications_and_df_import,
         az_intake_has_disqualifying_df_data,
-        az_intake_submitted_ssn_duplicate
+        az_intake_submitted_ssn_duplicate_1
       ]
       expect(results).to match_array(intakes_to_message)
     end
