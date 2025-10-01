40c0548f1db67fa6c2beb0345741409501bd4b59
Mike Rotondo <mrotondo@codeforamerica.org>
2025-10-01 16:26:19 -0700

    welcome message copy changes

--- app/models/state_file/automated_message/welcome.rb
+++ app/models/state_file/automated_message/welcome.rb
@@ -9,14 +9,23 @@ module StateFile::AutomatedMessage
     end
 
     def sms_body(**args)
-      I18n.t("messages.state_file.welcome.sms", **args)
+      if Flipper.enabled?(:immediate_df_closure)
+        I18n.t("messages.state_file.welcome.immediate_df_closure_sms", **args)
+      else
+        I18n.t("messages.state_file.welcome.sms", **args)
+      end
     end
 
     def email_subject(**args)
-      I18n.t("messages.state_file.welcome.email.subject", **args)
+      if Flipper.enabled?(:immediate_df_closure)
+        I18n.t("messages.state_file.welcome.email.immediate_df_closure_subject", **args)
+      else
+        I18n.t("messages.state_file.welcome.email.subject", **args)
+      end
     end
 
     def email_body(**args)
+
       I18n.t("messages.state_file.welcome.email.body", **args)
     end
   end
