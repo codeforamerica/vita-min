96b3b2a9edcf67d6a1f77b68736cfbb29b39d2d9
Tahsina Islam <tislam@codeforamerica.org>
2025-12-11 11:24:40 -0600

    Fix migration files

--- db/migrate/20251201215441_add_new_vehicle_purchased_to_intakes.rb
+++ db/migrate/20251201215441_add_new_vehicle_purchased_to_intakes.rb
@@ -1,6 +1,5 @@
 class AddNewVehiclePurchasedToIntakes < ActiveRecord::Migration[7.1]
   def change
     add_column :intakes, :new_vehicle_purchased, :integer, default: 0
-    add_column :intakes, :vin_number, :string
   end
 end
