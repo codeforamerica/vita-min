549e40600d30309f3db6a24a5bc5edae71966a0e
Em Barnard-Shao <ebarnard@codeforamerica.org>
2026-01-12 15:21:37 -0500

    Normalize and annotate

--- spec/factories/campaign_contacts.rb
+++ spec/factories/campaign_contacts.rb
@@ -1,3 +1,34 @@
+# == Schema Information
+#
+# Table name: campaign_contacts
+#
+#  id                        :bigint           not null, primary key
+#  email_address             :citext
+#  email_notification_opt_in :boolean          default(FALSE)
+#  first_name                :string           not null
+#  gyr_2025_preseason_email  :datetime
+#  gyr_2025_preseason_sms    :datetime
+#  gyr_intake_ids            :bigint           default([]), is an Array
+#  last_name                 :string
+#  locale                    :string
+#  sign_up_ids               :bigint           default([]), is an Array
+#  sms_notification_opt_in   :boolean          default(FALSE)
+#  sms_phone_number          :string
+#  state_file_intake_refs    :jsonb            not null
+#  created_at                :datetime         not null
+#  updated_at                :datetime         not null
+#
+# Indexes
+#
+#  index_campaign_contacts_on_email_address              (email_address) UNIQUE WHERE (email_address IS NOT NULL)
+#  index_campaign_contacts_on_email_notification_opt_in  (email_notification_opt_in)
+#  index_campaign_contacts_on_first_name_and_last_name   (first_name,last_name)
+#  index_campaign_contacts_on_gyr_intake_ids             (gyr_intake_ids) USING gin
+#  index_campaign_contacts_on_sign_up_ids                (sign_up_ids) USING gin
+#  index_campaign_contacts_on_sms_notification_opt_in    (sms_notification_opt_in)
+#  index_campaign_contacts_on_sms_phone_number           (sms_phone_number)
+#  index_campaign_contacts_on_state_file_intake_refs     (state_file_intake_refs) USING gin
+#
 FactoryBot.define do
   factory :campaign_contact do
     sequence(:email_address) { |n| "person#{n}@example.com" }
