8ea722da2ee51761ed76799bb5be70c550f38fd4
spompea-cfa <170136902+spompea-cfa@users.noreply.github.com>
2026-01-16 17:39:05 -0500

    add send_mixpanel_event

--- app/controllers/vita_providers_controller.rb
+++ app/controllers/vita_providers_controller.rb
@@ -6,6 +6,7 @@ class VitaProvidersController < ApplicationController
   end
 
   def index
+    send_mixpanel_event(event_name: "provider_page_view", data: {})
     redirect_to IRS_VITA_SITE_LOCATOR_URL, allow_other_host: true
   end
 end
