80d100e7a97db716de9fdc9ac80c2ef258a3d95e
Em Barnard-Shao <ebarnard@codeforamerica.org>
2026-02-03 20:09:00 -0500

    Add to new route

--- app/controllers/mailgun_webhooks_controller.rb
+++ app/controllers/mailgun_webhooks_controller.rb
@@ -1,6 +1,7 @@
 class MailgunWebhooksController < ActionController::Base
   skip_before_action :verify_authenticity_token
-  before_action :authenticate_mailgun_request
+  before_action :authenticate_mailgun_request, except: [:update_campaign_email_status]
+  before_action :authenticate_outreach_mailgun_request, only: [:update_campaign_email_status]
   before_action :re_optin_when_client_replies, only: :create_incoming_email
 
   REGEX_FROM_ENVELOPE = /.*\<(?<address>(.*))>/.freeze
@@ -158,6 +159,15 @@ class MailgunWebhooksController < ActionController::Base
     head :ok
   end
 
+  def update_campaign_email_status
+    message_id = params.dig("event-data", "message", "headers", "message-id")
+    email_to_update = CampaignEmail.find_by(mailgun_message_id: message_id)
+    DatadogApi.increment("mailgun.update_campaign_email_status.email_not_found") if email_to_update.nil?
+
+    email_to_update&.update(mailgun_status: params.dig("event-data", "event"))
+    head :ok
+  end
+
   private
 
   def re_optin_when_client_replies
@@ -192,6 +202,15 @@ class MailgunWebhooksController < ActionController::Base
     authenticate_or_request_with_http_basic do |name, password|
       expected_name = EnvironmentCredentials.dig(:mailgun, :basic_auth_name)
       expected_password = EnvironmentCredentials.dig(:mailgun, :basic_auth_password)
+      ActiveSupport::SecurityUtils.secure_compare(name, expected_name) &&
+        ActiveSupport::SecurityUtils.secure_compare(password, expected_password)
+    end
+  end
+
+  def authenticate_outreach_mailgun_request
+    authenticate_or_request_with_http_basic do |name, password|
+      expected_name = ENV["MAILGUN_OUTREACH_BASIC_AUTH_NAME"]
+      expected_password = ENV["MAILGUN_OUTREACH_BASIC_AUTH_PASSWORD"]
       ActiveSupport::SecurityUtils.secure_compare(name, expected_name) &&
         ActiveSupport::SecurityUtils.secure_compare(password, expected_password)
     end
--- config/routes.rb
+++ config/routes.rb
@@ -399,6 +399,7 @@ Rails.application.routes.draw do
     # Mailgun webhook routes
     post "/incoming_emails", to: "mailgun_webhooks#create_incoming_email", as: :incoming_emails
     post "/outgoing_email_status", to: "mailgun_webhooks#update_outgoing_email_status", as: :outgoing_email_status
+    post "/campaign_email_status", to: "mailgun_webhooks#update_campaign_email_status", as: :campaign_email_status
 
     # OAuth login callback routes
     devise_for :users, path: "hub", only: :omniauth_callbacks, skip: [:session, :invitation], controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
