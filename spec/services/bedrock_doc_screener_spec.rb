9b8614ef2c60ab762693503dcd6817644744fe21
Tahsina Islam <tislam@codeforamerica.org>
2026-02-04 12:41:10 -0600

    GYR1-918 Resolve pdf conversion for doc screener

--- spec/services/bedrock_doc_screener_spec.rb
+++ spec/services/bedrock_doc_screener_spec.rb
@@ -218,12 +218,9 @@ describe BedrockDocScreener do
 
     before do
       allow(upload).to receive(:download).and_return("%PDF-1.4 ... fake pdf bytes ...") # pretend the "download" returns the PDF bytes
-      fake_image = double("MiniMagick::Image")
-      fake_pages = [double("page1"), double("page2")] # fake MiniMagick image with two pages
-      convert_stub = double("MiniMagick::Tool::Convert").as_null_object
-      allow(MiniMagick::Image).to receive(:open).and_return(fake_image)
-      allow(fake_image).to receive(:pages).and_return(fake_pages)
-      allow(MiniMagick::Tool::Convert).to receive(:new).and_yield(convert_stub)
+      allow(Dir).to receive(:mktmpdir).and_yield("/tmp/fake_dir")
+      allow(described_class).to receive(:system).and_return(true)
+      allow(Dir).to receive(:glob).and_return(%w[/tmp/fake_dir/page-1.png /tmp/fake_dir/page-2.png])
       allow(File).to receive(:binread).and_return("png-binary-data")
     end
 
@@ -238,7 +235,7 @@ describe BedrockDocScreener do
     end
 
     it "raises error when conversion fails" do
-      allow(MiniMagick::Image).to receive(:open).and_raise(MiniMagick::Error.new("boom"))
+      allow(described_class).to receive(:system).and_return(false)
 
       expect { described_class.pdf_to_png_base64(upload) }.to raise_error(/failed to convert pdf pages to images/)
     end
