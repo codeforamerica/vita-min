036225ea51d04a8e4406495548b4c59874cce0fe
Em Barnard-Shao <ebarnard@codeforamerica.org>
2026-02-09 12:54:46 -0500

    annotate

--- spec/models/campaign_email_spec.rb
+++ spec/models/campaign_email_spec.rb
@@ -1,3 +1,32 @@
+# == Schema Information
+#
+# Table name: campaign_emails
+#
+#  id                  :bigint           not null, primary key
+#  error_code          :string
+#  event_data          :jsonb
+#  from_email          :string
+#  mailgun_status      :string           default("created"), not null
+#  message_name        :string
+#  scheduled_send_at   :datetime
+#  sent_at             :datetime
+#  subject             :text
+#  to_email            :string
+#  created_at          :datetime         not null
+#  updated_at          :datetime         not null
+#  campaign_contact_id :bigint           not null
+#  mailgun_message_id  :string
+#
+# Indexes
+#
+#  index_campaign_emails_on_campaign_contact_id          (campaign_contact_id)
+#  index_campaign_emails_on_contact_id_and_message_name  (campaign_contact_id,message_name) UNIQUE
+#  index_campaign_emails_on_mailgun_message_id           (mailgun_message_id) UNIQUE
+#
+# Foreign Keys
+#
+#  fk_rails_...  (campaign_contact_id => campaign_contacts.id)
+#
 require "rails_helper"
 
 RSpec.describe CampaignEmail, type: :model do
