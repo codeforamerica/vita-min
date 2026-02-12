c9b470200ae2fd5480bf1fc9d36153fe2436d5e7
Em Barnard-Shao <ebarnard@codeforamerica.org>
2026-02-12 16:38:48 -0500

    schema chang

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_02_10_020905) do
+ActiveRecord::Schema[7.1].define(version: 2026_02_12_205449) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
   enable_extension "plpgsql"
