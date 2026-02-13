fa9fa42f3a5fc9aba1a126e1455e943641d91027
Em Barnard-Shao <ebarnard@codeforamerica.org>
2026-02-13 15:15:20 -0500

    add test

--- spec/controllers/redirects_controller_spec.rb
+++ spec/controllers/redirects_controller_spec.rb
@@ -2,12 +2,33 @@ require "rails_helper"
 
 RSpec.describe RedirectsController, type: :controller do
   describe "GET #outreach" do
+
     it "redirects to the outreach URL" do
-      get :outreach
+      get :outreach, params: { locale: nil }
 
       expect(response).to redirect_to(
-                            "http://test.host/en?utm_campaign=w1&utm_medium=sms&utm_source=gyr"
+                            "http://test.host/?utm_campaign=w1&utm_medium=sms&utm_source=gyr"
                           )
+    end
+
+    context "when locale is english" do
+      it "redirects to the english outreach URL" do
+        get :outreach, params: { locale: :en }
+
+        expect(response).to redirect_to(
+                              "http://test.host/en?utm_campaign=w1&utm_medium=sms&utm_source=gyr"
+                            )
+      end
+    end
+
+    context "when locale is spanish" do
+      it "redirects to the spanish outreach URL" do
+        get :outreach, params: { locale: :es }
+
+        expect(response).to redirect_to(
+                              "http://test.host/es?utm_campaign=w1&utm_medium=sms&utm_source=gyr"
+                            )
+      end
     end
 
     it "returns a 302 status" do
