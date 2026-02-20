7ee39b7c5707a1593c7caa9f8692e3f56347edc9
spompea-cfa <170136902+spompea-cfa@users.noreply.github.com>
2026-02-20 03:45:00 -0500

    spec

--- app/forms/file_yourself_form.rb
+++ app/forms/file_yourself_form.rb
@@ -3,10 +3,6 @@ class FileYourselfForm < Form
   attr_accessor :diy_intake
   set_attributes_for :diy_intake, :email_address, :preferred_first_name, :filing_frequency, :referrer, :locale, :visitor_id, :source
 
-  #validates :email_address, presence: true, 'valid_email_2/email': { mx: true }
-  #validates :preferred_first_name, presence: true
-  #validates :filing_frequency, presence: true
-
   def initialize(diy_intake = nil, params = {})
     @diy_intake = diy_intake
     super(params)
