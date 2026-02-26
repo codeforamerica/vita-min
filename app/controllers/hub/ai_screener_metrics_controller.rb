87e79ada1aaabb25f1d8ddc8061ac48f72ebee13
Drew Proebstel <dproebstel@codeforamerica.org>
2026-02-26 15:55:44 -0800

    ai screener metrics page

--- app/controllers/hub/ai_screener_metrics_controller.rb
+++ app/controllers/hub/ai_screener_metrics_controller.rb
@@ -3,7 +3,7 @@ module Hub
     before_action :require_admin
 
     def show
-      @ai_accuracy = Document
+      @metrics = AiScreenerMetricsService.new.call
     end
   end
 end
--- app/controllers/hub/analytics_controller.rb
+++ app/controllers/hub/analytics_controller.rb
@@ -1,11 +1,8 @@
 module Hub
   class AnalyticsController < Hub::BaseController
     before_action :require_admin
-    load_and_authorize_resource :client
-    load_and_authorize_resource :user, parent: false, only: [:index]
-    layout "hub"
 
-    def index
+    def show
     end
   end
 end
