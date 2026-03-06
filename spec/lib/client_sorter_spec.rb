933b021b6f3bf4e241a3e76a433eebd0a13e55bb
Em Barnard-Shao <ebarnard@codeforamerica.org>
2026-03-05 19:34:46 -0500

    removed redundant test and setup

--- spec/lib/client_sorter_spec.rb
+++ spec/lib/client_sorter_spec.rb
@@ -5,82 +5,63 @@ RSpec.describe ClientSorter do
   let(:user) { create :user, role: user_role }
   let(:subject) { described_class.new(Client.all, user, params, {}) }
 
-  before do
-    allow(subject).to receive(:current_user).and_return(user)
-  end
-
-
   describe "#filtered_and_sorted_clients" do
     context "when user is a greeter" do
       let(:user_role) { build(:greeter_role) }
       let(:params) { {} }
 
-      context "with a broad input scope" do
-        let(:subject) { described_class.new(Client.all, user, params, {}) }
+      let!(:assigned_tax_return) { create(:gyr_tax_return, :prep_ready_for_prep, assigned_user: user) }
+      let!(:other_user) { create(:user) }
+      let!(:other_assigned_tax_return) { create(:gyr_tax_return, :prep_ready_for_prep, assigned_user: other_user) }
 
-        let!(:assigned_tax_return) do
-          create(:gyr_tax_return, :prep_ready_for_prep, assigned_user: user)
+      context "there are greetable clients" do
+        let!(:greetable_tax_return) { create(:gyr_tax_return, :intake_ready) }
+
+        before do
+          [assigned_tax_return, other_assigned_tax_return, greetable_tax_return].each do |tr|
+            tr.client.update!(filterable_product_year: Rails.configuration.product_year)
+          end
+          SearchIndexer.refresh_filterable_properties([
+                                                        assigned_tax_return.client_id,
+                                                        other_assigned_tax_return.client_id,
+                                                        greetable_tax_return.client_id
+                                                      ])
         end
 
-        let!(:other_user) { create(:user) }
-        let!(:other_assigned_tax_return) do
-          create(:gyr_tax_return, :prep_ready_for_prep, assigned_user: other_user)
+        it "returns assigned clients plus greetable clients, but not other user's assigned clients" do
+          result = subject.filtered_and_sorted_clients.to_a
+          expect(result).to include(assigned_tax_return.client)
+          expect(result).to include(greetable_tax_return.client)
+          expect(result).not_to include(other_assigned_tax_return.client)
+        end
+      end
+
+      context "there are not greetable clients" do
+        before do
+          [assigned_tax_return, other_assigned_tax_return].each do |tr|
+            tr.client.update!(filterable_product_year: Rails.configuration.product_year)
+          end
+          SearchIndexer.refresh_filterable_properties([
+                                                        assigned_tax_return.client_id,
+                                                        other_assigned_tax_return.client_id
+                                                      ])
         end
 
-        context "there are greetable clients" do
-          let!(:greetable_tax_return) { create(:gyr_tax_return, :intake_ready) }
-
-          before do
-            [assigned_tax_return, other_assigned_tax_return, greetable_tax_return].each do |tr|
-              tr.client.update!(filterable_product_year: Rails.configuration.product_year)
-            end
-
-            SearchIndexer.refresh_filterable_properties([
-                                                          assigned_tax_return.client_id,
-                                                          other_assigned_tax_return.client_id,
-                                                          greetable_tax_return.client_id
-                                                        ])
-          end
-
-          it "returns assigned clients plus greetable clients, but not other user's assigned clients" do
-            result = subject.filtered_and_sorted_clients.to_a
-
-            expect(result).to include(assigned_tax_return.client)
-            expect(result).to include(greetable_tax_return.client)
-            expect(result).not_to include(other_assigned_tax_return.client)
-          end
-        end
-
-        context "there are not greetable clients" do
-          before do
-            [assigned_tax_return, other_assigned_tax_return].each do |tr|
-              tr.client.update!(filterable_product_year: Rails.configuration.product_year)
-            end
-
-            SearchIndexer.refresh_filterable_properties([
-                                                          assigned_tax_return.client_id,
-                                                          other_assigned_tax_return.client_id
-                                                        ])
-          end
-
-          it "returns assigned clients only (not other user's assigned clients)" do
-            result = subject.filtered_and_sorted_clients.to_a
-
-            expect(result).to match_array([assigned_tax_return.client])
-          end
+        it "returns assigned clients only (not other user's assigned clients)" do
+          expect(subject.filtered_and_sorted_clients.to_a).to match_array([assigned_tax_return.client])
         end
       end
     end
 
     context "default sort order" do
-      let(:user_role) { build :admin_role}
+      let(:user_role) { build :admin_role }
       let(:params) { {} }
       let!(:client_1) { create :client, last_outgoing_communication_at: 2.days.ago, filterable_product_year: Rails.configuration.product_year }
       let!(:client_2) { create :client, last_outgoing_communication_at: 5.days.ago, filterable_product_year: Rails.configuration.product_year }
       let!(:client_3) { create :client, last_outgoing_communication_at: 3.days.ago, filterable_product_year: Rails.configuration.product_year }
 
       it "sorts clients by last_outgoing_communication_at" do
-        expect(subject.filtered_and_sorted_clients).to eq ([client_2, client_3, client_1])
+        expect(subject.filtered_and_sorted_clients).to eq([client_2, client_3, client_1])
       end
     end
 
@@ -96,34 +77,25 @@ RSpec.describe ClientSorter do
         create(:client, filterable_product_year: Rails.configuration.product_year)
       end
 
-      let!(:matching_intake) do
-        create(
-          :intake,
-          client: matching_client,
-          primary_last_name: "Quezada",
-          visitor_id: "v1",
-          product_year: Rails.configuration.product_year
-        )
-      end
-
-      let!(:nonmatching_intake) do
-        create(
-          :intake,
-          client: nonmatching_client,
-          primary_last_name: "Smith",
-          visitor_id: "v2",
-          product_year: Rails.configuration.product_year
-        )
-      end
-
       before do
+        create(:intake,
+               client: matching_client,
+               primary_last_name: "Quezada",
+               visitor_id: "v1",
+               product_year: Rails.configuration.product_year
+        )
+        create(:intake,
+               client: nonmatching_client,
+               primary_last_name: "Smith",
+               visitor_id: "v2",
+               product_year: Rails.configuration.product_year
+        )
         create(:gyr_tax_return, :intake_ready, client: matching_client)
         create(:gyr_tax_return, :intake_ready, client: nonmatching_client)
 
         past = 1.second.ago.to_fs(:db)
-        Intake.where(id: [matching_intake.id, nonmatching_intake.id])
+        Intake.where(client: [matching_client, nonmatching_client])
               .update_all("needs_to_flush_searchable_data_set_at = '#{past}'")
-
         SearchIndexer.refresh_search_index(limit: 10_000)
       end
 
@@ -143,25 +115,11 @@ RSpec.describe ClientSorter do
       end
 
       let!(:matching_client) { create(:client, vita_partner: vita_partner) }
-      let!(:other_client)    { create(:client) }
-
-      let!(:matching_intake) do
-        create(:intake,
-               client: matching_client,
-               product_year: Rails.configuration.product_year,
-               visitor_id: "v1"
-        )
-      end
-
-      let!(:other_intake) do
-        create(:intake,
-               client: other_client,
-               product_year: Rails.configuration.product_year,
-               visitor_id: "v2"
-        )
-      end
+      let!(:other_client) { create(:client) }
 
       before do
+        create(:intake, client: matching_client, product_year: Rails.configuration.product_year, visitor_id: "v1")
+        create(:intake, client: other_client, product_year: Rails.configuration.product_year, visitor_id: "v2")
         create(:gyr_tax_return, :intake_ready, client: matching_client)
         create(:gyr_tax_return, :intake_ready, client: other_client)
 
@@ -183,15 +141,8 @@ RSpec.describe ClientSorter do
 
         let!(:site_client) { create(:client, vita_partner: site) }
 
-        let!(:site_intake) do
-          create(:intake,
-                 client: site_client,
-                 product_year: Rails.configuration.product_year,
-                 visitor_id: "v3"
-          )
-        end
-
         before do
+          create(:intake, client: site_client, product_year: Rails.configuration.product_year, visitor_id: "v3")
           create(:gyr_tax_return, :intake_ready, client: site_client)
           SearchIndexer.refresh_filterable_properties([site_client.id])
         end
@@ -209,23 +160,9 @@ RSpec.describe ClientSorter do
       let!(:online_client) { create(:client) }
       let!(:dropoff_client) { create(:client) }
 
-      let!(:online_intake) do
-        create(:intake,
-               client: online_client,
-               product_year: Rails.configuration.product_year,
-               visitor_id: "v-online"
-        )
-      end
-
-      let!(:dropoff_intake) do
-        create(:intake,
-               client: dropoff_client,
-               product_year: Rails.configuration.product_year,
-               visitor_id: "v-dropoff"
-        )
-      end
-
       before do
+        create(:intake, client: online_client, product_year: Rails.configuration.product_year, visitor_id: "v-online")
+        create(:intake, client: dropoff_client, product_year: Rails.configuration.product_year, visitor_id: "v-dropoff")
         create(:gyr_tax_return, :intake_ready, client: online_client, service_type: "online_intake")
         create(:gyr_tax_return, :intake_ready, client: dropoff_client, service_type: "drop_off")
 
@@ -251,44 +188,12 @@ RSpec.describe ClientSorter do
 
     context "with a selected assigned user id" do
       let(:user_role) { build :admin_role }
-      let(:user) { create :user }
       let(:params) { { assigned_user_id: user.id } }
-      let(:subject) { described_class.new(Client.all, user, params, {}) }
 
-      let!(:assigned_client) { create(:client) }
-      let!(:unassigned_client) { create(:client) }
+      let!(:assigned_client) { create(:client, consented_to_service_at: Time.current) }
+      let!(:unassigned_client) { create(:client, consented_to_service_at: Time.current) }
 
       before do
-        assigned_client.update!(consented_to_service_at: Time.current)
-        unassigned_client.update!(consented_to_service_at: Time.current)
-
-        create(:intake, client: assigned_client, product_year: Rails.configuration.product_year, visitor_id: "v-assigned")
-        create(:intake, client: unassigned_client, product_year: Rails.configuration.product_year, visitor_id: "v-unassigned")
-
-        create(:gyr_tax_return, :intake_ready, client: assigned_client, assigned_user_id: user.id)
-        create(:gyr_tax_return, :intake_ready, client: unassigned_client, assigned_user_id: nil)
-
-        SearchIndexer.refresh_filterable_properties([assigned_client.id, unassigned_client.id])
-      end
-
-      it "returns clients with tax returns assigned to the selected user" do
-        expect(subject.filtered_and_sorted_clients.to_a).to match_array([assigned_client])
-      end
-    end
-
-    context "with a selected assigned user id" do
-      let(:user_role) { build :admin_role }
-      let(:user) { create :user }
-      let(:params) { { assigned_user_id: user.id } }
-      let(:subject) { described_class.new(Client.all, user, params, {}) }
-
-      let!(:assigned_client) { create(:client) }
-      let!(:unassigned_client) { create(:client) }
-
-      before do
-        assigned_client.update!(consented_to_service_at: Time.current)
-        unassigned_client.update!(consented_to_service_at: Time.current)
-
         create(:intake, client: assigned_client, product_year: Rails.configuration.product_year, visitor_id: "v-assigned")
         create(:intake, client: unassigned_client, product_year: Rails.configuration.product_year, visitor_id: "v-unassigned")
 
@@ -305,30 +210,19 @@ RSpec.describe ClientSorter do
 
     context "searching for phone numbers" do
       let(:user_role) { build :admin_role }
-      let(:user) { create :user }
-      let(:subject) { described_class.new(Client.all, user, params, {}) }
 
-      context "with a simple phone number digit-only search" do
-        let(:params) { { search: "4155551212" } }
-
-        it "normalizes the number" do
-          expect(subject.filters[:search]).to eq("+14155551212")
-        end
-      end
-
-      context "with a phone number in a common local format" do
-        let(:params) { { search: "(415) 555-1212" } }
-
-        it "normalizes the number" do
-          expect(subject.filters[:search]).to eq("+14155551212")
-        end
-      end
-
-      context "with a phone number in an unofficial but commonly entered format" do
-        let(:params) { { search: "415.555.1212" } }
-
-        it "normalizes the number" do
-          expect(subject.filters[:search]).to eq("+14155551212")
+      {
+        "simple digit-only" => ["4155551212", "+14155551212"],
+        "common local format" => ["(415) 555-1212", "+14155551212"],
+        "dot-separated format" => ["415.555.1212", "+14155551212"],
+        "mixed query with phone" => ["colleen 415555(1212)", "colleen +14155551212"],
+        "already e.164" => ["colleen +14155551212", "colleen +14155551212"],
+      }.each do |description, (input, expected)|
+        context "with #{description}" do
+          let(:params) { { search: input } }
+          it "normalizes to #{expected}" do
+            expect(subject.filters[:search]).to eq(expected)
+          end
         end
       end
 
@@ -339,80 +233,25 @@ RSpec.describe ClientSorter do
           expect(subject.filters[:search]).to eq("555-1212")
         end
       end
-
-      context "with a phone number and another field in the search query" do
-        let(:params) { { search: "colleen 415555(1212)" } }
-
-        it "normalizes just the phone substring" do
-          expect(subject.filters[:search]).to eq("colleen +14155551212")
-        end
-      end
-
-      context "with a phone number in e.164 international format" do
-        let(:params) { { search: "colleen +14155551212" } }
-
-        it "leaves it as-is" do
-          expect(subject.filters[:search]).to eq("colleen +14155551212")
-        end
-      end
     end
   end
 
   describe "#has_search_and_sort_params?" do
-    let(:user) { create(:user) }
-    let(:subject) { described_class.new(Client.all, user, params, {}) }
     context "when containing a sort or search param" do
-      context "search" do
-        let(:params) { { search: "que" } }
-        it "returns true" do
-          expect(subject.has_search_and_sort_params?).to eq true
-        end
-      end
-
-      context "status" do
-        let(:params) { { status: "prep_ready_for_prep" } }
-        it "returns true" do
-          expect(subject.has_search_and_sort_params?).to eq true
-        end
-      end
-
-      context "unassigned" do
-        let(:params) { { unassigned: true } }
-        it "returns true" do
-          expect(subject.has_search_and_sort_params?).to eq true
-        end
-      end
-
-      context "assigned_to_me" do
-        let(:params) { { assigned_to_me: true } }
-        it "returns true" do
-          expect(subject.has_search_and_sort_params?).to eq true
-        end
-      end
-
-      context "flagged_at" do
-        let(:params) { { flagged: true } }
-        it "returns true" do
-          expect(subject.has_search_and_sort_params?).to eq true
-        end
-      end
-
-      context "year" do
-        let(:params) { { year: 2019 } }
-        it "returns true" do
-          expect(subject.has_search_and_sort_params?).to eq true
-        end
-      end
-
-      context "vita_partners" do
-        let(:params) do
-          {
-            vita_partners: [{ id: 1, name: "Partner name", value: 1 }].to_json
-          }
-        end
-
-        it "returns true" do
-          expect(subject.has_search_and_sort_params?).to eq true
+      {
+        search: "que",
+        status: "prep_ready_for_prep",
+        unassigned: true,
+        assigned_to_me: true,
+        flagged: true,
+        year: 2019,
+        vita_partners: [{ id: 1, name: "Partner name", value: 1 }].to_json,
+      }.each do |param_name, param_value|
+        context "with #{param_name}" do
+          it "returns true" do
+            sorter = described_class.new(Client.all, user, { param_name => param_value }, {})
+            expect(sorter.has_search_and_sort_params?).to eq true
+          end
         end
       end
     end
