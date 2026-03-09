264fd13f6977759e68d75f739e0d7e9e025133f7
Em Barnard-Shao <ebarnard@codeforamerica.org>
2026-03-09 14:31:03 -0400

    Fix utm tracking issue

--- app/controllers/redirects_controller.rb
+++ app/controllers/redirects_controller.rb
@@ -1,7 +1,8 @@
 class RedirectsController < ApplicationController
   def outreach
+    # for SMS only b/c utm_medium is defined as 'sms'
     redirect_to(
-      root_url(locale: params[:locale], utm_source: "gyr", utm_medium: "sms", utm_campaign: "w1"),
+      root_url(locale: params[:locale].presence || I18n.default_locale, utm_source: "gyr", utm_medium: "sms", utm_campaign: "w1"),
       allow_other_host: true
     )
   end
