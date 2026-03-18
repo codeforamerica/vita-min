a04147b95842e29a35a11a6dc8a86d64f5e6beee
Em Barnard-Shao <ebarnard@codeforamerica.org>
2026-03-17 22:34:45 -0400

    annotations

--- spec/factories/verification_emails.rb
+++ spec/factories/verification_emails.rb
@@ -4,14 +4,12 @@
 #
 #  id                    :bigint           not null, primary key
 #  mailgun_status        :string
-#  client_id             :bigint
 #  email_access_token_id :bigint           not null
 #  mailgun_id            :string
 #  visitor_id            :string           not null
 #
 # Indexes
 #
-#  index_email_login_requests_on_client_id              (client_id)
 #  index_email_login_requests_on_email_access_token_id  (email_access_token_id)
 #  index_email_login_requests_on_mailgun_id             (mailgun_id)
 #  index_email_login_requests_on_visitor_id             (visitor_id)
