f7ed114b81f8be072147d15b729b8da5f47b9e3a
spompea-cfa <170136902+spompea-cfa@users.noreply.github.com>
2026-04-07 19:54:41 -0400

    fix

--- app/controllers/questions/questions_controller.rb
+++ app/controllers/questions/questions_controller.rb
@@ -53,7 +53,7 @@ module Questions
     end
 
     def has_unsure_option?
-      layout = self.send :_layout, self.lookup_context, [""]
+      layout = self.send :_layout, self.lookup_context, [""], nil
       return false unless layout == "yes_no_question"
       enum_options = parent_class.try(method_name.pluralize)
       enum_options&.has_key?("unsure")
