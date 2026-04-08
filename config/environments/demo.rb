49902a102d048bd660f626adbbc0b4bb84d2bd10
Em Barnard-Shao <ebarnard@codeforamerica.org>
2026-04-08 15:37:09 -0400

    Allow host to resolve stricter host configuration settings with upgrade

--- config/environments/demo.rb
+++ config/environments/demo.rb
@@ -30,6 +30,9 @@ Rails.application.configure do
   config.action_mailer.asset_host = config.gyr_url
   config.hide_ctc = false
 
+
   Rails.application.default_url_options = config.action_mailer.default_url_options
   config.efile_environment = "test"
+
+  config.hosts << "demo.getyourrefund.org"
 end
