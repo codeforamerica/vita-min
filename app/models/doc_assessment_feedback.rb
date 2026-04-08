a3e37c11d3d7907f4689e15d07a7105e7a1d660a
spompea-cfa <170136902+spompea-cfa@users.noreply.github.com>
2026-04-08 16:24:56 -0400

    first take

--- app/models/doc_assessment_feedback.rb
+++ app/models/doc_assessment_feedback.rb
@@ -27,4 +27,23 @@ class DocAssessmentFeedback < ApplicationRecord
   enum feedback: { unfilled: 0, correct: 1, incorrect: 2 }, _prefix: :feedback
 
   validates :feedback, presence: true
+
+  after_commit :post_processing
+
+  private
+
+  def post_processing
+    update_document_display_name if feedback_correct?
+  end
+
+  def update_document_display_name
+    doc = doc_assessment.document
+    name = doc.document_type
+    tally = Document.where({intake_id: doc.intake_id,
+                            document_type: doc.document_type}).count
+    if tally > 1
+      name += ' ' + tally.to_s
+    end
+    doc.update!(display_name: name)
+  end
 end
