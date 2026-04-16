abc3b0e7d1fa945c8696f9e5575f555d4a2b9699
em barnard <49880002+embarnard@users.noreply.github.com>
2026-04-16 17:53:43 -0400

    Fix ITIN regex validation  (#6269)

--- app/validators/individual_taxpayer_identification_number_validator.rb
+++ app/validators/individual_taxpayer_identification_number_validator.rb
@@ -1,8 +1,12 @@
 class IndividualTaxpayerIdentificationNumberValidator < ActiveModel::EachValidator
-  ITIN_REGEX = /\A(?=(9))\d{3}-?([7]\d|8[0-8]|9([0-2]|[4-9]))-?\d{4}\z/
+  ITIN_REGEX = /\A9\d{2}(5\d|6[0-5]|7\d|8[0-8]|9[0-2]|9[4-9])\d{4}\z/
 
   def validate_each(record, attr_name, value)
-    unless ITIN_REGEX.match?(value)
+    return if value.blank?
+
+    normalized_value = value.to_s.gsub(/\D/, '')
+
+    unless ITIN_REGEX.match?(normalized_value)
       record.errors.add(attr_name, I18n.t("validators.itin"))
     end
   end
