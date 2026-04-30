17bdb957d71b7f1b13b8d95ad13e45834b813568
spompea-cfa <170136902+spompea-cfa@users.noreply.github.com>
2026-04-30 18:55:41 -0400

    tidy

--- app/jobs/refresh_caches_job.rb
+++ app/jobs/refresh_caches_job.rb
@@ -2,9 +2,8 @@ class RefreshCachesJob < ApplicationJob
   # Each class/item in the MANIFEST needs to have a
   # public method named `refresh_cache`; from there, the
   # internal impl. is up to the maintainer of that class.
-
   MANIFEST = [
-    AiScreenerMetricsService ]
+    AiScreenerMetricsService]
 
   def perform
     MANIFEST.map(&:refresh_cache)
