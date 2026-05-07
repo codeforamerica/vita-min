d64c21c16ff6b1c21d061bafd3d0928fd3cb76c3
spompea-cfa <170136902+spompea-cfa@users.noreply.github.com>
2026-05-07 18:41:09 -0400

    fix

--- config/initializers/rack_profiler.rb
+++ config/initializers/rack_profiler.rb
@@ -1,7 +1,9 @@
-Rack::MiniProfiler.config.start_hidden = true if Rails.env.demo?
-Rack::MiniProfiler.config.toggle_shortcut = 'CTRL+Y'
+if defined?(Rack::MiniProfiler)
+  Rack::MiniProfiler.config.start_hidden = true if Rails.env.demo?
+  Rack::MiniProfiler.config.toggle_shortcut = 'CTRL+Y'
 
-if ENV['REDIS_URL'].present? && (Rails.env.demo? || Rails.env.development? || Rails.env.heroku? || Rails.env.staging?)
-  Rack::MiniProfiler.config.storage_options = { url: ENV["REDIS_URL"] }
-  Rack::MiniProfiler.config.storage = Rack::MiniProfiler::RedisStore
+  if ENV['REDIS_URL'].present? && (Rails.env.demo? || Rails.env.development? || Rails.env.heroku? || Rails.env.staging?)
+    Rack::MiniProfiler.config.storage_options = { url: ENV["REDIS_URL"] }
+    Rack::MiniProfiler.config.storage = Rack::MiniProfiler::RedisStore
+  end
 end
