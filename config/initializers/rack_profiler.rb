a9951997745737794f306e77119973b47706f543
spompea-cfa <170136902+spompea-cfa@users.noreply.github.com>
2026-05-07 18:22:04 -0400

    ok

--- config/initializers/rack_profiler.rb
+++ config/initializers/rack_profiler.rb
@@ -1,7 +1,5 @@
-if Rails.env.heroku? || Rails.env.demo?
-  # Use the "Alt+P" keyboard shortcut to toggle visibility
-  Rack::MiniProfiler.config.start_hidden = true
-end
+Rack::MiniProfiler.config.start_hidden = true if Rails.env.demo?
+Rack::MiniProfiler.config.toggle_shortcut = 'CTRL+Y'
 
 if ENV['REDIS_URL'].present? && (Rails.env.demo? || Rails.env.development? || Rails.env.heroku? || Rails.env.staging?)
   Rack::MiniProfiler.config.storage_options = { url: ENV["REDIS_URL"] }
