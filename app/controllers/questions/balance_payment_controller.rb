5814237dffbe258c35fe1309278e384b4a213836
spompea-cfa <170136902+spompea-cfa@users.noreply.github.com>
2026-05-13 19:38:46 -0400

    update balance payment screen

--- app/controllers/questions/balance_payment_controller.rb
+++ app/controllers/questions/balance_payment_controller.rb
@@ -2,14 +2,12 @@ module Questions
   class BalancePaymentController < QuestionsController
     include AuthenticatedClientConcern
 
+    layout 'yes_no_question'
+
     private
 
     def method_name
       "balance_pay_from_bank"
-    end
-
-    def form_params
-      params.fetch(:balance_payment_form, {}).permit(:balance_payment_choice, :balance_pay_from_bank, :payment_in_installments)
     end
   end
 end
