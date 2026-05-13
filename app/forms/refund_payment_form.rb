37505b8b6f98aae660521318ed25d0a20c7280de
spompea-cfa <170136902+spompea-cfa@users.noreply.github.com>
2026-05-12 21:17:59 -0400

    fix

--- app/forms/refund_payment_form.rb
+++ app/forms/refund_payment_form.rb
@@ -5,6 +5,7 @@ class RefundPaymentForm < QuestionsForm
                      :refund_check_by_mail
 
   def save
+    refund_direct_deposit = attributes_for(:intake)[:refund_direct_deposit]
     intake.update(
       refund_direct_deposit: refund_direct_deposit,
       refund_check_by_mail: refund_direct_deposit == 'yes' ? 'no' : 'yes',
