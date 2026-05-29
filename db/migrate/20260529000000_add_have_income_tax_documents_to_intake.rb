abb4b7c2583f65e4012499eb585ec1d4c8c71fa9
spompea-cfa <170136902+spompea-cfa@users.noreply.github.com>
2026-05-29 02:01:19 -0400

    tweak

--- db/migrate/20260529000000_add_have_income_tax_documents_to_intake.rb
+++ db/migrate/20260529000000_add_have_income_tax_documents_to_intake.rb
@@ -1,4 +1,4 @@
-class AddHaveIncomeTaxDocumentsToIntake < ActiveRecord::Migration[7.1]
+class AddHaveIncomeTaxDocumentsToIntake < ActiveRecord::Migration[7.2]
   def change
     add_column :intakes, :have_income_tax_documents, :integer, default: 0
   end
