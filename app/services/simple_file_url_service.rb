20c74c4c40206cea97c8ee3ca54608d0a39ef81a
em barnard <49880002+embarnard@users.noreply.github.com>
2026-07-27 14:29:53 -0400

    Direct homepage simple file link to the welcome page (#6370)

--- app/services/simple_file_url_service.rb
+++ app/services/simple_file_url_service.rb
@@ -1,42 +1,42 @@
 class SimpleFileUrlService
   SOURCES = %w[gyrsel gyrhomepage].freeze
-
-  STATE_CODES = {
-    "CO" => "co",
-    "NJ" => "nj"
-  }.freeze
-
+  SUPPORTED_STATES = %w[CO NJ].freeze
   SUPPORTED_LOCALES = %w[en es].freeze
 
   attr_reader :intake, :locale, :source
 
-  def initialize(intake:, locale:, source:)
+  def initialize(intake:, locale:, source: "")
     @intake = intake
     @locale = locale.to_s
     @source = source.to_s
   end
 
   def url
-    uri = URI.join(
-      normalized_base_url,
-      "#{supported_locale}/service-selection/recommendation/simplefile"
-    )
+    build_url("service-selection/recommendation/simplefile", state_code: state_code, source: filtered_source)
+  end
 
-    query_params = { state_code: state_code, source: supported_source }.compact
-
-    uri.query = query_params.to_query if query_params.any?
-
-    uri.to_s
+  def welcome_url
+    build_url("welcome", source: filtered_source)
   end
 
   private
 
+  def build_url(path, params = {})
+    uri = localized_uri(path)
+    query_params = params.compact
+    uri.query = query_params.to_query if query_params.any?
+    uri.to_s
+  end
+
+  def localized_uri(path)
+    URI.join(normalized_base_url, "#{supported_locale}/#{path}")
+  end
+
   def normalized_base_url
     base_url = Rails.configuration.simple_file_url
-
     raise "Simple File URL is not configured" if base_url.blank?
 
-    "#{base_url.chomp("/")}/"
+    "#{base_url.chomp('/')}/"
   end
 
   def supported_locale
@@ -45,11 +45,12 @@ class SimpleFileUrlService
     I18n.default_locale.to_s
   end
 
-  def supported_source
+  def filtered_source
     source if source.in?(SOURCES)
   end
 
   def state_code
-    STATE_CODES[intake&.state_of_residence]
+    state = intake&.state_of_residence
+    state.downcase if state.in?(SUPPORTED_STATES)
   end
 end
