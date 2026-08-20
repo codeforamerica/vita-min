31d2eb0ed7e66a836c10a66dd067ba81b8c774ae
Em Barnard-Shao <ebarnard@codeforamerica.org>
2026-08-20 14:28:07 -0400

    Annotate

--- app/models/bulk_client_flag_update.rb
+++ app/models/bulk_client_flag_update.rb
@@ -1,3 +1,21 @@
+# == Schema Information
+#
+# Table name: bulk_client_flag_updates
+#
+#  id                      :bigint           not null, primary key
+#  enabled                 :boolean          not null
+#  created_at              :datetime         not null
+#  updated_at              :datetime         not null
+#  tax_return_selection_id :bigint           not null
+#
+# Indexes
+#
+#  index_bulk_client_flag_updates_on_tax_return_selection_id  (tax_return_selection_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (tax_return_selection_id => tax_return_selections.id)
+#
 class BulkClientFlagUpdate < ApplicationRecord
   has_one :user_notification, as: :notifiable, dependent: :destroy
   belongs_to :tax_return_selection
--- spec/factories/bulk_client_flag_updates.rb
+++ spec/factories/bulk_client_flag_updates.rb
@@ -1,3 +1,21 @@
+# == Schema Information
+#
+# Table name: bulk_client_flag_updates
+#
+#  id                      :bigint           not null, primary key
+#  enabled                 :boolean          not null
+#  created_at              :datetime         not null
+#  updated_at              :datetime         not null
+#  tax_return_selection_id :bigint           not null
+#
+# Indexes
+#
+#  index_bulk_client_flag_updates_on_tax_return_selection_id  (tax_return_selection_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (tax_return_selection_id => tax_return_selections.id)
+#
 FactoryBot.define do
   factory :bulk_client_flag_update do
     tax_return_selection
