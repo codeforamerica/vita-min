9be3f927f13bebd51a2f23c0b3952638de21796c
spompea-cfa <170136902+spompea-cfa@users.noreply.github.com>
2026-09-10 20:25:05 -0400

    first take

--- app/controllers/hub/documents_controller.rb
+++ app/controllers/hub/documents_controller.rb
@@ -92,10 +92,16 @@ module Hub
       @document_type_options = [DocumentTypes::Identity, DocumentTypes::SsnItin] + (DocumentTypes::ALL_TYPES - DocumentTypes::IDENTITY_TYPES - DocumentTypes::SECONDARY_IDENTITY_TYPES)
     end
 
+    def sort_by_tax_year
+      @documents.except(:order).includes(:tax_return).order('tax_return.year' => @sort_order)
+    end
+
     def sorted_documents
       @sort_order = sort_order
       @sort_column = sort_column
-      @documents.except(:order).order({ @sort_column => @sort_order })
+      @sort_column == 'tax_return' ?
+        sort_by_tax_year :
+        @documents.except(:order).order({ @sort_column => @sort_order })
     end
 
     def log_document_access!
@@ -114,7 +120,7 @@ module Hub
     end
 
     def sort_column
-      %w[created_at display_name document_type].include?(params[:column]) ? params[:column] : "document_type"
+      %w[created_at display_name document_type tax_return].include?(params[:column]) ? params[:column] : "document_type"
     end
 
     def sort_order
