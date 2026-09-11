440154d21b9e6b673289d0be5c4182d7a6ce51a7
Joan Nonon <jnonon@codeforamerica.org>
2026-09-11 16:42:06 -0400

    Removed resources :ctc_intake_capacity

--- config/routes.rb
+++ config/routes.rb
@@ -391,7 +391,6 @@ Rails.application.routes.draw do
         resources :tools, only: [:index]
         resources :admin_tools, only: [:index]
         resources :state_file_admin_tools, only: [:index]
-        resources :ctc_intake_capacity, only: [:index, :create]
         resources :admin_toggles, only: [:index, :create]
         get "/profile" => "users#profile", as: :user_profile
         patch "/profile/notification_preferences" => "users#update_notification_preferences", as: :update_notification_preferences
