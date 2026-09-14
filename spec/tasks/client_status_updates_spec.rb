494bdf3c49a4ea7304d133840a4165f630881b40
Drew Proebstel <d.proebstel@gmail.com>
2026-09-14 10:47:14 -0700

    remove ctc experience survey (#6422)
    Co-authored-by: Drew Proebstel <dproebstel@codeforamerica.org>

--- spec/tasks/client_status_updates_spec.rb
+++ spec/tasks/client_status_updates_spec.rb
@@ -5,31 +5,6 @@ describe "client_status_updates:send_completion_surveys" do
 
   around { |example| capture_output { example.run } }
 
-  describe "AutomatedMessage::CtcExperienceSurvey" do
-    let!(:client) do
-      Timecop
-        .freeze(25.hours.ago) do
-          create :ctc_tax_return,
-                 status,
-                 client:
-                   build(
-                     :client,
-                     ctc_experience_survey_sent_at: nil,
-                     intake:
-                       build(:ctc_intake, primary_consented_to_service: "yes")
-                   )
-        end
-        .client
-    end
-    let(:status) { :file_accepted }
-
-    it "enqueues surveys" do
-      expect { task.invoke }.to have_enqueued_job(
-        SendClientCtcExperienceSurveyJob
-      ).with(client)
-    end
-  end
-
   describe "AutomatedMessage::CompletionSurvey" do
     let(:completion_survey_sent_at) { nil }
     let!(:client) do
